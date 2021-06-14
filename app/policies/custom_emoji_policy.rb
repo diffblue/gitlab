3f6864207f176fc5dac407f33cdb8e8d34c539f1
Rajendra Kadam <rkadam@gitlab.com>
2021-06-14 20:47:44 +0530

    Add delete GraphQL api for custom emoji
    Add specs and update custom emoji access policy
    
    Update gql docs

--- app/policies/custom_emoji_policy.rb
+++ app/policies/custom_emoji_policy.rb
@@ -2,4 +2,12 @@
 
 class CustomEmojiPolicy < BasePolicy
   delegate { @subject.group }
+
+  condition(:admin_custom_emoji) do
+    @subject.group.member?(@user, Gitlab::Access::MAINTAINER) || @subject.creator == @user
+  end
+
+  rule { admin_custom_emoji }.policy do
+    enable :delete_custom_emoji
+  end
 end
