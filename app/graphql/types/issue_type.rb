67ebd3eea7e70ae6229bbedc966ffe0e4da7a4da
Eulyeon Ko <eko@gitlab.com>
2021-07-12 11:36:03 +0900

    Expose mergeRequestCounts for Issue GraphQL type
    Changelog: changed

--- app/graphql/types/issue_type.rb
+++ app/graphql/types/issue_type.rb
@@ -60,6 +60,8 @@ module Types
           description: 'Number of upvotes the issue has received.'
     field :downvotes, GraphQL::Types::Int, null: false,
           description: 'Number of downvotes the issue has received.'
+    field :merge_requests_count, GraphQL::INT_TYPE, null: false,
+          description: 'Number of merge requests that close the issue on merge.'
     field :user_notes_count, GraphQL::Types::Int, null: false,
           description: 'Number of user notes of the issue.',
           resolver: Resolvers::UserNotesCountResolver
