3f3ff664ba2cea8cbaacb6f3b240b302439ffaf6
Vitaly Slobodin <vslobodin@gitlab.com>
2021-07-13 12:35:09 +0300

    Add new entry point for extending/reactivating trial button

--- ee/app/assets/javascripts/pages/groups/billings/index.js
+++ ee/app/assets/javascripts/pages/groups/billings/index.js
@@ -1,7 +1,9 @@
 import { shouldQrtlyReconciliationMount } from 'ee/billings/qrtly_reconciliation';
 import initSubscriptions from 'ee/billings/subscriptions';
+import { shouldExtendReactivateTrialButtonMount } from 'ee/trials/extend_reactivate_trial';
 import PersistentUserCallout from '~/persistent_user_callout';
 
 PersistentUserCallout.factory(document.querySelector('.js-gold-trial-callout'));
 initSubscriptions();
+shouldExtendReactivateTrialButtonMount();
 shouldQrtlyReconciliationMount();
