2b5ac0a8d5e242c03ac0eafc76e59ea8383a32ca
Jonathan Schafer <jschafer@gitlab.com>
2021-07-13 15:19:56 -0500

    Fixed association between Finding, Finding Links
    Changelog: fixed
    EE: true

--- ee/app/models/vulnerabilities/finding_link.rb
+++ ee/app/models/vulnerabilities/finding_link.rb
@@ -4,7 +4,7 @@ module Vulnerabilities
   class FindingLink < ApplicationRecord
     self.table_name = 'vulnerability_finding_links'
 
-    belongs_to :finding, class_name: 'Vulnerabilities::Finding', inverse_of: :finding_identifiers, foreign_key: 'vulnerability_occurrence_id'
+    belongs_to :finding, class_name: 'Vulnerabilities::Finding', inverse_of: :finding_links, foreign_key: 'vulnerability_occurrence_id'
 
     validates :finding, presence: true
     validates :url, presence: true, length: { maximum: 255 }
