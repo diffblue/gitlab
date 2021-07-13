5179afecaea91a7e68b5b8994dd9c98340cbba02
Justin Ho <hduong@gitlab.com>
2021-07-13 16:18:11 +0700

    Update urlParams on Jira issues list page
    When already set to the default values, we don't need to
    include params like page, sort, state in urlParams.
    
    This prevents the URL to flash and update on load from
    /-/integrations/jira/issues to
    /-/integrations/jira/issues?page=1&sort=created_desc&state=opened
    
    Changelog: changed
    EE: true

--- ee/app/assets/javascripts/integrations/jira/issues_list/components/jira_issues_list_root.vue
+++ ee/app/assets/javascripts/integrations/jira/issues_list/components/jira_issues_list_root.vue
@@ -74,10 +74,10 @@ export default {
     urlParams() {
       return {
         'labels[]': this.filterParams.labels,
-        page: this.currentPage,
         search: this.filterParams.search,
-        sort: this.sortedBy,
-        state: this.currentState,
+        ...(this.currentPage === 1 ? {} : { page: this.currentPage }),
+        ...(this.sortedBy === this.initialSortBy ? {} : { sort: this.sortedBy }),
+        ...(this.currentState === this.initialState ? {} : { state: this.currentState }),
       };
     },
   },
