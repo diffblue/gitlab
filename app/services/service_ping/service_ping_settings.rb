41a46db7f32197180c53dfdf02717411851bf24a
Rajendra Kadam <rkadam@gitlab.com>
2021-07-15 10:04:52 +0530

    Add service ping setting module specs

---
+++ app/services/service_ping/service_ping_settings.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module ServicePing
+  module ServicePingSettings
+    class << self
+      def product_intelligence_enabled?
+        pings_enabled? && !User.single_user&.requires_usage_stats_consent?
+      end
+
+      def pings_enabled?
+        ::Gitlab::CurrentSettings.usage_ping_enabled?
+      end
+    end
+  end
+end
