a446cd93ee9ac8fb188210727f7969f56640b937
Jonathan Schafer <jschafer@gitlab.com>
2021-07-19 14:24:36 -0500

    Add request and response to SupportingMessage
    Includes change to allow requests and responses to not reference
    evidence
    
    Changelog: changed

--- ee/app/models/vulnerabilities/finding/evidence/response.rb
+++ ee/app/models/vulnerabilities/finding/evidence/response.rb
@@ -8,8 +8,19 @@ module Vulnerabilities
 
         self.table_name = 'vulnerability_finding_evidence_responses'
 
-        belongs_to :evidence, class_name: 'Vulnerabilities::Finding::Evidence', inverse_of: :response, foreign_key: 'vulnerability_finding_evidence_id', optional: false
-        has_many :headers, class_name: 'Vulnerabilities::Finding::Evidence::Header', inverse_of: :response, foreign_key: 'vulnerability_finding_evidence_response_id'
+        belongs_to :evidence,
+                   class_name: 'Vulnerabilities::Finding::Evidence',
+                   inverse_of: :response,
+                   foreign_key: 'vulnerability_finding_evidence_id'
+        belongs_to :supporting_message,
+                   class_name: 'Vulnerabilities::Finding::Evidence::SupportingMessage',
+                   inverse_of: :response,
+                   foreign_key: 'vulnerability_finding_evidence_supporting_message_id'
+
+        has_many :headers,
+                 class_name: 'Vulnerabilities::Finding::Evidence::Header',
+                 inverse_of: :response,
+                 foreign_key: 'vulnerability_finding_evidence_response_id'
 
         validates :reason_phrase, length: { maximum: 2048 }
       end
