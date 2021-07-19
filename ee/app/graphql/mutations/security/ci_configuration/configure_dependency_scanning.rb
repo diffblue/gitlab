8bc0ef8611a6393f7f93cb9ae87c6e552e6e5ba4
Adam Cohen <acohen@gitlab.com>
2021-07-19 12:39:06 +0000

    Add ConfigureSecurityAnalyzer superclass [RUN AS-IF-FOSS]

--- ee/app/graphql/mutations/security/ci_configuration/configure_dependency_scanning.rb
+++ ee/app/graphql/mutations/security/ci_configuration/configure_dependency_scanning.rb
@@ -3,9 +3,7 @@
 module Mutations
   module Security
     module CiConfiguration
-      class ConfigureDependencyScanning < BaseMutation
-        include FindsProject
-
+      class ConfigureDependencyScanning < BaseSecurityAnalyzer
         graphql_name 'ConfigureDependencyScanning'
         description <<~DESC
           Configure Dependency Scanning for a project by enabling Dependency Scanning in a new or modified
@@ -13,33 +11,8 @@ module Mutations
           create a Merge Request are a part of the response.
         DESC
 
-        argument :project_path, GraphQL::ID_TYPE,
-          required: true,
-          description: 'Full path of the project.'
-
-        field :success_path, GraphQL::STRING_TYPE, null: true,
-          description: 'Redirect path to use when the response is successful.'
-
-        field :branch, GraphQL::STRING_TYPE, null: true,
-          description: 'Branch that has the new/modified `.gitlab-ci.yml` file.'
-
-        authorize :push_code
-
-        def resolve(project_path:)
-          project = authorized_find!(project_path)
-
-          result = ::Security::CiConfiguration::DependencyScanningCreateService.new(project, current_user).execute
-          prepare_response(result)
-        end
-
-        private
-
-        def prepare_response(result)
-          {
-            branch: result.payload[:branch],
-            success_path: result.payload[:success_path],
-            errors: result.errors
-          }
+        def configure_analyzer(project, **_args)
+          ::Security::CiConfiguration::DependencyScanningCreateService.new(project, current_user).execute
         end
       end
     end
