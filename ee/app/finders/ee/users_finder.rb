cfa3e79ebc86e53b8de8f1090c524ec72db14f5b
Jackie Fraser <jfraser@gitlab.com>
2021-07-20 20:39:55 +0000

    Add SAML provider search to UsersFinder

--- ee/app/finders/ee/users_finder.rb
+++ ee/app/finders/ee/users_finder.rb
@@ -6,13 +6,21 @@ module EE
 
     override :execute
     def execute
-      by_non_ldap(super)
+      users = by_non_ldap(super)
+      by_saml_provider_id(users)
     end
 
     def by_non_ldap(users)
       return users unless params[:skip_ldap]
 
       users.non_ldap
+    end
+
+    def by_saml_provider_id(users)
+      saml_provider_id = params[:by_saml_provider_id]
+      return users unless saml_provider_id
+
+      users.limit_to_saml_provider(saml_provider_id)
     end
   end
 end
--- ee/spec/finders/users_finder_spec.rb
+++ ee/spec/finders/users_finder_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe UsersFinder do
 
     context 'with a normal user' do
       context 'with LDAP users' do
-        let!(:ldap_user) { create(:omniauth_user, provider: 'ldap') }
+        let_it_be(:ldap_user) { create(:omniauth_user, provider: 'ldap') }
 
         it 'returns ldap users by default' do
           users = described_class.new(normal_user).execute
@@ -20,6 +20,29 @@ RSpec.describe UsersFinder do
           users = described_class.new(normal_user, skip_ldap: true).execute
 
           expect(users).to contain_exactly(normal_user, blocked_user, omniauth_user, external_user, internal_user, admin_user)
+        end
+      end
+
+      context 'with SAML users' do
+        let_it_be(:group) { create(:group) }
+        let_it_be(:saml_provider) { create(:saml_provider, group: group, enabled: true, enforced_sso: true) }
+        let_it_be(:saml_user) { create(:user) }
+        let_it_be(:non_saml_user) { create(:user) }
+
+        before do
+          create(:identity, provider: 'group_saml1', saml_provider_id: saml_provider.id, user: saml_user)
+        end
+
+        it 'returns all users by default' do
+          users = described_class.new(normal_user).execute
+
+          expect(users).to contain_exactly(normal_user, blocked_user, omniauth_user, external_user, internal_user, admin_user, saml_user, non_saml_user)
+        end
+
+        it 'returns only saml users from the provided saml_provider_id' do
+          users = described_class.new(normal_user, by_saml_provider_id: saml_provider.id).execute
+
+          expect(users).to contain_exactly(saml_user)
         end
       end
     end
