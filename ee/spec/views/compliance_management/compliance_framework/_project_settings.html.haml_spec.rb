c6582d8ca858aa505b7d94984f10b21839cee593
Robert Hunt <rhunt@gitlab.com>
2021-07-20 11:05:45 +0100

    Move the compliance framework selector to a new expandable section
    This moves the compliance framework selector from the Projects top
    settings section to its own dedicated expandable section just after
    badges. This work does not change the functionality of the compliance
    framework selector other than moving it.
    
    Changelog: changed
    MR: https://gitlab.com/gitlab-org/gitlab/-/merge_requests/66429
    EE: true

--- ee/spec/views/compliance_management/compliance_framework/_project_settings.html.haml_spec.rb
+++ ee/spec/views/compliance_management/compliance_framework/_project_settings.html.haml_spec.rb
@@ -2,16 +2,23 @@
 
 require 'spec_helper'
 
-RSpec.describe 'projects/edit.html.haml' do
+RSpec.describe 'compliance_management/compliance_framework/_project_settings.html.haml' do
   let_it_be(:group) { create(:group) }
   let_it_be(:group_owner) { create(:user) }
   let_it_be(:project) { create(:project, namespace: group) }
 
   before do
     allow(view).to receive(:current_user).and_return(group_owner)
+    allow(view).to receive(:expanded).and_return(true)
     allow(group_owner).to receive(:can?).and_return(true)
     assign(:project, project)
     stub_licensed_features(custom_compliance_frameworks: true)
+  end
+
+  it 'shows the section description' do
+    render
+
+    expect(rendered).to have_text 'Select a framework that applies to this project. How are these added?'
   end
 
   context 'group has compliance frameworks' do
@@ -20,20 +27,13 @@ RSpec.describe 'projects/edit.html.haml' do
     it 'includes a dropdown including that framework' do
       render
 
-      expect(rendered).to match /Custom framework 23/
+      expect(rendered).to have_select('project[compliance_framework_setting_attributes][framework]', with_options: ['Custom framework 23'])
     end
 
-    it 'does not include warning message' do
+    it 'shows the submit button' do
       render
 
-      expect(rendered).not_to match /Customizable by owners./
-    end
-
-    it 'contains the dropdown' do
-      render
-
-      expect(rendered).to have_css('select[id=project_compliance_framework_setting_attributes_framework]')
-      expect(rendered).not_to have_css('select[id=project_compliance_framework_setting_attributes_framework][disabled="disabled"]')
+      expect(rendered).to have_button('Save changes')
     end
 
     context 'user is group maintainer' do
@@ -44,16 +44,20 @@ RSpec.describe 'projects/edit.html.haml' do
         allow(view).to receive(:current_user).and_return(maintainer)
       end
 
-      it 'includes warning message' do
+      it 'shows the no permissions text' do
         render
 
-        expect(rendered).to match /Customizable by owners./
+        expect(rendered).to have_text('Customizable by owners.')
       end
 
       it 'disables the dropdown' do
         render
 
         expect(rendered).to have_css('input[id=project_compliance_framework_setting_attributes_framework][disabled="disabled"]')
+      end
+
+      it 'hides the submit button' do
+        expect(rendered).not_to have_button('Save changes')
       end
     end
   end
@@ -63,10 +67,33 @@ RSpec.describe 'projects/edit.html.haml' do
       group.compliance_management_frameworks.delete_all
     end
 
-    it 'shows a notification' do
+    it 'shows the empty text' do
       render
 
       expect(rendered).to match /No compliance frameworks are in use. Create one from the .* section in Group Settings./
+    end
+
+    it 'hides the submit button' do
+      expect(rendered).not_to have_button('Save changes')
+    end
+
+    context 'user is group maintainer' do
+      let_it_be(:maintainer) { create(:user) }
+
+      before do
+        group.add_maintainer(maintainer)
+        allow(view).to receive(:current_user).and_return(maintainer)
+      end
+
+      it 'shows the empty text' do
+        render
+
+        expect(rendered).to have_text('No compliance frameworks are in use.')
+      end
+
+      it 'hides the submit button' do
+        expect(rendered).not_to have_button('Save changes')
+      end
     end
   end
 end
