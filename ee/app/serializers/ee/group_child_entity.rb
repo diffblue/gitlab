78c8851d4403f878076c698a6b6cc528db019ff3
Robert Hunt <rhunt@gitlab.com>
2021-07-26 14:42:27 +0100

    Add the compliance framework label to group projects listing
    - Add compliance framework to entity
    - Add compliance framework label component to group item
    - Add compliance framework to store
    - Create tests
    
    Changelog: added
    MR: https://gitlab.com/gitlab-org/gitlab/-/merge_requests/66819
    EE: true

--- ee/app/serializers/ee/group_child_entity.rb
+++ ee/app/serializers/ee/group_child_entity.rb
@@ -9,6 +9,16 @@ module EE
       expose :marked_for_deletion do |instance|
         instance.marked_for_deletion?
       end
+
+      expose :compliance_management_framework, if: lambda { |_instance, _options| compliance_framework_available? }
+    end
+
+    private
+
+    def compliance_framework_available?
+      return unless project?
+
+      object.licensed_feature_available?(:compliance_framework)
     end
   end
 end
