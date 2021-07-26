52f633930a5b30a1fa491b00463e6d9092062846
Changzheng Liu <cliu@gitlab.com>
2021-07-26 14:04:24 +0000

    Add worker_resource_boundary to Global Search workers

--- ee/app/workers/elastic_index_bulk_cron_worker.rb
+++ ee/app/workers/elastic_index_bulk_cron_worker.rb
@@ -4,6 +4,7 @@ class ElasticIndexBulkCronWorker # rubocop:disable Scalability/IdempotentWorker
   include Elastic::BulkCronWorker
 
   feature_category :global_search
+  worker_resource_boundary :cpu
   urgency :throttled
   # Even though this worker is idempotent, until https://gitlab.com/gitlab-org/gitlab/-/issues/325291 is done
   # we can't use it with read-only database replicas
