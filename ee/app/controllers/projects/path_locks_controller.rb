546e5390d23fc356ab032f075163ae29379e9382
Vasilii Iakliushin <viakliushin@gitlab.com>
2021-07-27 11:57:48 +0200

    Extract LfsPointersFinder from ExtractPath
    Contributes to https://gitlab.com/gitlab-org/gitlab/-/issues/330406
    
    **Problem**
    
    Method `lfs_blob_ids` is tightly-coupled with ExtractPath module. It
    makes it diffucult to reuse it.
    
    **Solution**
    
    1. Create LfsPointersFinder to fetch blob ids by repository and path.
    2. Replace old code with LfsPointersFinder implementation

--- ee/app/controllers/projects/path_locks_controller.rb
+++ ee/app/controllers/projects/path_locks_controller.rb
@@ -2,7 +2,6 @@
 
 class Projects::PathLocksController < Projects::ApplicationController
   include PathLocksHelper
-  include ExtractsPath
 
   # Authorize
   before_action :require_non_empty_project
@@ -10,8 +9,6 @@ class Projects::PathLocksController < Projects::ApplicationController
   before_action :authorize_push_code!, only: [:toggle]
 
   before_action :check_license
-  before_action :assign_ref_vars, only: :toggle
-  before_action :lfs_blob_ids, only: :toggle
 
   feature_category :source_code_management
 
@@ -21,7 +18,7 @@ class Projects::PathLocksController < Projects::ApplicationController
 
   # rubocop: disable CodeReuse/ActiveRecord
   def toggle
-    path_lock = @project.path_locks.find_by(path: params[:path])
+    path_lock = @project.path_locks.find_by(path: path)
 
     if path_lock
       unlock_file(path_lock)
@@ -62,13 +59,13 @@ class Projects::PathLocksController < Projects::ApplicationController
   end
 
   def lock_file
-    path_lock = PathLocks::LockService.new(project, current_user).execute(params[:path])
+    path_lock = PathLocks::LockService.new(project, current_user).execute(path)
 
     if path_lock.persisted? && sync_with_lfs?
       Lfs::LockFileService.new(
         project,
         current_user,
-        path: params[:path],
+        path: path,
         create_path_lock: false
       ).execute
     end
@@ -82,23 +79,21 @@ class Projects::PathLocksController < Projects::ApplicationController
     end
   end
 
-  # Override get_id from ExtractsPath.
-  # We don't support file locking per branch, that's why we use the root branch.
-  def get_id
-    id = project.repository.root_ref
-    id += "/#{params[:path]}" if params[:path].present?
-    id
-  end
-
   def lfs_file?
-    blob = project.repository.blob_at_branch(@ref, @path)
+    blob = repository.blob_at_branch(repository.root_ref, path)
 
     return false unless blob
 
-    @lfs_blob_ids.include?(blob.id)
+    lfs_blob_ids = LfsPointersFinder.new(repository, path).execute
+
+    lfs_blob_ids.include?(blob.id)
   end
 
   def sync_with_lfs?
     project.lfs_enabled? && lfs_file?
+  end
+
+  def path
+    params[:path]
   end
 end
