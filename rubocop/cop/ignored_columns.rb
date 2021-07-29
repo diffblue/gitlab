613d13ba15fe5647764f651369aca02d008c6cab
Corinna Wiesner <cwiesner@gitlab.com>
2021-07-29 15:58:20 +0200

    Add rubocop rule for ignore_column(s) usages
    Prior to this change, ignore rules for a columns were defined in CE or
    EE models. But if a model exists in CE and EE, the ignore rule has to
    be in the CE model. If the model only exists in EE, then it has to be
    added there. A new rubocop rule will check for the correct location of
    the ignore rule.

--- rubocop/cop/ignored_columns.rb
+++ rubocop/cop/ignored_columns.rb
@@ -4,16 +4,50 @@ module RuboCop
   module Cop
     # Cop that blacklists the usage of Group.public_or_visible_to_user
     class IgnoredColumns < RuboCop::Cop::Cop
-      MSG = 'Use `IgnoredColumns` concern instead of adding to `self.ignored_columns`.'
+      USE_CONCERN_MSG = 'Use `IgnoredColumns` concern instead of adding to `self.ignored_columns`.'
+      WRONG_MODEL_MSG = 'If the model exists in CE and EE, the column has to be ignored ' \
+        'in the CE model. If the model only exists in EE, then it has to be added there.'
 
       def_node_matcher :ignored_columns?, <<~PATTERN
         (send (self) :ignored_columns)
       PATTERN
 
-      def on_send(node)
-        return unless ignored_columns?(node)
+      def_node_matcher :ignore_columns?, <<~PATTERN
+        (send nil? :ignore_columns ...)
+      PATTERN
 
-        add_offense(node, location: :expression)
+      def_node_matcher :ignore_column?, <<~PATTERN
+        (send nil? :ignore_column ...)
+      PATTERN
+
+      def on_send(node)
+        if ignored_columns?(node)
+          add_offense(node, location: :expression, message: USE_CONCERN_MSG)
+        end
+
+        if concern_usage?(node) && used_in_wrong_model?
+          add_offense(node, location: :expression, message: WRONG_MODEL_MSG)
+        end
+      end
+
+      private
+
+      def concern_usage?(node)
+        ignore_columns?(node) || ignore_column?(node)
+      end
+
+      def used_in_wrong_model?
+        file_path = processed_source.file_path
+
+        ee_model?(file_path) && ce_model_exists?(file_path)
+      end
+
+      def ee_model?(path)
+        path.include?('ee/')
+      end
+
+      def ce_model_exists?(path)
+        File.exist?(path.gsub(%r{ee/}, ''))
       end
     end
   end
