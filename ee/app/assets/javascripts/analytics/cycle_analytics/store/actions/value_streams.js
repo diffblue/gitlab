1e511a25a84f55772aa688dfdea649fb8fdd928a
Ezekiel Kigbo <ekigbo@gitlab.com>
2021-08-02 08:12:44 +0000

    [VSA] Split Vuex actions into separate files

---
+++ ee/app/assets/javascripts/analytics/cycle_analytics/store/actions/value_streams.js
@@ -0,0 +1,96 @@
+import Api from 'ee/api';
+import { FETCH_VALUE_STREAM_DATA } from '../../constants';
+import * as types from '../mutation_types';
+
+export const receiveCreateValueStreamSuccess = ({ commit, dispatch }, valueStream = {}) => {
+  commit(types.RECEIVE_CREATE_VALUE_STREAM_SUCCESS, valueStream);
+  return dispatch('fetchCycleAnalyticsData');
+};
+
+export const createValueStream = ({ commit, dispatch, getters }, data) => {
+  const { currentGroupPath } = getters;
+  commit(types.REQUEST_CREATE_VALUE_STREAM);
+
+  return Api.cycleAnalyticsCreateValueStream(currentGroupPath, data)
+    .then(({ data: newValueStream }) => dispatch('receiveCreateValueStreamSuccess', newValueStream))
+    .catch(({ response } = {}) => {
+      const { data: { message, payload: { errors } } = null } = response;
+      commit(types.RECEIVE_CREATE_VALUE_STREAM_ERROR, { message, errors, data });
+    });
+};
+
+export const updateValueStream = (
+  { commit, dispatch, getters },
+  { id: valueStreamId, ...data },
+) => {
+  const { currentGroupPath } = getters;
+  commit(types.REQUEST_UPDATE_VALUE_STREAM);
+
+  return Api.cycleAnalyticsUpdateValueStream({ groupId: currentGroupPath, valueStreamId, data })
+    .then(({ data: newValueStream }) => {
+      commit(types.RECEIVE_UPDATE_VALUE_STREAM_SUCCESS, newValueStream);
+      return dispatch('fetchCycleAnalyticsData');
+    })
+    .catch(({ response } = {}) => {
+      const { data: { message, payload: { errors } } = null } = response;
+      commit(types.RECEIVE_UPDATE_VALUE_STREAM_ERROR, { message, errors, data });
+    });
+};
+
+export const deleteValueStream = ({ commit, dispatch, getters }, valueStreamId) => {
+  const { currentGroupPath } = getters;
+  commit(types.REQUEST_DELETE_VALUE_STREAM);
+
+  return Api.cycleAnalyticsDeleteValueStream(currentGroupPath, valueStreamId)
+    .then(() => commit(types.RECEIVE_DELETE_VALUE_STREAM_SUCCESS))
+    .then(() => dispatch('fetchCycleAnalyticsData'))
+    .catch(({ response } = {}) => {
+      const { data: { message } = null } = response;
+      commit(types.RECEIVE_DELETE_VALUE_STREAM_ERROR, message);
+    });
+};
+
+export const fetchValueStreamData = ({ dispatch }) =>
+  Promise.resolve()
+    .then(() => dispatch('fetchGroupStagesAndEvents'))
+    .then(() => dispatch('fetchStageMedianValues'))
+    .then(() => dispatch('durationChart/fetchDurationData'));
+
+export const setSelectedValueStream = ({ commit, dispatch }, valueStream) => {
+  commit(types.SET_SELECTED_VALUE_STREAM, valueStream);
+  return dispatch(FETCH_VALUE_STREAM_DATA);
+};
+
+export const receiveValueStreamsSuccess = (
+  { state: { selectedValueStream = null }, commit, dispatch },
+  data = [],
+) => {
+  commit(types.RECEIVE_VALUE_STREAMS_SUCCESS, data);
+
+  if (!selectedValueStream && data.length) {
+    const [firstStream] = data;
+    return Promise.resolve()
+      .then(() => dispatch('setSelectedValueStream', firstStream))
+      .then(() => dispatch('fetchStageCountValues'));
+  }
+
+  return Promise.resolve()
+    .then(() => dispatch(FETCH_VALUE_STREAM_DATA))
+    .then(() => dispatch('fetchStageCountValues'));
+};
+
+export const fetchValueStreams = ({ commit, dispatch, getters }) => {
+  const { currentGroupPath } = getters;
+
+  commit(types.REQUEST_VALUE_STREAMS);
+
+  return Api.cycleAnalyticsValueStreams(currentGroupPath)
+    .then(({ data }) => dispatch('receiveValueStreamsSuccess', data))
+    .catch((error) => {
+      const {
+        response: { status },
+      } = error;
+      commit(types.RECEIVE_VALUE_STREAMS_ERROR, status);
+      throw error;
+    });
+};
