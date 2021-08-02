5046693207e3d49052a0c49c7cd9b155aaba03aa
Vitaly Slobodin <vslobodin@gitlab.com>
2021-08-02 16:16:34 +0300

    Refactor init logic of merge request options on project settings page
    Changelog: fixed
    MR: https://gitlab.com/gitlab-org/gitlab/-/merge_requests/67334

---
+++ ee/spec/frontend/pages/projects/edit/merge_options_spec.js
@@ -0,0 +1,31 @@
+import {
+  initMergeOptionSettings,
+  ERROR_LOADING_MERGE_OPTION_SETTINGS,
+} from 'ee/pages/projects/edit/merge_options';
+import { setHTMLFixture } from 'helpers/fixtures';
+import createFlash from '~/flash';
+import * as createDefaultClient from '~/lib/graphql';
+
+jest.mock('~/flash');
+
+describe('MergOptions', () => {
+  beforeEach(() => {
+    setHTMLFixture(`
+      <div id="project-merge-options"></div/>
+    `);
+
+    createDefaultClient.default = jest.fn(() => ({
+      query: jest.fn().mockRejectedValue('Error'),
+    }));
+  });
+
+  it('shows flash message on network error', async () => {
+    await initMergeOptionSettings();
+
+    expect(createFlash).toHaveBeenCalledWith({
+      message: ERROR_LOADING_MERGE_OPTION_SETTINGS,
+      error: 'Error',
+      captureError: true,
+    });
+  });
+});
