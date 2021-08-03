81109da95d4628fad060bc8e414506fe9337d6d7
Brett Walker <bwalker@gitlab.com>
2021-08-03 17:32:29 -0500

    Use reference cache for iterations
    And remove some unused specs
    
    Changelog: performance
    EE: true

--- ee/spec/lib/banzai/filter/references/iteration_reference_filter_spec.rb
+++ ee/spec/lib/banzai/filter/references/iteration_reference_filter_spec.rb
@@ -168,111 +168,6 @@ RSpec.describe Banzai::Filter::References::IterationReferenceFilter do
     end
   end
 
-  shared_examples 'linking to a iteration as the entire link' do
-    let(:unquoted_reference) { "#{Iteration.reference_prefix}#{iteration.name}" }
-    let(:link) { urls.iteration_url(iteration) }
-    let(:link_reference) { %Q{<a href="#{link}">#{link}</a>} }
-
-    it 'replaces the link text with the iteration reference' do
-      doc = reference_filter("See #{link}")
-
-      expect(doc.css('a').first.text).to eq(unquoted_reference)
-    end
-
-    it 'includes a data-project attribute' do
-      doc = reference_filter("Iteration #{link_reference}")
-      link = doc.css('a').first
-
-      expect(link).to have_attribute('data-project')
-      expect(link.attr('data-project')).to eq project.id.to_s
-    end
-
-    it 'includes a data-iteration attribute' do
-      doc = reference_filter("See #{link_reference}")
-      link = doc.css('a').first
-
-      expect(link).to have_attribute('data-iteration')
-      expect(link.attr('data-iteration')).to eq iteration.id.to_s
-    end
-  end
-
-  shared_examples 'cross-project / cross-namespace complete reference' do
-    let(:namespace) { create(:namespace) }
-    let(:another_project) { create(:project, :public, namespace: namespace) }
-    let(:iteration) { create(:iteration, project: another_project) }
-    let(:reference) { "#{another_project.full_path}*iteration:#{iteration.iid}" }
-    let!(:result) { reference_filter("See #{reference}") }
-
-    it 'points to referenced project iteration page' do
-      expect(result.css('a').first.attr('href'))
-        .to eq(urls.project_iteration_url(another_project, iteration))
-    end
-
-    it 'link has valid text' do
-      doc = reference_filter("See (#{reference}.)")
-
-      expect(doc.css('a').first.text)
-        .to eq("#{iteration.reference_link_text} in #{another_project.full_path}")
-    end
-
-    it 'has valid text' do
-      doc = reference_filter("See (#{reference}.)")
-
-      expect(doc.text)
-        .to eq("See (#{iteration.reference_link_text} in #{another_project.full_path}.)")
-    end
-
-    it 'escapes the name attribute' do
-      allow_next_instance_of(Iteration) do |instance|
-        allow(instance).to receive(:title).and_return(%{"></a>whatever<a title="})
-      end
-
-      doc = reference_filter("See #{reference}")
-
-      expect(doc.css('a').first.text)
-        .to eq "#{iteration.reference_link_text} in #{another_project.full_path}"
-    end
-  end
-
-  shared_examples 'cross project shorthand reference' do
-    let(:namespace) { create(:namespace) }
-    let(:project) { create(:project, :public, namespace: namespace) }
-    let(:another_project) { create(:project, :public, namespace: namespace) }
-    let(:iteration) { create(:iteration, project: another_project) }
-    let(:reference) { "#{another_project.path}*iteration:#{iteration.iid}" }
-    let!(:result) { reference_filter("See #{reference}") }
-
-    it 'points to referenced project iteration page' do
-      expect(result.css('a').first.attr('href')).to eq urls
-                                                         .project_iteration_url(another_project, iteration)
-    end
-
-    it 'link has valid text' do
-      doc = reference_filter("See (#{reference}.)")
-
-      expect(doc.css('a').first.text)
-        .to eq("#{iteration.reference_link_text} in #{another_project.path}")
-    end
-
-    it 'has valid text' do
-      doc = reference_filter("See (#{reference}.)")
-
-      expect(doc.text)
-        .to eq("See (#{iteration.reference_link_text} in #{another_project.path}.)")
-    end
-
-    it 'escapes the name attribute' do
-      allow_next_instance_of(Iteration) do |instance|
-        allow(instance).to receive(:title).and_return(%{"></a>whatever<a title="})
-      end
-
-      doc = reference_filter("See #{reference}")
-
-      expect(doc.css('a').first.text)
-        .to eq "#{iteration.reference_link_text} in #{another_project.path}"
-    end
-  end
-
   shared_examples 'references with HTML entities' do
     before do
       iteration.update!(title: '&lt;html&gt;')
@@ -301,9 +196,14 @@ RSpec.describe Banzai::Filter::References::IterationReferenceFilter do
     it_behaves_like 'String-based multi-word references in quotes'
     it_behaves_like 'referencing a iteration in a link href'
     it_behaves_like 'references with HTML entities'
+
     it_behaves_like 'HTML text with references' do
       let(:resource) { iteration }
       let(:resource_text) { resource.title }
+    end
+
+    it_behaves_like 'Integer-based references' do
+      let(:reference) { iteration.to_reference(format: :id) }
     end
 
     it 'does not support references by IID' do
@@ -335,7 +235,7 @@ RSpec.describe Banzai::Filter::References::IterationReferenceFilter do
     end
 
     it 'supports parent group references' do
-      # we have to update iterations_cadence group first in order to avoid invallid record
+      # we have to update iterations_cadence group first in order to avoid an invalid record
       iteration.iterations_cadence.update_column(:group_id, parent_group.id)
       iteration.update_column(:group_id, parent_group.id)
 
@@ -397,6 +297,57 @@ RSpec.describe Banzai::Filter::References::IterationReferenceFilter do
       let(:iteration) { create(:iteration, :closed, group: group) }
 
       include_context 'group iterations'
+    end
+  end
+
+  context 'checking N+1' do
+    let_it_be(:group) { create(:group) }
+    let_it_be(:group2) { create(:group, parent: group) }
+    let_it_be(:iteration) { create(:iteration, group: group) }
+    let_it_be(:iteration_reference) { iteration.to_reference(format: :name) }
+    let_it_be(:iteration2) { create(:iteration, group: group) }
+    let_it_be(:iteration2_reference) { iteration2.to_reference(format: :id) }
+    let_it_be(:iteration3) { create(:iteration, group: group2) }
+    let_it_be(:iteration3_reference) { iteration3.to_reference(format: :name) }
+
+    it 'does not have N+1 per multiple references per group', :use_sql_query_cache, :aggregate_failures do
+      max_count = 3
+      markdown = "#{iteration_reference}"
+
+      # warm the cache
+      reference_filter(markdown)
+
+      expect do
+        reference_filter(markdown)
+      end.not_to exceed_all_query_limit(max_count)
+
+      markdown = "#{iteration_reference} *iteration:\"Not Found\" *iteration:\"Not Found2\" #{iteration2_reference}"
+
+      expect do
+        reference_filter(markdown)
+      end.not_to exceed_all_query_limit(max_count)
+    end
+
+    it 'has N+1 for multiple unique group references', :use_sql_query_cache do
+      markdown = "#{iteration_reference}"
+      max_count = 3
+
+      # warm the cache
+      reference_filter(markdown, { project: nil, group: group2 })
+
+      expect do
+        reference_filter(markdown, { project: nil, group: group2 })
+      end.not_to exceed_all_query_limit(max_count)
+
+      # Since we're not batching iteration queries across groups,
+      # queries increase when a new group is referenced.
+      # TODO: https://gitlab.com/gitlab-org/gitlab/-/issues/330359
+      markdown = "#{iteration_reference} #{iteration2_reference} #{iteration3_reference}"
+      max_count += 1
+
+      expect do
+        reference_filter(markdown, { project: nil, group: group2 })
+      end.not_to exceed_all_query_limit(max_count)
     end
   end
 end
