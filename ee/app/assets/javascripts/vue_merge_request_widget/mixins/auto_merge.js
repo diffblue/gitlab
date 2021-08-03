e2c89667907cf4ed05cbf43ed1152f896f5a5755
Phil Hughes <me@iamphill.com>
2021-08-03 09:52:02 +0100

    Updated auto merge widget to match new designs
    This changes the design of the auto merge widget to match the
    new designs.
    It also removes the auto merge failed widget which will
    make the merge enabled widget be visible
    and the error message visible above the widget.
    
    Closes https://gitlab.com/gitlab-org/gitlab/-/issues/324178

--- ee/app/assets/javascripts/vue_merge_request_widget/mixins/auto_merge.js
+++ ee/app/assets/javascripts/vue_merge_request_widget/mixins/auto_merge.js
@@ -1,38 +1,33 @@
 import { s__ } from '~/locale';
-import {
-  MT_MERGE_STRATEGY,
-  MTWPS_MERGE_STRATEGY,
-  MWPS_MERGE_STRATEGY,
-} from '~/vue_merge_request_widget/constants';
+import { MT_MERGE_STRATEGY, MTWPS_MERGE_STRATEGY } from '~/vue_merge_request_widget/constants';
 
 export default {
   computed: {
-    statusTextBeforeAuthor() {
-      if (this.autoMergeStrategy === MT_MERGE_STRATEGY) {
-        return s__('mrWidget|Added to the merge train by');
-      }
-
-      return s__('mrWidget|Set by');
-    },
-    statusTextAfterAuthor() {
+    statusText() {
       const { mergeTrainsCount } = this.glFeatures.mergeRequestWidgetGraphql ? this.state : this.mr;
 
-      if (this.autoMergeStrategy === MTWPS_MERGE_STRATEGY && mergeTrainsCount === 0) {
-        return s__('mrWidget|to start a merge train when the pipeline succeeds');
+      if (this.autoMergeStrategy === MT_MERGE_STRATEGY) {
+        return s__('mrWidget|Added to the merge train by %{merge_author}');
+      } else if (this.autoMergeStrategy === MTWPS_MERGE_STRATEGY && mergeTrainsCount === 0) {
+        return s__(
+          'mrWidget|Set by %{merge_author} to start a merge train when the pipeline succeeds',
+        );
       } else if (this.autoMergeStrategy === MTWPS_MERGE_STRATEGY && mergeTrainsCount !== 0) {
-        return s__('mrWidget|to be added to the merge train when the pipeline succeeds');
-      } else if (this.autoMergeStrategy === MWPS_MERGE_STRATEGY) {
-        return s__('mrWidget|to be merged automatically when the pipeline succeeds');
+        return s__(
+          'mrWidget|Set by %{merge_author} to be added to the merge train when the pipeline succeeds',
+        );
       }
 
-      return '';
+      return s__(
+        'mrWidget|Set by %{merge_author} to be merged automatically when the pipeline succeeds',
+      );
     },
     cancelButtonText() {
       if (this.autoMergeStrategy === MT_MERGE_STRATEGY) {
         return s__('mrWidget|Remove from merge train');
       }
 
-      return s__('mrWidget|Cancel');
+      return s__('mrWidget|Cancel auto-merge');
     },
   },
 };
