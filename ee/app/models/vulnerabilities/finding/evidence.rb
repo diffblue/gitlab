0a29032709474631ce6a95c3e09eb2f769df0ba5
Jonathan Schafer <jschafer@gitlab.com>
2021-08-05 12:51:06 -0500

    Change Evidence Supporting Messages to array
    Changelog: changed
    EE: true

--- ee/app/models/vulnerabilities/finding/evidence.rb
+++ ee/app/models/vulnerabilities/finding/evidence.rb
@@ -5,13 +5,32 @@ module Vulnerabilities
     class Evidence < ApplicationRecord
       self.table_name = 'vulnerability_finding_evidences'
 
-      belongs_to :finding, class_name: 'Vulnerabilities::Finding', inverse_of: :evidence, foreign_key: 'vulnerability_occurrence_id', optional: false
+      belongs_to :finding,
+                 class_name: 'Vulnerabilities::Finding',
+                 inverse_of: :evidence,
+                 foreign_key: 'vulnerability_occurrence_id',
+                 optional: false
 
-      has_one :request, class_name: 'Vulnerabilities::Finding::Evidence::Request', inverse_of: :evidence, foreign_key: 'vulnerability_finding_evidence_id'
-      has_one :response, class_name: 'Vulnerabilities::Finding::Evidence::Response', inverse_of: :evidence, foreign_key: 'vulnerability_finding_evidence_id'
-      has_one :supporting_message, class_name: 'Vulnerabilities::Finding::Evidence::SupportingMessage', inverse_of: :evidence, foreign_key: 'vulnerability_finding_evidence_id'
-      has_one :source, class_name: 'Vulnerabilities::Finding::Evidence::Source', inverse_of: :evidence, foreign_key: 'vulnerability_finding_evidence_id'
-      has_many :assets, class_name: 'Vulnerabilities::Finding::Evidence::Asset', inverse_of: :evidence, foreign_key: 'vulnerability_finding_evidence_id'
+      has_one :request,
+              class_name: 'Vulnerabilities::Finding::Evidence::Request',
+              inverse_of: :evidence,
+              foreign_key: 'vulnerability_finding_evidence_id'
+      has_one :response,
+              class_name: 'Vulnerabilities::Finding::Evidence::Response',
+              inverse_of: :evidence,
+              foreign_key: 'vulnerability_finding_evidence_id'
+      has_one :source,
+              class_name: 'Vulnerabilities::Finding::Evidence::Source',
+              inverse_of: :evidence,
+              foreign_key: 'vulnerability_finding_evidence_id'
+      has_many :supporting_messages,
+               class_name: 'Vulnerabilities::Finding::Evidence::SupportingMessage',
+               inverse_of: :evidence,
+               foreign_key: 'vulnerability_finding_evidence_id'
+      has_many :assets,
+               class_name: 'Vulnerabilities::Finding::Evidence::Asset',
+               inverse_of: :evidence,
+               foreign_key: 'vulnerability_finding_evidence_id'
 
       validates :summary, length: { maximum: 8_000_000 }
     end
