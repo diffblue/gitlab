6d7078e736f513bc0bfd3fd0979ba59754dc58c6
Bala Kumar <sbalakumar@gitlab.com>
2021-08-10 10:02:02 +0000

    Resolve "Use Same Endpoint for HTML and JSON in OperationsController"

--- ee/spec/views/operations/environments.html.haml_spec.rb
+++ ee/spec/views/operations/environments.html.haml_spec.rb
@@ -6,8 +6,8 @@ RSpec.describe 'operations/environments.html.haml' do
   it 'renders the frontend configuration' do
     render
 
-    expect(rendered).to match %r{data-add-path="/-/operations"}
-    expect(rendered).to match %r{data-list-path="/-/operations/environments_list"}
+    expect(rendered).to match %r{data-add-path="/-/operations/environments.json"}
+    expect(rendered).to match %r{data-list-path="/-/operations/environments.json"}
     expect(rendered).to match %r{data-empty-dashboard-svg-path="/assets/illustrations/operations-dashboard_empty.*\.svg"}
     expect(rendered).to match %r{data-empty-dashboard-help-path="/help/ci/environments/environments_dashboard.md"}
     expect(rendered).to match %r{data-environments-dashboard-help-path="/help/ci/environments/environments_dashboard.md"}
