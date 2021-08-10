d2ceef94d4e18bcd8f929769d7ffb6f393be44f3
Andrejs Cunskis <acunskis@gitlab.com>
2021-08-10 01:02:06 +0000

    E2E: Move quarantine and context selection logic to formatter

--- qa/qa.rb
+++ qa/qa.rb
@@ -630,7 +630,9 @@ module QA
 
     module Helpers
       autoload :ContextSelector, 'qa/specs/helpers/context_selector'
+      autoload :ContextFormatter, 'qa/specs/helpers/context_formatter'
       autoload :Quarantine, 'qa/specs/helpers/quarantine'
+      autoload :QuarantineFormatter, 'qa/specs/helpers/quarantine_formatter'
       autoload :RSpec, 'qa/specs/helpers/rspec'
     end
   end
@@ -678,6 +680,7 @@ module QA
     autoload :WaitForRequests, 'qa/support/wait_for_requests'
     autoload :OTP, 'qa/support/otp'
     autoload :SSH, 'qa/support/ssh'
+    autoload :AllureMetadataFormatter, 'qa/support/allure_metadata_formatter.rb'
   end
 end
 
