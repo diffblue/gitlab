cba5473c71cdea4156087a78e8046ff51eb6b91c
Andrejs Cunskis <acunskis@gitlab.com>
2021-08-12 21:26:25 +0300

    E2E: Group Migration - epic migration validation
    Implement spec for epic migration validation
    
    Add ability to fetch and compare epics from group
    
    Make epic confidential
    
    Update test names and add testcase link
    
    Remove irrelevant comments
    
    Remove redundant validation of array size
    
    Fix calling confidential during ui fabrication

--- qa/qa/ee/resource/epic.rb
+++ qa/qa/ee/resource/epic.rb
@@ -4,25 +4,22 @@ module QA
   module EE
     module Resource
       class Epic < QA::Resource::Base
-        attr_accessor :title
+        attributes :iid,
+                   :title,
+                   :description,
+                   :start_date_is_fixed,
+                   :start_date_fixed,
+                   :due_date_is_fixed,
+                   :due_date_fixed,
+                   :confidential
 
         attribute :group do
           QA::Resource::Group.fabricate!
         end
 
-        attribute :id
-        attribute :iid
-        attribute :start_date_is_fixed
-        attribute :start_date_fixed
-        attribute :due_date_is_fixed
-        attribute :due_date_fixed
-        attribute :confidential
-
         def initialize
           @start_date_is_fixed = false
-          @start_date_fixed = nil
           @due_date_is_fixed = false
-          @due_date_fixed = nil
           @confidential = false
         end
 
@@ -33,30 +30,69 @@ module QA
 
           QA::EE::Page::Group::Epic::Index.perform(&:click_new_epic)
 
-          QA::EE::Page::Group::Epic::New.perform do |new|
-            new.set_title(@title)
-            new.enable_confidential_epic if @confidential
-            new.create_new_epic
+          QA::EE::Page::Group::Epic::New.perform do |new_epic_page|
+            new_epic_page.set_title(title)
+            new_epic_page.enable_confidential_epic if @confidential
+            new_epic_page.create_new_epic
           end
         end
 
         def api_get_path
-          "/groups/#{group.id}/epics/#{id}"
+          "/groups/#{CGI.escape(group.full_path)}/epics/#{iid}"
         end
 
         def api_post_path
-          "/groups/#{group.id}/epics"
+          "/groups/#{CGI.escape(group.full_path)}/epics"
         end
 
         def api_post_body
           {
-            title: title,
+            title: @title,
             start_date_is_fixed: @start_date_is_fixed,
             start_date_fixed: @start_date_fixed,
             due_date_is_fixed: @due_date_is_fixed,
             due_date_fixed: @due_date_fixed,
-            confidential: @confidential
+            confidential: @confidential,
+            parent_id: @parent_id
           }
+        end
+
+        # Object comparison
+        #
+        # @param [QA::EE::Resource::Epic] other
+        # @return [Boolean]
+        def ==(other)
+          other.is_a?(Epic) && comparable_epic == other.comparable_epic
+        end
+
+        # Override inspect for a better rspec failure diff output
+        #
+        # @return [String]
+        def inspect
+          JSON.pretty_generate(comparable_epic)
+        end
+
+        protected
+
+        # Return subset of fields for comparing epics
+        #
+        # @return [Hash]
+        def comparable_epic
+          reload! if api_response.nil?
+
+          api_resource.slice(
+            :title,
+            :description,
+            :state,
+            :start_date_is_fixed,
+            :start_date_fixed,
+            :due_date_is_fixed,
+            :due_date_fixed,
+            :confidential,
+            :labels,
+            :upvotes,
+            :downvotes
+          )
         end
       end
     end
--- qa/qa/specs/features/ee/api/1_manage/bulk_import_group_spec.rb
+++ qa/qa/specs/features/ee/api/1_manage/bulk_import_group_spec.rb
@@ -29,22 +29,6 @@ module QA
         end
       end
 
-      let(:subgroup) do
-        Resource::Group.fabricate_via_api! do |group|
-          group.api_client = api_client
-          group.sandbox = source_group
-          group.path = "subgroup-for-import-#{SecureRandom.hex(4)}"
-        end
-      end
-
-      let(:imported_subgroup) do
-        Resource::Group.init do |group|
-          group.api_client = api_client
-          group.sandbox = imported_group
-          group.path = subgroup.path
-        end
-      end
-
       let(:imported_group) do
         Resource::BulkImportGroup.fabricate_via_api! do |group|
           group.api_client = api_client
@@ -53,42 +37,30 @@ module QA
         end
       end
 
+      let(:imported_epics) do
+        imported_group.epics
+      end
+
       before do
         Runtime::Feature.enable(:bulk_import) unless staging?
         Runtime::Feature.enable(:top_level_group_creation_enabled) if staging?
 
         sandbox.add_member(user, Resource::Members::AccessLevel::MAINTAINER)
 
-        Resource::GroupLabel.fabricate_via_api! do |label|
-          label.api_client = api_client
-          label.group = source_group
-          label.title = "source-group-#{SecureRandom.hex(4)}"
-        end
-        Resource::GroupLabel.fabricate_via_api! do |label|
-          label.api_client = api_client
-          label.group = subgroup
-          label.title = "subgroup-#{SecureRandom.hex(4)}"
+        EE::Resource::Epic.fabricate_via_api! do |epic|
+          epic.api_client = api_client
+          epic.group = source_group
+          epic.title = "Bulk group import test"
+          epic.confidential = true
         end
       end
 
-      # Non blocking issues:
-      # https://gitlab.com/gitlab-org/gitlab/-/issues/331252
-      # https://gitlab.com/gitlab-org/gitlab/-/issues/333678 <- can cause 500 when creating user and group back to back
-      it(
-        'imports group with subgroups and labels',
-        testcase: 'https://gitlab.com/gitlab-org/quality/testcases/-/issues/1871'
-      ) do
+      it 'imports group epics', testcase: 'https://gitlab.com/gitlab-org/quality/testcases/-/issues/1874' do
         expect { imported_group.import_status }.to(
           eventually_eq('finished').within(max_duration: 300, sleep_interval: 2)
         )
 
-        aggregate_failures do
-          expect(imported_group.reload!).to eq(source_group)
-          expect(imported_group.labels).to include(*source_group.labels)
-
-          expect(imported_subgroup.reload!).to eq(subgroup)
-          expect(imported_subgroup.labels).to include(*subgroup.labels)
-        end
+        expect(imported_epics).to eq(source_group.epics)
       end
 
       after do
