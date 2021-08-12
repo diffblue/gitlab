cf05575659cc1aa29048fa7dc2b25254340a1c27
Raimund Hook <raimund.hook@exfo.com>
2021-08-12 09:59:25 +0100

    Updated message on DevOps Adoption Report
    Message now more clearly shows that the update is automatic at the
    beginning of the month, and reflects the previous calendar month.
    
    Fixes https://gitlab.com/gitlab-org/gitlab/-/issues/335640
    
    Changelog: changed
    EE: true
    Signed-off-by: Raimund Hook <raimund.hook@exfo.com>

--- ee/spec/frontend/analytics/devops_report/devops_adoption/components/devops_adoption_section_spec.js
+++ ee/spec/frontend/analytics/devops_report/devops_adoption/components/devops_adoption_section_spec.js
@@ -96,7 +96,7 @@ describe('DevopsAdoptionSection', () => {
       createComponent();
 
       const text =
-        'Feature adoption is based on usage in the previous calendar month. Last updated: 2020-10-31 23:59.';
+        'Feature adoption is based on usage in the previous calendar month. Data is updated at the beginning of each month. Last updated: 2020-10-31 23:59.';
       expect(getByText(wrapper.element, text)).not.toBeNull();
     });
 
