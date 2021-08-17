228b70ae4b246daad75945074d33db5ca58b63a2
Olena Horal-Koretska <ohoralkoretska@gitlab.com>
2021-08-17 18:27:45 +0300

    Diff stats dropdown styling update due to migration to GlDropdown
    Changelog: changed

--- ee/app/assets/javascripts/pages/groups/wikis/index.js
+++ ee/app/assets/javascripts/pages/groups/wikis/index.js
@@ -1,5 +1,7 @@
 import initClonePanel from '~/clone_panel';
+import { initDiffStatsDropdown } from '~/init_diff_stats_dropdown';
 import initWikis from '~/pages/shared/wikis';
 
 initWikis();
 initClonePanel();
+initDiffStatsDropdown();
