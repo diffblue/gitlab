e4007af2e0d50dee6ee4a9869a224c94d55ab85f
James Fargher <jfargher@gitlab.com>
2021-08-19 14:47:11 +1200

    Remove repository backup concurrency code
    Now that we do not need to maintain the original rails based repository
    backup strategy we can remove all the associated concurrency code.
    Concurrency will now be handled by the `gitaly-backup` command.

--- ee/spec/lib/ee/backup/repositories_spec.rb
+++ ee/spec/lib/ee/backup/repositories_spec.rb
@@ -9,6 +9,8 @@ RSpec.describe Backup::Repositories do
   subject { described_class.new(progress, strategy: strategy) }
 
   describe '#dump' do
+    let_it_be(:groups) { create_list(:group, 5, :wiki_repo) }
+
     context 'hashed storage' do
       let_it_be(:project) { create(:project, :repository) }
       let_it_be(:group) { create(:group, :wiki_repo) }
@@ -16,7 +18,7 @@ RSpec.describe Backup::Repositories do
       it 'calls enqueue for each repository type', :aggregate_failures do
         create(:wiki_page, container: group)
 
-        subject.dump(max_concurrency: 1, max_storage_concurrency: 1)
+        subject.dump
 
         expect(strategy).to have_received(:start).with(:create)
         expect(strategy).to have_received(:enqueue).with(project, Gitlab::GlRepository::PROJECT)
@@ -25,46 +27,30 @@ RSpec.describe Backup::Repositories do
       end
     end
 
-    context 'no concurrency' do
-      let_it_be(:groups) { create_list(:group, 5, :wiki_repo) }
+    context 'command failure' do
+      it 'enqueue_group raises an error' do
+        allow(strategy).to receive(:enqueue).with(anything, Gitlab::GlRepository::WIKI).and_raise(IOError)
 
-      it 'creates the expected number of threads' do
-        expect(Thread).not_to receive(:new)
-
-        expect(strategy).to receive(:start).with(:create)
-        groups.each do |group|
-          expect(strategy).to receive(:enqueue).with(group, Gitlab::GlRepository::WIKI)
-        end
-        expect(strategy).to receive(:wait)
-
-        subject.dump(max_concurrency: 1, max_storage_concurrency: 1)
+        expect { subject.dump }.to raise_error(IOError)
       end
 
-      describe 'command failure' do
-        it 'enqueue_group raises an error' do
-          allow(strategy).to receive(:enqueue).with(anything, Gitlab::GlRepository::WIKI).and_raise(IOError)
+      it 'group query raises an error' do
+        allow(Group).to receive_message_chain(:includes, :find_each).and_raise(ActiveRecord::StatementTimeout)
 
-          expect { subject.dump(max_concurrency: 1, max_storage_concurrency: 1) }.to raise_error(IOError)
-        end
-
-        it 'group query raises an error' do
-          allow(Group).to receive_message_chain(:includes, :find_each).and_raise(ActiveRecord::StatementTimeout)
-
-          expect { subject.dump(max_concurrency: 1, max_storage_concurrency: 1) }.to raise_error(ActiveRecord::StatementTimeout)
-        end
+        expect { subject.dump }.to raise_error(ActiveRecord::StatementTimeout)
       end
+    end
 
-      it 'avoids N+1 database queries' do
-        control_count = ActiveRecord::QueryRecorder.new do
-          subject.dump(max_concurrency: 1, max_storage_concurrency: 1)
-        end.count
+    it 'avoids N+1 database queries' do
+      control_count = ActiveRecord::QueryRecorder.new do
+        subject.dump
+      end.count
 
-        create_list(:group, 2, :wiki_repo)
+      create_list(:group, 2, :wiki_repo)
 
-        expect do
-          subject.dump(max_concurrency: 1, max_storage_concurrency: 1)
-        end.not_to exceed_query_limit(control_count)
-      end
+      expect do
+        subject.dump
+      end.not_to exceed_query_limit(control_count)
     end
   end
 
