219f579907bd9aa9ef6c4a9d9f5c061e005cad1c
Alexander Turinske <aturinske@gitlab.com>
2021-08-20 10:32:43 -0600

    Create human-reable actions and rules
    - for all scan execution policy types
    - update drawer to show correct data
    - update tests

--- ee/app/assets/javascripts/threat_monitoring/components/policy_editor/scan_execution_policy/lib/index.js
+++ ee/app/assets/javascripts/threat_monitoring/components/policy_editor/scan_execution_policy/lib/index.js
@@ -2,6 +2,7 @@ export { fromYaml } from './from_yaml';
 export { toYaml } from './to_yaml';
 
 export * from './constants';
+export * from './humanize';
 export * from './utils';
 
 export const DEFAULT_SCAN_EXECUTION_POLICY = `type: scan_execution_policy
