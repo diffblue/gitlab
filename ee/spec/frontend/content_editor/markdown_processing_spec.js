2518ceeb9ffe09b270cb7bb1fbd6e98d8ee8db0f
Chad Woolley <cwoolley@gitlab.com>
2021-08-20 12:40:00 -0700

    Refactor markdown golden master specs
    - See https://gitlab.com/gitlab-org/gitlab/-/merge_requests/68671

--- ee/spec/frontend/content_editor/markdown_processing_spec.js
+++ ee/spec/frontend/content_editor/markdown_processing_spec.js
@@ -1,10 +1,13 @@
 import path from 'path';
-import { createSharedExamples, loadMarkdownApiExamples } from './markdown_processing_spec_helper';
+import {
+  createSharedExamples,
+  loadMarkdownApiExamples,
+} from 'jest/content_editor/markdown_processing_spec_helper';
 
 jest.mock('~/emoji');
 
 // See spec/fixtures/markdown/markdown_golden_master_examples.yml for documentation on how this spec works.
-describe('markdown processing in ContentEditor', () => {
+describe('EE markdown processing in ContentEditor', () => {
   // Ensure we generate same markdown that was provided to Markdown API.
   const markdownYamlPath = path.join(
     __dirname,
