6a4c856c8ce8e16d32ce1fdd3e4aac5842cceb96
Peter Leitzen <pleitzen@gitlab.com>
2021-08-23 16:44:33 +0200

    Status Page: Track incident issue without subtransaction
    `StatusPage::PublishedIncident#track` used `safe_find_or_create_by` to
    circumvent a potential race condition in case of a duplicate published
    issue.  This, however, uses subtransactions under the hood which is
    problematic in nested transactions.
    
    See https://gitlab.com/groups/gitlab-org/-/epics/6540 for more context.
    
    In the rare event of `ActiveRecord::RecordNotUnique` users end up seeing
    a meaningful error message. This behaviour is acceptable and that's why
    switched to unsafe method `find_or_create_by`.

--- ee/app/models/status_page/published_incident.rb
+++ ee/app/models/status_page/published_incident.rb
@@ -8,8 +8,22 @@ module StatusPage
     belongs_to :issue, inverse_of: :status_page_published_incident
     validates :issue, presence: true
 
+    # NOTE: This method is not atomic and might raise
+    # +ActiveRecord::RecordNotUnique+ in case of a duplicate published issue.
+    #
+    # Previously, we've used +safe_find_or_create_by+ to circumvent this fact
+    # but it uses subtransactions under the hood which is problematic in nested
+    # transactions.
+    #
+    # See https://gitlab.com/groups/gitlab-org/-/epics/6540 for more context.
+    #
+    # In the rare event of +ActiveRecord::RecordNotUnique+ users end up seeing
+    # a meaningful error message. This behaviour is acceptable and that's why
+    # switched to unsafe method +find_or_create_by+.
+    #
+    # @raise ActiveRecord::RecordNotUnique
     def self.track(issue)
-      safe_find_or_create_by(issue: issue)
+      find_or_create_by!(issue: issue)
     end
 
     def self.untrack(issue)
