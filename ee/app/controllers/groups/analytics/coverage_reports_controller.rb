f04ef9b180359e75fbc6e61fea49ca400760c7a1
Pavel Shutsin <pshutsin@gitlab.com>
2021-08-24 15:20:11 +0200

    Deny access for repository coverage info for guests
    Repository coverage information should be accessible
    for Reporter+ role only
    
    Changelog: security
    EE: true

--- ee/app/controllers/groups/analytics/coverage_reports_controller.rb
+++ ee/app/controllers/groups/analytics/coverage_reports_controller.rb
@@ -6,7 +6,7 @@ class Groups::Analytics::CoverageReportsController < Groups::Analytics::Applicat
   COVERAGE_PARAM = 'coverage'
 
   before_action :load_group
-  before_action -> { check_feature_availability!(:group_coverage_reports) }
+  before_action -> { authorize_view_by_action!(:read_group_coverage_reports) }
 
   def index
     respond_to do |format|
