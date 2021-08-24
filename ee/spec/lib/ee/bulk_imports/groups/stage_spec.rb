d8db3d37859d8d7795dfc85e8e3853821c55d628
George Koltsov <gkoltsov@gitlab.com>
2021-08-24 17:06:30 +0100

    Add empty project migration to Group Migration
      - GitLab Group Migration now includes empty projects
        migrated from source instance of GitLab if
        bulk_import_projects feature flag is enabled

--- ee/spec/lib/ee/bulk_imports/groups/stage_spec.rb
+++ ee/spec/lib/ee/bulk_imports/groups/stage_spec.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-require 'fast_spec_helper'
+require 'spec_helper'
 
-RSpec.describe BulkImports::Stage do
+RSpec.describe BulkImports::Groups::Stage do
   let(:pipelines) do
     [
       [0, BulkImports::Groups::Pipelines::GroupPipeline],
@@ -13,15 +13,16 @@ RSpec.describe BulkImports::Stage do
       [1, BulkImports::Groups::Pipelines::MilestonesPipeline],
       [1, BulkImports::Groups::Pipelines::BadgesPipeline],
       [1, BulkImports::Groups::Pipelines::IterationsPipeline],
+      [1, BulkImports::Groups::Pipelines::ProjectEntitiesPipeline],
       [2, BulkImports::Groups::Pipelines::BoardsPipeline],
       [2, BulkImports::Groups::Pipelines::EpicsPipeline],
-      [4, BulkImports::Groups::Pipelines::EntityFinisher]
+      [4, BulkImports::Common::Pipelines::EntityFinisher]
     ]
   end
 
   describe '#each' do
     it 'iterates over all pipelines with the stage number' do
-      expect(described_class.pipelines).to match_array(pipelines)
+      expect(subject.pipelines).to match_array(pipelines)
     end
   end
 end
