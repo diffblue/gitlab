d43914ce6144c4f248713f158b1fc244ec612aaa
Heinrich Lee Yu <heinrich@gitlab.com>
2021-08-26 19:42:09 +0800

    Remove subtransaction from DastSite
    This changes the service to use regular find_or_create_by so that we
    don't start a subtransaction

--- ee/app/services/dast_sites/find_or_create_service.rb
+++ ee/app/services/dast_sites/find_or_create_service.rb
@@ -7,17 +7,13 @@ module DastSites
     def execute!(url:)
       raise PermissionsError, 'Insufficient permissions' unless allowed?
 
-      find_or_create_by!(url)
+      DastSite.find_or_create_by!(project: project, url: url) # rubocop:disable CodeReuse/ActiveRecord
     end
 
     private
 
     def allowed?
       Ability.allowed?(current_user, :create_on_demand_dast_scan, project)
-    end
-
-    def find_or_create_by!(url)
-      DastSite.safe_find_or_create_by!(project: project, url: url)
     end
   end
 end
