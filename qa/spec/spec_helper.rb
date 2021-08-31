58f8a3e7b63d721c712429281c8cbe2ddcbd9fcf
Andrejs Cunskis <acunskis@gitlab.com>
2021-08-31 19:08:13 +0300

    Push test stats directly to influxdb instance
    Improve logging and add filename
    
    Log successful push, trim filepath
    
    Use getters for values
    
    Use job name by default for run_type
    
    Improve exported data
    
    Improve error handling
    
    Check for top upstream mr iid variable
    
    Add flag to disable metrics export
    
    Fix quarantine key name
    
    Remove nil safe operator
    
    Use single timestamp for export
    
    Use pipeline creation timestamp
    
    Remove pipeline id tag
    
    Add metrics exporter specs
    
    Do not export metrics for qa unit tests
    
    Simplify stop notification send method
    
    Remove unused variable
    
    Add pipeline_id value
    
    Separate block style let var with empty line
    
    Do not fallback on job_name if QA_RUN_TYPE is not set

--- qa/spec/spec_helper.rb
+++ qa/spec/spec_helper.rb
@@ -24,7 +24,7 @@ RSpec.configure do |config|
 
   config.add_formatter QA::Support::Formatters::ContextFormatter
   config.add_formatter QA::Support::Formatters::QuarantineFormatter
-  config.add_formatter QA::Support::Formatters::TestStatsFormatter if QA::Runtime::Env.running_in_ci?
+  config.add_formatter QA::Support::Formatters::TestStatsFormatter if QA::Runtime::Env.export_metrics?
 
   config.before do |example|
     QA::Runtime::Logger.debug("\nStarting test: #{example.full_description}\n")
