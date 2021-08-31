36c681d1bf50f2a8a76614f70629cb606be348ba
Aditya Tiwari <atiwari@gitlab.com>
2021-08-31 08:58:12 +0000

    Implements Scheduling logic for on demand DAST

--- ee/app/models/dast/profile_schedule.rb
+++ ee/app/models/dast/profile_schedule.rb
@@ -3,27 +3,72 @@
 class Dast::ProfileSchedule < ApplicationRecord
   include CronSchedulable
 
+  CRON_DEFAULT = '* * * * *'
+
   self.table_name = 'dast_profile_schedules'
 
   belongs_to :project
-  belongs_to :dast_profile, class_name: 'Dast::Profile', optional: false, inverse_of: :dast_profile_schedules
+  belongs_to :dast_profile, class_name: 'Dast::Profile', optional: false, inverse_of: :dast_profile_schedule
   belongs_to :owner, class_name: 'User', optional: true, foreign_key: :user_id
 
-  validates :cron, presence: true
-  validates :next_run_at, presence: true
+  validates :timezone, presence: true, inclusion: { in: :timezones }
+  validates :starts_at, presence: true
+  validates :cadence, json_schema: { filename: 'dast_profile_schedule_cadence', draft: 7 }
+  validates :dast_profile_id, uniqueness: true
+
+  serialize :cadence, Serializers::Json # rubocop:disable Cop/ActiveRecordSerialize
 
   scope :with_project, -> { includes(:project) }
   scope :with_profile, -> { includes(dast_profile: [:dast_site_profile, :dast_scanner_profile]) }
   scope :with_owner, -> { includes(:owner) }
   scope :active, -> { where(active: true) }
 
+  before_save :set_cron, :set_next_run_at
+
+  def repeat?
+    cadence.present?
+  end
+
+  def schedule_next_run!
+    return deactivate! unless repeat?
+
+    super
+  end
+
+  def audit_details
+    owner&.name
+  end
+
   private
 
+  def deactivate!
+    update!(active: false)
+  end
+
   def cron_timezone
-    next_run_at.zone
+    Time.zone.name
+  end
+
+  def set_cron
+    self.cron =
+      if repeat?
+        Gitlab::Ci::CronParser.parse_natural_with_timestamp(starts_at, cadence)
+      else
+        CRON_DEFAULT
+      end
+  end
+
+  def set_next_run_at
+    return super unless will_save_change_to_starts_at?
+
+    self.next_run_at = cron_worker_next_run_from(starts_at)
   end
 
   def worker_cron_expression
     Settings.cron_jobs['app_sec_dast_profile_schedule_worker']['cron']
+  end
+
+  def timezones
+    @timezones ||= ActiveSupport::TimeZone.all.map { |tz| tz.tzinfo.identifier }
   end
 end
--- ee/spec/models/dast/profile_schedule_spec.rb
+++ ee/spec/models/dast/profile_schedule_spec.rb
@@ -7,14 +7,52 @@ RSpec.describe Dast::ProfileSchedule, type: :model do
 
   describe 'associations' do
     it { is_expected.to belong_to(:project) }
-    it { is_expected.to belong_to(:dast_profile).class_name('Dast::Profile').required.inverse_of(:dast_profile_schedules) }
+    it { is_expected.to belong_to(:dast_profile).class_name('Dast::Profile').required.inverse_of(:dast_profile_schedule) }
     it { is_expected.to belong_to(:owner).class_name('User').with_foreign_key(:user_id) }
   end
 
   describe 'validations' do
+    let(:timezones) { ActiveSupport::TimeZone.all.map { |tz| tz.tzinfo.identifier } }
+
     it { is_expected.to be_valid }
-    it { is_expected.to validate_presence_of(:cron) }
-    it { is_expected.to validate_presence_of(:next_run_at) }
+    it { is_expected.to validate_presence_of(:timezone) }
+    it { is_expected.to validate_inclusion_of(:timezone).in_array(timezones) }
+    it { is_expected.to validate_presence_of(:starts_at) }
+    it { is_expected.to validate_uniqueness_of(:dast_profile_id) }
+
+    describe 'cadence' do
+      context 'when valid values' do
+        [
+          { unit: 'day',   duration: 1 },
+          { unit: 'week',  duration: 1 },
+          { unit: 'month', duration: 1 },
+          { unit: 'month', duration: 3 },
+          { unit: 'month', duration: 6 },
+          { unit: 'year',  duration: 1 },
+          {}
+        ].each do |cadence|
+          it "allows #{cadence[:unit]} values" do
+            schedule = build(:dast_profile_schedule, cadence: cadence)
+
+            expect(schedule).to be_valid
+            expect(schedule.cadence).to eq(cadence.stringify_keys)
+          end
+        end
+      end
+
+      context 'when invalid values' do
+        [
+          { unit: 'day', duration: 3 },
+          { unit: 'month_foo', duration: 100 }
+        ].each do |cadence|
+          it "disallow #{cadence[:unit]} values" do
+            expect { build(:dast_profile_schedule, cadence: cadence).validate! }.to raise_error(ActiveRecord::RecordInvalid) do |err|
+              expect(err.record.errors.full_messages).to include('Cadence must be a valid json schema')
+            end
+          end
+        end
+      end
+    end
   end
 
   describe 'scopes' do
@@ -31,13 +69,13 @@ RSpec.describe Dast::ProfileSchedule, type: :model do
       end
     end
 
-    describe 'runnable_schedules' do
+    describe '.runnable_schedules' do
       subject { described_class.runnable_schedules }
 
       context 'when there are runnable schedules' do
         let!(:profile_schedule) do
-          travel_to(1.day.ago) do
-            create(:dast_profile_schedule)
+          travel_to(2.days.ago) do
+            create(:dast_profile_schedule, cadence: { unit: 'day', duration: 1 })
           end
         end
 
@@ -80,15 +118,73 @@ RSpec.describe Dast::ProfileSchedule, type: :model do
     end
   end
 
-  describe '#set_next_run_at' do
-    it_behaves_like 'handles set_next_run_at' do
-      let(:schedule) { create(:dast_profile_schedule, cron: '*/1 * * * *') }
-      let(:schedule_1) { create(:dast_profile_schedule) }
-      let(:schedule_2) { create(:dast_profile_schedule) }
-      let(:new_cron) { '0 0 1 1 *' }
+  describe 'before_save' do
+    describe '#set_cron' do
+      context 'when repeat? is true' do
+        it 'sets the cron value' do
+          freeze_time do
+            cron_statement = Gitlab::Ci::CronParser.parse_natural_with_timestamp(subject.starts_at, subject.cadence)
 
-      let(:ideal_next_run_at) { schedule.send(:ideal_next_run_from, Time.zone.now) }
-      let(:cron_worker_next_run_at) { schedule.send(:cron_worker_next_run_from, Time.zone.now) }
+            expect(subject.cron).to eq cron_statement
+          end
+        end
+      end
+
+      context 'when repeat? is false' do
+        subject { create(:dast_profile_schedule, cadence: {}) }
+
+        it 'sets the cron value to default when non repeating' do
+          expect(subject.cron).to eq Dast::ProfileSchedule::CRON_DEFAULT
+        end
+      end
+    end
+  end
+
+  describe '#set_next_run_at' do
+    let(:schedule) { create(:dast_profile_schedule, cadence: { unit: 'day', duration: 1 }, starts_at: Time.zone.now) }
+    let(:schedule_1) { create(:dast_profile_schedule, cadence: { unit: 'day', duration: 1 }) }
+    let(:schedule_2) { create(:dast_profile_schedule, cadence: { unit: 'day', duration: 1 }) }
+
+    let(:cron_worker_next_run_at) { schedule.send(:cron_worker_next_run_from, Time.zone.now) }
+
+    context 'when schedule runs every minute' do
+      it "updates next_run_at to the worker's execution time" do
+        travel_to(1.day.ago) do
+          expect(schedule.next_run_at.to_i).to eq(cron_worker_next_run_at.to_i)
+        end
+      end
+    end
+
+    context 'when there are two different schedules in the same time zones' do
+      it 'sets the sames next_run_at' do
+        expect(schedule_1.next_run_at.to_i).to eq(schedule_2.next_run_at.to_i)
+      end
+    end
+
+    context 'when starts_at is updated for existing schedules' do
+      it 'updates next_run_at automatically' do
+        expect { schedule.update!(starts_at: Time.zone.now + 2.days) }.to change { schedule.next_run_at }
+      end
+    end
+  end
+
+  describe '#schedule_next_run!' do
+    context 'when repeat? is true' do
+      it 'sets active to true' do
+        subject.schedule_next_run!
+
+        expect(subject.active).to be true
+      end
+    end
+
+    context 'when repeat? is false' do
+      it 'sets active to false' do
+        subject.update_column(:cadence, {})
+
+        subject.schedule_next_run!
+
+        expect(subject.active).to be false
+      end
     end
   end
 end
