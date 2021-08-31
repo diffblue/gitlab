62b47eef7c86d4b147fe9574dccdc5894b8c705b
Alejandro Guerrero <argdealba@gitlab.com>
2021-08-31 22:03:41 +0000

    Strip whitespace from saml_group_name

--- ee/app/models/saml_group_link.rb
+++ ee/app/models/saml_group_link.rb
@@ -5,6 +5,8 @@ class SamlGroupLink < ApplicationRecord
 
   enum access_level: ::Gitlab::Access.options_with_owner
 
+  before_validation :strip_whitespace_from_saml_group_name
+
   validates :group, :access_level, presence: true
   validates :saml_group_name, presence: true, uniqueness: { scope: [:group_id] }, length: { maximum: 255 }
 
@@ -12,4 +14,9 @@ class SamlGroupLink < ApplicationRecord
   scope :by_saml_group_name, -> (name) { where(saml_group_name: name) }
   scope :by_group_id, ->(group_id) { where(group_id: group_id) }
   scope :preload_group, -> { preload(group: :route) }
+
+  def strip_whitespace_from_saml_group_name
+    saml_group_name.strip!
+  end
+
 end
