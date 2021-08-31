8de7a2fb5f4b3f518f8325437ed23d0afc08c5d0
Sashi Kumar Kumaresan <skumar@gitlab.com>
2021-08-31 10:55:12 +0000

    Move vulnerability statistics update out of transaction
    This commit moves the updation of vulnerability statistics
    and vulnerability historic statistics out of transaction while
    creating a vulnerability in Vulnerabilities::CreateService.
    
    Changelog: performance
    EE: true

--- ee/app/services/vulnerabilities/create_service.rb
+++ ee/app/services/vulnerabilities/create_service.rb
@@ -18,11 +18,14 @@ module Vulnerabilities
 
       Vulnerabilities::Finding.transaction(requires_new: true) do # rubocop:disable Performance/ActiveRecordSubtransactions
         save_vulnerability(vulnerability, finding)
-        Statistics::UpdateService.update_for(vulnerability)
-        HistoricalStatistics::UpdateService.update_for(@project)
       rescue ActiveRecord::RecordNotFound
         vulnerability.errors.add(:base, _('finding is not found or is already attached to a vulnerability'))
         raise ActiveRecord::Rollback
+      end
+
+      if vulnerability.persisted?
+        Statistics::UpdateService.update_for(vulnerability)
+        HistoricalStatistics::UpdateService.update_for(@project)
       end
 
       vulnerability
