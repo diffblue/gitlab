a42396a04f14559200393961c3a313b45ec4510c
nmilojevic1 <nmilojevic@gitlab.com>
2021-09-01 13:35:44 +0200

    Change job hash support multiple wal locations

--- lib/gitlab/database/load_balancing/sidekiq_client_middleware.rb
+++ lib/gitlab/database/load_balancing/sidekiq_client_middleware.rb
@@ -10,7 +10,7 @@ module Gitlab
 
           if load_balancing_enabled?(worker_class)
             job['worker_data_consistency'] = worker_class.get_data_consistency
-            set_data_consistency_location!(job) unless location_already_provided?(job)
+            set_data_consistency_locations!(job) unless job['wal_locations']
           else
             job['worker_data_consistency'] = ::WorkerAttributes::DEFAULT_DATA_CONSISTENCY
           end
@@ -27,16 +27,24 @@ module Gitlab
             worker_class.get_data_consistency_feature_flag_enabled?
         end
 
-        def set_data_consistency_location!(job)
-          if Session.current.use_primary?
-            job['database_write_location'] = load_balancer.primary_write_location
-          else
-            job['database_replica_location'] = load_balancer.host.database_replica_location
-          end
+        def set_data_consistency_locations!(job)
+          # Once we add support for multiple databases to our load balancer, we would use something like this:
+          #   job['wal_locations'] = Gitlab::Database::DATABASES.transform_values do |connection|
+          #      connection.load_balancer.primary_write_location.
+          #   end
+          #
+          # TODO: Replace hardcoded database config name :main when we merge unification strategy
+          # https://gitlab.com/gitlab-org/gitlab/-/issues/336566
+          wal_location = calculate_wal_location
+          job['wal_locations'] = { main: wal_location } if wal_location
         end
 
-        def location_already_provided?(job)
-          job['database_replica_location'] || job['database_write_location']
+        def calculate_wal_location
+          if Session.current.use_primary?
+            load_balancer.primary_write_location
+          else
+            load_balancer.host.database_replica_location
+          end
         end
 
         def load_balancer
