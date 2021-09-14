6692bcccd2da273f40329d5b34b97838bd0a398e
Adam Hegyi <ahegyi@gitlab.com>
2021-09-14 13:38:44 +0200

    Allow left-join stage events
    This change adds the capability to left-join stage event timestamp
    columns.

--- ee/spec/lib/gitlab/analytics/cycle_analytics/stage_events/issue_closed_spec.rb
+++ ee/spec/lib/gitlab/analytics/cycle_analytics/stage_events/issue_closed_spec.rb
@@ -4,4 +4,9 @@ require 'spec_helper'
 
 RSpec.describe Gitlab::Analytics::CycleAnalytics::StageEvents::IssueClosed do
   it_behaves_like 'value stream analytics event'
+
+  it_behaves_like 'LEFT JOIN-able value stream analytics event' do
+    let_it_be(:record_with_data) { create(:issue, closed_at: Time.current) }
+    let_it_be(:record_without_data) { create(:issue) }
+  end
 end
