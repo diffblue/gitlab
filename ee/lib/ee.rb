8723e980dbb5b969e0b024c0bb8f923c98763caf
Zhu Shung <memorycancel@gmail.com>
2021-09-17 15:20:13 +0000

    Use methods to define constants in ee/lib/ee.rb

--- ee/lib/ee.rb
+++ ee/lib/ee.rb
@@ -2,15 +2,15 @@
 
 module EE
   SUBSCRIPTIONS_URL = ::Gitlab::SubscriptionPortal::SUBSCRIPTIONS_URL
-  SUBSCRIPTIONS_COMPARISON_URL = "https://about.gitlab.com/pricing/gitlab-com/feature-comparison"
-  SUBSCRIPTIONS_GRAPHQL_URL = "#{SUBSCRIPTIONS_URL}/graphql"
-  SUBSCRIPTIONS_MORE_MINUTES_URL = "#{SUBSCRIPTIONS_URL}/buy_pipeline_minutes"
-  SUBSCRIPTIONS_MORE_STORAGE_URL = "#{SUBSCRIPTIONS_URL}/buy_storage"
-  SUBSCRIPTIONS_MANAGE_URL = "#{SUBSCRIPTIONS_URL}/subscriptions"
-  SUBSCRIPTIONS_PLANS_URL = "#{SUBSCRIPTIONS_URL}/plans"
-  SUBSCRIPTION_PORTAL_ADMIN_EMAIL = ENV.fetch('SUBSCRIPTION_PORTAL_ADMIN_EMAIL', 'gl_com_api@gitlab.com')
-  SUBSCRIPTION_PORTAL_ADMIN_TOKEN = ENV.fetch('SUBSCRIPTION_PORTAL_ADMIN_TOKEN', 'customer_admin_token')
-  CUSTOMER_SUPPORT_URL = 'https://support.gitlab.com'
-  CUSTOMER_LICENSE_SUPPORT_URL = 'https://support.gitlab.com/hc/en-us/requests/new?ticket_form_id=360000071293'
-  GITLAB_COM_STATUS_URL = "https://status.gitlab.com"
+  SUBSCRIPTIONS_COMPARISON_URL = ::Gitlab::SubscriptionPortal.subscriptions_comparison_url.freeze
+  SUBSCRIPTIONS_GRAPHQL_URL = ::Gitlab::SubscriptionPortal.subscriptions_graphql_url.freeze
+  SUBSCRIPTIONS_MORE_MINUTES_URL = ::Gitlab::SubscriptionPortal.subscriptions_more_minutes_url.freeze
+  SUBSCRIPTIONS_MORE_STORAGE_URL = ::Gitlab::SubscriptionPortal.subscriptions_more_storage_url.freeze
+  SUBSCRIPTIONS_MANAGE_URL = ::Gitlab::SubscriptionPortal.subscriptions_manage_url.freeze
+  SUBSCRIPTIONS_PLANS_URL = ::Gitlab::SubscriptionPortal.subscriptions_plans_url.freeze
+  SUBSCRIPTION_PORTAL_ADMIN_EMAIL = ::Gitlab::SubscriptionPortal.subscription_portal_admin_email.freeze
+  SUBSCRIPTION_PORTAL_ADMIN_TOKEN = ::Gitlab::SubscriptionPortal.subscription_portal_admin_token.freeze
+  CUSTOMER_SUPPORT_URL = ::Gitlab::Saas.customer_support_url.freeze
+  CUSTOMER_LICENSE_SUPPORT_URL = ::Gitlab::Saas.customer_license_support_url.freeze
+  GITLAB_COM_STATUS_URL = ::Gitlab::Saas.gitlab_com_status_url.freeze
 end
