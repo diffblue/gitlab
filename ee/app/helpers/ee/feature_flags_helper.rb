ced6a706001fffa62e64ca9407d63908166825e2
Andrew Fontaine <afontaine@gitlab.com>
2021-09-17 11:36:11 -0400

    Refactor Edit Feature Flags Data Set
    Instead of adding several methods to the feature flags helper that then
    need to be overridden in the EE version, we can create one method that
    returns the entire hash of values and then append to that map with the
    EE only properties by overriding that single method.

--- ee/app/helpers/ee/feature_flags_helper.rb
+++ ee/app/helpers/ee/feature_flags_helper.rb
@@ -4,7 +4,13 @@ module EE
   module FeatureFlagsHelper
     extend ::Gitlab::Utils::Override
 
-    override :feature_flag_issues_links_endpoint
+    override :edit_feature_flag_data
+    def edit_feature_flag_data
+      super.merge(feature_flag_issues_endpoint: feature_flag_issues_links_endpoint(@project, @feature_flag, current_user))
+    end
+
+    private
+
     def feature_flag_issues_links_endpoint(project, feature_flag, user)
       return '' unless can?(user, :admin_feature_flags_issue_links, project)
 
