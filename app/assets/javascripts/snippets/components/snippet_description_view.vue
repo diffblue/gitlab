54d658f752b9eba2a3ed5234aaacf839bfaa2da3
imrishabh18 <belikerishabh@gmail.com>
2021-09-21 17:48:29 +0000

    refactor: replacing v-html with v-safe-html

--- app/assets/javascripts/snippets/components/snippet_description_view.vue
+++ app/assets/javascripts/snippets/components/snippet_description_view.vue
@@ -1,9 +1,13 @@
 <script>
+import { GlSafeHTMLDirective as Safehtml } from "@gitlab/ui";
 import MarkdownFieldView from '~/vue_shared/components/markdown/field_view.vue';
 
 export default {
   components: {
     MarkdownFieldView,
+  },
+  directives: {
+    Safehtml
   },
   props: {
     description: {
@@ -18,7 +22,7 @@ export default {
   <markdown-field-view class="snippet-description" data-qa-selector="snippet_description_content">
     <div
       class="md js-snippet-description"
-      v-html="description /* eslint-disable-line vue/no-v-html */"
+      v-safe-html="description"
     ></div>
   </markdown-field-view>
 </template>
