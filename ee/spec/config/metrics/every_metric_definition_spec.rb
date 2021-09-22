96806577b479cddf2e0e1b0a8a417702ee5e3865
Siddharth Asthana <siddharthasthana31@gmail.com>
2021-09-22 23:34:09 +0530

    Remove metrics that keeps count of deleted container images
    Changelog: other
    EE: true

--- ee/spec/config/metrics/every_metric_definition_spec.rb
+++ ee/spec/config/metrics/every_metric_definition_spec.rb
@@ -14,6 +14,7 @@ RSpec.describe 'Every metric definition' do
       package_events_i_package_container_push_package
       package_events_i_package_container_pull_package
       package_events_i_package_debian_push_package
+      package_events_i_package_container_delete_package
     ).freeze
   end
 
