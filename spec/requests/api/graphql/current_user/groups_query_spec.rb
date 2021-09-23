c008db060ca456662e80a58d4db38bf47fa12999
Rajendra Kadam <rkadam@gitlab.com>
2021-09-23 15:36:09 +0530

    Revert change for N+1 specs

--- spec/requests/api/graphql/current_user/groups_query_spec.rb
+++ spec/requests/api/graphql/current_user/groups_query_spec.rb
@@ -45,8 +45,7 @@ RSpec.describe 'Query current user groups' do
     new_group = create(:group, :private)
     new_group.add_maintainer(current_user)
 
-    # Adds an extra query for checking ip restrictions on group
-    expect { post_graphql(query, current_user: current_user) }.not_to exceed_query_limit(control).with_threshold(1)
+    expect { post_graphql(query, current_user: current_user) }.not_to exceed_query_limit(control)
   end
 
   it 'returns all groups where the user is a direct member' do
