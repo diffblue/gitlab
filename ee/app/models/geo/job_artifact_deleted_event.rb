6cf2a77e66605167ad7372ff2e5886316b872ec2
Krasimir Angelov <kangelov@gitlab.com>
2021-09-27 14:33:44 +1300

    Remove column ignore rules for migrated PKs
    Remove column ignore rules for `ci_build_needs`,
    `ci_build_trace_chunks`, `ci_builds_runner_session`, `ci_stages`,
    `deployments`, `geo_job_artifact_deleted_events`:
    
    * https://gitlab.com/gitlab-org/gitlab/-/issues/338060
    * https://gitlab.com/gitlab-org/gitlab/-/issues/338069
    * https://gitlab.com/gitlab-org/gitlab/-/issues/338057
    * https://gitlab.com/gitlab-org/gitlab/-/issues/338066
    * https://gitlab.com/gitlab-org/gitlab/-/issues/338054
    * https://gitlab.com/gitlab-org/gitlab/-/issues/338050
    
    All columns were removed in 14.3.

--- ee/app/models/geo/job_artifact_deleted_event.rb
+++ ee/app/models/geo/job_artifact_deleted_event.rb
@@ -7,8 +7,6 @@ module Geo
     include BulkInsertSafe
     include IgnorableColumns
 
-    ignore_column :job_artifact_id_convert_to_bigint, remove_with: '14.2', remove_after: '2021-08-22'
-
     belongs_to :job_artifact, class_name: 'Ci::JobArtifact'
 
     validates :job_artifact, :file_path, presence: true
