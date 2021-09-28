13e3ca75327523f4c088b40b6fb661d5cf2c3215
Jeremy Wu <jeremyw@jihulab.com>
2021-09-28 11:44:19 +0000

    Feature ZenTao integration: Integrate ZenTao with External Issue

---
+++ ee/app/assets/javascripts/integrations/zentao/issues_list/zentao_issues_list_bundle.js
@@ -0,0 +1,23 @@
+import externalIssuesListFactory from 'ee/external_issues_list';
+import zentaoLogo from 'images/logos/zentao.svg';
+import { s__ } from '~/locale';
+import getIssuesQuery from './graphql/queries/get_zentao_issues.query.graphql';
+import zentaoIssues from './graphql/resolvers/zentao_issues';
+
+export default externalIssuesListFactory({
+  query: zentaoIssues,
+  provides: {
+    getIssuesQuery,
+    externalIssuesLogo: zentaoLogo,
+    // This like below is passed to <gl-sprintf :message="%authorName in {}" />
+    // So we don't translate it since this should be a proper noun
+    externalIssueTrackerName: 'ZenTao',
+    searchInputPlaceholderText: s__('Integrations|Search ZenTao issues'),
+    recentSearchesStorageKey: 'zentao_issues',
+    createNewIssueText: s__('Integrations|Create new issue in ZenTao'),
+    logoContainerClass: 'logo-container',
+    emptyStateNoIssueText: s__(
+      'Integrations|ZenTao issues display here when you create issues in your project in ZenTao.',
+    ),
+  },
+});
---
+++ ee/app/assets/javascripts/pages/projects/integrations/zentao/issues/index/index.js
@@ -0,0 +1,3 @@
+import initZentaoIssuesList from 'ee/integrations/zentao/issues_list/zentao_issues_list_bundle';
+
+initZentaoIssuesList({ mountPointSelector: '.js-zentao-issues-list' });
