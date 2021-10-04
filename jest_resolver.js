e2069f7963288b6d9693f48f1ea105f34d3c1968
Albert Salim <asalim@gitlab.com>
2021-10-04 15:20:21 +0800

    Remove dependency on jest-resolver

--- jest_resolver.js
+++ jest_resolver.js
@@ -1,15 +1,11 @@
-/* eslint-disable import/no-commonjs */
-/* eslint-disable import/no-extraneous-dependencies */
-const Resolver = require('jest-resolve');
+const fs = require('fs');
 
 // Wrap jest default resolver to detect missing frontend fixtures.
 module.exports = (request, options) => {
   try {
     return options.defaultResolver(request, options);
   } catch (e) {
-    if (Resolver.tryCastModuleNotFoundError(e) && request.match(/tmp\/tests\/frontend\/fixtures/)) {
-      /* eslint-disable no-console */
-      /* eslint-disable @gitlab/require-i18n-strings */
+    if (request.match(/tmp\/tests\/frontend\/fixtures/) && !fs.existsSync(request)) {
       console.error(
         '\x1b[1m\x1b[41m\x1b[30m %s \x1b[0m %s',
         '!',
