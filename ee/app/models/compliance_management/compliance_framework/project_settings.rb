973da3fc3a4b28a587c24118803d39b2998618d7
Max Woolf <mwoolf@gitlab.com>
2021-10-04 10:18:30 +0100

    Remove ignoring framework column for compliance project settings
    Removes the ignore statement on framework column for
    compliance management frameworks. This column was removed months ago
    and is no longer used.
    
    Changelog: removed
    EE: true

--- ee/app/models/compliance_management/compliance_framework/project_settings.rb
+++ ee/app/models/compliance_management/compliance_framework/project_settings.rb
@@ -5,12 +5,8 @@ require_dependency 'compliance_management/compliance_framework'
 module ComplianceManagement
   module ComplianceFramework
     class ProjectSettings < ApplicationRecord
-      include IgnorableColumns
-
       self.table_name = 'project_compliance_framework_settings'
       self.primary_key = :project_id
-
-      ignore_columns :framework, remove_after: '2020-12-06', remove_with: '13.7'
 
       belongs_to :project
       belongs_to :compliance_management_framework, class_name: "ComplianceManagement::Framework", foreign_key: :framework_id
