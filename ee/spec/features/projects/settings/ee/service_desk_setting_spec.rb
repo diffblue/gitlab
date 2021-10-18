9887a3617731374cea9ef4599bb75f4d2325e6a6
Rajat Jain <rjain@gitlab.com>
2021-10-18 10:29:24 +0530

    Render inherited templates from group in service desk
    Right now, only project level templates are rendered in the service desk.
    With the introduction of inheritance, a project has access to group level
    templates as well. With this change, you can see all the templates available
    -- project level + group level.
    
    Changelog: added

--- ee/spec/features/projects/settings/ee/service_desk_setting_spec.rb
+++ ee/spec/features/projects/settings/ee/service_desk_setting_spec.rb
@@ -17,14 +17,22 @@ RSpec.describe 'Service Desk Setting', :js, :clean_gitlab_redis_cache do
     }
   end
 
+  let_it_be(:issue_template_file) do
+    {
+      '.gitlab/issue_templates/template.md' => 'Template file contents'
+    }
+  end
+
+  let_it_be(:project_with_issue_template) { create(:project, :custom_repo, files: issue_template_file) }
   let_it_be_with_reload(:group) { create(:group)}
   let_it_be_with_reload(:project) { create(:project, :custom_repo, group: group, files: issuable_project_template_files) }
-  let_it_be(:group_template_repo) { create(:project, :custom_repo, group: group, files: issuable_group_template_files) }
+  let_it_be(:group_template_repo) { create(:project, :custom_repo, files: issuable_group_template_files) }
   let_it_be(:user) { create(:user) }
   let_it_be(:presenter) { project.present(current_user: user) }
 
   before do
     stub_licensed_features(custom_file_templates_for_namespace: true, custom_file_templates: true)
+    stub_ee_application_setting(file_template_project: project_with_issue_template)
 
     project.add_maintainer(user)
     sign_in(user)
@@ -39,9 +47,28 @@ RSpec.describe 'Service Desk Setting', :js, :clean_gitlab_redis_cache do
       expect(proj_instance).to receive(:present).with(current_user: user).and_return(presenter)
     end
 
+    create(:project_group_link, project: group_template_repo, group: group)
     group.update_columns(file_template_project_id: group_template_repo.id)
     visit edit_project_path(project)
   end
 
-  it_behaves_like 'issue description templates from current project only'
+  it 'loads group, project and instance issue description templates', :aggregate_failures do
+    within('#service-desk-template-select') do
+      expect(page).to have_content(:all, 'project-issue-bar')
+      expect(page).to have_content(:all, 'project-issue-foo')
+      expect(page).to have_content(:all, 'group-issue-bar')
+      expect(page).to have_content(:all, 'group-issue-foo')
+      expect(page).to have_content(:all, 'template')
+    end
+  end
+
+  it 'persists file_template_project_id on save' do
+    find('#service-desk-template-select').click
+    find('.gl-new-dropdown-item-text-primary', exact_text: 'template').click
+    find('[data-testid="save_service_desk_settings_button"]').click
+
+    wait_for_requests
+
+    expect(project.service_desk_setting.file_template_project_id).to eq(project_with_issue_template.id)
+  end
 end
