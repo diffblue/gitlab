cc875bcf654978d41aca7384ace8020aa21abc42
Vitali Tatarintev <Kalastiuz@gmail.com>
2021-10-19 16:14:07 +0200

    Resolve escalation status when incident is closed

--- spec/services/issues/close_service_spec.rb
+++ spec/services/issues/close_service_spec.rb
@@ -83,6 +83,10 @@ RSpec.describe Issues::CloseService do
       service.execute(issue)
     end
 
+    it 'does not change escalation status' do
+      expect { service.execute(issue) }.not_to change { IncidentManagement::IssuableEscalationStatus.where(issue: issue).count }
+    end
+
     context 'issue is incident type' do
       let(:issue) { create(:incident, project: project) }
       let(:current_user) { user }
@@ -90,6 +94,22 @@ RSpec.describe Issues::CloseService do
       subject { service.execute(issue) }
 
       it_behaves_like 'an incident management tracked event', :incident_management_incident_closed
+
+      it 'creates a new escalation resolved escalation status', :aggregate_failures do
+        expect { service.execute(issue) }.to change { IncidentManagement::IssuableEscalationStatus.where(issue: issue).count }.to(1)
+
+        expect(issue.incident_management_issuable_escalation_status).to be_resolved
+      end
+
+      context 'when there is an escalation status' do
+        before do
+          create(:incident_management_issuable_escalation_status, issue: issue)
+        end
+
+        it 'changes escalations status to resolved' do
+          expect { service.execute(issue) }.to change { issue.incident_management_issuable_escalation_status.reload.resolved? }.to(true)
+        end
+      end
     end
   end
 
@@ -237,7 +257,7 @@ RSpec.describe Issues::CloseService do
 
       it 'verifies the number of queries' do
         recorded = ActiveRecord::QueryRecorder.new { close_issue }
-        expected_queries = 27
+        expected_queries = 32
 
         expect(recorded.count).to be <= expected_queries
         expect(recorded.cached_count).to eq(0)
