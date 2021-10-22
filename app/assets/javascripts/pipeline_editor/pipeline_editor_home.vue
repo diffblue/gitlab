a062e45a40d6ebd7f14697ae6177a7317d671cc4
Mireya Andres <mandres@gitlab.com>
2021-10-22 14:46:17 +0000

    Show linked pipelines mini graph in the pipeline editor

--- app/assets/javascripts/pipeline_editor/pipeline_editor_home.vue
+++ app/assets/javascripts/pipeline_editor/pipeline_editor_home.vue
@@ -58,6 +58,7 @@ export default {
       :ci-config-data="ciConfigData"
       :commit-sha="commitSha"
       :is-new-ci-config-file="isNewCiConfigFile"
+      v-on="$listeners"
     />
     <pipeline-editor-tabs
       :ci-config-data="ciConfigData"
