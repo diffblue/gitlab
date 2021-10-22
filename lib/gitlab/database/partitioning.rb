fa7ed8b6821ad71dfbd56a2a817b4a0eb6cba266
Lin Jen-Shin <jen-shin@gitlab.com>
2021-10-22 18:22:27 +0800

    Mark some module methods as private

--- lib/gitlab/database/partitioning.rb
+++ lib/gitlab/database/partitioning.rb
@@ -18,54 +18,58 @@ module Gitlab
         end
       end
 
-      def self.register_models(models)
-        registered_models.merge(models)
-      end
-
-      def self.register_tables(tables)
-        registered_tables.merge(tables)
-      end
-
-      def self.registered_models
-        @registered_models ||= Set.new
-      end
-
-      def self.registered_tables
-        @registered_tables ||= Set.new
-      end
-
-      def self.registered_for_sync
-        registered_models + registered_tables.map do |table|
-          TableWithoutModel.new(**table)
-        end
-      end
-
-      def self.sync_partitions(models_to_sync = registered_for_sync)
-        Gitlab::AppLogger.info(message: 'Syncing dynamic postgres partitions')
-
-        Gitlab::Database::EachDatabase.each_model_connection(models_to_sync) do |model|
-          PartitionManager.new(model).sync_partitions
+      class << self
+        def register_models(models)
+          registered_models.merge(models)
         end
 
-        Gitlab::AppLogger.info(message: 'Finished sync of dynamic postgres partitions')
-      end
-
-      def self.report_metrics(models_to_monitor = registered_models)
-        partition_monitoring = PartitionMonitoring.new
-
-        Gitlab::Database::EachDatabase.each_model_connection(models_to_monitor) do |model|
-          partition_monitoring.report_metrics_for_model(model)
-        end
-      end
-
-      def self.drop_detached_partitions
-        Gitlab::AppLogger.info(message: 'Dropping detached postgres partitions')
-
-        Gitlab::Database::EachDatabase.each_database_connection do
-          DetachedPartitionDropper.new.perform
+        def register_tables(tables)
+          registered_tables.merge(tables)
         end
 
-        Gitlab::AppLogger.info(message: 'Finished dropping detached postgres partitions')
+        def sync_partitions(models_to_sync = registered_for_sync)
+          Gitlab::AppLogger.info(message: 'Syncing dynamic postgres partitions')
+
+          Gitlab::Database::EachDatabase.each_model_connection(models_to_sync) do |model|
+            PartitionManager.new(model).sync_partitions
+          end
+
+          Gitlab::AppLogger.info(message: 'Finished sync of dynamic postgres partitions')
+        end
+
+        def report_metrics(models_to_monitor = registered_models)
+          partition_monitoring = PartitionMonitoring.new
+
+          Gitlab::Database::EachDatabase.each_model_connection(models_to_monitor) do |model|
+            partition_monitoring.report_metrics_for_model(model)
+          end
+        end
+
+        def drop_detached_partitions
+          Gitlab::AppLogger.info(message: 'Dropping detached postgres partitions')
+
+          Gitlab::Database::EachDatabase.each_database_connection do
+            DetachedPartitionDropper.new.perform
+          end
+
+          Gitlab::AppLogger.info(message: 'Finished dropping detached postgres partitions')
+        end
+
+        private
+
+        def registered_models
+          @registered_models ||= Set.new
+        end
+
+        def registered_tables
+          @registered_tables ||= Set.new
+        end
+
+        def registered_for_sync
+          registered_models + registered_tables.map do |table|
+            TableWithoutModel.new(**table)
+          end
+        end
       end
     end
   end
