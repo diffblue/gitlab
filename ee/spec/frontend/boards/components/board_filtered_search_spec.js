777720997f6c9252f87ee9bd14a1bf22f3760ba1
sstern <sstern@gitlab.com>
2021-10-25 10:50:54 -0700

    Fix board scope issue for issue board filtered search

--- ee/spec/frontend/boards/components/board_filtered_search_spec.js
+++ ee/spec/frontend/boards/components/board_filtered_search_spec.js
@@ -1,12 +1,22 @@
 import { shallowMount } from '@vue/test-utils';
+import Vue from 'vue';
+import Vuex from 'vuex';
 import BoardFilteredSearch from 'ee/boards/components/board_filtered_search.vue';
 import BoardFilteredSearchCe from '~/boards/components/board_filtered_search.vue';
+import { createStore } from '~/boards/stores';
+import * as urlUtility from '~/lib/utils/url_utility';
+
+Vue.use(Vuex);
 
 describe('ee/BoardFilteredSearch', () => {
   let wrapper;
+  let store;
 
   const createComponent = () => {
-    wrapper = shallowMount(BoardFilteredSearch, { propsData: { tokens: [] } });
+    wrapper = shallowMount(BoardFilteredSearch, {
+      store,
+      propsData: { tokens: [] },
+    });
   };
 
   const findFilteredSearch = () => wrapper.findComponent(BoardFilteredSearchCe);
@@ -15,12 +25,55 @@ describe('ee/BoardFilteredSearch', () => {
     wrapper.destroy();
   });
 
-  describe('default', () => {
+  describe('when boardScopeConfig watcher is triggered', () => {
+    beforeEach(async () => {
+      store = createStore();
+
+      createComponent();
+
+      jest.spyOn(store, 'dispatch').mockImplementation();
+      jest.spyOn(urlUtility, 'updateHistory');
+
+      store.state.boardConfig = { labels: [{ title: 'test', color: 'black', id: '1' }] };
+
+      await wrapper.vm.$nextTick();
+    });
+
+    it('calls performSearch', () => {
+      expect(store.dispatch).toHaveBeenCalledWith('performSearch');
+    });
+
+    it('calls historyPushState', () => {
+      expect(urlUtility.updateHistory).toHaveBeenCalledWith({
+        url: '?label_name[]=test',
+      });
+    });
+
+    it('passes the correct props to BoardFilteredSearchCe', () => {
+      expect(findFilteredSearch().props()).toEqual(
+        expect.objectContaining({ eeFilters: { labelName: ['test'] } }),
+      );
+    });
+  });
+
+  describe('when resetFilters is true and boardConfig is not empty', () => {
     beforeEach(() => {
+      store = createStore();
+
       createComponent();
     });
 
-    it('renders FilteredSearch', () => {
+    it('renders BoardFilteredSearchCe', async () => {
+      store.state.boardConfig = {};
+
+      await wrapper.vm.$nextTick();
+
+      expect(findFilteredSearch().exists()).toEqual(false);
+
+      store.state.boardConfig = { labels: [] };
+
+      await wrapper.vm.$nextTick();
+
       expect(findFilteredSearch().exists()).toBe(true);
     });
   });
