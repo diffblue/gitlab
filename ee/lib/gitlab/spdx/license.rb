611d581864c9c1bc5159d235d4d7bac70f1f3905
Reuben Pereira <rpereira@gitlab.com>
2021-11-01 13:55:49 +0530

    Rubocop: Mark ee/lib/gitlab/spdx/license.rb as todo for naming
    We can fix the naming once the upstream PR
    https://github.com/rubocop/rubocop/pull/10223 is merged. The PR will
    fix issue https://github.com/rubocop/rubocop/issues/10221.

--- ee/lib/gitlab/spdx/license.rb
+++ ee/lib/gitlab/spdx/license.rb
@@ -1,4 +1,7 @@
-# rubocop:disable Naming/FileName
+# rubocop:todo Naming/FileName
+# Upstream issue: https://github.com/rubocop/rubocop/issues/10221
+# Upstream fix PR: https://github.com/rubocop/rubocop/pull/10223
+
 # frozen_string_literal: true
 
 module Gitlab
@@ -6,5 +9,3 @@ module Gitlab
     License = Struct.new(:id, :name, :deprecated, keyword_init: true)
   end
 end
-
-# rubocop:enable Naming/FileName
