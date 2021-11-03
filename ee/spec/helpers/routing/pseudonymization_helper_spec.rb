7df113e9256747683017433639fe508fa334e664
Rajendra Kadam <rkadam@gitlab.com>
2021-11-03 08:41:50 +0000

    Fix pseudonymization helper
    For group and project insight 
    controllers pseudonymization helper raise
    errors, due to unrecognized path. This is 
    caused by the way how Rails handle internally
    constraints. This commit add special 
    handling for those cases

---
+++ ee/spec/helpers/routing/pseudonymization_helper_spec.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe ::Routing::PseudonymizationHelper do
+  let_it_be(:group) { create(:group) }
+  let_it_be(:project) { create(:project, group: group) }
+
+  before do
+    stub_feature_flags(mask_page_urls: true)
+    allow(helper).to receive(:group).and_return(group)
+    allow(helper).to receive(:project).and_return(project)
+  end
+
+  shared_examples 'masked url' do
+    it 'generates masked page url' do
+      expect(helper.masked_page_url).to eq(masked_url)
+    end
+  end
+
+  describe 'when url has params to mask' do
+    context 'when project/insights page is loaded' do
+      let(:masked_url) { "http://localhost//namespace#{group.id}/project#{project.id}/insights/" }
+      let(:request) do
+        double(:Request,
+          path_parameters: {
+                controller: 'projects/insights',
+                action: 'show',
+                namespace_id: group.name,
+                project_id: project.name
+               },
+               protocol: 'http',
+               host: 'localhost',
+               query_string: '')
+      end
+
+      before do
+        allow(helper).to receive(:request).and_return(request)
+      end
+
+      it_behaves_like 'masked url'
+    end
+
+    context 'when groups/insights page is loaded' do
+      let(:masked_url) { "http://localhost//groups/namespace#{group.id}/-/insights/" }
+      let(:request) do
+        double(:Request,
+          path_parameters: {
+                controller: 'groups/insights',
+                action: 'show',
+                group_id: group.name
+               },
+               protocol: 'http',
+               host: 'localhost',
+               query_string: '')
+      end
+
+      before do
+        allow(helper).to receive(:request).and_return(request)
+      end
+
+      it_behaves_like 'masked url'
+    end
+  end
+end
