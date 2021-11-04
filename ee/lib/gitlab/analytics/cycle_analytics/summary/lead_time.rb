bab37ef94b7a3ace02cd86b742bf09b988bcd8a4
Martin Wortschack <mwortschack@gitlab.com>
2021-11-04 18:47:27 +0000

    [VSA] Make lead time tile a link to lead time chart

--- ee/lib/gitlab/analytics/cycle_analytics/summary/lead_time.rb
+++ ee/lib/gitlab/analytics/cycle_analytics/summary/lead_time.rb
@@ -16,6 +16,22 @@ module Gitlab
           def end_event_identifier
             :issue_closed
           end
+
+          def links
+            helpers = Gitlab::Routing.url_helpers
+
+            dashboard_link =
+              if @stage.parent.is_a?(::Group)
+                helpers.group_analytics_ci_cd_analytics_path(@stage.parent, tab: 'lead-time')
+              else
+                helpers.charts_project_pipelines_path(@stage.parent, chart: 'lead-time')
+              end
+
+            [
+              { "name" => _('Lead Time'), "url" => dashboard_link, "label" => s_('ValueStreamAnalytics|Dashboard') },
+              { "name" => _('Lead Time'), "url" => helpers.help_page_path('user/analytics/index', anchor: 'definitions'), "docs_link" => true, "label" => s_('ValueStreamAnalytics|Go to docs') }
+            ]
+          end
         end
       end
     end
