b000e17571d15d04b89bc0264495dfa7a6b65d92
Tom Quirk <tquirk@gitlab.com>
2021-11-04 15:02:05 +0930

    Reorganise Jira Connect UI
    The commit reorganises the UI logic for the Jira Connect
    App. This is part-UI enhancement, part tech-debt cleanup.
    
    To the user, the main change is that the Add Namespace button
    is now in the empty state when the user is signed in but has no
    subscriptions.
    
    This restructuring also enables us to move some HAML
    markup to Vue.
    
    Changelog: changed

--- spec/frontend/jira_connect/subscriptions/components/app_spec.js
+++ spec/frontend/jira_connect/subscriptions/components/app_spec.js
@@ -1,4 +1,4 @@
-import { GlAlert, GlLink } from '@gitlab/ui';
+import { GlAlert, GlLink, GlEmptyState } from '@gitlab/ui';
 import { mount, shallowMount } from '@vue/test-utils';
 
 import JiraConnectApp from '~/jira_connect/subscriptions/components/app.vue';
@@ -7,6 +7,7 @@ import SignInButton from '~/jira_connect/subscriptions/components/sign_in_button
 import createStore from '~/jira_connect/subscriptions/store';
 import { SET_ALERT } from '~/jira_connect/subscriptions/store/mutation_types';
 import { __ } from '~/locale';
+import { mockSubscription } from '../mock_data';
 
 jest.mock('~/jira_connect/subscriptions/utils', () => ({
   retrieveAlert: jest.fn().mockReturnValue({ message: 'error message' }),
@@ -20,6 +21,7 @@ describe('JiraConnectApp', () => {
   const findAlertLink = () => findAlert().findComponent(GlLink);
   const findSignInButton = () => wrapper.findComponent(SignInButton);
   const findAddNamespaceButton = () => wrapper.findComponent(AddNamespaceButton);
+  const findEmptyState = () => wrapper.findComponent(GlEmptyState);
 
   const createComponent = ({ provide, mountFn = shallowMount } = {}) => {
     store = createStore();
@@ -34,7 +36,7 @@ describe('JiraConnectApp', () => {
     wrapper.destroy();
   });
 
-  describe('template', () => {
+  describe('with subscriptions', () => {
     describe.each`
       scenario                   | usersPath    | expectSignInButton | expectNamespaceButton
       ${'user is not signed in'} | ${'/users'}  | ${true}            | ${false}
@@ -44,6 +46,7 @@ describe('JiraConnectApp', () => {
         createComponent({
           provide: {
             usersPath,
+            subscriptions: [mockSubscription],
           },
         });
       });
@@ -56,71 +59,96 @@ describe('JiraConnectApp', () => {
         expect(findAddNamespaceButton().exists()).toBe(expectNamespaceButton);
       });
     });
+  });
 
-    describe('alert', () => {
-      it.each`
-        message          | variant      | alertShouldRender
-        ${'Test error'}  | ${'danger'}  | ${true}
-        ${'Test notice'} | ${'info'}    | ${true}
-        ${''}            | ${undefined} | ${false}
-        ${undefined}     | ${undefined} | ${false}
-      `(
-        'renders correct alert when message is `$message` and variant is `$variant`',
-        async ({ message, alertShouldRender, variant }) => {
-          createComponent();
+  describe('with no subscriptions', () => {
+    describe.each`
+      scenario                   | usersPath    | expectSignInButton | expectEmptyState
+      ${'user is not signed in'} | ${'/users'}  | ${true}            | ${false}
+      ${'user is signed in'}     | ${undefined} | ${false}           | ${true}
+    `('when $scenario', ({ usersPath, expectSignInButton, expectEmptyState }) => {
+      beforeEach(() => {
+        createComponent({
+          provide: {
+            usersPath,
+            subscriptions: [],
+          },
+        });
+      });
 
-          store.commit(SET_ALERT, { message, variant });
-          await wrapper.vm.$nextTick();
+      it('renders sign in button as expected', () => {
+        expect(findSignInButton().exists()).toBe(expectSignInButton);
+      });
 
-          const alert = findAlert();
+      it('renders empty state as expected', () => {
+        expect(findEmptyState().exists()).toBe(expectEmptyState);
+      });
+    });
+  });
 
-          expect(alert.exists()).toBe(alertShouldRender);
-          if (alertShouldRender) {
-            expect(alert.isVisible()).toBe(alertShouldRender);
-            expect(alert.html()).toContain(message);
-            expect(alert.props('variant')).toBe(variant);
-            expect(findAlertLink().exists()).toBe(false);
-          }
-        },
-      );
-
-      it('hides alert on @dismiss event', async () => {
+  describe('alert', () => {
+    it.each`
+      message          | variant      | alertShouldRender
+      ${'Test error'}  | ${'danger'}  | ${true}
+      ${'Test notice'} | ${'info'}    | ${true}
+      ${''}            | ${undefined} | ${false}
+      ${undefined}     | ${undefined} | ${false}
+    `(
+      'renders correct alert when message is `$message` and variant is `$variant`',
+      async ({ message, alertShouldRender, variant }) => {
         createComponent();
 
-        store.commit(SET_ALERT, { message: 'test message' });
+        store.commit(SET_ALERT, { message, variant });
         await wrapper.vm.$nextTick();
 
-        findAlert().vm.$emit('dismiss');
-        await wrapper.vm.$nextTick();
+        const alert = findAlert();
 
-        expect(findAlert().exists()).toBe(false);
+        expect(alert.exists()).toBe(alertShouldRender);
+        if (alertShouldRender) {
+          expect(alert.isVisible()).toBe(alertShouldRender);
+          expect(alert.html()).toContain(message);
+          expect(alert.props('variant')).toBe(variant);
+          expect(findAlertLink().exists()).toBe(false);
+        }
+      },
+    );
+
+    it('hides alert on @dismiss event', async () => {
+      createComponent();
+
+      store.commit(SET_ALERT, { message: 'test message' });
+      await wrapper.vm.$nextTick();
+
+      findAlert().vm.$emit('dismiss');
+      await wrapper.vm.$nextTick();
+
+      expect(findAlert().exists()).toBe(false);
+    });
+
+    it('renders link when `linkUrl` is set', async () => {
+      createComponent({ mountFn: mount });
+
+      store.commit(SET_ALERT, {
+        message: __('test message %{linkStart}test link%{linkEnd}'),
+        linkUrl: 'https://gitlab.com',
       });
+      await wrapper.vm.$nextTick();
 
-      it('renders link when `linkUrl` is set', async () => {
-        createComponent({ mountFn: mount });
+      const alertLink = findAlertLink();
 
-        store.commit(SET_ALERT, {
-          message: __('test message %{linkStart}test link%{linkEnd}'),
-          linkUrl: 'https://gitlab.com',
-        });
-        await wrapper.vm.$nextTick();
+      expect(alertLink.exists()).toBe(true);
+      expect(alertLink.text()).toContain('test link');
+      expect(alertLink.attributes('href')).toBe('https://gitlab.com');
+    });
 
-        const alertLink = findAlertLink();
+    describe('when alert is set in localStoage', () => {
+      it('renders alert on mount', () => {
+        createComponent();
 
-        expect(alertLink.exists()).toBe(true);
-        expect(alertLink.text()).toContain('test link');
-        expect(alertLink.attributes('href')).toBe('https://gitlab.com');
-      });
+        const alert = findAlert();
 
-      describe('when alert is set in localStoage', () => {
-        it('renders alert on mount', () => {
-          createComponent();
-
-          const alert = findAlert();
-
-          expect(alert.exists()).toBe(true);
-          expect(alert.html()).toContain('error message');
-        });
+        expect(alert.exists()).toBe(true);
+        expect(alert.html()).toContain('error message');
       });
     });
   });
