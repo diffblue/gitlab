15bbaded1444589824ca739b22c14771953e92d4
Kamil Trzciński <ayufan@ayufan.eu>
2021-11-10 12:05:14 +0100

    Change `DISABLE_CROSS_DATABASE_MODIFICATION_DETECTION` to `ENABLE_`
    This changes the feature flag to make it much easier
    to digest when used with conditions.

--- config/initializers/database_query_analyzers.rb
+++ config/initializers/database_query_analyzers.rb
@@ -5,7 +5,7 @@ if Gitlab.dev_or_test_env? || Gitlab::Utils.to_boolean(ENV['GITLAB_ENABLE_QUERY_
   Gitlab::Database::QueryAnalyzer.instance.hook!
   Gitlab::Database::QueryAnalyzer.instance.all_analyzers.append(::Gitlab::Database::QueryAnalyzers::GitlabSchemasMetrics)
 
-  if Rails.env.test? || ENV['DISABLE_CROSS_DATABASE_MODIFICATION_DETECTION']
+  if Rails.env.test? || Gitlab::Utils.to_boolean(ENV['ENABLE_CROSS_DATABASE_MODIFICATION_DETECTION'], default: false)
     Gitlab::Database::QueryAnalyzer.instance.all_analyzers.append(::Gitlab::Database::QueryAnalyzers::PreventCrossDatabaseModification)
   end
 
