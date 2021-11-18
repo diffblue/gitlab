f40b198b457da35bebb56bbe51a9f1bb3f3f291f
Florie Guibert <fguibert@gitlab.com>
2021-11-18 13:05:23 +1000

    Add Release token to board filtering
    Allow filtering issue board by release tag

--- ee/spec/frontend/boards/components/issue_board_filtered_search_spec.js
+++ ee/spec/frontend/boards/components/issue_board_filtered_search_spec.js
@@ -14,6 +14,7 @@ describe('IssueBoardFilter', () => {
       propsData: { fullPath: 'gitlab-org', boardType: 'group' },
       provide: {
         isSignedIn: true,
+        releasesFetchPath: '/releases',
         glFeatures: {
           iterationCadences: true,
         },
