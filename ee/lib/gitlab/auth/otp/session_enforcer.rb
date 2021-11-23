e6551492cbfaedbdc3ae2141e58a95b9bf0f40b0
nmilojevic1 <nmilojevic@gitlab.com>
2021-11-23 15:45:46 +0100

    Use Redis session instance
    - Replace Gitlab::Redis::SharedState with Sessions
    - Control with ENV variable
    - Extract ENV check to helper class

--- ee/lib/gitlab/auth/otp/session_enforcer.rb
+++ ee/lib/gitlab/auth/otp/session_enforcer.rb
@@ -4,20 +4,20 @@ module Gitlab
   module Auth
     module Otp
       class SessionEnforcer
-        OTP_SESSIONS_NAMESPACE = 'session:otp'
+        include ::Gitlab::Redis::SessionsStoreHelper
 
         def initialize(key)
           @key = key
         end
 
         def update_session
-          Gitlab::Redis::SharedState.with do |redis|
+          redis_store_class.with do |redis|
             redis.setex(key_name, session_expiry_in_seconds, true)
           end
         end
 
         def access_restricted?
-          Gitlab::Redis::SharedState.with do |redis|
+          redis_store_class.with do |redis|
             !redis.get(key_name)
           end
         end
@@ -27,7 +27,7 @@ module Gitlab
         attr_reader :key
 
         def key_name
-          @key_name ||= "#{OTP_SESSIONS_NAMESPACE}:#{key.id}"
+          @key_name ||= "#{Gitlab::Redis::Sessions::OTP_SESSIONS_NAMESPACE}:#{key.id}"
         end
 
         def session_expiry_in_seconds
