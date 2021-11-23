092f06a75f77e0b8cb2578dba160402063e0429e
Piotr Skorupa <pskorupa@gitlab.com>
2021-11-23 02:09:03 +0100

    Add validations to DevOpsReport::Metric
    This commit:
    
    - Adds presence and numericality validations to DevOpsReport::Metric
    model to not rely on database constraints alone when handling record
    creation.
    
    - Moves the `METRICS` constant to the model to have a single source of
    truth for these metric attributes.

--- app/services/service_ping/devops_report_service.rb
+++ app/services/service_ping/devops_report_service.rb
@@ -2,16 +2,6 @@
 
 module ServicePing
   class DevopsReportService
-    METRICS = %w[leader_issues instance_issues percentage_issues leader_notes instance_notes
-                 percentage_notes leader_milestones instance_milestones percentage_milestones
-                 leader_boards instance_boards percentage_boards leader_merge_requests
-                 instance_merge_requests percentage_merge_requests leader_ci_pipelines
-                 instance_ci_pipelines percentage_ci_pipelines leader_environments instance_environments
-                 percentage_environments leader_deployments instance_deployments percentage_deployments
-                 leader_projects_prometheus_active instance_projects_prometheus_active
-                 percentage_projects_prometheus_active leader_service_desk_issues instance_service_desk_issues
-                 percentage_service_desk_issues].freeze
-
     def initialize(data)
       @data = data
     end
@@ -22,7 +12,7 @@ module ServicePing
       return unless metrics.except('usage_data_id').present?
 
       DevOpsReport::Metric.create!(
-        metrics.slice(*METRICS)
+        metrics.slice(*DevOpsReport::Metric::METRICS)
       )
     end
   end
