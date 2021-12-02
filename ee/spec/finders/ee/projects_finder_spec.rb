69b2ac8026ea90e399c9e349597be145e35c32d2
huzaifaiftikhar1 <hiftikhar@gitlab.com>
2021-12-02 19:43:11 +0530

    Remove projects that are marked for deletion from UI
    Projects that are marked for deletion will not be accessible
    anywhere except the interfaces designed to list projects
    marked for deletion.
    
    Changelog: changed

--- ee/spec/finders/ee/projects_finder_spec.rb
+++ ee/spec/finders/ee/projects_finder_spec.rb
@@ -57,6 +57,14 @@ RSpec.describe ProjectsFinder do
         it { is_expected.to contain_exactly(aimed_for_deletion_project) }
       end
 
+      context 'filter by not aimed for deletion' do
+        let_it_be(:params) { { not_aimed_for_deletion: true } }
+        let_it_be(:aimed_for_deletion_project) { create(:project, :public, marked_for_deletion_at: 2.days.ago, pending_delete: false) }
+        let_it_be(:deleted_project) { create(:project, :public, marked_for_deletion_at: 1.month.ago, pending_delete: true) }
+
+        it { is_expected.to contain_exactly(ultimate_project, ultimate_project2, premium_project, no_plan_project) }
+      end
+
       private
 
       def create_project(plan)
