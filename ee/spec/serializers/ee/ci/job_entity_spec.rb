3afd7d898314f4c21c2527e38154ebeb588f9d40
lauraMon <lmontemayor@gitlab.com>
2021-12-02 16:22:16 +0100

    Rename BuildSerializer to JobSerliazer
    * Rename the object in JobEntity to BuildEntity

--- ee/spec/serializers/ee/ci/job_entity_spec.rb
+++ ee/spec/serializers/ee/ci/job_entity_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 require 'spec_helper'
 
-RSpec.describe JobEntity do
+RSpec.describe Ci::JobEntity do
   using RSpec::Parameterized::TableSyntax
 
   let_it_be(:group) { create(:group) }
