69179973ae47758f1c0a566c2372898b0f48f098
Adam Hegyi <ahegyi@gitlab.com>
2021-12-03 11:36:48 +0000

    Use aggregated VSA backend in time summary

--- ee/lib/gitlab/analytics/cycle_analytics/summary/base_time.rb
+++ ee/lib/gitlab/analytics/cycle_analytics/summary/base_time.rb
@@ -10,7 +10,7 @@ module Gitlab
             @current_user = current_user
             @options = options
 
-            assign_event_identifiers
+            assign_stage_metadata
           end
 
           def value
@@ -35,10 +35,20 @@ module Gitlab
 
           private
 
-          def assign_event_identifiers
+          # rubocop: disable CodeReuse/ActiveRecord
+          def assign_stage_metadata
             @stage.start_event_identifier = self.class.start_event_identifier
             @stage.end_event_identifier = self.class.end_event_identifier
+
+            if @options[:use_aggregated_data_collector]
+              # Gitlab::Analytics::CycleAnalytics::DistinctStageLoader ensures that we have StageEventHash record
+              # for the subclasses (LeadTime, CycleTime) however, it is an asynchronous process. There can be a short period
+              # of time where the query below returns nil. To handle this, we're pre-setting "None" value.
+              @stage.stage_event_hash_id = ::Analytics::CycleAnalytics::StageEventHash.find_by(hash_sha256: @stage.events_hash_code)&.id
+              @value = Gitlab::CycleAnalytics::Summary::Value::None.new if @stage.stage_event_hash_id.blank?
+            end
           end
+          # rubocop: enable CodeReuse/ActiveRecord
 
           def data_collector
             Gitlab::Analytics::CycleAnalytics::DataCollector.new(
@@ -48,7 +58,8 @@ module Gitlab
                 to: @options[:to] || DateTime.now,
                 project_ids: @options[:projects],
                 end_event_filter: @options[:end_event_filter],
-                current_user: @current_user
+                current_user: @current_user,
+                use_aggregated_data_collector: @options[:use_aggregated_data_collector]
               }.merge(@options.slice(*::Gitlab::Analytics::CycleAnalytics::RequestParams::FINDER_PARAM_NAMES))
             )
           end
