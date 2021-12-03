7d81e0a72481567157ddc92173375966a6522dc1
Thong Kuah <tkuah@gitlab.com>
2021-12-03 21:05:12 +1300

    Test run_after_commit that is run after transaction

--- spec/models/concerns/after_commit_queue_spec.rb
+++ spec/models/concerns/after_commit_queue_spec.rb
@@ -4,14 +4,34 @@ require 'spec_helper'
 
 RSpec.describe AfterCommitQueue do
   describe '#run_after_commit' do
-    it 'runs after transaction is committed' do
+    it 'runs after record is saved' do
       called = false
       test_proc = proc { called = true }
 
       project = build(:project)
       project.run_after_commit(&test_proc)
 
+      expect(called).to be false
+
+      # save! is run in its own transaction
       project.save!
+
+      expect(called).to be true
+    end
+
+    it 'runs after transaction is committed' do
+      called = false
+      test_proc = proc { called = true }
+
+      project = build(:project)
+
+      Project.transaction do
+        project.run_after_commit(&test_proc)
+
+        project.save!
+
+        expect(called).to be false
+      end
 
       expect(called).to be true
     end
