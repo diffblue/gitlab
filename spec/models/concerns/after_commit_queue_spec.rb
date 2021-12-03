c02ba4684f20ca16bcb81ee09697db57e9b7c045
Thong Kuah <tkuah@gitlab.com>
2021-12-03 14:40:31 +1300

    Add missing specs for run_after_commit_or_now

--- spec/models/concerns/after_commit_queue_spec.rb
+++ spec/models/concerns/after_commit_queue_spec.rb
@@ -3,15 +3,51 @@
 require 'spec_helper'
 
 RSpec.describe AfterCommitQueue do
-  it 'runs after transaction is committed' do
-    called = false
-    test_proc = proc { called = true }
+  describe '#run_after_commit' do
+    it 'runs after transaction is committed' do
+      called = false
+      test_proc = proc { called = true }
 
-    project = build(:project)
-    project.run_after_commit(&test_proc)
+      project = build(:project)
+      project.run_after_commit(&test_proc)
 
-    project.save!
+      project.save!
 
-    expect(called).to be true
+      expect(called).to be true
+    end
+  end
+
+  describe '#run_after_commit_or_now' do
+    it 'runs immediately if not within a transction' do
+      called = false
+      test_proc = proc { called = true }
+
+      project = build(:project)
+
+      project.run_after_commit_or_now(&test_proc)
+
+      expect(called).to be true
+    end
+
+    it 'runs after transaction has completed' do
+      called = false
+      test_proc = proc { called = true }
+
+      project = build(:project)
+
+      Project.transaction do
+        # Add this record to the current transaction so that after commit hooks
+        # are called
+        Project.connection.add_transaction_record(project)
+
+        project.run_after_commit_or_now(&test_proc)
+
+        project.save!
+
+        expect(called).to be false
+      end
+
+      expect(called).to be true
+    end
   end
 end
