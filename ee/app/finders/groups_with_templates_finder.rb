5fe40a793d74ec6b17b566a3974ded3b593afc1d
Francisco Javier López <fjlopez@gitlab.com>
2021-12-04 10:12:18 +0100

    Use linear version GroupsWithTemplatesFinder#extended_group_search
    In this commit we're removing the ff
    `linear_groups_template_finder_extended_group_search_ancestors_scopes`
    and enabling the linear version of
    `GroupsWithTemplatesFinder#extended_group_search`.
    
    Changelog: changed
    EE: true

--- ee/app/finders/groups_with_templates_finder.rb
+++ ee/app/finders/groups_with_templates_finder.rb
@@ -18,16 +18,11 @@ class GroupsWithTemplatesFinder
   attr_reader :group_id
 
   def extended_group_search
-    # We're adding an extra query that will be removed once we remove the feature flag in https://gitlab.com/gitlab-org/gitlab/-/issues/339439
-    groups = if Feature.enabled?(:linear_groups_template_finder_extended_group_search_ancestors_scopes, current_group, default_enabled: :yaml)
-               Group.with_project_templates.self_and_ancestors
-             else
-               Gitlab::ObjectHierarchy
-                 .new(Group.with_project_templates)
-                 .base_and_ancestors
-             end
-
-    groups.with_feature_available_in_plan(:group_project_templates).self_and_descendants
+    Group
+      .with_project_templates
+      .self_and_ancestors
+      .with_feature_available_in_plan(:group_project_templates)
+      .self_and_descendants
   end
 
   def simple_group_search(groups)
@@ -35,10 +30,5 @@ class GroupsWithTemplatesFinder
     return Group.none unless groups
 
     groups.with_project_templates
-  end
-
-  # This method will be removed https://gitlab.com/gitlab-org/gitlab/-/issues/339439
-  def current_group
-    Group.find_by(id: group_id) # rubocop:disable CodeReuse/ActiveRecord
   end
 end
