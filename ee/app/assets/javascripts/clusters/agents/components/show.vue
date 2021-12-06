b7b473f3fc272f7aca1637025a63c5b886d36180
Alexander Turinske <aturinske@gitlab.com>
2021-12-06 16:50:48 -0700

    Create agent-specific vulnerability list
    - use vulnerability-list component
    - pass up additional data
    - update tests

--- ee/app/assets/javascripts/clusters/agents/components/show.vue
+++ ee/app/assets/javascripts/clusters/agents/components/show.vue
@@ -3,23 +3,18 @@ import { GlTab } from '@gitlab/ui';
 import { s__ } from '~/locale';
 import glFeatureFlagMixin from '~/vue_shared/mixins/gl_feature_flags_mixin';
 import AgentShowPage from '~/clusters/agents/components/show.vue';
+import AgentVulnerabilityReport from 'ee/security_dashboard/components/agent/agent_vulnerability_report.vue';
 
 export default {
   i18n: {
     securityTabTitle: s__('ClusterAgents|Security'),
   },
-  components: { AgentShowPage, GlTab },
-  mixins: [glFeatureFlagMixin()],
-  props: {
-    agentName: {
-      required: true,
-      type: String,
-    },
-    projectPath: {
-      required: true,
-      type: String,
-    },
+  components: {
+    AgentShowPage,
+    GlTab,
+    AgentVulnerabilityReport,
   },
+  mixins: [glFeatureFlagMixin()],
   computed: {
     showSecurityTab() {
       return (
@@ -33,8 +28,9 @@ export default {
 <template>
   <agent-show-page v-bind="$props">
     <template v-if="showSecurityTab" #ee-security-tab>
-      <!-- Placeholder for https://gitlab.com/gitlab-org/gitlab/-/issues/343912-->
-      <gl-tab :title="$options.i18n.securityTabTitle"><div></div></gl-tab>
+      <gl-tab :title="$options.i18n.securityTabTitle">
+        <agent-vulnerability-report />
+      </gl-tab>
     </template>
   </agent-show-page>
 </template>
