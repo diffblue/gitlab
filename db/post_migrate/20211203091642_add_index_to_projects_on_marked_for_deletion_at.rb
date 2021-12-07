8d942cb13519b075cb84a3df39afab3fe5825ba7
huzaifaiftikhar1 <hiftikhar@gitlab.com>
2021-12-07 12:21:18 +0530

    Replace marked_for_deletion_at column with id to get index only scan

--- db/post_migrate/20211203091642_add_index_to_projects_on_marked_for_deletion_at.rb
+++ db/post_migrate/20211203091642_add_index_to_projects_on_marked_for_deletion_at.rb
@@ -6,10 +6,10 @@ class AddIndexToProjectsOnMarkedForDeletionAt < Gitlab::Database::Migration[1.0]
   INDEX_NAME = 'index_projects_not_aimed_for_deletion'
 
   def up
-    add_concurrent_index :projects, :marked_for_deletion_at, where: 'marked_for_deletion_at IS NULL', name: INDEX_NAME
+    add_concurrent_index :projects, :id, where: 'marked_for_deletion_at IS NULL', name: INDEX_NAME
   end
 
   def down
-    remove_concurrent_index :projects, :marked_for_deletion_at, name: INDEX_NAME
+    remove_concurrent_index :projects, :id, name: INDEX_NAME
   end
 end
