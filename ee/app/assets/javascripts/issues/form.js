cb4b64bcf75b0c06dfd0778b0dbddb7a64319b7f
Coung Ngo <cngo@gitlab.com>
2021-12-08 11:05:10 +0000

    Move issues code out of ~/pages

--- ee/app/assets/javascripts/issues/form.js
+++ ee/app/assets/javascripts/issues/form.js
@@ -1,5 +1,5 @@
 import initEpicsSelect from 'ee/vue_shared/components/sidebar/epics_select/epics_select_bundle';
-import initForm from '~/pages/projects/issues/form';
+import initForm from '~/issues/form';
 
 export default () => {
   initEpicsSelect();
