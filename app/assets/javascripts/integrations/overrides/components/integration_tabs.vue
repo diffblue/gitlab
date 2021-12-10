47be1dd15ee4b03698cba06fbc093190c6730aa0
Justin Ho <hduong@gitlab.com>
2021-12-10 11:57:46 +0700

    Add projects overrides counter to tabs
    Move override tabs to Vue
    
    Changlog: changed

---
+++ app/assets/javascripts/integrations/overrides/components/integration_tabs.vue
@@ -0,0 +1,46 @@
+<script>
+import { GlBadge, GlTabs, GlTab } from '@gitlab/ui';
+
+export default {
+  components: {
+    GlBadge,
+    GlTabs,
+    GlTab,
+  },
+  inject: {
+    editPath: {
+      default: '',
+    },
+  },
+  props: {
+    projectOverridesCount: {
+      type: Number,
+      required: false,
+      default: null,
+    },
+  },
+};
+</script>
+
+<template>
+  <gl-tabs>
+    <template #tabs-start>
+      <li class="nav-item" role="presentation">
+        <a class="nav-link gl-tab-nav-item" :href="editPath">{{ __('Settings') }}</a>
+      </li>
+    </template>
+
+    <gl-tab active>
+      <template #title>
+        {{ s__('Integrations|Projects using custom settings') }}
+        <gl-badge
+          v-if="projectOverridesCount !== null"
+          variant="muted"
+          size="sm"
+          class="gl-tab-counter-badge"
+          >{{ projectOverridesCount }}</gl-badge
+        >
+      </template>
+    </gl-tab>
+  </gl-tabs>
+</template>
