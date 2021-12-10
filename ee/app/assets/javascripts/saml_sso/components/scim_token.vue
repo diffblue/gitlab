d7e4392658926e6aeb0798fbd582fd9a9a478bcb
Peter Hegman <phegman@gitlab.com>
2021-12-10 18:53:58 +0000

    Convert SCIM token management to Vue

---
+++ ee/app/assets/javascripts/saml_sso/components/scim_token.vue
@@ -0,0 +1,190 @@
+<script>
+import { GlSprintf, GlButton, GlLoadingIcon, GlModal } from '@gitlab/ui';
+
+import axios from '~/lib/utils/axios_utils';
+import createFlash from '~/flash';
+import InputCopyToggleVisibility from '~/vue_shared/components/form/input_copy_toggle_visibility.vue';
+import { s__, __ } from '~/locale';
+
+export default {
+  components: {
+    GlSprintf,
+    InputCopyToggleVisibility,
+    GlButton,
+    GlLoadingIcon,
+    GlModal,
+  },
+  i18n: {
+    copyToken: s__('GroupSaml|Copy SCIM token'),
+    copyEndpointUrl: s__('GroupSaml|Copy SCIM API endpoint URL'),
+    tokenLabel: s__('GroupSaml|Your SCIM token'),
+    endpointUrlLabel: s__('GroupSaml|SCIM API endpoint URL'),
+    generateTokenButtonText: s__('GroupSAML|Generate a SCIM token'),
+    tokenHasNotBeenGeneratedMessage: s__(
+      'GroupSAML|Generate a SCIM token to set up your System for Cross-Domain Identity Management.',
+    ),
+    tokenNeedsToBeResetDescription: s__(
+      'GroupSAML|The SCIM token is now hidden. To see the value of the token again, you need to %{linkStart}reset it%{linkEnd}.',
+    ),
+    tokenHasBeenGeneratedOrResetDescription: s__(
+      "GroupSAML|Make sure you save this token — you won't be able to access it again.",
+    ),
+    generateTokenErrorMessage: s__(
+      'GroupSAML|An error occurred generating your SCIM token. Please try again.',
+    ),
+    resetTokenErrorMessage: s__(
+      'GroupSAML|An error occurred resetting your SCIM token. Please try again.',
+    ),
+    modal: {
+      title: __('Are you sure?'),
+      body: s__(
+        'GroupSAML|Are you sure you want to reset the SCIM token? SCIM provisioning will stop working until the new token is updated.',
+      ),
+    },
+  },
+  modal: {
+    id: 'reset-scim-token-modal',
+    actionPrimary: {
+      text: s__('GroupSAML|Reset SCIM token'),
+      attributes: {
+        variant: 'danger',
+      },
+    },
+    actionSecondary: {
+      text: __('Cancel'),
+      attributes: {
+        variant: 'default',
+      },
+    },
+  },
+  tokenInputId: 'scim_token',
+  endpointUrlInputId: 'scim_token_endpoint_url',
+  inject: ['initialEndpointUrl', 'generateTokenPath'],
+  data() {
+    return {
+      loading: false,
+      token: '',
+      endpointUrl: this.initialEndpointUrl,
+      modalVisible: false,
+    };
+  },
+  computed: {
+    tokenHasNotBeenGenerated() {
+      return !this.endpointUrl && this.token === '';
+    },
+    tokenNeedsToBeReset() {
+      return this.endpointUrl && this.token === '';
+    },
+    tokenInputValue() {
+      return this.tokenNeedsToBeReset ? '*'.repeat(20) : this.token;
+    },
+    tokenFormInputGroupProps() {
+      return { id: this.$options.tokenInputId, class: 'gl-form-input-xl' };
+    },
+    tokenEndpointUrlFormInputGroupProps() {
+      return { id: this.$options.endpointUrlInputId, class: 'gl-form-input-xl' };
+    },
+    contentContainerClasses() {
+      return { 'gl-visibility-hidden': this.loading };
+    },
+  },
+  methods: {
+    async callApi(errorMessage) {
+      this.loading = true;
+
+      try {
+        const {
+          data: { scim_api_url: endpointUrl, scim_token: token },
+        } = await axios.post(this.generateTokenPath);
+
+        this.token = token;
+        this.endpointUrl = endpointUrl;
+      } catch (error) {
+        createFlash({
+          message: errorMessage,
+          captureError: true,
+          error,
+        });
+      } finally {
+        this.loading = false;
+      }
+    },
+    handleGenerateTokenButtonClick() {
+      this.callApi(this.$options.i18n.generateTokenErrorMessage);
+    },
+    handleModalPrimary() {
+      this.callApi(this.$options.i18n.resetTokenErrorMessage);
+    },
+    handleResetButtonClick() {
+      this.modalVisible = true;
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="gl-mt-5 gl-relative">
+    <gl-modal
+      v-model="modalVisible"
+      :modal-id="$options.modal.id"
+      :title="$options.i18n.modal.title"
+      :action-primary="$options.modal.actionPrimary"
+      :action-secondary="$options.modal.actionSecondary"
+      size="sm"
+      @primary="handleModalPrimary"
+    >
+      {{ $options.i18n.modal.body }}
+    </gl-modal>
+    <div
+      v-if="loading"
+      class="gl-absolute gl-top-5 gl-left-0 gl-right-0 gl-display-flex gl-justify-content-center"
+    >
+      <gl-loading-icon size="md" />
+    </div>
+    <div
+      v-if="tokenHasNotBeenGenerated"
+      :class="contentContainerClasses"
+      data-testid="content-container"
+    >
+      <p>
+        {{ $options.i18n.tokenHasNotBeenGeneratedMessage }}
+      </p>
+      <gl-button @click="handleGenerateTokenButtonClick">{{
+        $options.i18n.generateTokenButtonText
+      }}</gl-button>
+    </div>
+    <div v-else :class="contentContainerClasses" data-testid="content-container">
+      <input-copy-toggle-visibility
+        :label="$options.i18n.tokenLabel"
+        :label-for="$options.tokenInputId"
+        :form-input-group-props="tokenFormInputGroupProps"
+        :value="tokenInputValue"
+        :copy-button-title="$options.i18n.copyToken"
+        :show-toggle-visibility-button="!tokenNeedsToBeReset"
+        :show-copy-button="!tokenNeedsToBeReset"
+      >
+        <template #description>
+          <gl-sprintf
+            v-if="tokenNeedsToBeReset"
+            :message="$options.i18n.tokenNeedsToBeResetDescription"
+          >
+            <template #link="{ content }">
+              <gl-button variant="link" @click="handleResetButtonClick">{{ content }}</gl-button>
+            </template>
+          </gl-sprintf>
+          <template v-else>
+            {{ $options.i18n.tokenHasBeenGeneratedOrResetDescription }}
+          </template>
+        </template>
+      </input-copy-toggle-visibility>
+      <input-copy-toggle-visibility
+        :label="$options.i18n.endpointUrlLabel"
+        :label-for="$options.endpointUrlInputId"
+        :form-input-group-props="tokenEndpointUrlFormInputGroupProps"
+        :value="endpointUrl"
+        :copy-button-title="$options.i18n.copyEndpointUrl"
+        :show-toggle-visibility-button="false"
+      />
+    </div>
+  </div>
+</template>
--- ee/spec/features/groups/scim_token_spec.rb
+++ ee/spec/features/groups/scim_token_spec.rb
@@ -3,47 +3,114 @@
 require 'spec_helper'
 
 RSpec.describe 'SCIM Token handling', :js do
-  let(:user) { create(:user) }
-  let(:group) { create(:group) }
+  include Spec::Support::Helpers::ModalHelpers
+
+  let_it_be(:user) { create(:user) }
+  let_it_be(:group) { create(:group) }
 
   before do
     group.add_owner(user)
     stub_licensed_features(group_saml: true)
   end
 
-  describe 'group has no existing scim token' do
+  context 'when `scim_token_vue` feature flag is disabled' do
+    before do
+      stub_feature_flags(scim_token_vue: false)
+    end
+
+    describe 'group has no existing scim token' do
+      before do
+        sign_in(user)
+        visit group_saml_providers_path(group)
+      end
+
+      it 'displays generate token form' do
+        expect(page).to have_selector('.js-generate-scim-token-container', visible: true)
+        expect(page).to have_selector('.js-scim-token-container', visible: false)
+
+        page.within '.js-generate-scim-token-container' do
+          expect(page).to have_content('Generate a SCIM token to set up your System for Cross-Domain Identity Management.')
+          expect(page).to have_button('Generate a SCIM token')
+        end
+      end
+    end
+
+    describe 'group has existing scim token' do
+      let!(:scim_token) { create(:scim_oauth_access_token, group: group) }
+
+      before do
+        sign_in(user)
+        visit group_saml_providers_path(group)
+      end
+
+      it 'displays the scim form with an obfuscated token' do
+        expect(page).to have_selector('.js-generate-scim-token-container', visible: false)
+        expect(page).to have_selector('.js-scim-token-container', visible: true)
+
+        page.within '.js-scim-token-container' do
+          expect(page).to have_button('reset it.')
+          expect(page.find('#scim_token').value).to eq('********************')
+          expect(page.find('#scim_endpoint_url').value).to eq(scim_token.as_entity_json[:scim_api_url])
+        end
+      end
+    end
+  end
+
+  def find_token_field
+    page.find_field('Your SCIM token')
+  end
+
+  def find_api_endpoint_url_field
+    page.find_field('SCIM API endpoint URL')
+  end
+
+  context 'when group has no existing SCIM token' do
     before do
       sign_in(user)
       visit group_saml_providers_path(group)
     end
 
     it 'displays generate token form' do
-      expect(page).to have_selector('.js-generate-scim-token-container', visible: true)
-      expect(page).to have_selector('.js-scim-token-container', visible: false)
-
-      page.within '.js-generate-scim-token-container' do
-        expect(page).to have_content('Generate a SCIM token to set up your System for Cross-Domain Identity Management.')
-        expect(page).to have_button('Generate a SCIM token')
-      end
+      expect(page).to have_content('Generate a SCIM token to set up your System for Cross-Domain Identity Management.')
+      expect(page).to have_button('Generate a SCIM token')
     end
   end
 
-  describe 'group has existing scim token' do
-    let!(:scim_token) { create(:scim_oauth_access_token, group: group) }
+  context 'when group has existing SCIM token' do
+    let_it_be(:scim_token) { create(:scim_oauth_access_token, group: group) }
 
     before do
       sign_in(user)
       visit group_saml_providers_path(group)
     end
 
-    it 'displays the scim form with an obfuscated token' do
-      expect(page).to have_selector('.js-generate-scim-token-container', visible: false)
-      expect(page).to have_selector('.js-scim-token-container', visible: true)
+    it 'displays the SCIM form with an obfuscated token' do
+      expect(page).to have_button('reset it')
+      expect(find_token_field.value).to eq('********************')
+      expect(page).not_to have_button('Click to reveal')
+      expect(page).not_to have_button('Copy SCIM token')
+      expect(find_api_endpoint_url_field.value).to eq(scim_token.as_entity_json[:scim_api_url])
+    end
 
-      page.within '.js-scim-token-container' do
-        expect(page).to have_button('reset it.')
-        expect(page.find('#scim_token').value).to eq('********************')
-        expect(page.find('#scim_endpoint_url').value).to eq(scim_token.as_entity_json[:scim_api_url])
+    context 'when `reset it` button is clicked' do
+      before do
+        accept_gl_confirm(
+          'Are you sure you want to reset the SCIM token? SCIM provisioning will stop working until the new token is updated.',
+          button_text: 'Reset SCIM token'
+        ) do
+          page.click_button('reset it')
+        end
+      end
+
+      it 'displays the SCIM form with an obfuscated token that can be copied or shown' do
+        expect(find_api_endpoint_url_field.value).to eq(scim_token.as_entity_json[:scim_api_url])
+
+        expect(page).to have_button('Copy SCIM token')
+
+        expect(find_token_field.value).to eq('********************')
+        page.click_button('Click to reveal')
+        expect(find_token_field.value).not_to eq('********************')
+        expect(find_token_field.value).not_to eq('')
       end
     end
   end
