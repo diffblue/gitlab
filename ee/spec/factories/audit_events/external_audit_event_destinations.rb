86fadb1762a800c000fc2743be2c48f227091fc8
Peter Leitzen <pleitzen@gitlab.com>
2021-12-15 00:43:49 +0100

    Use static known URL for ExternalAuditEventDestination factory
    Previously, a random generated URL could point to a valid IP which might
    be considered as "local network" or "loopback" by Gitlab::UrlBlocker and
    fail during record creation with:
    
    Validation failed: Destination url is blocked: Requests to loopback
    addresses are not allowed
    
    example.org is a known, static and public URL.

--- ee/spec/factories/audit_events/external_audit_event_destinations.rb
+++ ee/spec/factories/audit_events/external_audit_event_destinations.rb
@@ -3,6 +3,6 @@
 FactoryBot.define do
   factory :external_audit_event_destination, class: 'AuditEvents::ExternalAuditEventDestination' do
     group
-    sequence(:destination_url) { |n| "#{FFaker::Internet.http_url}/#{n}" }
+    sequence(:destination_url) { |n| "http://example.org/#{n}" }
   end
 end
