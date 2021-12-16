23b5db8d7a360c06a352f2ede1650e717a75cd96
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2021-12-16 09:51:27 +0100

    Remove unused manual mocks

--- ee/spec/frontend/on_demand_scans_form/mocks/mock_data.js
+++ ee/spec/frontend/on_demand_scans_form/mocks/mock_data.js
@@ -1,8 +1,3 @@
-import {
-  REDACTED_PASSWORD,
-  REDACTED_REQUEST_HEADERS,
-} from 'ee/security_configuration/dast_profiles/dast_site_profiles/constants';
-
 export const policyScannerProfile = {
   id: 'gid://gitlab/DastScannerProfile/3',
   profileName: 'Scanner profile #3',
@@ -14,48 +9,6 @@ export const policyScannerProfile = {
   editPath: '/scanner_profile/edit/3',
   referencedInSecurityPolicies: ['some_policy'],
 };
-
-export const siteProfiles = [
-  {
-    id: 'gid://gitlab/DastSiteProfile/1',
-    profileName: 'Site profile #1',
-    targetUrl: 'https://foo.com',
-    targetType: 'WEBSITE',
-    normalizedTargetUrl: 'https://foo.com:443',
-    editPath: '/site_profiles/edit/1',
-    validationStatus: 'PENDING_VALIDATION',
-    auth: {
-      enabled: true,
-      url: 'https://foo.com/login',
-      usernameField: 'username',
-      passwordField: 'password',
-      username: 'admin',
-      password: REDACTED_PASSWORD,
-    },
-    excludedUrls: ['https://foo.com/logout', 'https://foo.com/send_mail'],
-    requestHeaders: REDACTED_REQUEST_HEADERS,
-    referencedInSecurityPolicies: [],
-  },
-  {
-    id: 'gid://gitlab/DastSiteProfile/2',
-    profileName: 'Site profile #2',
-    targetUrl: 'https://bar.com',
-    targetType: 'API',
-    normalizedTargetUrl: 'https://bar.com:443',
-    editPath: '/site_profiles/edit/2',
-    validationStatus: 'PASSED_VALIDATION',
-    auth: {
-      enabled: false,
-      url: 'https://foo.com/login',
-      usernameField: 'username',
-      passwordField: 'password',
-      username: 'admin',
-    },
-    excludedUrls: ['https://bar.com/logout'],
-    requestHeaders: REDACTED_REQUEST_HEADERS,
-    referencedInSecurityPolicies: [],
-  },
-];
 
 export const policySiteProfile = {
   id: 'gid://gitlab/DastSiteProfile/6',
