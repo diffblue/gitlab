b88ed2225418f8a49366c647282a29628ef222fc
Bala Kumar <sbalakumar@gitlab.com>
2021-12-16 16:17:36 +0000

    Fix N+1 in environments dashboard list

--- ee/app/serializers/dashboard_environments_serializer.rb
+++ ee/app/serializers/dashboard_environments_serializer.rb
@@ -4,4 +4,38 @@ class DashboardEnvironmentsSerializer < BaseSerializer
   include WithPagination
 
   entity DashboardEnvironmentsProjectEntity
+
+  def represent(resource, opts = {})
+    resource = @paginator.paginate(resource) if paginated?
+
+    super(batch_load(resource), opts)
+  end
+
+  private
+
+  # rubocop: disable CodeReuse/ActiveRecord
+  def batch_load(projects)
+    ActiveRecord::Associations::Preloader.new.preload(projects, [
+      :route,
+      environments_for_dashboard: [
+        last_visible_pipeline: [
+          :user,
+          project: [:route, :group, :project_feature, namespace: :route]
+        ],
+        last_visible_deployment: [
+          deployable: [
+            :metadata,
+            :pipeline,
+            project: [:project_feature, :group, :route, namespace: :route]
+          ],
+          project: [:route, namespace: :route]
+        ],
+        project: [:project_feature, :group, namespace: :route]
+      ],
+      namespace: [:route, :owner]
+    ])
+
+    projects
+  end
+  # rubocop: enable CodeReuse/ActiveRecord
 end
--- ee/app/services/dashboard/environments/list_service.rb
+++ ee/app/services/dashboard/environments/list_service.rb
@@ -3,47 +3,17 @@
 module Dashboard
   module Environments
     class ListService
+      attr_reader :user
+
       def initialize(user)
         @user = user
       end
 
       def execute
-        load_projects(user)
-      end
-
-      private
-
-      attr_reader :user
-
-      # rubocop: disable CodeReuse/ActiveRecord
-      def load_projects(user)
-        projects = ::Dashboard::Projects::ListService
+        ::Dashboard::Projects::ListService
           .new(user, feature: :operations_dashboard)
           .execute(user.ops_dashboard_projects)
-
-        ActiveRecord::Associations::Preloader.new.preload(projects, [
-          :route,
-          environments_for_dashboard: [
-            last_visible_pipeline: [
-              :user,
-              project: [:route, :group, :project_feature, namespace: :route]
-            ],
-            last_visible_deployment: [
-              deployable: [
-                :metadata,
-                :pipeline,
-                project: [:project_feature, :group, :route, namespace: :route]
-              ],
-              project: [:route, namespace: :route]
-            ],
-            project: [:project_feature, :group, namespace: :route]
-          ],
-          namespace: [:route, :owner]
-        ])
-
-        projects
       end
-      # rubocop: enable CodeReuse/ActiveRecord
     end
   end
 end
