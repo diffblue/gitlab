8dc36b19d2e3105a7d8220b598e96822b63e286f
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2021-12-20 20:35:21 +0100

    Get saved scan counts from finder

--- ee/app/helpers/projects/on_demand_scans_helper.rb
+++ ee/app/helpers/projects/on_demand_scans_helper.rb
@@ -5,8 +5,8 @@ module Projects::OnDemandScansHelper
   def on_demand_scans_data(project)
     on_demand_scans = project.all_pipelines.where(source: Enums::Ci::Pipeline.sources[:ondemand_dast_scan])
     running_scan_count, finished_scan_count = count_running_and_finished_scans(on_demand_scans)
-    saved_scans = ::Dast::ProfilesFinder.new({ project_id: project.id }).execute
-    scheduled_scans = saved_scans.count { |scan| !scan.dast_profile_schedule.nil? }
+    scheduled_scans_count = ::Dast::ProfilesFinder.new({ project_id: project.id, has_dast_profile_schedule: true }).execute.count
+    saved_scans_count = ::Dast::ProfilesFinder.new({ project_id: project.id }).execute.count
 
     common_data(project).merge({
       'project-on-demand-scan-counts-etag' => graphql_etag_project_on_demand_scan_counts_path(project),
@@ -14,8 +14,8 @@ module Projects::OnDemandScansHelper
         all: on_demand_scans.length,
         running: running_scan_count,
         finished: finished_scan_count,
-        scheduled: scheduled_scans,
-        saved: saved_scans.length
+        scheduled: scheduled_scans_count,
+        saved: saved_scans_count
       }.to_json,
       'new-dast-scan-path' => new_project_on_demand_scan_path(project),
       'empty-state-svg-path' => image_path('illustrations/empty-state/ondemand-scan-empty.svg'),
