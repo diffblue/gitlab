6fd891f6aa2592165023ca36e368351727d043b8
Sean Arnold <sarnold@gitlab.com>
2021-12-20 14:37:42 +1300

    Move to post-migrate, mark job as succeeded

--- lib/gitlab/background_migration/backfill_incident_issue_escalation_statuses.rb
+++ lib/gitlab/background_migration/backfill_incident_issue_escalation_statuses.rb
@@ -9,12 +9,23 @@ module Gitlab
       def perform(start_id, stop_id)
         ActiveRecord::Base.connection.execute <<~SQL
           INSERT INTO incident_management_issuable_escalation_statuses (issue_id, created_at, updated_at)
-            SELECT issues.id, now(), now()
+            SELECT issues.id, current_timestamp, current_timestamp
             FROM issues
             WHERE issues.issue_type = 1
             AND issues.id BETWEEN #{start_id} AND #{stop_id}
             ON CONFLICT (issue_id) DO NOTHING;
         SQL
+
+        mark_job_as_succeeded(start_id, stop_id)
+      end
+
+      private
+
+      def mark_job_as_succeeded(*arguments)
+        ::Gitlab::Database::BackgroundMigrationJob.mark_all_as_succeeded(
+          self.class.name.demodulize,
+          arguments
+        )
       end
     end
   end
