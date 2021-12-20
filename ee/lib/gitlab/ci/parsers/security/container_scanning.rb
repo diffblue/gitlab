f408212cde138e14b8e960d5b54223d0800d9f89
Thiago Figueiró <tfigueiro@gitlab.com>
2021-12-20 21:42:07 +0000

    Remove improved_container_scan_matching feature flag

--- ee/lib/gitlab/ci/parsers/security/container_scanning.rb
+++ ee/lib/gitlab/ci/parsers/security/container_scanning.rb
@@ -13,21 +13,14 @@ module Gitlab
               operating_system: location_data['operating_system'],
               package_name: location_data.dig('dependency', 'package', 'name'),
               package_version: location_data.dig('dependency', 'version'),
-              default_branch_image: default_branch_image(location_data),
-              improved_container_scan_matching_enabled: improved_container_scan_matching_enabled?
+              default_branch_image: default_branch_image(location_data)
             )
           end
 
           def default_branch_image(location_data)
-            return unless improved_container_scan_matching_enabled?
-
             return if @report.pipeline.default_branch?
 
             location_data['default_branch_image']
-          end
-
-          def improved_container_scan_matching_enabled?
-            Feature.enabled?(:improved_container_scan_matching, @report.pipeline.project, default_enabled: :yaml)
           end
         end
       end
--- ee/spec/lib/gitlab/ci/reports/security/locations/container_scanning_spec.rb
+++ ee/spec/lib/gitlab/ci/reports/security/locations/container_scanning_spec.rb
@@ -23,102 +23,57 @@ RSpec.describe Gitlab::Ci::Reports::Security::Locations::ContainerScanning do
   describe 'fingerprint' do
     sha1_of = -> (input) { Digest::SHA1.hexdigest(input) }
 
-    context 'with feature enabled' do
-      where(:image, :default_branch_image, :expected_fingerprint_input) do
+    where(:image, :default_branch_image, :expected_fingerprint_input) do
+      [
+        ['alpine:3.7.3', nil, 'alpine:3.7.3:glibc'],
+        ['alpine:3.7', nil, 'alpine:3.7:glibc'],
+        ['alpine:8101518288111119448185914762536722131810', nil, 'alpine:glibc'],
+        ['alpine:1.0.0-beta', nil, 'alpine:1.0.0-beta:glibc'],
         [
-          ['alpine:3.7.3', nil, 'alpine:3.7.3:glibc'],
-          ['alpine:3.7', nil, 'alpine:3.7:glibc'],
-          ['alpine:8101518288111119448185914762536722131810', nil, 'alpine:glibc'],
-          ['alpine:1.0.0-beta', nil, 'alpine:1.0.0-beta:glibc'],
-          [
-            'gdk.local:5000/group/project/branch:307e0a35643f63652a713d0820db7c388012f724',
-            nil,
-            'gdk.local:5000/group/project/branch:glibc'
-          ],
-          [
-            'registry.gitlab.com/group/project/tmp:af864bd61230d3d694eb01d6205b268b4ad63ac0',
-            nil,
-            'registry.gitlab.com/group/project/tmp:glibc'
-          ],
-          [
-            'registry.gitlab.com/group/project/feature:5b1a4a921d7a50c3757aae3f7df2221878775af4',
-            'registry.gitlab.com/group/project/master:ec301f43f14a2b477806875e49cfc4d3fa0d22c3',
-            'registry.gitlab.com/group/project/master:glibc'
-          ],
-          [
-            'registry.gitlab.com/group/project/feature:d6704dc0b8e33fb550a86f7847d6a3036d4f8bd5',
-            'registry.gitlab.com/group/project:latest',
-            'registry.gitlab.com/group/project:glibc'
-          ],
-          [
-            'registry.gitlab.com/group/project@sha256:a418bbb80b9411f9a08025baa4681e192aaafd16505039bdcb113ccdb90a88fd',
-            'registry.gitlab.com/group/project:latest',
-            'registry.gitlab.com/group/project:glibc'
-          ],
-          [
-            'registry.gitlab.com/group/project/feature:latest',
-            'registry.gitlab.com/group/project:1.0.0',
-            'registry.gitlab.com/group/project:1.0.0:glibc'
-          ]
+          'gdk.local:5000/group/project/branch:307e0a35643f63652a713d0820db7c388012f724',
+          nil,
+          'gdk.local:5000/group/project/branch:glibc'
+        ],
+        [
+          'registry.gitlab.com/group/project/tmp:af864bd61230d3d694eb01d6205b268b4ad63ac0',
+          nil,
+          'registry.gitlab.com/group/project/tmp:glibc'
+        ],
+        [
+          'registry.gitlab.com/group/project/feature:5b1a4a921d7a50c3757aae3f7df2221878775af4',
+          'registry.gitlab.com/group/project/master:ec301f43f14a2b477806875e49cfc4d3fa0d22c3',
+          'registry.gitlab.com/group/project/master:glibc'
+        ],
+        [
+          'registry.gitlab.com/group/project/feature:d6704dc0b8e33fb550a86f7847d6a3036d4f8bd5',
+          'registry.gitlab.com/group/project:latest',
+          'registry.gitlab.com/group/project:glibc'
+        ],
+        [
+          'registry.gitlab.com/group/project@sha256:a418bbb80b9411f9a08025baa4681e192aaafd16505039bdcb113ccdb90a88fd',
+          'registry.gitlab.com/group/project:latest',
+          'registry.gitlab.com/group/project:glibc'
+        ],
+        [
+          'registry.gitlab.com/group/project/feature:latest',
+          'registry.gitlab.com/group/project:1.0.0',
+          'registry.gitlab.com/group/project:1.0.0:glibc'
         ]
-      end
-
-      with_them do
-        let(:params) do
-          {
-            image: image,
-            default_branch_image: default_branch_image,
-            operating_system: 'debian:9',
-            package_name: 'glibc',
-            package_version: '1.2.3',
-            improved_container_scan_matching_enabled: true
-          }
-        end
-
-        specify { expect(subject.fingerprint).to eq(sha1_of.call(expected_fingerprint_input)) }
-      end
+      ]
     end
 
-    context 'with feature disabled' do
+    with_them do
       let(:params) do
         {
-          image: 'registry.gitlab.com/group/project/feature:ec301f43f14a2b477806875e49cfc4d3fa0d22c3',
-          default_branch_image: 'registry.gitlab.com/group/project/master:ec301f43f14a2b477806875e49cfc4d3fa0d22c3',
+          image: image,
+          default_branch_image: default_branch_image,
           operating_system: 'debian:9',
           package_name: 'glibc',
           package_version: '1.2.3'
         }
       end
 
-      it 'ignores default_branch_image' do
-        expect(subject.fingerprint).to eq(sha1_of.call('registry.gitlab.com/group/project/feature:glibc'))
-      end
-
-      where(:image, :expected_fingerprint_input) do
-        [
-          ['alpine:3.7.3', 'alpine:3.7.3:glibc'],
-          ['alpine:3.7', 'alpine:3.7:glibc'],
-          ['alpine:8101518288111119448185914762536722131810', 'alpine:glibc'],
-          ['alpine:1.0.0-beta', 'alpine:1.0.0-beta:glibc'],
-          [
-            'registry.gitlab.com/group/project/tmp:af864bd61230d3d694eb01d6205b268b4ad63ac0',
-            'registry.gitlab.com/group/project/tmp:glibc'
-          ]
-        ]
-      end
-
-      with_them do
-        let(:params) do
-          {
-            image: image,
-            operating_system: 'debian:9',
-            package_name: 'glibc',
-            package_version: '1.2.3'
-          }
-        end
-
-        specify { expect(subject.fingerprint).to eq(sha1_of.call(expected_fingerprint_input)) }
-      end
+      specify { expect(subject.fingerprint).to eq(sha1_of.call(expected_fingerprint_input)) }
     end
   end
 end
