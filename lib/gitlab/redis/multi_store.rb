c3bab430e72dc0464fb28c48d68eed1d6fb6ecce
Aleksei Lipniagov <alipniagov@gitlab.com>
2021-12-21 17:50:22 +0300

    Revert "Fix Multistore FF for empty DB"
    This reverts commit f7bdab1bce77e2f7c47871739b60050e53682a27.

--- lib/gitlab/redis/multi_store.rb
+++ lib/gitlab/redis/multi_store.rb
@@ -97,15 +97,11 @@ module Gitlab
       end
 
       def use_primary_and_secondary_stores?
-        feature_flags_available? &&
-          Feature.enabled?("use_primary_and_secondary_stores_for_#{instance_name.underscore}", default_enabled: :yaml) &&
-          !same_redis_store?
+        Feature.enabled?("use_primary_and_secondary_stores_for_#{instance_name.underscore}", default_enabled: :yaml) && !same_redis_store?
       end
 
       def use_primary_store_as_default?
-        feature_flags_available? &&
-          Feature.enabled?("use_primary_store_as_default_for_#{instance_name.underscore}", default_enabled: :yaml) &&
-          !same_redis_store?
+        Feature.enabled?("use_primary_store_as_default_for_#{instance_name.underscore}", default_enabled: :yaml) && !same_redis_store?
       end
 
       private
