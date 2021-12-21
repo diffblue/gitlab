5e4cc7ebac15542c14fa82ba408d21d75345d774
Magdalena Frankiewicz <mfrankiewicz@gitlab.com>
2021-12-21 17:23:50 +0100

    Rate limit update username action
    This is to mitigate misuse, for example to mass-probe which usernames
    are in use.
    
    Changelog: security

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,6 +6,9 @@ class ProfilesController < Profiles::ApplicationController
 
   before_action :user
   before_action :authorize_change_username!, only: :update_username
+  before_action only: :update_username do
+    check_rate_limit!(:profile_update_username, scope: current_user)
+  end
   skip_before_action :require_email, only: [:show, :update]
   before_action do
     push_frontend_feature_flag(:webauthn, default_enabled: :yaml)
