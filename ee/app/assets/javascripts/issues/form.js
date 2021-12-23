e46c540259148483aef783b097dea52d74c9d5f7
Coung Ngo <cngo@gitlab.com>
2021-12-23 07:01:42 +0000

    Tidy ~/issues init functions

--- ee/app/assets/javascripts/issues/form.js
+++
@@ -1,7 +0,0 @@
-import initEpicsSelect from 'ee/vue_shared/components/sidebar/epics_select/epics_select_bundle';
-import initForm from '~/issues/form';
-
-export default () => {
-  initEpicsSelect();
-  initForm();
-};
