31f207b0d3aff075dbefcc45fd6a8677d26efa5d
Kev <kevslashnull@gmail.com>
2021-12-23 16:24:40 +0000

    Fix RSpec/TimecopFreeze offenses (Part 2/2)

--- ee/spec/lib/gitlab/geo/log_cursor/daemon_spec.rb
+++ ee/spec/lib/gitlab/geo/log_cursor/daemon_spec.rb
@@ -81,12 +81,10 @@ RSpec.describe Gitlab::Geo::LogCursor::Daemon, :clean_gitlab_redis_shared_state 
 
           is_expected.to receive(:find_and_handle_events!).and_raise('any error').twice
 
-          Timecop.freeze do
-            daemon.run_once!
+          daemon.run_once!
 
-            Timecop.travel(described_class::MAX_ERROR_DURATION + 1.second) do
-              daemon.run_once!
-            end
+          travel_to((described_class::MAX_ERROR_DURATION + 1.second).from_now) do
+            daemon.run_once!
           end
         end
       end
@@ -95,17 +93,15 @@ RSpec.describe Gitlab::Geo::LogCursor::Daemon, :clean_gitlab_redis_shared_state 
         it 'does not call `#exit!`' do
           is_expected.not_to receive(:exit!)
 
-          Timecop.freeze do
-            is_expected.to receive(:find_and_handle_events!).and_raise('any error')
+          is_expected.to receive(:find_and_handle_events!).and_raise('any error')
+          daemon.run_once!
+
+          travel_to((described_class::MAX_ERROR_DURATION + 1.second).from_now) do
+            is_expected.to receive(:find_and_handle_events!) # successful
             daemon.run_once!
 
-            Timecop.travel(described_class::MAX_ERROR_DURATION + 1.second) do
-              is_expected.to receive(:find_and_handle_events!) # successful
-              daemon.run_once!
-
-              is_expected.to receive(:find_and_handle_events!).and_raise('any error')
-              daemon.run_once!
-            end
+            is_expected.to receive(:find_and_handle_events!).and_raise('any error')
+            daemon.run_once!
           end
         end
       end
