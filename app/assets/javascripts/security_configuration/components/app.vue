92d6e517e4ae1fb5ec84efd014932bcb55d87baf
David Pisek <dpisek@gitlab.com>
2021-12-24 13:35:03 +0000

    Update security configuration tabs to utilize lazy loading

--- app/assets/javascripts/security_configuration/components/app.vue
+++ app/assets/javascripts/security_configuration/components/app.vue
@@ -160,7 +160,7 @@ export default {
       </template>
     </user-callout-dismisser>
 
-    <gl-tabs content-class="gl-pt-0" sync-active-tab-with-query-params>
+    <gl-tabs content-class="gl-pt-0" sync-active-tab-with-query-params lazy>
       <gl-tab
         data-testid="security-testing-tab"
         :title="$options.i18n.securityTesting"
