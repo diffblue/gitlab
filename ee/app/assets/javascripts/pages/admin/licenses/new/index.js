ce1c426d689ace07641add0904266d5b204dbc31
Ammar Alakkad <am.alakkad@gmail.com>
2021-12-29 15:32:00 +0300

    Change file input on upload license page to a dropzone
    Substitutes a Rails-rendered file input with a Vue dropzone component on upload
    license page. Now the user can use file picker window or drag-n-drop a file
    onto the form.
    
    Because the dropzone is used inside a Ruby form (not inside a Vue form), some
    changes were made to upload_dropzone component to support this: Allow
    upload_dropzone to use dynamic input name and to modify its input files
    property when files are droppped on it.
    
    Changelog: changed
    EE: true

--- ee/app/assets/javascripts/pages/admin/licenses/new/index.js
+++ ee/app/assets/javascripts/pages/admin/licenses/new/index.js
@@ -1,3 +1,6 @@
+import Vue from 'vue';
+import LicenseNewApp from 'ee/admin/licenses/new/components/license_new_app.vue';
+
 const licenseFile = document.querySelector('.license-file');
 const licenseKey = document.querySelector('.license-key');
 const acceptEULACheckBox = document.querySelector('#accept_eula');
@@ -15,6 +18,21 @@ const toggleUploadLicenseButton = () => {
   uploadLicenseBtn.toggleAttribute('disabled', !acceptEULACheckBox.checked);
 };
 
+const initLicenseUploadDropzone = () => {
+  const el = document.getElementById('js-license-new-app');
+
+  return new Vue({
+    el,
+    components: {
+      LicenseNewApp,
+    },
+    render(createElement) {
+      return createElement(LicenseNewApp);
+    },
+  });
+};
+
 licenseType.forEach((el) => el.addEventListener('change', showLicenseType));
 acceptEULACheckBox.addEventListener('change', toggleUploadLicenseButton);
 showLicenseType();
+initLicenseUploadDropzone();
