a1a5cb34efe5218b562c69e621fc586625b3ed0a
Fabio Huser <fabio.huser@siemens.com>
2021-12-29 13:55:23 +0100

    Add group level access token UI
    This commit is a follow up to
    https://gitlab.com/gitlab-org/gitlab/-/merge_requests/77236
    and adds the group level counterpart to the existing
    Project Access Token UI. Group Access Tokens can already be created
    by instance administrators via rails console or users via GitLab
    REST API but not yet on the UI itself. The new page allows group
    owners to list, create and delete said tokens.
    
    Closes https://gitlab.com/gitlab-org/gitlab/-/issues/214045
    
    Changelog: added

--- ee/spec/requests/projects/settings/access_tokens_controller_spec.rb
+++ ee/spec/requests/projects/settings/access_tokens_controller_spec.rb
@@ -5,7 +5,7 @@ require 'spec_helper'
 RSpec.describe Projects::Settings::AccessTokensController, :saas do
   let_it_be(:user) { create(:user) }
   let_it_be(:group) { create(:group_with_plan, plan: :bronze_plan) }
-  let_it_be(:project) { create(:project, group: group) }
+  let_it_be(:resource) { create(:project, group: group) }
   let_it_be(:bot_user) { create(:user, :project_bot) }
 
   before do
@@ -15,49 +15,58 @@ RSpec.describe Projects::Settings::AccessTokensController, :saas do
   end
 
   before_all do
-    project.add_maintainer(bot_user)
-    project.add_maintainer(user)
+    resource.add_maintainer(bot_user)
+    resource.add_maintainer(user)
   end
 
   shared_examples 'feature unavailable' do
     context 'with a free plan' do
       let(:group) { create(:group_with_plan, plan: :free_plan) }
-      let(:project) { create(:project, group: group) }
+      let(:resource) { create(:project, group: group) }
 
       it { is_expected.to have_gitlab_http_status(:not_found) }
     end
 
     context 'when user is not a maintainer with a paid group plan' do
       before do
-        project.add_developer(user)
+        resource.add_developer(user)
       end
 
       it { is_expected.to have_gitlab_http_status(:not_found) }
     end
   end
 
-  describe '#index' do
-    subject { get :index, params: { namespace_id: project.namespace, project_id: project } }
+  describe 'GET /:namespace/:project/-/settings/access_tokens' do
+    subject do
+      get project_settings_access_tokens_path(resource)
+      response
+    end
 
     it_behaves_like 'feature unavailable'
-    it_behaves_like 'project access tokens available #index'
+    it_behaves_like 'GET resource access tokens available'
   end
 
-  describe '#create' do
+  describe 'POST /:namespace/:project/-/settings/access_tokens' do
     let_it_be(:access_token_params) { { name: 'Nerd bot', scopes: ["api"], expires_at: Date.today + 1.month } }
 
-    subject { post :create, params: { namespace_id: project.namespace, project_id: project }.merge(project_access_token: access_token_params) }
+    subject do
+      post project_settings_access_tokens_path(resource), params: { resource_access_token: access_token_params }
+      response
+    end
 
     it_behaves_like 'feature unavailable'
-    it_behaves_like 'project access tokens available #create'
+    it_behaves_like 'POST resource access tokens available'
   end
 
-  describe '#revoke', :sidekiq_inline do
-    let(:project_access_token) { create(:personal_access_token, user: bot_user) }
+  describe 'PUT /:namespace/:project/-/settings/access_tokens/:id', :sidekiq_inline do
+    let(:resource_access_token) { create(:personal_access_token, user: bot_user) }
 
-    subject { put :revoke, params: { namespace_id: project.namespace, project_id: project, id: project_access_token } }
+    subject do
+      put revoke_project_settings_access_token_path(resource, resource_access_token)
+      response
+    end
 
     it_behaves_like 'feature unavailable'
-    it_behaves_like 'project access tokens available #revoke'
+    it_behaves_like 'PUT resource access tokens available'
   end
 end
