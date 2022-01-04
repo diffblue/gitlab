3182a9464822dc2d15c61fc1c6fa970e7fd267c4
Drew Blessing <drew@gitlab.com>
2022-01-04 14:57:46 -0600

    Prevent Group API N+1 loading group plans
    When querying the Groups Project API and including subgroups,
    N+1 queries can take place when loading plans for all the groups
    in the hierarchy. Plans are only applied at the top-level group
    so it is unnecessarily inefficient to load plans for each project
    and associated group in the hierarchy. This change loads the plans
    once and memoizes them for all project groups.
    
    Changelog: fixed
    EE: true

---
+++ ee/app/models/ee/preloaders/single_hierarchy_project_group_plans_preloader.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module EE
+  module Preloaders
+    module SingleHierarchyProjectGroupPlansPreloader
+      def execute
+        return unless ::Feature.enabled?(:group_project_api_preload_plans, default_enabled: :yaml)
+        return unless ::Gitlab::CurrentSettings.should_check_namespace_plan?
+        return unless project = projects.take
+
+        plans = project.namespace.root_ancestor.plans
+
+        preload_plans(plans)
+      end
+
+      def preload_plans(plans)
+        return unless plans.any?
+
+        projects.each do |project|
+          project.namespace.memoized_plans = plans
+        end
+      end
+    end
+  end
+end
