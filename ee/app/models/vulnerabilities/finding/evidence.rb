44fa8380e8f353b14b19d4dabe956fe9f22f2e14
Jonathan Schafer <jschafer@gitlab.com>
2022-01-06 13:28:33 -0600

    Remove summary from Finding Evidence
    The summary field in the Vulnerability Finding Evidenced
    is not used and can be removed.
    
    Changelog: changed

--- ee/app/models/vulnerabilities/finding/evidence.rb
+++ ee/app/models/vulnerabilities/finding/evidence.rb
@@ -11,7 +11,6 @@ module Vulnerabilities
                  foreign_key: 'vulnerability_occurrence_id',
                  optional: false
 
-      validates :summary, length: { maximum: 8_000_000 }
       validates :data, length: { maximum: 16_000_000 }, presence: true
     end
   end
