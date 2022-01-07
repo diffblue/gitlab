aa4207006a9b63d2c3731e680551eb886c90df9e
Aakriti Gupta <agupta@gitlab.com>
2022-01-07 00:10:18 +0000

    Geo: Verify LFS objects
    Remove feature flag geo_lfs_object_verification.
    
    EE: true
    Changelog: changed

--- ee/app/replicators/geo/lfs_object_replicator.rb
+++ ee/app/replicators/geo/lfs_object_replicator.rb
@@ -15,7 +15,7 @@ module Geo
 
     override :verification_feature_flag_enabled?
     def self.verification_feature_flag_enabled?
-      Feature.enabled?(:geo_lfs_object_verification, default_enabled: :yaml)
+      true
     end
   end
 end
