470b4983f54cf40567339ba918a0da1cfa017f2e
vvempati <vvempati@gitlab.com>
2022-01-11 12:21:37 +1300

    Make propsData an arg so we can use createComponent for CI minutes tests

--- ee/spec/frontend/subscriptions/buy_addons_shared/app_spec.js
+++ ee/spec/frontend/subscriptions/buy_addons_shared/app_spec.js
@@ -29,31 +29,14 @@ import { mockStoragePlans } from 'ee_jest/subscriptions/mock_data';
 const localVue = createLocalVue();
 localVue.use(VueApollo);
 
-describe('Buy Storage App', () => {
+describe('Buy Addons Shared App', () => {
   let wrapper;
 
-  function createComponent(apolloProvider) {
+  function createComponent(apolloProvider, propsData) {
     wrapper = shallowMountExtended(App, {
       localVue,
       apolloProvider,
-      propsData: {
-        config: {
-          alertText: '',
-          formula: I18N_DETAILS_FORMULA,
-          formulaWithAlert: I18N_DETAILS_FORMULA_WITH_ALERT,
-          formulaTotal: I18N_STORAGE_FORMULA_TOTAL,
-          hasExpiration: true,
-          pricePerUnit: I18N_STORAGE_PRICE_PER_UNIT,
-          productLabel: I18N_STORAGE_PRODUCT_LABEL,
-          productUnit: I18N_STORAGE_PRODUCT_UNIT,
-          quantityPerPack: STORAGE_PER_PACK,
-          summaryTitle: i18nStorageSummaryTitle,
-          summaryTotal: I18N_STORAGE_SUMMARY_TOTAL,
-          title: I18N_STORAGE_TITLE,
-          tooltipNote: I18N_STORAGE_TOOLTIP_NOTE,
-        },
-        tags: [planTags.STORAGE_PLAN],
-      },
+      propsData,
       stubs: {
         Checkout,
         AddonPurchaseDetails,
@@ -64,6 +47,24 @@ describe('Buy Storage App', () => {
     return waitForPromises();
   }
 
+  const STORAGE_ADDON_PROPS = {
+    config: {
+      alertText: '',
+      formula: I18N_DETAILS_FORMULA,
+      formulaWithAlert: I18N_DETAILS_FORMULA_WITH_ALERT,
+      formulaTotal: I18N_STORAGE_FORMULA_TOTAL,
+      hasExpiration: true,
+      pricePerUnit: I18N_STORAGE_PRICE_PER_UNIT,
+      productLabel: I18N_STORAGE_PRODUCT_LABEL,
+      productUnit: I18N_STORAGE_PRODUCT_UNIT,
+      quantityPerPack: STORAGE_PER_PACK,
+      summaryTitle: i18nStorageSummaryTitle,
+      summaryTotal: I18N_STORAGE_SUMMARY_TOTAL,
+      title: I18N_STORAGE_TITLE,
+      tooltipNote: I18N_STORAGE_TOOLTIP_NOTE,
+    },
+    tags: [planTags.STORAGE_PLAN],
+  };
   const getStoragePlan = () => pick(mockStoragePlans[0], ['id', 'code', 'pricePerYear', 'name']);
   const findCheckout = () => wrapper.findComponent(Checkout);
   const findOrderSummary = () => wrapper.findComponent(OrderSummary);
@@ -77,112 +78,114 @@ describe('Buy Storage App', () => {
     wrapper.destroy();
   });
 
-  describe('when data is received', () => {
-    beforeEach(() => {
-      const mockApollo = createMockApolloProvider({
-        plansQueryMock: jest.fn().mockResolvedValue({ data: { plans: mockStoragePlans } }),
+  describe('Storage', () => {
+    describe('when data is received', () => {
+      beforeEach(() => {
+        const mockApollo = createMockApolloProvider({
+          plansQueryMock: jest.fn().mockResolvedValue({ data: { plans: mockStoragePlans } }),
+        });
+        return createComponent(mockApollo, STORAGE_ADDON_PROPS);
       });
-      return createComponent(mockApollo);
-    });
 
-    it('should display the root element', () => {
-      expect(findRootElement().exists()).toBe(true);
-      expect(wrapper.findComponent(GlEmptyState).exists()).toBe(false);
-    });
+      it('should display the root element', () => {
+        expect(findRootElement().exists()).toBe(true);
+        expect(wrapper.findComponent(GlEmptyState).exists()).toBe(false);
+      });
 
-    it('provides the correct props to checkout', () => {
-      expect(findCheckout().props()).toMatchObject({
-        plan: { ...getStoragePlan, isAddon: true },
+      it('provides the correct props to checkout', () => {
+        expect(findCheckout().props()).toMatchObject({
+          plan: { ...getStoragePlan, isAddon: true },
+        });
+      });
+
+      it('provides the correct props to order summary', () => {
+        expect(findOrderSummary().props()).toMatchObject({
+          plan: { ...getStoragePlan, isAddon: true },
+          title: I18N_STORAGE_TITLE,
+        });
       });
     });
 
-    it('provides the correct props to order summary', () => {
-      expect(findOrderSummary().props()).toMatchObject({
-        plan: { ...getStoragePlan, isAddon: true },
-        title: I18N_STORAGE_TITLE,
+    describe('when data is not received', () => {
+      it('should display the GlEmptyState for empty data', async () => {
+        const mockApollo = createMockApolloProvider({
+          plansQueryMock: jest.fn().mockResolvedValue({ data: null }),
+        });
+        await createComponent(mockApollo, STORAGE_ADDON_PROPS);
+
+        expect(findRootElement().exists()).toBe(false);
+        expect(wrapper.findComponent(GlEmptyState).exists()).toBe(true);
+      });
+
+      it('should display the GlEmptyState for empty plans', async () => {
+        const mockApollo = createMockApolloProvider({
+          plansQueryMock: jest.fn().mockResolvedValue({ data: { plans: null } }),
+        });
+        await createComponent(mockApollo, STORAGE_ADDON_PROPS);
+
+        expect(findRootElement().exists()).toBe(false);
+        expect(wrapper.findComponent(GlEmptyState).exists()).toBe(true);
+      });
+
+      it('should display the GlEmptyState for plans data of wrong type', async () => {
+        const mockApollo = createMockApolloProvider({
+          plansQueryMock: jest.fn().mockResolvedValue({ data: { plans: {} } }),
+        });
+        await createComponent(mockApollo, STORAGE_ADDON_PROPS);
+
+        expect(findRootElement().exists()).toBe(false);
+        expect(wrapper.findComponent(GlEmptyState).exists()).toBe(true);
       });
     });
-  });
 
-  describe('when data is not received', () => {
-    it('should display the GlEmptyState for empty data', async () => {
-      const mockApollo = createMockApolloProvider({
-        plansQueryMock: jest.fn().mockResolvedValue({ data: null }),
+    describe('when an error is received', () => {
+      it('should display the GlEmptyState', async () => {
+        const mockApollo = createMockApolloProvider({
+          plansQueryMock: jest.fn().mockRejectedValue(new Error('An error happened!')),
+        });
+        await createComponent(mockApollo, STORAGE_ADDON_PROPS);
+
+        expect(findRootElement().exists()).toBe(false);
+        expect(wrapper.findComponent(GlEmptyState).exists()).toBe(true);
       });
-      await createComponent(mockApollo);
-
-      expect(findRootElement().exists()).toBe(false);
-      expect(wrapper.findComponent(GlEmptyState).exists()).toBe(true);
     });
 
-    it('should display the GlEmptyState for empty plans', async () => {
-      const mockApollo = createMockApolloProvider({
-        plansQueryMock: jest.fn().mockResolvedValue({ data: { plans: null } }),
+    describe('labels', () => {
+      it('shows labels correctly for 1 pack', async () => {
+        const mockApollo = createMockApolloProvider({
+          plansQueryMock: jest.fn().mockResolvedValue({ data: { plans: mockStoragePlans } }),
+        });
+        await createComponent(mockApollo, STORAGE_ADDON_PROPS);
+
+        expect(findQuantityText().text()).toMatchInterpolatedText(
+          'x 10 GB per pack = 10 GB of storage',
+        );
+        expect(findSummaryLabel().text()).toBe('1 storage pack');
+        expect(findSummaryTotal().text()).toBe('Total storage: 10 GB');
+        expect(findPriceLabel().text()).toBe('$60 per 10 GB storage pack per year');
       });
-      await createComponent(mockApollo);
 
-      expect(findRootElement().exists()).toBe(false);
-      expect(wrapper.findComponent(GlEmptyState).exists()).toBe(true);
-    });
+      it('shows labels correctly for 2 packs', async () => {
+        const mockApollo = createMockApolloProvider({
+          plansQueryMock: jest.fn().mockResolvedValue({ data: { plans: mockStoragePlans } }),
+        }, { quantity: 2 });
+        await createComponent(mockApollo, STORAGE_ADDON_PROPS);
 
-    it('should display the GlEmptyState for plans data of wrong type', async () => {
-      const mockApollo = createMockApolloProvider({
-        plansQueryMock: jest.fn().mockResolvedValue({ data: { plans: {} } }),
+        expect(findQuantityText().text()).toMatchInterpolatedText(
+          'x 10 GB per pack = 20 GB of storage',
+        );
+        expect(findSummaryLabel().text()).toBe('2 storage packs');
+        expect(findSummaryTotal().text()).toBe('Total storage: 20 GB');
       });
-      await createComponent(mockApollo);
 
-      expect(findRootElement().exists()).toBe(false);
-      expect(wrapper.findComponent(GlEmptyState).exists()).toBe(true);
-    });
-  });
+      it('does not show labels if input is invalid', async () => {
+        const mockApollo = createMockApolloProvider({
+          plansQueryMock: jest.fn().mockResolvedValue({ data: { plans: mockStoragePlans } }),
+        }, { quantity: -1 });
+        await createComponent(mockApollo, STORAGE_ADDON_PROPS);
 
-  describe('when an error is received', () => {
-    it('should display the GlEmptyState', async () => {
-      const mockApollo = createMockApolloProvider({
-        plansQueryMock: jest.fn().mockRejectedValue(new Error('An error happened!')),
+        expect(findQuantityText().text()).toMatchInterpolatedText('x 10 GB per pack');
       });
-      await createComponent(mockApollo);
-
-      expect(findRootElement().exists()).toBe(false);
-      expect(wrapper.findComponent(GlEmptyState).exists()).toBe(true);
-    });
-  });
-
-  describe('labels', () => {
-    it('shows labels correctly for 1 pack', async () => {
-      const mockApollo = createMockApolloProvider({
-        plansQueryMock: jest.fn().mockResolvedValue({ data: { plans: mockStoragePlans } }),
-      });
-      await createComponent(mockApollo);
-
-      expect(findQuantityText().text()).toMatchInterpolatedText(
-        'x 10 GB per pack = 10 GB of storage',
-      );
-      expect(findSummaryLabel().text()).toBe('1 storage pack');
-      expect(findSummaryTotal().text()).toBe('Total storage: 10 GB');
-      expect(findPriceLabel().text()).toBe('$60 per 10 GB storage pack per year');
-    });
-
-    it('shows labels correctly for 2 packs', async () => {
-      const mockApollo = createMockApolloProvider({
-        plansQueryMock: jest.fn().mockResolvedValue({ data: { plans: mockStoragePlans } }),
-      }, { quantity: 2 });
-      await createComponent(mockApollo);
-
-      expect(findQuantityText().text()).toMatchInterpolatedText(
-        'x 10 GB per pack = 20 GB of storage',
-      );
-      expect(findSummaryLabel().text()).toBe('2 storage packs');
-      expect(findSummaryTotal().text()).toBe('Total storage: 20 GB');
-    });
-
-    it('does not show labels if input is invalid', async () => {
-      const mockApollo = createMockApolloProvider({
-        plansQueryMock: jest.fn().mockResolvedValue({ data: { plans: mockStoragePlans } }),
-      }, { quantity: -1 });
-      await createComponent(mockApollo);
-
-      expect(findQuantityText().text()).toMatchInterpolatedText('x 10 GB per pack');
     });
   });
 });
