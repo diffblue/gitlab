d3bf60c2b6922c545af18ed1b2da8f84594835d7
Kamil Trzciński <ayufan@ayufan.eu>
2022-01-12 15:52:55 +0100

    Fix `terraform_state_version_replicator_spec.rb` cross-DB modification
    This fixes specs cross-DB modification to allow remove it from
    cross-DB-allowlist.yml.

--- ee/spec/replicators/geo/terraform_state_version_replicator_spec.rb
+++ ee/spec/replicators/geo/terraform_state_version_replicator_spec.rb
@@ -3,7 +3,10 @@
 require 'spec_helper'
 
 RSpec.describe Geo::TerraformStateVersionReplicator do
-  let(:model_record) { build(:terraform_state_version, terraform_state: create(:terraform_state)) }
+  let_it_be(:ci_build) { create(:ci_build) }
+  let_it_be(:terraform_state) { create(:terraform_state, project: ci_build.project) }
+
+  let(:model_record) { build(:terraform_state_version, build: ci_build, terraform_state: terraform_state) }
 
   include_examples 'a blob replicator'
   include_examples 'a verifiable replicator'
