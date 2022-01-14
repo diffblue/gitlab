d51060439e3a04f0061f56c8c6124f70d41e6d74
Sarah Yasonik <syasonik@gitlab.com>
2022-01-14 11:00:45 +0000

    Add paging status field and mutation for incidents
    Adds a status field in GraphQL for issues which
    support escalations. Functionality available
    behind the :incident_escalations feature flag.

--- app/graphql/mutations/issues/set_escalation_status.rb
+++ app/graphql/mutations/issues/set_escalation_status.rb
@@ -2,17 +2,14 @@
 
 module Mutations
   module Issues
-    class SetEscalationPolicy < Base
-      graphql_name 'IssueSetEscalationPolicy'
+    class SetEscalationStatus < Base
+      graphql_name 'IssueSetEscalationStatus'
 
-      argument :escalation_policy_id,
-               ::Types::GlobalIDType[::IncidentManagement::EscalationPolicy],
-               required: false,
-               loads: Types::IncidentManagement::EscalationPolicyType,
-               description: 'Global ID of the escalation policy to assign to the issue. ' \
-               'Policy will be removed if absent or set to null.'
+      argument :status, Types::IncidentManagement::EscalationStatusEnum,
+               required: true,
+               description: 'Set the escalation status.'
 
-      def resolve(project_path:, iid:, escalation_policy:)
+      def resolve(project_path:, iid:, status:)
         issue = authorized_find!(project_path: project_path, iid: iid)
         project = issue.project
 
@@ -22,7 +19,7 @@ module Mutations
         ::Issues::UpdateService.new(
           project: project,
           current_user: current_user,
-          params: { escalation_status: { policy: escalation_policy } }
+          params: { escalation_status: { status: status } }
         ).execute(issue)
 
         {
