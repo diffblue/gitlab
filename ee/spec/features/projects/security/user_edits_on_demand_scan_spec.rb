b880dcb10a7a24989f9cfe9bb5986ceb20eacad8
Paul Gascou-Vaillancourt <pgascouvaillancourt@gitlab.com>
2022-01-14 17:02:11 +0000

    Update on-demand scans breadcrumbs
    This adds a link to the on-demand scans library in the on-demand scans
    form's breadcrumbs.
    
    Changelog: changed
    EE: true

---
+++ ee/spec/features/projects/security/user_edits_on_demand_scan_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe 'User edits On-demand Scan' do
+  let_it_be(:user) { create(:user) }
+  let_it_be(:project) { create(:project, :repository) }
+  let_it_be(:dast_profile) { create(:dast_profile, project: project) }
+
+  let(:on_demand_scans_path) { project_on_demand_scans_path(project) }
+  let(:edit_on_demand_scan_path) { edit_project_on_demand_scan_path(project, dast_profile) }
+
+  before_all do
+    project.add_developer(user)
+  end
+
+  before do
+    sign_in(user)
+    stub_licensed_features(security_on_demand_scans: true)
+    visit(edit_on_demand_scan_path)
+  end
+
+  it 'shows edit scan page', :aggregate_failures, :js do
+    expect(page).to have_content 'Edit on-demand DAST scan'
+
+    page.within '.breadcrumbs' do
+      expect(page).to have_link('On-demand Scans', href: project_on_demand_scans_path(project, anchor: 'saved'))
+      expect(page).to have_link('Edit on-demand DAST scan', href: edit_on_demand_scan_path)
+    end
+  end
+end
