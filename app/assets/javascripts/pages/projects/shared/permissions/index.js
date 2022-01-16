9002c838ebc1522bb68cbfdf0a99df4c9078c46b
Ezekiel Kigbo <ekigbo@gitlab.com>
2022-01-17 01:36:55 +1100

    Move save visbility settings button into vue
    Moves the save visibility settings submit button
    from haml to the settings

--- app/assets/javascripts/pages/projects/shared/permissions/index.js
+++ app/assets/javascripts/pages/projects/shared/permissions/index.js
@@ -6,8 +6,28 @@ export default function initProjectPermissionsSettings() {
   const componentPropsEl = document.querySelector('.js-project-permissions-form-data');
   const componentProps = JSON.parse(componentPropsEl.innerHTML);
 
+  const {
+    additionalInformation,
+    confirmDangerMessage,
+    confirmButtonText,
+    phrase: confirmationPhrase,
+  } = mountPoint.dataset;
+  console.log('mountPoint', mountPoint.dataset);
+
   return new Vue({
     el: mountPoint,
-    render: (createElement) => createElement(settingsPanel, { props: { ...componentProps } }),
+    provide: {
+      htmlConfirmationMessage: true,
+      additionalInformation,
+      confirmDangerMessage,
+      confirmButtonText,
+    },
+    render: (createElement) =>
+      createElement(settingsPanel, {
+        props: {
+          ...componentProps,
+          confirmationPhrase,
+        },
+      }),
   });
 }
