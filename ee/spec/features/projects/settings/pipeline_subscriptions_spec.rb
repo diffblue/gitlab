3891ce09fcfa1e4f891b11c4faf70974f2095615
Caleb Cooper <6532460-calebcooper@users.noreply.gitlab.com>
2022-01-17 22:52:05 +0000

    Improving pipeline subscriptions screen

--- ee/spec/features/projects/settings/pipeline_subscriptions_spec.rb
+++ ee/spec/features/projects/settings/pipeline_subscriptions_spec.rb
@@ -5,11 +5,13 @@ require 'spec_helper'
 RSpec.describe 'Project Subscriptions', :js do
   let(:project) { create(:project, :public, :repository) }
   let(:upstream_project) { create(:project, :public, :repository) }
+  let(:downstream_project) { create(:project, :public, :repository, upstream_projects: [project]) }
   let(:user) { create(:user) }
 
   before do
     project.add_maintainer(user)
     upstream_project.add_maintainer(user)
+    downstream_project.add_maintainer(user)
 
     stub_licensed_features(ci_project_subscriptions: true)
 
@@ -25,6 +27,21 @@ RSpec.describe 'Project Subscriptions', :js do
     end
   end
 
+  it 'renders the list of downstream projects' do
+    within '[data-testid="downstream-project-subscriptions"]' do
+      expect(find('.badge-pill').text).to eq '1'
+    end
+
+    expect(page).to have_content(downstream_project.name)
+    expect(page).to have_content(downstream_project.owner.name)
+  end
+
+  it 'doesn\'t allow to delete downstream projects' do
+    within '[data-testid="downstream-project-subscriptions"]' do
+      expect(page).not_to have_content('[data-testid="delete-subscription"]')
+    end
+  end
+
   it 'successfully creates new pipeline subscription' do
     within '#pipeline-subscriptions' do
       within 'form' do
@@ -33,7 +50,9 @@ RSpec.describe 'Project Subscriptions', :js do
         click_on 'Subscribe'
       end
 
-      expect(find('.badge-pill').text).to eq '1'
+      within '[data-testid="upstream-project-subscriptions"]' do
+        expect(find('.badge-pill').text).to eq '1'
+      end
 
       expect(page).to have_content(upstream_project.name)
       expect(page).to have_content(upstream_project.owner.name)
@@ -50,8 +69,10 @@ RSpec.describe 'Project Subscriptions', :js do
         click_on 'Subscribe'
       end
 
-      expect(find('.badge-pill').text).to eq '0'
-      expect(all('tbody tr').count).to eq(0)
+      within '[data-testid="upstream-project-subscriptions"]' do
+        expect(find('.badge-pill').text).to eq '0'
+        expect(page).to have_content('This project is not subscribed to any project pipelines.')
+      end
     end
 
     expect(page).to have_content('This project path either does not exist or you do not have access.')
