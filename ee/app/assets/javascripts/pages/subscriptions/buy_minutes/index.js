360dab06873c20b3652de05642984950da8486f7
Lauren Barker <lbarker@gitlab.com>
2022-01-19 14:28:30 -0700

    Add to cart event for Google Tag Manager
    This commit adds a "add to cart" event for the
    Google Tag Manager data layer when a user clicks
    the "buy additional minutes" from the usage tab.
    
    See https://gitlab.com/gitlab-org/gitlab/-/merge_requests/79038

--- ee/app/assets/javascripts/pages/subscriptions/buy_minutes/index.js
+++ ee/app/assets/javascripts/pages/subscriptions/buy_minutes/index.js
@@ -1,3 +1,5 @@
 import mountSubscriptionsApplication from 'ee/subscriptions/buy_minutes';
+import { trackAddToCartUsageTab } from '~/google_tag_manager';
 
 mountSubscriptionsApplication(document.getElementById('js-buy-minutes'));
+trackAddToCartUsageTab();
