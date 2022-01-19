48c95031377b2cbd1a057f186b939fa59906d5a0
Frédéric Caplette <fcaplette@gitlab.com>
2022-01-19 15:13:05 -0500

    Keep branch when going to Pipeline Editor through nav
    When going to the pipeline editor through the menu, we
    should preserve the branch the user was currently on.
    This now passes the current ref in the nav item
    so that we can load the right ref context right
    when arriving in the pipeline editor.
    
    Changelog: changed

--- ee/spec/lib/ee/sidebars/projects/menus/ci_cd_menu_spec.rb
+++ ee/spec/lib/ee/sidebars/projects/menus/ci_cd_menu_spec.rb
@@ -5,7 +5,7 @@ require 'spec_helper'
 RSpec.describe Sidebars::Projects::Menus::CiCdMenu do
   let(:project) { build(:project) }
   let(:user) { project.owner }
-  let(:context) { Sidebars::Projects::Context.new(current_user: user, container: project, can_view_pipeline_editor: true) }
+  let(:context) { Sidebars::Projects::Context.new(current_user: user, current_ref: 'main', container: project, can_view_pipeline_editor: true) }
 
   describe 'Test Cases' do
     subject { described_class.new(context).renderable_items.index { |e| e.item_id == :test_cases} }
