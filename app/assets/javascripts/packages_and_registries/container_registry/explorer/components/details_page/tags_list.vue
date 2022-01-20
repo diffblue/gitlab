20afc0d1848d2768262487f7e735d9bec216cf86
orozot <ztren@gitlab.cn>
2022-01-20 18:23:06 +0800

    Simplify the import module path

--- app/assets/javascripts/packages_and_registries/container_registry/explorer/components/details_page/tags_list.vue
+++ app/assets/javascripts/packages_and_registries/container_registry/explorer/components/details_page/tags_list.vue
@@ -6,7 +6,7 @@ import { joinPaths } from '~/lib/utils/url_utility';
 import RegistryList from '~/packages_and_registries/shared/components/registry_list.vue';
 
 import PersistedSearch from '~/packages_and_registries/shared/components/persisted_search.vue';
-import { FILTERED_SEARCH_TERM } from '~/packages_and_registries/shared/constants/index';
+import { FILTERED_SEARCH_TERM } from '~/packages_and_registries/shared/constants';
 import TagsLoader from '~/packages_and_registries/shared/components/tags_loader.vue';
 import {
   REMOVE_TAGS_BUTTON_TITLE,
