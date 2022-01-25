b48ca1eaf7524f5e8dadcea8c77916109e2ac317
Gerardo <ggutierrez@gitlab.com>
2022-01-25 09:07:49 -0600

    Fixed working and regenerated loacle files

--- ee/app/assets/javascripts/vue_shared/purchase_flow/constants.js
+++ ee/app/assets/javascripts/vue_shared/purchase_flow/constants.js
@@ -1,5 +1,5 @@
 import { s__ } from '~/locale';
 
 export const GENERAL_ERROR_MESSAGE = s__(
-  'PurchaseStep|An error occurred in the purchase step. If the problem persists please contact at https://support.gitlab.com.',
+  'PurchaseStep|An error occurred in the purchase step. If the problem persists please contact support at https://support.gitlab.com.',
 );
