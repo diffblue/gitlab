b35e6517cd0618665858e784c0b28e162824c894
Adam Hegyi <ahegyi@gitlab.com>
2022-01-25 14:14:55 +0100

    Remove GL.com check

--- db/post_migrate/20220124214131_remove_projects_ci_refs_project_id_fk.rb
+++ db/post_migrate/20220124214131_remove_projects_ci_refs_project_id_fk.rb
@@ -4,7 +4,6 @@ class RemoveProjectsCiRefsProjectIdFk < Gitlab::Database::Migration[1.0]
   disable_ddl_transaction!
 
   def up
-    return if Gitlab.com? # unsafe migration, skip on GitLab.com due to https://gitlab.com/groups/gitlab-org/-/epics/7249#note_819625526
     return unless foreign_key_exists?(:ci_refs, :projects, name: "fk_rails_4249db8cc3")
 
     with_lock_retries do
