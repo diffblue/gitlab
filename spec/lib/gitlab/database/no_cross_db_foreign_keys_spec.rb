ed97fc5739f8bd8e699574afd4c4ec55dca9b14b
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2022-01-26 17:19:13 -0300

    Swap FK ci_job_token_project_scope_links to projects for LFK
    Swaps FK for ci_job_token_project_scope_links.target_project_id to projects
    
    Changelog: changed

--- spec/lib/gitlab/database/no_cross_db_foreign_keys_spec.rb
+++ spec/lib/gitlab/database/no_cross_db_foreign_keys_spec.rb
@@ -18,7 +18,6 @@ RSpec.describe 'cross-database foreign keys' do
       ci_freeze_periods.project_id
       ci_job_artifacts.project_id
       ci_job_token_project_scope_links.added_by_id
-      ci_job_token_project_scope_links.target_project_id
       ci_pending_builds.namespace_id
       ci_pending_builds.project_id
       ci_pipeline_schedules.owner_id
