a937d1c0d7ede56d6e2e19ff12787896ceb05938
Sarah Yasonik <syasonik@gitlab.com>
2022-01-26 08:50:00 +0000

    Add elements to incident page to render escalation info
    Adds elements to html served by backend for frontend to
    render incident status and escaltion policy. Includes
    exposing permissions.
    
    Moves feature flag check from EE to CE, to ensure the
    page does not contain new elements if the feature is
    disabled.

--- ee/spec/serializers/ee/issue_sidebar_basic_entity_spec.rb
+++ ee/spec/serializers/ee/issue_sidebar_basic_entity_spec.rb
@@ -3,34 +3,98 @@
 require 'spec_helper'
 
 RSpec.describe EE::IssueSidebarBasicEntity do
-  let(:admin) { create(:admin) }
-  let(:user) { create(:user) }
-  let(:project) { create(:project, :repository) }
-  let(:issue) { create(:issue, project: project, assignees: [user]) }
-  let(:subject) { IssueSerializer.new(current_user: user, project: project) }
+  let_it_be(:admin) { create(:admin) }
+  let_it_be(:user) { create(:user) }
+  let_it_be(:project) { create(:project, :repository) }
+  let_it_be(:issue, reload: true) { create(:issue, project: project, assignees: [user]) }
 
-  context "When serializing" do
-    context "with the cve_id_request_button" do
-      using RSpec::Parameterized::TableSyntax
+  let(:serializer) { IssueSerializer.new(current_user: user, project: project) }
 
-      where(:is_gitlab_com, :is_public, :is_admin, :expected_value) do
-        true  | true  | true  | true
-        true  | false | true  | false
-        true  | false | false | false
-        false | false | true  | false
-        false | false | false | false
+  subject(:entity) { serializer.represent(issue, serializer: 'sidebar') }
+
+  it 'contains keys related to issuables' do
+    expect(entity).to include(
+      :scoped_labels_available, :supports_weight, :supports_iterations,
+      :supports_escalation_policies
+    )
+  end
+
+  it 'contains attributes related to the issue' do
+    expect(entity).to include(:supports_epic, :features_available, :request_cve_enabled_for_user)
+  end
+
+  it 'contains attributes related to the available features' do
+    expect(entity[:features_available]).to include(:health_status, :issue_weights, :epics)
+  end
+
+  describe 'request_cve_enabled_for_user' do
+    using RSpec::Parameterized::TableSyntax
+
+    where(:is_gitlab_com, :is_public, :is_admin, :expected_value) do
+      true  | true  | true  | true
+      true  | false | true  | false
+      true  | false | false | false
+      false | false | true  | false
+      false | false | false | false
+    end
+    with_them do
+      before do
+        allow(issue.project).to receive(:public?).and_return(is_public)
+        issue.project.add_maintainer(user) if is_admin
+        allow(Gitlab).to receive(:com?).and_return(is_gitlab_com)
       end
-      with_them do
-        before do
-          allow(issue.project).to receive(:public?).and_return(is_public)
-          issue.project.add_maintainer(user) if is_admin
-          allow(Gitlab).to receive(:com?).and_return(is_gitlab_com)
-        end
 
-        it 'uses the value from request_cve_enabled_for_user' do
-          data = subject.represent(issue, serializer: 'sidebar')
-          expect(data[:request_cve_enabled_for_user]).to eq(expected_value)
-        end
+      it 'uses the value from request_cve_enabled_for_user' do
+        expect(entity[:request_cve_enabled_for_user]).to eq(expected_value)
+      end
+    end
+  end
+
+  describe 'can_update_escalation_policy' do
+    before do
+      issue.update!(issue_type: Issue.issue_types[:incident])
+      stub_licensed_features(oncall_schedules: true, escalation_policies: true)
+      project.add_developer(user)
+    end
+
+    it 'is present and true' do
+      expect(entity[:current_user][:can_update_escalation_policy]).to be(true)
+    end
+
+    context 'for a standard issue' do
+      subject(:entity) { serializer.represent(create(:issue, project: project), serializer: 'sidebar') }
+
+      it 'is not present' do
+        expect(entity[:current_user]).not_to have_key(:can_update_escalation_policy)
+      end
+    end
+
+    context 'with escalations policies disabled' do
+      before do
+        stub_licensed_features(escalation_policies: false)
+      end
+
+      it 'is not present' do
+        expect(entity[:current_user]).not_to have_key(:can_update_escalation_policy)
+      end
+    end
+
+    context 'without permissions' do
+      let(:serializer) { IssueSerializer.new(current_user: create(:user), project: project) }
+
+      it 'is present and false' do
+        expect(entity[:current_user]).to have_key(:can_update_escalation_policy)
+        expect(entity[:current_user][:can_update_escalation_policy]).to be(false)
+      end
+    end
+
+    context 'with :incident_escalations feature flag disabled' do
+      before do
+        stub_feature_flags(incident_escalations: false)
+      end
+
+      it 'is not present' do
+        expect(entity[:current_user]).not_to include(:can_update_escalation_policy)
       end
     end
   end
