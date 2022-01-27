d0ea399892e21a55236474c9f1fe74d16a66671f
Mehmet Emin INAC <minac@gitlab.com>
2022-01-28 00:07:18 +0300

    Sort vulnerability identifiers on ingestion to prevent Deadlock errors
    It is possible to have multiple threads trying to ingest vulnerability
    identifiers for the same project in different orders which can cause
    multiple connections to wait for each other to release row locks to
    proceed which introduces a deadlock error.
    
    By sorting the identifiers by fingerprint before ingestion, we prevent
    the risk of having multiple transactions to wait for each other.
    
    Changelog: fixed
    EE: true

--- ee/spec/services/security/ingestion/finding_map_collection_spec.rb
+++ ee/spec/services/security/ingestion/finding_map_collection_spec.rb
@@ -4,25 +4,30 @@ require 'spec_helper'
 
 RSpec.describe Security::Ingestion::FindingMapCollection do
   describe '#each_slice' do
-    let(:security_scan) { create(:security_scan) }
-    let(:security_findings) { create_list(:security_finding, 3, scan: security_scan, deduplicated: true) }
-    let(:report_findings) { [] }
+    let_it_be(:security_scan) { create(:security_scan) }
+    let_it_be(:security_finding_1) { create(:security_finding, overridden_uuid: '18a77231-f01d-40eb-80f0-de2ddb769a2c', uuid: '78a77231-f01d-40eb-80f0-de2ddb769a2c', scan: security_scan, deduplicated: true) }
+    let_it_be(:security_finding_2) { create(:security_finding, uuid: '88a77231-f01d-40eb-80f0-de2ddb769a2c', scan: security_scan, deduplicated: true) }
+    let_it_be(:security_finding_3) { create(:security_finding, overridden_uuid: '28a77231-f01d-40eb-80f0-de2ddb769a2c', uuid: '98a77231-f01d-40eb-80f0-de2ddb769a2c', scan: security_scan, deduplicated: true) }
+
     let(:finding_map_collection) { described_class.new(security_scan) }
     let(:finding_maps) { [] }
+    let(:report_findings) { [] }
     let(:finding_pairs) { finding_maps.map { |finding_map| [finding_map.security_finding, finding_map.report_finding] } }
     let(:test_block) { proc { |slice| finding_maps.concat(slice) } }
     let(:expected_finding_pairs) do
       [
-        [security_findings[0], report_findings[0]],
-        [security_findings[1], report_findings[1]],
-        [security_findings[2], report_findings[2]]
+        [security_finding_3, report_findings[2]],
+        [security_finding_1, report_findings[0]],
+        [security_finding_2, report_findings[1]]
       ]
     end
 
     before do
       create(:security_finding, scan: security_scan, deduplicated: false)
 
-      security_findings.each { |security_finding| report_findings << create(:ci_reports_security_finding, uuid: security_finding.uuid) }
+      report_findings << create(:ci_reports_security_finding, uuid: security_finding_1.overridden_uuid)
+      report_findings << create(:ci_reports_security_finding, uuid: security_finding_2.uuid)
+      report_findings << create(:ci_reports_security_finding, uuid: security_finding_3.overridden_uuid)
 
       allow(security_scan).to receive(:report_findings).and_return(report_findings)
       allow(finding_maps).to receive(:concat).and_call_original
@@ -35,7 +40,7 @@ RSpec.describe Security::Ingestion::FindingMapCollection do
         run_each_slice
 
         expect(finding_maps).to have_received(:concat).exactly(3).times
-        expect(finding_pairs).to match_array(expected_finding_pairs)
+        expect(finding_pairs).to eq(expected_finding_pairs)
       end
     end
   end
