aa9612f1bb8ea820a7490e80290ce2bc77a05200
Pedro Pombeiro <noreply@pedro.pombei.ro>
2022-02-02 17:06:12 +0100

    Log CI runner unregistration audit events
    Changelog: added
    EE: true

---
+++ ee/app/services/ee/ci/unregister_runner_service.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+module EE
+  module Ci
+    # Unregisters a CI Runner and logs an audit event
+    #
+    module UnregisterRunnerService
+      extend ::Gitlab::Utils::Override
+      include ::Audit::Changes
+
+      override :execute
+      def execute
+        scopes = runner_scopes # Save the scopes before destroying the record
+
+        result = super
+
+        audit_log_event(scopes)
+
+        result
+      end
+
+      private
+
+      def runner_scopes
+        case runner.runner_type
+        when 'instance_type'
+          [nil]
+        when 'group_type'
+          runner.groups.to_a
+        when 'project_type'
+          runner.projects.to_a
+        end
+      end
+
+      def audit_log_event(scopes)
+        scopes.each do |scope|
+          ::AuditEvents::UnregisterRunnerAuditEventService.new(runner, author, scope)
+            .track_event
+        end
+      end
+    end
+  end
+end
---
+++ ee/spec/services/ci/unregister_runner_service_spec.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe ::Ci::UnregisterRunnerService, '#execute' do
+  let(:audit_service) { instance_double(::AuditEvents::UnregisterRunnerAuditEventService) }
+  let(:current_user) { nil }
+  let(:token) { 'abc123' }
+
+  subject { described_class.new(runner, current_user || token).execute }
+
+  context 'on an instance runner' do
+    let(:runner) { create(:ci_runner) }
+
+    it 'logs an audit event with the instance scope' do
+      expect(audit_service).to receive(:track_event).once.and_return('track_event_return_value')
+      expect(::AuditEvents::UnregisterRunnerAuditEventService).to receive(:new)
+        .with(runner, token, nil)
+        .once.and_return(audit_service)
+
+      subject
+    end
+  end
+
+  context 'on a group runner' do
+    let(:group) { create(:group) }
+    let(:runner) { create(:ci_runner, :group, groups: [group]) }
+    let(:current_user) { build(:user) }
+
+    it 'logs an audit event with the group scope' do
+      expect(audit_service).to receive(:track_event).once.and_return('track_event_return_value')
+      expect(::AuditEvents::UnregisterRunnerAuditEventService).to receive(:new)
+        .with(runner, current_user, group)
+        .once.and_return(audit_service)
+
+      subject
+    end
+  end
+
+  context 'on a project runner' do
+    let(:project1) { create(:project) }
+    let(:project2) { create(:project) }
+    let(:runner) { create(:ci_runner, :project, projects: [project1, project2]) }
+
+    it 'logs an audit event per project' do
+      expect(audit_service).to receive(:track_event).twice.and_return('track_event_return_value')
+      expect(::AuditEvents::UnregisterRunnerAuditEventService).to receive(:new)
+        .with(runner, token, project1)
+        .once.and_return(audit_service)
+      expect(::AuditEvents::UnregisterRunnerAuditEventService).to receive(:new)
+        .with(runner, token, project2)
+        .once.and_return(audit_service)
+
+      subject
+    end
+  end
+end
