34bea73fad73634d477f4e8fc3a17feecae28dd0
Corinna Wiesner <cwiesner@gitlab.com>
2022-02-03 10:33:41 +0100

    Convert timestamps to utc in license usage export
    Prior to this change, the timestamps included in the license usage file
    that can be exported on the subscription page did not include any
    timezone information. This change will ensure that the included
    timestamps are converted to utc.
    
    Changelog: changed
    MR: https://gitlab.com/gitlab-org/gitlab/-/merge_requests/79832
    EE: true

--- ee/app/services/historical_user_data/csv_service.rb
+++ ee/app/services/historical_user_data/csv_service.rb
@@ -22,7 +22,7 @@ module HistoricalUserData
 
     def header_to_value_hash
       {
-        'Date' => -> (historical_datum) { historical_datum.recorded_at.to_s(:csv) },
+        'Date' => -> (historical_datum) { historical_datum.recorded_at.utc.to_s(:csv) },
         'Active User Count' => 'active_user_count'
       }
     end
@@ -38,7 +38,7 @@ module HistoricalUserData
         csv << ['License Start Date', license.starts_at&.to_s(:csv)]
         csv << ['License End Date', license.expires_at&.to_s(:csv)]
         csv << ['Company', license.licensee_company]
-        csv << ['Generated At', Time.current.to_s(:csv)]
+        csv << ['Generated At', Time.current.utc.to_s(:csv)]
         csv << ['', '']
       end
     end
