335317a0a024264e182bae7324fdacdf4c0262b6
Matthias Käppler <mkaeppler@gitlab.com>
2022-02-03 16:17:35 +0000

    Record and publish application boot time
    This change captures and emits the time it takes
    for the primary Rails process to start up.
    
    - Log it to application JSON log
    - Emit it to prometheus
    - Print it to the console message
    
    This is only active for Puma, Sidekiq and Rails console.
    
    Changelog: added

---
+++ ee/spec/lib/gitlab/console_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'fast_spec_helper'
+
+RSpec.describe Gitlab::Console do
+  describe '.welcome!' do
+    context 'when running in the Rails console' do
+      before do
+        allow(Gitlab::Runtime).to receive(:console?).and_return(true)
+        allow(Gitlab::Geo).to receive(:enabled?).and_return(true)
+        allow(Gitlab::Metrics::BootTimeTracker.instance).to receive(:startup_time).and_return(42)
+      end
+
+      it 'prints a welcome message' do
+        expect($stdout).to receive(:puts).ordered.with(include("--"))
+        expect($stdout).to receive(:puts).ordered.with(include("Ruby:"))
+        expect($stdout).to receive(:puts).ordered.with(include("GitLab:"))
+        expect($stdout).to receive(:puts).ordered.with(include("GitLab Shell:"))
+        expect($stdout).to receive(:puts).ordered.with(include("PostgreSQL:"))
+        expect($stdout).to receive(:puts).ordered.with(include("Geo enabled:"))
+        expect($stdout).to receive(:puts).ordered.with(include("Geo server:"))
+        expect($stdout).to receive(:puts).ordered.with(include("--"))
+        expect($stdout).not_to receive(:puts).ordered
+
+        described_class.welcome!
+      end
+    end
+  end
+end
