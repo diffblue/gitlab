4daa5ea85c2f43911b5471a68335d106b55730e0
Heinrich Lee Yu <heinrich@gitlab.com>
2022-02-04 15:48:15 +0800

    Fix label links in MR system notes
    This changes label change system notes in MRs to link to the merge
    request list instead of the issue list.
    
    Changelog: fixed

--- ee/app/models/ee/resource_label_event.rb
+++ ee/app/models/ee/resource_label_event.rb
@@ -15,19 +15,23 @@ module EE
       end
     end
 
-    override :discussion_id_key
-    def discussion_id_key
-      (super + [label&.scoped_label?]).compact # label can be nil as we don't remove resource events when deleting labels
-    end
-
     override :issuable
     def issuable
       epic || super
     end
 
-    override :banzai_render_context
-    def banzai_render_context(field)
-      epic ? super.merge(label_url_method: :group_epics_url) : super
+    private
+
+    override :label_url_method
+    def label_url_method
+      return :group_epics_url if issuable.is_a?(Epic)
+
+      super
+    end
+
+    override :discussion_id_key
+    def discussion_id_key
+      (super + [label&.scoped_label?]).compact # label can be nil as we don't remove resource events when deleting labels
     end
   end
 end
