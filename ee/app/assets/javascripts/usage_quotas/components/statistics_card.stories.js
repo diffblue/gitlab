457f0ebc7b539c4021697c336f9d5209553f1ad3
Ammar Alakkad <am.alakkad@gmail.com>
2022-02-09 09:06:03 +0300

    Add statistics card component with tests
    Changelog: other
    EE: true

---
+++ ee/app/assets/javascripts/usage_quotas/components/statistics_card.stories.js
@@ -0,0 +1,47 @@
+import { GlCard } from '@gitlab/ui';
+import StatisticsCard from './statistics_card.vue';
+
+export default {
+  component: StatisticsCard,
+  title: 'usage_quotas/components/statistics_card',
+};
+
+const Template = (_, { argTypes }) => ({
+  components: { StatisticsCard, GlCard },
+  props: Object.keys(argTypes),
+  template: `<gl-card class="gl-w-half">
+      <statistics-card v-bind="$props">
+      </statistics-card>
+     </gl-card>`,
+});
+export const Default = Template.bind({});
+
+/* eslint-disable @gitlab/require-i18n-strings */
+Default.args = {
+  usageValue: '1,400',
+  totalValue: '1,500',
+  description: 'Additional minutes used',
+  helpLink: 'dummy.com/link',
+  percentage: 84,
+};
+
+export const WithUnits = (_, { argTypes }) => ({
+  components: { StatisticsCard, GlCard },
+  props: Object.keys(argTypes),
+  template: `<gl-card class="gl-w-half">
+      <statistics-card v-bind="$props">
+      </statistics-card>
+     </gl-card>`,
+});
+
+WithUnits.args = {
+  usageValue: '250.0',
+  usageUnit: 'MiB',
+  totalValue: '15.0',
+  totalUnit: 'GiB',
+  description: 'Storage used',
+  helpLink: 'dummy.com/link',
+  percentage: 4,
+  purchaseButtonLink: 'purchase.com/test',
+  purchaseButtonText: 'Purchase storage',
+};
