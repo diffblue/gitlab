1c457ae5f9e91cd7ee289279e996bda142b21625
Jannik Lehmann <jlehmann@gitlab.com>
2022-02-15 23:59:29 +0000

    Refactor security configuration page data accumulation
    This commit refactors the security configuration
    data accumulation to be consistent across CE & EE.

--- ee/app/assets/javascripts/pages/projects/security/configuration/index.js
+++
@@ -1,7 +0,0 @@
-import { initSecurityConfiguration } from '~/security_configuration';
-
-const el =
-  document.querySelector('#js-security-configuration') ||
-  document.querySelector('#js-security-configuration-static');
-
-initSecurityConfiguration(el);
