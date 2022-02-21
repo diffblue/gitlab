57fba881483e890e2a568cd22510a5e73231d8e7
Alex Kalderimis <akalderimis@gitlab.com>
2022-02-21 13:30:59 +0000

    Add test for post-migration scheduling

--- db/post_migrate/20220204194347_encrypt_integration_properties.rb
+++ db/post_migrate/20220204194347_encrypt_integration_properties.rb
@@ -3,13 +3,18 @@
 class EncryptIntegrationProperties < Gitlab::Database::Migration[1.0]
   disable_ddl_transaction!
   MIGRATION = 'EncryptIntegrationProperties'
+  BATCH_SIZE = 5_000
+  INTERVAL = 2.minutes.to_i
 
   def up
+    requiring_update = define_batchable_model('integrations').where.not(properties: nil)
+
     queue_background_migration_jobs_by_range_at_intervals(
-      define_batchable_model('integrations').where.not(properties: nil),
+      requiring_update,
       MIGRATION,
-      2.minutes.to_i,
-      track_jobs: true
+      INTERVAL,
+      track_jobs: true,
+      batch_size: BATCH_SIZE
     )
   end
 
