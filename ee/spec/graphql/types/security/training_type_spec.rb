a9fcbb72f1449357a5692b04818b8b983ce287e4
Mehmet Emin INAC <minac@gitlab.com>
2022-02-21 17:09:32 +0100

    Fix response of `securityTrainingUpdate` mutation
    Changelog: fixed
    EE: true

--- ee/spec/graphql/types/security/training_type_spec.rb
+++ ee/spec/graphql/types/security/training_type_spec.rb
@@ -6,4 +6,33 @@ RSpec.describe GitlabSchema.types['ProjectSecurityTraining'] do
   let(:fields) { %i[id name description url logo_url is_enabled is_primary] }
 
   it { expect(described_class).to have_graphql_fields(fields) }
+
+  describe '#is_primary' do
+    let(:training) { create(:security_training, :primary) }
+    let(:query) { double('query', schema: GitlabSchema, with_error_handling: true) }
+    let(:query_context) { GraphQL::Query::Context.new(query: query, values: {}, object: nil) }
+    let(:type_instance) { described_class.authorized_new(training, query_context) }
+
+    subject { type_instance.is_primary }
+
+    context 'when the object is destroyed' do
+      before do
+        training.destroy!
+      end
+
+      it { is_expected.to be(false) }
+    end
+
+    context 'when the object is not destroyed' do
+      context 'when the object is not primary' do
+        let(:training) { create(:security_training) }
+
+        it { is_expected.to be(false) }
+      end
+
+      context 'when the object is primary' do
+        it { is_expected.to be(true) }
+      end
+    end
+  end
 end
