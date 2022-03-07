c4dab676900eccc30c466c45dea6d154ff91a93c
Felipe Artur <felipefac@gmail.com>
2022-03-07 11:53:37 -0300

    Allow to destroy related epics association on internal API
    Allow to destroy related epic links on internal API
    
    Changelog: added
    EE: true

--- ee/spec/requests/groups/epics/related_epic_links_controller_spec.rb
+++ ee/spec/requests/groups/epics/related_epic_links_controller_spec.rb
@@ -5,11 +5,38 @@ require 'spec_helper'
 RSpec.describe Groups::Epics::RelatedEpicLinksController do
   let_it_be(:user) { create(:user) }
   let_it_be(:epic) { create(:epic) }
-  let_it_be(:epic_link1) { create(:related_epic_link, source: epic) }
+  let_it_be(:epic2) { create(:epic, group: epic.group) }
+  let_it_be(:epic_link1) { create(:related_epic_link, source: epic, target: epic2) }
   let_it_be(:epic_link2) { create(:related_epic_link, source: epic) }
 
   before do
     stub_licensed_features(epics: true, related_epics: true)
+  end
+
+  shared_examples 'a not available action' do
+    context 'when related_epics flag is disabled' do
+      before do
+        stub_feature_flags(related_epics_widget: false)
+      end
+
+      it 'returns not_found error' do
+        request
+
+        expect(response).to have_gitlab_http_status(:not_found)
+      end
+    end
+
+    context 'when related_epics are not available' do
+      before do
+        stub_licensed_features(epics: true, related_epics: false)
+      end
+
+      it 'returns not_found error' do
+        request
+
+        expect(response).to have_gitlab_http_status(:not_found)
+      end
+    end
   end
 
   describe 'GET /*group_id/:group_id/epics/:epic_id/related_epic_links' do
@@ -21,6 +48,8 @@ RSpec.describe Groups::Epics::RelatedEpicLinksController do
       epic.group.add_guest(user)
       login_as user
     end
+
+    it_behaves_like 'a not available action'
 
     it 'returns JSON response' do
       list_service_response = Epics::RelatedEpicLinks::ListService.new(epic, user).execute
@@ -44,27 +73,42 @@ RSpec.describe Groups::Epics::RelatedEpicLinksController do
 
       expect { do_request }.not_to exceed_query_limit(control)
     end
+  end
 
-    context 'when related_epics flag is disabled' do
-      before do
-        stub_feature_flags(related_epics_widget: false)
-      end
+  describe 'DELETE /*group_id/:group_id/epics/:epic_id/related_epic_links/:link_id' do
+    subject(:request) do
+      delete group_epic_related_epic_link_path(id: epic_link1.id, group_id: epic.group, epic_id: epic.iid, format: :json)
+    end
 
-      it 'returns not_found error' do
-        request
+    before do
+      epic.group.add_reporter(user)
+      login_as user
+    end
+
+    it_behaves_like 'a not available action'
+
+    it 'deletes related epic link' do
+      expect { request }.to change(Epic::RelatedEpicLink, :count).by(-1)
+      expect(response).to have_gitlab_http_status(:ok)
+    end
+
+    context 'when related epic link id is not valid' do
+      it 'returns 404' do
+        delete group_epic_related_epic_link_path(id: 999, group_id: epic.group, epic_id: epic.iid, format: :json)
 
         expect(response).to have_gitlab_http_status(:not_found)
       end
     end
 
-    context 'when related_epics are not available' do
-      before do
-        stub_licensed_features(epics: true, related_epics: false)
+    context 'when related epic link does not belong to epic' do
+      let!(:link) { create(:related_epic_link) }
+
+      subject(:request) do
+        delete group_epic_related_epic_link_path(id: link.id, group_id: epic.group, epic_id: epic.iid, format: :json)
       end
 
-      it 'returns not_found error' do
-        request
-
+      it 'does not delete related epic link' do
+        expect { request }.not_to change(Epic::RelatedEpicLink, :count)
         expect(response).to have_gitlab_http_status(:not_found)
       end
     end
