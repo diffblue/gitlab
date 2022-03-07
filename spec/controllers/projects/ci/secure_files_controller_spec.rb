406444b425a8dcdf85b8e589e296199a0fdb950b
Darby Frey <dfrey@gitlab.com>
2022-03-07 10:19:01 -0600

    Updated secure_files_controller specs

--- spec/controllers/projects/ci/secure_files_controller_spec.rb
+++ spec/controllers/projects/ci/secure_files_controller_spec.rb
@@ -3,18 +3,15 @@
 require 'spec_helper'
 
 RSpec.describe Projects::Ci::SecureFilesController do
-  let_it_be(:project) { create(:project, :repository) }
+  let_it_be(:project) { create(:project) }
   let_it_be(:user) { create(:user) }
 
   subject(:show_request) { get :show, params: { namespace_id: project.namespace, project_id: project } }
 
-  before do
-    sign_in(user)
-  end
-
   describe 'GET #show' do
     context 'with enough privileges' do
       before do
+        sign_in(user)
         project.add_developer(user)
         show_request
       end
@@ -28,12 +25,24 @@ RSpec.describe Projects::Ci::SecureFilesController do
 
     context 'without enough privileges' do
       before do
+        sign_in(user)
         project.add_reporter(user)
         show_request
       end
 
       it 'responds with 404' do
         expect(response).to have_gitlab_http_status(:not_found)
+      end
+    end
+
+    context 'an unauthenticated user' do
+      before do
+        show_request
+      end
+
+      it 'redirects to sign in' do
+        expect(response).to have_gitlab_http_status(:found)
+        expect(response).to redirect_to('/users/sign_in')
       end
     end
   end
