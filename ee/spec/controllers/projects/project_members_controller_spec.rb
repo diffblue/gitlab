0f375f698d5ae1db0c44219d376e727ba15f523f
Doug Stull <dstull@gitlab.com>
2022-03-08 09:57:27 -0500

    Remove member create controller code
    - no longer needed since we now create members via API only

--- ee/spec/controllers/projects/project_members_controller_spec.rb
+++ ee/spec/controllers/projects/project_members_controller_spec.rb
@@ -44,37 +44,6 @@ RSpec.describe Projects::ProjectMembersController do
     end
   end
 
-  describe 'POST create' do
-    let(:stranger) { create(:user) }
-    subject(:create_member) do
-      post :create, params: {
-        user_ids: stranger.id,
-        namespace_id: project.namespace,
-        access_level: access_level,
-        project_id: project
-      }
-    end
-
-    let(:access_level) { nil }
-
-    before do
-      project.add_maintainer(user)
-      sign_in(user)
-    end
-
-    context 'when project group has membership lock enabled' do
-      before do
-        project.namespace.update!(membership_lock: true)
-      end
-
-      it 'responds with 403' do
-        create_member
-
-        expect(response).to have_gitlab_http_status(:forbidden)
-      end
-    end
-  end
-
   describe 'GET import' do
     subject(:import) do
       get :import, params: {
