bde7a342ac79b1c9d0490677cbad7996a8930f42
mlunoe <michael.lunoe@gmail.com>
2022-03-09 16:04:15 +0100

    Refactor(Groups SSO): migrate to gl-tabs
    Migrate Bootstrap tabs to Pajamas the compatible
    GlTabs
    
    https://gitlab.com/groups/gitlab-org/-/epics/3983
    
    Changelog: changed
    EE: true

--- ee/app/assets/javascripts/pages/groups/sso/index.js
+++ ee/app/assets/javascripts/pages/groups/sso/index.js
@@ -1,7 +1,9 @@
 import { redirectUserWithSSOIdentity } from 'ee/saml_sso';
+import { GlTabsBehavior } from '~/tabs';
 import UsernameValidator from '~/pages/sessions/new/username_validator';
 import initConfirmDanger from '~/init_confirm_danger';
 
+new GlTabsBehavior(document.querySelector('.new-session-tabs')); // eslint-disable-line no-new
 new UsernameValidator(); // eslint-disable-line no-new
 redirectUserWithSSOIdentity();
 initConfirmDanger();
