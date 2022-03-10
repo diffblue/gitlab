e56c04cb5db42f4b43f716b91ff71192308f5b75
Alishan Ladhani <aladhani@gitlab.com>
2022-03-10 11:58:24 -0500

    Update job page for blocked deployments
    Link to environments page where a user can approve/reject
    the deployment.
    
    Changelog: changed
    EE: true

---
+++ ee/spec/features/projects/jobs/blocked_deployment_job_page_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe 'Blocked deployment job page', :js do
+  let(:project) { create(:project, :repository) }
+  let(:user) { create(:user) }
+  let(:build) { create(:ci_build, :manual, environment: 'production', project: project) }
+
+  before do
+    environment = create(:environment, name: 'production', project: project)
+    create(:protected_environment, project: project, name: 'production', required_approval_count: 1)
+    create(:deployment, :blocked, project: project, environment: environment, deployable: build)
+
+    project.add_developer(user)
+    sign_in(user)
+
+    visit(project_job_path(project, build))
+  end
+
+  it 'displays a button linking to the environments page' do
+    expect(page).to have_text('Waiting for approval')
+    expect(page).to have_link('Go to environments page to approve or reject', href: project_environments_path(project))
+
+    find("[data-testid='job-empty-state-action']").click
+
+    expect(page).to have_current_path(project_environments_path(project))
+  end
+end
