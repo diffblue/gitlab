004f78ac56a0f3a4303f6b93fc44059902c5343a
Brett Walker <bwalker@gitlab.com>
2022-03-10 19:04:13 +0000

    Set resoure_iteration_events to ghost user
    when the user is deleted
    
    Changelog: fixed
    EE: true

--- ee/app/services/ee/users/migrate_to_ghost_user_service.rb
+++ ee/app/services/ee/users/migrate_to_ghost_user_service.rb
@@ -3,9 +3,16 @@
 module EE
   module Users
     module MigrateToGhostUserService
+      BATCH_SIZE = 1000
+
       private
 
       def migrate_records
+        # these should always be ghosted
+        migrate_resource_iteration_events
+
+        return super if hard_delete
+
         migrate_epics
         migrate_requirements_management_requirements
         migrate_vulnerabilities_feedback
@@ -26,6 +33,12 @@ module EE
       def migrate_vulnerabilities_feedback
         user.vulnerability_feedback.update_all(author_id: ghost_user.id)
         user.commented_vulnerability_feedback.update_all(comment_author_id: ghost_user.id)
+      end
+
+      def migrate_resource_iteration_events
+        ResourceIterationEvent.by_user(user).each_batch(of: BATCH_SIZE) do |batch|
+          batch.update_all(user_id: ghost_user.id)
+        end
       end
     end
   end
--- ee/spec/services/ee/users/migrate_to_ghost_user_service_spec.rb
+++ ee/spec/services/ee/users/migrate_to_ghost_user_service_spec.rb
@@ -3,10 +3,11 @@
 require 'spec_helper'
 
 RSpec.describe Users::MigrateToGhostUserService do
-  context 'epics' do
-    let!(:user)   { create(:user) }
-    let(:service) { described_class.new(user) }
+  let!(:user)   { create(:user) }
+  let(:service) { described_class.new(user) }
+  let(:always_ghost) { false }
 
+  context 'epics' do
     context 'deleted user is present as both author and edited_user' do
       include_examples "migrating a deleted user's associated records to the ghost user", Epic, [:author, :last_edited_by] do
         let(:created_record) do
@@ -23,29 +24,28 @@ RSpec.describe Users::MigrateToGhostUserService do
   end
 
   context 'vulnerability_feedback author' do
-    let!(:user)   { create(:user) }
-    let(:service) { described_class.new(user) }
-
     include_examples "migrating a deleted user's associated records to the ghost user", Vulnerabilities::Feedback, [:author] do
       let(:created_record) { create(:vulnerability_feedback, author: user) }
     end
   end
 
   context 'vulnerability_feedback comment author' do
-    let!(:user)   { create(:user) }
-    let(:service) { described_class.new(user) }
-
     include_examples "migrating a deleted user's associated records to the ghost user", Vulnerabilities::Feedback, [:comment_author] do
       let(:created_record) { create(:vulnerability_feedback, comment_author: user) }
     end
   end
 
   context 'requirements' do
-    let!(:user)   { create(:user) }
-    let(:service) { described_class.new(user) }
-
     include_examples "migrating a deleted user's associated records to the ghost user", RequirementsManagement::Requirement, [:author] do
       let(:created_record) { create(:requirement, author: user) }
+    end
+  end
+
+  context 'resource_iteration_events' do
+    let(:always_ghost) { true }
+
+    include_examples "migrating a deleted user's associated records to the ghost user", ResourceIterationEvent, [:user] do
+      let(:created_record) { create(:resource_iteration_event, issue: create(:issue), user: user, iteration: create(:iteration)) }
     end
   end
 end
