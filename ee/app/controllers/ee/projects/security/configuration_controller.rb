b504880a7f839d48025938fe632269a1be926cc8
Aditya Tiwari <atiwari@gitlab.com>
2022-03-11 12:55:56 +0000

    Remove corpus management feature flags
    Remove :corpus_management and :corpus_management_ui feature flags.
    
    Changelog: other
    EE: true

--- ee/app/controllers/ee/projects/security/configuration_controller.rb
+++ ee/app/controllers/ee/projects/security/configuration_controller.rb
@@ -14,7 +14,6 @@ module EE
 
           before_action only: [:show] do
             push_frontend_feature_flag(:security_auto_fix, project, default_enabled: false)
-            push_frontend_feature_flag(:corpus_management_ui, project, default_enabled: :yaml)
             push_frontend_feature_flag(:secure_vulnerability_training, project, default_enabled: :yaml)
           end
 
