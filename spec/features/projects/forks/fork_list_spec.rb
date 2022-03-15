5f4c1308b409e4218cb5661f298ff06b95cc6b67
Gary Holtz <gholtz@gitlab.com>
2022-03-15 15:49:28 -0500

    Moving spec to a view spec for cleaner code

--- spec/features/projects/forks/fork_list_spec.rb
+++ spec/features/projects/forks/fork_list_spec.rb
@@ -24,28 +24,6 @@ RSpec.describe 'listing forks of a project' do
     end
   end
 
-  context "when a fork is set to allow only project members to access features" do
-    let(:outside_user) { create(:user) }
-
-    before do
-      sign_in(outside_user)
-
-      allow_any_instance_of(ProjectsHelper).to receive(:able_to_see_last_commit?).and_return(false)
-      allow_any_instance_of(ProjectsHelper).to receive(:able_to_see_merge_requests?).and_return(false)
-      allow_any_instance_of(ProjectsHelper).to receive(:able_to_see_issues?).and_return(false)
-    end
-
-    it 'will not show that information in the original forks list' do
-      visit project_forks_path(source)
-
-      page.within('li.project-row') do
-        expect(page).not_to have_css('a.commit-row-message')
-        expect(page).not_to have_css('a.issues')
-        expect(page).not_to have_css('a.merge-requests')
-      end
-    end
-  end
-
   it 'does not show the commit message when an external authorization service is used' do
     enable_external_authorization_service_check
 
