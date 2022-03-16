06ba9523d947fea4155c81194173190ded6a7163
Vijay Hawoldar <vhawoldar@gitlab.com>
2022-03-16 15:31:31 +0000

    Handle auth error for container registry
    When a root namespace cannot have container registry images pushed, we
    should return an error. Our only opportunity is during authentication,
    so we handle it there and return an appropriate message
    
    EE: true
    Changelog: added

--- ee/app/services/ee/auth/container_registry_authentication_service.rb
+++ ee/app/services/ee/auth/container_registry_authentication_service.rb
@@ -5,6 +5,19 @@ module EE
     module ContainerRegistryAuthenticationService
       extend ::Gitlab::Utils::Override
 
+      StorageError = Class.new(StandardError)
+
+      override :execute
+      def execute(authentication_abilities:)
+        super
+      rescue StorageError
+        error(
+          'DENIED',
+          status: 403,
+          message: 'You are above your storage quota! Visit https://docs.gitlab.com/ee/user/usage_quotas.html to learn more.'
+        )
+      end
+
       private
 
       override :can_access?
@@ -13,6 +26,8 @@ module EE
           @access_denied_in_maintenance_mode = true # rubocop:disable Gitlab/ModuleWithInstanceVariables
           return false
         end
+
+        raise StorageError if storage_error?(requested_project, requested_action)
 
         super
       end
@@ -29,6 +44,13 @@ module EE
 
       def access_denied_in_maintenance_mode?
         @access_denied_in_maintenance_mode
+      end
+
+      def storage_error?(project, action)
+        return false unless project
+        return false unless action == 'push'
+
+        project.root_ancestor.over_storage_limit?
       end
     end
   end
--- ee/spec/services/ee/auth/container_registry_authentication_service_spec.rb
+++ ee/spec/services/ee/auth/container_registry_authentication_service_spec.rb
@@ -53,4 +53,66 @@ RSpec.describe Auth::ContainerRegistryAuthenticationService do
   context 'when not in maintenance mode' do
     it_behaves_like 'a container registry auth service'
   end
+
+  context 'when over storage limit' do
+    include_context 'container registry auth service context'
+
+    let_it_be(:current_user) { create(:user) }
+    let_it_be(:namespace) { create(:group) }
+
+    before do
+      allow_next_found_instance_of(Project) do |instance|
+        allow(instance).to receive(:root_ancestor).and_return namespace
+      end
+
+      allow(namespace).to receive(:over_storage_limit?).and_return true
+    end
+
+    context 'when there is a project' do
+      let_it_be(:project) { create(:project, namespace: namespace) }
+
+      before do
+        project.add_developer(current_user)
+      end
+
+      context 'does not allow developer to push images' do
+        let(:current_params) do
+          { scopes: ["repository:#{project.full_path}:push"] }
+        end
+
+        it_behaves_like 'not a container repository factory' do
+          it 'returns an appropriate response' do
+            expect(subject[:errors].first).to include(
+              code: 'DENIED',
+              message: 'You are above your storage quota! Visit https://docs.gitlab.com/ee/user/usage_quotas.html to learn more.'
+            )
+          end
+        end
+      end
+
+      context 'allows developers to pull images' do
+        let(:current_params) do
+          { scopes: ["repository:#{project.full_path}:pull"] }
+        end
+
+        it_behaves_like 'a pullable'
+      end
+
+      context 'allows maintainers to delete images' do
+        before do
+          project.add_maintainer(current_user)
+        end
+
+        it_behaves_like 'allowed to delete container repository images'
+      end
+    end
+
+    context 'when there is no project' do
+      let(:project) { nil }
+
+      it 'does not return a storage error' do
+        expect(subject[:errors]).to be_nil
+      end
+    end
+  end
 end
