0ecf9ab89104f45c3902fcd385d34fa3a6801586
Gary Holtz <gholtz@gitlab.com>
2022-03-18 13:34:35 -0500

    Adjusting some specs based on review

--- lib/gitlab/git/diff.rb
+++ lib/gitlab/git/diff.rb
@@ -158,9 +158,7 @@ module Gitlab
           raise "Invalid raw diff type: #{raw_diff.class}"
         end
 
-        if Feature.enabled?(:convert_diff_to_utf8_with_replacement_symbol, default_enabled: :yaml)
-          @diff = Gitlab::EncodingHelper.encode_utf8_with_replacement_character(@diff) if replace_invalid_utf8_chars && !detect_binary?(@diff)
-        end
+        encode_diff_to_utf8(replace_invalid_utf8_chars)
       end
 
       def to_hash
@@ -230,6 +228,13 @@ module Gitlab
       end
 
       private
+
+      def encode_diff_to_utf8(replace_invalid_utf8_chars)
+        return unless Feature.enabled?(:convert_diff_to_utf8_with_replacement_symbol, default_enabled: :yaml)
+        return unless replace_invalid_utf8_chars && !detect_binary?(@diff)
+
+        @diff = Gitlab::EncodingHelper.encode_utf8_with_replacement_character(@diff)
+      end
 
       def init_from_hash(hash)
         raw_diff = hash.symbolize_keys
