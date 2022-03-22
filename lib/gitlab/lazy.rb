182e996b85945ba14407ad9d0ba5c360e7b1ddc3
Roy Zwambag <rzwambag@gitlab.com>
2022-03-22 12:09:52 +0100

    Add ... to Gitlab::Lazy

--- lib/gitlab/lazy.rb
+++ lib/gitlab/lazy.rb
@@ -15,10 +15,10 @@ module Gitlab
       @block = block
     end
 
-    def method_missing(name, *args, **kwargs, &block)
+    def method_missing(...)
       __evaluate__
 
-      @result.__send__(name, *args, **kwargs, &block) # rubocop:disable GitlabSecurity/PublicSend
+      @result.__send__(...) # rubocop:disable GitlabSecurity/PublicSend
     end
 
     def respond_to_missing?(name, include_private = false)
