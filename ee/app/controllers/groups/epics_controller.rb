ba2094afa6f7021f0cdbdfbf518ed472e8c05391
Phil Hughes <me@iamphill.com>
2022-03-23 09:46:05 +0000

    Remove improved_emoji_picker feature flag
    Removes the improved emoji picker feature flag which makes
    the improved emoji picker available in issuables.
    
    Changelog: added

--- ee/app/controllers/groups/epics_controller.rb
+++ ee/app/controllers/groups/epics_controller.rb
@@ -17,7 +17,6 @@ class Groups::EpicsController < Groups::ApplicationController
   after_action :log_epic_show, only: :show
 
   before_action do
-    push_frontend_feature_flag(:improved_emoji_picker, @group, type: :development, default_enabled: :yaml)
     push_frontend_feature_flag(:related_epics_widget, @group, type: :development, default_enabled: :yaml)
   end
 
--- lib/gitlab/gon_helper.rb
+++ lib/gitlab/gon_helper.rb
@@ -53,7 +53,6 @@ module Gitlab
       # made globally available to the frontend
       push_frontend_feature_flag(:usage_data_api, type: :ops, default_enabled: :yaml)
       push_frontend_feature_flag(:security_auto_fix, default_enabled: false)
-      push_frontend_feature_flag(:improved_emoji_picker, default_enabled: :yaml)
       push_frontend_feature_flag(:new_header_search, default_enabled: :yaml)
       push_frontend_feature_flag(:bootstrap_confirmation_modals, default_enabled: :yaml)
       push_frontend_feature_flag(:sandboxed_mermaid, default_enabled: :yaml)
