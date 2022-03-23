3ba6b57ce5652ef98515f7e2e9e4515d949d44d7
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2022-03-23 14:47:34 -0400

    Prettify setupArkoseLabs script

--- ee/app/assets/javascripts/arkose_labs/index.js
+++ ee/app/assets/javascripts/arkose_labs/index.js
@@ -25,7 +25,7 @@ export const setupArkoseLabs = () => {
           usernameSelector: USERNAME_SELECTOR,
           submitSelector: SUBMIT_SELECTOR,
         },
-      })
-    }
+      });
+    },
   });
 };
