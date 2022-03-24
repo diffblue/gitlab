a510f49ac47a48b41fbc738d17aa67fb13691a6a
Artur Fedorov <afedorov@gitlab.com>
2022-03-24 18:42:20 +0100

    This MR adds info badge to DAST configuration card
    New component, feature-card-badge, is displayed on DAST
    configuration card. It is shown based on feature configuration.
    New property "badge" can be added to
    a specific feature via backend or frontend.
    Layout of the card is adjusted accordingly
    based on configuration. Specs are adjusted accordingly.
    feature-card-badge has internal logic for conditional
    tooltip rendering.
    
    Changelog: changed
    MR: https://gitlab.com/gitlab-org/gitlab/-/merge_requests/83723
    EE: true

--- ee/spec/presenters/ee/projects/security/configuration_presenter_spec.rb
+++ ee/spec/presenters/ee/projects/security/configuration_presenter_spec.rb
@@ -22,4 +22,29 @@ RSpec.describe Projects::Security::ConfigurationPresenter do
       expect(result[:can_toggle_auto_fix_settings]).to be_truthy
     end
   end
+
+  describe '#to_html_data_attribute' do
+    subject(:result) { described_class.new(project, auto_fix_permission: true, current_user: current_user).to_h }
+
+    before do
+      stub_licensed_features(security_on_demand_scans: true, security_configuration_in_ui: true)
+    end
+
+    let(:meta_info_path) { "/#{project.namespace.path}/#{project.name}/-/on_demand_scans" }
+    let(:features) { result[:features] }
+
+    it 'includes feature meta information for dast scanner' do
+      feature = features.find { |scan| scan[:type].to_s == 'dast' }
+
+      expect(feature[:type].to_s).to eq('dast')
+      expect(feature[:meta_info_path]).to eq(meta_info_path)
+    end
+
+    it 'does not include feature meta information for other scanner' do
+      feature = features.find { |scan| scan[:type].to_s == 'sast' }
+
+      expect(feature[:type].to_s).to eq('sast')
+      expect(feature[:meta_info_path]).to be_nil
+    end
+  end
 end
