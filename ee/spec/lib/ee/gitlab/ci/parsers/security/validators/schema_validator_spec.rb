c03aa7d1e136fdcf51273070f8eb4370922b8f64
Michał Zając <mzajac@gitlab.com>
2022-03-28 01:20:17 +0200

    Adjust remaining specs

--- ee/spec/lib/ee/gitlab/ci/parsers/security/validators/schema_validator_spec.rb
+++ ee/spec/lib/ee/gitlab/ci/parsers/security/validators/schema_validator_spec.rb
@@ -5,17 +5,17 @@ require 'spec_helper'
 RSpec.describe Gitlab::Ci::Parsers::Security::Validators::SchemaValidator do
   using RSpec::Parameterized::TableSyntax
 
-  where(:report_type, :expected_errors, :valid_data) do
-    :cluster_image_scanning     | ['root is missing required keys: vulnerabilities']                   | { 'version' => '10.0.0', 'vulnerabilities' => [] }
-    :container_scanning         | ['root is missing required keys: vulnerabilities']                   | { 'version' => '10.0.0', 'vulnerabilities' => [] }
-    :coverage_fuzzing           | ['root is missing required keys: vulnerabilities']                   | { 'version' => '10.0.0', 'vulnerabilities' => [] }
-    :dast                       | ['root is missing required keys: vulnerabilities']                   | { 'version' => '10.0.0', 'vulnerabilities' => [] }
-    :dependency_scanning        | ['root is missing required keys: dependency_files, vulnerabilities'] | { 'version' => '10.0.0', 'vulnerabilities' => [], 'dependency_files' => [] }
-    :api_fuzzing                | ['root is missing required keys: vulnerabilities']                   | { 'version' => '10.0.0', 'vulnerabilities' => [] }
+  where(:report_type, :expected_errors, :expected_warnings, :valid_data) do
+    :cluster_image_scanning     | ['root is missing required keys: vulnerabilities']                   | lazy { expected_warnings_array } | { 'version' => '10.0.0', 'vulnerabilities' => [] }
+    :container_scanning         | ['root is missing required keys: vulnerabilities']                   | lazy { expected_warnings_array } | { 'version' => '10.0.0', 'vulnerabilities' => [] }
+    :coverage_fuzzing           | ['root is missing required keys: vulnerabilities']                   | lazy { expected_warnings_array } | { 'version' => '10.0.0', 'vulnerabilities' => [] }
+    :dast                       | ['root is missing required keys: vulnerabilities']                   | lazy { expected_warnings_array } | { 'version' => '10.0.0', 'vulnerabilities' => [] }
+    :dependency_scanning        | ['root is missing required keys: dependency_files, vulnerabilities'] | lazy { expected_warnings_array } | { 'version' => '10.0.0', 'vulnerabilities' => [], 'dependency_files' => [] }
+    :api_fuzzing                | ['root is missing required keys: vulnerabilities']                   | lazy { expected_warnings_array } | { 'version' => '10.0.0', 'vulnerabilities' => [] }
   end
 
   with_them do
-    let(:validator) { described_class.new(report_type, report_data) }
+    let(:validator) { described_class.new(report_type, report_data, valid_data['version']) }
 
     describe '#valid?' do
       subject { validator.valid? }
@@ -30,6 +30,22 @@ RSpec.describe Gitlab::Ci::Parsers::Security::Validators::SchemaValidator do
         let(:report_data) { valid_data }
 
         it { is_expected.to be_truthy }
+      end
+    end
+
+    describe '#warnings' do
+      subject { validator.warnings }
+
+      context 'when given data is valid according to the schema' do
+        let(:report_data) { valid_data }
+        let(:supported_version) { described_class::SUPPORTED_VERSIONS[report_type].join(", ") }
+        let(:expected_warnings_array) do
+          [
+            "Version 10.0.0 for report type #{report_type} has been deprecated, supported versions for this report type are: #{supported_version}"
+          ]
+        end
+
+        it { is_expected.to eq(expected_warnings) }
       end
     end
 
