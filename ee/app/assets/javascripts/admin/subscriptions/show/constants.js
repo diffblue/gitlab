98d725bf2402b95351b5dfce7533ae515949d4c7
Vitaly Slobodin <vslobodin@gitlab.com>
2022-03-28 12:35:45 +0000

    Hide "Users in Subscription" card for Ultimate plans
    Changelog: changed
    EE: true

--- ee/app/assets/javascripts/admin/subscriptions/show/constants.js
+++ ee/app/assets/javascripts/admin/subscriptions/show/constants.js
@@ -38,7 +38,6 @@ export const copySubscriptionIdButtonText = __('Copy');
 export const licenseFileText = __('License file');
 export const onlineCloudLicenseText = s__('SuperSonics|Cloud license');
 export const offlineCloudLicenseText = s__('SuperSonics|Offline cloud');
-export const usersInSubscriptionUnlimited = __('Unlimited');
 export const detailsLabels = {
   address: __('Address'),
   company: __('Company'),
@@ -55,22 +54,7 @@ export const detailsLabels = {
 export const removeLicense = __('Remove license');
 export const removeLicenseConfirm = __('Are you sure you want to remove the license?');
 export const removeLicenseButtonLabel = __('Remove license');
-export const billableUsersTitle = s__('SuperSonics|Billable users');
-export const maximumUsersTitle = s__('SuperSonics|Maximum users');
-export const usersInSubscriptionTitle = s__('SuperSonics|Users in subscription');
-export const usersOverSubscriptionTitle = s__('SuperSonics|Users over subscription');
-export const billableUsersText = s__(
-  'SuperSonics|This is the number of %{billableUsersLinkStart}billable users%{billableUsersLinkEnd} on your installation, and this is the minimum number you need to purchase when you renew your license.',
-);
-export const maximumUsersText = s__(
-  'SuperSonics|This is the highest peak of users on your installation since the license started.',
-);
-export const usersInSubscriptionText = s__(
-  `SuperSonics|Users with a Guest role or those who don't belong to a Project or Group will not use a seat from your license.`,
-);
-export const usersOverSubscriptionText = s__(
-  `SuperSonics|You'll be charged for %{trueUpLinkStart}users over license%{trueUpLinkEnd} on a quarterly or annual basis, depending on the terms of your agreement.`,
-);
+
 export const subscriptionTable = {
   activatedAt: __('Activated on'),
   expiresOn: __('Expires on'),
