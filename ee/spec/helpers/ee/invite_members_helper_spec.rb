801c1f172b6e5e669bd31a565d79ff7eba2d27e0
Serhii Yarynovskyi <syarynovskyi@gitlab.com>
2022-03-30 10:07:44 +0000

    Add user limit notification for invite members modal
    Now when invite members modal appears, customer can see indication
    how many seats are remaining when namespace approaches user limit
    
    Changelog: added

--- ee/spec/helpers/ee/invite_members_helper_spec.rb
+++ ee/spec/helpers/ee/invite_members_helper_spec.rb
@@ -2,7 +2,54 @@
 require 'spec_helper'
 
 RSpec.describe EE::InviteMembersHelper do
-  describe '.users_filter_data' do
+  include Devise::Test::ControllerHelpers
+
+  describe '#common_invite_modal_dataset', :saas do
+    let(:project) { build(:project) }
+
+    let(:notification_attributes) do
+      {
+        free_users_limit: 5,
+        members_count: 0,
+        new_trial_registration_path: '/-/trials/new',
+        purchase_path: "/groups/#{project.root_ancestor.path}/-/billings"
+      }
+    end
+
+    context 'when applying the free user cap is not valid' do
+      let!(:group) do
+        build(:group, projects: [project], gitlab_subscription: build(:gitlab_subscription, :default))
+      end
+
+      it 'does not include users limit notification data' do
+        expect(helper.common_invite_modal_dataset(project)).not_to include(notification_attributes)
+      end
+    end
+
+    context 'when applying the free user cap is valid' do
+      context 'when user namespace' do
+        let!(:user_namespace) do
+          build(:user_namespace, projects: [project], gitlab_subscription: build(:gitlab_subscription, :free))
+        end
+
+        it 'does not include users limit notification data' do
+          expect(helper.common_invite_modal_dataset(project)).not_to include(notification_attributes)
+        end
+      end
+
+      context 'when group namespace' do
+        let!(:group) do
+          build(:group, projects: [project], gitlab_subscription: build(:gitlab_subscription, :free))
+        end
+
+        it 'includes users limit notification data' do
+          expect(helper.common_invite_modal_dataset(project)).to include(notification_attributes)
+        end
+      end
+    end
+  end
+
+  describe '#users_filter_data' do
     let_it_be(:group) { create(:group) }
     let_it_be(:saml_provider) { create(:saml_provider, group: group) }
 
