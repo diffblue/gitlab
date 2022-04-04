d6b6f84df621cc6fe5ce3e369206ed247dca75bb
nmilojevic1 <nmilojevic@gitlab.com>
2022-04-04 17:34:08 +0200

    Provide author as local attribute to the partial

--- app/mailers/emails/reviews.rb
+++ app/mailers/emails/reviews.rb
@@ -20,6 +20,7 @@ module Emails
 
     def setup_review_email(review_id, recipient_id)
       review = Review.find_by_id(review_id)
+
       @notes = review.notes
       @discussions = Discussion.build_discussions(review.discussion_ids, preload_note_diff_file: true)
       @include_diff_discussion_stylesheet = @discussions.values.any? do |discussion|
