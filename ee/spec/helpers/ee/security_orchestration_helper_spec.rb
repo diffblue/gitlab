aa89bd58638630dee5f5a2227191f5afa188de6d
Alexander Turinske <aturinske@gitlab.com>
2022-04-05 08:28:36 -0600

    Add group level policy new page
    - create initial backend helpers
    - create update js pages to accomodate different types
      of namespaces
    - update policy selection to navigate directly to a new
      scan execution policy for a group
    - update tests

---
+++ ee/spec/helpers/ee/security_orchestration_helper_spec.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe EE::SecurityOrchestrationHelper do
+  let_it_be_with_reload(:namespace) { create(:group, :public) }
+
+  describe '#security_orchestration_policy_data' do
+    let(:approvers) { %w(approver1 approver2) }
+    let(:owner) { namespace.first_owner }
+    let(:base_data) do
+      {
+        assigned_policy_project: "null",
+        disable_scan_policy_update: false,
+        create_agent_help_path: kind_of(String),
+        policy: policy&.to_json,
+        policy_editor_empty_state_svg_path: kind_of(String),
+        policy_type: policy_type,
+        policies_path: nil,
+        scan_policy_documentation_path: kind_of(String),
+        scan_result_approvers: approvers&.to_json
+      }
+    end
+
+    before do
+      allow(helper).to receive(:current_user) { owner }
+    end
+
+    subject { helper.security_orchestration_policy_data(namespace, policy_type, policy, approvers) }
+
+    context 'when a new policy is being created' do
+      let(:policy) { nil }
+      let(:policy_type) { nil }
+      let(:approvers) { nil }
+
+      it { is_expected.to match(base_data) }
+    end
+
+    context 'when an existing policy is being edited' do
+      let(:policy_type) { 'scan_execution_policy' }
+
+      let(:policy) do
+        Gitlab::Kubernetes::CiliumNetworkPolicy.new(
+          name: 'policy',
+          namespace: 'another',
+          selector: { matchLabels: { role: 'db' } },
+          ingress: [{ from: [{ namespaceSelector: { matchLabels: { group: 'mygroup' } } }] }]
+        )
+      end
+
+      it { is_expected.to match(base_data) }
+    end
+  end
+end
