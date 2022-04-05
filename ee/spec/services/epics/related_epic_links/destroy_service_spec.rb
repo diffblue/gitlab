798a01887aad4a789aefd0580785255bed773e50
Felipe Artur <felipefac@gmail.com>
2022-04-05 15:38:51 -0300

    Track blocking epics removed on usage data
    Record blocking epic removed on usage ping
    and use RedisHLL for data aggregation.
    
    Changelog: added
    EE: true

--- ee/spec/services/epics/related_epic_links/destroy_service_spec.rb
+++ ee/spec/services/epics/related_epic_links/destroy_service_spec.rb
@@ -17,18 +17,34 @@ RSpec.describe Epics::RelatedEpicLinks::DestroyService do
     it_behaves_like 'a destroyable issuable link'
 
     context 'event tracking' do
+      let(:issuable_link) { create(:related_epic_link, link_type: link_type ) }
+
       before do
         issuable_link.source.resource_parent.add_reporter(user)
         issuable_link.target.resource_parent.add_reporter(user)
       end
 
-      context 'for relates_to link type' do
-        it 'records event for related epic link destroyed' do
+      shared_examples 'a recorded event' do
+        it 'records event for destroyed link' do
           expect(Gitlab::UsageDataCounters::EpicActivityUniqueCounter)
-            .to receive(:track_linked_epic_with_type_relates_to_removed).with(author: user).once
+            .to receive(tracking_method).with(author: user).once
 
           subject
         end
+      end
+
+      context 'for relates_to link type' do
+        let(:link_type) { IssuableLink::TYPE_RELATES_TO }
+        let(:tracking_method) { :track_linked_epic_with_type_relates_to_removed }
+
+        it_behaves_like 'a recorded event'
+      end
+
+      context 'for blocks link type' do
+        let(:link_type) { IssuableLink::TYPE_BLOCKS }
+        let(:tracking_method) { :track_linked_epic_with_type_blocks_removed }
+
+        it_behaves_like 'a recorded event'
       end
     end
   end
