fc05549a86405c971c333ef5501cd4a7760f50e8
Sean Arnold <sarnold@gitlab.com>
2022-04-06 11:36:25 +1000

    Fix some failing specs

--- ee/app/assets/javascripts/issues/show/components/incidents/service.js
+++ ee/app/assets/javascripts/issues/show/components/incidents/service.js
@@ -7,23 +7,23 @@ function replaceModelIId(payload = {}) {
 }
 
 export const getMetricImages = async (payload) => {
-  const apiPayload = replaceModelIId(payload);
-  const response = await Api.fetchIssueMetricImages(apiPayload);
+  payload = replaceModelIId(payload);
+  const response = await Api.fetchIssueMetricImages(payload);
   return convertObjectPropsToCamelCase(response.data, { deep: true });
 };
 export const uploadMetricImage = async (payload) => {
-  const apiPayload = replaceModelIId(payload);
-  const response = await Api.uploadIssueMetricImage(apiPayload);
+  payload = replaceModelIId(payload);
+  const response = await Api.uploadIssueMetricImage(payload);
   return convertObjectPropsToCamelCase(response.data);
 };
 export const updateMetricImage = async (payload) => {
-  const apiPayload = replaceModelIId(payload);
-  const response = await Api.updateIssueMetricImage(apiPayload);
+  payload = replaceModelIId(payload);
+  const response = await Api.updateIssueMetricImage(payload);
   return convertObjectPropsToCamelCase(response.data);
 };
 export const deleteMetricImage = async (payload) => {
-  const apiPayload = replaceModelIId(payload);
-  const response = await Api.deleteMetricImage(apiPayload);
+  payload = replaceModelIId(payload);
+  const response = await Api.deleteMetricImage(payload);
   return convertObjectPropsToCamelCase(response.data);
 };
 
