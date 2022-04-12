abd0d42c0127fd9bfb15623239dc03a9d333fd60
Martin Tan <lmtan@jihulab.com>
2022-04-12 09:48:45 +0000

    Refactor: add prepend mod to trail registrations controller

---
+++ ee/spec/requests/trial_registrations_controller_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe TrialRegistrationsController do
+  let(:com) { true }
+
+  before do
+    allow(Gitlab).to receive(:com?).and_return(com)
+  end
+
+  describe 'POST new' do
+    let(:user_params) do
+      build_stubbed(:user)
+        .slice(:first_name, :last_name, :email, :username, :password)
+    end
+
+    context 'when email_opted_in does not exist in params' do
+      it 'sets user email_opted_in to false' do
+        post trial_registrations_path, params: { user: user_params }
+
+        expect(response).to have_gitlab_http_status(:found)
+        expect(User.last.email_opted_in).to be_nil
+      end
+    end
+
+    context 'when email_opted_in is true in params' do
+      it 'sets user email_opted_in to true' do
+        post trial_registrations_path, params: {
+          user: user_params.merge(email_opted_in: true)
+        }
+
+        expect(response).to have_gitlab_http_status(:found)
+        expect(User.last.email_opted_in).to be true
+      end
+    end
+  end
+end
