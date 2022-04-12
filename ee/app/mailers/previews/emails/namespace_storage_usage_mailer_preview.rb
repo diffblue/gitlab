cb824d06387698453c0f89f1e419066b5d607bd9
Jason Goodman <jgoodman@gitlab.com>
2022-04-12 16:36:21 +0000

    Add remaining storage size to namespace storage email notification
    Include remaining storage size along with percentage in email body
    
    Changelog: changed

--- ee/app/mailers/previews/emails/namespace_storage_usage_mailer_preview.rb
+++ ee/app/mailers/previews/emails/namespace_storage_usage_mailer_preview.rb
@@ -7,7 +7,7 @@ module Emails
     end
 
     def limit_warning
-      ::Emails::NamespaceStorageUsageMailer.notify_limit_warning(Group.last, %w(bob@example.com), 25)
+      ::Emails::NamespaceStorageUsageMailer.notify_limit_warning(Group.last, %w(bob@example.com), 25, 1.25.gigabytes)
     end
   end
 end
--- ee/app/services/namespaces/storage/email_notification_service.rb
+++ ee/app/services/namespaces/storage/email_notification_service.rb
@@ -11,12 +11,11 @@ module Namespaces
         return unless namespace.root_storage_statistics
 
         root_storage_size = ::Namespace::RootStorageSize.new(namespace)
-        usage_ratio = root_storage_size.usage_ratio
-        level = notification_level(usage_ratio)
+        level = notification_level(root_storage_size)
         last_level = namespace.root_storage_statistics.notification_level.to_sym
 
         if level != last_level
-          send_notification(level, namespace, usage_ratio)
+          send_notification(level, namespace, root_storage_size)
           update_notification_level(level, namespace)
         end
       end
@@ -25,8 +24,8 @@ module Namespaces
 
       attr_reader :mailer
 
-      def notification_level(usage_ratio)
-        case usage_ratio
+      def notification_level(root_storage_size)
+        case root_storage_size.usage_ratio
         when 0...0.7 then :storage_remaining
         when 0.7...0.85 then :caution
         when 0.85...0.95 then :warning
@@ -35,7 +34,7 @@ module Namespaces
         end
       end
 
-      def send_notification(level, namespace, usage_ratio)
+      def send_notification(level, namespace, root_storage_size)
         return if level == :storage_remaining
 
         owner_emails = namespace.owners.map(&:email)
@@ -43,8 +42,9 @@ module Namespaces
         if level == :exceeded
           mailer.notify_out_of_storage(namespace, owner_emails)
         else
-          percentage = storage_remaining_percentage(usage_ratio)
-          mailer.notify_limit_warning(namespace, owner_emails, percentage)
+          percentage = remaining_storage_percentage(root_storage_size)
+          size = remaining_storage_size(root_storage_size)
+          mailer.notify_limit_warning(namespace, owner_emails, percentage, size)
         end
       end
 
@@ -52,8 +52,12 @@ module Namespaces
         namespace.root_storage_statistics.update!(notification_level: level)
       end
 
-      def storage_remaining_percentage(usage_ratio)
-        (100 - usage_ratio * 100).floor
+      def remaining_storage_percentage(root_storage_size)
+        (100 - root_storage_size.usage_ratio * 100).floor
+      end
+
+      def remaining_storage_size(root_storage_size)
+        root_storage_size.limit - root_storage_size.current_size
       end
     end
   end
