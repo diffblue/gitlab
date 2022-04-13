f97f2ef1b81218fd4a61e45c87953751492b8768
Josianne Hyson <jhyson@gitlab.com>
2022-04-13 12:59:41 +1200

    Ensure max_seats_used_changed_at stays up to date
    Add a callback to the GitlabSubscription model to ensure that the
    max_seats_used_changed_at field is set when the max_seats_used value
    changes for the subscription. Clear the value when the subscription
    renews for a new term.
    
    Issue: https://gitlab.com/gitlab-org/gitlab/-/issues/348481
    Changelog: added
    MR: https://gitlab.com/gitlab-org/gitlab/-/merge_requests/84913

--- ee/app/models/gitlab_subscription.rb
+++ ee/app/models/gitlab_subscription.rb
@@ -10,6 +10,7 @@ class GitlabSubscription < ApplicationRecord
 
   default_value_for(:start_date) { Date.today }
 
+  before_update :set_max_seats_used_changed_at
   before_update :log_previous_state_for_update
   before_update :reset_seats_for_new_term
 
@@ -185,6 +186,13 @@ class GitlabSubscription < ApplicationRecord
 
     self.max_seats_used = attributes['seats_in_use']
     self.seats_owed = calculate_seats_owed
+    self.max_seats_used_changed_at = nil
+  end
+
+  def set_max_seats_used_changed_at
+    return if new_term? || !max_seats_used_changed?
+
+    self.max_seats_used_changed_at = Time.current
   end
 
   def new_term?
