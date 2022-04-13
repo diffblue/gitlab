9ac4f7ecfebc86c789f54575653d7fbf538814e8
Thong Kuah <tkuah@gitlab.com>
2022-04-13 02:02:10 +0000

    Update issue link in comment

--- spec/features/projects/issues/design_management/user_uploads_designs_spec.rb
+++ spec/features/projects/issues/design_management/user_uploads_designs_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe 'User uploads new design', :js do
   let(:issue) { create(:issue, project: project) }
 
   before do
-    # Cause of raising query limiting threshold https://gitlab.com/gitlab-org/gitlab/-/issues/347334
+    # Cause of raising query limiting threshold https://gitlab.com/gitlab-org/gitlab/-/issues/358845
     stub_const("Gitlab::QueryLimiting::Transaction::THRESHOLD", 106)
 
     sign_in(user)
