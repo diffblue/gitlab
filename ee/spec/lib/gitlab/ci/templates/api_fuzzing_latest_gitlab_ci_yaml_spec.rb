0871fe54d3ba570923af92a9277c0c297baa9c77
Michael Eddington <meddington@gitlab.com>
2022-04-14 13:52:27 +0000

    Change _VERSION_TAG to _IMAGE_SUFFIX in API Sec templates

--- ee/spec/lib/gitlab/ci/templates/api_fuzzing_latest_gitlab_ci_yaml_spec.rb
+++ ee/spec/lib/gitlab/ci/templates/api_fuzzing_latest_gitlab_ci_yaml_spec.rb
@@ -146,8 +146,8 @@ RSpec.describe 'API-Fuzzing.latest.gitlab-ci.yml' do
             create(:ci_variable, project: project, key: 'FUZZAPI_TARGET_URL', value: 'http://example.com')
           end
 
-          it 'sets FUZZAPI_VERSION_TAG to ""' do
-            expect(build_variables).to be_include(['FUZZAPI_VERSION_TAG', ''])
+          it 'sets FUZZAPI_IMAGE_SUFFIX to ""' do
+            expect(build_variables).to be_include(['FUZZAPI_IMAGE_SUFFIX', ''])
           end
         end
 
@@ -159,8 +159,8 @@ RSpec.describe 'API-Fuzzing.latest.gitlab-ci.yml' do
             create(:ci_variable, project: project, key: 'CI_GITLAB_FIPS_MODE', value: 'true')
           end
 
-          it 'sets FUZZAPI_VERSION_TAG to "-fips"' do
-            expect(build_variables).to be_include(['FUZZAPI_VERSION_TAG', '-fips'])
+          it 'sets FUZZAPI_IMAGE_SUFFIX to "-fips"' do
+            expect(build_variables).to be_include(['FUZZAPI_IMAGE_SUFFIX', '-fips'])
           end
         end
       end
