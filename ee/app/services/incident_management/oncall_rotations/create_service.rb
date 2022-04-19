6cc4a32c6546396c2f0fa905b2261508f6920f12
Sarah Yasonik <syasonik@gitlab.com>
2022-04-19 07:53:35 +0000

    Swap OncallRotations::BaseService to use ::BaseProjectService
    Per developer style guides, we prefer to use a BaseContainerService
    class as the base class for any services.

--- ee/app/services/incident_management/oncall_rotations/create_service.rb
+++ ee/app/services/incident_management/oncall_rotations/create_service.rb
@@ -8,22 +8,22 @@ module IncidentManagement
       # @param schedule [IncidentManagement::OncallSchedule]
       # @param project [Project]
       # @param user [User]
-      # @param params [Hash<Symbol,Any>]
-      # @param params - name [String] The name of the on-call rotation.
-      # @param params - length [Integer] The length of the rotation.
-      # @param params - length_unit [String] The unit of the rotation length. (One of 'hours', days', 'weeks')
-      # @param params - starts_at [DateTime] The datetime the rotation starts on.
-      # @param params - ends_at [DateTime] The datetime the rotation ends on.
-      # @param params - active_period_start [String] The time the on-call shifts should start, for example: "08:00"
-      # @param params - active_period_end [String] The time the on-call shifts should end, for example: "17:00"
-      # @param params - participants [Array<hash>] An array of hashes defining participants of the on-call rotations.
+      # @param create_params [Hash<Symbol,Any>]
+      # @param create_params - name [String] The name of the on-call rotation.
+      # @param create_params - length [Integer] The length of the rotation.
+      # @param create_params - length_unit [String] The unit of the rotation length. (One of 'hours', days', 'weeks')
+      # @param create_params - starts_at [DateTime] The datetime the rotation starts on.
+      # @param create_params - ends_at [DateTime] The datetime the rotation ends on.
+      # @param create_params - active_period_start [String] The time the on-call shifts should start, for example: "08:00"
+      # @param create_params - active_period_end [String] The time the on-call shifts should end, for example: "17:00"
+      # @param create_params - participants [Array<hash>] An array of hashes defining participants of the on-call rotations.
       # @option opts  - participant [User] The user who is part of the rotation
       # @option opts  - color_palette [String] The color palette to assign to the on-call user, for example: "blue".
       # @option opts  - color_weight [String] The color weight to assign to for the on-call user, for example "500". Max 4 chars.
-      def initialize(schedule, project, user, params)
+      def initialize(schedule, project, user, create_params)
+        super(project: project, current_user: user, params: create_params)
+
         @schedule = schedule
-        @project = project
-        @user = user
         @rotation_params = params.except(:participants)
         @participants_params = Array(params[:participants])
       end
@@ -50,7 +50,7 @@ module IncidentManagement
 
       private
 
-      attr_reader :schedule, :project, :user, :rotation_params, :participants_params, :oncall_rotation
+      attr_reader :schedule, :rotation_params, :participants_params, :oncall_rotation
 
       def error_no_permissions
         error('You have insufficient permissions to create an on-call rotation for this project')
