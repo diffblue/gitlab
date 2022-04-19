a93a42b8aeaed4d3099b49bc18a07b11814fd704
Adam Hegyi <ahegyi@gitlab.com>
2022-04-19 13:37:52 +0000

    Optimize followed users queries
    This change prepares a new index on the events table and changes the
    followed users queries to use the in-operator optimization.
    
    Changelog: added

--- ee/lib/ee/event_filter.rb
+++ ee/lib/ee/event_filter.rb
@@ -16,6 +16,20 @@ module EE
       end
     end
 
+    def in_operator_query_builder_params(user_ids)
+      case filter
+      when EPIC
+        in_operator_params(
+          array_scope_ids: user_ids,
+          scope: ::Event.epics,
+          in_column: :action,
+          in_values: ::Event.actions.values_at(*::Event::EPIC_ACTIONS)
+        )
+      else
+        super
+      end
+    end
+
     private
 
     override :filters
