88c71e08b4d276abe8d840d739ee1047961ec8c6
Gregory Havenga <11164960-ghavenga@users.noreply.gitlab.com>
2022-04-19 07:43:11 +0000

    Optimise Security::Finding cleanup by clearing build_id scope
    Unscoping the where: :build_id scope from the cleanup_service
    scan_batch relation resolves a strange issue where this query was being
    carried through to the Security::Finding queries.
    
    `unscope` is not permitted for use outside of models, so this action was
    moved to a scope on the Security::Scan model.
    
    Changelog: performance
    MR: https://gitlab.com/gitlab-org/gitlab/-/merge_requests/84283
    EE: true

--- ee/app/services/security/findings/cleanup_service.rb
+++ ee/app/services/security/findings/cleanup_service.rb
@@ -4,7 +4,8 @@ module Security
   module Findings
     class CleanupService
       MAX_STALE_SCANS_SIZE = 50_000
-      BATCH_DELETE_SIZE = 10_000
+      SCAN_BATCH_SIZE = 100
+      BATCH_DELETE_SIZE = 100
 
       class << self
         def delete_stale_records
@@ -25,7 +26,7 @@ module Security
       end
 
       def execute
-        security_scans.in_batches(&method(:purge)) # rubocop:disable Cop/InBatches (`each_batch` does not let us set a global limit of records to be batched)
+        security_scans.in_batches(of: SCAN_BATCH_SIZE, &method(:purge)) # rubocop:disable Cop/InBatches (`each_batch` does not let us set a global limit of records to be batched)
       end
 
       private
@@ -33,14 +34,16 @@ module Security
       attr_reader :security_scans
 
       def purge(scan_batch)
-        delete_findings_for(scan_batch)
+        delete_findings_for(scan_batch.where_values_hash['id'])
 
-        scan_batch.update_all(status: :purged)
+        scan_batch.unscope(where: :build_id).update_all(status: :purged) # rubocop:disable CodeReuse/ActiveRecord unlikely that a `unscope where build_id` scope would be used elsewhere
       end
 
       def delete_findings_for(scan_batch)
-        Security::Finding.by_scan(scan_batch).each_batch(of: BATCH_DELETE_SIZE) do |finding_batch|
-          finding_batch.delete_all
+        findings_relation = Security::Finding.by_scan(scan_batch).limit(BATCH_DELETE_SIZE)
+
+        loop do
+          break if findings_relation.delete_all < BATCH_DELETE_SIZE
         end
       end
     end
