5f37260aaa733c9ba914e286bfe47a192f565891
Tom Quirk <tquirk@gitlab.com>
2022-04-21 14:49:53 +0930

    Jira connect: add multi-version sign in form
    This commit adds the UI for users to specify
    which GitLab version they are using for Jira Connect.
    
    It is currently behind the jira_connect_oauth_self_managed
    feature flag, disabled by default.
    
    A future commit will integrate the feature fully so that
    it is usable.

---
+++ spec/frontend/jira_connect/subscriptions/pages/sign_in/sign_in_gitlab_multiversion/version_select_form_spec.js
@@ -0,0 +1,76 @@
+import { GlFormInput, GlFormRadioGroup, GlForm } from '@gitlab/ui';
+import { nextTick } from 'vue';
+
+import { shallowMountExtended } from 'helpers/vue_test_utils_helper';
+import VersionSelectForm from '~/jira_connect/subscriptions/pages/sign_in/sign_in_gitlab_multiversion/version_select_form.vue';
+import createStore from '~/jira_connect/subscriptions/store';
+
+describe('VersionSelectForm', () => {
+  let wrapper;
+  let store;
+
+  const findSelfManagedInstanceInput = () => wrapper.findComponent(GlFormInput);
+  const findFormRadioGroup = () => wrapper.findComponent(GlFormRadioGroup);
+  const findForm = () => wrapper.findComponent(GlForm);
+  const findInput = () => wrapper.findComponent(GlFormInput);
+
+  const submitForm = () => findForm().vm.$emit('submit', new Event('submit'));
+
+  const createComponent = () => {
+    store = createStore();
+
+    wrapper = shallowMountExtended(VersionSelectForm, {
+      store,
+    });
+  };
+
+  afterEach(() => {
+    wrapper.destroy();
+  });
+
+  describe('default state', () => {
+    beforeEach(() => {
+      createComponent();
+    });
+
+    it('selects saas radio option by default', () => {
+      expect(findFormRadioGroup().vm.$attrs.checked).toBe(VersionSelectForm.radioOptions.saas);
+    });
+
+    it('does not render instance input', () => {
+      expect(findSelfManagedInstanceInput().exists()).toBe(false);
+    });
+
+    describe('when form is submitted', () => {
+      it('emits "submit" event with gitlab.com as the payload', () => {
+        submitForm();
+
+        expect(wrapper.emitted('submit')[0][0]).toBe('https://gitlab.com');
+      });
+    });
+  });
+
+  describe('when "self-managed" radio option is selected', () => {
+    beforeEach(async () => {
+      createComponent();
+
+      findFormRadioGroup().vm.$emit('input', VersionSelectForm.radioOptions.selfManaged);
+      await nextTick();
+    });
+
+    it('reveals the self-managed input field', () => {
+      expect(findInput().exists()).toBe(true);
+    });
+
+    describe('when form is submitted', () => {
+      it('emits "submit" event with the input field value as the payload', () => {
+        const mockInstanceUrl = 'https://gitlab.example.com';
+
+        findInput().vm.$emit('input', mockInstanceUrl);
+        submitForm();
+
+        expect(wrapper.emitted('submit')[0][0]).toBe(mockInstanceUrl);
+      });
+    });
+  });
+});
