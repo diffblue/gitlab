b4722344d7435ad4aa00693de53be8f99e90eed1
Coung Ngo <cngo@gitlab.com>
2022-04-22 13:26:30 +0100

    Update feature specs for vue_issues_list
    The vue_issues_list feature flag is now default enabled in the code.
    The feature specs were updated with this feature flag enabled in
    https://gitlab.com/gitlab-org/gitlab/-/merge_requests/61123
    but it has a large diff so it is being split into several MRs
    to make reviewing easier.

--- ee/spec/features/issues/user_views_issues_spec.rb
+++ ee/spec/features/issues/user_views_issues_spec.rb
@@ -10,6 +10,9 @@ RSpec.describe 'User views issues page', :js do
   let_it_be(:issue3) { create(:issue, project: project, health_status: 'at_risk') }
 
   before do
+    stub_feature_flags(vue_issues_list: true)
+    stub_licensed_features(blocked_issues: true, issuable_health_status: true, issue_weights: true)
+
     sign_in(user)
     visit project_issues_path(project)
   end
