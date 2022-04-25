6446a957f11d1c89636c2c866494242d8be97041
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2022-04-25 17:50:21 -0400

    Clean-up UserVerificationService spec
    This cleans up the UserVerificationService spec by de-duplicating the
    ways we use to stub verify requests.

--- ee/spec/services/arkose/user_verification_service_spec.rb
+++ ee/spec/services/arkose/user_verification_service_spec.rb
@@ -7,18 +7,32 @@ RSpec.describe Arkose::UserVerificationService do
   let_it_be_with_reload(:user) { create(:user, id: '1999') }
 
   let(:service) { Arkose::UserVerificationService.new(session_token: session_token, user: user) }
-  let(:response) { instance_double(HTTParty::Response, success?: true, code: 200, parsed_response: arkose_ec_response) }
+  let(:verify_api_url) { "https://verify-api.arkoselabs.com/api/v4/verify/" }
+  let(:response_status_code) { 200 }
   let(:arkose_labs_private_api_key) { 'foo' }
 
   subject { service.execute }
 
+  before do
+    stub_request(:post, verify_api_url)
+      .with(
+        body: /.*/,
+        headers: {
+          'Accept' => '*/*'
+        }
+      ).to_return(status: response_status_code, body: arkose_ec_response.to_json, headers: {
+        'Content-Type' => 'application/json'
+      })
+  end
+
   describe '#execute' do
-    shared_examples_for 'interacting with Arkose verify API' do |verify_api_url|
+    shared_examples_for 'interacting with Arkose verify API' do |url|
+      let(:verify_api_url) { url }
+
       context 'when the user did not solve the challenge' do
         let(:arkose_ec_response) { Gitlab::Json.parse(File.read(Rails.root.join('ee/spec/fixtures/arkose/failed_ec_response.json'))) }
 
         it 'returns false' do
-          allow(Gitlab::HTTP).to receive(:perform_request).and_return(response)
           expect(subject).to be_falsey
         end
       end
@@ -28,16 +42,6 @@ RSpec.describe Arkose::UserVerificationService do
           context 'when the risk score is not high' do
             let(:arkose_ec_response) { Gitlab::Json.parse(File.read(Rails.root.join('ee/spec/fixtures/arkose/successfully_solved_ec_response.json'))) }
 
-            before do
-              stub_request(:post, verify_api_url)
-                .with(
-                  body: /.*/,
-                  headers: {
-                    'Accept' => '*/*'
-                  }
-                ).to_return(status: 200, body: "", headers: {})
-            end
-
             it 'makes a request to the Verify API' do
               subject
 
@@ -45,12 +49,10 @@ RSpec.describe Arkose::UserVerificationService do
             end
 
             it 'returns true' do
-              allow(Gitlab::HTTP).to receive(:perform_request).and_return(response)
               expect(subject).to be_truthy
             end
 
             it 'adds arkose data to custom attributes' do
-              allow(Gitlab::HTTP).to receive(:perform_request).and_return(response)
               subject
               expect(user.custom_attributes.count).to eq(4)
 
@@ -61,7 +63,6 @@ RSpec.describe Arkose::UserVerificationService do
             end
 
             it 'logs Arkose verify response' do
-              allow(Gitlab::HTTP).to receive(:perform_request).and_return(response)
               allow(Gitlab::AppLogger).to receive(:info)
               allow(Gitlab::ApplicationContext).to receive(:current).and_return({ 'correlation_id': 'be025cf83013ac4f52ffd2bf712b11a2' })
 
@@ -84,17 +85,17 @@ RSpec.describe Arkose::UserVerificationService do
               let(:arkose_ec_response) { Gitlab::Json.parse(File.read(Rails.root.join('ee/spec/fixtures/arkose/successfully_solved_ec_response_high_risk.json'))) }
 
               it 'returns false' do
-                allow(Gitlab::HTTP).to receive(:perform_request).and_return(response)
                 expect(subject).to be_falsey
               end
 
               context 'when the session is allowlisted' do
-                before do
-                  arkose_ec_response['session_details']['telltale_list'].push(Arkose::UserVerificationService::ALLOWLIST_TELLTALE)
+                let(:arkose_ec_response) do
+                  json = Gitlab::Json.parse(File.read(Rails.root.join('ee/spec/fixtures/arkose/successfully_solved_ec_response.json')))
+                  json['session_details']['telltale_list'].push(Arkose::UserVerificationService::ALLOWLIST_TELLTALE)
+                  json
                 end
 
                 it 'returns true' do
-                  allow(Gitlab::HTTP).to receive(:perform_request).and_return(response)
                   expect(subject).to be_truthy
                 end
               end
@@ -107,7 +108,6 @@ RSpec.describe Arkose::UserVerificationService do
             let(:arkose_ec_response) { Gitlab::Json.parse(File.read(Rails.root.join('ee/spec/fixtures/arkose/successfully_solved_ec_response_without_session_risk.json'))) }
 
             it 'returns true' do
-              allow(Gitlab::HTTP).to receive(:perform_request).and_return(response)
               expect(subject).to be_truthy
             end
           end
@@ -116,7 +116,6 @@ RSpec.describe Arkose::UserVerificationService do
             let(:arkose_ec_response) { Gitlab::Json.parse(File.read(Rails.root.join('ee/spec/fixtures/arkose/failed_ec_response_without_risk_session.json'))) }
 
             it 'returns false' do
-              allow(Gitlab::HTTP).to receive(:perform_request).and_return(response)
               expect(subject).to be_falsey
             end
           end
@@ -124,8 +123,10 @@ RSpec.describe Arkose::UserVerificationService do
       end
 
       context 'when an error occurs during the Arkose request' do
+        let(:arkose_ec_response) { {} }
+        let(:response_status_code) { 500 }
+
         it 'returns true' do
-          allow(Gitlab::HTTP).to receive(:perform_request).and_raise(Gitlab::HTTP::BlockedUrlError)
           expect(subject).to be_truthy
         end
       end
@@ -157,7 +158,6 @@ RSpec.describe Arkose::UserVerificationService do
         let(:arkose_ec_response) { Gitlab::Json.parse(File.read(Rails.root.join('ee/spec/fixtures/arkose/successfully_solved_ec_response_high_risk.json'))) }
 
         it 'returns true' do
-          allow(Gitlab::HTTP).to receive(:perform_request).and_return(response)
           expect(subject).to be_truthy
         end
       end
