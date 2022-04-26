03fd3d9164785096a8a2ce8ba9f691d46732b9c0
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2022-04-26 08:29:09 -0400

    Define ARKOSE_LABS_DEFAULT_NAMESPACE in migration

--- db/migrate/20220419223906_add_arkose_namespace_to_application_settings.rb
+++ db/migrate/20220419223906_add_arkose_namespace_to_application_settings.rb
@@ -3,12 +3,14 @@
 class AddArkoseNamespaceToApplicationSettings < Gitlab::Database::Migration[2.0]
   disable_ddl_transaction!
 
+  ARKOSE_LABS_DEFAULT_NAMESPACE = 'client'
+
   def up
     unless column_exists?(:application_settings, :arkose_labs_namespace)
       add_column :application_settings,
                  :arkose_labs_namespace,
                  :text,
-                 default: ::Arkose::UserVerificationService::ARKOSE_LABS_DEFAULT_NAMESPACE,
+                 default: ARKOSE_LABS_DEFAULT_NAMESPACE,
                  null: false
     end
 
