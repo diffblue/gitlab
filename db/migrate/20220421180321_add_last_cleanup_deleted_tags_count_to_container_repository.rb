5e2ab3d0690dab6e50806c30943a0a73b27a15b7
Jose Vargas <jvargas@gitlab.com>
2022-04-26 15:59:12 -0500

    Add tests for reset_last_cleanup_deleted_tags_count

--- db/migrate/20220421180321_add_last_cleanup_deleted_tags_count_to_container_repository.rb
+++ db/migrate/20220421180321_add_last_cleanup_deleted_tags_count_to_container_repository.rb
@@ -2,6 +2,6 @@
 
 class AddLastCleanupDeletedTagsCountToContainerRepository < Gitlab::Database::Migration[2.0]
   def change
-    add_column :container_repositories, :last_cleanup_deleted_tags_count, :integer, default: nil, null: true
+    add_column :container_repositories, :last_cleanup_deleted_tags_count, :integer
   end
 end
