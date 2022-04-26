1a96cfcb5f208d171a503da06a267cd1fd674d59
Zack Cuddy <zcuddy@gitlab.com>
2022-04-26 12:10:49 -0500

    Geo Sites Form - Cleanup Learn more links
    This change cleans up the
    learn more links scattered
    across the Geo Sites UI.
    
    This is done by consilidating
    links and adding some better
    popovers.
    
    Changelog: changed
    EE: true

--- ee/spec/frontend/geo_node_form/components/geo_node_form_selective_sync_spec.js
+++ ee/spec/frontend/geo_node_form/components/geo_node_form_selective_sync_spec.js
@@ -1,5 +1,5 @@
-import { GlFormGroup, GlSprintf, GlBadge } from '@gitlab/ui';
-import { shallowMount } from '@vue/test-utils';
+import { GlFormGroup, GlSprintf, GlBadge, GlIcon, GlPopover, GlLink } from '@gitlab/ui';
+import { shallowMountExtended } from 'helpers/vue_test_utils_helper';
 import GeoNodeFormNamespaces from 'ee/geo_node_form/components/geo_node_form_namespaces.vue';
 import GeoNodeFormSelectiveSync from 'ee/geo_node_form/components/geo_node_form_selective_sync.vue';
 import GeoNodeFormShards from 'ee/geo_node_form/components/geo_node_form_shards.vue';
@@ -20,7 +20,7 @@ describe('GeoNodeFormSelectiveSync', () => {
   };
 
   const createComponent = (props = {}) => {
-    wrapper = shallowMount(GeoNodeFormSelectiveSync, {
+    wrapper = shallowMountExtended(GeoNodeFormSelectiveSync, {
       stubs: { GlFormGroup, GlSprintf },
       propsData: {
         ...defaultProps,
@@ -35,25 +35,33 @@ describe('GeoNodeFormSelectiveSync', () => {
 
   const findGeoNodeFormSyncContainer = () =>
     wrapper.findComponent({ ref: 'geoNodeFormSelectiveSyncContainer' });
-  const findGeoNodeFormSelectiveSyncMoreInfoLink = () =>
-    wrapper.find('[data-testid="selectiveSyncMoreInfo"]');
+
+  const findGeoNodeFormSyncTypeFormGroup = () => wrapper.findByTestId('selective-sync-form-group');
+  const findGeoNodeFormSyncTypePopoverIcon = () =>
+    findGeoNodeFormSyncTypeFormGroup().findComponent(GlIcon);
+  const findGeoNodeFormSyncTypePopover = () =>
+    findGeoNodeFormSyncTypeFormGroup().findComponent(GlPopover);
+  const findGeoNodeFormSyncTypePopoverLink = () =>
+    findGeoNodeFormSyncTypePopover().findComponent(GlLink);
+
+  const findGeoNodeFormObjectStorageFormGroup = () =>
+    wrapper.findByTestId('object-storage-form-group');
+  const findGeoNodeFormObjectStoragePopoverIcon = () =>
+    findGeoNodeFormObjectStorageFormGroup().findComponent(GlIcon);
+  const findGeoNodeFormObjectStoragePopover = () =>
+    findGeoNodeFormObjectStorageFormGroup().findComponent(GlPopover);
+  const findGeoNodeFormObjectStoragePopoverLink = () =>
+    findGeoNodeFormObjectStoragePopover().findComponent(GlLink);
+
   const findGeoNodeFormSyncTypeField = () => wrapper.find('#node-selective-synchronization-field');
   const findGeoNodeFormNamespacesField = () => wrapper.findComponent(GeoNodeFormNamespaces);
   const findGeoNodeFormShardsField = () => wrapper.findComponent(GeoNodeFormShards);
   const findGeoNodeObjectStorageField = () => wrapper.find('#node-object-storage-field');
-  const findGeoNodeFormObjectStorageMoreInformation = () =>
-    wrapper.find('[data-testid="objectStorageMoreInfo"]');
   const findGeoNodeObjectStorageBetaBadge = () => wrapper.findComponent(GlBadge);
 
   describe('template', () => {
     beforeEach(() => {
       createComponent();
-    });
-
-    it('renders section More Information link correctly', () => {
-      expect(findGeoNodeFormSelectiveSyncMoreInfoLink().attributes('href')).toBe(
-        SELECTIVE_SYNC_MORE_INFO,
-      );
     });
 
     it('renders Geo Node Form Sync Container', () => {
@@ -68,14 +76,50 @@ describe('GeoNodeFormSelectiveSync', () => {
       expect(findGeoNodeObjectStorageField().exists()).toBe(true);
     });
 
-    it('renders Geo Node Form Object Storage More Information link correctly', () => {
-      expect(findGeoNodeFormObjectStorageMoreInformation().attributes('href')).toBe(
-        OBJECT_STORAGE_MORE_INFO,
-      );
-    });
-
     it('renders Geo Node Form Object Beta Badge link correctly', () => {
       expect(findGeoNodeObjectStorageBetaBadge().attributes('href')).toBe(OBJECT_STORAGE_BETA);
+    });
+
+    describe('selective sync type popover', () => {
+      it('renders the question icon correctly', () => {
+        expect(findGeoNodeFormSyncTypePopoverIcon().exists()).toBe(true);
+        expect(findGeoNodeFormSyncTypePopoverIcon().props('name')).toBe('question-o');
+      });
+
+      it('renders the GlPopover', () => {
+        expect(findGeoNodeFormSyncTypePopover().exists()).toBe(true);
+        expect(findGeoNodeFormSyncTypePopover().text()).toContain(
+          GeoNodeFormSelectiveSync.i18n.selectiveSyncPopoverText,
+        );
+      });
+
+      it('renders the popover link correctly', () => {
+        expect(findGeoNodeFormSyncTypePopoverLink().exists()).toBe(true);
+        expect(findGeoNodeFormSyncTypePopoverLink().attributes('href')).toBe(
+          SELECTIVE_SYNC_MORE_INFO,
+        );
+      });
+    });
+
+    describe('object storage popover', () => {
+      it('renders the question icon correctly', () => {
+        expect(findGeoNodeFormObjectStoragePopoverIcon().exists()).toBe(true);
+        expect(findGeoNodeFormObjectStoragePopoverIcon().props('name')).toBe('question-o');
+      });
+
+      it('renders the GlPopover', () => {
+        expect(findGeoNodeFormObjectStoragePopover().exists()).toBe(true);
+        expect(findGeoNodeFormObjectStoragePopover().text()).toContain(
+          GeoNodeFormSelectiveSync.i18n.objectStorageFieldPopoverText,
+        );
+      });
+
+      it('renders the popover link correctly', () => {
+        expect(findGeoNodeFormObjectStoragePopoverLink().exists()).toBe(true);
+        expect(findGeoNodeFormObjectStoragePopoverLink().attributes('href')).toBe(
+          OBJECT_STORAGE_MORE_INFO,
+        );
+      });
     });
 
     describe.each`
