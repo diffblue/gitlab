9b13a5f0e66e6fb1faf6d5503eb32e477ba4368a
Frédéric Caplette <fcaplette@gitlab.com>
2022-04-27 15:17:04 +0000

    Add pipeline tabs redirection support
    When the feature flag pipeline_tabs_vue is on,
    we now use GlTabs instead of haml ones in
    the pipeline page. We want to support two usecase:
    the legacy one had each tab on its own route, so
    now we redirect the user to the right tab if that's
    the case. The other use case is by using query params.

--- ee/app/assets/javascripts/pipelines/components/pipeline_tabs.vue
+++ ee/app/assets/javascripts/pipelines/components/pipeline_tabs.vue
@@ -1,6 +1,7 @@
 <script>
 import { GlTab } from '@gitlab/ui';
 import BasePipelineTabs from '~/pipelines/components/pipeline_tabs.vue';
+import { codeQualityTabName, licensesTabName, securityTabName } from '~/pipelines/constants';
 import glFeatureFlagMixin from '~/vue_shared/mixins/gl_feature_flags_mixin';
 import { __ } from '~/locale';
 import CodequalityReportApp from 'ee/codequality_report/codequality_report.vue';
@@ -16,6 +17,11 @@ export default {
       codeQualityTitle: __('Code Quality'),
     },
   },
+  tabNames: {
+    security: securityTabName,
+    licenses: licensesTabName,
+    codeQuality: codeQualityTabName,
+  },
   components: {
     BasePipelineTabs,
     CodequalityReportApp,
@@ -28,6 +34,7 @@ export default {
   inject: [
     'canGenerateCodequalityReports',
     'codequalityReportDownloadPath',
+    'defaultTabValue',
     'exposeSecurityDashboard',
     'exposeLicenseScanningData',
   ],
@@ -45,6 +52,11 @@ export default {
       return Boolean(this.exposeSecurityDashboard);
     },
   },
+  methods: {
+    isActive(tabName) {
+      return tabName === this.defaultTabValue;
+    },
+  },
 };
 </script>
 
@@ -53,6 +65,7 @@ export default {
     <gl-tab
       v-if="showSecurityTab"
       :title="$options.i18n.tabs.securityTitle"
+      :active="isActive($options.tabNames.security)"
       data-testid="security-tab"
     >
       <pipeline-security-dashboard />
@@ -60,6 +73,7 @@ export default {
     <gl-tab
       v-if="showLicenseTab"
       :title="$options.i18n.tabs.licenseTitle"
+      :active="isActive($options.tabNames.licenses)"
       data-testid="license-tab"
     >
       <license-compliance-app />
@@ -67,6 +81,7 @@ export default {
     <gl-tab
       v-if="showCodeQualityTab"
       :title="$options.i18n.tabs.codeQualityTitle"
+      :active="isActive($options.tabNames.codeQuality)"
       data-testid="code-quality-tab"
       data-track-action="click_button"
       data-track-label="get_codequality_report"
