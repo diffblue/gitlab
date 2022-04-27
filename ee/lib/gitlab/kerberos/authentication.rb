b7af085c8bd24dc71f055a3fb6d0e416e9cd46bb
Bogdan Denkovych <bdenkovych@gitlab.com>
2022-04-27 16:27:08 +0300

    Remove `omniauth-kerberos` gem
    Related to https://gitlab.com/gitlab-org/gitlab/-/issues/2908
    
    Changelog: removed
    EE: true

--- ee/lib/gitlab/kerberos/authentication.rb
+++ ee/lib/gitlab/kerberos/authentication.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-# This calls helps to authenticate to Kerberos by providing username and password
 module Gitlab
   module Kerberos
     class Authentication
@@ -11,46 +10,12 @@ module Gitlab
         default_realm
       end
 
-      def self.login(login, password)
-        return unless Devise.omniauth_providers.include?(:kerberos)
-        return unless login.present? && password.present?
-
-        auth = new(login, password)
-        auth.login
-      end
-
       def self.krb5_class
         @krb5_class ||= begin
           require "krb5_auth"
           Krb5Auth::Krb5
         end
       end
-
-      def initialize(login, password)
-        @login = login
-        @password = password
-        @krb5 = self.class.krb5_class.new
-      end
-
-      def valid?
-        @krb5.get_init_creds_password(@login, @password)
-      rescue self.class.krb5_class::Exception
-        false
-      end
-
-      def login
-        # get_default_principal consistently returns the canonical Kerberos principal name, with realm
-        valid? && find_by_login(@krb5.get_default_principal)
-      end
-
-      private
-
-      # rubocop: disable CodeReuse/ActiveRecord
-      def find_by_login(login)
-        identity = ::Identity.with_extern_uid(:kerberos, login).take
-        identity && identity.user
-      end
-      # rubocop: enable CodeReuse/ActiveRecord
     end
   end
 end
