816c6b424b96876fb22d633b27a74ffa57200b91
Jose Ivan Vargas <jvargas@gitlab.com>
2022-04-28 14:20:43 +0000

    Refactor the usage quotas -> ci/cd minutes page
    This changes the overall design of the usage quotas ->
    CI/CD minutes usage page inside user's preferences
    to better display the usage of their CI/CD minutes
    as well as adds the Shared Runner Duration graph
    
    Changelog: added
    EE: true

--- ee/app/assets/javascripts/ci_minutes_usage/components/app.vue
+++ ee/app/assets/javascripts/ci_minutes_usage/components/app.vue
@@ -1,18 +1,28 @@
 <script>
-import { formatDate } from '~/lib/utils/datetime_utility';
-import getCiMinutesUsage from '../graphql/queries/ci_minutes.graphql';
+import { GlTab, GlTabs } from '@gitlab/ui';
+import { s__ } from '~/locale';
+import HelpPopover from '~/vue_shared/components/help_popover.vue';
+import getCiMinutesUsage from '../graphql/queries/ci_minutes.query.graphql';
+import {
+  USAGE_BY_MONTH_HEADER,
+  USAGE_BY_PROJECT_HEADER,
+  CI_CD_MINUTES_USAGE,
+  SHARED_RUNNER_USAGE,
+} from '../constants';
 import MinutesUsageMonthChart from './minutes_usage_month_chart.vue';
 import MinutesUsageProjectChart from './minutes_usage_project_chart.vue';
+import SharedRunnerUsageMonthChart from './shared_runner_usage_month_chart.vue';
+import NoMinutesAlert from './no_minutes_alert.vue';
 
 export default {
   components: {
+    GlTab,
+    GlTabs,
+    HelpPopover,
+    NoMinutesAlert,
     MinutesUsageMonthChart,
     MinutesUsageProjectChart,
-  },
-  data() {
-    return {
-      ciMinutesUsage: [],
-    };
+    SharedRunnerUsageMonthChart,
   },
   apollo: {
     ciMinutesUsage: {
@@ -22,24 +32,59 @@ export default {
       },
     },
   },
+  data() {
+    return {
+      ciMinutesUsage: [],
+    };
+  },
   computed: {
-    minutesUsageDataByMonth() {
-      return this.ciMinutesUsage
-        .slice()
-        .sort((a, b) => {
-          return new Date(a.monthIso8601) - new Date(b.monthIso8601);
-        })
-        .map((cur) => [formatDate(cur.monthIso8601, 'mmm yyyy'), cur.minutes]);
+    hasMinutes() {
+      return this.ciMinutesUsage.length > 0;
     },
   },
+  popoverOptions: {
+    triggers: 'hover',
+    placement: 'top',
+    content: s__(
+      'CICDAnalytics|Shared runner usage is the total runtime of all jobs that ran on shared runners',
+    ),
+    title: s__('CICDAnalytics|What is shared runner usage?'),
+  },
+  USAGE_BY_MONTH_HEADER,
+  USAGE_BY_PROJECT_HEADER,
+  CI_CD_MINUTES_USAGE,
+  SHARED_RUNNER_USAGE,
 };
 </script>
 <template>
-  <div class="gl-border-b-solid gl-border-gray-200 gl-border-b-1 gl-mb-3">
-    <minutes-usage-month-chart
-      class="gl-border-b-solid gl-border-gray-200 gl-border-b-1"
-      :minutes-usage-data="minutesUsageDataByMonth"
-    />
-    <minutes-usage-project-chart :minutes-usage-data="ciMinutesUsage" />
+  <div class="gl-border-b-solid gl-border-gray-200 gl-border-b-1 gl-mb-3 gl-pl-4">
+    <h4 class="gl-font-lg gl-mb-5">{{ $options.USAGE_BY_MONTH_HEADER }}</h4>
+    <gl-tabs>
+      <gl-tab :title="$options.CI_CD_MINUTES_USAGE">
+        <no-minutes-alert v-if="!hasMinutes" />
+        <minutes-usage-month-chart
+          v-else
+          class="gl-border-b-solid gl-border-gray-200 gl-border-b-1"
+          :ci-minutes-usage="ciMinutesUsage"
+        />
+      </gl-tab>
+      <gl-tab>
+        <template #title>
+          <div id="shared-runner-message-popover-container" class="gl-display-flex">
+            <span class="gl-mr-2">{{ $options.SHARED_RUNNER_USAGE }}</span>
+            <help-popover :options="$options.popoverOptions" />
+          </div>
+        </template>
+        <no-minutes-alert v-if="!hasMinutes" />
+        <shared-runner-usage-month-chart v-else :ci-minutes-usage="ciMinutesUsage" />
+      </gl-tab>
+    </gl-tabs>
+    <h4 class="gl-font-lg gl-mb-5">{{ $options.USAGE_BY_PROJECT_HEADER }}</h4>
+    <gl-tabs>
+      <gl-tab :title="$options.CI_CD_MINUTES_USAGE">
+        <no-minutes-alert v-if="!hasMinutes" />
+        <minutes-usage-project-chart v-else :minutes-usage-data="ciMinutesUsage" />
+      </gl-tab>
+    </gl-tabs>
   </div>
 </template>
