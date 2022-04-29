4d9fe57034d71ce70fde2abc5d50d5c13bf3935e
Doug Stull <dstull@gitlab.com>
2022-04-29 20:42:55 +0000

    Prevent projects creating group links outside hierarchy
    - currently prevented at group layer, but is also needed
      at the project layer.
    
    Changelog: changed

--- ee/app/services/ee/projects/group_links/create_service.rb
+++ ee/app/services/ee/projects/group_links/create_service.rb
@@ -7,40 +7,43 @@ module EE
         extend ::Gitlab::Utils::Override
 
         override :execute
-        def execute(group)
-          return error(error_message, 409) unless allowed_to_be_shared_with?(group)
+        def execute
+          return error(error_message, 409) unless allowed_to_be_shared_with?
 
-          result = super
-
-          log_audit_event(result[:link]) if result[:status] == :success
-          result
+          super
         end
 
         private
 
-        def allowed_to_be_shared_with?(group)
-          project_can_be_shared_with_group = project_can_be_shared_with_group?(group, project)
-          source_project_can_be_shared_with_group = project.forked? ? project_can_be_shared_with_group?(group, project.forked_from_project) : true
+        def after_successful_save
+          super
+
+          log_audit_event
+        end
+
+        def allowed_to_be_shared_with?
+          project_can_be_shared_with_group = project_can_be_shared_with_group?(project)
+          source_project_can_be_shared_with_group = project.forked? ? project_can_be_shared_with_group?(project.forked_from_project) : true
 
           project_can_be_shared_with_group && source_project_can_be_shared_with_group
         end
 
-        def project_can_be_shared_with_group?(group, given_project)
+        def project_can_be_shared_with_group?(given_project)
           return true unless given_project.root_ancestor.kind == 'group' && given_project.root_ancestor.enforced_sso?
 
-          group.root_ancestor == given_project.root_ancestor
+          shared_with_group.root_ancestor == given_project.root_ancestor
         end
 
         def error_message
           _('This group cannot be invited to a project inside a group with enforced SSO')
         end
 
-        def log_audit_event(group_link)
+        def log_audit_event
           ::AuditEventService.new(
             current_user,
-            group_link.group,
+            link.group,
             action: :create
-          ).for_project_group_link(group_link).security_event
+          ).for_project_group_link(link).security_event
         end
       end
     end
