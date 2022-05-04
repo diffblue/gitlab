20548c5fc27555a520d9b22de3e4af52c50d2335
Peter Leitzen <pleitzen@gitlab.com>
2022-05-04 12:38:24 +0200

    Add specs for HAML linter InlineJavaScript

--- haml_lint/linter/inline_javascript.rb
+++ haml_lint/linter/inline_javascript.rb
@@ -5,16 +5,18 @@ module HamlLint
     class InlineJavaScript < Linter
       include ::HamlLint::LinterRegistry
 
+      MSG = 'Inline JavaScript is discouraged (https://docs.gitlab.com/ee/development/gotchas.html#do-not-use-inline-javascript-in-views)'
+
       def visit_filter(node)
         return unless node.filter_type == 'javascript'
 
-        record_lint(node, 'Inline JavaScript is discouraged (https://docs.gitlab.com/ee/development/gotchas.html#do-not-use-inline-javascript-in-views)')
+        record_lint(node, MSG)
       end
 
       def visit_tag(node)
         return unless node.tag_name == 'script'
 
-        record_lint(node, 'Inline JavaScript is discouraged (https://docs.gitlab.com/ee/development/gotchas.html#do-not-use-inline-javascript-in-views)')
+        record_lint(node, MSG)
       end
     end
   end
