diffblue/gitlab

commit dd9a12b7b9b41b36b21fffc544eef1e91e63170a

Update Slack app installation flow for GBP
This modifies the OAuth installation flow for the Slack app to
accommodate the migration from Classic to GBP format, as described in
https://api.slack.com/authentication/migration.

- Use v2 endpoints to authorize and exchange access tokens.
- Gate the behaviour behind a feature flag, and pass on a param
  through the flow so we know which type we're dealing with.

ee/app/services/projects/slack_application_install_service.rb
@@ -4,33 +4,57 @@ module Projects
   class SlackApplicationInstallService < BaseService
     include Gitlab::Routing
 
-    SLACK_EXCHANGE_TOKEN_URL = 'https://slack.com/api/oauth.access'
+    # These scopes are requested when installing the app, additional scopes
+    # will need reauthorization.
+    # https://api.slack.com/authentication/oauth-v2#asking
+    DEFAULT_SCOPES = %w[commands].freeze
+
+    # Endpoint to initiate the OAuth flow, redirects to Slack's authorization screen
+    # https://api.slack.com/authentication/oauth-v2#asking
+    SLACK_AUTHORIZE_URL = 'https://slack.com/oauth/v2/authorize'
+    SLACK_AUTHORIZE_URL_LEGACY = 'https://slack.com/oauth/authorize'
+
+    # Endpoint to exchange the temporary authorization code for an access token
+    # https://api.slack.com/authentication/oauth-v2#exchanging
+    SLACK_EXCHANGE_TOKEN_URL = 'https://slack.com/api/oauth.v2.access'
+    SLACK_EXCHANGE_TOKEN_URL_LEGACY = 'https://slack.com/api/oauth.access'
+
+    def self.use_v2_flow?
+      Feature.enabled?(:slack_app_use_v2_flow, default_enabled: :yaml)
+    end
 
     def execute
       slack_data = exchange_slack_token
 
       return error("Slack: #{slack_data['error']}") unless slack_data['ok']
 
-      unless project.gitlab_slack_application_integration
-        project.create_gitlab_slack_application_integration
-      end
-
-      service = project.gitlab_slack_application_integration
+      integration = project.create_or_load_association(:gitlab_slack_application_integration)
 
       SlackIntegration.create!(
-        service_id: service.id,
-        team_id: slack_data['team_id'],
-        team_name: slack_data['team_name'],
+        service_id: integration.id,
+        team_id: slack_data.dig('team', 'id'),
+        team_name: slack_data.dig('team', 'name'),
         alias: project.full_path,
-        user_id: slack_data['user_id']
+        user_id: slack_data.dig('authed_user', 'id')
       )
 
-      make_sure_chat_name_created(slack_data)
+      # NOTE: Skip this in v2 because we don't receive a user name anymore.
+      # This means the installing user will have to manually authorize
+      # through Slack if they want to use the app, like other users.
+      #
+      # Can be removed with https://gitlab.com/gitlab-org/gitlab/-/issues/356005
+      make_sure_chat_name_created(slack_data) unless using_v2_flow?
 
       success
     end
 
     private
+
+    # The `v2=true` param is passed through the redirect_uri, so we can use
+    # this to keep the whole flow in the same version when the FF is toggled.
+    def using_v2_flow?
+      params[:v2] == 'true'
+    end
 
     # rubocop: disable CodeReuse/ActiveRecord
     def make_sure_chat_name_created(slack_data)
@@ -56,12 +80,31 @@ module Projects
     # rubocop: enable CodeReuse/ActiveRecord
 
     def exchange_slack_token
-      Gitlab::HTTP.get(SLACK_EXCHANGE_TOKEN_URL, query: {
+      query = {
         client_id: Gitlab::CurrentSettings.slack_app_id,
         client_secret: Gitlab::CurrentSettings.slack_app_secret,
-        redirect_uri: slack_auth_project_settings_slack_url(project),
-        code: params[:code]
-      })
+        code: params[:code],
+        # NOTE: Needs to match the `redirect_uri` passed to the authorization endpoint,
+        # otherwise we get a `bad_redirect_uri` error.
+        redirect_uri: slack_auth_project_settings_slack_url(project)
+      }
+
+      if using_v2_flow?
+        exchange_url = SLACK_EXCHANGE_TOKEN_URL
+        query[:redirect_uri] += '?v2=true'
+      else
+        exchange_url = SLACK_EXCHANGE_TOKEN_URL_LEGACY
+      end
+
+      data = Gitlab::HTTP.get(exchange_url, query: query).to_hash
+
+      # Tweak the format of the legacy response so we can treat them the same
+      unless using_v2_flow?
+        data['authed_user'] ||= { 'id' => data['user_id'] }
+        data['team'] ||= { 'id' => data['team_id'], 'name' => data['team_name'] }
+      end
+
+      data
     end
   end
 end

ee/spec/services/projects/slack_application_install_service_spec.rb
@@ -3,50 +3,153 @@
 require 'spec_helper'
 
 RSpec.describe Projects::SlackApplicationInstallService do
-  let!(:user) { create(:user) }
-  let!(:project) { create(:project) }
+  let_it_be(:user) { create(:user) }
+  let_it_be_with_refind(:project) { create(:project) }
 
-  def service(params = {})
-    Projects::SlackApplicationInstallService.new(project, user, params)
+  let(:slack_app_id) { 'A12345' }
+  let(:slack_app_secret) { 'secret' }
+  let(:oauth_code) { 'code' }
+  let(:params) { { code: oauth_code, v2: 'true' } }
+  let(:exchange_url) { described_class::SLACK_EXCHANGE_TOKEN_URL }
+  let(:redirect_url) { Gitlab::Routing.url_helpers.slack_auth_project_settings_slack_url(project, v2: true) }
+
+  subject(:service) { described_class.new(project, user, params) }
+
+  before do
+    stub_application_setting(slack_app_id: slack_app_id, slack_app_secret: slack_app_secret)
+
+    query = {
+      client_id: slack_app_id,
+      client_secret: slack_app_secret,
+      code: oauth_code,
+      redirect_uri: redirect_url
+    }
+
+    stub_request(:get, exchange_url)
+      .with(query: query)
+      .to_return(body: response.to_json, headers: { 'Content-Type' => 'application/json' })
   end
 
-  def stub_slack_response_with(response)
-    expect_next_instance_of(Projects::SlackApplicationInstallService) do |instance|
-      expect(instance).to receive(:exchange_slack_token).and_return(response.stringify_keys)
+  def expect_slack_integration_is_created
+    project.reload
+    integration = project.gitlab_slack_application_integration
+    installation = integration.slack_integration
+
+    expect(integration).to be_present
+    expect(installation).to be_present
+    expect(installation).to have_attributes(
+      service_id: integration.id,
+      team_id: 'T12345',
+      team_name: 'Team name',
+      alias: project.full_path,
+      user_id: 'U12345'
+    )
+  end
+
+  def expect_chat_name_is_created
+    expect(user.chat_names.first).to have_attributes(
+      service_id: project.gitlab_slack_application_integration.id,
+      team_id: 'T12345',
+      team_domain: 'Team name',
+      chat_id: 'U12345',
+      chat_name: 'username',
+      user: user
+    )
+  end
+
+  context 'Slack responds with an error' do
+    let(:response) do
+      {
+        ok: false,
+        error: 'something is wrong'
+      }
+    end
+
+    it 'returns error result' do
+      result = service.execute
+
+      expect(result).to eq(message: 'Slack: something is wrong', status: :error)
     end
   end
 
-  def expect_slack_integration_is_created(project)
-    integration = SlackIntegration.find_by(service_id: project.gitlab_slack_application_integration.id)
-    expect(integration).to be_present
-  end
+  context 'Slack responds with an access token' do
+    let(:response) do
+      {
+        ok: true,
+        app_id: 'A12345',
+        authed_user: { id: 'U12345' },
+        scope: 'commands',
+        token_type: 'bot',
+        access_token: 'token-XXXXX',
+        bot_user_id: 'U99999',
+        team: { id: 'T12345', name: 'Team name' },
+        enterprise: { is_enterprise_install: false }
+      }
+    end
 
-  def expect_chat_name_is_created(project)
-    chat_name = ChatName.find_by(service_id: project.gitlab_slack_application_integration.id)
-    expect(chat_name).to be_present
-  end
+    shared_context 'legacy response' do
+      let(:exchange_url) { described_class::SLACK_EXCHANGE_TOKEN_URL_LEGACY }
+      let(:redirect_url) { super().delete_suffix('?v2=true') }
+      let(:response) do
+        {
+          ok: true,
+          access_token: 'token-XXXXX',
+          user_id: 'U12345',
+          user_name: 'username',
+          team_id: 'T12345',
+          team_name: 'Team name'
+        }
+      end
+    end
 
-  it 'returns error result' do
-    stub_slack_response_with(ok: false, error: 'something is wrong')
+    it 'returns success result and creates all needed records' do
+      result = service.execute
 
-    result = service.execute
+      expect(result).to eq(status: :success)
+      expect_slack_integration_is_created
+      expect(ChatName.count).to be_zero
+    end
 
-    expect(result).to eq(message: 'Slack: something is wrong', status: :error)
-  end
+    context 'when the v2 param is missing in the query string' do
+      let(:params) { super().without(:v2) }
 
-  it 'returns success result and creates all the needed records' do
-    stub_slack_response_with(
-      ok: true,
-      access_token: 'XXXX',
-      user_id: 'U12345',
-      team_id: 'T1265',
-      team_name: 'super-team'
-    )
+      include_context 'legacy response'
 
-    result = service.execute
+      it 'uses the legacy endpoint and creates all needed records' do
+        result = service.execute
 
-    expect(result).to eq(status: :success)
-    expect_slack_integration_is_created(project)
-    expect_chat_name_is_created(project)
+        expect(result).to eq(status: :success)
+        expect_slack_integration_is_created
+        expect_chat_name_is_created
+      end
+    end
+
+    context 'when the FF :slack_app_use_v2_flow is disabled' do
+      before do
+        stub_feature_flags(slack_app_use_v2_flow: false)
+      end
+
+      it 'still uses the v2 endpoint and creates all needed records' do
+        result = service.execute
+
+        expect(result).to eq(status: :success)
+        expect_slack_integration_is_created
+        expect(ChatName.count).to be_zero
+      end
+
+      context 'when the v2 param is missing in the query string' do
+        let(:params) { super().without(:v2) }
+
+        include_context 'legacy response'
+
+        it 'uses the legacy endpoint and creates all needed records' do
+          result = service.execute
+
+          expect(result).to eq(status: :success)
+          expect_slack_integration_is_created
+          expect_chat_name_is_created
+        end
+      end
+    end
   end
 end

config/feature_flags/development/slack_app_use_v2_flow.yml
@@ -1,0 +1,8 @@
+---
+name: slack_app_use_v2_flow
+introduced_by_url: https://gitlab.com/gitlab-org/gitlab/-/merge_requests/85726
+rollout_issue_url: https://gitlab.com/gitlab-org/gitlab/-/issues/360680
+milestone: '15.0'
+type: development
+group: group::integrations
+default_enabled: false

ee/app/helpers/ee/integrations_helper.rb
@@ -29,7 +29,21 @@ module EE
     end
 
     def add_to_slack_link(project, slack_app_id)
-      "https://slack.com/oauth/authorize?scope=commands&client_id=#{slack_app_id}&redirect_uri=#{slack_auth_project_settings_slack_url(project)}&state=#{escaped_form_authenticity_token}"
+      query = {
+        scope: ::Projects::SlackApplicationInstallService::DEFAULT_SCOPES.join(','),
+        client_id: slack_app_id,
+        redirect_uri: slack_auth_project_settings_slack_url(project),
+        state: form_authenticity_token
+      }
+
+      if ::Projects::SlackApplicationInstallService.use_v2_flow?
+        authorize_url = ::Projects::SlackApplicationInstallService::SLACK_AUTHORIZE_URL
+        query[:redirect_uri] += '?v2=true'
+      else
+        authorize_url = ::Projects::SlackApplicationInstallService::SLACK_AUTHORIZE_URL_LEGACY
+      end
+
+      "#{authorize_url}?#{query.to_query}"
     end
 
     def gitlab_slack_application_data(projects)
@@ -41,10 +55,6 @@ module EE
         gitlab_logo_path: image_path('illustrations/gitlab_logo.svg'),
         slack_logo_path: image_path('illustrations/slack_logo.svg')
       }
-    end
-
-    def escaped_form_authenticity_token
-      CGI.escape(form_authenticity_token)
     end
 
     def jira_issues_show_data

ee/spec/helpers/ee/integrations_helper_spec.rb
@@ -81,12 +81,36 @@ RSpec.describe EE::IntegrationsHelper do
   end
 
   describe '#add_to_slack_link' do
-    it 'encodes a masked CSRF token' do
-      expect(subject).to receive(:form_authenticity_token).and_return('a token')
-      slack_link = subject.add_to_slack_link(project, '123456')
+    let(:slack_link) { subject.add_to_slack_link(project, 'A12345') }
+    let(:query) { Rack::Utils.parse_query(URI.parse(slack_link).query) }
 
-      expect(slack_link).to start_with('https://slack.com/oauth/authorize')
-      expect(slack_link).to include('redirect_uri=http://some-path/project/1&state=a+token')
+    before do
+      expect(subject).to receive(:form_authenticity_token).and_return('a token')
+    end
+
+    it 'returns the endpoint URL with all needed params' do
+      expect(slack_link).to start_with(Projects::SlackApplicationInstallService::SLACK_AUTHORIZE_URL)
+      expect(slack_link).to include('&state=a+token')
+
+      expect(query).to include(
+        'scope' => 'commands',
+        'client_id' => 'A12345',
+        'redirect_uri' => subject.slack_auth_project_settings_slack_url(project) + '?v2=true',
+        'state' => 'a token'
+      )
+    end
+
+    context 'when the FF :slack_app_use_v2_flow is disabled' do
+      before do
+        stub_feature_flags(slack_app_use_v2_flow: false)
+      end
+
+      it 'returns the URL for the legacy endpoint' do
+        expect(slack_link).to start_with(Projects::SlackApplicationInstallService::SLACK_AUTHORIZE_URL_LEGACY)
+        expect(query).to include(
+          'redirect_uri' => subject.slack_auth_project_settings_slack_url(project)
+        )
+      end
     end
   end
 

commit 8ee61098b09204858939bf4ee9ab223c6654e842

Fix bug when reinstalling Slack app
The switch to `create_or_load_association` in dd9a12b7b9b introduced
a bug where the Slack app could not be installed if the integration
record already exists (after e.g. uninstalling the app).

This reverts to the previous behaviour where we manually fetched or
created the record.

Sentry: https://sentry.gitlab.net/gitlab/gitlabcom/issues/3284004
Changelog: fixed
EE: true

ee/app/services/projects/slack_application_install_service.rb
@@ -28,7 +28,8 @@ module Projects
 
       return error("Slack: #{slack_data['error']}") unless slack_data['ok']
 
-      integration = project.create_or_load_association(:gitlab_slack_application_integration)
+      integration = project.gitlab_slack_application_integration \
+        || project.create_gitlab_slack_application_integration!
 
       SlackIntegration.create!(
         service_id: integration.id,

ee/spec/services/projects/slack_application_install_service_spec.rb
@@ -87,7 +87,18 @@ RSpec.describe Projects::SlackApplicationInstallService do
       }
     end
 
-    shared_context 'legacy response' do
+    shared_examples 'success response' do
+      it 'returns success result and creates all needed records' do
+        result = service.execute
+
+        expect(result).to eq(status: :success)
+        expect_slack_integration_is_created
+        expect(ChatName.count).to be_zero
+      end
+    end
+
+    shared_examples 'legacy response' do
+      let(:params) { super().without(:v2) }
       let(:exchange_url) { described_class::SLACK_EXCHANGE_TOKEN_URL_LEGACY }
       let(:redirect_url) { super().delete_suffix('?v2=true') }
       let(:response) do
@@ -100,20 +111,6 @@ RSpec.describe Projects::SlackApplicationInstallService do
           team_name: 'Team name'
         }
       end
-    end
-
-    it 'returns success result and creates all needed records' do
-      result = service.execute
-
-      expect(result).to eq(status: :success)
-      expect_slack_integration_is_created
-      expect(ChatName.count).to be_zero
-    end
-
-    context 'when the v2 param is missing in the query string' do
-      let(:params) { super().without(:v2) }
-
-      include_context 'legacy response'
 
       it 'uses the legacy endpoint and creates all needed records' do
         result = service.execute
@@ -124,32 +121,39 @@ RSpec.describe Projects::SlackApplicationInstallService do
       end
     end
 
+    it_behaves_like 'success response'
+    it_behaves_like 'legacy response'
+
+    context 'when integration record already exists' do
+      before do
+        project.create_gitlab_slack_application_integration!
+      end
+
+      it_behaves_like 'success response'
+
+      context 'when installation record already exists' do
+        before do
+          project.gitlab_slack_application_integration.create_slack_integration!(
+            team_id: 'T12345',
+            team_name: 'Team name',
+            alias: project.full_path,
+            user_id: 'U12345'
+          )
+        end
+
+        it 'returns error and does not create any records' do
+          expect { service.execute }.to raise_error(ActiveRecord::RecordInvalid)
+        end
+      end
+    end
+
     context 'when the FF :slack_app_use_v2_flow is disabled' do
       before do
         stub_feature_flags(slack_app_use_v2_flow: false)
       end
 
-      it 'still uses the v2 endpoint and creates all needed records' do
-        result = service.execute
-
-        expect(result).to eq(status: :success)
-        expect_slack_integration_is_created
-        expect(ChatName.count).to be_zero
-      end
-
-      context 'when the v2 param is missing in the query string' do
-        let(:params) { super().without(:v2) }
-
-        include_context 'legacy response'
-
-        it 'uses the legacy endpoint and creates all needed records' do
-          result = service.execute
-
-          expect(result).to eq(status: :success)
-          expect_slack_integration_is_created
-          expect_chat_name_is_created
-        end
-      end
+      it_behaves_like 'success response'
+      it_behaves_like 'legacy response'
     end
   end
 end