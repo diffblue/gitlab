c5c91fc64bc4b54317cc6ec9489ba6daa9e2b25c
Coung Ngo <cngo@gitlab.com>
2022-05-10 18:38:25 +0000

    Update feature specs for vue_issues_list feature flag
    The vue_issues_list feature flag is now default enabled in the code.
    The feature specs were updated with this feature flag enabled in
    https://gitlab.com/gitlab-org/gitlab/-/merge_requests/61123
    but it has a large diff so it is being split into several MRs
    to make reviewing easier.

---
+++ ee/spec/features/issues/filtered_search/filter_issues_by_multiple_assignees_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe 'Filter issues by multiple assignees', :js do
+  include FilteredSearchHelpers
+
+  let_it_be(:user) { create(:user) }
+  let_it_be(:user2) { create(:user) }
+  let_it_be(:project) { create(:project) }
+  let_it_be(:issue) { create(:issue, project: project, author: user, assignees: [user, user2]) }
+  let_it_be(:other_issue) { create(:issue, project: project) }
+
+  before_all do
+    project.add_maintainer(user)
+    project.add_developer(user2)
+  end
+
+  before do
+    stub_feature_flags(vue_issues_list: true)
+
+    sign_in(user)
+    visit project_issues_path(project)
+  end
+
+  it 'filters issues by multiple assignees' do
+    select_tokens 'Assignee', '=', user.username, 'Assignee', '=', user2.username, submit: true
+
+    expect_assignee_token(user.name)
+    expect_assignee_token(user2.name)
+    expect_issues_list_count(1)
+    expect_empty_search_term
+  end
+end
