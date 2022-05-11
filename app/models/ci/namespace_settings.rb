ef1317fee6f21c25d0cebbc6e379b7c42a08769c
Pedro Pombeiro <noreply@pedro.pombei.ro>
2022-05-11 08:58:23 +0200

    Fix module definition

--- app/models/ci/namespace_settings.rb
+++ app/models/ci/namespace_settings.rb
@@ -4,14 +4,16 @@
 #
 # This module is intended to encapsulate CI/CD settings-specific logic
 # and be prepended in the `Namespace` model
-module Ci::NamespaceSettings
-  # Overridden in EE::Namespace
-  def allow_stale_runner_pruning?
-    false
-  end
+module Ci
+  module NamespaceSettings
+    # Overridden in EE::Namespace
+    def allow_stale_runner_pruning?
+      false
+    end
 
-  # Overridden in EE::Namespace
-  def allow_stale_runner_pruning=(_value)
-    raise NotImplementedError
+    # Overridden in EE::Namespace
+    def allow_stale_runner_pruning=(_value)
+      raise NotImplementedError
+    end
   end
 end
--- ee/app/models/ee/ci/namespace_settings.rb
+++ ee/app/models/ee/ci/namespace_settings.rb
@@ -5,22 +5,24 @@ module EE
   #
   # This module is intended to encapsulate EE-specific model logic
   # and be prepended in the `Namespace` model
-  module Ci::NamespaceSettings
-    extend ::Gitlab::Utils::Override
+  module Ci
+    module NamespaceSettings
+      extend ::Gitlab::Utils::Override
 
-    override :allow_stale_runner_pruning?
-    def allow_stale_runner_pruning?
-      return false unless ci_cd_settings
+      override :allow_stale_runner_pruning?
+      def allow_stale_runner_pruning?
+        return false unless ci_cd_settings
 
-      ci_cd_settings.allow_stale_runner_pruning?
-    end
+        ci_cd_settings.allow_stale_runner_pruning?
+      end
 
-    override :allow_stale_runner_pruning=
-    def allow_stale_runner_pruning=(value)
-      return if ci_cd_settings.blank? && !value
+      override :allow_stale_runner_pruning=
+      def allow_stale_runner_pruning=(value)
+        return if ci_cd_settings.blank? && !value
 
-      ci_cd_settings ||= ::NamespaceCiCdSetting.find_or_initialize_by(namespace_id: id)
-      ci_cd_settings.update(allow_stale_runner_pruning: value)
+        ci_cd_settings ||= ::NamespaceCiCdSetting.find_or_initialize_by(namespace_id: id)
+        ci_cd_settings.update(allow_stale_runner_pruning: value)
+      end
     end
   end
 end
