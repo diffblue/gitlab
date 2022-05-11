d2c1b327bafe18f840d5cfd38ed0a237aa97d29a
Alexander Turinske <aturinske@gitlab.com>
2022-05-11 08:21:01 +0100

    Remove network policies vuex store
    - it is no longer used with the removal of network policies

--- ee/app/assets/javascripts/threat_monitoring/store/index.js
+++ ee/app/assets/javascripts/threat_monitoring/store/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import networkPolicies from './modules/network_policies';
 import threatMonitoring from './modules/threat_monitoring';
 import scanResultPolicies from './modules/scan_result_policies';
 
@@ -10,7 +9,6 @@ export default () =>
   new Vuex.Store({
     modules: {
       threatMonitoring: threatMonitoring(),
-      networkPolicies: networkPolicies(),
       scanResultPolicies: scanResultPolicies(),
     },
   });
--- ee/app/assets/javascripts/threat_monitoring/store/modules/network_policies/state.js
+++
@@ -1,7 +0,0 @@
-export default () => ({
-  policiesEndpoint: '',
-  isUpdatingPolicy: false,
-  errorUpdatingPolicy: false,
-  isRemovingPolicy: false,
-  errorRemovingPolicy: false,
-});
