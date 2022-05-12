d9810c94d8fe36c690d7f045df8e50c92dbdab2f
Max Woolf <mwoolf@gitlab.com>
2022-05-12 09:13:48 +0000

    Add group push rule changes to audit log
    When a group owner makes changes to
    the group push rules, we now audit
    those changes at the group level.
    
    EE: true
    Changelog: added

--- ee/spec/services/push_rules/create_or_update_service_spec.rb
+++ ee/spec/services/push_rules/create_or_update_service_spec.rb
@@ -3,12 +3,12 @@
 require 'spec_helper'
 
 RSpec.describe PushRules::CreateOrUpdateService, '#execute' do
-  let_it_be_with_reload(:project) { create(:project) }
+  let_it_be_with_reload(:container) { create(:project) }
   let_it_be(:user) { create(:user) }
 
   let(:params) { { max_file_size: 28 } }
 
-  subject { described_class.new(container: project, current_user: user, params: params) }
+  subject { described_class.new(container: container, current_user: user, params: params) }
 
   shared_examples 'a failed update' do
     let(:params) { { max_file_size: -28 } }
@@ -18,12 +18,12 @@ RSpec.describe PushRules::CreateOrUpdateService, '#execute' do
 
       expect(response).to be_error
       expect(response.message).to eq('Max file size must be greater than or equal to 0')
-      expect(response.payload).to match(push_rule: project.push_rule)
+      expect(response.payload).to match(push_rule: container.push_rule)
     end
   end
 
   context 'with existing push rule' do
-    let_it_be(:push_rule) { create(:push_rule, project: project) }
+    let_it_be(:push_rule) { create(:push_rule, project: container) }
 
     it 'updates existing push rule' do
       expect { subject.execute }
@@ -38,6 +38,14 @@ RSpec.describe PushRules::CreateOrUpdateService, '#execute' do
       expect(response.payload).to match(push_rule: push_rule)
     end
 
+    context 'when container is a group' do
+      let_it_be(:container) { create(:group) }
+
+      it 'audits the changes' do
+        expect { subject.execute }.to change { AuditEvent.count }.by(1)
+      end
+    end
+
     it_behaves_like 'a failed update'
   end
 
@@ -45,14 +53,14 @@ RSpec.describe PushRules::CreateOrUpdateService, '#execute' do
     it 'creates a new push rule', :aggregate_failures do
       expect { subject.execute }.to change { PushRule.count }.by(1)
 
-      expect(project.push_rule.max_file_size).to eq(28)
+      expect(container.push_rule.max_file_size).to eq(28)
     end
 
     it 'responds with a successful service response', :aggregate_failures do
       response = subject.execute
 
       expect(response).to be_success
-      expect(response.payload).to match(push_rule: project.push_rule)
+      expect(response.payload).to match(push_rule: container.push_rule)
     end
 
     it_behaves_like 'a failed update'
