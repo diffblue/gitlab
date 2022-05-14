f4f2034849cc7e10836b7ca72f6ade092949ba53
Timo Furrer <tuxtimo@gmail.com>
2022-05-14 09:56:57 +0200

    Implement `/metadata` REST API endpoint
    This change set implements a new `/metadata` REST API endpoint by
    leveraging the already existing GraphQL `Query.metadata`.
    Therefore, this endpoint is inherently *on par* with the GraphQL API.
    
    This new endpoint somewhat makes the already existing `/version`
    REST API endpoint obsolete. The reason for not just extending the
    `/version` endpoint with the missing metadata is because the GraphQL API
    also uses the *metadata* wording for that endpoint. I think the more
    consistency the better here.
    
    A follow up may (???) therefore properly deprecate the
    `/version` REST API endpoint in favor of the new `/metadata` endpoint.
    
    Changelog: added
    MR: https://gitlab.com/gitlab-org/gitlab/-/merge_requests/87622
    Issue: https://gitlab.com/gitlab-org/gitlab/-/issues/357032
    Epic: https://gitlab.com/groups/gitlab-org/-/epics/7204

--- lib/api/api.rb
+++ lib/api/api.rb
@@ -241,6 +241,7 @@ module API
       mount ::API::MergeRequestApprovals
       mount ::API::MergeRequestDiffs
       mount ::API::MergeRequests
+      mount ::API::Metadata
       mount ::API::Metrics::Dashboard::Annotations
       mount ::API::Metrics::UserStarredDashboards
       mount ::API::Namespaces
