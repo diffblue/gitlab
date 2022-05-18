2ae849439c5d429e5f12409043639179559b39bd
Brett Walker <bwalker@gitlab.com>
2022-05-18 14:05:38 -0500

    IssuesConnectionExtension should be FieldExtension
    not a ConnectionExtension

--- lib/gitlab/graphql/board/issues_connection_extension.rb
+++ lib/gitlab/graphql/board/issues_connection_extension.rb
@@ -2,16 +2,7 @@
 module Gitlab
   module Graphql
     module Board
-      class IssuesConnectionExtension < GraphQL::Schema::Field::ConnectionExtension
-        DEFAULT_CONNECTION_ARGS = %w[after before first last].freeze
-
-        def apply
-          # GraphQL::Schema::Field::ConnectionExtension redefines the
-          # :after, :before, :first, and :last arguments. This causes a
-          # DuplicateNamesError for graphql gem > 1.13.
-          super unless (field.arguments.keys & DEFAULT_CONNECTION_ARGS).any?
-        end
-
+      class IssuesConnectionExtension < GraphQL::Schema::FieldExtension
         def after_resolve(value:, object:, context:, **rest)
           ::Boards::Issues::ListService
             .initialize_relative_positions(object.list.board, context[:current_user], value.nodes)
