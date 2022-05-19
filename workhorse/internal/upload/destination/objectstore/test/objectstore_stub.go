diffblue/gitlab

commit 7efb5f0a7a7b7c1725e52abfb2c5f525346b0d16

Refactor Preparer and SaveFileOpts handling
This commit ensures that a Preparer is always passed into an upload
handler and eliminates the possibility of a nil handler.

SaveFileOpts is now passed directly to downstream handlers to ensure a
cleaner interface separation between the Preparer and the handlers.

This is split out from the work for
https://gitlab.com/gitlab-org/gitlab-workhorse/-/merge_requests/466.

internal/upload/uploads_test.go
@@ -56,7 +56,12 @@ func TestUploadTempPathRequirement(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	HandleFileUploads(response, request, nilHandler, &api.Response{}, nil)
+	apiResponse := &api.Response{}
+	preparer := &DefaultPreparer{}
+	opts, _, err := preparer.Prepare(apiResponse)
+	require.NoError(t, err)
+
+	HandleFileUploads(response, request, nilHandler, apiResponse, &testFormProcessor{}, opts)
 	testhelper.AssertResponseCode(t, response, 500)
 }
 
@@ -91,7 +96,13 @@ func TestUploadHandlerForwardingRawData(t *testing.T) {
 	response := httptest.NewRecorder()
 
 	handler := newProxy(ts.URL)
-	HandleFileUploads(response, httpRequest, handler, &api.Response{TempPath: tempPath}, nil)
+	apiResponse := &api.Response{TempPath: tempPath}
+	preparer := &DefaultPreparer{}
+	opts, _, err := preparer.Prepare(apiResponse)
+	require.NoError(t, err)
+
+	HandleFileUploads(response, httpRequest, handler, apiResponse, nil, opts)
+
 	testhelper.AssertResponseCode(t, response, 202)
 	if response.Body.String() != "RESPONSE" {
 		t.Fatal("Expected RESPONSE in response body")
@@ -191,7 +202,13 @@ func TestUploadHandlerRewritingMultiPartData(t *testing.T) {
 	response := httptest.NewRecorder()
 
 	handler := newProxy(ts.URL)
-	HandleFileUploads(response, httpRequest, handler, &api.Response{TempPath: tempPath}, &testFormProcessor{})
+
+	apiResponse := &api.Response{TempPath: tempPath}
+	preparer := &DefaultPreparer{}
+	opts, _, err := preparer.Prepare(apiResponse)
+	require.NoError(t, err)
+
+	HandleFileUploads(response, httpRequest, handler, apiResponse, &testFormProcessor{}, opts)
 	testhelper.AssertResponseCode(t, response, 202)
 
 	cancel() // this will trigger an async cleanup
@@ -263,7 +280,12 @@ func TestUploadHandlerDetectingInjectedMultiPartData(t *testing.T) {
 			response := httptest.NewRecorder()
 
 			handler := newProxy(ts.URL)
-			HandleFileUploads(response, httpRequest, handler, &api.Response{TempPath: tempPath}, &testFormProcessor{})
+			apiResponse := &api.Response{TempPath: tempPath}
+			preparer := &DefaultPreparer{}
+			opts, _, err := preparer.Prepare(apiResponse)
+			require.NoError(t, err)
+
+			HandleFileUploads(response, httpRequest, handler, apiResponse, &testFormProcessor{}, opts)
 			testhelper.AssertResponseCode(t, response, test.response)
 
 			cancel() // this will trigger an async cleanup
@@ -292,7 +314,13 @@ func TestUploadProcessingField(t *testing.T) {
 	httpRequest.Header.Set("Content-Type", writer.FormDataContentType())
 
 	response := httptest.NewRecorder()
-	HandleFileUploads(response, httpRequest, nilHandler, &api.Response{TempPath: tempPath}, &testFormProcessor{})
+	apiResponse := &api.Response{TempPath: tempPath}
+	preparer := &DefaultPreparer{}
+	opts, _, err := preparer.Prepare(apiResponse)
+	require.NoError(t, err)
+
+	HandleFileUploads(response, httpRequest, nilHandler, apiResponse, &testFormProcessor{}, opts)
+
 	testhelper.AssertResponseCode(t, response, 500)
 }
 
@@ -347,7 +375,13 @@ func TestUploadProcessingFile(t *testing.T) {
 			httpRequest.Header.Set("Content-Type", writer.FormDataContentType())
 
 			response := httptest.NewRecorder()
-			HandleFileUploads(response, httpRequest, nilHandler, &test.preauth, &testFormProcessor{})
+			apiResponse := &api.Response{TempPath: tempPath}
+			preparer := &DefaultPreparer{}
+			opts, _, err := preparer.Prepare(apiResponse)
+			require.NoError(t, err)
+
+			HandleFileUploads(response, httpRequest, nilHandler, apiResponse, &testFormProcessor{}, opts)
+
 			testhelper.AssertResponseCode(t, response, 200)
 		})
 	}
@@ -390,7 +424,12 @@ func TestInvalidFileNames(t *testing.T) {
 		httpRequest.Header.Set("Content-Type", writer.FormDataContentType())
 
 		response := httptest.NewRecorder()
-		HandleFileUploads(response, httpRequest, nilHandler, &api.Response{TempPath: tempPath}, &SavedFileTracker{Request: httpRequest})
+		apiResponse := &api.Response{TempPath: tempPath}
+		preparer := &DefaultPreparer{}
+		opts, _, err := preparer.Prepare(apiResponse)
+		require.NoError(t, err)
+
+		HandleFileUploads(response, httpRequest, nilHandler, apiResponse, &SavedFileTracker{Request: httpRequest}, opts)
 		testhelper.AssertResponseCode(t, response, testCase.code)
 	}
 }
@@ -441,7 +480,12 @@ func TestUploadHandlerRemovingExif(t *testing.T) {
 	response := httptest.NewRecorder()
 
 	handler := newProxy(ts.URL)
-	HandleFileUploads(response, httpRequest, handler, &api.Response{TempPath: tempPath}, &testFormProcessor{})
+	apiResponse := &api.Response{TempPath: tempPath}
+	preparer := &DefaultPreparer{}
+	opts, _, err := preparer.Prepare(apiResponse)
+	require.NoError(t, err)
+
+	HandleFileUploads(response, httpRequest, handler, apiResponse, &testFormProcessor{}, opts)
 	testhelper.AssertResponseCode(t, response, 200)
 }
 
@@ -478,7 +522,12 @@ func TestUploadHandlerRemovingInvalidExif(t *testing.T) {
 	response := httptest.NewRecorder()
 
 	handler := newProxy(ts.URL)
-	HandleFileUploads(response, httpRequest, handler, &api.Response{TempPath: tempPath}, &testFormProcessor{})
+	apiResponse := &api.Response{TempPath: tempPath}
+	preparer := &DefaultPreparer{}
+	opts, _, err := preparer.Prepare(apiResponse)
+	require.NoError(t, err)
+
+	HandleFileUploads(response, httpRequest, handler, apiResponse, &testFormProcessor{}, opts)
 	testhelper.AssertResponseCode(t, response, 422)
 }
 

internal/artifacts/artifacts_upload.go
@@ -141,10 +141,15 @@ func (a *artifactsUploadProcessor) Name() string {
 	return "artifacts"
 }
 
-func UploadArtifacts(myAPI *api.API, h http.Handler) http.Handler {
+func UploadArtifacts(myAPI *api.API, h http.Handler, p upload.Preparer) http.Handler {
 	return myAPI.PreAuthorizeHandler(func(w http.ResponseWriter, r *http.Request, a *api.Response) {
-		mg := &artifactsUploadProcessor{opts: filestore.GetOpts(a), SavedFileTracker: upload.SavedFileTracker{Request: r}}
+		opts, _, err := p.Prepare(a)
+		if err != nil {
+			helper.Fail500(w, r, fmt.Errorf("UploadArtifacts: error preparing file storage options"))
+			return
+		}
 
-		upload.HandleFileUploads(w, r, h, a, mg)
+		mg := &artifactsUploadProcessor{opts: opts, SavedFileTracker: upload.SavedFileTracker{Request: r}}
+		upload.HandleFileUploads(w, r, h, a, mg, opts)
 	}, "/authorize")
 }

internal/artifacts/artifacts_upload_test.go
@@ -154,7 +154,7 @@ func testUploadArtifacts(t *testing.T, contentType, url string, body io.Reader) 
 	testhelper.ConfigureSecret()
 	apiClient := api.NewAPI(parsedURL, "123", roundTripper)
 	proxyClient := proxy.NewProxy(parsedURL, "123", roundTripper)
-	UploadArtifacts(apiClient, proxyClient).ServeHTTP(response, httpRequest)
+	UploadArtifacts(apiClient, proxyClient, &upload.DefaultPreparer{}).ServeHTTP(response, httpRequest)
 	return response
 }
 

internal/lfs/lfs.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 
 	"gitlab.com/gitlab-org/gitlab-workhorse/internal/api"
+	"gitlab.com/gitlab-org/gitlab-workhorse/internal/config"
 	"gitlab.com/gitlab-org/gitlab-workhorse/internal/filestore"
 	"gitlab.com/gitlab-org/gitlab-workhorse/internal/upload"
 )
@@ -32,6 +33,10 @@ func (l *object) Verify(fh *filestore.FileHandler) error {
 
 type uploadPreparer struct{}
 
+func NewLfsUploadPreparer(c config.Config) upload.Preparer {
+	return &uploadPreparer{}
+}
+
 func (l *uploadPreparer) Prepare(a *api.Response) (*filestore.SaveFileOpts, upload.Verifier, error) {
 	opts := filestore.GetOpts(a)
 	opts.TempFilePrefix = a.LfsOid
@@ -39,6 +44,6 @@ func (l *uploadPreparer) Prepare(a *api.Response) (*filestore.SaveFileOpts, uplo
 	return opts, &object{oid: a.LfsOid, size: a.LfsSize}, nil
 }
 
-func PutStore(a *api.API, h http.Handler) http.Handler {
-	return upload.BodyUploader(a, h, &uploadPreparer{})
+func PutStore(a *api.API, h http.Handler, p upload.Preparer) http.Handler {
+	return upload.BodyUploader(a, h, p)
 }

internal/upload/accelerate.go
@@ -1,11 +1,13 @@
 package upload
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/dgrijalva/jwt-go"
 
 	"gitlab.com/gitlab-org/gitlab-workhorse/internal/api"
+	"gitlab.com/gitlab-org/gitlab-workhorse/internal/helper"
 )
 
 const RewrittenFieldsHeader = "Gitlab-Workhorse-Multipart-Fields"
@@ -15,9 +17,16 @@ type MultipartClaims struct {
 	jwt.StandardClaims
 }
 
-func Accelerate(rails PreAuthorizer, h http.Handler) http.Handler {
+func Accelerate(rails PreAuthorizer, h http.Handler, p Preparer) http.Handler {
 	return rails.PreAuthorizeHandler(func(w http.ResponseWriter, r *http.Request, a *api.Response) {
 		s := &SavedFileTracker{Request: r}
-		HandleFileUploads(w, r, h, a, s)
+
+		opts, _, err := p.Prepare(a)
+		if err != nil {
+			helper.Fail500(w, r, fmt.Errorf("Accelerate: error preparing file storage options"))
+			return
+		}
+
+		HandleFileUploads(w, r, h, a, s, opts)
 	}, "/authorize")
 }

internal/upload/body_uploader.go
@@ -29,9 +29,9 @@ type Preparer interface {
 	Prepare(a *api.Response) (*filestore.SaveFileOpts, Verifier, error)
 }
 
-type defaultPreparer struct{}
+type DefaultPreparer struct{}
 
-func (s *defaultPreparer) Prepare(a *api.Response) (*filestore.SaveFileOpts, Verifier, error) {
+func (s *DefaultPreparer) Prepare(a *api.Response) (*filestore.SaveFileOpts, Verifier, error) {
 	return filestore.GetOpts(a), nil, nil
 }
 
@@ -39,10 +39,6 @@ func (s *defaultPreparer) Prepare(a *api.Response) (*filestore.SaveFileOpts, Ver
 // uploading it.
 // Providing an Preparer allows to customize the upload process
 func BodyUploader(rails PreAuthorizer, h http.Handler, p Preparer) http.Handler {
-	if p == nil {
-		p = &defaultPreparer{}
-	}
-
 	return rails.PreAuthorizeHandler(func(w http.ResponseWriter, r *http.Request, a *api.Response) {
 		opts, verifier, err := p.Prepare(a)
 		if err != nil {

internal/upload/body_uploader_test.go
@@ -29,7 +29,7 @@ func TestBodyUploader(t *testing.T) {
 
 	body := strings.NewReader(fileContent)
 
-	resp := testUpload(&rails{}, nil, echoProxy(t, fileLen), body)
+	resp := testUpload(&rails{}, &alwaysLocalPreparer{}, echoProxy(t, fileLen), body)
 	require.Equal(t, http.StatusOK, resp.StatusCode)
 
 	uploadEcho, err := ioutil.ReadAll(resp.Body)
@@ -63,7 +63,7 @@ func TestBodyUploaderCustomVerifier(t *testing.T) {
 }
 
 func TestBodyUploaderAuthorizationFailure(t *testing.T) {
-	testNoProxyInvocation(t, http.StatusUnauthorized, &rails{unauthorized: true}, nil)
+	testNoProxyInvocation(t, http.StatusUnauthorized, &rails{unauthorized: true}, &alwaysLocalPreparer{})
 }
 
 func TestBodyUploaderErrors(t *testing.T) {

internal/upload/rewrite.go
@@ -61,7 +61,7 @@ func init() {
 	prometheus.MustRegister(multipartFiles)
 }
 
-func rewriteFormFilesFromMultipart(r *http.Request, writer *multipart.Writer, preauth *api.Response, filter MultipartFormProcessor) error {
+func rewriteFormFilesFromMultipart(r *http.Request, writer *multipart.Writer, preauth *api.Response, filter MultipartFormProcessor, opts *filestore.SaveFileOpts) error {
 	// Create multipart reader
 	reader, err := r.MultipartReader()
 	if err != nil {
@@ -100,7 +100,7 @@ func rewriteFormFilesFromMultipart(r *http.Request, writer *multipart.Writer, pr
 		}
 
 		if p.FileName() != "" {
-			err = rew.handleFilePart(r.Context(), name, p)
+			err = rew.handleFilePart(r.Context(), name, p, opts)
 		} else {
 			err = rew.copyPart(r.Context(), name, p)
 		}
@@ -113,7 +113,7 @@ func rewriteFormFilesFromMultipart(r *http.Request, writer *multipart.Writer, pr
 	return nil
 }
 
-func (rew *rewriter) handleFilePart(ctx context.Context, name string, p *multipart.Part) error {
+func (rew *rewriter) handleFilePart(ctx context.Context, name string, p *multipart.Part, opts *filestore.SaveFileOpts) error {
 	multipartFiles.WithLabelValues(rew.filter.Name()).Inc()
 
 	filename := p.FileName()
@@ -122,7 +122,6 @@ func (rew *rewriter) handleFilePart(ctx context.Context, name string, p *multipa
 		return fmt.Errorf("illegal filename: %q", filename)
 	}
 
-	opts := filestore.GetOpts(rew.preauth)
 	opts.TempFilePrefix = filename
 
 	var inputReader io.Reader

commit 65396f909a3a7e6bfb2f5094f7017bc52bc19aa7

Workhorse: fix TestUploadProcessingFile
This test was broken by 7efb5f0a7a7b7c1725e52abfb2c5f525346b0d16.
While we're here, make sure that the uploaded file actually gets
persisted.

Changelog: other

workhorse/internal/upload/destination/objectstore/test/objectstore_stub.go
@@ -22,7 +22,8 @@ type partsEtagMap map[int]string
 // Instead of storing objects it will just save md5sum.
 type ObjectstoreStub struct {
 	// bucket contains md5sum of uploaded objects
-	bucket map[string]string
+	bucket   map[string]string
+	contents map[string][]byte
 	// overwriteMD5 contains overwrites for md5sum that should be return instead of the regular hash
 	overwriteMD5 map[string]string
 	// multipart is a map of MultipartUploads
@@ -48,6 +49,7 @@ func StartObjectStoreWithCustomMD5(md5Hashes map[string]string) (*ObjectstoreStu
 		multipart:    make(map[string]partsEtagMap),
 		overwriteMD5: make(map[string]string),
 		headers:      make(map[string]*http.Header),
+		contents:     make(map[string][]byte),
 	}
 
 	for k, v := range md5Hashes {
@@ -80,6 +82,15 @@ func (o *ObjectstoreStub) GetObjectMD5(path string) string {
 	defer o.m.Unlock()
 
 	return o.bucket[path]
+}
+
+// GetObject returns the contents of the uploaded object. The caller must
+// not modify the byte slice.
+func (o *ObjectstoreStub) GetObject(path string) []byte {
+	o.m.Lock()
+	defer o.m.Unlock()
+
+	return o.contents[path]
 }
 
 // GetHeader returns a given HTTP header of the object uploaded to the path
@@ -154,11 +165,11 @@ func (o *ObjectstoreStub) putObject(w http.ResponseWriter, r *http.Request) {
 
 	etag, overwritten := o.overwriteMD5[objectPath]
 	if !overwritten {
+		buf, _ := io.ReadAll(r.Body)
+		o.contents[objectPath] = buf
 		hasher := md5.New()
-		io.Copy(hasher, r.Body)
-
-		checksum := hasher.Sum(nil)
-		etag = hex.EncodeToString(checksum)
+		hasher.Write(buf)
+		etag = hex.EncodeToString(hasher.Sum(nil))
 	}
 
 	o.headers[objectPath] = &r.Header

workhorse/internal/upload/uploads_test.go
@@ -12,6 +12,7 @@ import (
 	"net/http/httptest"
 	"net/textproto"
 	"os"
+	"path"
 	"regexp"
 	"strconv"
 	"strings"
@@ -286,11 +287,12 @@ func TestUploadingMultipleFiles(t *testing.T) {
 }
 
 func TestUploadProcessingFile(t *testing.T) {
+	testhelper.ConfigureSecret()
 	tempPath, err := ioutil.TempDir("", "uploads")
 	require.NoError(t, err)
 	defer os.RemoveAll(tempPath)
 
-	_, testServer := test.StartObjectStore()
+	objectStore, testServer := test.StartObjectStore()
 	defer testServer.Close()
 
 	storeUrl := testServer.URL + test.ObjectPath
@@ -298,21 +300,24 @@ func TestUploadProcessingFile(t *testing.T) {
 	tests := []struct {
 		name    string
 		preauth *api.Response
+		content func(t *testing.T) []byte
 	}{
 		{
 			name:    "FileStore Upload",
 			preauth: &api.Response{TempPath: tempPath},
+			content: func(t *testing.T) []byte {
+				entries, err := os.ReadDir(tempPath)
+				require.NoError(t, err)
+				require.Len(t, entries, 1)
+				content, err := os.ReadFile(path.Join(tempPath, entries[0].Name()))
+				require.NoError(t, err)
+				return content
+			},
 		},
 		{
 			name:    "ObjectStore Upload",
-			preauth: &api.Response{RemoteObject: api.RemoteObject{StoreURL: storeUrl}},
-		},
-		{
-			name: "ObjectStore and FileStore Upload",
-			preauth: &api.Response{
-				TempPath:     tempPath,
-				RemoteObject: api.RemoteObject{StoreURL: storeUrl},
-			},
+			preauth: &api.Response{RemoteObject: api.RemoteObject{StoreURL: storeUrl, ID: "123"}},
+			content: func(*testing.T) []byte { return objectStore.GetObject(test.ObjectPath) },
 		},
 	}
 
@@ -330,17 +335,16 @@ func TestUploadProcessingFile(t *testing.T) {
 			httpRequest.Header.Set("Content-Type", writer.FormDataContentType())
 
 			response := httptest.NewRecorder()
-			apiResponse := &api.Response{TempPath: tempPath}
 			preparer := &DefaultPreparer{}
-			opts, err := preparer.Prepare(apiResponse)
+			opts, err := preparer.Prepare(test.preauth)
 			require.NoError(t, err)
 
-			interceptMultipartFiles(response, httpRequest, nilHandler, apiResponse, &testFormProcessor{}, opts)
+			interceptMultipartFiles(response, httpRequest, nilHandler, test.preauth, &testFormProcessor{}, opts)
 
 			require.Equal(t, 200, response.Code)
+			require.Equal(t, "test", string(test.content(t)))
 		})
 	}
-
 }
 
 func TestInvalidFileNames(t *testing.T) {