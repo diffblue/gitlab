beeb662dc91d45fa6a8c4c31327a4983bee8a3be
João Cunha <j.a.cunha@gmail.com>
2022-05-18 23:30:21 -0300

    Create ClusterEnabledGrant model and populates table
    This new table is going to be used to temporary permit a set of users to
    use the certificate-based-clusters feature, which allowing us to remove
    for the majority of other users. On our first attemp we tried to do this
    with FF only on the namespace leve. But the spectrum of users that need
    this feature enabled for longer is too big. So big that toggling the FF
    for all these actors is something that our FF framework wasn't ready to
    handle. So checking the enabled users directly from a table will be much
    simpler, faster and easy on memory consumption.
    
    Changelog: other

--- db/post_migrate/20220510175036_test_batch_migration_for_cr.rb
+++ db/post_migrate/20220510175036_test_batch_migration_for_cr.rb
@@ -4,54 +4,57 @@
 # for more information on how to write migrations for GitLab.
 
 class TestBatchMigrationForCr < Gitlab::Database::Migration[2.0]
-  # When using the methods "add_concurrent_index" or "remove_concurrent_index"
-  # you must disable the use of transactions
-  # as these methods can not run in an existing transaction.
-  # When using "add_concurrent_index" or "remove_concurrent_index" methods make sure
-  # that either of them is the _only_ method called in the migration,
-  # any other changes should go in a separate migration.
-  # This ensures that upon failure _only_ the index creation or removing fails
-  # and can be retried or reverted easily.
-  #
-  # To disable transactions uncomment the following line and remove these
-  # comments:
-  disable_ddl_transaction!
-  #
-  # Configure the `gitlab_schema` to perform data manipulation (DML).
-  # Visit: https://docs.gitlab.com/ee/development/database/migrations_for_multiple_databases.html
   restrict_gitlab_migration gitlab_schema: :gitlab_main
 
+  disable_ddl_transaction!
+
+  class ClusterEnabledGrant < MigrationRecord
+    self.table_name = 'cluster_enabled_grants'
+  end
+
   def up
-    root_ancestor_set = Set[]
+    Group
+      .select(:id)
+      .where(id:
+          Group.joins(:cluster_groups)
+               .select('DISTINCT(traversal_ids[1])')
+            )
+      .each_batch(of: 1000) do |batch|
+      values = batch.map do |namespace|
+        "(#{namespace.id}, NOW())"
+      end.join(',')
 
-    Group.where(id:
-                  Group.joins(:cluster_groups)
-                       .select('DISTINCT(traversal_ids[1])')
-               ).find_each(batch_size: 1000) do |namespace|
-      root_ancestor_set.add(namespace)
+      bulk_insert_query = <<-SQL
+        INSERT INTO cluster_enabled_grants (namespace_id)
+        VALUES #{values}
+        ON CONFLICT (namespace_id) DO NOTHING;
+      SQL
+
+      connection.execute(bulk_insert_query)
     end
 
-    Namespace.where(id:
-                      Namespace.joins(projects: :cluster_project)
-                               .select('DISTINCT(traversal_ids[1])')
-                   ).find_each(batch_size: 1000) do |namespace|
-      root_ancestor_set.add(namespace)
-    end
+    Namespace
+      .select(:id)
+      .where(id:
+        Namespace.joins(projects: :cluster_project)
+                 .select('DISTINCT(traversal_ids[1])')
+            )
+      .each_batch(of: 1000) do |batch|
+      values = batch.map do |namespace|
+        "(#{namespace.id}, NOW())"
+      end.join(',')
 
-    # Group.joins(:cluster_groups).distinct.find_each(batch_size: 1000) do |namespace|
-    #   root_ancestor_set.add(namespace.root_ancestor)
-    # end
-    #
-    # Namespace.joins(projects: :cluster_project).distinct.find_each(batch_size: 1000) do |namespace|
-    #   root_ancestor_set.add(namespace.root_ancestor)
-    # end
+      bulk_insert_query = <<-SQL
+        INSERT INTO cluster_enabled_grants (namespace_id, created_at)
+        VALUES #{values}
+        ON CONFLICT (namespace_id) DO NOTHING;
+      SQL
 
-    root_ancestor_set.each do |root_ancestor|
-      Feature.enable(:certificate_based_clusters, root_ancestor)
+      connection.execute(bulk_insert_query)
     end
   end
 
   def down
-    #  no op
+    ClusterEnabledGrant.delete_all
   end
 end
