651ba2f1df0b22999f1fd5656c878f4902f1ec6f
Stan Hu <stanhu@gmail.com>
2022-05-19 20:57:59 -0700

    Relax Elasticsearch client spec for Ruby versions

--- ee/spec/lib/gitlab/elastic/client_spec.rb
+++ ee/spec/lib/gitlab/elastic/client_spec.rb
@@ -103,16 +103,14 @@ RSpec.describe Gitlab::Elastic::Client do
           stub_request(:get, 'http://example-elastic:9200/foo/_doc/1')
             .with(
               headers: {
-                'Authorization'         => 'AWS4-HMAC-SHA256 Credential=0/20170303/us-east-1/es/aws4_request, SignedHeaders=content-type;host;user-agent;x-amz-content-sha256;x-amz-date;x-elastic-client-meta;x-opaque-id, Signature=8af14ba710c4f06ae2d5f217a33e8819f2f412970fa030a875fa3dc33d9e15ac',
+                'Authorization'         => /^AWS4-HMAC-SHA256 Credential=0/,
                 'Content-Type'          => 'application/json',
                 'Expect'                => '',
                 'Host'                  => 'example-elastic:9200',
-
-                # User-Agent is a part of SignedHeaders; if this changes, the signature will change
-                'User-Agent'            => 'elasticsearch-ruby/7.13.3 (RUBY_VERSION: 2.7.5; linux x86_64; Faraday v1.10.0)',
+                'User-Agent'            => /^elasticsearch-ruby/,
                 'X-Amz-Content-Sha256'  => 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855',
                 'X-Amz-Date'            => '20170303T133952Z',
-                'X-Elastic-Client-Meta' => 'es=7.13.3,rb=2.7.5,t=7.13.3,fd=1.10.0,ty=1.4.0',
+                'X-Elastic-Client-Meta' => "es=7.13.3,rb=#{RUBY_VERSION},t=7.13.3,fd=1.10.0,ty=1.4.0",
                 'X-Opaque-Id'           => 'new-correlation-id'
               }
             ).to_return(status: 200, body: [:fake_response])
