749b77fa5cd2a75aed5d145f3e268fc9bbc58183
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2022-05-23 16:52:28 -0300

    Patch ActiveRecord::Generators::Migration#configured_migrated_path
    We can set the migrations_paths in the database configurations
    to tell Rails where the migrations for the additional database
    will live when using the --database flag. We use this setting
    also to include the Geo post-deployment migrations path. The
    problem is that the Rails migration generator joins the elements
    within the array into one path while creating the migration file
    which breaks the regular and post-migration generators for the
    Geo database.
    
    This modifies ActiveRecord::Generators::Migration#configured_migrated_path
    to exclude any post-migration path, and always return only one
    value while creating a migration file.
    
    Changelog: fixed
    EE: true

--- ee/spec/lib/gitlab/patch/geo_database_tasks_spec.rb
+++ ee/spec/lib/gitlab/patch/geo_database_tasks_spec.rb
@@ -3,44 +3,130 @@
 require 'spec_helper'
 
 RSpec.describe Gitlab::Patch::GeoDatabaseTasks do
-  subject do
-    Class.new do
-      prepend Gitlab::Patch::GeoDatabaseTasks
+  describe Gitlab::Patch::GeoDatabaseTasks::ActiveRecordDatabaseTasksDumpFilename do
+    subject do
+      Class.new do
+        prepend Gitlab::Patch::GeoDatabaseTasks::ActiveRecordDatabaseTasksDumpFilename
 
-      def dump_filename(db_config_name, format = ApplicationRecord.schema_format)
-        'foo.sql'
+        def dump_filename(db_config_name, format = ApplicationRecord.schema_format)
+          'foo.sql'
+        end
+
+        def cache_dump_filename(db_config_name, format = ApplicationRecord.schema_format)
+          'bar.yml'
+        end
+      end.new
+    end
+
+    describe '#dump_filename' do
+      context 'with geo database config name' do
+        it 'returns the path for the structure.sql file in the Geo database dir' do
+          expect(subject.dump_filename(:geo)).to eq Rails.root.join('ee/db/geo/structure.sql').to_s
+        end
       end
 
-      def cache_dump_filename(db_config_name, format = ApplicationRecord.schema_format)
-        'bar.yml'
+      context 'with other database config name' do
+        it 'calls super' do
+          expect(subject.dump_filename(:main)).to eq 'foo.sql'
+        end
       end
-    end.new
+    end
+
+    describe '#cache_dump_filename' do
+      context 'with geo database config name' do
+        it 'returns the path for the schema_cache file in the Geo database dir' do
+          expect(subject.cache_dump_filename(:geo)).to eq Rails.root.join('ee/db/geo/schema_cache.yml').to_s
+        end
+      end
+
+      context 'with other database config name' do
+        it 'calls super' do
+          expect(subject.cache_dump_filename(:main)).to eq 'bar.yml'
+        end
+      end
+    end
   end
 
-  describe '#dump_filename' do
-    context 'with geo database config name' do
-      it 'returns the path for the structure.sql file in the Geo database dir' do
-        expect(subject.dump_filename(:geo)).to eq Rails.root.join('ee/db/geo/structure.sql').to_s
-      end
-    end
+  describe Gitlab::Patch::GeoDatabaseTasks::ActiveRecordMigrationConfiguredMigratePath do
+    describe '#configured_migrate_path' do
+      context 'when super returns nil' do
+        subject do
+          Class.new do
+            prepend Gitlab::Patch::GeoDatabaseTasks::ActiveRecordMigrationConfiguredMigratePath
 
-    context 'with other database config name' do
-      it 'calls super' do
-        expect(subject.dump_filename(:main)).to eq 'foo.sql'
-      end
-    end
-  end
+            def configured_migrate_path
+              nil
+            end
+          end.new
+        end
 
-  describe '#cache_dump_filename' do
-    context 'with geo database config name' do
-      it 'returns the path for the schema_cache file in the Geo database dir' do
-        expect(subject.cache_dump_filename(:geo)).to eq Rails.root.join('ee/db/geo/schema_cache.yml').to_s
+        it 'returns nil' do
+          expect(subject.configured_migrate_path).to be_nil
+        end
       end
-    end
 
-    context 'with other database config name' do
-      it 'calls super' do
-        expect(subject.cache_dump_filename(:main)).to eq 'bar.yml'
+      context 'when super returns only one regular migration path' do
+        subject do
+          Class.new do
+            prepend Gitlab::Patch::GeoDatabaseTasks::ActiveRecordMigrationConfiguredMigratePath
+
+            def configured_migrate_path
+              'ee/db/geo/migrate'
+            end
+          end.new
+        end
+
+        it 'returns the configured migrate path' do
+          expect(subject.configured_migrate_path).to eq('ee/db/geo/migrate')
+        end
+      end
+
+      context 'when super returns only one post migrations path' do
+        subject do
+          Class.new do
+            prepend Gitlab::Patch::GeoDatabaseTasks::ActiveRecordMigrationConfiguredMigratePath
+
+            def configured_migrate_path
+              'ee/db/geo/post_migrate'
+            end
+          end.new
+        end
+
+        it 'returns nil' do
+          expect(subject.configured_migrate_path).to be_nil
+        end
+      end
+
+      context 'when super does not include a post migrations path' do
+        subject do
+          Class.new do
+            prepend Gitlab::Patch::GeoDatabaseTasks::ActiveRecordMigrationConfiguredMigratePath
+
+            def configured_migrate_path
+              'ee/db/geo/migrate'
+            end
+          end.new
+        end
+
+        it 'returns the configured migrations path' do
+          expect(subject.configured_migrate_path).to eq('ee/db/geo/migrate')
+        end
+      end
+
+      context 'when super includes a post migrations path' do
+        subject do
+          Class.new do
+            prepend Gitlab::Patch::GeoDatabaseTasks::ActiveRecordMigrationConfiguredMigratePath
+
+            def configured_migrate_path
+              ['ee/db/geo/migrate', 'ee/db/geo/post_migrate']
+            end
+          end.new
+        end
+
+        it 'returns the regular migration path' do
+          expect(subject.configured_migrate_path).to eq('ee/db/geo/migrate')
+        end
       end
     end
   end
