58900afe9e8720381dae115a01928290278f62ba
Pedro Pombeiro <noreply@pedro.pombei.ro>
2022-06-01 22:18:01 +0200

    Only prune namespaces with Ultimate license

--- ee/spec/services/ci/runners/stale_group_runners_prune_service_spec.rb
+++ ee/spec/services/ci/runners/stale_group_runners_prune_service_spec.rb
@@ -3,35 +3,17 @@
 require 'spec_helper'
 
 RSpec.describe Ci::Runners::StaleGroupRunnersPruneService do
-  let!(:group) { create(:group) }
   let(:service) { described_class.new }
 
   subject(:status) { service.perform(NamespaceCiCdSetting.allowing_stale_runner_pruning.select(:namespace_id)) }
 
-  context 'with empty groups relation' do
-    let!(:stale_runner) do
-      create(:ci_runner, :group, groups: [group], created_at: 5.months.ago, contacted_at: 4.months.ago)
-    end
-
-    it 'does not prune any runners and returns :success status' do
-      expect(service).not_to receive(:delete_stale_group_runners_in_batches)
-
-      expect do
-        expect(status).to match({
-          status: :success,
-          total_pruned: 0
-        })
-      end.not_to change { Ci::Runner.count }.from(1)
-    end
-  end
-
-  context 'with group' do
+  shared_context 'with some stale group runners on group1' do
     let!(:active_runner) do
-      create(:ci_runner, :group, groups: [group], created_at: 5.months.ago, contacted_at: 10.seconds.ago)
+      create(:ci_runner, :group, groups: [group1], created_at: 5.months.ago, contacted_at: 10.seconds.ago)
     end
 
     let!(:stale_runners) do
-      create_list(:ci_runner, 3, :group, groups: [group], created_at: 5.months.ago, contacted_at: 4.months.ago)
+      create_list(:ci_runner, 3, :group, groups: [group1], created_at: 5.months.ago, contacted_at: 4.months.ago)
     end
 
     let(:group2) { create(:group) }
@@ -39,37 +21,152 @@ RSpec.describe Ci::Runners::StaleGroupRunnersPruneService do
     before do
       stub_const("#{described_class}::GROUP_BATCH_SIZE", 1)
 
-      group.ci_cd_settings.update!(allow_stale_runner_pruning: true)
+      group1.ci_cd_settings.update!(allow_stale_runner_pruning: true)
       group2.ci_cd_settings.update!(allow_stale_runner_pruning: true)
     end
+  end
 
-    context 'with :stale_runner_cleanup_for_namespace_development FF enabled' do
-      before do
-        stub_feature_flags(stale_runner_cleanup_for_namespace_development: [group, group2])
+  shared_examples 'perform on empty groups relation does not prune any runners' do
+    context 'with empty groups relation' do
+      let!(:stale_runner) do
+        create(:ci_runner, :group, groups: [group1], created_at: 5.months.ago, contacted_at: 4.months.ago)
       end
 
-      it 'prunes all runners in batches' do
-        expect do
-          expect(status).to match({
-            status: :success,
-            total_pruned: 3
-          })
-        end.to change { Ci::Runner.count }.from(4).to(1)
-      end
-    end
+      it 'does not prune any runners and returns :success status' do
+        expect(service).not_to receive(:delete_stale_group_runners_in_batches)
 
-    context 'with :stale_runner_cleanup_for_namespace_development FF disabled for group' do
-      before do
-        stub_feature_flags(stale_runner_cleanup_for_namespace_development: [group2])
-      end
-
-      it 'does not prune any runners' do
         expect do
           expect(status).to match({
             status: :success,
             total_pruned: 0
           })
-        end.not_to change { Ci::Runner.count }
+        end.not_to change { Ci::Runner.count }.from(1)
+      end
+    end
+  end
+
+  shared_examples 'pruning is executed on stale runners' do
+    it 'prunes all runners in batches' do
+      expect do
+        expect(status).to match({
+          status: :success,
+          total_pruned: 3
+        })
+      end.to change { Ci::Runner.count }.from(4).to(1)
+    end
+  end
+
+  shared_examples 'pruning is not executed on stale runners' do
+    it 'does not prune any runners' do
+      expect do
+        expect(status).to match({
+          status: :success,
+          total_pruned: 0
+        })
+      end.not_to change { Ci::Runner.count }
+    end
+  end
+
+  context 'on self-managed instance' do
+    let!(:group1) { create(:group) }
+
+    before do
+      stub_application_setting(check_namespace_plan: false)
+    end
+
+    context 'when stale_runner_cleanup_for_namespace licensed feature is available' do
+      before do
+        stub_licensed_features(stale_runner_cleanup_for_namespace: true)
+      end
+
+      context 'with :stale_runner_cleanup_for_namespace_development FF enabled' do
+        before do
+          stub_feature_flags(stale_runner_cleanup_for_namespace_development: true)
+        end
+
+        it_behaves_like 'perform on empty groups relation does not prune any runners'
+      end
+
+      context 'with some stale group runners on group1' do
+        include_context 'with some stale group runners on group1'
+
+        context 'with :stale_runner_cleanup_for_namespace_development FF enabled' do
+          before do
+            stub_feature_flags(stale_runner_cleanup_for_namespace_development: [group1, group2])
+          end
+
+          it_behaves_like 'pruning is executed on stale runners'
+        end
+
+        context 'with :stale_runner_cleanup_for_namespace_development FF disabled for group1' do
+          before do
+            stub_feature_flags(stale_runner_cleanup_for_namespace_development: [group2])
+          end
+
+          it_behaves_like 'pruning is not executed on stale runners'
+        end
+      end
+    end
+
+    context 'when stale_runner_cleanup_for_namespace licensed feature is unavailable' do
+      before do
+        stub_licensed_features(stale_runner_cleanup_for_namespace: false)
+      end
+
+      context 'with some stale group runners on group1' do
+        include_context 'with some stale group runners on group1'
+
+        context 'with :stale_runner_cleanup_for_namespace_development FF enabled' do
+          before do
+            stub_feature_flags(stale_runner_cleanup_for_namespace_development: true)
+          end
+
+          it_behaves_like 'pruning is not executed on stale runners'
+        end
+      end
+    end
+  end
+
+  context 'on .com', :saas do
+    let(:namespace_plan) { :ultimate_plan }
+    let!(:group1) { create(:group_with_plan, plan: namespace_plan) }
+
+    before do
+      stub_application_setting(check_namespace_plan: true)
+      stub_licensed_features(stale_runner_cleanup_for_namespace: true)
+    end
+
+    context 'with :stale_runner_cleanup_for_namespace_development FF enabled' do
+      before do
+        stub_feature_flags(stale_runner_cleanup_for_namespace_development: true)
+      end
+
+      it_behaves_like 'perform on empty groups relation does not prune any runners'
+    end
+
+    context 'with some stale group runners on group1' do
+      include_context 'with some stale group runners on group1'
+
+      context 'with :stale_runner_cleanup_for_namespace_development FF enabled' do
+        before do
+          stub_feature_flags(stale_runner_cleanup_for_namespace_development: [group1, group2])
+        end
+
+        it_behaves_like 'pruning is executed on stale runners'
+
+        context 'with premium plan' do
+          let(:namespace_plan) { :premium_plan }
+
+          it_behaves_like 'pruning is not executed on stale runners'
+        end
+      end
+
+      context 'with :stale_runner_cleanup_for_namespace_development FF disabled for group1' do
+        before do
+          stub_feature_flags(stale_runner_cleanup_for_namespace_development: [group2])
+        end
+
+        it_behaves_like 'pruning is not executed on stale runners'
       end
     end
   end
--- ee/spec/workers/ci/runners/stale_group_runners_prune_cron_worker_spec.rb
+++ ee/spec/workers/ci/runners/stale_group_runners_prune_cron_worker_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe Ci::Runners::StaleGroupRunnersPruneCronWorker do
   let(:worker) { described_class.new }
 
   describe '#perform' do
-    let_it_be(:group1) { create(:group) }
     let_it_be(:group2) { create(:group) }
 
     let!(:runner1) do
@@ -16,46 +15,85 @@ RSpec.describe Ci::Runners::StaleGroupRunnersPruneCronWorker do
     let!(:runner2) { create(:ci_runner, :group, groups: [group1]) }
     let!(:runner3) { create(:ci_runner, :group, groups: [group2], created_at: 2.months.ago) }
 
-    it 'delegates to Ci::Runners::StaleGroupRunnersPruneService' do
+    before do
       group1.update!(allow_stale_runner_pruning: true)
       group2.update!(allow_stale_runner_pruning: false)
-
-      expect_next_instance_of(Ci::Runners::StaleGroupRunnersPruneService) do |service|
-        expect(service)
-          .to receive(:perform)
-          .with([group1.ci_cd_settings])
-          .and_call_original
-      end
-
-      worker.perform
-
-      expect(worker.logging_extras).to eq({
-        "extra.ci_runners_stale_group_runners_prune_cron_worker.status" => :success,
-        "extra.ci_runners_stale_group_runners_prune_cron_worker.total_pruned" => 1
-      })
     end
 
-    it_behaves_like 'an idempotent worker' do
-      context 'prunes stale runners when group1 is set to allow pruning' do
-        it 'prunes stale runners' do
-          group1.update!(allow_stale_runner_pruning: true)
-          group2.update!(allow_stale_runner_pruning: false)
+    context 'on saas', :saas do
+      using RSpec::Parameterized::TableSyntax
 
-          expect { subject }.to change { Ci::Runner.count }.from(3).to(2)
+      let_it_be(:group1) { create(:group_with_plan, plan: :ultimate_plan) }
 
-          expect(Ci::Runner.all).to match_array [runner2, runner3]
+      where(:check_namespace_plan, :feature_licensed, :expected_total_pruned) do
+        true | true | 1
+        false | true | 1
+        true | false | 0
+        false | false | 0
+      end
+
+      with_them do
+        before do
+          stub_application_setting(check_namespace_plan: check_namespace_plan)
+          stub_licensed_features(stale_runner_cleanup_for_namespace: feature_licensed)
+        end
+
+        it 'delegates to Ci::Runners::StaleGroupRunnersPruneService' do
+          expect_next_instance_of(Ci::Runners::StaleGroupRunnersPruneService) do |service|
+            expect(service)
+              .to receive(:perform)
+                    .with([group1.ci_cd_settings])
+                    .and_call_original
+          end
+
+          worker.perform
+
+          expect(worker.logging_extras).to eq({
+            "extra.ci_runners_stale_group_runners_prune_cron_worker.status" => :success,
+            "extra.ci_runners_stale_group_runners_prune_cron_worker.total_pruned" => expected_total_pruned
+          })
         end
       end
 
-      context 'does not prune any runners when only group2 is set to allow pruning' do
-        it 'does not prune runners' do
-          group1.update!(allow_stale_runner_pruning: false)
-          group2.update!(allow_stale_runner_pruning: true)
-
-          expect { subject }.not_to change { Ci::Runner.count }
-
-          expect(Ci::Runner.all).to match_array [runner1, runner2, runner3]
+      it_behaves_like 'an idempotent worker' do
+        before do
+          stub_application_setting(check_namespace_plan: true)
+          stub_licensed_features(stale_runner_cleanup_for_namespace: true)
         end
+
+        context 'prunes stale runners when group1 is set to allow pruning' do
+          it 'prunes stale runners' do
+            expect { subject }.to change { Ci::Runner.count }.from(3).to(2)
+
+            expect(Ci::Runner.all).to match_array [runner2, runner3]
+          end
+        end
+
+        context 'does not prune any runners when only group2 is set to allow pruning' do
+          it 'does not prune runners' do
+            group1.update!(allow_stale_runner_pruning: false)
+            group2.update!(allow_stale_runner_pruning: true)
+
+            expect { subject }.not_to change { Ci::Runner.count }
+
+            expect(Ci::Runner.all).to match_array [runner1, runner2, runner3]
+          end
+        end
+      end
+    end
+
+    context 'on non-saas' do
+      let_it_be(:group1) { create(:group) }
+
+      before do
+        stub_application_setting(check_namespace_plan: true)
+        stub_licensed_features(stale_runner_cleanup_for_namespace: true)
+      end
+
+      it 'does not prune runners' do
+        expect { subject }.not_to change { Ci::Runner.count }
+
+        expect(Ci::Runner.all).to match_array [runner1, runner2, runner3]
       end
     end
   end
