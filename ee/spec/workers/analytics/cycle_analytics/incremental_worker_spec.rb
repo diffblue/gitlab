eb1584340e24ac6511b21205fd2598f87a9e2765
Adam Hegyi <ahegyi@gitlab.com>
2022-06-02 10:05:21 +0200

    Remove the vsa_incremental_worker feature flag
    This change removes the vsa_incremental_worker feature flag.
    
    Changelog: removed

--- ee/spec/workers/analytics/cycle_analytics/incremental_worker_spec.rb
+++ ee/spec/workers/analytics/cycle_analytics/incremental_worker_spec.rb
@@ -5,6 +5,6 @@ require 'spec_helper'
 RSpec.describe Analytics::CycleAnalytics::IncrementalWorker do
   it_behaves_like 'aggregator worker examples' do
     let(:expected_mode) { :incremental }
-    let(:feature_flag) { :vsa_incremental_worker }
+    let(:feature_flag) { nil } # TODO: remove when cleaning up the other worker feature flags
   end
 end
