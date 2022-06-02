6975822909273d04c89cd537df4fa28d62a2f236
Heinrich Lee Yu <heinrich@gitlab.com>
2022-06-02 19:04:34 +0800

    Fix incorrect open issue count in project sidebar
    The new issue list includes all issue types so the open issue count
    query should also include all issue types
    
    Changelog: fixed
    EE: true

--- ee/spec/services/projects/open_issues_count_service_spec.rb
+++ ee/spec/services/projects/open_issues_count_service_spec.rb
@@ -6,12 +6,12 @@ RSpec.describe Projects::OpenIssuesCountService, :use_clean_rails_memory_store_c
   let(:project) { create(:project) }
 
   describe '#count' do
-    it 'does not count test cases' do
+    it 'includes all issue types' do
       create(:issue, :opened, project: project)
       create(:incident, :opened, project: project)
       create(:quality_test_case, :opened, project: project)
 
-      expect(described_class.new(project).count).to eq(2)
+      expect(described_class.new(project).count).to eq(3)
     end
   end
 end
