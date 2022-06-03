bbf963d830e7b49dd34a0166e007b5d74543a18c
Rajat Jain <rjain@gitlab.com>
2022-06-03 13:55:23 +0530

    Render WorkItemLinks widget to issue page
    This adds ability to render WorkItemLinks on both work items
    view and issue show view

--- ee/app/assets/javascripts/pages/projects/issues/show/index.js
+++ ee/app/assets/javascripts/pages/projects/issues/show/index.js
@@ -2,6 +2,7 @@ import { initRelatedFeatureFlags, initUnableToLinkVulnerabilityError } from 'ee/
 import { initShow } from '~/issues';
 import { store } from '~/notes/stores';
 import initRelatedIssues from '~/related_issues';
+import initWorkItemLinks from '~/work_items/components/work_item_links';
 import initSidebarBundle from '~/sidebar/sidebar_bundle';
 import UserCallout from '~/user_callout';
 
@@ -10,6 +11,7 @@ initSidebarBundle(store);
 initRelatedIssues();
 initRelatedFeatureFlags();
 initUnableToLinkVulnerabilityError();
+initWorkItemLinks();
 
 new UserCallout({ className: 'js-epics-sidebar-callout' }); // eslint-disable-line no-new
 new UserCallout({ className: 'js-weight-sidebar-callout' }); // eslint-disable-line no-new
