f3351de21f766da798dd0481e29f9926b178048b
Kerri Miller <kerrizor@kerrizor.com>
2022-06-05 14:12:54 -0700

    Add basic specs for CheckDeniedPolicies

---
+++ ee/spec/services/merge_requests/mergeability/check_denied_policies_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe MergeRequests::Mergeability::CheckDeniedPolicies do
+  subject(:check_denied_policies) { described_class.new(merge_request: merge_request, params: {}) }
+
+  let(:merge_request) { build(:merge_request) }
+
+  describe "#execute" do
+    before do
+      expect(merge_request).to receive(:has_denied_policies?).and_return(has_denied_policies)
+    end
+
+    context "when the merge request has denied policies" do
+      let(:has_denied_policies) { true }
+
+      it "returns a check result with status failed" do
+        expect(check_denied_policies.execute.status)
+          .to eq Gitlab::MergeRequests::Mergeability::CheckResult::FAILED_STATUS
+      end
+    end
+
+    context "when the merge request does not have denied policies" do
+      let(:has_denied_policies) { false }
+
+      it "returns a check result with status success" do
+        expect(check_denied_policies.execute.status)
+          .to eq Gitlab::MergeRequests::Mergeability::CheckResult::SUCCESS_STATUS
+      end
+    end
+  end
+
+  describe '#skip?' do
+    it 'returns false' do
+      expect(check_denied_policies.skip?).to eq false
+    end
+  end
+
+  describe '#cacheable?' do
+    it 'returns false' do
+      expect(check_denied_policies.cacheable?).to eq false
+    end
+  end
+end
