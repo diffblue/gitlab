da851f67eb047a9434130a46f34e782589bccaea
Marcos Rocha <mrocha@gitlab.com>
2022-06-07 00:06:01 +0000

    Convert captcha check endpoint to POST request
    This Merge Request converts the users captcha check
    endpoint to a POST request
    
    Changelog: fixed
    EE: true

--- ee/lib/api/captcha_check.rb
+++ ee/lib/api/captcha_check.rb
@@ -28,6 +28,35 @@ module API
           present(::Users::CaptchaChallengeService.new(user).execute, with: Entities::CaptchaCheck)
         end
       end
+
+      desc 'Post captcha check result for ArkoseLabs'
+      post '/captcha_check' do
+        not_found! 'User' unless Feature.enabled?(:arkose_labs_login_challenge)
+
+        rate_limit_reached = false
+        check_rate_limit!(:search_rate_limit_unauthenticated, scope: [ip_address]) do
+          rate_limit_reached = true
+        end
+
+        status 200
+
+        if rate_limit_reached || !valid_username?(params[:username])
+          present({ result: true }, with: Entities::CaptchaCheck)
+        else
+          user = ::User.by_login(params[:username])
+          present(::Users::CaptchaChallengeService.new(user).execute, with: Entities::CaptchaCheck)
+        end
+      end
+    end
+
+    helpers do
+      def valid_username?(username)
+        username.present? && valid_length?(username)
+      end
+
+      def valid_length?(username)
+        username.length >= User::MIN_USERNAME_LENGTH && username.length <= User::MAX_USERNAME_LENGTH
+      end
     end
   end
 end
--- ee/spec/requests/api/captcha_check_spec.rb
+++ ee/spec/requests/api/captcha_check_spec.rb
@@ -122,4 +122,141 @@ RSpec.describe API::CaptchaCheck do
       end
     end
   end
+
+  describe 'POST users/captcha_check' do
+    context 'when the feature flag arkose_labs_login_challenge is disabled' do
+      before do
+        stub_feature_flags(arkose_labs_login_challenge: false)
+      end
+
+      it 'does return not found status' do
+        post api('/users/captcha_check'), params: { username: username }
+
+        expect(response).to have_gitlab_http_status(:not_found)
+      end
+    end
+
+    context 'when the feature flag arkose_labs_login_challenge is enabled' do
+      before do
+        stub_feature_flags(arkose_labs_login_challenge: true)
+      end
+
+      context 'when the username is invalid' do
+        let(:invalid_username) { 'invalidUsername' }
+
+        it 'does return 200 OK status to prevent enumeration' do
+          post api('/users/captcha_check'), params: { username: invalid_username }
+
+          expect(response).to have_gitlab_http_status(:ok)
+          expect(json_response['result']).to be_truthy
+        end
+      end
+
+      context 'when the username has a dot' do
+        let_it_be(:username) { 'valid.Username' }
+        let_it_be(:dot_user) { create(:user, username: username) }
+
+        it 'does return 200 status' do
+          post api('/users/captcha_check'), params: { username: username }
+
+          expect(response).to have_gitlab_http_status(:ok)
+        end
+      end
+
+      context 'when the username is nil' do
+        let_it_be(:username) { nil }
+
+        it 'does return 200 OK status to prevent enumeration' do
+          post api('/users/captcha_check'), params: { username: username }
+
+          expect(response).to have_gitlab_http_status(:ok)
+          expect(json_response['result']).to be_truthy
+        end
+      end
+
+      context 'when the username is an array' do
+        let_it_be(:username) { %w[guess1 guess2 root] }
+
+        it 'does return 200 OK status to prevent enumeration' do
+          post api('/users/captcha_check'), params: { username: username }
+
+          expect(response).to have_gitlab_http_status(:bad_request)
+        end
+      end
+
+      context 'when the email is valid' do
+        it 'returns status ok' do
+          post api('/users/captcha_check'), params: { username: email }
+
+          expect(response).to have_gitlab_http_status(:ok)
+        end
+      end
+
+      context 'when the email is unknown' do
+        let(:unknown_email) { 'unknown_email@email.com' }
+
+        it 'returns 200 OK status to prevent enumeration' do
+          post api('/users/captcha_check'), params: { username: unknown_email }
+
+          expect(response).to have_gitlab_http_status(:ok)
+          expect(json_response['result']).to be_truthy
+        end
+      end
+
+      context 'when the email is invalid' do
+        let(:invalid_email) { 'invalid_email@' }
+
+        it 'returns 200 OK status to prevent enumeration' do
+          post api('/users/captcha_check'), params: { username: invalid_email }
+
+          expect(response).to have_gitlab_http_status(:ok)
+          expect(json_response['result']).to be_truthy
+        end
+      end
+
+      context 'when the user meets the criteria for the captcha check' do
+        before do
+          user.last_sign_in_at = nil
+        end
+
+        it 'does return true' do
+          post api('/users/captcha_check'), params: { username: username }
+
+          expect(response).to have_gitlab_http_status(:ok)
+          expect(json_response['result']).to be_truthy
+        end
+      end
+
+      context 'when the user does not meets the criteria for the captcha check' do
+        before do
+          user.last_sign_in_at = Date.today - 2.months
+          user.last_sign_in_ip = '192.168.1.1'
+          user.save!
+        end
+
+        it 'does return true' do
+          post api('/users/captcha_check'), params: { username: username }, headers: { 'REMOTE_ADDR' => '192.168.1.1' }
+
+          expect(response).to have_gitlab_http_status(:ok)
+          expect(json_response['result']).to be_falsey
+        end
+      end
+
+      context 'when the user reach the rate limit' do
+        before do
+          user.last_sign_in_at = Date.today - 2.months
+          user.last_sign_in_ip = '192.168.1.1'
+          user.save!
+        end
+
+        it 'does return true' do
+          allow(::Gitlab::ApplicationRateLimiter).to receive(:throttled?).and_return(true)
+          post api('/users/captcha_check'), params: { username: username }, headers: { 'REMOTE_ADDR' => '192.168.1.1' }
+
+          expect(response).to have_gitlab_http_status(:ok)
+          expect(json_response['result']).to be_truthy
+        end
+      end
+    end
+  end
 end
