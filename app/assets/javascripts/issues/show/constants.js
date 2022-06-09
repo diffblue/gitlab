3ffdf8ac79a3e7d6d5d542dd7153ab427a0f3473
Rajendra Kadam <rkadam@gitlab.com>
2022-06-09 22:11:01 +0530

    Revert changes to change paths for incidents

--- app/assets/javascripts/issues/show/constants.js
+++ app/assets/javascripts/issues/show/constants.js
@@ -1,8 +1,10 @@
 import { __ } from '~/locale';
 
 export const INCIDENT_TYPE = 'incident';
+export const INCIDENT_TYPE_PATH = 'issues/incident';
 export const ISSUE_STATE_EVENT_CLOSE = 'CLOSE';
 export const ISSUE_STATE_EVENT_REOPEN = 'REOPEN';
+export const ISSUE_TYPE_PATH = 'issues';
 export const JOIN_ZOOM_MEETING = __('Join Zoom meeting');
 export const POLLING_DELAY = 2000;
 export const STATUS_PAGE_PUBLISHED = __('Published on status page');
