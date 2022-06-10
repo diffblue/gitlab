932218946b1fdde8f21ead2bbc4b8fb751b293f6
Justin Ho <hduong@gitlab.com>
2022-06-10 11:03:50 +0700

    Add a new table_utility for header width
    The new one does not include DEFAULT_TH_CLASSES
    which are no longer needed with `.gl-table`.
    
    Mark the old one as deprecated.

--- app/assets/javascripts/lib/utils/table_utility.js
+++ app/assets/javascripts/lib/utils/table_utility.js
@@ -2,12 +2,20 @@ import { convertToSnakeCase, convertToCamelCase } from '~/lib/utils/text_utility
 import { DEFAULT_TH_CLASSES } from './constants';
 
 /**
- * Generates the table header classes to be used for GlTable fields.
+ * Deprecated: Generates the table header classes to be used for GlTable fields.
  *
  * @param {Number} width - The column width as a percentage.
  * @returns {String} The classes to be used in GlTable fields object.
  */
 export const thWidthClass = (width) => `gl-w-${width}p ${DEFAULT_TH_CLASSES}`;
+
+/**
+ * Generates the table header class for width to be used for GlTable fields.
+ *
+ * @param {Number} width - The column width as a percentage.
+ * @returns {String} The class to be used in GlTable fields object.
+ */
+export const thWidthPercent = (width) => `gl-w-${width}p`;
 
 /**
  * Converts a GlTable sort-changed event object into string format.
