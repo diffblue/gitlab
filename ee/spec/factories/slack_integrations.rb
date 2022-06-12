cd9755914e6737b0e83f5b196003bb0a8ad82bf9
Markus Koller <mkoller@gitlab.com>
2022-06-12 23:48:30 +0000

    Add Slack app installations to Service Ping
    Track the number of Slack app installations, as well as the number of
    installations that have upgraded to the new "GBP" version.
    
    Based on these two metrics we can calculate the progress of the update
    rollout.
    
    Changelog: changed
    EE: true

--- ee/spec/factories/slack_integrations.rb
+++ ee/spec/factories/slack_integrations.rb
@@ -4,9 +4,16 @@ FactoryBot.define do
   factory :slack_integration do
     sequence(:team_id) { |n| "T123#{n}" }
     sequence(:user_id) { |n| "U123#{n}" }
+    sequence(:bot_user_id) { |n| "U123#{n}" }
+    sequence(:bot_access_token) { |n| OpenSSL::Digest::SHA256.hexdigest(n.to_s) }
     sequence(:team_name) { |n| "team#{n}" }
     sequence(:alias) { |n| "namespace#{n}/project_name#{n}" }
 
     integration factory: :gitlab_slack_application_integration
+
+    trait :legacy do
+      bot_user_id { nil }
+      bot_access_token { nil }
+    end
   end
 end
