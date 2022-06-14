d613e0e4654b733d7b48595b00fd68e392225331
Robert Hunt <rhunt@gitlab.com>
2022-06-14 15:45:39 +0100

    Add custom headers form to audit events streaming UI using feature flag
    - Added :custom_headers_streaming_audit_events_ui feature flag
    - Pass flag status to frontend via injection and helper
    - Added table structure to show header inputs
    - Add add/update/delete row functionality
    - Added validation to stop duplicate headers
    - Added automatic row addition when previous row is filled out
    - Added max limit to row count and message when limit is reached
    - Added ability to disable a row
    - Disabled all active checkboxes for now
    - Add/update specs
    - Remove snapshots from specs as the app is getting complicated

--- ee/app/assets/javascripts/audit_events/components/stream/stream_destination_editor.vue
+++ ee/app/assets/javascripts/audit_events/components/stream/stream_destination_editor.vue
@@ -1,21 +1,64 @@
 <script>
-import { GlAlert, GlButton, GlForm, GlFormGroup, GlFormInput } from '@gitlab/ui';
+import {
+  GlAlert,
+  GlButton,
+  GlForm,
+  GlFormCheckbox,
+  GlFormGroup,
+  GlFormInput,
+  GlSprintf,
+  GlTableLite,
+} from '@gitlab/ui';
 import createFlash from '~/flash';
+import { thWidthPercent } from '~/lib/utils/table_utility';
 import externalAuditEventDestinationCreate from '../../graphql/create_external_destination.mutation.graphql';
-import { ADD_STREAM_EDITOR_I18N, AUDIT_STREAMS_NETWORK_ERRORS } from '../../constants';
+import {
+  ADD_STREAM_EDITOR_I18N,
+  AUDIT_STREAMS_NETWORK_ERRORS,
+  createBlankHeader,
+} from '../../constants';
 
 const { CREATING_ERROR } = AUDIT_STREAMS_NETWORK_ERRORS;
+
+const thClasses = `gl-border-top-0! gl-p-3!`;
+const tdClasses = `gl-p-3!`;
+
 export default {
   components: {
     GlAlert,
     GlButton,
     GlForm,
+    GlFormCheckbox,
     GlFormGroup,
     GlFormInput,
+    GlSprintf,
+    GlTableLite,
   },
-  inject: ['groupPath'],
+  inject: ['groupPath', 'showStreamsHeaders', 'maxHeaders'],
   data() {
-    return { destinationUrl: '', loading: false };
+    return {
+      destinationUrl: '',
+      loading: false,
+      headers: [createBlankHeader()],
+    };
+  },
+  computed: {
+    hasReachedMaxHeaders() {
+      return this.headers.length >= this.maxHeaders;
+    },
+    hasHeaderValidationErrors() {
+      return this.headers.some((header) => header.validationErrors.name !== '');
+    },
+    hasMissingKeyValuePairs() {
+      return this.headers.some(
+        (header) =>
+          (header.name !== '' && header.value === '') ||
+          (header.name === '' && header.value !== ''),
+      );
+    },
+    isSubmitButtonDisabled() {
+      return !this.destinationUrl || this.hasHeaderValidationErrors || this.hasMissingKeyValuePairs;
+    },
   },
   methods: {
     async addDestinationUrl() {
@@ -31,7 +74,9 @@ export default {
             isSingleRequest: true,
           },
         });
+
         const { errors } = data.externalAuditEventDestinationCreate;
+
         if (errors.length > 0) {
           createFlash({
             message: errors[0],
@@ -47,8 +92,88 @@ export default {
         this.loading = false;
       }
     },
+    isRowFilled(index) {
+      return this.headers[index].name !== '' && this.headers[index].value !== '';
+    },
+    isLastRow(index) {
+      return this.headers.length === index + 1;
+    },
+    headerNameExists(value) {
+      return this.headers.some((header) => header.name === value);
+    },
+    addBlankHeader() {
+      this.headers.push(createBlankHeader());
+    },
+    addBlankRowIfNeeded(index) {
+      if (!this.hasReachedMaxHeaders && this.isRowFilled(index) && this.isLastRow(index)) {
+        this.addBlankHeader();
+      }
+    },
+    handleHeaderNameInput(index, name) {
+      const header = this.headers[index];
+
+      if (name !== '' && this.headerNameExists(name)) {
+        header.validationErrors.name = ADD_STREAM_EDITOR_I18N.HEADER_INPUT_DUPLICATE_ERROR;
+      } else {
+        const updatedHeader = {
+          ...header,
+          name,
+          validationErrors: {
+            ...header.validationErrors,
+            name: '',
+          },
+          deletionDisabled: false,
+        };
+
+        this.$set(this.headers, index, updatedHeader);
+      }
+
+      this.addBlankRowIfNeeded(index);
+    },
+    handleHeaderValueInput(index, value) {
+      this.$set(this.headers, index, { ...this.headers[index], value, deletionDisabled: false });
+      this.addBlankRowIfNeeded(index);
+    },
+    handleHeaderActiveInput(index, active) {
+      this.$set(this.headers, index, { ...this.headers[index], active });
+    },
+    removeHeader(index) {
+      this.headers.splice(index, 1);
+      const headersCount = this.headers.length;
+
+      // Add a new blank row if headers is now empty or the last row is filled out
+      if (headersCount === 0 || this.isRowFilled(headersCount - 1)) {
+        this.addBlankHeader();
+      }
+    },
   },
   i18n: { ...ADD_STREAM_EDITOR_I18N, CREATING_ERROR },
+  fields: [
+    {
+      key: 'name',
+      label: ADD_STREAM_EDITOR_I18N.TABLE_COLUMN_NAME_LABEL,
+      thClass: `${thClasses} ${thWidthPercent(40)}`,
+      tdClass: tdClasses,
+    },
+    {
+      key: 'value',
+      label: ADD_STREAM_EDITOR_I18N.TABLE_COLUMN_VALUE_LABEL,
+      thClass: `${thClasses} ${thWidthPercent(50)}`,
+      tdClass: tdClasses,
+    },
+    {
+      key: 'active',
+      label: ADD_STREAM_EDITOR_I18N.TABLE_COLUMN_ACTIVE_LABEL,
+      thClass: `${thClasses} ${thWidthPercent(5)}`,
+      tdClass: tdClasses,
+    },
+    {
+      key: 'actions',
+      label: '',
+      thClass: thClasses,
+      tdClass: `${tdClasses} gl-text-right`,
+    },
+  ],
 };
 </script>
 
@@ -64,12 +189,89 @@ export default {
     </gl-alert>
 
     <gl-form @submit.prevent="addDestinationUrl">
-      <gl-form-group :label="$options.i18n.DESTINATION_URL_LABEL">
-        <gl-form-input v-model="destinationUrl" :placeholder="$options.i18n.PLACEHOLDER" />
+      <gl-form-group
+        :label="$options.i18n.DESTINATION_URL_LABEL"
+        data-testid="destination-url-form-group"
+      >
+        <gl-form-input
+          v-model="destinationUrl"
+          :placeholder="$options.i18n.DESTINATION_URL_PLACEHOLDER"
+          data-testid="destination-url"
+        />
       </gl-form-group>
+
+      <div v-if="showStreamsHeaders" class="gl-mb-5">
+        <strong class="gl-display-block gl-mb-3">{{ $options.i18n.HEADERS_LABEL }}</strong>
+        <gl-table-lite :items="headers" :fields="$options.fields">
+          <template
+            #cell(name)="{
+              index,
+              item: {
+                disabled,
+                validationErrors: { name: feedback = '' },
+                name,
+              },
+            }"
+          >
+            <gl-form-group
+              class="gl-m-0"
+              label-class="gl-m-0! gl-p-0!"
+              :invalid-feedback="feedback"
+            >
+              <gl-form-input
+                :value="name"
+                :placeholder="$options.i18n.HEADER_INPUT_PLACEHOLDER"
+                :disabled="disabled"
+                :state="feedback === ''"
+                data-testid="header-name-input"
+                @input="handleHeaderNameInput(index, $event)"
+              />
+            </gl-form-group>
+          </template>
+          <template #cell(value)="{ index, item: { disabled, value } }">
+            <gl-form-group class="gl-m-0" label-class="gl-m-0! gl-p-0!">
+              <gl-form-input
+                :value="value"
+                :placeholder="$options.i18n.VALUE_INPUT_PLACEHOLDER"
+                :disabled="disabled"
+                data-testid="header-value-input"
+                @input="handleHeaderValueInput(index, $event)"
+              />
+            </gl-form-group>
+          </template>
+          <template #cell(active)="{ index, item: { active } }">
+            <gl-form-checkbox
+              class="gl-mt-3"
+              :checked="active"
+              :disabled="true"
+              @input="handleHeaderActiveInput(index, $event)"
+            />
+          </template>
+          <template #cell(actions)="{ index, item: { deletionDisabled } }">
+            <gl-button
+              category="tertiary"
+              icon="remove"
+              :disabled="deletionDisabled"
+              @click="removeHeader(index)"
+            />
+          </template>
+        </gl-table-lite>
+        <p
+          v-if="hasReachedMaxHeaders"
+          class="gl-mt-5 gl-mb-0 gl-text-gray-500"
+          data-testid="maximum-headers"
+        >
+          <gl-sprintf :message="$options.i18n.MAXIMUM_HEADERS_TEXT">
+            <template #number>
+              {{ maxHeaders }}
+            </template>
+          </gl-sprintf>
+        </p>
+      </div>
+
       <div class="gl-display-flex">
         <gl-button
-          :disabled="!destinationUrl"
+          :disabled="isSubmitButtonDisabled"
           :loading="loading"
           :name="$options.i18n.ADD_BUTTON_NAME"
           class="gl-mr-3"
--- ee/spec/frontend/audit_events/components/stream/stream_destination_editor_spec.js
+++ ee/spec/frontend/audit_events/components/stream/stream_destination_editor_spec.js
@@ -1,10 +1,15 @@
 import VueApollo from 'vue-apollo';
 import { createLocalVue } from '@vue/test-utils';
-import { GlFormInput, GlForm } from '@gitlab/ui';
+import { GlAlert, GlButton, GlFormCheckbox, GlForm, GlTableLite } from '@gitlab/ui';
 import createFlash from '~/flash';
+import { sprintf } from '~/locale';
 import createMockApollo from 'helpers/mock_apollo_helper';
 import waitForPromises from 'helpers/wait_for_promises';
-import { mountExtended } from 'helpers/vue_test_utils_helper';
+import {
+  shallowMountExtended,
+  mountExtended,
+  extendedWrapper,
+} from 'helpers/vue_test_utils_helper';
 import externalAuditEventDestinationCreate from 'ee/audit_events/graphql/create_external_destination.mutation.graphql';
 import StreamDestinationEditor from 'ee/audit_events/components/stream/stream_destination_editor.vue';
 import { AUDIT_STREAMS_NETWORK_ERRORS, ADD_STREAM_EDITOR_I18N } from 'ee/audit_events/constants';
@@ -20,29 +25,56 @@ const externalAuditEventDestinationCreateSpy = jest
 describe('StreamDestinationEditor', () => {
   let wrapper;
 
+  const maxHeaders = 3;
+
   const createComponent = (
+    mountFn = shallowMountExtended,
+    provide = {},
     destinationCreateMutationSpy = externalAuditEventDestinationCreateSpy,
   ) => {
     const mockApollo = createMockApollo([
       [externalAuditEventDestinationCreate, destinationCreateMutationSpy],
     ]);
-    wrapper = mountExtended(StreamDestinationEditor, {
+    wrapper = mountFn(StreamDestinationEditor, {
       provide: {
         groupPath,
+        showStreamsHeaders: false,
+        maxHeaders,
+        ...provide,
       },
       apolloProvider: mockApollo,
       localVue,
     });
   };
 
+  const findWarningMessage = () => wrapper.findComponent(GlAlert);
   const findDestinationForm = () => wrapper.findComponent(GlForm);
+  const findHeadersTable = () => wrapper.findComponent(GlTableLite);
+  const findMaximumHeadersText = () => wrapper.findByTestId('maximum-headers').text();
+  const findAddBtn = () => wrapper.findByTestId('stream-destination-add-button');
   const findCancelBtn = () => wrapper.findByTestId('stream-destination-cancel-button');
-  const setDestinationUrl = () =>
-    wrapper.findComponent(GlFormInput).setValue(ADD_STREAM_EDITOR_I18N.PLACEHOLDER);
 
-  beforeEach(() => {
-    createComponent();
-  });
+  const findDestinationUrlFormGroup = () => wrapper.findByTestId('destination-url-form-group');
+  const findDestinationUrl = () => wrapper.findByTestId('destination-url');
+
+  const findHeadersRows = () => findHeadersTable().find('tbody').findAll('tr');
+  const findHeadersHeaderCell = (tdIdx) =>
+    findHeadersTable().find('thead tr').findAll('th').at(tdIdx);
+  const findHeadersCell = (trIdx, tdIdx) => findHeadersRows().at(trIdx).findAll('td').at(tdIdx);
+  const findHeaderCheckbox = (trIdx) => findHeadersRows().at(trIdx).findComponent(GlFormCheckbox);
+  const findHeaderDeleteBtn = (trIdx) => findHeadersRows().at(trIdx).findComponent(GlButton);
+  const findHeaderNameInput = (trIdx) =>
+    extendedWrapper(findHeadersRows().at(trIdx)).findByTestId('header-name-input');
+  const findHeaderValueInput = (trIdx) =>
+    extendedWrapper(findHeadersRows().at(trIdx)).findByTestId('header-value-input');
+
+  const setHeaderNameInput = (trIdx, name) => findHeaderNameInput(trIdx).setValue(name);
+  const setHeaderValueInput = (trIdx, value) => findHeaderValueInput(trIdx).setValue(value);
+
+  const setHeadersRowData = async (trIdx, { name, value }) => {
+    await setHeaderNameInput(trIdx, name);
+    await setHeaderValueInput(trIdx, value);
+  };
 
   afterEach(() => {
     wrapper.destroy();
@@ -51,15 +83,65 @@ describe('StreamDestinationEditor', () => {
   });
 
   describe('when initialized', () => {
-    it('should render correctly', () => {
-      expect(wrapper.element).toMatchSnapshot();
+    describe('destinations URL', () => {
+      beforeEach(() => {
+        createComponent();
+      });
+
+      it('should render the destinations warning', () => {
+        expect(findWarningMessage().props('title')).toBe(ADD_STREAM_EDITOR_I18N.WARNING_TITLE);
+        expect(findWarningMessage().text()).toBe(ADD_STREAM_EDITOR_I18N.WARNING_CONTENT);
+      });
+
+      it('should render the destination URL input', () => {
+        expect(findDestinationUrlFormGroup().exists()).toBe(true);
+        expect(findDestinationUrl().attributes('placeholder')).toBe(
+          ADD_STREAM_EDITOR_I18N.DESTINATION_URL_PLACEHOLDER,
+        );
+      });
+    });
+
+    describe('HTTP headers', () => {
+      beforeEach(() => {
+        createComponent(mountExtended, { showStreamsHeaders: true });
+      });
+
+      it('should render the table', () => {
+        expect(findHeadersRows()).toHaveLength(1);
+
+        expect(findHeadersHeaderCell(0).text()).toBe(
+          ADD_STREAM_EDITOR_I18N.TABLE_COLUMN_NAME_LABEL,
+        );
+        expect(findHeadersHeaderCell(1).text()).toBe(
+          ADD_STREAM_EDITOR_I18N.TABLE_COLUMN_VALUE_LABEL,
+        );
+        expect(findHeadersHeaderCell(2).text()).toBe(
+          ADD_STREAM_EDITOR_I18N.TABLE_COLUMN_ACTIVE_LABEL,
+        );
+        expect(findHeadersHeaderCell(3).text()).toBe('');
+
+        expect(findHeaderNameInput(0).attributes('placeholder')).toBe(
+          ADD_STREAM_EDITOR_I18N.HEADER_INPUT_PLACEHOLDER,
+        );
+        expect(findHeaderValueInput(0).attributes('placeholder')).toBe(
+          ADD_STREAM_EDITOR_I18N.VALUE_INPUT_PLACEHOLDER,
+        );
+        expect(findHeaderCheckbox(0).find('input').attributes('disabled')).toBe('disabled');
+        expect(findHeaderDeleteBtn(0).exists()).toBe(true);
+      });
     });
   });
 
   describe('add destination event', () => {
     it('should emit add event after destination added', async () => {
-      await setDestinationUrl();
-      await findDestinationForm().trigger('submit');
+      createComponent(
+        shallowMountExtended,
+        {},
+        jest.fn().mockResolvedValue(destinationCreateMutationPopulator()),
+      );
+
+      findDestinationUrl().vm.$emit('input', 'https://example.test');
+      findDestinationForm().vm.$emit('submit', { preventDefault: () => {} });
       await waitForPromises();
 
       expect(createFlash).not.toHaveBeenCalled();
@@ -68,9 +150,14 @@ describe('StreamDestinationEditor', () => {
 
     it('should not emit add destination event and reports error when server returns error', async () => {
       const errorMsg = 'Destination hosts limit exceeded';
-      createComponent(jest.fn().mockResolvedValue(destinationCreateMutationPopulator([errorMsg])));
-      await setDestinationUrl();
-      await findDestinationForm().trigger('submit');
+      createComponent(
+        shallowMountExtended,
+        {},
+        jest.fn().mockResolvedValue(destinationCreateMutationPopulator([errorMsg])),
+      );
+
+      findDestinationUrl().vm.$emit('input', 'https://example.test');
+      findDestinationForm().vm.$emit('submit', { preventDefault: () => {} });
       await waitForPromises();
 
       expect(createFlash).toHaveBeenCalledWith({
@@ -80,9 +167,10 @@ describe('StreamDestinationEditor', () => {
     });
 
     it('should not emit add destination event and reports error when network error occurs', async () => {
-      createComponent(jest.fn().mockRejectedValue());
-      await setDestinationUrl();
-      await findDestinationForm().trigger('submit');
+      createComponent(shallowMountExtended, {}, jest.fn().mockRejectedValue({}));
+
+      findDestinationUrl().vm.$emit('input', 'https://example.test');
+      findDestinationForm().vm.$emit('submit', { preventDefault: () => {} });
       await waitForPromises();
 
       expect(createFlash).toHaveBeenCalledWith({
@@ -93,10 +181,121 @@ describe('StreamDestinationEditor', () => {
   });
 
   describe('cancel event', () => {
-    it('should emit cancel event correctly', async () => {
-      await findCancelBtn().trigger('click');
+    beforeEach(() => {
+      createComponent();
+    });
+
+    it('should emit cancel event correctly', () => {
+      findCancelBtn().vm.$emit('click');
 
       expect(wrapper.emitted('cancel')).toBeDefined();
+    });
+  });
+
+  describe('HTTP headers table', () => {
+    beforeEach(() => {
+      createComponent(mountExtended, { showStreamsHeaders: true });
+    });
+
+    it.each`
+      name     | value    | rowCount
+      ${'abc'} | ${''}    | ${1}
+      ${''}    | ${'abc'} | ${1}
+      ${'abc'} | ${'abc'} | ${2}
+    `(
+      'should add a new blank row only when both the name and value are filled',
+      async ({ name, value, rowCount }) => {
+        await setHeadersRowData(0, { name, value });
+
+        expect(findHeadersRows()).toHaveLength(rowCount);
+      },
+    );
+
+    it.each`
+      name     | value    | disabled
+      ${'abc'} | ${''}    | ${true}
+      ${''}    | ${'abc'} | ${true}
+      ${'abc'} | ${'abc'} | ${false}
+    `(
+      'should enable the add button only when both the name and value are filled',
+      async ({ name, value, disabled }) => {
+        await findDestinationUrl().setValue('https://example.test');
+        await setHeadersRowData(0, { name, value });
+
+        expect(findAddBtn().props('disabled')).toBe(disabled);
+      },
+    );
+
+    it('should not add a new blank row if the header value is a duplicate', async () => {
+      await findDestinationUrl().setValue('https://example.test');
+      await setHeadersRowData(0, { name: 'row header', value: 'row value' });
+
+      expect(findHeadersRows()).toHaveLength(2);
+
+      await setHeadersRowData(1, { name: 'row header', value: 'row value' });
+
+      expect(findHeadersRows()).toHaveLength(2);
+      expect(findHeaderNameInput(1).classes()).toContain('is-invalid');
+      expect(findHeadersCell(1, 0).text()).toContain(
+        ADD_STREAM_EDITOR_I18N.HEADER_INPUT_DUPLICATE_ERROR,
+      );
+      expect(findAddBtn().props('disabled')).toBe(true);
+    });
+
+    it('should add a new blank row once a duplicate value is changed', async () => {
+      await findDestinationUrl().setValue('https://example.test');
+      await setHeadersRowData(0, { name: 'row header', value: 'row value' });
+      await setHeadersRowData(1, { name: 'row header', value: 'row value' });
+
+      expect(findHeadersRows()).toHaveLength(2);
+      expect(findHeaderNameInput(1).classes()).toContain('is-invalid');
+      expect(findAddBtn().props('disabled')).toBe(true);
+
+      await setHeadersRowData(1, { name: 'row header 2', value: 'row value' });
+
+      expect(findHeaderNameInput(1).classes()).toContain('is-valid');
+      expect(findAddBtn().props('disabled')).toBe(false);
+      expect(findHeadersRows()).toHaveLength(3);
+    });
+
+    it('should delete a row when the delete button is clicked', async () => {
+      await setHeadersRowData(0, { name: 'row header', value: 'row value' });
+      await setHeadersRowData(1, { name: 'row header 2', value: 'row value 2' });
+
+      expect(findHeadersRows()).toHaveLength(3);
+
+      await findHeaderDeleteBtn(1).trigger('click');
+
+      expect(findHeadersRows()).toHaveLength(2);
+      expect(findHeaderNameInput(0).element.value).toBe('row header');
+      expect(findHeaderValueInput(0).element.value).toBe('row value');
+      expect(findHeaderNameInput(1).element.value).toBe('');
+      expect(findHeaderValueInput(1).element.value).toBe('');
+    });
+
+    it('should add a blank row if the last row was deleted', async () => {
+      await setHeadersRowData(0, { name: 'row header', value: '' });
+
+      expect(findHeadersRows()).toHaveLength(1);
+
+      await findHeaderDeleteBtn(0).trigger('click');
+
+      expect(findHeadersRows()).toHaveLength(1);
+      expect(findHeaderNameInput(0).element.value).toBe('');
+      expect(findHeaderValueInput(0).element.value).toBe('');
+    });
+
+    it('should show the maximum number of rows message when the maximum is reached', async () => {
+      for (let i = 0; i < maxHeaders; i += 1) {
+        // This should be done synchronously because each new input will trigger a new one up to the maximum
+        // eslint-disable-next-line no-await-in-loop
+        await setHeadersRowData(i, { name: `row header ${i}`, value: `row value ${i}` });
+      }
+
+      expect(findHeadersRows()).toHaveLength(maxHeaders);
+      expect(findMaximumHeadersText()).toMatchInterpolatedText(
+        sprintf(ADD_STREAM_EDITOR_I18N.MAXIMUM_HEADERS_TEXT, { number: maxHeaders }),
+      );
     });
   });
 });
