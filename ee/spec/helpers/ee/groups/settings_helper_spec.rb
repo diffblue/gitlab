c44523bf43a07b8f4d8a99c6a7eee992012bc1c6
Jiaan Louw <3468028-jiaan@users.noreply.gitlab.com>
2022-06-15 18:43:56 +0000

    Update group deletion protection settings
    Updates the appearance of the group-level delayed project deletion
    settings.
    
    Changelog: changed
    EE: true

--- ee/spec/helpers/ee/groups/settings_helper_spec.rb
+++ ee/spec/helpers/ee/groups/settings_helper_spec.rb
@@ -3,36 +3,40 @@
 require 'spec_helper'
 
 RSpec.describe EE::Groups::SettingsHelper do
-  include AdminModeHelper
-
-  let_it_be(:user) { create(:user) }
-  let_it_be(:admin) { create(:user, :admin) }
-
   describe('#delayed_project_removal_help_text') do
     using RSpec::Parameterized::TableSyntax
 
-    settings_path = '/admin/application_settings/general#js-visibility-settings'
-
-    where(:is_admin, :expected) do
-      true  | "Projects will be permanently deleted after a 7-day delay. This delay can be <a href=\"#{settings_path}\">customized by an admin</a> in instance settings. Inherited by subgroups."
-      false | 'Projects will be permanently deleted after a 7-day delay. Inherited by subgroups.'
+    where(:admin_only, :expected) do
+      true  | 'Only administrators can delete projects.'
+      false | 'Owners and administrators can delete projects.'
     end
 
     with_them do
       before do
-        stub_application_setting(deletion_adjourned_period: 7)
-        allow(helper).to receive(:general_admin_application_settings_path).with(anchor: 'js-visibility-settings').and_return(settings_path)
-
-        if is_admin
-          allow(helper).to receive(:current_user).and_return(admin)
-          enable_admin_mode!(admin)
-        else
-          allow(helper).to receive(:current_user).and_return(user)
-        end
+        stub_application_setting(default_project_deletion_protection: admin_only)
       end
 
       it "returns expected helper text" do
         expect(helper.delayed_project_removal_help_text).to eq expected
+      end
+    end
+  end
+
+  describe('#keep_deleted_option_label') do
+    using RSpec::Parameterized::TableSyntax
+
+    where(:adjourned_period, :expected) do
+      1 | 'Keep deleted projects for 1 day'
+      4 | 'Keep deleted projects for 4 days'
+    end
+
+    with_them do
+      before do
+        stub_application_setting(deletion_adjourned_period: adjourned_period)
+      end
+
+      it "returns expected helper text" do
+        expect(helper.keep_deleted_option_label).to eq expected
       end
     end
   end
