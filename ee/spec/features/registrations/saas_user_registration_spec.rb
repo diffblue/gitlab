7ca6078d2cd9ecd89e72deb31742f0d5751496c3
Hinam Mehra <hmehra@gitlab.com>
2022-06-16 12:48:57 +1000

    Apply standard registration flow to single-sign on registrations
    - Create a feature flag update_oauth_registration_flow
    - Add an intent to register to omniauth params to redirect users to
    users_sign_up_welcome_path after single-sign on registration
    - Automatically agree to terms when a new OAuth user is created
    - Add feature specs for oauth_registrations & oauth flow in
    saas_user_registration_spec
    
    Changelog: changed

--- ee/spec/features/registrations/saas_user_registration_spec.rb
+++ ee/spec/features/registrations/saas_user_registration_spec.rb
@@ -4,6 +4,9 @@ require 'spec_helper'
 
 RSpec.describe "User registration", :js, :saas do
   include AfterNextHelpers
+  include LoginHelpers
+  include DeviseHelpers
+  include TermsHelper
 
   let(:service_with_success) do
     instance_double(GitlabSubscriptions::CreateTrialOrLeadService, execute: ServiceResponse.success)
@@ -19,12 +22,21 @@ RSpec.describe "User registration", :js, :saas do
       # This is the feature flag for the new registration flows where the user is
       # required to provide company details when registering for their company in
       # both the standard registration and trial flows.
-      about_your_company_registration_flow: true
+      about_your_company_registration_flow: true,
+
+      # This is a feature flag to update the single-sign on registration flow
+      # to match the standard registration flow
+      update_oauth_registration_flow: true
     )
 
     stub_application_setting(
       # Saas doesn't require admin approval.
       require_admin_approval_after_user_signup: false
+    )
+
+    stub_omniauth_setting(
+      # users can sign up on saas freely.
+      block_auto_created_users: false
     )
 
     # The groups_and_projects_controller (on `click_on 'Create project'`) is over
@@ -341,6 +353,43 @@ RSpec.describe "User registration", :js, :saas do
             expect(page).to have_content 'Test Group / Learn GitLab'
           end
         end
+      end
+    end
+  end
+
+  describe "single-sign on registration flow" do
+    before do
+      stub_omniauth_provider(provider)
+      register_via(provider, uid, email)
+      clear_browser_session
+
+      # terms are enforced by default in saas
+      enforce_terms
+    end
+
+    around do |example|
+      with_omniauth_full_host { example.run }
+    end
+
+    context "when provider sends verified email address" do
+      let(:provider) { 'github' }
+      let(:uid) { 'my-uid' }
+      let(:email) { 'user@github.com' }
+
+      it "presents the initial welcome step" do
+        expect(page).to have_current_path users_sign_up_welcome_path
+        expect(page).to have_content('Welcome to GitLab, mockuser!')
+      end
+    end
+
+    context "when provider does not send a verified email address" do
+      let(:provider) { 'github' }
+      let(:uid) { 'my-uid' }
+      let(:email) { 'temp-email-for-oauth@email.com' }
+
+      it "presents the profile page to add an email address" do
+        expect(page).to have_current_path profile_path
+        expect(page).to have_content('Please complete your profile with email address')
       end
     end
   end
