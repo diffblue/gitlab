8cfe268e7e8550a59984e9f5e4a1ff2b6d6fc709
Rostyslav Safonov <elhowm@gmail.com>
2022-06-20 00:11:21 +0300

    [epic 354234] Add github_importer_issue_events_import feature flag

--- app/workers/gitlab/github_import/stage/import_issue_events_worker.rb
+++ app/workers/gitlab/github_import/stage/import_issue_events_worker.rb
@@ -16,14 +16,30 @@ module Gitlab
         # project - An instance of Project.
         def import(client, project)
           importer = ::Gitlab::GithubImport::Importer::SingleEndpointIssueEventsImporter
+          return skip_to_next_stage(project, importer) if feature_disabled?(project)
+
+          start_importer(project, importer, client)
+        end
+
+        private
+
+        def start_importer(project, importer, client)
           info(project.id, message: "starting importer", importer: importer.name)
           waiter = importer.new(project, client).execute
+          move_to_next_stage(project, waiter.key => waiter.jobs_remaining)
+        end
 
-          AdvanceStageWorker.perform_async(
-            project.id,
-            { waiter.key => waiter.jobs_remaining },
-            :notes
-          )
+        def skip_to_next_stage(project, importer)
+          info(project.id, message: "skipping importer", importer: importer.name)
+          move_to_next_stage(project)
+        end
+
+        def move_to_next_stage(project, waiters = {})
+          AdvanceStageWorker.perform_async(project.id, waiters, :notes)
+        end
+
+        def feature_disabled?(project)
+          Feature.disabled?(:github_importer_issue_events_import, project.group, type: :ops)
         end
       end
     end
--- spec/workers/gitlab/github_import/stage/import_issue_events_worker_spec.rb
+++ spec/workers/gitlab/github_import/stage/import_issue_events_worker_spec.rb
@@ -3,15 +3,21 @@
 require 'spec_helper'
 
 RSpec.describe Gitlab::GithubImport::Stage::ImportIssueEventsWorker do
+  subject(:worker) { described_class.new }
+
   let(:project) { create(:project) }
-  let(:import_state) { create(:import_state, project: project) }
-  let(:worker) { described_class.new }
+  let!(:group) { create(:group, projects: [project]) }
+  let(:feature_flag_state) { [group] }
 
   describe '#import' do
     let(:importer) { instance_double('Gitlab::GithubImport::Importer::SingleEndpointIssueEventsImporter') }
     let(:client) { instance_double('Gitlab::GithubImport::Client') }
 
-    it 'imports all the pull requests' do
+    before do
+      stub_feature_flags(github_importer_issue_events_import: feature_flag_state)
+    end
+
+    it 'imports all the issue events' do
       waiter = Gitlab::JobWaiter.new(2, '123')
 
       expect(Gitlab::GithubImport::Importer::SingleEndpointIssueEventsImporter)
@@ -26,6 +32,17 @@ RSpec.describe Gitlab::GithubImport::Stage::ImportIssueEventsWorker do
         .with(project.id, { '123' => 2 }, :notes)
 
       worker.import(client, project)
+    end
+
+    context 'when feature flag is disabled' do
+      let(:feature_flag_state) { false }
+
+      it 'skips issue events import and calls next stage' do
+        expect(Gitlab::GithubImport::Importer::SingleEndpointIssueEventsImporter).not_to receive(:new)
+        expect(Gitlab::GithubImport::AdvanceStageWorker).to receive(:perform_async).with(project.id, {}, :notes)
+
+        worker.import(client, project)
+      end
     end
   end
 end
