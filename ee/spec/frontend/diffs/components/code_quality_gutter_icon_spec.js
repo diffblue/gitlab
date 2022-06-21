2e0433aada7d1b3a688d680a0ecf7ec1e0bd2f78
Heinrich Lee Yu <heinrich@gitlab.com>
2022-06-21 14:24:40 +0000

    Revert "Merge branch 'jnnkl-codequality-findings-diff-inline' into 'master'"
    This reverts merge request !88576
    
    Changelog: other

--- ee/spec/frontend/diffs/components/code_quality_gutter_icon_spec.js
+++ ee/spec/frontend/diffs/components/code_quality_gutter_icon_spec.js
@@ -1,4 +1,4 @@
-import { GlPopover, GlIcon, GlTooltip } from '@gitlab/ui';
+import { GlIcon, GlPopover } from '@gitlab/ui';
 import { shallowMount } from '@vue/test-utils';
 import Vue from 'vue';
 import Vuex from 'vuex';
@@ -6,88 +6,48 @@ import CodeQualityGutterIcon from 'ee/diffs/components/code_quality_gutter_icon.
 import createDiffsStore from 'jest/diffs/create_diffs_store';
 import CodequalityIssueBody from '~/reports/codequality_report/components/codequality_issue_body.vue';
 import { SEVERITY_CLASSES, SEVERITY_ICONS } from '~/reports/codequality_report/constants';
-import {
-  multipleFindings,
-  singularFinding,
-} from '../../../../../spec/frontend/diffs/mock_data/diff_code_quality';
 
 Vue.use(Vuex);
 
-let wrapper;
-const findIcon = () => wrapper.findComponent(GlIcon);
-let store;
-let codequalityDiff;
+describe('EE CodeQualityGutterIcon', () => {
+  let store;
+  let wrapper;
+  let codequalityDiff;
 
-const createComponent = (props = {}, flag = false) => {
-  store = createDiffsStore();
-  store.state.diffs.codequalityDiff = codequalityDiff;
+  const findIcon = () => wrapper.findComponent(GlIcon);
 
-  const payload = {
-    propsData: { ...multipleFindings, ...props },
-    provide: {
-      glFeatures: {
-        refactorCodeQualityInlineFindings: flag,
+  const defaultProps = {
+    filePath: 'index.js',
+    codequality: [
+      {
+        severity: 'major',
+        description:
+          'Function `aVeryLongFunction` has 52 lines of code (exceeds 25 allowed). Consider refactoring.',
+        line: 3,
       },
-    },
-    store,
+      {
+        severity: 'minor',
+        description: 'Arrow function has too many statements (52). Maximum allowed is 30.',
+        line: 3,
+      },
+    ],
   };
 
-  wrapper = shallowMount(CodeQualityGutterIcon, payload);
-};
+  const createComponent = (props = {}, extendStore = () => {}) => {
+    store = createDiffsStore();
+    store.state.diffs.codequalityDiff = codequalityDiff;
 
-describe('EE CodeQualityGutterIcon with flag off', () => {
+    extendStore(store);
+
+    wrapper = shallowMount(CodeQualityGutterIcon, {
+      propsData: { ...defaultProps, ...props },
+      store,
+    });
+  };
+
   afterEach(() => {
     wrapper.destroy();
   });
-
-  it.each(['info', 'minor', 'major', 'critical', 'blocker', 'unknown'])(
-    'shows icon for %s degradation',
-    (severity) => {
-      createComponent({ codequality: [{ severity }] });
-
-      expect(findIcon().exists()).toBe(true);
-      expect(findIcon().attributes()).toMatchObject({
-        class: expect.stringContaining(SEVERITY_CLASSES[severity]),
-        name: SEVERITY_ICONS[severity],
-        size: '12',
-      });
-    },
-  );
-
-  describe('code quality gutter icon', () => {
-    beforeEach(() => {
-      createComponent();
-    });
-
-    it('shows a popover on hover', () => {
-      const popoverTarget = 'codequality-index.js:2';
-
-      wrapper.findComponent(GlIcon).trigger('mouseover');
-
-      expect(wrapper.findComponent(GlPopover).props().target).toBe(popoverTarget);
-    });
-
-    it('passes the issue data into the issue components correctly', () => {
-      const issueProps = wrapper
-        .findAllComponents(CodequalityIssueBody)
-        .wrappers.map((w) => w.props());
-
-      expect(issueProps).toHaveLength(3);
-      expect(issueProps).toEqual(
-        multipleFindings.codequality.map((codequality) => ({
-          issue: {
-            severity: codequality.severity,
-            name: codequality.description,
-          },
-          status: 'neutral',
-        })),
-      );
-    });
-  });
-});
-
-describe('EE CodeQualityGutterIcon with flag on', () => {
-  const containsATooltip = (container) => container.findComponent(GlTooltip).exists();
 
   it.each`
     severity
@@ -102,42 +62,46 @@ describe('EE CodeQualityGutterIcon with flag on', () => {
 
     expect(findIcon().exists()).toBe(true);
     expect(findIcon().attributes()).toMatchObject({
-      class: expect.stringContaining(SEVERITY_CLASSES[severity]),
+      class: `gl-hover-cursor-pointer codequality-severity-icon ${SEVERITY_CLASSES[severity]}`,
       name: SEVERITY_ICONS[severity],
       size: '12',
     });
   });
 
   describe('code quality gutter icon', () => {
-    describe('with multiple findings', () => {
-      beforeEach(() => {
-        createComponent(multipleFindings, true);
-      });
-
-      it('contains a tooltip', () => {
-        expect(containsATooltip(wrapper)).toBe(true);
-      });
-
-      it('displays correct popover text with multiple codequality findings', () => {
-        expect(wrapper.findComponent(GlTooltip).text()).toContain('3 Code quality findings');
-      });
-
-      it('emits showCodeQualityFindings event on click', () => {
-        wrapper.trigger('click');
-        expect(wrapper.emitted('showCodeQualityFindings').length).toBe(1);
-      });
+    beforeEach(() => {
+      createComponent();
     });
-    describe('with singular finding', () => {
-      beforeEach(() => {
-        createComponent(singularFinding, true);
-      });
 
-      it('displays correct popover text with multiple codequality findings', () => {
-        expect(wrapper.findComponent(GlTooltip).text()).toContain('1 Code quality finding');
-      });
+    it('shows a popover on hover', () => {
+      const popoverTarget = 'codequality-index.js:3';
+
+      wrapper.findComponent(GlIcon).trigger('mouseover');
+
+      expect(wrapper.findComponent(GlPopover).props().target).toBe(popoverTarget);
     });
-    afterEach(() => {
-      wrapper.destroy();
+
+    it('passes the issue data into the issue components correctly', () => {
+      const issueProps = wrapper
+        .findAllComponents(CodequalityIssueBody)
+        .wrappers.map((w) => w.props());
+
+      expect(issueProps).toEqual([
+        {
+          issue: {
+            severity: defaultProps.codequality[0].severity,
+            name: defaultProps.codequality[0].description,
+          },
+          status: 'neutral',
+        },
+        {
+          issue: {
+            severity: defaultProps.codequality[1].severity,
+            name: defaultProps.codequality[1].description,
+          },
+          status: 'neutral',
+        },
+      ]);
     });
   });
 });
