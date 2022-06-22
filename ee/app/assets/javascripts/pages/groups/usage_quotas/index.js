810fb89ad082624b23ebcd000c115784681cec3d
Zack Cuddy <zcuddy@gitlab.com>
2022-06-22 14:14:09 +0000

    GlTabsBehavior - Support hash updating
    This change adds hash updating
    functionality to GlTabsBehavior
    and converts the existing
    implementations over to it.
    
    This also fixes "jumping around"
    behavior when clicking on tabs
    in these cases.
    
    Changelog: fixed

--- ee/app/assets/javascripts/pages/groups/usage_quotas/index.js
+++ ee/app/assets/javascripts/pages/groups/usage_quotas/index.js
@@ -3,29 +3,8 @@ import SeatUsageApp from 'ee/usage_quotas/seats';
 import initPipelineUsageApp from 'ee/usage_quotas/pipelines';
 import initNamespaceStorage from 'ee/usage_quotas/storage/init_namespace_storage';
 import initCiMinutesUsageApp from 'ee/usage_quotas/ci_minutes_usage';
-import { GlTabsBehavior, TAB_SHOWN_EVENT } from '~/tabs';
+import { GlTabsBehavior, TAB_SHOWN_EVENT, HISTORY_TYPE_HASH } from '~/tabs';
 import { trackAddToCartUsageTab } from '~/google_tag_manager';
-import { historyReplaceState } from '~/lib/utils/common_utils';
-
-/**
- * onTabChange and loadInitialTab are both set to be removed with https://gitlab.com/gitlab-org/gitlab/-/issues/364037.
- *
- * These are workaround as part of refactoring LinkedTabs -> GlTabsBehavior.
- * Currently GlTabsBehavior doesn't support hash manipulation while LinkedTabs did out of the box.
- * To preserve the behavior during the refactor adding this logic here has been the path of least resistance.
- * Ideally GlTabsBehavior will handle this logic internally and will be implemented in the above mentioned issue.
- */
-const onTabChange = (tabsEl) => {
-  tabsEl.addEventListener(TAB_SHOWN_EVENT, (event) => {
-    const tab = event.target;
-    historyReplaceState(tab.getAttribute('href'));
-  });
-};
-
-const loadInitialTab = (glTabs) => {
-  const tab = glTabs.tabList.querySelector(`a[href="${window.location.hash}"]`);
-  glTabs.activateTab(tab || glTabs.activeTab);
-};
 
 const initGlTabs = () => {
   const tabsEl = document.querySelector('.js-storage-tabs');
@@ -33,9 +12,8 @@ const initGlTabs = () => {
     return;
   }
 
-  const glTabs = new GlTabsBehavior(tabsEl);
-  onTabChange(tabsEl);
-  loadInitialTab(glTabs);
+  // eslint-disable-next-line no-new
+  new GlTabsBehavior(tabsEl, { history: HISTORY_TYPE_HASH });
 };
 
 /**
