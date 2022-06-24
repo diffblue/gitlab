5304dbcfc89d2ee9a26d923cecbed4ca5c5bde19
KunQian <kunqian@gitlab.cn>
2022-06-24 17:47:46 +0800

    Add frontend form password complexity validation
    Make password complexity validation work on password setting pages after this feature is configured in admin settings
    
    Changelog: added
    EE: true

---
+++ ee/spec/frontend/admin/signup_restrictions/components/password_complexity_checkbox_group_spec.js
@@ -0,0 +1,58 @@
+import { nextTick } from 'vue';
+import { shallowMount } from '@vue/test-utils';
+import PasswordComplexityCheckboxGroup from 'ee/pages/admin/application_settings/general/components/password_complexity_checkbox_group.vue';
+import SignupCheckbox from '~/pages/admin/application_settings/general/components/signup_checkbox.vue';
+import { extendedWrapper } from 'helpers/vue_test_utils_helper';
+import { mockData } from 'jest/admin/signup_restrictions/mock_data';
+
+describe('Password Checkbox Group', () => {
+  let wrapper;
+  const {
+    passwordNumberRequired,
+    passwordLowercaseRequired,
+    passwordUppercaseRequired,
+    passwordSymbolRequired,
+  } = mockData;
+
+  const mountComponent = () => {
+    wrapper = extendedWrapper(
+      shallowMount(PasswordComplexityCheckboxGroup, {
+        provide: mockData,
+        stubs: {
+          SignupCheckbox,
+        },
+      }),
+    );
+  };
+
+  afterEach(() => {
+    wrapper.destroy();
+  });
+
+  describe('Signup Checkbox', () => {
+    beforeEach(() => {
+      mountComponent();
+    });
+    it.each`
+      prop                           | propValue                    | expected                     | testId
+      ${'passwordNumberRequired'}    | ${passwordNumberRequired}    | ${passwordNumberRequired}    | ${'password-number-required-checkbox'}
+      ${'passwordLowercaseRequired'} | ${passwordLowercaseRequired} | ${passwordLowercaseRequired} | ${'password-lowercase-required-checkbox'}
+      ${'passwordUppercaseRequired'} | ${passwordUppercaseRequired} | ${passwordUppercaseRequired} | ${'password-uppercase-required-checkbox'}
+      ${'passwordSymbolRequired'}    | ${passwordSymbolRequired}    | ${passwordSymbolRequired}    | ${'password-symbol-required-checkbox'}
+    `(
+      'component form data should be $expected when prop $prop is set to $propValue',
+      async ({ prop, expected, testId }) => {
+        const checkbox = wrapper.findByTestId(testId);
+        expect(checkbox.props('value')).toBe(expected);
+        checkbox.vm.$emit('input', !expected);
+        await nextTick();
+
+        expect(checkbox.props('value')).toBe(!expected);
+        expect(wrapper.emitted('set-password-complexity')).toBeTruthy();
+        expect(wrapper.emitted('set-password-complexity')[0]).toEqual([
+          { name: prop, value: !expected },
+        ]);
+      },
+    );
+  });
+});
---
+++ ee/spec/frontend/admin/signup_restrictions/components/signup_form_spec.js
@@ -0,0 +1,61 @@
+import { shallowMount } from '@vue/test-utils';
+import { extendedWrapper } from 'helpers/vue_test_utils_helper';
+import SignupForm from '~/pages/admin/application_settings/general/components/signup_form.vue';
+import { mockData } from 'jest/admin/signup_restrictions/mock_data';
+
+describe('Signup Form', () => {
+  let wrapper;
+
+  const mountComponent = ({ injectedProps = {}, mountFn = shallowMount, stubs = {} } = {}) => {
+    wrapper = extendedWrapper(
+      mountFn(SignupForm, {
+        provide: {
+          glFeatures: {
+            passwordComplexity: true,
+          },
+          ...mockData,
+          ...injectedProps,
+        },
+        stubs,
+      }),
+    );
+  };
+
+  afterEach(() => {
+    wrapper.destroy();
+  });
+
+  describe('form data', () => {
+    beforeEach(() => {
+      mountComponent();
+    });
+
+    it.each`
+      prop                           | propValue                             | elementSelector                                                | formElementPassedDataType | formElementKey | expected
+      ${'passwordNumberRequired'}    | ${mockData.passwordNumberRequired}    | ${'[name="application_setting[password_number_required]"]'}    | ${'prop'}                 | ${'value'}     | ${mockData.passwordNumberRequired}
+      ${'passwordLowercaseRequired'} | ${mockData.passwordLowercaseRequired} | ${'[name="application_setting[password_lowercase_required]"]'} | ${'prop'}                 | ${'value'}     | ${mockData.passwordLowercaseRequired}
+      ${'passwordUppercaseRequired'} | ${mockData.passwordUppercaseRequired} | ${'[name="application_setting[password_uppercase_required]"]'} | ${'prop'}                 | ${'value'}     | ${mockData.passwordUppercaseRequired}
+      ${'passwordSymbolRequired'}    | ${mockData.passwordSymbolRequired}    | ${'[name="application_setting[password_symbol_required]"]'}    | ${'prop'}                 | ${'value'}     | ${mockData.passwordSymbolRequired}
+    `(
+      'form element $elementSelector gets $expected value for $formElementKey $formElementPassedDataType when prop $prop is set to $propValue',
+      ({ elementSelector, expected, formElementKey, formElementPassedDataType }) => {
+        const formElement = wrapper.find(elementSelector);
+
+        switch (formElementPassedDataType) {
+          case 'attribute':
+            expect(formElement.attributes(formElementKey)).toBe(expected);
+            break;
+          case 'prop':
+            expect(formElement.props(formElementKey)).toBe(expected);
+            break;
+          case 'value':
+            expect(formElement.element.value).toBe(expected);
+            break;
+          default:
+            expect(formElement.props(formElementKey)).toBe(expected);
+            break;
+        }
+      },
+    );
+  });
+});
