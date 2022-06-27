e4631ac355de462b5672c51eb7e1bc05ad396ecf
Brian Williams <bwilliams@gitlab.com>
2022-06-27 21:16:31 +0000

    Add SBoM Component tables and models
    This adds two new tables named `sbom_components` and
    `sbom_components_versions`, along with associated models. This is part
    of a three-phase implementation for adding new models to support the
    tracking of Software Bill of Materials (SBoM) components in GitLab.
    
    Changelog: added

---
+++ ee/spec/models/sbom/component_version_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Sbom::ComponentVersion, type: :model do
+  describe 'associations' do
+    it { is_expected.to belong_to(:component).required }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:version) }
+    it { is_expected.to validate_length_of(:version).is_at_most(255) }
+  end
+end
