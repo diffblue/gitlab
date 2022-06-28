1928603e2e45ea3462118fd2cf21654439f1b698
Manoj M J <mmj@gitlab.com>
2022-06-28 10:43:50 +0200

    Async only refresh

--- app/workers/concerns/waitable_worker.rb
+++ app/workers/concerns/waitable_worker.rb
@@ -7,14 +7,14 @@ module WaitableWorker
     # Schedules multiple jobs and waits for them to be completed.
     def bulk_perform_and_wait(args_list, timeout: 10)
       # Short-circuit: it's more efficient to do small numbers of jobs inline
-      return bulk_perform_inline(args_list) if args_list.size <= 3 && !async_only_refresh?
+      return bulk_perform_inline(args_list) if args_list.size <= 3
 
       # Don't wait if there's too many jobs to be waited for. Not including the
       # waiter allows them to be deduplicated and it skips waiting for jobs that
       # are not likely to finish within the timeout. This assumes we can process
       # 10 jobs per second:
       # https://gitlab.com/gitlab-com/gl-infra/scalability/-/issues/205
-      return bulk_perform_async(args_list) if args_list.length >= 10 * timeout
+      return bulk_perform_async(args_list) if (args_list.length >= 10 * timeout) || async_only_refresh?
 
       waiter = Gitlab::JobWaiter.new(args_list.size, worker_label: self.to_s)
 
