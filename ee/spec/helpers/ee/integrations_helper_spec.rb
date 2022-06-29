f914aa74af44f3825e828d2c9587dc764ee7b6e2
Luke Duncalfe <lduncalfe@gitlab.com>
2022-06-29 15:29:44 +0000

    Sanitize ZenTao breadcrumb links
    Merge branch 'security-360540-zentao-xss-links' into 'master'
    
    See merge request gitlab-org/security/gitlab!2539
    
    Changelog: security

--- ee/spec/helpers/ee/integrations_helper_spec.rb
+++ ee/spec/helpers/ee/integrations_helper_spec.rb
@@ -112,6 +112,48 @@ RSpec.describe EE::IntegrationsHelper do
     end
   end
 
+  describe '#jira_issue_breadcrumb_link' do
+    let(:expected_html) { '<img width="15" height="15" class="gl-mr-2 lazy" data-src="/assets/illustrations/logos/jira-d90a9462f8323a5a2d9aef3c3bbb5c8a40275419aabf3cfbe6826113162b18a1.svg" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" /><a rel="noopener noreferrer" class="gl-display-flex gl-align-items-center gl-white-space-nowrap" href="">my-ref</a>' }
+
+    subject { helper.jira_issue_breadcrumb_link(issue_reference) }
+
+    context 'with a valid issue_reference' do
+      let(:issue_reference) { 'my-ref' }
+
+      it 'returns the correct HTML' do
+        is_expected.to eq(expected_html)
+      end
+    end
+
+    context 'when issue_reference contains HTML' do
+      let(:issue_reference) { "<script>alert('XSS')</script>my-ref" }
+
+      it 'strips all tags' do
+        is_expected.to eq(expected_html)
+      end
+    end
+  end
+
+  describe '#zentao_issue_breadcrumb_link' do
+    subject { helper.zentao_issue_breadcrumb_link(issue_json) }
+
+    context 'with valid issue JSON' do
+      let(:issue_json) { { id: "my-ref", web_url: "https://example.com" } }
+
+      it 'returns the correct HTML' do
+        is_expected.to eq('<img width="15" height="15" class="gl-mr-2 lazy" data-src="/assets/logos/zentao-91a4a40cfe1a1640cb4fcf645db75e0ce23fbb9984f649c0675e616d6ff8c632.svg" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" /><a target="_blank" rel="noopener noreferrer" class="gl-display-flex gl-align-items-center gl-white-space-nowrap" href="https://example.com">my-ref</a>')
+      end
+    end
+
+    context 'when issue_reference contains XSS' do
+      let(:issue_json) { { id: "<script>alert('XSS')</script>my-ref", web_url: "javascript:alert('XSS')" } }
+
+      it 'strips all tags and sanitizes' do
+        is_expected.to eq('<img width="15" height="15" class="gl-mr-2 lazy" data-src="/assets/logos/zentao-91a4a40cfe1a1640cb4fcf645db75e0ce23fbb9984f649c0675e616d6ff8c632.svg" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" /><a target="_blank" rel="noopener noreferrer" class="gl-display-flex gl-align-items-center gl-white-space-nowrap">my-ref</a>')
+      end
+    end
+  end
+
   describe '#gitlab_slack_application_data' do
     let_it_be(:projects) { create_list(:project, 3) }
 
