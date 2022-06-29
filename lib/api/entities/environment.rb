c61c746453ea4575c6bf297aa9e278b3af3796c2
Rajendra Kadam <rkadam@gitlab.com>
2022-06-29 11:57:15 +0530

    Remove unused private methods

--- lib/api/entities/environment.rb
+++ lib/api/entities/environment.rb
@@ -4,32 +4,11 @@ module API
   module Entities
     class Environment < Entities::EnvironmentBasic
       include RequestAwareEntity
-      include Gitlab::Utils::StrongMemoize
 
       expose :tier
       expose :project, using: Entities::BasicProjectDetails
       expose :last_deployment, using: Entities::Deployment, if: { last_deployment: true }
       expose :state
-
-      private
-
-      alias_method :environment, :object
-
-      def can_read_pod_logs?
-        strong_memoize(:can_read_pod_logs) do
-          current_user&.can?(:read_pod_logs, environment.project)
-        end
-      end
-
-      def cluster
-        strong_memoize(:cluster) do
-          environment&.last_deployment&.cluster
-        end
-      end
-
-      def current_user
-        options[:current_user]
-      end
     end
   end
 end
