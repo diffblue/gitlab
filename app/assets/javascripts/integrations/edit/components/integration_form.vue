ffb5059096d39116a276923e8f94fca6e5cf6dad
Justin Ho Tuan Duong <hduong@gitlab.com>
2022-06-30 16:05:59 +0000

    Show more details for testing integration error
    For some integrations, the response when testing an
    integration includes "service_response" which provides
    a few more details why an integration has failed.
    
    This change adds this so the user has more info on
    how to fix the issue.
    
    Changelog: changed

--- app/assets/javascripts/integrations/edit/components/integration_form.vue
+++ app/assets/javascripts/integrations/edit/components/integration_form.vue
@@ -140,15 +140,24 @@ export default {
       this.isTesting = true;
 
       testIntegrationSettings(this.propsSource.testPath, this.getFormData())
-        .then(({ data: { error, message = I18N_FETCH_TEST_SETTINGS_DEFAULT_ERROR_MESSAGE } }) => {
-          if (error) {
-            this.setIsValidated();
-            this.$toast.show(message);
-            return;
-          }
+        .then(
+          ({
+            data: {
+              error,
+              message = I18N_FETCH_TEST_SETTINGS_DEFAULT_ERROR_MESSAGE,
+              service_response: serviceResponse,
+            },
+          }) => {
+            if (error) {
+              const errorMessage = serviceResponse ? [message, serviceResponse].join(' ') : message;
+              this.setIsValidated();
+              this.$toast.show(errorMessage);
+              return;
+            }
 
-          this.$toast.show(I18N_SUCCESSFUL_CONNECTION_MESSAGE);
-        })
+            this.$toast.show(I18N_SUCCESSFUL_CONNECTION_MESSAGE);
+          },
+        )
         .catch((error) => {
           this.$toast.show(I18N_DEFAULT_ERROR_MESSAGE);
           Sentry.captureException(error);
