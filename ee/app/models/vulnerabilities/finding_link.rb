3983c4c90803e07a93599720ced4fe70ccedff41
Mehmet Emin INAC <minac@gitlab.com>
2022-07-13 21:07:46 +0300

    Fix attribute validations for the `Vulnerabilities::FindingLink` model
    Length validations for the `name` and `url` attributes were mixed up.
    
    Changelog: fixed
    EE: true

--- ee/app/models/vulnerabilities/finding_link.rb
+++ ee/app/models/vulnerabilities/finding_link.rb
@@ -7,8 +7,8 @@ module Vulnerabilities
     belongs_to :finding, class_name: 'Vulnerabilities::Finding', inverse_of: :finding_links, foreign_key: 'vulnerability_occurrence_id'
 
     validates :finding, presence: true
-    validates :url, presence: true, length: { maximum: 255 }
-    validates :name, length: { maximum: 2048 }
+    validates :url, presence: true, length: { maximum: 2048 }
+    validates :name, length: { maximum: 255 }
 
     scope :by_finding_id, -> (finding_ids) { where(vulnerability_occurrence_id: finding_ids) }
   end
