40e3207bf1a1e989bcb186d992b9b42d6d7d53e5
Aditya Tiwari <atiwari@gitlab.com>
2022-07-14 23:23:07 +0000

    Fixes a typo in constants.js
    - https://gitlab.com/gitlab-org/gitlab/-/merge_requests/92336

--- ee/app/assets/javascripts/security_configuration/api_fuzzing/utils.js
+++ ee/app/assets/javascripts/security_configuration/api_fuzzing/utils.js
@@ -3,10 +3,7 @@ import {
   API_FUZZING_SCAN_MODE_PLACEHOLDER,
   API_FUZZING_SPECIFICATION_FILE_PATH_PLACEHOLDER,
   API_FUZZING_PROFILE_PLACEHOLDER,
-  API_FUZZING_AUTH_PASSWORD_VAR_PLACEHOLDER,
-  API_FUZZING_AUTH_USERNAME_VAR_PLACEHOLDER,
   API_FUZZING_YAML_CONFIGURATION_TEMPLATE,
-  API_FUZZING_YAML_CONFIGURATION_AUTH_TEMPLATE,
 } from './constants';
 
 export const buildConfigurationSnippet = ({
@@ -30,10 +27,10 @@ export const buildConfigurationSnippet = ({
     .replace(API_FUZZING_PROFILE_PLACEHOLDER, scanProfile);
 
   if (authUsername && authPassword) {
-    template += API_FUZZING_YAML_CONFIGURATION_AUTH_TEMPLATE.replace(
-      API_FUZZING_AUTH_USERNAME_VAR_PLACEHOLDER,
-      authUsername,
-    ).replace(API_FUZZING_AUTH_PASSWORD_VAR_PLACEHOLDER, authPassword);
+    // eslint-disable-next-line @gitlab/require-i18n-strings
+    template += `
+  FUZZAPI_HTTP_USERNAME: "${authUsername}"
+  FUZZAPI_HTTP_PASSWORD: "${authPassword}"`;
   }
   return template;
 };
