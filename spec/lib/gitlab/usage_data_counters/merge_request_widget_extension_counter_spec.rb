9a7d24d06055cf33e7aee5567c52c056ae9f4825
Thomas Randolph <trandolph@gitlab.com>
2022-07-14 22:36:02 -0600

    Include `code_review` in the counter spec

--- spec/lib/gitlab/usage_data_counters/merge_request_widget_extension_counter_spec.rb
+++ spec/lib/gitlab/usage_data_counters/merge_request_widget_extension_counter_spec.rb
@@ -5,5 +5,5 @@ require 'spec_helper'
 RSpec.describe Gitlab::UsageDataCounters::MergeRequestWidgetExtensionCounter do
   it_behaves_like 'a redis usage counter', 'Widget Extension', :test_summary_count_expand
 
-  it_behaves_like 'a redis usage counter with totals', :i_merge_request_widget, test_summary_count_expand: 5
+  it_behaves_like 'a redis usage counter with totals', :i_code_review_merge_request_widget, test_summary_count_expand: 5
 end
