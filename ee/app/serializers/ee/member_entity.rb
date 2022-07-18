6d3734f3f269381a749be5b341a888ac22b181c8
eugielimpin <elimpin@gitlab.com>
2022-07-18 10:58:38 +0800

    Use alias to expose can_unban?

--- ee/app/serializers/ee/member_entity.rb
+++ ee/app/serializers/ee/member_entity.rb
@@ -21,9 +21,7 @@ module EE
 
       expose :provisioned_by_this_group?, as: :provisioned_by_this_group
 
-      expose :can_unban do |member|
-        member.can_unban?
-      end
+      expose :can_unban?, as: :can_unban
 
       expose :banned do |member|
         member.user.present? && member.user.banned_from_namespace?(group)
