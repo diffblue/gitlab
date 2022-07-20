715dc6f4edc3d66521e1e107c4e98241498a5ccb
Kamil Trzciński <ayufan@ayufan.eu>
2022-07-20 17:42:35 +0200

    Revert changes to `migrations_helpers.rb`
    This brings back old `database:` support
    which is slightly broken.

--- spec/support_specs/helpers/migrations_helpers_spec.rb
+++ spec/support_specs/helpers/migrations_helpers_spec.rb
@@ -17,12 +17,58 @@ RSpec.describe MigrationsHelpers do
     it 'returns the main base model' do
       expect(helper.active_record_base).to eq(ActiveRecord::Base)
     end
+
+    context 'ci database configured' do
+      before do
+        skip_if_multiple_databases_not_setup
+      end
+
+      it 'returns the CI base model' do
+        expect(helper.active_record_base(database: :ci)).to eq(Ci::ApplicationRecord)
+      end
+    end
+
+    context 'ci database not configured' do
+      before do
+        skip_if_multiple_databases_are_setup
+      end
+
+      it 'returns the main base model' do
+        expect(helper.active_record_base(database: :ci)).to eq(ActiveRecord::Base)
+      end
+    end
+
+    it 'raises ArgumentError for bad database argument' do
+      expect { helper.active_record_base(database: :non_existent) }.to raise_error(ArgumentError)
+    end
   end
 
   describe '#table' do
     it 'creates a class based on main base model' do
       klass = helper.table(:projects)
       expect(klass.connection_specification_name).to eq('ActiveRecord::Base')
+    end
+
+    context 'ci database configured' do
+      before do
+        skip_if_multiple_databases_not_setup
+      end
+
+      it 'create a class based on the CI base model' do
+        klass = helper.table(:ci_builds, database: :ci)
+        expect(klass.connection_specification_name).to eq('Ci::ApplicationRecord')
+      end
+    end
+
+    context 'ci database not configured' do
+      before do
+        skip_if_multiple_databases_are_setup
+      end
+
+      it 'creates a class based on main base model' do
+        klass = helper.table(:ci_builds, database: :ci)
+        expect(klass.connection_specification_name).to eq('ActiveRecord::Base')
+      end
     end
   end
 end
