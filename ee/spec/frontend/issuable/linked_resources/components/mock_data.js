fa9f772a8c77bc4502fb7d78fd69ab6433c3cb7e
Rajendra Kadam <rkadam@gitlab.com>
2022-07-21 12:48:44 +0530

    Fix linter error for GQL query

--- ee/spec/frontend/issuable/linked_resources/components/mock_data.js
+++ ee/spec/frontend/issuable/linked_resources/components/mock_data.js
@@ -1,30 +1,18 @@
 export const mockResourceLinks = [
   {
     id: 'gid://gitlab/IncidentManagement::IssuableResourceLink/1',
-    issue: {
-      id: 'gid://gitlab/Issue/455',
-      title: 'Demo incident',
-    },
     link: 'http://docs.gitlab.com/incident-info',
     linkType: 'zoom',
     linkText: 'Doclink for incident',
   },
   {
     id: 'gid://gitlab/IncidentManagement::IssuableResourceLink/2',
-    issue: {
-      id: 'gid://gitlab/Issue/455',
-      title: 'Demo incident',
-    },
     link: 'http://docs.gitlab.com/incident-info2',
     linkType: 'zoom',
     linkText: 'Doclink for incident 2',
   },
   {
     id: 'gid://gitlab/IncidentManagement::IssuableResourceLink/3',
-    issue: {
-      id: 'gid://gitlab/Issue/455',
-      title: 'Demo incident',
-    },
     link: 'http://docs.gitlab.com/incident-info3',
     linkType: 'zoom',
     linkText: 'Doclink for incident 3',
@@ -34,6 +22,7 @@ export const mockResourceLinks = [
 export const resourceLinksListResponse = {
   data: {
     issue: {
+      id: 'gid://gitlab/Issue/455',
       issuableResourceLinks: {
         nodes: mockResourceLinks,
       },
@@ -44,6 +33,7 @@ export const resourceLinksListResponse = {
 export const resourceLinksEmptyResponse = {
   data: {
     issue: {
+      id: 'gid://gitlab/Issue/456',
       issuableResourceLinks: {
         nodes: [],
       },
