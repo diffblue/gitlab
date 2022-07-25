ab4aa0a04b03cd8aeb12d3d6e05098febd98868c
George Koltsov <gkoltsov@gitlab.com>
2022-07-25 12:30:37 +0000

    Remove prohibited branches after project import
    Merge branch 'security-remove-prohibited-branches-after-import' into 'master'
    
    See merge request gitlab-org/security/gitlab!2587
    
    Changelog: security

--- app/models/project.rb
+++ app/models/project.rb
@@ -2116,6 +2116,7 @@ class Project < ApplicationRecord
   end
 
   def after_import
+    repository.remove_prohibited_branches
     repository.expire_content_cache
     wiki.repository.expire_content_cache
 
