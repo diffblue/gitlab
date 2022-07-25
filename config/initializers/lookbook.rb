146263102257eceb2fbbc4af3444a56003c055c0
Thomas Hutterer <thutterer@gitlab.com>
2022-07-25 18:30:13 +0200

    Fix undercoverage warning
    This code is only active in RAILS_ENV=development.

--- config/initializers/lookbook.rb
+++ config/initializers/lookbook.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 if Rails.env.development?
+  # :nocov: Lookbook is only available in development
   Lookbook::ApplicationController.class_eval do
     content_security_policy false
   end
@@ -9,4 +10,5 @@ if Rails.env.development?
     config.lookbook.experimental_features = [:pages]
     config.lookbook.page_paths = ["#{config.root}/spec/components/docs"]
   end
+  # :nocov:
 end
