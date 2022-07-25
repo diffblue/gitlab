90653ac0e3e159ed3b3a24de36bed7cfd93b453b
Pedro Pombeiro <noreply@pedro.pombei.ro>
2022-07-25 11:51:08 +0200

    Change runner cron job to run daily
    Change ci_runner_versions_reconciliation_worker  to @daily
    with randomness for better workload distribution
    
    Changelog: changed

--- app/workers/ci/runners/reconcile_existing_runner_versions_cron_worker.rb
+++ app/workers/ci/runners/reconcile_existing_runner_versions_cron_worker.rb
@@ -13,8 +13,22 @@ module Ci
       urgency :low
 
       idempotent!
+      deduplicate :until_executing
 
-      def perform
+      def perform(cronjob_scheduled = true)
+        if cronjob_scheduled
+          # Introduce some randomness across the day so that instances don't all hit the GitLab Releases API
+          # around the same time of day
+          period = rand(0..ActiveSupport::Duration::SECONDS_PER_DAY)
+          self.class.perform_in(period, false)
+
+          Sidekiq.logger.info(
+            class: self.class.name,
+            message: "rescheduled job for #{period.seconds.from_now}")
+
+          return
+        end
+
         result = ::Ci::Runners::ReconcileExistingRunnerVersionsService.new.execute
         result.each { |key, value| log_extra_metadata_on_done(key, value) }
       end
