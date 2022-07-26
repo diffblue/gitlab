086bc6904ae72cf5c5581f070225d7374756d808
Erick Banks <ebanks@gitlab.com>
2022-07-26 18:59:40 +0000

    Reformat search tests and aggregate failures

--- qa/qa/specs/features/ee/api/9_enablement/elasticsearch/elasticsearch_api_spec.rb
+++ qa/qa/specs/features/ee/api/9_enablement/elasticsearch/elasticsearch_api_spec.rb
@@ -4,7 +4,12 @@ require 'airborne'
 
 module QA
   RSpec.describe 'Enablement:Search' do
-    describe 'When using elasticsearch API to search for a known blob', :orchestrated, :elasticsearch, :requires_admin do
+    describe(
+      'When using elasticsearch API to search for a known blob',
+      :orchestrated,
+      :elasticsearch,
+      :requires_admin
+    ) do
       let(:project_file_content) { "elasticsearch: #{SecureRandom.hex(8)}" }
       let(:non_member_user) { Resource::User.fabricate_or_use('non_member_user', 'non_member_user_password') }
       let(:api_client) { Runtime::API::Client.new(:gitlab) }
@@ -37,7 +42,10 @@ module QA
         end
       end
 
-      it 'searches public project and finds a blob as an non-member user', testcase: 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/348063' do
+      it(
+        'searches public project and finds a blob as an non-member user',
+        testcase: 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/348063'
+      ) do
         successful_search(non_member_api_client)
       end
 
@@ -46,15 +54,26 @@ module QA
           project.set_visibility(:private)
         end
 
-        it 'finds a blob as an authorized user', testcase: 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/348064' do
+        it(
+          'finds a blob as an authorized user',
+          testcase: 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/348064'
+        ) do
           successful_search(api_client)
         end
 
-        it 'does not find a blob as an non-member user', testcase: 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/348065' do
-          QA::Support::Retrier.retry_on_exception(max_attempts: Runtime::Search::RETRY_MAX_ITERATION, sleep_interval: Runtime::Search::RETRY_SLEEP_INTERVAL) do
-            get Runtime::Search.create_search_request(non_member_api_client, 'blobs', project_file_content).url
-            expect_status(QA::Support::API::HTTP_STATUS_OK)
-            expect(json_body).to be_empty
+        it(
+          'does not find a blob as an non-member user',
+          testcase: 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/348065'
+        ) do
+          QA::Support::Retrier.retry_on_exception(
+            max_attempts: Runtime::Search::RETRY_MAX_ITERATION,
+            sleep_interval: Runtime::Search::RETRY_SLEEP_INTERVAL
+          ) do
+            get(Runtime::Search.create_search_request(non_member_api_client, 'blobs', project_file_content).url)
+            aggregate_failures do
+              expect_status(QA::Support::API::HTTP_STATUS_OK)
+              expect(json_body).to be_empty
+            end
           end
         end
       end
@@ -62,14 +81,17 @@ module QA
       private
 
       def successful_search(api_client)
-        QA::Support::Retrier.retry_on_exception(max_attempts: Runtime::Search::RETRY_MAX_ITERATION, sleep_interval: Runtime::Search::RETRY_SLEEP_INTERVAL) do
-          get Runtime::Search.create_search_request(api_client, 'blobs', project_file_content).url
+        QA::Support::Retrier.retry_on_exception(
+          max_attempts: Runtime::Search::RETRY_MAX_ITERATION,
+          sleep_interval: Runtime::Search::RETRY_SLEEP_INTERVAL
+        ) do
+          get(Runtime::Search.create_search_request(api_client, 'blobs', project_file_content).url)
           expect_status(QA::Support::API::HTTP_STATUS_OK)
-
-          raise 'Empty search result returned' if json_body.empty?
-
-          expect(json_body[0][:data]).to match(project_file_content)
-          expect(json_body[0][:project_id]).to equal(project.id)
+          aggregate_failures do
+            expect(json_body).not_to be_empty
+            expect(json_body[0][:data]).to match(project_file_content)
+            expect(json_body[0][:project_id]).to equal(project.id)
+          end
         end
       end
     end
--- qa/qa/specs/features/ee/api/9_enablement/elasticsearch/index_tests/main_index/blob_index_spec.rb
+++ qa/qa/specs/features/ee/api/9_enablement/elasticsearch/index_tests/main_index/blob_index_spec.rb
@@ -4,7 +4,12 @@ require 'airborne'
 
 module QA
   RSpec.describe 'Enablement:Search' do
-    describe 'When using elasticsearch API to search for a public blob', :orchestrated, :elasticsearch, :requires_admin do
+    describe(
+      'When using elasticsearch API to search for a public blob',
+      :orchestrated,
+      :elasticsearch,
+      :requires_admin
+    ) do
       let(:api_client) { Runtime::API::Client.new(:gitlab) }
 
       let(:project) do
@@ -38,15 +43,21 @@ module QA
         project.remove_via_api!
       end
 
-      it 'finds blob that matches file content', testcase: 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/347632' do
-        QA::Support::Retrier.retry_on_exception(max_attempts: Runtime::Search::RETRY_MAX_ITERATION, sleep_interval: Runtime::Search::RETRY_SLEEP_INTERVAL) do
-          get Runtime::Search.create_search_request(api_client, 'blobs', project_file_content).url
-          expect_status(QA::Support::API::HTTP_STATUS_OK)
-
-          raise 'Empty search result returned' if json_body.empty?
-
-          expect(json_body[0][:data]).to match(project_file_content)
-          expect(json_body[0][:project_id]).to equal(project.id)
+      it(
+        'finds blob that matches file content',
+        testcase: 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/347632'
+      ) do
+        QA::Support::Retrier.retry_on_exception(
+          max_attempts: Runtime::Search::RETRY_MAX_ITERATION,
+          sleep_interval: Runtime::Search::RETRY_SLEEP_INTERVAL
+        ) do
+          get(Runtime::Search.create_search_request(api_client, 'blobs', project_file_content).url)
+          aggregate_failures do
+            expect_status(QA::Support::API::HTTP_STATUS_OK)
+            expect(json_body).not_to be_empty
+            expect(json_body[0][:data]).to match(project_file_content)
+            expect(json_body[0][:project_id]).to equal(project.id)
+          end
         end
       end
     end
