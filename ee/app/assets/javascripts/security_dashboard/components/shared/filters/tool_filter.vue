2e8a9a34834b5495e0b21d6bdccd6082a23d6029
Samantha Ming <sming@gitlab.com>
2022-07-28 22:17:38 +0200

    Display all option in tool dropdown
    Issue: https://gitlab.com/gitlab-org/gitlab/-/issues/368250

--- ee/app/assets/javascripts/security_dashboard/components/shared/filters/tool_filter.vue
+++ ee/app/assets/javascripts/security_dashboard/components/shared/filters/tool_filter.vue
@@ -1,13 +1,22 @@
 <script>
 import SimpleFilter from './simple_filter.vue';
 import FilterBody from './filter_body.vue';
+import FilterItem from './filter_item.vue';
 
 export default {
-  components: { FilterBody },
+  components: { FilterBody, FilterItem },
   extends: SimpleFilter,
 };
 </script>
 
 <template>
-  <filter-body :name="filter.name" :selected-options="selectedOptionsOrAll" />
+  <filter-body :name="filter.name" :selected-options="selectedOptionsOrAll">
+    <filter-item
+      v-if="filter.allOption"
+      :is-checked="isNoOptionsSelected"
+      :text="filter.allOption.name"
+      data-testid="all"
+      @click="deselectAllOptions"
+    />
+  </filter-body>
 </template>
