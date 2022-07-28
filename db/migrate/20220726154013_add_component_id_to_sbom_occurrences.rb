c73b6368b6acd095bd42ff259a7b28b7d302a303
Brian Williams <bwilliams@gitlab.com>
2022-07-28 12:15:18 -0500

    Ensure that sbom_occurrences table is empty prior to migrating

--- db/migrate/20220726154013_add_component_id_to_sbom_occurrences.rb
+++ db/migrate/20220726154013_add_component_id_to_sbom_occurrences.rb
@@ -4,8 +4,11 @@ class AddComponentIdToSbomOccurrences < Gitlab::Database::Migration[2.0]
   enable_lock_retries!
 
   def up
-    # Code using this table has not been implemented yet, so it should be empty.
-    add_column :sbom_occurrences, :component_id, :bigint, null: false  # rubocop:disable Rails/NotNullColumn
+    # Code using this table has not been implemented yet.
+    # The migration prior to this one ensures that it is empty.
+    # rubocop:disable Rails/NotNullColumn
+    add_column :sbom_occurrences, :component_id, :bigint, null: false
+    # rubocop:enable Rails/NotNullColumn
   end
 
   def down
