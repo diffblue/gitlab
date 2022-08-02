5237fc31dc8aed4f443189639bbcd1945cd75fe3
Miguel Rincon <miguelrincon@mrincon-laptop.local>
2022-08-02 14:29:59 +0200

    Added custom matcher tests

--- spec/frontend/__helpers__/matchers/to_equal_graphql_fixture.js
+++ spec/frontend/__helpers__/matchers/to_equal_graphql_fixture.js
@@ -2,23 +2,26 @@ import { isEqual } from 'lodash';
 import { stripTypenames } from 'helpers/graphql_helpers';
 
 export function toEqualGraphqlFixture(received, match) {
+  let clearReceived;
   let clearMatch;
 
   try {
+    clearReceived = JSON.parse(JSON.stringify(received));
     clearMatch = stripTypenames(match);
   } catch (e) {
-    return { message: 'The comparator value is not an object', pass: false };
+    return { message: () => 'The comparator value is not an object', pass: false };
   }
-  const pass = isEqual(received, clearMatch);
+  const pass = isEqual(clearReceived, clearMatch);
+  // console.log(this.utils);
   const message = pass
     ? () => `
         Expected to not be: ${this.utils.printExpected(clearMatch)}
-        Received:           ${this.utils.printReceived(received)}
+        Received:           ${this.utils.printReceived(clearReceived)}
         `
     : () =>
         `
       Expected to be: ${this.utils.printExpected(clearMatch)}
-      Received:       ${this.utils.printReceived(received)}
+      Received:       ${this.utils.printReceived(clearReceived)}
       `;
 
   return { actual: received, message, pass };
