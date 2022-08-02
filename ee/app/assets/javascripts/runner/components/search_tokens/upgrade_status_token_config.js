eace3b02c63a86366c39b4853e335fd96bb0363c
Miguel Rincon <mrincon@gitlab.com>
2022-08-02 20:23:10 +0000

    Restrict to upgrade status filters to Ultimate
    This change removes a filter from EE that should only be available
    for Ultimate license users.
    
    Changelog: fixed
    EE: true

--- ee/app/assets/javascripts/runner/components/search_tokens/upgrade_status_token_config.js
+++ ee/app/assets/javascripts/runner/components/search_tokens/upgrade_status_token_config.js
@@ -8,27 +8,38 @@ import {
   PARAM_KEY_UPGRADE_STATUS,
 } from '../../constants';
 
-const options = [
-  { value: UPGRADE_STATUS_AVAILABLE, title: s__('Runners|Available') },
-  { value: UPGRADE_STATUS_RECOMMENDED, title: s__('Runners|Recommended') },
-  { value: UPGRADE_STATUS_NOT_AVAILABLE, title: s__('Runners|Up to date') },
-];
+const getToken = () => {
+  const isAvailable =
+    gon.licensed_features?.runnerUpgradeManagement ||
+    gon.licensed_features?.runnerUpgradeManagementForNamespace;
+  if (!isAvailable) {
+    return null;
+  }
 
-export const upgradeStatusTokenConfig = {
-  icon: 'upgrade',
-  title: s__('Runners|Upgrade Status'),
-  type: PARAM_KEY_UPGRADE_STATUS,
-  token: BaseToken,
-  unique: true,
-  options: options.map(({ value, title }) => ({
-    value,
-    // Replace whitespace with a special character to avoid
-    // splitting this value.
-    // Replacing in each option, as translations may also
-    // contain spaces!
-    // see: https://gitlab.com/gitlab-org/gitlab/-/issues/344142
-    // see: https://gitlab.com/gitlab-org/gitlab-ui/-/issues/1438
-    title: title.replace(/\s/g, '\u00a0'),
-  })),
-  operators: OPERATOR_IS_ONLY,
+  const options = [
+    { value: UPGRADE_STATUS_AVAILABLE, title: s__('Runners|Available') },
+    { value: UPGRADE_STATUS_RECOMMENDED, title: s__('Runners|Recommended') },
+    { value: UPGRADE_STATUS_NOT_AVAILABLE, title: s__('Runners|Up to date') },
+  ];
+
+  return {
+    icon: 'upgrade',
+    title: s__('Runners|Upgrade Status'),
+    type: PARAM_KEY_UPGRADE_STATUS,
+    token: BaseToken,
+    unique: true,
+    options: options.map(({ value, title }) => ({
+      value,
+      // Replace whitespace with a special character to avoid
+      // splitting this value.
+      // Replacing in each option, as translations may also
+      // contain spaces!
+      // see: https://gitlab.com/gitlab-org/gitlab/-/issues/344142
+      // see: https://gitlab.com/gitlab-org/gitlab-ui/-/issues/1438
+      title: title.replace(/\s/g, '\u00a0'),
+    })),
+    operators: OPERATOR_IS_ONLY,
+  };
 };
+
+export const upgradeStatusTokenConfig = getToken();
