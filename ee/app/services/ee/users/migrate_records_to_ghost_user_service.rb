3911bcbf3c90400537ed41caaf6605f80dbf416d
Imre Farkas <ifarkas@gitlab.com>
2022-08-06 09:54:58 +0200

    Add Users::MigrateRecordsToGhostUserService
    The new service extracts parts of the existing Users::DestroyService and
    Users::MigrateToGhostUserService. This will be used by a cron job doing
    interruptible batched migration / deletion of users and associated
    records with limited execution time.

---
+++ ee/app/services/ee/users/migrate_records_to_ghost_user_service.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+module EE
+  module Users
+    module MigrateRecordsToGhostUserService
+      extend ::Gitlab::Utils::Override
+
+      private
+
+      override :migrate_records
+      def migrate_records
+        # these should always be ghosted
+        migrate_resource_iteration_events
+
+        return super if hard_delete
+
+        migrate_epics
+        migrate_requirements_management_requirements
+        migrate_vulnerabilities_feedback
+        migrate_vulnerabilities
+        migrate_vulnerabilities_external_issue_links
+        super
+      end
+
+      override :post_migrate_records
+      def post_migrate_records
+        log_audit_event(user) if super.try(:destroyed?)
+      end
+
+      def migrate_epics
+        batched_migrate(::Epic, :author_id)
+        batched_migrate(::Epic, :last_edited_by_id)
+      end
+
+      def migrate_requirements_management_requirements
+        user.requirements.update_all(author_id: ghost_user.id)
+      end
+
+      def migrate_vulnerabilities_feedback
+        batched_migrate(Vulnerabilities::Feedback, :author_id)
+        batched_migrate(Vulnerabilities::Feedback, :comment_author_id)
+      end
+
+      def migrate_vulnerabilities
+        batched_migrate(::Vulnerability, :author_id)
+      end
+
+      def migrate_vulnerabilities_external_issue_links
+        batched_migrate(Vulnerabilities::ExternalIssueLink, :author_id)
+      end
+
+      def migrate_resource_iteration_events
+        batched_migrate(ResourceIterationEvent, :user_id)
+      end
+
+      def log_audit_event(user)
+        ::AuditEventService.new(
+          initiator_user,
+          user,
+          action: :destroy
+        ).for_user.security_event
+      end
+    end
+  end
+end
---
+++ ee/spec/services/ee/users/migrate_records_to_ghost_user_service_spec.rb
@@ -0,0 +1,98 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Users::MigrateRecordsToGhostUserService do
+  let!(:user) { create(:user) }
+  let(:service) { described_class.new(user, admin) }
+
+  let_it_be(:admin) { create(:admin) }
+
+  context "when migrating a user's associated records to the ghost user" do
+    context 'for epics' do
+      context 'when deleted user is present as both author and edited_user' do
+        include_examples 'migrating records to the ghost user', Epic, [:author, :last_edited_by] do
+          let(:created_record) do
+            create(:epic, group: create(:group), author: user, last_edited_by: user)
+          end
+        end
+      end
+
+      context 'when deleted user is present only as edited_user' do
+        include_examples 'migrating records to the ghost user', Epic, [:last_edited_by] do
+          let(:created_record) { create(:epic, group: create(:group), author: create(:user), last_edited_by: user) }
+        end
+      end
+    end
+
+    context 'for vulnerability_feedback author' do
+      include_examples 'migrating records to the ghost user', Vulnerabilities::Feedback, [:author] do
+        let(:created_record) { create(:vulnerability_feedback, author: user) }
+      end
+    end
+
+    context 'for vulnerability_feedback comment author' do
+      include_examples 'migrating records to the ghost user', Vulnerabilities::Feedback, [:comment_author] do
+        let(:created_record) { create(:vulnerability_feedback, comment_author: user) }
+      end
+    end
+
+    context 'for vulnerability author' do
+      include_examples 'migrating records to the ghost user', Vulnerability, [:author] do
+        let(:created_record) { create(:vulnerability, author: user) }
+      end
+    end
+
+    context 'for vulnerability_external_issue_link author' do
+      include_examples 'migrating records to the ghost user', Vulnerabilities::ExternalIssueLink, [:author] do
+        let(:created_record) { create(:vulnerabilities_external_issue_link, author: user) }
+      end
+    end
+
+    context 'for requirements' do
+      include_examples 'migrating records to the ghost user', RequirementsManagement::Requirement, [:author] do
+        let(:created_record) { create(:requirement, author: user) }
+      end
+    end
+
+    context 'for resource_iteration_events' do
+      let(:always_ghost) { true }
+
+      include_examples 'migrating records to the ghost user', ResourceIterationEvent, [:user] do
+        let(:created_record) do
+          create(:resource_iteration_event, issue: create(:issue),
+                                            user: user,
+                                            iteration: create(:iteration))
+        end
+      end
+    end
+  end
+
+  context 'on post-migrate cleanups' do
+    subject(:operation) { service.execute }
+
+    describe 'audit events' do
+      include_examples 'audit event logging' do
+        let(:fail_condition!) do
+          expect(user).to receive(:destroy).and_return(user)
+          expect(user).to receive(:destroyed?).and_return(false)
+        end
+
+        let(:attributes) do
+          {
+            author_id: admin.id,
+            entity_id: user.id,
+            entity_type: 'User',
+            details: {
+              remove: 'user',
+              author_name: admin.name,
+              target_id: user.id,
+              target_type: 'User',
+              target_details: user.full_path
+            }
+          }
+        end
+      end
+    end
+  end
+end
