eaaa08cfa45989341d9fd1da5ca7283da8e3d01d
Heinrich Lee Yu <heinrich@gitlab.com>
2022-08-10 16:00:49 +0800

    Use GraphQL fixture for mocked responses

--- ee/spec/frontend/issuable/popover/components/issue_popover_spec.js
+++ ee/spec/frontend/issuable/popover/components/issue_popover_spec.js
@@ -1,6 +1,7 @@
 import { shallowMount } from '@vue/test-utils';
 import Vue from 'vue';
 import VueApollo from 'vue-apollo';
+import issueQueryResponse from 'test_fixtures/ee/graphql/issuable/popover/queries/issue.query.graphql.json';
 import issueQuery from 'ee_else_ce/issuable/popover/queries/issue.query.graphql';
 import createMockApollo from 'helpers/mock_apollo_helper';
 import waitForPromises from 'helpers/wait_for_promises';
@@ -11,32 +12,6 @@ describe('Issue Popover', () => {
   let wrapper;
 
   Vue.use(VueApollo);
-
-  const issueQueryResponse = {
-    data: {
-      project: {
-        __typename: 'Project',
-        id: '1',
-        issue: {
-          __typename: 'Issue',
-          id: 'gid://gitlab/Issue/1',
-          createdAt: '2020-07-01T04:08:01Z',
-          state: 'opened',
-          title: 'Issue title',
-          confidential: true,
-          dueDate: '2020-07-05',
-          milestone: {
-            __typename: 'Milestone',
-            id: 'gid://gitlab/Milestone/1',
-            title: '15.2',
-            startDate: '2020-07-01',
-            dueDate: '2020-07-30',
-          },
-          weight: 3,
-        },
-      },
-    },
-  };
 
   const mountComponent = ({
     queryResponse = jest.fn().mockResolvedValue(issueQueryResponse),
