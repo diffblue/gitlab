523bffdf8f8de8df11d29e89222496ab97765275
Vijay Hawoldar <vhawoldar@gitlab.com>
2022-08-10 09:40:27 +0100

    Revert "Adjust migration to remove default value"
    This reverts commit 0d9605b78657f1883a5b9a6b7336382a8eaa6444.

--- db/migrate/20220808133824_add_timestamps_to_project_statistics.rb
+++ db/migrate/20220808133824_add_timestamps_to_project_statistics.rb
@@ -1,14 +1,7 @@
 # frozen_string_literal: true
 
 class AddTimestampsToProjectStatistics < Gitlab::Database::Migration[2.0]
-  def up
+  def change
     add_timestamps_with_timezone(:project_statistics, null: false, default: -> { 'NOW()' })
-
-    change_column_default :project_statistics, :created_at, nil
-    change_column_default :project_statistics, :updated_at, nil
-  end
-
-  def down
-    remove_timestamps(:project_statistics)
   end
 end
