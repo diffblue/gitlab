ab9ebb6bf0daa94826cdb06c4e7718386f7eccae
Jason Zhang <jzhang@gitlab.com>
2022-08-12 16:01:14 -0700

    Rubocop

--- qa/qa/specs/features/browser_ui/3_create/pages/pages_pipeline_spec.rb
+++ qa/qa/specs/features/browser_ui/3_create/pages/pages_pipeline_spec.rb
@@ -45,7 +45,7 @@ module QA
           pages.go_to_access_page
           page.driver.browser.switch_to.window(page.driver.browser.window_handles.last)
           Support::Waiter.wait_until(sleep_interval: 2, max_duration: 60, reload_page: page,
-          retry_on_exception: true) do
+                                     retry_on_exception: true) do
             expect(page).to have_content('Write an awesome description for your new site here.')
           end
         end
