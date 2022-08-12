1b605f1e36ae138f9e913501187c54a692ec8100
Felipe Artur <felipefac@gmail.com>
2022-08-12 16:41:05 -0300

    Improve how fields can bre preloaded for WidgetInterface
    Improvement on how we  preload fields selcted from
    widget interface.
    
    Changelog: other

--- ee/app/graphql/ee/resolvers/work_items_resolver.rb
+++ ee/app/graphql/ee/resolvers/work_items_resolver.rb
@@ -8,20 +8,9 @@ module EE
 
       private
 
-      override :filtered_preloads
-      def filtered_preloads
-        widgets = node_selection&.selection(:widgets)
-        return [] unless widgets&.selected?
-
-        verification_status = widgets.selection(
-          :verification_status,
-          selected_type: ::Types::WorkItems::Widgets::VerificationStatusType
-        )
-
-        return [] unless verification_status&.selected?
-
-        # If verificationStatus field is present we need to preload test reports to prevent N+1 queries
-        [{ requirement: :recent_test_reports }]
+      override :preloads
+      def preloads
+        super.merge(verification_status: { requirement: :recent_test_reports })
       end
     end
   end
