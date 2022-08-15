0fe68f0b66a9af471ebb7739f4495c92b0b0c1cd
Olivier Gonzalez <52092-gonzoyumo@users.noreply.gitlab.com>
2022-08-15 18:10:00 +0000

    Fix broken link to documentation

--- ee/app/helpers/vulnerabilities_helper.rb
+++ ee/app/helpers/vulnerabilities_helper.rb
@@ -20,7 +20,7 @@ module VulnerabilitiesHelper
       create_mr_url: create_vulnerability_feedback_merge_request_path(vulnerability.finding.project),
       discussions_url: discussions_project_security_vulnerability_path(vulnerability.project, vulnerability),
       notes_url: project_security_vulnerability_notes_path(vulnerability.project, vulnerability),
-      related_issues_help_path: help_page_path('user/application_security/index', anchor: 'managing-related-issues-for-a-vulnerability'),
+      related_issues_help_path: help_page_path('user/application_security/vulnerabilities', anchor: 'linking-a-vulnerability-to-issues'),
       pipeline: vulnerability_pipeline_data(pipeline),
       can_modify_related_issues: current_user.can?(:admin_vulnerability_issue_link, vulnerability),
       issue_tracking_help_path: help_page_path('user/project/settings', anchor: 'sharing-and-permissions'),
