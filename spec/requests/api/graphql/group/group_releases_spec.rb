c3c51e65eb6ed425e13a0aa88f0cddbd0efd11b9
Missy Davies <ms.melissadavies@gmail.com>
2022-08-16 14:15:48 +0200

    Add group releases resolver to create GraphQL endpoint
    We already have GroupReleasesFinder, which is used for internal
    Groups::ReleasesController. Add a resolver for group releases
    in preparation for migrating the Merge Request filter to use the
    new GraphQL endpoint.
    
    Changelog: changed

--- spec/requests/api/graphql/group/group_releases_spec.rb
+++ spec/requests/api/graphql/group/group_releases_spec.rb
@@ -2,23 +2,24 @@
 
 require 'spec_helper'
 
-RSpec.describe 'Query.project(fullPath).releases()', feature_category: :release_orchestration do
+RSpec.describe 'Query.group(fullPath).releases()', feature_category: :release_orchestration do
   include GraphqlHelpers
 
   include_context 'when releases and group releases shared context'
 
-  let(:resource_type) { :project }
-  let(:resource) { project }
+  let(:resource_type) { :group }
+  let(:resource) { group }
 
   describe "ensures that the correct data is returned based on the project's visibility and the user's access level" do
-    context 'when the project is private' do
-      let_it_be(:project) { create(:project, :repository, :private) }
+    context 'when the group is private' do
+      let_it_be(:group) { create(:group, :private) }
+      let_it_be(:project) { create(:project, :repository, :private, group: group) }
       let_it_be(:release) { create(:release, :with_evidence, project: project) }
 
       before_all do
-        project.add_guest(guest)
-        project.add_reporter(reporter)
-        project.add_developer(developer)
+        group.add_guest(guest)
+        group.add_reporter(reporter)
+        group.add_developer(developer)
       end
 
       context 'when the user is not logged in' do
@@ -48,21 +49,21 @@ RSpec.describe 'Query.project(fullPath).releases()', feature_category: :release_
       end
     end
 
-    context 'when the project is public' do
-      let_it_be(:project) { create(:project, :repository, :public) }
+    context 'when the group is public' do
+      let_it_be(:group) { create(:group, :public) }
+      let_it_be(:project) { create(:project, :repository, :public, group: group) }
       let_it_be(:release) { create(:release, :with_evidence, project: project) }
 
       before_all do
-        project.add_guest(guest)
-        project.add_reporter(reporter)
-        project.add_developer(developer)
+        group.add_guest(guest)
+        group.add_reporter(reporter)
+        group.add_developer(developer)
       end
 
       context 'when the user is not logged in' do
         let(:current_user) { stranger }
 
-        it_behaves_like 'full access to all repository-related fields'
-        it_behaves_like 'no access to editUrl'
+        it_behaves_like 'no access to any release data'
       end
 
       context 'when the user has Guest permissions' do
@@ -89,29 +90,34 @@ RSpec.describe 'Query.project(fullPath).releases()', feature_category: :release_
   end
 
   describe 'sorting and pagination' do
-    let_it_be(:sort_project) { create(:project, :public) }
+    let_it_be(:group) { create(:group, :public) }
+    let_it_be(:project) { create(:project, :public, group: group) }
+    let(:current_user) { developer }
 
-    let(:data_path)          { [:project, :releases] }
-    let(:current_user)       { developer }
+    let(:data_path) { [:group, :releases] }
+
+    before_all do
+      group.add_developer(developer)
+    end
 
     def pagination_query(params)
       graphql_query_for(
-        resource_type,
-        { full_path: sort_project.full_path },
+        :group,
+        { full_path: group.full_path },
         query_graphql_field(:releases, params, "#{page_info} nodes { tagName }")
       )
     end
 
     def pagination_results_data(nodes)
-      nodes.map { |release| release['tagName'] }
+      nodes.pluck('tagName')
     end
 
     context 'when sorting by released_at' do
-      let_it_be(:release5) { create(:release, project: sort_project, tag: 'v5.5.0', released_at: 3.days.from_now) }
-      let_it_be(:release1) { create(:release, project: sort_project, tag: 'v5.1.0', released_at: 3.days.ago) }
-      let_it_be(:release4) { create(:release, project: sort_project, tag: 'v5.4.0', released_at: 2.days.from_now) }
-      let_it_be(:release2) { create(:release, project: sort_project, tag: 'v5.2.0', released_at: 2.days.ago) }
-      let_it_be(:release3) { create(:release, project: sort_project, tag: 'v5.3.0', released_at: 1.day.ago) }
+      let_it_be(:release5) { create(:release, project: project, tag: 'v5.5.0', released_at: 3.days.from_now) }
+      let_it_be(:release1) { create(:release, project: project, tag: 'v5.1.0', released_at: 3.days.ago) }
+      let_it_be(:release4) { create(:release, project: project, tag: 'v5.4.0', released_at: 2.days.from_now) }
+      let_it_be(:release2) { create(:release, project: project, tag: 'v5.2.0', released_at: 2.days.ago) }
+      let_it_be(:release3) { create(:release, project: project, tag: 'v5.3.0', released_at: 1.day.ago) }
 
       context 'when ascending' do
         it_behaves_like 'sorted paginated query' do
@@ -124,30 +130,6 @@ RSpec.describe 'Query.project(fullPath).releases()', feature_category: :release_
       context 'when descending' do
         it_behaves_like 'sorted paginated query' do
           let(:sort_param) { :RELEASED_AT_DESC }
-          let(:first_param) { 2 }
-          let(:all_records) { [release5.tag, release4.tag, release3.tag, release2.tag, release1.tag] }
-        end
-      end
-    end
-
-    context 'when sorting by created_at' do
-      let_it_be(:release5) { create(:release, project: sort_project, tag: 'v5.5.0', created_at: 3.days.from_now) }
-      let_it_be(:release1) { create(:release, project: sort_project, tag: 'v5.1.0', created_at: 3.days.ago) }
-      let_it_be(:release4) { create(:release, project: sort_project, tag: 'v5.4.0', created_at: 2.days.from_now) }
-      let_it_be(:release2) { create(:release, project: sort_project, tag: 'v5.2.0', created_at: 2.days.ago) }
-      let_it_be(:release3) { create(:release, project: sort_project, tag: 'v5.3.0', created_at: 1.day.ago) }
-
-      context 'when ascending' do
-        it_behaves_like 'sorted paginated query' do
-          let(:sort_param) { :CREATED_ASC }
-          let(:first_param) { 2 }
-          let(:all_records) { [release1.tag, release2.tag, release3.tag, release4.tag, release5.tag] }
-        end
-      end
-
-      context 'when descending' do
-        it_behaves_like 'sorted paginated query' do
-          let(:sort_param) { :CREATED_DESC }
           let(:first_param) { 2 }
           let(:all_records) { [release5.tag, release4.tag, release3.tag, release2.tag, release1.tag] }
         end
