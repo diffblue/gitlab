a06d24d37b7cc6fc7c19dc4b1d111d7a2a98f454
Florie Guibert <fguibert@gitlab.com>
2022-08-16 12:31:18 +1000

    Disabled related items expand button when no items
    Changelog: changed

--- app/assets/javascripts/related_issues/components/related_issues_block.vue
+++ app/assets/javascripts/related_issues/components/related_issues_block.vue
@@ -213,6 +213,7 @@ export default {
             size="small"
             :icon="toggleIcon"
             :aria-label="toggleLabel"
+            :disabled="!hasRelatedIssues"
             data-testid="toggle-links"
             @click="handleToggle"
           />
