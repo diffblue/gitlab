8766f656da4ac2ccdbc9b628986ad30f07e1b133
Savas Vedova <svedova@gitlab.com>
2022-08-17 10:52:44 +0300

    Display summary highlights
    The MR Security Widget displays in a summary the total count of
    vulnerabilities split by severity in the summary. This commit adds
    that functionality.

---
+++ ee/app/assets/javascripts/vue_merge_request_widget/extensions/security_reports/summary_highlights.vue
@@ -0,0 +1,49 @@
+<script>
+import { GlSprintf } from '@gitlab/ui';
+import { CRITICAL, HIGH } from '~/vulnerabilities/constants';
+import i18n from './i18n';
+
+export default {
+  components: {
+    GlSprintf,
+  },
+  i18n,
+  props: {
+    highlights: {
+      type: Object,
+      required: true,
+      validate: (highlights) =>
+        [CRITICAL, HIGH, 'other'].every(
+          (requiredField) => typeof highlights[requiredField] !== 'undefined',
+        ),
+    },
+  },
+  computed: {
+    criticalSeverity() {
+      return this.highlights[CRITICAL];
+    },
+    highSeverity() {
+      return this.highlights[HIGH];
+    },
+    otherSeverity() {
+      return this.highlights.other;
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <gl-sprintf :message="$options.i18n.highlights">
+      <template #critical="{ content }"
+        ><strong class="gl-text-red-800">{{ criticalSeverity }} {{ content }}</strong></template
+      >
+      <template #high="{ content }"
+        ><strong class="gl-text-red-600">{{ highSeverity }} {{ content }}</strong></template
+      >
+      <template #other="{ content }"
+        ><strong>{{ otherSeverity }} {{ content }}</strong></template
+      >
+    </gl-sprintf>
+  </div>
+</template>
