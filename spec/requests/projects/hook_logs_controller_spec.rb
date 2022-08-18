a12212be33745ef8d5edf89f3cfaa3d1a36d3eb2
Luke Duncalfe <lduncalfe@eml.cc>
2022-08-19 03:06:56 +1200

    Refactor HookLogsControllers to module
    This refactors the 3 HookLogsControllers to share implementation using
    a module, in a similar vein as WebHooks::HookActions.
    
    https://gitlab.com/gitlab-org/gitlab/-/issues/370296

--- spec/requests/projects/hook_logs_controller_spec.rb
+++ spec/requests/projects/hook_logs_controller_spec.rb
@@ -2,18 +2,18 @@
 
 require 'spec_helper'
 
-RSpec.describe Groups::HookLogsController do
+RSpec.describe Projects::HookLogsController do
   let_it_be(:user) { create(:user) }
-  let_it_be_with_refind(:web_hook) { create(:group_hook) }
+  let_it_be_with_refind(:web_hook) { create(:project_hook) }
   let_it_be_with_refind(:web_hook_log) { create(:web_hook_log, web_hook: web_hook) }
 
-  let(:group) { web_hook.group }
+  let(:project) { web_hook.project }
 
   it_behaves_like WebHooks::HookLogActions do
-    let(:edit_hook_path) { edit_group_hook_url(group, web_hook) }
+    let(:edit_hook_path) { edit_project_hook_url(project, web_hook) }
 
     before do
-      group.add_owner(user)
+      project.add_owner(user)
     end
   end
 end
