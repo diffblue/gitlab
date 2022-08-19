9d381d3017aeddaef5c37a59c2538189e6c4140f
Doug Stull <dstull@gitlab.com>
2022-08-19 09:33:00 -0400

    Remove code to limit inviting groups in hierarchy for free user cap
    - change in direction
    
    Changelog: other

--- ee/spec/models/namespaces/free_user_cap_spec.rb
+++ ee/spec/models/namespaces/free_user_cap_spec.rb
@@ -25,26 +25,6 @@ RSpec.describe Namespaces::FreeUserCap do
     end
   end
 
-  describe '.group_sharing_remediation_enabled?' do
-    subject { described_class.group_sharing_remediation_enabled? }
-
-    context 'when :free_user_cap_group_sharing_remediation is disabled' do
-      before do
-        stub_feature_flags(free_user_cap_group_sharing_remediation: false)
-      end
-
-      it { is_expected.to be false }
-    end
-
-    context 'when :free_user_cap_group_sharing_remediation is enabled' do
-      before do
-        stub_feature_flags(free_user_cap_group_sharing_remediation: true)
-      end
-
-      it { is_expected.to be true }
-    end
-  end
-
   describe '.enforce_preview_or_standard?' do
     let(:namespace) { build(:namespace) }
 
