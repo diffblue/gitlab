793dbbc94f567d1bde87df0fae463a58f6c796ac
Jason Zhang <jzhang@gitlab.com>
2022-08-18 17:06:47 -0700

    Fix typo

--- qa/qa/page/project/settings/pages.rb
+++ qa/qa/page/project/settings/pages.rb
@@ -14,7 +14,7 @@ module QA
           def go_to_access_page
             within_element(:access_page_container) do
               find('a').click
-              driver.browser.switch_to.window(page.driver.browser.window_handles.last)
+              page.driver.browser.switch_to.window(page.driver.browser.window_handles.last)
             end
           end
         end
