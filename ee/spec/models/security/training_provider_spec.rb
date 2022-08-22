93c183fb344dcac98fe22124041dece065c08cfd
Subashis <schakraborty@gitlab.com>
2022-08-22 13:05:21 -0600

    Add db fixtures for providers
    Changelog: fixed
    MR: https://gitlab.com/gitlab-org/gitlab/-/merge_requests/95953
    EE: true

--- ee/spec/models/security/training_provider_spec.rb
+++ ee/spec/models/security/training_provider_spec.rb
@@ -31,8 +31,9 @@ RSpec.describe Security::TrainingProvider do
 
     context 'when the `only_enabled` flag is provided as `false`' do
       let(:only_enabled) { false }
+      let(:expected_array) { [security_training_provider_1, security_training_provider_2, security_training_provider_3] }
 
-      it { is_expected.to match_array([security_training_provider_1, security_training_provider_2, security_training_provider_3]) }
+      it { is_expected.to match_array(expected_array) }
     end
 
     context 'when the `only_enabled` flag is provided as `true`' do
@@ -43,21 +44,19 @@ RSpec.describe Security::TrainingProvider do
 
     describe 'virtual attributes' do
       let(:only_enabled) { false }
+      let(:expected_array) { [an_object_having_attributes(is_primary: true, is_enabled: true), an_object_having_attributes(is_primary: false, is_enabled: true), an_object_having_attributes(is_primary: false, is_enabled: false)] }
 
       it 'sets the virtual attributes correctly' do
-        is_expected.to match_array([
-          an_object_having_attributes(is_primary: true, is_enabled: true),
-          an_object_having_attributes(is_primary: false, is_enabled: true),
-          an_object_having_attributes(is_primary: false, is_enabled: false)
-        ])
+        is_expected.to match_array(expected_array)
       end
     end
 
     describe '.ordered_by_is_primary_desc' do
       let(:only_enabled) { false }
+      let(:expected_array) { [security_training_provider_2, security_training_provider_1, security_training_provider_3] }
 
       it "returns primary providers first" do
-        expect(subject.ordered_by_is_primary_desc).to eq([security_training_provider_2, security_training_provider_1, security_training_provider_3])
+        expect(subject.ordered_by_is_primary_desc).to eq(expected_array)
       end
     end
   end
