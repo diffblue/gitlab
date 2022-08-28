fffc77825e3cde0dd5dfa9622fdb726be6be9812
Paulina Sędłak-Jakubowska <pjjakubowska@gmail.com>
2022-08-28 19:17:19 +0200

    Change copy on issues empty state for logged users
    Changelog: other
    EE: true

--- ee/spec/features/issues/user_sees_empty_state_spec.rb
+++ ee/spec/features/issues/user_sees_empty_state_spec.rb
@@ -10,8 +10,8 @@ RSpec.describe 'Issues', :js do
     it "shows empty state #{expect_button ? 'with' : 'without'} \"New issue\" button" do
       visit project_issues_path(project)
 
-      expect(page).to have_content('The Issue Tracker is the place to add things that need to be improved or solved in a project')
-      expect(page).to have_content('Issues can be bugs, tasks or ideas to be discussed. Also, issues are searchable and filterable.')
+      expect(page).to have_content('Use issues to collaborate on ideas, solve problems, and plan work')
+      expect(page).to have_content('Learn more about issues.')
       expect(page.has_link?('New issue')).to be(expect_button)
     end
   end
