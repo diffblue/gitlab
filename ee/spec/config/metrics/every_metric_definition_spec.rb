603efdff2fe25874ad2a6fd89f5b2c36f3a958ed
Stan Hu <stanhu@gmail.com>
2022-08-30 17:23:09 +0000

    Revert "Migrate pending ci_templated redis_hll metrics"
    This reverts
    https://gitlab.com/gitlab-org/gitlab/-/merge_requests/96120 because it
    broke FOSS `master`:
    https://gitlab.com/gitlab-org/gitlab/-/issues/372148

--- ee/spec/config/metrics/every_metric_definition_spec.rb
+++ ee/spec/config/metrics/every_metric_definition_spec.rb
@@ -27,11 +27,23 @@ RSpec.describe 'Every metric definition' do
       mock_ci
       mock_monitoring
       user_auth_by_provider
+      p_ci_templates_5_min_production_app
+      p_ci_templates_aws_cf_deploy_ec2
+      p_ci_templates_auto_devops_build
+      p_ci_templates_auto_devops_deploy
+      p_ci_templates_auto_devops_deploy_latest
+      p_ci_templates_implicit_auto_devops_build
+      p_ci_templates_implicit_auto_devops_deploy_latest
+      p_ci_templates_implicit_auto_devops_deploy
     ).freeze
   end
 
   let(:events_not_defined_yet) do
     %w(
+      redis_hll_counters.ci_templates.p_ci_templates_implicit_jobs_sast_iac_monthly
+      redis_hll_counters.ci_templates.p_ci_templates_implicit_jobs_sast_iac_weekly
+      redis_hll_counters.ci_templates.p_ci_templates_implicit_security_sast_iac_monthly
+      redis_hll_counters.ci_templates.p_ci_templates_implicit_security_sast_iac_weekly
       redis_hll_counters.code_review.i_code_review_create_note_in_ipynb_diff_commit_monthly
       redis_hll_counters.code_review.i_code_review_create_note_in_ipynb_diff_commit_weekly
       redis_hll_counters.code_review.i_code_review_create_note_in_ipynb_diff_monthly
