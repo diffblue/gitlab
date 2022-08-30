0728b4c565cb7df93dcb84c9d9e7ba6c0e5e48c7
Mehmet Emin INAC <minac@gitlab.com>
2022-08-31 02:11:35 +0300

    Create partitioned `security_findings` table
    Instead of creating the table from scratch, we are using the existing
    table as a partition to save the data.
    
    Changelog: other

---
+++ db/migrate/20220816075639_move_security_findings_table_to_gitlab_partitions_dynamic_schema.rb
@@ -0,0 +1,201 @@
+# frozen_string_literal: true
+
+class MoveSecurityFindingsTableToGitlabPartitionsDynamicSchema < Gitlab::Database::Migration[2.0]
+  enable_lock_retries!
+
+  INDEX_MAPPING_OF_PARTITION = {
+    index_security_findings_on_unique_columns: :security_findings_1_uuid_scan_id_partition_number_idx,
+    index_security_findings_on_confidence: :security_findings_1_confidence_idx,
+    index_security_findings_on_project_fingerprint: :security_findings_1_project_fingerprint_idx,
+    index_security_findings_on_scan_id_and_deduplicated: :security_findings_1_scan_id_deduplicated_idx,
+    index_security_findings_on_scan_id_and_id: :security_findings_1_scan_id_id_idx,
+    index_security_findings_on_scanner_id: :security_findings_1_scanner_id_idx,
+    index_security_findings_on_severity: :security_findings_1_severity_idx
+  }.freeze
+
+  INDEX_MAPPING_AFTER_CREATING_FROM_PARTITION = {
+    partition_name_placeholder_pkey: :security_findings_pkey,
+    partition_name_placeholder_uuid_scan_id_partition_number_idx: :index_security_findings_on_unique_columns,
+    partition_name_placeholder_confidence_idx: :index_security_findings_on_confidence,
+    partition_name_placeholder_project_fingerprint_idx: :index_security_findings_on_project_fingerprint,
+    partition_name_placeholder_scan_id_deduplicated_idx: :index_security_findings_on_scan_id_and_deduplicated,
+    partition_name_placeholder_scan_id_id_idx: :index_security_findings_on_scan_id_and_id,
+    partition_name_placeholder_scanner_id_idx: :index_security_findings_on_scanner_id,
+    partition_name_placeholder_severity_idx: :index_security_findings_on_severity
+  }.freeze
+
+  INDEX_MAPPING_AFTER_CREATING_FROM_ITSELF = {
+    security_findings_pkey1: :security_findings_pkey,
+    security_findings_uuid_scan_id_partition_number_idx1: :index_security_findings_on_unique_columns,
+    security_findings_confidence_idx1: :index_security_findings_on_confidence,
+    security_findings_project_fingerprint_idx1: :index_security_findings_on_project_fingerprint,
+    security_findings_scan_id_deduplicated_idx1: :index_security_findings_on_scan_id_and_deduplicated,
+    security_findings_scan_id_id_idx1: :index_security_findings_on_scan_id_and_id,
+    security_findings_scanner_id_idx1: :index_security_findings_on_scanner_id,
+    security_findings_severity_idx1: :index_security_findings_on_severity
+  }.freeze
+
+  LATEST_PARTITION_SQL = <<~SQL
+    SELECT
+      partitions.relname AS partition_name
+    FROM pg_inherits
+    JOIN pg_class parent ON pg_inherits.inhparent = parent.oid
+    JOIN pg_class partitions ON pg_inherits.inhrelid = partitions.oid
+    WHERE
+      parent.relname = 'security_findings'
+    ORDER BY (regexp_matches(partitions.relname, 'security_findings_(\\d+)'))[1]::int DESC
+    LIMIT 1
+  SQL
+
+  def up
+    execute(<<~SQL)
+      LOCK TABLE vulnerability_scanners, security_scans, security_findings IN ACCESS EXCLUSIVE MODE
+    SQL
+
+    execute(<<~SQL)
+      ALTER TABLE security_findings RENAME TO security_findings_1;
+    SQL
+
+    execute(<<~SQL)
+      ALTER INDEX security_findings_pkey RENAME TO security_findings_1_pkey;
+    SQL
+
+    execute(<<~SQL)
+      CREATE TABLE security_findings (
+        LIKE security_findings_1 INCLUDING ALL
+      ) PARTITION BY LIST (partition_number);
+    SQL
+
+    execute(<<~SQL)
+      ALTER SEQUENCE security_findings_id_seq OWNED BY #{connection.current_schema}.security_findings.id;
+    SQL
+
+    execute(<<~SQL)
+      ALTER TABLE security_findings
+      ADD CONSTRAINT fk_rails_729b763a54 FOREIGN KEY (scanner_id) REFERENCES vulnerability_scanners(id) ON DELETE CASCADE;
+    SQL
+
+    execute(<<~SQL)
+      ALTER TABLE security_findings
+      ADD CONSTRAINT fk_rails_bb63863cf1 FOREIGN KEY (scan_id) REFERENCES security_scans(id) ON DELETE CASCADE;
+    SQL
+
+    execute(<<~SQL)
+      ALTER TABLE security_findings_1 SET SCHEMA gitlab_partitions_dynamic;
+    SQL
+
+    execute(<<~SQL)
+      ALTER TABLE security_findings ATTACH PARTITION gitlab_partitions_dynamic.security_findings_1 FOR VALUES IN (1);
+    SQL
+
+    execute(<<~SQL)
+      ALTER TABLE security_findings DROP CONSTRAINT check_partition_number;
+    SQL
+
+    rename_indices('gitlab_partitions_dynamic', INDEX_MAPPING_OF_PARTITION)
+  end
+
+  def down
+    execute(<<~SQL)
+      LOCK TABLE vulnerability_scanners, security_scans, security_findings IN ACCESS EXCLUSIVE MODE
+    SQL
+
+    # If there is already a partition for the `security_findings` table,
+    # we can promote that table to be the original one to save the data.
+    # Otherwise, we have to bring back the non-partitioned `security_findings`
+    # table from the partitioned one.
+    if latest_partition
+      create_non_partitioned_security_findings_with_data
+    else
+      create_non_partitioned_security_findings_without_data
+    end
+  end
+
+  private
+
+  def latest_partition
+    @latest_partition ||= execute(LATEST_PARTITION_SQL).first&.fetch('partition_name', nil)
+  end
+
+  def latest_partition_number
+    latest_partition.match(/security_findings_(\d+)/).captures.first
+  end
+
+  # rubocop:disable Migration/DropTable (These methods are called from the `down` method)
+  def create_non_partitioned_security_findings_with_data
+    execute(<<~SQL)
+      ALTER TABLE security_findings DETACH PARTITION gitlab_partitions_dynamic.#{latest_partition};
+    SQL
+
+    execute(<<~SQL)
+      ALTER TABLE gitlab_partitions_dynamic.#{latest_partition} SET SCHEMA #{connection.current_schema};
+    SQL
+
+    execute(<<~SQL)
+      ALTER SEQUENCE security_findings_id_seq OWNED BY #{latest_partition}.id;
+    SQL
+
+    execute(<<~SQL)
+      DROP TABLE security_findings;
+    SQL
+
+    execute(<<~SQL)
+      ALTER TABLE #{latest_partition} RENAME TO security_findings;
+    SQL
+
+    execute(<<~SQL)
+      ALTER TABLE security_findings ADD CONSTRAINT check_partition_number CHECK ((partition_number = #{latest_partition_number}));
+    SQL
+
+    index_mapping = INDEX_MAPPING_AFTER_CREATING_FROM_PARTITION.transform_keys do |key|
+      key.to_s.sub('partition_name_placeholder', latest_partition)
+    end
+
+    rename_indices(connection.current_schema, index_mapping)
+  end
+
+  def create_non_partitioned_security_findings_without_data
+    execute(<<~SQL)
+      ALTER TABLE security_findings RENAME TO security_findings_1;
+    SQL
+
+    execute(<<~SQL)
+      CREATE TABLE security_findings (
+        LIKE security_findings_1 INCLUDING ALL
+      );
+    SQL
+
+    execute(<<~SQL)
+      ALTER SEQUENCE security_findings_id_seq OWNED BY #{connection.current_schema}.security_findings.id;
+    SQL
+
+    execute(<<~SQL)
+      DROP TABLE security_findings_1;
+    SQL
+
+    execute(<<~SQL)
+      ALTER TABLE security_findings ADD CONSTRAINT check_partition_number CHECK ((partition_number = 1));
+    SQL
+
+    execute(<<~SQL)
+      ALTER TABLE ONLY security_findings
+      ADD CONSTRAINT fk_rails_729b763a54 FOREIGN KEY (scanner_id) REFERENCES vulnerability_scanners(id) ON DELETE CASCADE;
+    SQL
+
+    execute(<<~SQL)
+      ALTER TABLE ONLY security_findings
+      ADD CONSTRAINT fk_rails_bb63863cf1 FOREIGN KEY (scan_id) REFERENCES security_scans(id) ON DELETE CASCADE;
+    SQL
+
+    rename_indices(connection.current_schema, INDEX_MAPPING_AFTER_CREATING_FROM_ITSELF)
+  end
+
+  def rename_indices(schema, mapping)
+    mapping.each do |index_name, new_index_name|
+      execute(<<~SQL)
+        ALTER INDEX #{schema}.#{index_name} RENAME TO #{new_index_name};
+      SQL
+    end
+  end
+  # rubocop:enable Migration/DropTable
+end
