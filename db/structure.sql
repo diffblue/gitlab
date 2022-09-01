7d11d710a650590ca37f047392206f5938460e78
Manoj M J <mmj@gitlab.com>
2022-09-01 11:35:35 +0200

    Shift  `can_create_group` to ApplicationSetting
    Shift  `can_create_group` to ApplicationSetting
    
    Changelog: added

--- db/structure.sql
+++ db/structure.sql
@@ -11484,6 +11484,7 @@ CREATE TABLE application_settings (
     dashboard_notification_limit integer DEFAULT 0 NOT NULL,
     dashboard_enforcement_limit integer DEFAULT 0 NOT NULL,
     dashboard_limit_new_namespace_creation_enforcement_date date,
+    can_create_group boolean DEFAULT true NOT NULL,
     CONSTRAINT app_settings_container_reg_cleanup_tags_max_list_size_positive CHECK ((container_registry_cleanup_tags_service_max_list_size >= 0)),
     CONSTRAINT app_settings_container_registry_pre_import_tags_rate_positive CHECK ((container_registry_pre_import_tags_rate >= (0)::numeric)),
     CONSTRAINT app_settings_dep_proxy_ttl_policies_worker_capacity_positive CHECK ((dependency_proxy_ttl_group_policy_worker_capacity >= 0)),
