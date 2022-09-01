e06f242af566d2cd8f020c8427db0b5289cf0896
Dheeraj Joshi <djoshi@gitlab.com>
2022-09-01 15:05:13 +0530

    Update copy for DAST API specification options
    This change is related to DAST API Security which
    is behind a feature flag.

--- ee/app/assets/javascripts/security_configuration/dast_profiles/dast_site_profiles/constants.js
+++ ee/app/assets/javascripts/security_configuration/dast_profiles/dast_site_profiles/constants.js
@@ -15,21 +15,19 @@ export const SCAN_METHODS = {
   HAR: {
     text: __('HTTP Archive (HAR)'),
     value: 'HAR',
-    inputLabel: __('HAR file path or URL'),
-    placeholder: s__(
-      'DastProfiles|folder/dast_example.har or https://example.com/dast_example.har',
-    ),
+    inputLabel: __('HAR file URL'),
+    placeholder: s__('DastProfiles|https://example.com/dast_example.har'),
   },
   OPENAPI: {
     text: __('OpenAPI'),
     value: 'OPENAPI',
-    inputLabel: __('OpenAPI Specification file path or URL'),
-    placeholder: s__('DastProfiles|folder/openapi.json or https://example.com/openapi.json'),
+    inputLabel: __('OpenAPI Specification file URL'),
+    placeholder: s__('DastProfiles|https://example.com/openapi.json'),
   },
   POSTMAN_COLLECTION: {
     text: __('Postman collection'),
     value: 'POSTMAN_COLLECTION',
-    inputLabel: __('Postman collection file path or URL'),
-    placeholder: s__('DastProfiles|folder/example.postman_collection.json or https://example.com/'),
+    inputLabel: __('Postman collection file URL'),
+    placeholder: s__('DastProfiles|https://example.com/postman_collection.json'),
   },
 };
