9927c2b43afd32be9d20b830bc24d505b86263ba
Sean McGivern <sean@gitlab.com>
2022-09-02 11:53:54 +0000

    Upgrade to GitLab Styles 9
    This contains upgrades RuboCop to 1.3.6 and RuboCop RSpec to 2.12.1.
    
    Notable changes here are:
    
    1. Cops now get their namespace from all but the last and the first two
       components, rather than simply the last component. See
       https://github.com/rubocop/rubocop/pull/8490
    2. There is no more TopLevelDescribe helper for Rubocop RSpec, it's now
       TopLevelGroup. See https://github.com/rubocop/rubocop-rspec/pull/977
    3. RuboCop Rails now complains if db/schema.rb doesn't contain a schema.

--- ee/db/geo/schema.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-# Disabled in favour of ee/db/geo/structure.sql
-# Please refer to https://gitlab.com/gitlab-org/gitlab/-/issues/341758 for details
-
-raise 'ee/db/geo/schema.rb should not be used anymore, use ee/db/geo/structure.sql instead'
