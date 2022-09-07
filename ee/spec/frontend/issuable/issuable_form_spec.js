b14acb9ca5a30c6e9c2133a932d25075b0552085
Laurent Deketelaere <10886527-ali_o_kan@users.noreply.gitlab.com>
2022-09-07 06:55:04 +0000

    Autosave weight in Issuable form
    Changelog: added
    EE: true

---
+++ ee/spec/frontend/issuable/issuable_form_spec.js
@@ -0,0 +1,64 @@
+import $ from 'jquery';
+import Autosave from '~/autosave';
+import { setHTMLFixture, resetHTMLFixture } from 'helpers/fixtures';
+import IssuableForm from 'ee/issuable/issuable_form';
+import IssuableFormCE from '~/issuable/issuable_form';
+
+jest.mock('~/autosave');
+
+const createIssuable = (form) => {
+  return new IssuableForm(form);
+};
+
+describe('IssuableForm', () => {
+  let $form;
+
+  beforeEach(() => {
+    setHTMLFixture(`
+      <form>
+        <input name="[title]" />
+        <textarea name="[description]"></textarea>
+      </form>
+    `);
+    $form = $('form');
+  });
+
+  afterEach(() => {
+    resetHTMLFixture();
+    $form = null;
+  });
+
+  describe('initAutosave', () => {
+    it('calls super initAutosave', () => {
+      const initAutosaveCE = jest.spyOn(IssuableFormCE.prototype, 'initAutosave');
+      createIssuable($form);
+      expect(initAutosaveCE).toHaveBeenCalledTimes(1);
+    });
+
+    it('creates weight autosave when weight input exist', () => {
+      $form.append('<input name="[weight]" />');
+      const $weight = $form.find('input[name*="[weight]"]');
+      createIssuable($form);
+
+      expect(Autosave).toHaveBeenCalledTimes(3);
+      expect(Autosave).toHaveBeenLastCalledWith($weight, ['/', '', 'weight'], 'autosave///=weight');
+    });
+  });
+
+  describe('resetAutosave', () => {
+    it('calls super resetAutosave', () => {
+      const resetAutosaveCE = jest.spyOn(IssuableFormCE.prototype, 'resetAutosave');
+      createIssuable($form).resetAutosave();
+
+      expect(resetAutosaveCE).toHaveBeenCalledTimes(1);
+    });
+
+    it('calls reset on weight when weight input exist', () => {
+      $form.append('<input name="[weight]" />');
+      const instance = createIssuable($form);
+      instance.resetAutosave();
+
+      expect(instance.autosaveWeight.reset).toHaveBeenCalledTimes(1);
+    });
+  });
+});
