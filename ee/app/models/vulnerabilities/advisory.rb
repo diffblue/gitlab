6f0345ea1e83f50e8175dee69e99e87cc9c9c97f
Brian Williams <bwilliams@gitlab.com>
2022-09-07 15:51:56 -0500

    Add validation for CVSS v3.1 vector strings
    Adds a new class for handling CVSS V3 vectors, and calls it in order to
    perform validation for the model. In the future, this class will also be
    able to perform other CVSS functions such as calculating severity.

--- ee/app/models/vulnerabilities/advisory.rb
+++ ee/app/models/vulnerabilities/advisory.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 module Vulnerabilities
   class Advisory < ApplicationRecord
+    include Gitlab::Utils::StrongMemoize
+
+    VECTOR_MAX_LENGTH = 128
+
     self.table_name = "vulnerability_advisories"
 
     validates :created_date, presence: true
@@ -11,8 +15,29 @@ module Vulnerabilities
     validates :affected_range, length: { maximum: 32 }
     validates :not_impacted, length: { maximum: 2048 }
     validates :solution, length: { maximum: 2048 }
-    validates :cvss_v2, length: { maximum: 128 }
-    validates :cvss_v3, length: { maximum: 128 }
+    validates :cvss_v2, length: { maximum: VECTOR_MAX_LENGTH }
     validates :description, length: { maximum: 2048 }
+
+    validate :validate_cvss_v3, if: -> { cvss_v3.present? }
+
+    def cvss_v3
+      return unless super
+
+      ::Gitlab::Vulnerabilities::Cvss::V3.new(super)
+    end
+    strong_memoize_attr :cvss_v3
+
+    def validate_cvss_v3
+      if cvss_v3.vector.length > VECTOR_MAX_LENGTH
+        # Validated here as this is a DB constraint and not part of the CVSS v3 specification.
+        errors.add(:cvss_v3, "vector string may not be longer than #{VECTOR_MAX_LENGTH} characters")
+
+        return
+      end
+
+      return if cvss_v3.valid?
+
+      cvss_v3.errors.each { |error| errors.add(:cvss_v3, error) }
+    end
   end
 end
--- ee/spec/models/vulnerabilities/advisory_spec.rb
+++ ee/spec/models/vulnerabilities/advisory_spec.rb
@@ -18,12 +18,44 @@ RSpec.describe Vulnerabilities::Advisory, type: :model do
         :not_impacted | 2048
         :solution | 2048
         :cvss_v2 | 128
-        :cvss_v3 | 128
         :description | 2048
       end
 
       with_them do
         it { is_expected.to validate_length_of(attribute).is_at_most(max_length) }
+      end
+    end
+
+    describe 'cvss_v3 validation' do
+      it 'validates length of vector' do
+        expect_next_instance_of(::Gitlab::Vulnerabilities::Cvss::V3) do |cvss|
+          expect(cvss).not_to receive(:valid?)
+        end
+
+        advisory.cvss_v3 = Array.new(described_class::VECTOR_MAX_LENGTH + 1).map { 'x' }.join('')
+        advisory.validate
+
+        expect(advisory.errors[:cvss_v3]).to include(
+          "vector string may not be longer than #{described_class::VECTOR_MAX_LENGTH} characters")
+      end
+
+      it 'delegates validation to CVSS class when vector is present' do
+        expect_next_instance_of(::Gitlab::Vulnerabilities::Cvss::V3) do |cvss|
+          expect(cvss).to receive(:valid?).and_call_original
+        end
+
+        expect(advisory).to be_valid
+      end
+
+      context 'when vector is not present' do
+        before do
+          advisory.cvss_v3 = nil
+        end
+
+        it 'does not use CVSS class' do
+          expect(::Gitlab::Vulnerabilities::Cvss::V3).not_to receive(:new)
+          expect(advisory).to be_valid
+        end
       end
     end
   end
