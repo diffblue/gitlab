f8402ad55ab74f0720a81580013402dce482619e
Nataliia Radina <nradina@gitlab.com>
2022-09-08 13:25:25 +0200

    Remove hidden checkboxes when not needed
    Changelog: fixed

--- app/assets/javascripts/pages/projects/merge_requests/edit/index.js
+++ app/assets/javascripts/pages/projects/merge_requests/edit/index.js
@@ -1,5 +1,7 @@
 import initMergeRequest from '~/pages/projects/merge_requests/init_merge_request';
 import initCheckFormState from './check_form_state';
+import initFormUpdate from './update_form';
 
 initMergeRequest();
+initFormUpdate();
 initCheckFormState();
