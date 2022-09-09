c4168b4625f971b655d41ac14f9b195bab80f32c
Alexander Turinske <aturinske@gitlab.com>
2022-09-09 06:07:38 +0000

    Update scanner action for DAST scanner
    - allow for user input of site and scanner profiles
    - use input boxes
    - update buildScannerAction to accept more options
    - update CSS so that the remove button is not overlapped
    - update tests

--- ee/app/assets/javascripts/security_orchestration/components/policy_editor/scan_execution_policy/policy_action_builder.vue
+++ ee/app/assets/javascripts/security_orchestration/components/policy_editor/scan_execution_policy/policy_action_builder.vue
@@ -1,8 +1,22 @@
 <script>
-import { GlButton, GlDropdown, GlDropdownItem, GlSprintf, GlForm } from '@gitlab/ui';
+import {
+  GlButton,
+  GlDropdown,
+  GlDropdownItem,
+  GlFormGroup,
+  GlFormInput,
+  GlSprintf,
+  GlForm,
+} from '@gitlab/ui';
 import { s__ } from '~/locale';
 import { ACTION_THEN_LABEL, ACTION_AND_LABEL } from '../constants';
-import { DEFAULT_SCANNER, TEMPORARY_LIST_OF_SCANNERS } from './constants';
+import {
+  DAST_HUMANIZED_TEMPLATE,
+  DEFAULT_SCANNER,
+  SCANNER_DAST,
+  SCANNER_HUMANIZED_TEMPLATE,
+  TEMPORARY_LIST_OF_SCANNERS,
+} from './constants';
 import { buildScannerAction } from './lib';
 
 export default {
@@ -12,6 +26,8 @@ export default {
     GlDropdown,
     GlDropdownItem,
     GlForm,
+    GlFormGroup,
+    GlFormInput,
     GlSprintf,
   },
   props: {
@@ -27,53 +43,104 @@ export default {
   },
   data() {
     return {
-      selectedAction: this.initAction.scan || DEFAULT_SCANNER,
+      selectedScanner: this.initAction.scan || DEFAULT_SCANNER,
     };
   },
   computed: {
     actionLabel() {
       return this.actionIndex === 0 ? ACTION_THEN_LABEL : ACTION_AND_LABEL;
     },
+    actionMessage() {
+      return this.selectedScanner === SCANNER_DAST
+        ? DAST_HUMANIZED_TEMPLATE
+        : SCANNER_HUMANIZED_TEMPLATE;
+    },
+    siteProfile: {
+      get() {
+        return this.initAction.site_profile?.trim() ?? '';
+      },
+      set(value) {
+        this.setSelectedScanner({ siteProfile: value });
+      },
+    },
+    scannerProfile: {
+      get() {
+        return this.initAction.scanner_profile?.trim() ?? '';
+      },
+      set(value) {
+        this.setSelectedScanner({ scannerProfile: value });
+      },
+    },
   },
   methods: {
-    setSelected(key) {
-      this.selectedAction = key;
+    setSelectedScanner({
+      scanner = this.selectedScanner,
+      siteProfile = this.siteProfile,
+      scannerProfile = this.scannerProfile,
+    }) {
+      if (scanner !== this.selectedScanner) {
+        this.selectedScanner = scanner;
+      }
 
-      this.$emit('changed', buildScannerAction(key));
+      this.$emit('changed', buildScannerAction({ scanner, siteProfile, scannerProfile }));
     },
   },
   i18n: {
-    humanizedTemplate: s__(
-      'ScanExecutionPolicy|%{thenLabelStart}Then%{thenLabelEnd} Require a %{scan} scan to run',
-    ),
+    selectedScannerProfilePlaceholder: s__('ScanExecutionPolicy|Select scanner profile'),
+    selectedSiteProfilePlaceholder: s__('ScanExecutionPolicy|Select site profile'),
   },
 };
 </script>
 
 <template>
-  <div class="gl-bg-gray-10 gl-rounded-base gl-px-5! gl-pt-5! gl-relative gl-pb-4">
-    <gl-form inline @submit.prevent>
-      <gl-sprintf :message="$options.i18n.humanizedTemplate">
+  <div class="gl-bg-gray-10 gl-rounded-base gl-pl-5 gl-pr-7 gl-py-5 gl-relative">
+    <gl-form inline class="gl-gap-3" @submit.prevent>
+      <gl-sprintf :message="actionMessage">
         <template #thenLabel>
-          <label class="text-uppercase gl-font-lg gl-mr-3" data-testid="action-component-label">
+          <label class="text-uppercase gl-font-lg" data-testid="action-component-label">
             {{ actionLabel }}
           </label>
         </template>
 
         <template #scan>
-          <gl-dropdown
-            class="gl-mx-3"
-            :text="$options.SCANNERS[selectedAction]"
-            data-testid="action-scanner-text"
-          >
+          <gl-dropdown :text="$options.SCANNERS[selectedScanner]" data-testid="action-scanner-text">
             <gl-dropdown-item
               v-for="(value, key) in $options.SCANNERS"
               :key="key"
-              @click="setSelected(key)"
+              @click="setSelectedScanner({ scanner: key })"
             >
               {{ value }}
             </gl-dropdown-item>
           </gl-dropdown>
+        </template>
+
+        <template #scannerProfile>
+          <gl-form-group
+            :label="s__('ScanExecutionPolicy|Scanner profile')"
+            label-for="scanner-profile"
+            label-sr-only
+          >
+            <gl-form-input
+              id="scanner-profile"
+              v-model="scannerProfile"
+              :placeholder="$options.i18n.selectedScannerProfilePlaceholder"
+              data-testid="scan-profile-selection"
+            />
+          </gl-form-group>
+        </template>
+        <template #siteProfile>
+          <gl-form-group
+            :label="s__('ScanExecutionPolicy|Site profile')"
+            label-for="site-profile"
+            label-sr-only
+          >
+            <gl-form-input
+              id="site-profile"
+              v-model="siteProfile"
+              :placeholder="$options.i18n.selectedSiteProfilePlaceholder"
+              data-testid="site-profile-selection"
+            />
+          </gl-form-group>
         </template>
       </gl-sprintf>
     </gl-form>
