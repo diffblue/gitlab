5e27cc5cca164b3ac9eb1989a69ff6c4f51158da
nmilojevic1 <nmilojevic@gitlab.com>
2022-09-15 14:05:58 +0200

    Fix rubocop/lint errors

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -430,7 +430,7 @@ RSpec.configure do |config|
     with_sidekiq_server_middleware do |chain|
       Gitlab::SidekiqMiddleware.server_configurator(
         metrics: false, # The metrics don't go anywhere in tests
-        arguments_logger: false, # We're not logging the regular messages for inline jobs
+        arguments_logger: false # We're not logging the regular messages for inline jobs
       ).call(chain)
       chain.add DisableQueryLimit
       chain.insert_after ::Gitlab::SidekiqMiddleware::RequestStoreMiddleware, IsolatedRequestStore
