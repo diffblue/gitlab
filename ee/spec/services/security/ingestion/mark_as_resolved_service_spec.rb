fa4d28c4917b048050e9a10fbb4ad474262069ee
Brian Williams <bwilliams@gitlab.com>
2022-09-19 07:19:45 -0500

    Add tests for present_on_default_branch scope

--- ee/spec/services/security/ingestion/mark_as_resolved_service_spec.rb
+++ ee/spec/services/security/ingestion/mark_as_resolved_service_spec.rb
@@ -3,10 +3,12 @@
 require 'spec_helper'
 
 RSpec.describe Security::Ingestion::MarkAsResolvedService do
-  let!(:project) { create(:project) }
-  let!(:not_ingested_vulnerability) { create(:vulnerability, project: project) }
-  let!(:ingested_vulnerability) { create(:vulnerability, project: project) }
-  let!(:generic_vulnerability) { create(:vulnerability, project: project, report_type: :generic) }
+  let_it_be(:project) { create(:project) }
+  let_it_be(:non_default_vulnerability) { create(:vulnerability, project: project, present_on_default_branch: false) }
+
+  let_it_be_with_reload(:not_ingested_vulnerability) { create(:vulnerability, project: project) }
+  let_it_be_with_reload(:ingested_vulnerability) { create(:vulnerability, project: project) }
+  let_it_be_with_reload(:generic_vulnerability) { create(:vulnerability, project: project, report_type: :generic) }
 
   let(:ingested_ids) { [ingested_vulnerability.id] }
   let(:service_object) { described_class.new(project, ingested_ids) }
@@ -19,6 +21,13 @@ RSpec.describe Security::Ingestion::MarkAsResolvedService do
         .to change { not_ingested_vulnerability.reload.resolved_on_default_branch }.from(false).to(true)
         .and not_change { ingested_vulnerability.reload.resolved_on_default_branch }.from(false)
         .and not_change { generic_vulnerability.reload.resolved_on_default_branch }.from(false)
+    end
+
+    it 'does not process vulnerabilities which are not present on the default branch' do
+      expect(service_object).to receive(:process_batch)
+        .with(match_array([not_ingested_vulnerability, ingested_vulnerability, generic_vulnerability]))
+
+      mark_as_resolved
     end
   end
 end
