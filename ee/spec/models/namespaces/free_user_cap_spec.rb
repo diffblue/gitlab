2383c6c3a0b56807be05f25aacc25abfeca64084
Doug Stull <dstull@gitlab.com>
2022-09-21 18:09:08 -0400

    Remove unused free user cap data feature flag
    - never used
    
    Changelog: other
    EE: true

--- ee/spec/models/namespaces/free_user_cap_spec.rb
+++ ee/spec/models/namespaces/free_user_cap_spec.rb
@@ -5,26 +5,6 @@ require 'spec_helper'
 RSpec.describe Namespaces::FreeUserCap do
   using RSpec::Parameterized::TableSyntax
 
-  describe '.trimming_enabled?' do
-    subject { described_class.trimming_enabled? }
-
-    context 'when free_user_cap_data_remediation_job is disabled' do
-      before do
-        stub_feature_flags(free_user_cap_data_remediation_job: false)
-      end
-
-      it { is_expected.to be false }
-    end
-
-    context 'when :free_user_cap_data_remediation_job is enabled' do
-      before do
-        stub_feature_flags(free_user_cap_data_remediation_job: true)
-      end
-
-      it { is_expected.to be true }
-    end
-  end
-
   describe '.enforce_preview_or_standard?' do
     let(:namespace) { build(:namespace) }
 
