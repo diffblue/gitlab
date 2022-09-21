0228cfc2b79f025d471d5de14875b847314ff18c
Heinrich Lee Yu <heinrich@gitlab.com>
2022-09-21 15:12:44 +0800

    Show tasks in dashboard issue list
    Tasks are visible in the project and group issue list so they should
    also be visible in the dashboard list
    
    Changelog: fixed

--- ee/spec/controllers/dashboard_controller_spec.rb
+++ ee/spec/controllers/dashboard_controller_spec.rb
@@ -13,14 +13,14 @@ RSpec.describe DashboardController do
     end
 
     describe 'GET issues' do
-      it 'does not list test cases' do
+      it 'includes test cases' do
         issue = create(:incident, project: project, author: user)
         incident = create(:incident, project: project, author: user)
-        create(:quality_test_case, project: project, author: user)
+        test_case = create(:quality_test_case, project: project, author: user)
 
         get :issues, params: { author_id: user.id }
 
-        expect(assigns(:issues)).to match_array([issue, incident])
+        expect(assigns(:issues)).to match_array([issue, incident, test_case])
       end
     end
   end
