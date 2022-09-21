7f9671f7abdbb1f9ebb180734a87e4d2b6d90aee
Doug Stull <dstull@gitlab.com>
2022-09-21 17:19:53 -0400

    Add logic for new free user cap enforcement direction
    - business requirements

--- ee/app/models/namespaces/free_user_cap/preview.rb
+++ ee/app/models/namespaces/free_user_cap/preview.rb
@@ -11,7 +11,16 @@ module Namespaces
       end
 
       def feature_enabled?
-        ::Feature.enabled?(:preview_free_user_cap, root_namespace)
+        return false unless ::Feature.enabled?(:preview_free_user_cap, root_namespace)
+
+        # If we are at the enforcement limit(and it is enabled) we treat this like a switch
+        # to turn off preview. This will help in areas that observe both and will ensure
+        # only one message is being shown to the user preview or enforcement.
+        # The rollout will start with preview having a limit number and being turned on
+        # before Standard does.  So this will cover the ones that are 'at' the number
+        # but not over for the Standard as they will always get a preview before being
+        # enforced with Standard.
+        !Standard.new(root_namespace).reached_limit?
       end
     end
   end
