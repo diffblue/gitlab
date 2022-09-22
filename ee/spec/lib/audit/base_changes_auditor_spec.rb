152d12be3b98bc5c9b9c67ed082da857b3cae151
Laurent Deketelaere <laurent@geturl.net>
2022-09-22 11:40:07 +0200

    Add Audit::BaseChangesAuditor test to increase coverage

---
+++ ee/spec/lib/audit/base_changes_auditor_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Audit::BaseChangesAuditor do
+  let_it_be(:user) { build_stubbed(:user) }
+  let_it_be(:group) { build_stubbed(:group) }
+  let_it_be(:project) { build_stubbed(:project, group: group) }
+
+  subject { described_class.new(user, project) }
+
+  describe '#attributes_from_auditable_model' do
+    it { expect { subject.send( 'attributes_from_auditable_model', nil) }.to raise_error(NotImplementedError) }
+  end
+end
