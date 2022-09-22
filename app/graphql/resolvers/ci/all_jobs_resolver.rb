da00764e7b6b414c3f1f3b292598812832fc1754
Andreas Deicha <andreas.deicha@noser.com>
2022-09-22 17:31:54 +0200

    Applied suggestions

--- app/graphql/resolvers/ci/all_jobs_resolver.rb
+++ app/graphql/resolvers/ci/all_jobs_resolver.rb
@@ -3,19 +3,14 @@
 module Resolvers
   module Ci
     class AllJobsResolver < BaseResolver
-      include Gitlab::Graphql::Authorize::AuthorizeResource
-      include LooksAhead
-
       type ::Types::Ci::JobType.connection_type, null: true
 
       argument :statuses, [::Types::Ci::JobStatusEnum],
               required: false,
               description: 'Filter jobs by status.'
 
-      def resolve_with_lookahead(statuses: nil)
-        jobs = ::Ci::JobsFinder.new(current_user: current_user, params: { scope: statuses }).execute
-
-        apply_lookahead(jobs)
+      def resolve(statuses: nil)
+        ::Ci::JobsFinder.new(current_user: current_user, params: { scope: statuses }).execute
       end
     end
   end
