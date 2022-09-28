00d2868622f99a84bd8e47045ae11e48edf020fc
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2022-09-28 14:35:54 -0300

    Ignore job_artifact_registry.success column

--- ee/app/models/geo/job_artifact_registry.rb
+++ ee/app/models/geo/job_artifact_registry.rb
@@ -4,9 +4,12 @@ class Geo::JobArtifactRegistry < Geo::BaseRegistry
   include Geo::Syncable
   include ::Geo::ReplicableRegistry
   include ::Geo::VerifiableRegistry
+  include IgnorableColumns
 
   MODEL_CLASS = ::Ci::JobArtifact
   MODEL_FOREIGN_KEY = :artifact_id
+
+  ignore_column :success, remove_with: '15.6', remove_after: '2022-10-22'
 
   belongs_to :job_artifact, class_name: 'Ci::JobArtifact', foreign_key: :artifact_id
 
