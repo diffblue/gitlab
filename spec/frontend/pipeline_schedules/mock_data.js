637f17aeaeefb6f7811090d0612f31d04d30815b
Payton Burdette <pburdette@gitlab.com>
2022-09-29 15:52:34 -0400

    Build schedules table
    Build out the pipeline
    schedules table behind feature
    flag.

---
+++ spec/frontend/pipeline_schedules/mock_data.js
@@ -0,0 +1,22 @@
+import mockGetPipelineSchedulesGraphQLResponse from 'test_fixtures/graphql/pipeline_schedules/get_pipeline_schedules.query.graphql.json';
+import mockGetPipelineSchedulesAsGuestGraphQLResponse from 'test_fixtures/graphql/pipeline_schedules/get_pipeline_schedules.query.graphql.as_guest.json';
+
+const {
+  data: {
+    project: {
+      pipelineSchedules: { nodes },
+    },
+  },
+} = mockGetPipelineSchedulesGraphQLResponse;
+
+const {
+  data: {
+    project: {
+      pipelineSchedules: { nodes: guestNodes },
+    },
+  },
+} = mockGetPipelineSchedulesAsGuestGraphQLResponse;
+
+export const mockPipelineScheduleNodes = nodes;
+
+export const mockPipelineScheduleAsGuestNodes = guestNodes;
