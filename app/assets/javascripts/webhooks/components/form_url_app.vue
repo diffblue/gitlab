48ccfbc708dd45a1c18bc129242b7a1495ba2bd1
Justin Ho <hduong@gitlab.com>
2022-10-03 13:39:19 +0700

    Add name attributes to properly update the backend
    Update translations

--- app/assets/javascripts/webhooks/components/form_url_app.vue
+++ app/assets/javascripts/webhooks/components/form_url_app.vue
@@ -12,7 +12,16 @@ export default {
   data() {
     return {
       maskEnabled: false,
+      url: null,
     };
+  },
+  computed: {
+    maskedUrl() {
+      return this.url;
+    },
+  },
+  i18n: {
+    urlPlaceholder: 'http://example.com/trigger-ci.json',
   },
 };
 </script>
@@ -26,20 +35,28 @@ export default {
         s__('Webhooks|URL must be percent-encoded if it contains one or more special characters.')
       "
     >
-      <gl-form-input id="webhook-url" :placeholder="__('http://example.com/trigger-ci.json')" />
+      <gl-form-input
+        id="webhook-url"
+        v-model="url"
+        name="hook[url]"
+        :placeholder="$options.i18n.urlPlaceholder"
+      />
     </gl-form-group>
     <div class="gl-mt-5">
       <gl-form-radio v-model="maskEnabled" :value="false">{{
         s__('Webhooks|Show full URL')
       }}</gl-form-radio>
-      <gl-form-radio v-model="maskEnabled" :value="true">{{
-        s__('Webhooks|Mask portions of URL')
-      }}</gl-form-radio>
+      <gl-form-radio v-model="maskEnabled" :value="true"
+        >{{ s__('Webhooks|Mask portions of URL') }}
+        <template #help>
+          {{ s__('Webhooks|Do not show sensitive data such as tokens in the UI.') }}
+        </template>
+      </gl-form-radio>
 
       <div v-show="maskEnabled" class="gl-ml-6">
-        <form-url-mask-item />
+        <form-url-mask-item :index="0" />
         <gl-form-group :label="s__('Webhooks|URL preview')" label-for="webhook-url-preview">
-          <gl-form-input id="webhook-url-preview" readonly />
+          <gl-form-input id="webhook-url-preview" :value="maskedUrl" readonly />
         </gl-form-group>
       </div>
     </div>
--- app/assets/javascripts/webhooks/components/form_url_mask_item.vue
+++ app/assets/javascripts/webhooks/components/form_url_mask_item.vue
@@ -15,11 +15,19 @@ export default {
     },
   },
   computed: {
-    sensitiveInputId() {
-      return `webhook-url-mask-item-sensitive-${this.index}`;
+    keyInputId() {
+      return this.inputId('key');
     },
-    previewInputId() {
-      return `webhook-url-mask-item-prview-${this.index}`;
+    valueInputId() {
+      return this.inputId('value');
+    },
+  },
+  methods: {
+    inputId(type) {
+      return `webhook-url-mask-item-${type}-${this.index}`;
+    },
+    inputName(type) {
+      return `hook[url_variables][][${type}]`;
     },
   },
 };
@@ -29,17 +37,17 @@ export default {
   <div class="gl-display-flex gl-align-items-flex-end gl-gap-3 gl-mb-5">
     <gl-form-group
       :label="s__('Webhooks|Sensitive portion of URL')"
-      :label-for="sensitiveInputId"
+      :label-for="valueInputId"
       class="gl-flex-grow-1 gl-mb-0"
     >
-      <gl-form-input :id="sensitiveInputId" />
+      <gl-form-input :id="valueInputId" :name="inputName('value')" />
     </gl-form-group>
     <gl-form-group
       :label="s__('Webhooks|How it looks in the UI')"
-      :label-for="previewInputId"
+      :label-for="keyInputId"
       class="gl-flex-grow-1 gl-mb-0"
     >
-      <gl-form-input :id="previewInputId" />
+      <gl-form-input :id="keyInputId" :name="inputName('key')" />
     </gl-form-group>
     <gl-button icon="remove" />
   </div>
