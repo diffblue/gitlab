cb28520a1cb1583ef1687da37e66e098ae887915
Mireya Andres <mandres@gitlab.com>
2022-10-04 11:17:00 +0000

    Show empty state for external CI config in pipeline editor
    If the CI config is in another project or websute, the pipeline editor
    will show a special empty state since the user can't edit the file.
    
    Changelog: added

--- app/models/project.rb
+++ app/models/project.rb
@@ -2720,6 +2720,7 @@ class Project < ApplicationRecord
     ci_config_path.blank? || ci_config_path == Gitlab::FileDetector::PATTERNS[:gitlab_ci]
   end
 
+  # DO NOT USE. This method will be deprecated soon
   def uses_external_project_ci_config?
     !!(ci_config_path =~ %r{@.+/.+})
   end
@@ -2844,6 +2845,7 @@ class Project < ApplicationRecord
     repository.gitlab_ci_yml_for(sha, ci_config_path_or_default)
   end
 
+  # DO NOT USE. This method will be deprecated soon
   def ci_config_external_project
     Project.find_by_full_path(ci_config_path.split('@', 2).last)
   end
