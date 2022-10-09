282388e4652f1e7c1a49d3e82cd0733cf4b49e45
chenqiting <qtchen@jihulab.com>
2022-10-09 14:06:53 +0800

    Resolve rubocop job

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -81,9 +81,9 @@ class GroupsController < Groups::ApplicationController
       successful_creation_hooks
 
       notice = if @group.chat_team.present?
-                 _("Group %{group_name} and its Mattermost team were successfully created.") % { group_name: @group.name }
+                 format(_("Group %{group_name} and its Mattermost team were successfully created."), group_name: @group.name)
                else
-                 _("Group %{group_name} was successfully created.") % { group_name: @group.name }
+                 format(_("Group %{group_name} was successfully created."), group_name: @group.name)
                end
 
       redirect_to @group, notice: notice
