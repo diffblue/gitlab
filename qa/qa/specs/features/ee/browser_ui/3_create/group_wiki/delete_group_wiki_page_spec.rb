7d092c35e2498865c9515087a5d6cf869e7eb9e1
Jason Zhang <jzhang@gitlab.com>
2022-10-10 02:04:12 +0000

    Increase Group Wiki e2e coverage

---
+++ qa/qa/specs/features/ee/browser_ui/3_create/group_wiki/delete_group_wiki_page_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module QA
+  RSpec.describe 'Create' do
+    describe 'Testing group wiki' do
+      let(:initial_wiki) do
+        Resource::Wiki::GroupPage.fabricate_via_api!
+      end
+
+      before do
+        Flow::Login.sign_in
+      end
+
+      it 'can delete a group wiki page',
+    testcase: 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/374707' do
+        initial_wiki.visit!
+
+        EE::Page::Group::Wiki::Show.perform(&:click_edit)
+        EE::Page::Group::Wiki::Edit.perform(&:delete_page)
+
+        EE::Page::Group::Wiki::Show.perform do |wiki|
+          expect(wiki).to have_no_page
+        end
+      end
+    end
+  end
+end
