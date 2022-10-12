df9ec360785f6e0a55a6878a4277e0e9ee3a957a
Quang-Minh Nguyen <qmnguyen@gitlab.com>
2022-10-12 17:02:13 +0700

    Clean up legacy Gitaly Wiki RPCs in Rails
    After feature flags for migrating Wiki RPCs to normal repository RPCs
    are enabled globally on production, we turn corresponding feature flag
    on by default. Now it's time to remove the code base leading to legacy
    Wiki RPCs.
    
    Issue: https://gitlab.com/gitlab-org/gitaly/-/issues/4512
    Changelog: other
    EE: true

--- ee/spec/features/groups/wikis_spec.rb
+++ ee/spec/features/groups/wikis_spec.rb
@@ -16,37 +16,14 @@ RSpec.describe 'Group wikis', :js do
     wiki.container.add_owner(user)
   end
 
-  context 'with legacy wiki rpcs' do
-    before do
-      stub_feature_flags(wiki_list_pages_with_normal_repository_rpcs: false)
-    end
-
-    it_behaves_like 'User creates wiki page'
-    it_behaves_like 'User deletes wiki page'
-    it_behaves_like 'User previews wiki changes'
-    it_behaves_like 'User updates wiki page'
-    it_behaves_like 'User uses wiki shortcuts'
-    it_behaves_like 'User views AsciiDoc page with includes'
-    it_behaves_like 'User views a wiki page'
-    it_behaves_like 'User views wiki pages'
-    it_behaves_like 'User views wiki sidebar'
-    it_behaves_like 'User views Git access wiki page'
-  end
-
-  context 'with normal repository rpcs' do
-    before do
-      stub_feature_flags(wiki_list_pages_with_normal_repository_rpcs: true)
-    end
-
-    it_behaves_like 'User creates wiki page'
-    it_behaves_like 'User deletes wiki page'
-    it_behaves_like 'User previews wiki changes'
-    it_behaves_like 'User updates wiki page'
-    it_behaves_like 'User uses wiki shortcuts'
-    it_behaves_like 'User views AsciiDoc page with includes'
-    it_behaves_like 'User views a wiki page'
-    it_behaves_like 'User views wiki pages', false
-    it_behaves_like 'User views wiki sidebar'
-    it_behaves_like 'User views Git access wiki page'
-  end
+  it_behaves_like 'User creates wiki page'
+  it_behaves_like 'User deletes wiki page'
+  it_behaves_like 'User previews wiki changes'
+  it_behaves_like 'User updates wiki page'
+  it_behaves_like 'User uses wiki shortcuts'
+  it_behaves_like 'User views AsciiDoc page with includes'
+  it_behaves_like 'User views a wiki page'
+  it_behaves_like 'User views wiki pages'
+  it_behaves_like 'User views wiki sidebar'
+  it_behaves_like 'User views Git access wiki page'
 end
