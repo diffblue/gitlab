6e744768ff1562b60326fdad6ec8813c072759ae
nmilojevic1 <nmilojevic@gitlab.com>
2022-10-12 11:35:56 +0200

    Clean memory_watchdog spec

--- spec/initializers/memory_watchdog_spec.rb
+++ spec/initializers/memory_watchdog_spec.rb
@@ -9,23 +9,21 @@ RSpec.describe 'memory watchdog' do
 
   context 'when GITLAB_MEMORY_WATCHDOG_ENABLED is truthy' do
     let(:env_switch) { 'true' }
-    let(:watchdog_monitors) do
-      [
-        Gitlab::Memory::Watchdog::Monitor::HeapFragmentation,
-        Gitlab::Memory::Watchdog::Monitor::UniqueMemoryGrowth
-      ]
-    end
 
     before do
       stub_env('GITLAB_MEMORY_WATCHDOG_ENABLED', env_switch)
-      watchdog_monitors.each do |watchdog_monitor|
-        allow(watchdog_monitor).to receive(:new)
-      end
     end
 
     context 'when runtime is an application' do
       let(:watchdog) { instance_double(Gitlab::Memory::Watchdog) }
       let(:background_task) { instance_double(Gitlab::BackgroundTask) }
+      let(:logger) { Gitlab::AppLogger }
+      let(:watchdog_monitors) do
+        [
+          Gitlab::Memory::Watchdog::Monitor::HeapFragmentation,
+          Gitlab::Memory::Watchdog::Monitor::UniqueMemoryGrowth
+        ]
+      end
 
       before do
         allow(Gitlab::Runtime).to receive(:application?).and_return(true)
@@ -37,35 +35,23 @@ RSpec.describe 'memory watchdog' do
         run_initializer
       end
 
-      shared_examples 'starts configured watchdog with handler' do |handler_class|
+      shared_examples 'starts configured watchdog' do |handler_class|
         let(:configuration) { Gitlab::Memory::Watchdog::Configuration.new }
 
-        before do
-          allow(Gitlab::Memory::Watchdog).to receive(:new).and_return(watchdog)
-          allow(watchdog).to receive(:configure).and_yield(configuration)
-          allow(Gitlab::BackgroundTask).to receive(:new).with(watchdog).and_return(background_task)
-          allow(background_task).to receive(:start)
-          allow(Gitlab::Cluster::LifecycleEvents).to receive(:on_worker_start).and_yield
-        end
-
-        it "correctly configures watchdog", :aggregate_failures do
+        it "correctly configures and starts watchdog", :aggregate_failures do
           expect(watchdog).to receive(:configure).and_yield(configuration)
 
           expect(watchdog_monitors).to all(receive(:new))
 
-          run_initializer
-
-          expect(configuration.handler).to be_an_instance_of(handler_class)
-          expect(configuration.logger).to eq(Gitlab::AppLogger)
-        end
-
-        it "starts the watchdog", :aggregate_failures do
           expect(Gitlab::Memory::Watchdog).to receive(:new).and_return(watchdog)
           expect(Gitlab::BackgroundTask).to receive(:new).with(watchdog).and_return(background_task)
           expect(background_task).to receive(:start)
           expect(Gitlab::Cluster::LifecycleEvents).to receive(:on_worker_start).and_yield
 
           run_initializer
+
+          expect(configuration.handler).to be_an_instance_of(handler_class)
+          expect(configuration.logger).to eq(logger)
         end
       end
 
@@ -87,7 +73,7 @@ RSpec.describe 'memory watchdog' do
           allow(Gitlab::Runtime).to receive(:puma?).and_return(true)
         end
 
-        it_behaves_like 'starts configured watchdog with handler', Gitlab::Memory::Watchdog::PumaHandler
+        it_behaves_like 'starts configured watchdog', Gitlab::Memory::Watchdog::PumaHandler
       end
       # rubocop: enable RSpec/VerifiedDoubles
 
@@ -96,11 +82,11 @@ RSpec.describe 'memory watchdog' do
           allow(Gitlab::Runtime).to receive(:sidekiq?).and_return(true)
         end
 
-        it_behaves_like 'starts configured watchdog with handler', Gitlab::Memory::Watchdog::TermProcessHandler
+        it_behaves_like 'starts configured watchdog', Gitlab::Memory::Watchdog::TermProcessHandler
       end
 
       context 'when other runtime' do
-        it_behaves_like 'starts configured watchdog with handler', Gitlab::Memory::Watchdog::NullHandler
+        it_behaves_like 'starts configured watchdog', Gitlab::Memory::Watchdog::NullHandler
       end
     end
 
