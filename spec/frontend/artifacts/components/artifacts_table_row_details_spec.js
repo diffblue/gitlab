21c71c714fb6dd7e3f1cca337d9f2f577be35dc0
mfluharty <mfluharty@gitlab.com>
2022-10-14 15:39:34 -0600

    Remove DynamicScroller from row details spec
    DynamicScroller doesn't have a jest config, which throws an eslint error
    Use mount to get around the need to stub DynamicScroller

--- spec/frontend/artifacts/components/artifacts_table_row_details_spec.js
+++ spec/frontend/artifacts/components/artifacts_table_row_details_spec.js
@@ -1,4 +1,3 @@
-import { RecycleScroller, DynamicScroller } from 'vendor/vue-virtual-scroller';
 import Vue from 'vue';
 import VueApollo from 'vue-apollo';
 import getJobArtifactsResponse from 'test_fixtures/graphql/artifacts/graphql/queries/get_job_artifacts.query.graphql.json';
@@ -6,7 +5,7 @@ import waitForPromises from 'helpers/wait_for_promises';
 import ArtifactsTableRowDetails from '~/artifacts/components/artifacts_table_row_details.vue';
 import ArtifactRow from '~/artifacts/components/artifact_row.vue';
 import createMockApollo from 'helpers/mock_apollo_helper';
-import { shallowMountExtended } from 'helpers/vue_test_utils_helper';
+import { mountExtended } from 'helpers/vue_test_utils_helper';
 import destroyArtifactMutation from '~/artifacts/graphql/mutations/destroy_artifact.mutation.graphql';
 import { i18n } from '~/artifacts/constants';
 import { createAlert } from '~/flash';
@@ -28,7 +27,7 @@ describe('ArtifactsTableRowDetails component', () => {
     },
   ) => {
     requestHandlers = handlers;
-    wrapper = shallowMountExtended(ArtifactsTableRowDetails, {
+    wrapper = mountExtended(ArtifactsTableRowDetails, {
       apolloProvider: createMockApollo([
         [destroyArtifactMutation, requestHandlers.destroyArtifactMutation],
       ]),
@@ -39,11 +38,6 @@ describe('ArtifactsTableRowDetails component', () => {
       },
       data() {
         return { deletingArtifactId: null };
-      },
-      stubs: {
-        RecycleScroller,
-        DynamicScroller,
-        ArtifactRow,
       },
     });
   };
@@ -57,16 +51,12 @@ describe('ArtifactsTableRowDetails component', () => {
       createComponent();
     });
 
-    it('to the dynamic scroller', () => {
-      expect(wrapper.findComponent(DynamicScroller).props()).toMatchObject({
-        items: artifacts.nodes,
-      });
-    });
-
-    it('to the artifact row', () => {
-      expect(wrapper.findAllComponents(ArtifactRow).at(0).props()).toMatchObject({
-        artifact: artifacts.nodes[0],
-        isLoading: false,
+    it('to the artifact rows', () => {
+      [0, 1, 2].forEach((index) => {
+        expect(wrapper.findAllComponents(ArtifactRow).at(index).props()).toMatchObject({
+          artifact: artifacts.nodes[index],
+          isLoading: false,
+        });
       });
     });
   });
