b5cb954ab7a316846fc35408e8d6b23c1ad1f48e
Tan Le <tle@gitlab.com>
2022-10-17 21:15:58 +1100

    Handle failures in project registration
    This fixes failure handling in the Suggested Reviewers project
    registration which results in:
    
    - NoMethodError exception raised when token creation fails
    - Multiple tokens generated on job retries
    
    We mitigate the above issues by:
    
    - Guarding against failed token creation response
    - Removing error propagating when project is already registered
    
    The ServiceResponse abstraction is also used to ensure consistent
    interface for both success and error cases.

--- ee/lib/gitlab/applied_ml/errors.rb
+++ ee/lib/gitlab/applied_ml/errors.rb
@@ -5,6 +5,7 @@ module Gitlab
     module Errors
       BaseError = Class.new(StandardError)
       ArgumentError = Class.new(BaseError)
+      ProjectAlreadyExists = Class.new(BaseError)
       ResourceNotAvailable = Class.new(BaseError)
       ConfigurationError = Class.new(BaseError)
     end
