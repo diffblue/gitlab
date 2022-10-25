baf6455227c09a6e9d6b0350a5127d663bbfe9b7
Pam Artiaga <partiaga@gitlab.com>
2022-10-25 14:50:42 +1100

    In setup, ensure project.first_owner connection is persisted
    This is done by using `create` instead of `build`

--- spec/lib/sidebars/projects/menus/infrastructure_menu_spec.rb
+++ spec/lib/sidebars/projects/menus/infrastructure_menu_spec.rb
@@ -3,7 +3,7 @@
 require 'spec_helper'
 
 RSpec.describe Sidebars::Projects::Menus::InfrastructureMenu do
-  let(:project) { build(:project) }
+  let(:project) { create(:project) }
   let(:user) { project.first_owner }
   let(:context) { Sidebars::Projects::Context.new(current_user: user, container: project, show_cluster_hint: false) }
 
