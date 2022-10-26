aad2fb963733e5f5afc88e2450179845dfc97cbf
Tiger <twatson@gitlab.com>
2022-10-27 10:41:54 +1100

    Move agent APIs into OpenAPI namespace

--- lib/api/api.rb
+++ lib/api/api.rb
@@ -171,6 +171,8 @@ module API
       namespace do
         mount ::API::AccessRequests
         mount ::API::Appearance
+        mount ::API::Clusters::Agents
+        mount ::API::Clusters::AgentTokens
         mount ::API::DeployKeys
         mount ::API::Deployments
         mount ::API::Metadata
@@ -209,8 +211,6 @@ module API
       mount ::API::Ci::SecureFiles
       mount ::API::Ci::Triggers
       mount ::API::Ci::Variables
-      mount ::API::Clusters::Agents
-      mount ::API::Clusters::AgentTokens
       mount ::API::CommitStatuses
       mount ::API::Commits
       mount ::API::ComposerPackages
