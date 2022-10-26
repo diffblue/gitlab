ea904f9e9e3838e562cace81099349e7206a975f
Artur Fedorov <afedorov@gitlab.com>
2022-10-27 00:31:17 +0200

    This MR request fix stuck validation
    Sometimes validation in pending or in_progress
    state stuck, this mr explicitly fails validation
    if validation runs over an hour
    
    Changelog: changed
    EE: true

--- ee/app/assets/javascripts/security_configuration/utils.js
+++ ee/app/assets/javascripts/security_configuration/utils.js
@@ -4,3 +4,19 @@ export const initFormField = ({ value, required = true, skipValidation = false }
   state: skipValidation ? true : null,
   feedback: null,
 });
+
+/**
+ * Calculate time difference in minutes between
+ * date and now
+ * @param startDate date used for calculation
+ * @returns {number} number of minutes
+ */
+export const getTimeDifferenceMinutes = (startDate) => {
+  if (!startDate) {
+    return 0;
+  }
+
+  const timeDifferenceMinutes = (Date.now() - new Date(startDate).getTime()) / 1000 / 60;
+
+  return Math.ceil(timeDifferenceMinutes);
+};
