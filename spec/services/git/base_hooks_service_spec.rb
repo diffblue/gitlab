7d41b9dad08afedef6eb9dd38aed94118989f2a8
James Fargher <jfargher@gitlab.com>
2022-10-28 10:00:45 +1300

    Remove GitHelpers
    This helper only defines a single function rugged_repo which is now
    unused.

--- spec/services/git/base_hooks_service_spec.rb
+++ spec/services/git/base_hooks_service_spec.rb
@@ -4,7 +4,6 @@ require 'spec_helper'
 
 RSpec.describe Git::BaseHooksService do
   include RepoHelpers
-  include GitHelpers
 
   let(:user) { create(:user) }
   let(:project) { create(:project, :repository) }
