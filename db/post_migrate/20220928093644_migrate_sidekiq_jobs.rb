d701a7cade945d2de120c6e0016eda44ffadf4a0
Gregorius Marco <gmarco@gitlab.com>
2022-10-27 12:46:39 +0800

    Copy implementation from sidekiq_migrate_jobs.rb

--- db/post_migrate/20220928093644_migrate_sidekiq_jobs.rb
+++ db/post_migrate/20220928093644_migrate_sidekiq_jobs.rb
@@ -6,21 +6,24 @@ class MigrateSidekiqJobs < Gitlab::Database::Migration[2.0]
   class SidekiqMigrateJobs
     LOG_FREQUENCY = 1_000
 
-    attr_reader :logger
+    attr_reader :logger, :mappings
 
-    def initialize(logger: nil)
+    # mappings is a hash of WorkerClassName => target_queue_name
+    def initialize(mappings, logger: nil)
+      @mappings = mappings
       @logger = logger
     end
 
+    # rubocop: disable Cop/SidekiqRedisCall
     # Migrates jobs from queues that are outside the mappings
-    # mappings is a hash of WorkerClassName => target_queue_name
-    def migrate_queues(mappings)
+    def migrate_queues
       routing_rules_queues = mappings.values.uniq
       logger&.info("List of queues based on routing rules: #{routing_rules_queues}")
-      Sidekiq.redis do |conn| # rubocop:disable Cop/SidekiqRedisCall
+      Sidekiq.redis do |conn|
         conn.scan_each(match: "queue:*", type: 'list') do |key|
-          queue_from = key.split(':').last
-          next if routing_rules_queues.include? queue_from
+          queue_from = key.split(':', 2).last
+          logger&.info("queue_from #{queue_from}")
+          next if routing_rules_queues.include?(queue_from)
 
           logger&.info("Migrating #{queue_from} queue")
 
@@ -36,10 +39,11 @@ class MigrateSidekiqJobs < Gitlab::Database::Migration[2.0]
               next unless mappings.has_key?(job_hash['class'])
 
               destination_queue = mappings[job_hash['class']]
-              conn.lpush("queue:#{destination_queue}", job)
+              job_hash['queue'] = destination_queue
+              conn.lpush("queue:#{destination_queue}", Sidekiq.dump_json(job_hash))
               migrated += 1
             rescue JSON::ParserError
-              logger&.error("Unmarshal JSON payload from MigrateSidekiqJobs failed. Job: #{job}")
+              logger&.error("Unmarshal JSON payload from SidekiqMigrateJobs failed. Job: #{job}")
               next
             end
           end
@@ -51,16 +55,17 @@ class MigrateSidekiqJobs < Gitlab::Database::Migration[2.0]
     private
 
     def queue_length(queue_name)
-      Sidekiq.redis do |conn| # rubocop:disable Cop/SidekiqRedisCall
+      Sidekiq.redis do |conn|
         conn.llen("queue:#{queue_name}")
       end
     end
   end
+  # rubocop: enable Cop/SidekiqRedisCall
 
   def up
     mappings = Gitlab::SidekiqConfig.worker_queue_mappings
     logger = ::Gitlab::BackgroundMigration::Logger.build
-    SidekiqMigrateJobs.new(logger: logger).migrate_queues(mappings)
+    SidekiqMigrateJobs.new(mappings, logger: logger).migrate_queues
   end
 
   def down
