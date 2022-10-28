7c04cd638a8fdbb87fdb32ed49df6858033d7711
Harsimar Sandhu <hsandhu@gitlab.com>
2022-10-28 07:59:52 +0000

    Migration and model for audit event type streaming filter
    This model is responsible for filtering audit event
    streaming as per external destinations
    
    Changelog: added

---
+++ ee/app/models/audit_events/streaming/event_type_filter.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module AuditEvents
+  module Streaming
+    class EventTypeFilter < ApplicationRecord
+      self.table_name = 'audit_events_streaming_event_type_filters'
+
+      belongs_to :external_audit_event_destination
+
+      validates :audit_event_type,
+                presence: true,
+                length: { maximum: 255 },
+                uniqueness: { scope: :external_audit_event_destination_id }
+    end
+  end
+end
