0440b5c7a96e8fe0b3e5d5eba735e8da671bf550
Joe Woodward <jwoodward@gitlab.com>
2022-10-31 10:22:45 +0000

    Add OpenAPI documentation for ProjectPushRules
    * Add field documentation for Grape::Entity
    * Add parameter documentation for API endpoints
    * Add response codes for API endpoints
    * Add tags for API endpoints

--- ee/lib/ee/api/entities/project_push_rule.rb
+++ ee/lib/ee/api/entities/project_push_rule.rb
@@ -5,12 +5,20 @@ module EE
     module Entities
       class ProjectPushRule < Grape::Entity
         extend ::API::Entities::EntityHelpers
-        expose :id, :project_id, :created_at
-        expose :commit_message_regex, :commit_message_negative_regex, :branch_name_regex, :deny_delete_tag
-        expose :member_check, :prevent_secrets, :author_email_regex
-        expose :file_name_regex, :max_file_size
-        expose_restricted :commit_committer_check, &:project
-        expose_restricted :reject_unsigned_commits, &:project
+        expose :id, documentation: { type: 'integer', example: 2 }
+        expose :project_id, documentation: { type: 'integer', example: 3 }
+        expose :created_at, documentation: { type: 'date-time', example: '2020-08-31T15:53:00.073Z' }
+        expose :commit_message_regex, documentation: { type: 'string', example: 'Fixed \d+\..*' }
+        expose :commit_message_negative_regex, documentation: { type: 'string', example: 'ssh\:\/\/' }
+        expose :branch_name_regex, documentation: { type: 'string', example: '(feature|hotfix)\/*' }
+        expose :deny_delete_tag, documentation: { type: 'boolean', example: true }
+        expose :member_check, documentation: { type: 'boolean', example: true }
+        expose :prevent_secrets, documentation: { type: 'boolean', example: true }
+        expose :author_email_regex, documentation: { type: 'string', example: '@my-company.com$' }
+        expose :file_name_regex, documentation: { type: 'string', example: '(jar|exe)$' }
+        expose :max_file_size, documentation: { type: 'integer', example: 1024 }
+        expose_restricted :commit_committer_check, documentation: { type: 'boolean', example: true }, &:project
+        expose_restricted :reject_unsigned_commits, documentation: { type: 'boolean', example: true }, &:project
       end
     end
   end
