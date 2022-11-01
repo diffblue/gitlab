75dab9124a967129cdcd06ea0cedc9b47eac66f9
Savas Vedova <svedova@gitlab.com>
2022-11-01 14:12:44 +0000

    Display help buttons for MR security reports widget
    - Behind `refactor_security_extension` feature flag
    - https://gitlab.com/gitlab-org/gitlab/-/merge_requests/101941

--- ee/app/assets/javascripts/vue_merge_request_widget/extensions/security_reports/i18n.js
+++ ee/app/assets/javascripts/vue_merge_request_widget/extensions/security_reports/i18n.js
@@ -1,18 +1,22 @@
 import { __, s__ } from '~/locale';
 
-export default {
+export const reportTypes = {
+  SAST: s__('ciReport|SAST'),
+  DAST: s__('ciReport|DAST'),
+  DEPENDENCY_SCANNING: s__('ciReport|Dependency scanning'),
+  SECRET_DETECTION: s__('ciReport|Secret detection'),
+  COVERAGE_FUZZING: s__('ciReport|Coverage fuzzing'),
+  API_FUZZING: s__('ciReport|API fuzzing'),
+};
+
+export const i18n = {
   new: __('New'),
   fixed: __('Fixed'),
+  learnMore: __('Learn more'),
   label: s__('ciReport|Security scanning'),
   loading: s__('ciReport|Security scanning is loading'),
-  sastScanner: s__('ciReport|SAST'),
-  dastScanner: s__('ciReport|DAST'),
-  dependencyScanner: s__('ciReport|Dependency scanning'),
-  secretDetectionScanner: s__('ciReport|Secret detection'),
-  coverageFuzzing: s__('ciReport|Coverage fuzzing'),
-  apiFuzzing: s__('ciReport|API fuzzing'),
-  securityScanning: s__('ciReport|Security scanning'),
   error: s__('ciReport|Security reports failed loading results'),
+  securityScanning: s__('ciReport|Security scanning'),
   highlights: s__(
     'ciReport|%{criticalStart}critical%{criticalEnd}, %{highStart}high%{highEnd} and %{otherStart}others%{otherEnd}',
   ),
@@ -20,4 +24,25 @@ export default {
     'ciReport|%{scanner} detected no %{boldStart}new%{boldEnd} potential vulnerabilities',
   ),
   newVulnerabilities: s__('ciReport|%{scanner} detected %{number} new potential %{vulnStr}'),
+};
+
+export const popovers = {
+  SAST_TEXT: s__('ciReport|Detects known vulnerabilities in your source code.'),
+  SAST_TITLE: s__('ciReport|Static Application Security Testing (SAST)'),
+
+  DAST_TEXT: s__('ciReport|Detects known vulnerabilities in your web application.'),
+  DAST_TITLE: s__('ciReport|Dynamic Application Security Testing (DAST)'),
+
+  DEPENDENCY_SCANNING_TITLE: reportTypes.DEPENDENCY_SCANNING,
+  DEPENDENCY_SCANNING_TEXT: s__(
+    "ciReport|Detects known vulnerabilities in your source code's dependencies.",
+  ),
+
+  SECRET_DETECTION_TITLE: reportTypes.SECRET_DETECTION,
+  SECRET_DETECTION_TEXT: s__(
+    'ciReport|Detects secrets and credentials vulnerabilities in your source code.',
+  ),
+
+  COVERAGE_FUZZING_TITLE: reportTypes.COVERAGE_FUZZING,
+  API_FUZZING_TITLE: reportTypes.API_FUZZING,
 };
