af2b791cd64bc41f4aa0c4aab0f06cb52c0bf5dd
bmarjanovic <bmarjanovic@gitlab.com>
2022-11-03 11:12:57 +0100

    Add GroupHooks to OpenAPI documetation
    * Add field documentation for Grape::Entity
    * Add response codes for API endpoints
    
    EE: true

--- ee/lib/ee/api/entities/group_hook.rb
+++ ee/lib/ee/api/entities/group_hook.rb
@@ -4,10 +4,18 @@ module EE
   module API
     module Entities
       class GroupHook < ::API::Entities::Hook
-        expose :group_id, :issues_events, :confidential_issues_events,
-               :note_events, :confidential_note_events, :pipeline_events, :wiki_page_events,
-               :job_events, :deployment_events, :releases_events, :subgroup_events
-        expose :push_events_branch_filter
+        expose :group_id, documentation: { type: 'string', example: 1 }
+        expose :issues_events, documentation: { type: 'boolean' }
+        expose :confidential_issues_events, documentation: { type: 'boolean' }
+        expose :note_events, documentation: { type: 'boolean' }
+        expose :confidential_note_events, documentation: { type: 'boolean' }
+        expose :pipeline_events, documentation: { type: 'boolean' }
+        expose :wiki_page_events, documentation: { type: 'boolean' }
+        expose :job_events, documentation: { type: 'boolean' }
+        expose :deployment_events, documentation: { type: 'boolean' }
+        expose :releases_events, documentation: { type: 'boolean' }
+        expose :subgroup_events, documentation: { type: 'boolean' }
+        expose :push_events_branch_filter, documentation: { type: 'string', example: 'my-branch-*' }
       end
     end
   end
