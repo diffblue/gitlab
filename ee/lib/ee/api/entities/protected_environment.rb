c449c675cbc38effbffaf0a87e88f43039916f85
Ahmed Hemdan <ahemdan@gitlab.com>
2022-11-04 16:23:58 +0200

    Add protected environments endpoints to OpenApi v2 docs
    Adds the endpoints for project-level and group-level protected
    environments APIs to OpenApi v2 docs

--- ee/lib/ee/api/entities/protected_environment.rb
+++ ee/lib/ee/api/entities/protected_environment.rb
@@ -4,9 +4,9 @@ module EE
   module API
     module Entities
       class ProtectedEnvironment < Grape::Entity
-        expose :name
+        expose :name, documentation: { type: 'string', example: 'production' }
         expose :deploy_access_levels, using: ::API::Entities::ProtectedEnvironments::DeployAccessLevel
-        expose :required_approval_count
+        expose :required_approval_count, documentation: { type: 'integer', example: 2 }
         expose :approval_rules, using: ::API::Entities::ProtectedEnvironments::ApprovalRule
       end
     end
