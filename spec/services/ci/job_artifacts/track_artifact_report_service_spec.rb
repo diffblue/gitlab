7f9b8d84d34362a1f18a57728fd4c972ac33ebed
samsam_kim <skim@gitlab.com>
2022-11-04 17:41:36 -0700

    Updates to test spec

--- spec/services/ci/job_artifacts/track_artifact_report_service_spec.rb
+++ spec/services/ci/job_artifacts/track_artifact_report_service_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe Ci::JobArtifacts::TrackArtifactReportService do
 
     let(:test_event_name_1) { 'i_testing_test_report_uploaded' }
     let(:test_event_name_2) { 'i_testing_coverage_report_uploaded' }
+
     let(:counter) { Gitlab::UsageDataCounters::HLLRedisCounter }
     let(:start_time) { 1.week.ago }
     let(:end_time) { 1.week.from_now }
@@ -40,21 +41,6 @@ RSpec.describe Ci::JobArtifacts::TrackArtifactReportService do
               }
           .by 1
       end
-
-      it 'tracks the coverage event using HLLRedisCounter' do
-        allow(Gitlab::UsageDataCounters::HLLRedisCounter)
-          .to receive(:track_event)
-          .with(test_event_name_2, values: user1.id)
-          .and_call_original
-
-        expect { track_artifact_report }
-          .to change {
-                counter.unique_events(event_names: test_event_name_2,
-                                      start_date: start_time,
-                                      end_date: end_time)
-              }
-          .by 1
-      end
     end
 
     context 'when pipeline does not have test reports' do
@@ -68,7 +54,7 @@ RSpec.describe Ci::JobArtifacts::TrackArtifactReportService do
           .with(anything, test_event_name_1)
       end
 
-      it 'does not track the coverage event' do
+      it 'does not track the coverage test event' do
         track_artifact_report
 
         expect(Gitlab::UsageDataCounters::HLLRedisCounter)
@@ -98,29 +84,6 @@ RSpec.describe Ci::JobArtifacts::TrackArtifactReportService do
         end
           .to change {
                 counter.unique_events(event_names: test_event_name_1,
-                                      start_date: start_time,
-                                      end_date: end_time)
-              }
-          .by 1
-      end
-
-      it 'tracks all pipelines using HLLRedisCounter by one user_id for the coverage event' do
-        allow(Gitlab::UsageDataCounters::HLLRedisCounter)
-          .to receive(:track_event)
-          .with(test_event_name_2, values: user1.id)
-          .and_call_original
-
-        allow(Gitlab::UsageDataCounters::HLLRedisCounter)
-          .to receive(:track_event)
-          .with(test_event_name_2, values: user1.id)
-          .and_call_original
-
-        expect do
-          described_class.new.execute(pipeline1)
-          described_class.new.execute(pipeline2)
-        end
-          .to change {
-                counter.unique_events(event_names: test_event_name_2,
                                       start_date: start_time,
                                       end_date: end_time)
               }
@@ -164,8 +127,66 @@ RSpec.describe Ci::JobArtifacts::TrackArtifactReportService do
               }
           .by 2
       end
+    end
 
-      it 'tracks all pipelines using HLLRedisCounter by multiple users for coverage reports' do
+    context 'when pipeline has coverage test reports' do
+      let_it_be(:pipeline) { create(:ci_pipeline, project: project, user: user1) }
+
+      before do
+        2.times do
+          pipeline.builds << build(:ci_build, :coverage_reports, pipeline: pipeline, project: pipeline.project)
+        end
+      end
+
+      it 'tracks the coverage test event using HLLRedisCounter' do
+        allow(Gitlab::UsageDataCounters::HLLRedisCounter)
+          .to receive(:track_event)
+          .with(test_event_name_2, values: user1.id)
+          .and_call_original
+
+        expect { track_artifact_report }
+          .to change {
+                counter.unique_events(event_names: test_event_name_2,
+                                      start_date: start_time,
+                                      end_date: end_time)
+              }
+          .by 1
+      end
+    end
+
+    context 'when a single user started multiple pipelines with coverage reports' do
+      let_it_be(:pipeline1) { create(:ci_pipeline, :with_coverage_reports, project: project, user: user1) }
+      let_it_be(:pipeline2) { create(:ci_pipeline, :with_coverage_reports, project: project, user: user1) }
+
+      it 'tracks all pipelines using HLLRedisCounter by one user_id for the coverage test event' do
+        allow(Gitlab::UsageDataCounters::HLLRedisCounter)
+          .to receive(:track_event)
+          .with(test_event_name_2, values: user1.id)
+          .and_call_original
+
+        allow(Gitlab::UsageDataCounters::HLLRedisCounter)
+          .to receive(:track_event)
+          .with(test_event_name_2, values: user1.id)
+          .and_call_original
+
+        expect do
+          described_class.new.execute(pipeline1)
+          described_class.new.execute(pipeline2)
+        end
+          .to change {
+                counter.unique_events(event_names: test_event_name_2,
+                                      start_date: start_time,
+                                      end_date: end_time)
+              }
+          .by 1
+      end
+    end
+
+    context 'when multiple users started multiple pipelines with coverage test reports' do
+      let_it_be(:pipeline1) { create(:ci_pipeline, :with_coverage_reports, project: project, user: user1) }
+      let_it_be(:pipeline2) { create(:ci_pipeline, :with_coverage_reports, project: project, user: user2) }
+
+      it 'tracks all pipelines using HLLRedisCounter by multiple users for coverage test reports' do
         allow(Gitlab::UsageDataCounters::HLLRedisCounter)
           .to receive(:track_event)
           .with(test_event_name_2, values: user1.id)
@@ -198,5 +219,6 @@ RSpec.describe Ci::JobArtifacts::TrackArtifactReportService do
           .by 2
       end
     end
+
   end
 end
