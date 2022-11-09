c084c747ba5867c865b3de8250d207e87ab053d4
bmarjanovic <bmarjanovic@gitlab.com>
2022-11-09 16:24:58 +0100

    Add LdapGroupLinks to OpenAPI documetation
    * Add field documentation for Grape::Entity
    * Add tags and response codes for API endpoints

--- ee/lib/ee/api/entities/ldap_group_link.rb
+++ ee/lib/ee/api/entities/ldap_group_link.rb
@@ -4,8 +4,12 @@ module EE
   module API
     module Entities
       class LdapGroupLink < Grape::Entity
-        expose :cn, :group_access, :provider
-        expose :filter, if: ->(_, _) { License.feature_available?(:ldap_group_sync_filter) }
+        expose :cn, documentation: { type: 'string', example: 'ldap-group-1' }
+        expose :group_access, documentation: { type: 'integer', example: 10 }
+        expose :provider, documentation: { type: 'string', example: 'ldapmain' }
+        expose :filter, documentation: { type: 'string', example: 'id >= 500' }, if: ->(_, _) do
+          License.feature_available?(:ldap_group_sync_filter)
+        end
       end
     end
   end
