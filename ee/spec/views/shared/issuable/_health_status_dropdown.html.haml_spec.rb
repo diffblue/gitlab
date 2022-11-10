037751289c6a5ca36a616e88879e7f5f4f59943a
Coung Ngo <cngo@gitlab.com>
2022-11-10 20:57:57 +0000

    Refactor EE sidebar for naming consistency
    Make names etc consistent

--- ee/spec/views/shared/issuable/_health_status_dropdown.html.haml_spec.rb
+++ ee/spec/views/shared/issuable/_health_status_dropdown.html.haml_spec.rb
@@ -6,6 +6,6 @@ RSpec.describe 'shared/issuable/_health_status_dropdown.html.haml' do
   it_behaves_like 'issuable bulk dropdown', 'shared/issuable/health_status_dropdown' do
     let(:feature_id) { :issuable_health_status }
     let(:input_selector) { 'input#issue_health_status_value[name="update[health_status]"]' }
-    let(:root_selector) { "#js-bulk-update-health-status-root" }
+    let(:root_selector) { ".js-health-status-dropdown-root" }
   end
 end
