f1b0b24b515b52c27a8b49e2fe3e82526499a86c
Ahmed Hemdan <ahemdan@gitlab.com>
2022-11-10 14:10:04 +0200

    Update OpenApi v2 docs for some API endpoints
    This commit updates the OpenApi v2 docs for release group related API
    endpoints that were missing some necessary changes.

--- ee/lib/api/entities/deployments/approval.rb
+++ ee/lib/api/entities/deployments/approval.rb
@@ -5,9 +5,9 @@ module API
     module Deployments
       class Approval < Grape::Entity
         expose :user, using: Entities::UserBasic
-        expose :status
-        expose :created_at
-        expose :comment
+        expose :status, documentation: { type: 'string', example: 'approved' }
+        expose :created_at, documentation: { type: 'dateTime', example: '2022-02-24T20:22:30.097Z' }
+        expose :comment, documentation: { type: 'string', example: 'LGTM' }
       end
     end
   end
