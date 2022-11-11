ea8511043f083e917a508a9f7563dcb605187baa
Sean McGivern <sean@gitlab.com>
2022-11-11 14:54:10 +0000

    Applies maintainer's suggestions

--- lib/api/entities/resource_milestone_event.rb
+++ lib/api/entities/resource_milestone_event.rb
@@ -3,13 +3,13 @@
 module API
   module Entities
     class ResourceMilestoneEvent < Grape::Entity
-      expose :id, documentation: { type: 'string', example: 142 }
+      expose :id, documentation: { type: 'integer', example: 142 }
       expose :user, using: Entities::UserBasic
       expose :created_at, documentation: { type: 'dateTime', example: '2018-08-20T13:38:20.077Z' }
       expose :resource_type, documentation: { type: 'string', example: 'Issue' } do |event, _options|
         event.issuable.class.name
       end
-      expose :resource_id, documentation: { type: 'string', example: 253 } do |event, _options|
+      expose :resource_id, documentation: { type: 'integer', example: 253 } do |event, _options|
         event.issuable.id
       end
       expose :milestone, using: Entities::Milestone
