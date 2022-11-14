4a3d818a81aed5956f29cab197cdcd931c810e9b
Max Orefice <morefice@gitlab.com>
2022-11-14 09:28:30 +0000

    Replace default_value_for on integrations models
    https://gitlab.com/gitlab-org/gitlab/-/issues/378997

--- ee/spec/models/integrations/gitlab_slack_application_spec.rb
+++ ee/spec/models/integrations/gitlab_slack_application_spec.rb
@@ -3,6 +3,10 @@
 require 'spec_helper'
 
 RSpec.describe Integrations::GitlabSlackApplication do
+  describe 'default values' do
+    it { expect(subject.category).to eq(:chat) }
+  end
+
   describe '#chat_responder' do
     it 'returns the chat responder to use' do
       expect(subject.chat_responder).to eq(Gitlab::Chat::Responder::Slack)
