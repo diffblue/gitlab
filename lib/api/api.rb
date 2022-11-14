7b62aa66038112b7eefbc7106c98c8f75f7c2acd
bmarjanovic <bmarjanovic@gitlab.com>
2022-11-14 15:37:05 +0100

    Add GroupPackages to OpenAPI documetation
    * Add field documentation for Grape::Entity
    * Add tags and response codes for API endpoints

--- lib/api/api.rb
+++ lib/api/api.rb
@@ -202,6 +202,7 @@ module API
         mount ::API::GroupClusters
         mount ::API::GroupExport
         mount ::API::GroupImport
+        mount ::API::GroupPackages
         mount ::API::GroupVariables
         mount ::API::ImportBitbucketServer
         mount ::API::ImportGithub
@@ -283,7 +284,6 @@ module API
       mount ::API::GroupDebianDistributions
       mount ::API::GroupLabels
       mount ::API::GroupMilestones
-      mount ::API::GroupPackages
       mount ::API::Groups
       mount ::API::HelmPackages
       mount ::API::Integrations
