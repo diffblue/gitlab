9ac5f29691115ebe94c9d1c33664da1a604367c5
Daniel Tian <dtian@gitlab.com>
2022-11-14 09:08:30 +0000

    Refactor vulnerability report status filter
    Changelog: changed
    MR:
    EE: true

---
+++ ee/app/assets/javascripts/security_dashboard/components/shared/filters/status_filter.vue
@@ -0,0 +1,98 @@
+<script>
+import { GlDropdown } from '@gitlab/ui';
+import { xor } from 'lodash';
+import { s__ } from '~/locale';
+import { VULNERABILITY_STATE_OBJECTS } from 'ee/vulnerabilities/constants';
+import DropdownButtonText from './dropdown_button_text.vue';
+import QuerystringSync from './querystring_sync.vue';
+import FilterItem from './filter_item.vue';
+import { ALL_ID } from './constants';
+
+const { detected, confirmed, dismissed, resolved } = VULNERABILITY_STATE_OBJECTS;
+// For backwards compatibility with existing bookmarks, the ID needs to be capitalized.
+export const DROPDOWN_OPTIONS = [
+  { id: detected.state.toUpperCase(), text: detected.buttonText },
+  { id: confirmed.state.toUpperCase(), text: confirmed.buttonText },
+  { id: dismissed.state.toUpperCase(), text: dismissed.buttonText },
+  { id: resolved.state.toUpperCase(), text: resolved.buttonText },
+];
+export const VALID_IDS = [ALL_ID, ...DROPDOWN_OPTIONS.map(({ id }) => id)];
+export const DEFAULT_IDS = [detected.state.toUpperCase(), confirmed.state.toUpperCase()];
+
+export default {
+  components: { GlDropdown, DropdownButtonText, QuerystringSync, FilterItem },
+  data: () => ({
+    selected: DEFAULT_IDS,
+  }),
+  computed: {
+    selectedIds() {
+      return this.selected.length ? this.selected : [ALL_ID];
+    },
+    selectedItemTexts() {
+      const options = DROPDOWN_OPTIONS.filter(({ id }) => this.selected.includes(id));
+      // Return the text for selected items, or all items if nothing is selected.
+      return options.length ? options.map(({ text }) => text) : [this.$options.i18n.allItemsText];
+    },
+  },
+  watch: {
+    selected() {
+      this.$emit('filter-changed', { state: this.selected });
+    },
+  },
+  methods: {
+    deselectAll() {
+      this.selected = [];
+    },
+    toggleSelected(id) {
+      this.selected = xor(this.selected, [id]);
+    },
+    setSelected(ids) {
+      if (ids.includes(ALL_ID)) {
+        this.selected = [];
+      } else if (!ids.length) {
+        this.selected = DEFAULT_IDS;
+      } else {
+        this.selected = ids;
+      }
+    },
+  },
+  i18n: {
+    label: s__('SecurityReports|Status'),
+    allItemsText: s__('SecurityReports|All statuses'),
+  },
+  DROPDOWN_OPTIONS,
+  VALID_IDS,
+  ALL_ID,
+};
+</script>
+
+<template>
+  <div>
+    <querystring-sync
+      querystring-key="state"
+      :value="selectedIds"
+      :valid-values="$options.VALID_IDS"
+      @input="setSelected"
+    />
+    <label class="gl-mb-2">{{ $options.i18n.label }}</label>
+    <gl-dropdown :header-text="$options.i18n.label" block toggle-class="gl-mb-0">
+      <template #button-text>
+        <dropdown-button-text :items="selectedItemTexts" />
+      </template>
+      <filter-item
+        :is-checked="!selected.length"
+        :text="$options.i18n.allItemsText"
+        :data-testid="$options.ALL_ID"
+        @click="deselectAll"
+      />
+      <filter-item
+        v-for="{ id, text } in $options.DROPDOWN_OPTIONS"
+        :key="id"
+        :data-testid="id"
+        :is-checked="selected.includes(id)"
+        :text="text"
+        @click="toggleSelected(id)"
+      />
+    </gl-dropdown>
+  </div>
+</template>
---
+++ ee/spec/frontend/security_dashboard/components/shared/filters/status_filter_spec.js
@@ -0,0 +1,164 @@
+import { GlDropdown } from '@gitlab/ui';
+import { nextTick } from 'vue';
+import StatusFilter, {
+  DEFAULT_IDS,
+  VALID_IDS,
+  DROPDOWN_OPTIONS,
+} from 'ee/security_dashboard/components/shared/filters/status_filter.vue';
+import { ALL_ID } from 'ee/security_dashboard/components/shared/filters/constants';
+import QuerystringSync from 'ee/security_dashboard/components/shared/filters/querystring_sync.vue';
+import DropdownButtonText from 'ee/security_dashboard/components/shared/filters/dropdown_button_text.vue';
+import FilterItem from 'ee/security_dashboard/components/shared/filters/filter_item.vue';
+import { mountExtended } from 'helpers/vue_test_utils_helper';
+
+const OPTION_IDS = DROPDOWN_OPTIONS.map(({ id }) => id);
+
+describe('Status Filter component', () => {
+  let wrapper;
+
+  const createWrapper = () => {
+    wrapper = mountExtended(StatusFilter, {
+      stubs: { QuerystringSync: true },
+    });
+  };
+
+  const findQuerystringSync = () => wrapper.findComponent(QuerystringSync);
+  const findDropdownItems = () => wrapper.findAllComponents(FilterItem);
+  const findDropdownItem = (id) => wrapper.findByTestId(id);
+
+  const clickDropdownItem = async (id) => {
+    findDropdownItem(id).vm.$emit('click');
+    await nextTick();
+  };
+
+  const expectSelectedItems = (ids) => {
+    const checkedItems = findDropdownItems()
+      .wrappers.filter((item) => item.props('isChecked'))
+      .map((item) => item.attributes('data-testid'));
+
+    expect(checkedItems).toEqual(ids);
+  };
+
+  beforeEach(() => {
+    createWrapper();
+  });
+
+  describe('QuerystringSync component', () => {
+    it('has expected props', () => {
+      expect(findQuerystringSync().props()).toMatchObject({
+        querystringKey: 'state',
+        value: DEFAULT_IDS,
+        validValues: VALID_IDS,
+      });
+    });
+
+    it('receives ALL_ID when All Statuses option is clicked', async () => {
+      await clickDropdownItem(ALL_ID);
+
+      expect(findQuerystringSync().props('value')).toEqual([ALL_ID]);
+    });
+
+    it.each`
+      emitted                      | expected
+      ${['CONFIRMED', 'RESOLVED']} | ${['CONFIRMED', 'RESOLVED']}
+      ${[]}                        | ${DEFAULT_IDS}
+      ${[ALL_ID]}                  | ${[ALL_ID]}
+    `('restores selected items - $emitted', async ({ emitted, expected }) => {
+      findQuerystringSync().vm.$emit('input', emitted);
+      await nextTick();
+
+      expectSelectedItems(expected);
+    });
+  });
+
+  describe('default view', () => {
+    it('shows the label', () => {
+      expect(wrapper.find('label').text()).toBe(StatusFilter.i18n.label);
+    });
+
+    it('shows the dropdown with correct header text', () => {
+      expect(wrapper.findComponent(GlDropdown).props('headerText')).toBe(StatusFilter.i18n.label);
+    });
+
+    it('shows the DropdownButtonText component with the correct items', () => {
+      expect(wrapper.findComponent(DropdownButtonText).props('items')).toEqual([
+        'Needs triage',
+        'Confirmed',
+      ]);
+    });
+  });
+
+  describe('dropdown items', () => {
+    it('shows all dropdown items with correct text', () => {
+      expect(findDropdownItems()).toHaveLength(DROPDOWN_OPTIONS.length + 1);
+
+      expect(findDropdownItem(ALL_ID).text()).toBe(StatusFilter.i18n.allItemsText);
+      DROPDOWN_OPTIONS.forEach(({ id, text }) => {
+        expect(findDropdownItem(id).text()).toBe(text);
+      });
+    });
+
+    it('allows multiple items to be selected', async () => {
+      const ids = [];
+      // Deselect everything to begin with.
+      clickDropdownItem(ALL_ID);
+
+      for await (const id of OPTION_IDS) {
+        await clickDropdownItem(id);
+        ids.push(id);
+
+        expectSelectedItems(ids);
+      }
+    });
+
+    it('toggles the item selection when clicked on', async () => {
+      // Deselect everything to begin with.
+      clickDropdownItem(ALL_ID);
+
+      for await (const id of OPTION_IDS) {
+        await clickDropdownItem(id);
+
+        expectSelectedItems([id]);
+
+        await clickDropdownItem(id);
+
+        expectSelectedItems([ALL_ID]);
+      }
+    });
+
+    it('selects default items when created', () => {
+      expectSelectedItems(DEFAULT_IDS);
+    });
+
+    it('selects ALL item and deselects everything else when it is clicked', async () => {
+      await clickDropdownItem(ALL_ID);
+      await clickDropdownItem(ALL_ID); // Click again to verify that it doesn't toggle.
+
+      expectSelectedItems([ALL_ID]);
+    });
+
+    it('deselects the ALL item when another item is clicked', async () => {
+      await clickDropdownItem(ALL_ID);
+      await clickDropdownItem(OPTION_IDS[0]);
+
+      expectSelectedItems([OPTION_IDS[0]]);
+    });
+  });
+
+  describe('filter-changed event', () => {
+    it('emits filter-changed event when selected item is changed', async () => {
+      const ids = [];
+      // Deselect everything to begin with.
+      await clickDropdownItem(ALL_ID);
+
+      expect(wrapper.emitted('filter-changed')[0][0].state).toEqual([]);
+
+      for await (const id of OPTION_IDS) {
+        await clickDropdownItem(id);
+        ids.push(id);
+
+        expect(wrapper.emitted('filter-changed')[ids.length][0].state).toEqual(ids);
+      }
+    });
+  });
+});
