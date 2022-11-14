9968ac330cd2b7bdd1b6cfb2071c4599e884094a
Nicolas Dular <ndular@gitlab.com>
2022-11-14 13:23:31 +0100

    Add OpenAPI documentation for RelatedEpicLinks
    - Add respond codes for endpoint
    - Documentation of the related entities

--- ee/lib/api/entities/related_epic_link.rb
+++ ee/lib/api/entities/related_epic_link.rb
@@ -5,7 +5,7 @@ module API
     class RelatedEpicLink < Grape::Entity
       expose :source, as: :source_epic, using: ::EE::API::Entities::Epic
       expose :target, as: :target_epic, using: ::EE::API::Entities::Epic
-      expose :link_type
+      expose :link_type, documentation: { type: "string", example: "relates_to" }
     end
   end
 end
