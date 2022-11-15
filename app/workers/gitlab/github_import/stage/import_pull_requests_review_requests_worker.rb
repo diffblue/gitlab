f5159c0c538bbc6d04ea6f754a285f7ae02d16cc
Rostyslav Safonov <elhowm@gmail.com>
2022-11-15 14:14:25 +0000

    [355137] Migrate MR review requests
    We already importing review approvals but now we also
    importing info who is currently assigned as reviewer.
    Details:
    https://gitlab.com/gitlab-org/gitlab/-/issues/355137
    
    Changelog: added

--- app/workers/gitlab/github_import/stage/import_pull_requests_review_requests_worker.rb
+++ app/workers/gitlab/github_import/stage/import_pull_requests_review_requests_worker.rb
@@ -3,7 +3,7 @@
 module Gitlab
   module GithubImport
     module Stage
-      class ImportPullRequestsMergedByWorker # rubocop:disable Scalability/IdempotentWorker
+      class ImportPullRequestsReviewRequestsWorker # rubocop:disable Scalability/IdempotentWorker
         include ApplicationWorker
 
         data_consistency :always
@@ -15,16 +15,16 @@ module Gitlab
         # client - An instance of Gitlab::GithubImport::Client.
         # project - An instance of Project.
         def import(client, project)
-          waiter = Importer::PullRequestsMergedByImporter
-            .new(project, client)
-            .execute
+          waiter = Importer::PullRequests::ReviewRequestsImporter
+           .new(project, client)
+           .execute
 
           project.import_state.refresh_jid_expiration
 
           AdvanceStageWorker.perform_async(
             project.id,
             { waiter.key => waiter.jobs_remaining },
-            :pull_request_review_requests
+            :pull_request_reviews
           )
         end
       end
