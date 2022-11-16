ad3bff0fb9a6c59769ae62681f3ba5bfe5b8c3b7
Terri Chu <tchu@gitlab.com>
2022-11-16 17:14:49 +0000

    Refactor feature specs for vertical Vue search nav
    Also add new rate limiting spec trait and update docs

--- ee/spec/features/search/user_searches_for_epics_spec.rb
+++ ee/spec/features/search/user_searches_for_epics_spec.rb
@@ -2,72 +2,80 @@
 
 require 'spec_helper'
 
-RSpec.describe 'User searches for epics', :js do
-  let!(:user) { create(:user) }
-  let!(:group) { create(:group) }
-  let!(:epic1) { create(:epic, title: 'Foo', group: group, updated_at: 6.days.ago) }
-  let!(:epic2) { create(:epic, :closed, :confidential, title: 'Bar', group: group) }
+RSpec.describe 'User searches for epics', :js, :disable_rate_limiter do
+  using RSpec::Parameterized::TableSyntax
+
+  let_it_be(:user) { create(:user) }
+  let_it_be(:group) { create(:group) }
+  let_it_be(:epic1) { create(:epic, title: 'Foo', group: group, updated_at: 6.days.ago) }
+  let_it_be(:epic2) { create(:epic, :closed, :confidential, title: 'Bar', group: group) }
 
   def search_for_epic(search)
     fill_in('dashboard_search', with: search)
     find('.gl-search-box-by-click-search-button').click
+
     select_search_scope('Epics')
   end
 
-  before do
-    stub_feature_flags(search_page_vertical_nav: false)
-    stub_licensed_features(epics: true)
+  where(search_page_vertical_nav_enabled: [true, false])
+  with_them do
+    before do
+      stub_feature_flags(search_page_vertical_nav: search_page_vertical_nav_enabled)
+      stub_licensed_features(epics: true)
 
-    group.add_maintainer(user)
-    sign_in(user)
+      group.add_maintainer(user)
+      sign_in(user)
 
-    visit(search_path(group_id: group.id))
-  end
-
-  include_examples 'top right search form'
-  include_examples 'search timeouts', 'epics'
-
-  it 'finds an epic' do
-    search_for_epic(epic1.title)
-
-    page.within('.results') do
-      expect(page).to have_link(epic1.title)
-      expect(page).to have_text('updated 6 days ago')
-      expect(page).not_to have_link(epic2.title)
+      visit(search_path(group_id: group.id))
     end
-  end
 
-  it 'hides confidential icon for non-confidential epics' do
-    search_for_epic(epic1.title)
-
-    page.within('.results') do
-      expect(page).not_to have_css('[data-testid="eye-slash-icon"]')
+    include_examples 'top right search form'
+    include_examples 'search timeouts', 'epics' do
+      let(:additional_params) { { group_id: group.id } }
     end
-  end
 
-  it 'shows confidential icon for confidential epics' do
-    search_for_epic(epic2.title)
+    it 'finds an epic' do
+      search_for_epic(epic1.title)
 
-    page.within('.results') do
-      expect(page).to have_css('[data-testid="eye-slash-icon"]')
+      page.within('.results') do
+        expect(page).to have_link(epic1.title)
+        expect(page).to have_text('updated 6 days ago')
+        expect(page).not_to have_link(epic2.title)
+      end
     end
-  end
 
-  it 'shows correct badge for open epics' do
-    search_for_epic(epic1.title)
+    it 'hides confidential icon for non-confidential epics' do
+      search_for_epic(epic1.title)
 
-    page.within('.results') do
-      expect(page).to have_css('.badge-success')
-      expect(page).not_to have_css('.badge-info')
+      page.within('.results') do
+        expect(page).not_to have_css('[data-testid="eye-slash-icon"]')
+      end
     end
-  end
 
-  it 'shows correct badge for closed epics' do
-    search_for_epic(epic2.title)
+    it 'shows confidential icon for confidential epics' do
+      search_for_epic(epic2.title)
 
-    page.within('.results') do
-      expect(page).not_to have_css('.badge-success')
-      expect(page).to have_css('.badge-info')
+      page.within('.results') do
+        expect(page).to have_css('[data-testid="eye-slash-icon"]')
+      end
+    end
+
+    it 'shows correct badge for open epics' do
+      search_for_epic(epic1.title)
+
+      page.within('.results') do
+        expect(page).to have_css('.badge-success')
+        expect(page).not_to have_css('.badge-info')
+      end
+    end
+
+    it 'shows correct badge for closed epics' do
+      search_for_epic(epic2.title)
+
+      page.within('.results') do
+        expect(page).not_to have_css('.badge-success')
+        expect(page).to have_css('.badge-info')
+      end
     end
   end
 end
