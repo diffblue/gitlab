5be9fd106f57f19af4c056a2409dd8d5d436f9d0
Daniel Tian <dtian@gitlab.com>
2022-11-17 11:32:12 +0000

    Fix missing QA selector for vulnerability report status filter

--- ee/app/assets/javascripts/security_dashboard/components/shared/filters/status_filter.vue
+++ ee/app/assets/javascripts/security_dashboard/components/shared/filters/status_filter.vue
@@ -77,7 +77,10 @@ export default {
     <label class="gl-mb-2">{{ $options.i18n.label }}</label>
     <gl-dropdown :header-text="$options.i18n.label" block toggle-class="gl-mb-0">
       <template #button-text>
-        <dropdown-button-text :items="selectedItemTexts" />
+        <dropdown-button-text
+          :items="selectedItemTexts"
+          data-qa-selector="filter_status_dropdown"
+        />
       </template>
       <filter-item
         :is-checked="!selected.length"
