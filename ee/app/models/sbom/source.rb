b96aa7426fbaf0519cfa2ed8c53606c1ff65c8c4
Brian Williams <bwilliams@gitlab.com>
2022-11-21 10:55:54 -0600

    Remove ignore_column for Sbom::Source.fingerprint
    This column was ignored in 15.5, removed in 15.6, and now the
    ignore_column statement can be removed with 15.7.

--- ee/app/models/sbom/source.rb
+++ ee/app/models/sbom/source.rb
@@ -2,9 +2,6 @@
 
 module Sbom
   class Source < ApplicationRecord
-    include IgnorableColumns
-    ignore_column :fingerprint, remove_with: '15.7', remove_after: '2022-12-22'
-
     enum source_type: {
       dependency_scanning: 0,
       container_scanning: 1
