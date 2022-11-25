1b404711422cf32a3d6b76511825f3f52a119fdb
Dzmitry Meshcharakou <12459192-dmeshcharakou@users.noreply.gitlab.com>
2022-11-25 16:46:25 +0100

    Add API::NugetGroupPackages endpoints to OpenApiV2 documentation

--- lib/api/api.rb
+++ lib/api/api.rb
@@ -232,6 +232,7 @@ module API
         mount ::API::Metrics::Dashboard::Annotations
         mount ::API::Metrics::UserStarredDashboards
         mount ::API::Namespaces
+        mount ::API::NugetGroupPackages
         mount ::API::PackageFiles
         mount ::API::Pages
         mount ::API::PersonalAccessTokens::SelfInformation
@@ -309,7 +310,6 @@ module API
       mount ::API::NotificationSettings
       mount ::API::NpmInstancePackages
       mount ::API::NpmProjectPackages
-      mount ::API::NugetGroupPackages
       mount ::API::NugetProjectPackages
       mount ::API::PagesDomains
       mount ::API::ProjectContainerRepositories
