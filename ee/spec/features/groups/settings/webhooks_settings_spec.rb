cdcd4613fe5f89783b4ded9227781154d64cc1bf
Zhiyuan Lu <1551755561@qq.com>
2022-11-28 13:45:20 +0000

    Rollout feature: webhook support regex
    featrue flag: enhanced_webhook_support_regex
    
    Changelog: added

--- ee/spec/features/groups/settings/webhooks_settings_spec.rb
+++ ee/spec/features/groups/settings/webhooks_settings_spec.rb
@@ -69,47 +69,21 @@ RSpec.describe 'Groups > Settings > Group Hooks' do
         expect(page).to have_content('Releases events')
       end
 
-      context 'when feature flag "enhanced_webhook_support_regex" is disabled' do
-        before do
-          stub_feature_flags(enhanced_webhook_support_regex: false)
-        end
+      it 'creates a group hook', :js do
+        visit webhooks_path
 
-        it 'creates a group hook', :js do
-          visit webhooks_path
+        fill_in 'URL', with: url
+        check 'Tag push events'
+        check 'Enable SSL verification'
+        check 'Job events'
 
-          fill_in 'URL', with: url
-          check 'Tag push events'
-          fill_in 'hook_push_events_branch_filter', with: 'master'
-          check 'Enable SSL verification'
-          check 'Job events'
+        expect { click_button 'Add webhook' }.to change(GroupHook, :count).by(1)
 
-          expect { click_button 'Add webhook' }.to change(GroupHook, :count).by(1)
-
-          expect(page).to have_content(url)
-          expect(page).to have_content('SSL Verification: enabled')
-          expect(page).to have_content('Push events')
-          expect(page).to have_content('Tag push events')
-          expect(page).to have_content('Job events')
-        end
-      end
-
-      context 'when feature flag "enhanced_webhook_support_regex" is enabled' do
-        it 'creates a group hook', :js do
-          visit webhooks_path
-
-          fill_in 'URL', with: url
-          check 'Tag push events'
-          check 'Enable SSL verification'
-          check 'Job events'
-
-          expect { click_button 'Add webhook' }.to change(GroupHook, :count).by(1)
-
-          expect(page).to have_content(url)
-          expect(page).to have_content('SSL Verification: enabled')
-          expect(page).to have_content('Tag push events')
-          expect(page).to have_content('Job events')
-          expect(page).to have_content('Push events')
-        end
+        expect(page).to have_content(url)
+        expect(page).to have_content('SSL Verification: enabled')
+        expect(page).to have_content('Tag push events')
+        expect(page).to have_content('Job events')
+        expect(page).to have_content('Push events')
       end
 
       it 'edits an existing group hook', :js do
