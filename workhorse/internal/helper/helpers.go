462ebf7eaf34925511c3909d99bbdc8442ec9db9
Jacob Vosmaer <jacob@gitlab.com>
2022-11-29 14:13:23 +0000

    Factor out workhorse command helpers

--- workhorse/internal/helper/helpers.go
+++ workhorse/internal/helper/helpers.go
@@ -9,15 +9,12 @@ import (
 	"net/http"
 	"net/url"
 	"os"
-	"os/exec"
 	"strings"
-	"syscall"
 
 	"github.com/sebest/xff"
 	"gitlab.com/gitlab-org/labkit/log"
 	"gitlab.com/gitlab-org/labkit/mask"
 )
-
 
 func logErrorWithFields(r *http.Request, err error, fields log.Fields) {
 	if err != nil {
@@ -118,35 +115,6 @@ func HeaderClone(h http.Header) http.Header {
 		h2[k] = vv2
 	}
 	return h2
-}
-
-func CleanUpProcessGroup(cmd *exec.Cmd) {
-	if cmd == nil {
-		return
-	}
-
-	process := cmd.Process
-	if process != nil && process.Pid > 0 {
-		// Send SIGTERM to the process group of cmd
-		syscall.Kill(-process.Pid, syscall.SIGTERM)
-	}
-
-	// reap our child process
-	cmd.Wait()
-}
-
-func ExitStatus(err error) (int, bool) {
-	exitError, ok := err.(*exec.ExitError)
-	if !ok {
-		return 0, false
-	}
-
-	waitStatus, ok := exitError.Sys().(syscall.WaitStatus)
-	if !ok {
-		return 0, false
-	}
-
-	return waitStatus.ExitStatus(), true
 }
 
 func FixRemoteAddr(r *http.Request) {
