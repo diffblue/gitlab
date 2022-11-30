03a9cd16ca9253d2c9876b86e6acb9fdba79501b
Careem Ahamed <cahamed@gitlab.com>
2022-11-30 17:42:42 +0800

    De-Quarantine 13_secure/enable_scanning_from_configuration_spec.rb

--- qa/qa/specs/features/ee/browser_ui/13_secure/enable_scanning_from_configuration_spec.rb
+++ qa/qa/specs/features/ee/browser_ui/13_secure/enable_scanning_from_configuration_spec.rb
@@ -108,11 +108,7 @@ module QA
 
         it(
           'runs sast job when enabled from configuration',
-          testcase: 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/347685',
-          quarantine: {
-            type: :stale,
-            issue: "https://gitlab.com/gitlab-org/gitlab/-/issues/381752"
-          }
+          testcase: 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/347685'
         ) do
           Page::Project::Menu.perform(&:click_on_security_configuration_link)
 
