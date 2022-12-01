de63b32890eaa0c5d5ffe5a93d9901c06d84230f
Allison Browne <abrowne@gitlab.com>
2022-12-01 11:54:29 -0500

    DRY allowlist spec with parameterized tables

--- spec/models/ci/job_token/allowlist_spec.rb
+++ spec/models/ci/job_token/allowlist_spec.rb
@@ -2,13 +2,12 @@
 
 require 'spec_helper'
 
-RSpec.describe Ci::JobToken::Allowlist do
+RSpec.describe Ci::JobToken::Allowlist, feature_category: :continuous_integration do
   using RSpec::Parameterized::TableSyntax
 
   let_it_be(:source_project) { create(:project) }
 
   let(:scope) { described_class.new(source_project, direction: direction) }
-
   let(:direction) { :outbound }
 
   describe '#projects' do
@@ -27,14 +26,14 @@ RSpec.describe Ci::JobToken::Allowlist do
     context 'when projects are added to the scope' do
       include_context 'with scoped projects'
 
-      where(:direction, :result) do
+      where(:direction, :additional_project) do
         :outbound | ref(:outbound_scoped_project)
         :inbound  | ref(:inbound_scoped_project)
       end
 
       with_them do
         it 'returns all projects that can be accessed from a given scope' do
-          expect(subject).to contain_exactly(source_project, result)
+          expect(all_projects).to contain_exactly(source_project, additional_project)
         end
       end
     end
@@ -43,29 +42,18 @@ RSpec.describe Ci::JobToken::Allowlist do
   describe '#includes?' do
     subject { scope.includes?(includes_project) }
 
-    context 'without projects' do
+    context 'without scoped projects' do
+      let(:unscoped_project) { build(:project) }
+
       context 'when project is self referential' do
-        where(:direction, :result) do
-          :outbound | true
-          :inbound  | true
+        where(:includes_project, :direction, :result) do
+          ref(:source_project)   | :outbound | true
+          ref(:source_project)   | :inbound  | true
+          ref(:unscoped_project) | :outbound | false
+          ref(:unscoped_project) | :inbound  | false
         end
 
         with_them do
-          let(:includes_project) { source_project }
-
-          it { is_expected.to be result }
-        end
-      end
-
-      context 'when project is not self referential' do
-        where(:direction, :result) do
-          :outbound | false
-          :inbound  | false
-        end
-
-        with_them do
-          let(:includes_project) { build(:project) }
-
           it { is_expected.to be result }
         end
       end
@@ -74,69 +62,21 @@ RSpec.describe Ci::JobToken::Allowlist do
     context 'with scoped projects' do
       include_context 'with scoped projects'
 
-      context 'when project is self referential' do
-        where(:direction, :result) do
-          :outbound | true
-          :inbound  | true
-        end
-
-        with_them do
-          let(:includes_project) { source_project }
-
-          it { is_expected.to be result }
-        end
+      where(:includes_project, :direction, :result) do
+        ref(:source_project)          | :outbound | true
+        ref(:source_project)          | :inbound  | true
+        ref(:inbound_scoped_project)  | :outbound | false
+        ref(:inbound_scoped_project)  | :inbound  | true
+        ref(:outbound_scoped_project) | :outbound | true
+        ref(:outbound_scoped_project) | :inbound  | false
+        ref(:unscoped_project1)       | :outbound | false
+        ref(:unscoped_project1)       | :inbound  | false
+        ref(:unscoped_project2)       | :outbound | false
+        ref(:unscoped_project2)       | :inbound  | false
       end
 
-      context 'when project is in inbound scope' do
-        where(:direction, :result) do
-          :outbound | false
-          :inbound  | true
-        end
-
-        with_them do
-          let(:includes_project) { inbound_scoped_project }
-
-          it { is_expected.to be result }
-        end
-      end
-
-      context 'when project is in outbound scope' do
-        where(:direction, :result) do
-          :outbound | true
-          :inbound  | false
-        end
-
-        with_them do
-          let(:includes_project) { outbound_scoped_project }
-
-          it { is_expected.to be result }
-        end
-      end
-
-      context 'when project is linked to a different project' do
-        where(:direction, :result) do
-          :outbound | false
-          :inbound  | false
-        end
-
-        with_them do
-          let(:includes_project) { unscoped_project1 }
-
-          it { is_expected.to be result }
-        end
-      end
-
-      context 'when project is unlinked to any project' do
-        where(:direction, :result) do
-          :outbound | false
-          :inbound  | false
-        end
-
-        with_them do
-          let(:includes_project) { unscoped_project2 }
-
-          it { is_expected.to be result }
-        end
+      with_them do
+        it { is_expected.to be result }
       end
     end
   end
