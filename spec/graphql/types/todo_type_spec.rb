97bc1191ab1dd195145bb31c9f20693cbe83aed3
Marco Zille <marco.zille@gmail.com>
2022-12-01 15:46:24 +0100

    Applied suggestions from review

--- spec/graphql/types/todo_type_spec.rb
+++ spec/graphql/types/todo_type_spec.rb
@@ -3,6 +3,11 @@
 require 'spec_helper'
 
 RSpec.describe GitlabSchema.types['Todo'] do
+  let_it_be(:current_user) { create(:user) }
+  let_it_be(:author) { create(:user) }
+
+  let(:issue) { create(:issue, project: project) }
+
   it 'has the correct fields' do
     expected_fields = [
       :id,
@@ -23,13 +28,9 @@ RSpec.describe GitlabSchema.types['Todo'] do
 
   specify { expect(described_class).to require_graphql_authorizations(:read_todo) }
 
-  describe 'project' do
-    let_it_be(:current_user) { create(:user) }
-    let_it_be(:author) { create(:user) }
+  subject { GitlabSchema.execute(query, context: { current_user: current_user }).as_json }
 
-    let_it_be(:project) { create(:project) }
-    let(:issue) { create(:issue, project: project) }
-
+  describe 'project field' do
     let(:todo) do
       create(:todo,
              user: current_user,
@@ -52,8 +53,6 @@ RSpec.describe GitlabSchema.types['Todo'] do
       )
     end
 
-    subject { GitlabSchema.execute(query, context: { current_user: current_user }).as_json }
-
     context 'when the project is public' do
       let_it_be(:project) { create(:project, :public) }
 
@@ -62,19 +61,11 @@ RSpec.describe GitlabSchema.types['Todo'] do
           expect(subject.dig('data', 'todo', 'project', 'id')).to eq(project.to_global_id.to_s)
         end
       end
-
-      context 'when the user does have access' do
-        before do
-          project.add_guest(current_user)
-        end
-
-        it 'returns the project' do
-          expect(subject.dig('data', 'todo', 'project', 'id')).to eq(project.to_global_id.to_s)
-        end
-      end
     end
 
     context 'when the project is not public' do
+      let_it_be(:project) { create(:project) }
+
       context 'when the user does not have access' do
         it 'returns null' do
           expect(subject.dig('data', 'todo', 'project')).to be_nil
@@ -93,14 +84,7 @@ RSpec.describe GitlabSchema.types['Todo'] do
     end
   end
 
-  describe 'group' do
-    let_it_be(:current_user) { create(:user) }
-    let_it_be(:author) { create(:user) }
-    let_it_be(:group) { create(:group) }
-    let_it_be(:project) { create(:project, group: group) }
-
-    let(:issue) { create(:issue, project: project) }
-
+  describe 'group field' do
     let(:todo) do
       create(:todo,
              user: current_user,
@@ -123,8 +107,6 @@ RSpec.describe GitlabSchema.types['Todo'] do
       )
     end
 
-    subject { GitlabSchema.execute(query, context: { current_user: current_user }).as_json }
-
     context 'when the group is public' do
       let_it_be(:group) { create(:group, :public) }
       let_it_be(:project) { create(:project, :public, group: group) }
@@ -134,19 +116,12 @@ RSpec.describe GitlabSchema.types['Todo'] do
           expect(subject.dig('data', 'todo', 'group', 'id')).to eq(group.to_global_id.to_s)
         end
       end
-
-      context 'when the user does have access' do
-        before do
-          group.add_guest(current_user)
-        end
-
-        it 'returns the group' do
-          expect(subject.dig('data', 'todo', 'group', 'id')).to eq(group.to_global_id.to_s)
-        end
-      end
     end
 
     context 'when the group is not public' do
+      let_it_be(:group) { create(:group) }
+      let_it_be(:project) { create(:project, group: group) }
+
       context 'when the user does not have access' do
         it 'returns null' do
           expect(subject.dig('data', 'todo', 'group')).to be_nil
