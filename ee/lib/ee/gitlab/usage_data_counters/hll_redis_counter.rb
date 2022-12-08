6158f0e91099b59bf1f3e5df05bfc30d0a912f1c
Niko Belokolodov <nbelokolodov@gitlab.com>
2022-12-08 18:59:47 +0000

    Remove adding Redis metrics from known_events
    This legacy method is removed now. All the existing metrics are migrated
    to instrumentation classes and added automatically
    
    Changelog: changed

--- ee/lib/ee/gitlab/usage_data_counters/hll_redis_counter.rb
+++ ee/lib/ee/gitlab/usage_data_counters/hll_redis_counter.rb
@@ -6,16 +6,6 @@ module EE
       module HLLRedisCounter
         extend ActiveSupport::Concern
         EE_KNOWN_EVENTS_PATH = File.expand_path('known_events/*.yml', __dir__)
-        EE_CATEGORIES_COLLECTED_FROM_METRICS_DEFINITIONS = %w[
-          ci_secrets_management
-          compliance
-          epic_boards_usage
-          epics_usage
-          incident_management_oncall
-          geo
-          growth
-          network_policies
-        ].freeze
 
         class_methods do
           extend ::Gitlab::Utils::Override
@@ -28,11 +18,6 @@ module EE
           override :known_events
           def known_events
             @known_events ||= (super + load_events(EE_KNOWN_EVENTS_PATH))
-          end
-
-          override :categories_collected_from_metrics_definitions
-          def categories_collected_from_metrics_definitions
-            (EE_CATEGORIES_COLLECTED_FROM_METRICS_DEFINITIONS + super).freeze
           end
         end
       end
