e3dec06bf783bd5d32f0423afdcd0ef1b4e682d0
Robert Hunt <rhunt@gitlab.com>
2022-12-12 13:08:06 +0000

    Add steps to check whether onboarding is finished
    It checks to see if the Jitsu key exists, if it doesn't then we're still
    in onboarding.
    
    Then it tries to query Cube, if cube fails or the returned count is 0
    then we're still in onboarding.
    
    If cube returns a count greater than 0 then we have data and we're onto
    listing dashboards!
    
    Signed-off-by: Robert Hunt <rhunt@gitlab.com>

---
+++ ee/spec/features/projects/product_analytics/dashboards_spec.rb
@@ -0,0 +1,163 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe 'Product Analytics Dashboard', :js, feature_category: :product_analytics do
+  let_it_be(:current_user) { create(:user) }
+  let_it_be(:user) { current_user }
+  let_it_be(:project) { create(:project, :repository) }
+  let_it_be(:query_response_with_unknown_error) { fixture_file('cube_js/query_with_error.json', dir: 'ee') }
+  let_it_be(:query_response_with_no_db_error) { fixture_file('cube_js/query_with_no_db_error.json', dir: 'ee') }
+  let_it_be(:query_response_with_data) { fixture_file('cube_js/query_with_data.json', dir: 'ee') }
+
+  let(:cube_api_url) { "https://cube.example.com/cubejs-api/v1/load" }
+
+  before do
+    stub_feature_flags(cube_api_proxy: true)
+    project.add_owner(user)
+    sign_in(user)
+  end
+
+  subject(:visit_page) { visit project_product_analytics_dashboards_path(project) }
+
+  shared_examples 'renders product analytics 404' do
+    before do
+      visit_page
+    end
+
+    it do
+      expect(page).to have_content(s_('404|Page Not Found'))
+    end
+  end
+
+  shared_examples 'renders the onboarding view' do
+    before do
+      visit_page
+    end
+
+    it do
+      expect(page).to have_content(s_('Product Analytics|Onboarding view'))
+    end
+  end
+
+  context 'without the required application settings' do
+    it_behaves_like 'renders product analytics 404'
+  end
+
+  context 'with the required application settings' do
+    before do
+      stub_application_setting(product_analytics_enabled?: true)
+      stub_application_setting(jitsu_host: 'https://jitsu.example.com')
+      stub_application_setting(jitsu_project_xid: '123')
+      stub_application_setting(jitsu_administrator_email: 'test@example.com')
+      stub_application_setting(jitsu_administrator_password: 'password')
+      stub_application_setting(clickhouse_connection_string: 'clickhouse://localhost:9000')
+      stub_application_setting(cube_api_base_url: 'https://cube.example.com')
+      stub_application_setting(cube_api_key: '123')
+    end
+
+    context 'with the feature flag disabled' do
+      before do
+        stub_feature_flags(product_analytics_internal_preview: false)
+      end
+
+      it_behaves_like 'renders product analytics 404'
+    end
+
+    context 'with the feature flag enabled' do
+      before do
+        stub_feature_flags(product_analytics_internal_preview: true)
+      end
+
+      context 'with the licensed feature disabled' do
+        before do
+          stub_licensed_features(product_analytics: false)
+        end
+
+        it_behaves_like 'renders product analytics 404'
+      end
+
+      context 'with the licensed feature enabled' do
+        before do
+          stub_licensed_features(product_analytics: true)
+        end
+
+        context 'without the Jitsu key' do
+          it_behaves_like 'renders the onboarding view'
+        end
+
+        context 'with the Jitsu key' do
+          before do
+            project.project_setting.update!(jitsu_key: '123')
+            project.reload
+          end
+
+          context 'when the cube API returns an unhandled error' do
+            before do
+              stub_cube_proxy_error
+              visit_page
+            end
+
+            it 'renders the error alert' do
+              error_msg = s_('ProductAnalytics|An error occurred while fetching data. Refresh the page to try again.')
+
+              expect(find('[data-testid="alert-danger"]')).to have_text(error_msg)
+            end
+          end
+
+          context 'when the clickhouse database does not exist' do
+            before do
+              stub_cube_no_database_error
+            end
+
+            it_behaves_like 'renders the onboarding view'
+          end
+
+          context 'when the cube API returns zero data' do
+            before do
+              stub_cube_proxy_zero_count
+            end
+
+            it_behaves_like 'renders the onboarding view'
+          end
+
+          context 'when the cube API returns data' do
+            before do
+              stub_cube_proxy_success
+              visit_page
+            end
+
+            it 'renders the dashboards view' do
+              expect(page).to have_content('All insights in one quick glance')
+            end
+          end
+        end
+      end
+    end
+  end
+
+  private
+
+  def stub_cube_proxy_error
+    stub_request(:post, cube_api_url)
+      .to_return(status: 200, body: query_response_with_unknown_error, headers: {})
+  end
+
+  def stub_cube_no_database_error
+    stub_request(:post, cube_api_url)
+      .to_return(status: 404, body: query_response_with_no_db_error, headers: {})
+  end
+
+  def stub_cube_proxy_zero_count
+    query_object = Gitlab::Json.parse(query_response_with_data)
+    query_object['results'][0]['data'][0]['Jitsu.count'] = 0
+
+    stub_request(:post, cube_api_url)
+      .to_return(status: 200, body: query_object.to_json, headers: {})
+  end
+
+  def stub_cube_proxy_success
+    stub_request(:post, cube_api_url)
+      .to_return(status: 200, body: query_response_with_data, headers: {})
+  end
+end
--- ee/spec/frontend/product_analytics/product_analytics_app_spec.js
+++ ee/spec/frontend/product_analytics/product_analytics_app_spec.js
@@ -5,6 +5,14 @@ import OnboardingView from 'ee/product_analytics/onboarding/onboarding_view.vue'
 import DashboardsView from 'ee/product_analytics/dashboards/dashboards_view.vue';
 import createRouter from 'ee/product_analytics/router';
 import waitForPromises from 'helpers/wait_for_promises';
+import cubeAnalytics from 'ee/product_analytics/dashboards/data_sources/cube_analytics';
+import { createAlert } from '~/flash';
+import { s__ } from '~/locale';
+
+jest.mock('ee/product_analytics/dashboards/data_sources/cube_analytics', () => ({
+  hasAnalyticsData: jest.fn(),
+}));
+jest.mock('~/flash');
 
 describe('ProductAnalyticsApp', () => {
   let wrapper;
@@ -13,38 +21,73 @@ describe('ProductAnalyticsApp', () => {
   const findOnboardingView = () => wrapper.findComponent(OnboardingView);
   const findDashboardsView = () => wrapper.findComponent(DashboardsView);
 
-  const createWrapper = (data = {}) => {
+  const createWrapper = (provided = {}) => {
     wrapper = shallowMount(AnalyticsApp, {
       router: createRouter(),
       provide: {
         jitsuKey: '123',
-      },
-      data() {
-        return {
-          isLoading: false,
-          isOnboarding: false,
-          ...data,
-        };
+        projectId: '1',
+        ...provided,
       },
     });
   };
 
+  afterEach(() => {
+    createAlert.mockClear();
+  });
+
   describe('when mounted', () => {
     it('shows the loading icon if loading', () => {
-      createWrapper({ isLoading: true });
+      createWrapper();
 
       expect(findLoadingIcon().exists()).toBe(true);
     });
 
-    it('should show the onboarding app if onboarding is ongoing', async () => {
-      createWrapper({ isOnboarding: true });
+    it('should show the onboarding app if there is no jitsuKey', async () => {
+      createWrapper({ jitsuKey: null });
 
       await waitForPromises();
 
       expect(findOnboardingView().exists()).toBe(true);
     });
 
-    it('should show the dashboards app if onboarding is complete', async () => {
+    // Cube.js passes errors using a custom error type
+    // In these tests we're mocking the output of that type
+    // https://github.com/cube-js/cube.js/blob/master/packages/cubejs-client-core/src/RequestError.js
+    it('should show the alert if there is an unhandled error', async () => {
+      const error = { response: { message: 'unknown error' } };
+
+      jest.spyOn(cubeAnalytics, 'hasAnalyticsData').mockRejectedValue(error);
+
+      createWrapper();
+
+      await waitForPromises();
+
+      expect(createAlert).toHaveBeenCalledWith({
+        message: s__(
+          'ProductAnalytics|An error occurred while fetching data. Refresh the page to try again.',
+        ),
+        captureError: true,
+        error,
+      });
+      expect(findLoadingIcon().exists()).toBe(false);
+      expect(findOnboardingView().exists()).toBe(false);
+      expect(findDashboardsView().exists()).toBe(false);
+    });
+
+    it('should show the onboarding app if there is no analytics data', async () => {
+      jest.spyOn(cubeAnalytics, 'hasAnalyticsData').mockReturnValue(false);
+
+      createWrapper();
+
+      await waitForPromises();
+
+      expect(findOnboardingView().exists()).toBe(true);
+    });
+
+    it('should show the dashboards app if there is analytics data', async () => {
+      jest.spyOn(cubeAnalytics, 'hasAnalyticsData').mockReturnValue(true);
+
       createWrapper();
 
       await waitForPromises();
