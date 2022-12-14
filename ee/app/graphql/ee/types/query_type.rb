3c7a322758fa6faf81c7028f8421dd87f7d73e25
Alexandru Croitor <acroitor@gitlab.com>
2022-12-14 19:33:03 +0200

    Sort root queries in QueryType alphabetically
    This should make it easier to maintain the list of root
    queries as well as the related spec.

--- ee/app/graphql/ee/types/query_type.rb
+++ ee/app/graphql/ee/types/query_type.rb
@@ -5,80 +5,69 @@ module EE
     module QueryType
       extend ActiveSupport::Concern
       prepended do
-        field :iteration, ::Types::IterationType,
+        field :ci_minutes_usage, ::Types::Ci::Minutes::NamespaceMonthlyUsageType.connection_type,
+               null: true,
+               description: 'CI/CD minutes usage data for a namespace.' do
+                 argument :namespace_id, ::Types::GlobalIDType[::Namespace],
+                   required: false,
+                   description: 'Global ID of the Namespace for the monthly CI/CD minutes usage.'
+               end
+        field :current_license, ::Types::Admin::CloudLicenses::CurrentLicenseType,
               null: true,
-              description: 'Find an iteration.' do
-          argument :id, ::Types::GlobalIDType[::Iteration],
-                   required: true,
-                   description: 'Find an iteration by its ID.'
-        end
-
-        field :vulnerabilities,
-              ::Types::VulnerabilityType.connection_type,
-              null: true,
-              extras: [:lookahead],
-              description: "Vulnerabilities reported on projects on the current user's instance security dashboard.",
-              resolver: ::Resolvers::VulnerabilitiesResolver
-
-        field :vulnerability,
-              ::Types::VulnerabilityType,
-              null: true,
-              description: "Find a vulnerability." do
-          argument :id, ::Types::GlobalIDType[::Vulnerability],
-                   required: true,
-                   description: 'Global ID of the Vulnerability.'
-        end
-
-        field :vulnerabilities_count_by_day,
-              ::Types::VulnerabilitiesCountByDayType.connection_type,
-              null: true,
-              resolver: ::Resolvers::VulnerabilitiesCountPerDayResolver,
-              description: 'The historical number of vulnerabilities per day for the projects on the current ' \
-                           'user\'s instance security dashboard.'
-
-        field :geo_node, ::Types::Geo::GeoNodeType,
-              null: true,
-              resolver: ::Resolvers::Geo::GeoNodeResolver,
-              description: 'Find a Geo node.'
-
-        field :instance_security_dashboard, ::Types::InstanceSecurityDashboardType,
-              null: true,
-              resolver: ::Resolvers::InstanceSecurityDashboardResolver,
-              description: 'Fields related to Instance Security Dashboard.'
-
+              resolver: ::Resolvers::Admin::CloudLicenses::CurrentLicenseResolver,
+              description: 'Fields related to the current license.'
         field :devops_adoption_enabled_namespaces,
               ::Types::Analytics::DevopsAdoption::EnabledNamespaceType.connection_type,
               null: true,
               description: 'Get configured DevOps adoption namespaces. **BETA** This endpoint is subject to change ' \
                            'without notice.',
               resolver: ::Resolvers::Analytics::DevopsAdoption::EnabledNamespacesResolver
-
-        field :current_license, ::Types::Admin::CloudLicenses::CurrentLicenseType,
+        field :epic_board_list, ::Types::Boards::EpicListType,
               null: true,
-              resolver: ::Resolvers::Admin::CloudLicenses::CurrentLicenseResolver,
-              description: 'Fields related to the current license.'
-
+              resolver: ::Resolvers::Boards::EpicListResolver
+        field :geo_node, ::Types::Geo::GeoNodeType,
+              null: true,
+              resolver: ::Resolvers::Geo::GeoNodeResolver,
+              description: 'Find a Geo node.'
+        field :iteration, ::Types::IterationType,
+              null: true,
+              description: 'Find an iteration.' do
+                argument :id, ::Types::GlobalIDType[::Iteration],
+                  required: true,
+                  description: 'Find an iteration by its ID.'
+              end
+        field :instance_security_dashboard, ::Types::InstanceSecurityDashboardType,
+              null: true,
+              resolver: ::Resolvers::InstanceSecurityDashboardResolver,
+              description: 'Fields related to Instance Security Dashboard.'
         field :license_history_entries, ::Types::Admin::CloudLicenses::LicenseHistoryEntryType.connection_type,
               null: true,
               resolver: ::Resolvers::Admin::CloudLicenses::LicenseHistoryEntriesResolver,
               description: 'Fields related to entries in the license history.'
-
         field :subscription_future_entries, ::Types::Admin::CloudLicenses::SubscriptionFutureEntryType.connection_type,
               null: true,
               resolver: ::Resolvers::Admin::CloudLicenses::SubscriptionFutureEntriesResolver,
               description: 'Fields related to entries in future subscriptions.'
-
-        field :ci_minutes_usage, ::Types::Ci::Minutes::NamespaceMonthlyUsageType.connection_type,
+        field :vulnerabilities,
+              ::Types::VulnerabilityType.connection_type,
               null: true,
-              description: 'CI/CD minutes usage data for a namespace.' do
-                argument :namespace_id, ::Types::GlobalIDType[::Namespace],
-                  required: false,
-                  description: 'Global ID of the Namespace for the monthly CI/CD minutes usage.'
+              extras: [:lookahead],
+              description: "Vulnerabilities reported on projects on the current user's instance security dashboard.",
+              resolver: ::Resolvers::VulnerabilitiesResolver
+        field :vulnerabilities_count_by_day,
+              ::Types::VulnerabilitiesCountByDayType.connection_type,
+              null: true,
+              resolver: ::Resolvers::VulnerabilitiesCountPerDayResolver,
+              description: 'The historical number of vulnerabilities per day for the projects on the current ' \
+                           'user\'s instance security dashboard.'
+        field :vulnerability,
+              ::Types::VulnerabilityType,
+              null: true,
+              description: "Find a vulnerability." do
+                argument :id, ::Types::GlobalIDType[::Vulnerability],
+                  required: true,
+                  description: 'Global ID of the Vulnerability.'
               end
-
-        field :epic_board_list, ::Types::Boards::EpicListType,
-               null: true,
-               resolver: ::Resolvers::Boards::EpicListResolver
       end
 
       def vulnerability(id:)
