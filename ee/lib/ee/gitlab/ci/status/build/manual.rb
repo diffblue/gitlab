25d6315cb0edc922b0df8a89a21525d1bb420bcc
Halil Coban <hcoban@gitlab.com>
2022-12-19 14:49:59 +0100

    Move outdated deployment warning to FOSS
    Changelog: changed

--- ee/lib/ee/gitlab/ci/status/build/manual.rb
+++ ee/lib/ee/gitlab/ci/status/build/manual.rb
@@ -15,8 +15,6 @@ module EE
             def generic_permission_failure_message
               if subject.persisted_environment.try(:protected_from?, user)
                 _("This deployment job does not run automatically and must be started manually, but you do not have access to this job's protected environment. The job can only be started by a project member allowed to deploy to the environment.")
-              elsif subject.outdated_deployment?
-                _("This deployment job does not run automatically and must be started manually, but it's older than the latest deployment, and therefore can't run.")
               else
                 super
               end
