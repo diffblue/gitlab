682e70bbb1c0a15b3e78c0452a2e0a618b78194c
Lee Tickett <ltickett@gitlab.com>
2022-12-23 12:58:09 +0000

    Add noteable commenters GraphQL field
    Changelog: added

--- ee/spec/graphql/types/epic_type_spec.rb
+++ ee/spec/graphql/types/epic_type_spec.rb
@@ -19,6 +19,7 @@ RSpec.describe GitlabSchema.types['Epic'] do
       user_notes_count user_discussions_count health_status current_user_todos
       award_emoji events ancestors color text_color blocked blocking_count
       blocked_by_count blocked_by_epics default_project_for_issue_creation
+      commenters
     ]
   end
 
