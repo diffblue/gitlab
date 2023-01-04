d40b4300f2eb78f97a75127e245aee52023ed65c
Valery Sizov <valery@gitlab.com>
2023-01-04 21:01:23 +0000

    Remove geo_dependency_proxy_manifest_replication flag
    FF removal

--- ee/app/graphql/types/geo/geo_node_type.rb
+++ ee/app/graphql/types/geo/geo_node_type.rb
@@ -25,9 +25,7 @@ module Types
       field :dependency_proxy_manifest_registries, ::Types::Geo::DependencyProxyManifestRegistryType.connection_type,
             null: true,
             resolver: ::Resolvers::Geo::DependencyProxyManifestRegistriesResolver,
-            description: 'Find Dependency Proxy Manifest registries on this Geo node. '\
-                         'Ignored if `geo_dependency_proxy_manifest_replication` feature flag is disabled.',
-            alpha: { milestone: '15.6' }
+            description: 'Find Dependency Proxy Manifest registries on this Geo node.'
       field :enabled, GraphQL::Types::Boolean, null: true, description: 'Indicates whether this Geo node is enabled.'
       field :files_max_capacity, GraphQL::Types::Int, null: true, description: 'Maximum concurrency of LFS/attachment backfill for this secondary node.'
       field :group_wiki_repository_registries, ::Types::Geo::GroupWikiRepositoryRegistryType.connection_type,
