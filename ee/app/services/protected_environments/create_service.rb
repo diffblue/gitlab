d6fe3b2e6f9be3faa91b0403c4c79a1ea2b4d189
Ahmed Hemdan <ahemdan@gitlab.com>
2023-01-04 21:31:35 +0100

    Add audit events for protected environments
    Adds two new audit events `environment_protected` and
    `environment_unprotected` for when an environment is protected or
    unprotected. Applies whether this is done via the API or UI, and works
    for group-level and project-level protected environments alike.
    
    Changelog: changed
    EE: true

--- ee/app/services/protected_environments/create_service.rb
+++ ee/app/services/protected_environments/create_service.rb
@@ -2,7 +2,29 @@
 module ProtectedEnvironments
   class CreateService < ProtectedEnvironments::BaseService
     def execute
-      container.protected_environments.create(sanitized_params)
+      container.protected_environments.create(sanitized_params).tap do |protected_environment|
+        log_audit_event(protected_environment) if protected_environment.persisted?
+      end
+    end
+
+    private
+
+    def log_audit_event(protected_environment)
+      message = if group_container?
+                  "Protected environments of #{protected_environment.name} tier"
+                else
+                  "Protected an environment: #{protected_environment.name}"
+                end
+
+      audit_context = {
+        name: 'environment_protected',
+        author: current_user,
+        scope: container,
+        target: protected_environment,
+        message: message
+      }
+
+      ::Gitlab::Audit::Auditor.audit(audit_context)
     end
   end
 end
--- ee/spec/services/protected_environments/destroy_service_spec.rb
+++ ee/spec/services/protected_environments/destroy_service_spec.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 require 'spec_helper'
 
-RSpec.describe ProtectedEnvironments::DestroyService, '#execute' do
-  let(:project) { create(:project) }
-  let(:user) { create(:user) }
+RSpec.describe ProtectedEnvironments::DestroyService, '#execute', feature_category: :environment_management do
+  let_it_be(:project) { create(:project) }
+  let_it_be(:user) { create(:user) }
+
   let!(:protected_environment) { create(:protected_environment, project: project) }
   let(:deploy_access_level) { protected_environment.deploy_access_levels.first }
 
@@ -23,13 +24,36 @@ RSpec.describe ProtectedEnvironments::DestroyService, '#execute' do
         subject
       end.to change { ProtectedEnvironments::DeployAccessLevel.count }.from(1).to(0)
     end
+
+    it 'stores and logs the audit event' do
+      audit_context = {
+        name: 'environment_unprotected',
+        author: user,
+        scope: project,
+        target: protected_environment,
+        message: "Unprotected an environment: #{protected_environment.name}"
+      }
+
+      expect(::Gitlab::Audit::Auditor).to receive(:audit).with(audit_context)
+
+      subject
+    end
   end
 
   context 'when the Protected Environment can not be deleted' do
+    let(:protected_environment_double) { instance_double(ProtectedEnvironment) }
+
     before do
-      allow(protected_environment).to receive(:destroy).and_return(false)
+      allow(protected_environment_double).to receive(:destroy).and_return(protected_environment)
+      allow(protected_environment).to receive(:tap).and_return(false)
     end
 
     it { is_expected.to be_falsy }
+
+    it 'does not store or log the audit event' do
+      expect(::Gitlab::Audit::Auditor).not_to receive(:audit)
+
+      subject
+    end
   end
 end
