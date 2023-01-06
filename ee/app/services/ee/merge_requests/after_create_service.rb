992f9f777da1bf69e1950d294f620ad6e61633a6
Tan Le <tle@gitlab.com>
2023-01-06 16:44:47 +1100

    Fix suggested reviewers job not run on branch MR
    This fixes a regression issue where suggested reviewers job not running
    on merge request created from a branch. The `modified_paths` check for
    `FetchSuggestedReviewersWorker` depends on `merge_request_diff` being
    persisted as part of `NewMergeRequestWorker`. However, in a recent
    refactor, `NewMergeRequestWorker` is scheduled to run after the check is
    invoked and hence causes this bug.

--- ee/app/services/ee/merge_requests/after_create_service.rb
+++ ee/app/services/ee/merge_requests/after_create_service.rb
@@ -10,12 +10,20 @@ module EE
         super
 
         schedule_sync_for(merge_request.head_pipeline_id)
+        schedule_fetch_suggested_reviewers(merge_request)
       end
 
       private
 
       def schedule_sync_for(pipeline_id)
         ::Ci::SyncReportsToReportApprovalRulesWorker.perform_async(pipeline_id) if pipeline_id
+      end
+
+      def schedule_fetch_suggested_reviewers(merge_request)
+        return unless merge_request.project.can_suggest_reviewers?
+        return unless merge_request.can_suggest_reviewers?
+
+        ::MergeRequests::FetchSuggestedReviewersWorker.perform_async(merge_request.id)
       end
     end
   end
--- ee/spec/services/ee/merge_requests/after_create_service_spec.rb
+++ ee/spec/services/ee/merge_requests/after_create_service_spec.rb
@@ -2,10 +2,11 @@
 
 require 'spec_helper'
 
-RSpec.describe MergeRequests::AfterCreateService do
+RSpec.describe MergeRequests::AfterCreateService, feature_category: :code_review do
   let_it_be(:merge_request) { create(:merge_request) }
+  let_it_be(:project) { merge_request.target_project }
 
-  let(:service_object) { described_class.new(project: merge_request.target_project, current_user: merge_request.author) }
+  let(:service_object) { described_class.new(project: project, current_user: merge_request.author) }
 
   describe '#execute' do
     subject(:execute) { service_object.execute(merge_request) }
@@ -35,6 +36,64 @@ RSpec.describe MergeRequests::AfterCreateService do
         execute
 
         expect(Ci::SyncReportsToReportApprovalRulesWorker).not_to have_received(:perform_async)
+      end
+    end
+
+    describe 'suggested reviewers' do
+      before do
+        allow(MergeRequests::FetchSuggestedReviewersWorker).to receive(:perform_async)
+        allow(merge_request).to receive(:ensure_merge_request_diff)
+      end
+
+      context 'when suggested reviewers is available for project' do
+        before do
+          allow(project).to receive(:can_suggest_reviewers?).and_return(true)
+        end
+
+        context 'when merge request can suggest reviewers' do
+          before do
+            allow(merge_request).to receive(:can_suggest_reviewers?).and_return(true)
+          end
+
+          it 'calls fetch worker for the merge request' do
+            execute
+
+            expect(merge_request).to have_received(:ensure_merge_request_diff).ordered
+            expect(MergeRequests::FetchSuggestedReviewersWorker).to have_received(:perform_async)
+              .with(merge_request.id)
+              .ordered
+          end
+        end
+
+        context 'when merge request cannot suggest reviewers' do
+          before do
+            allow(merge_request).to receive(:can_suggest_reviewers?).and_return(false)
+          end
+
+          it 'does not call fetch worker for the merge request' do
+            execute
+
+            expect(MergeRequests::FetchSuggestedReviewersWorker).not_to have_received(:perform_async)
+          end
+        end
+      end
+
+      context 'when suggested reviewers is not available for project' do
+        before do
+          allow(project).to receive(:can_suggest_reviewers?).and_return(false)
+        end
+
+        context 'when merge request can suggest reviewers' do
+          before do
+            allow(merge_request).to receive(:can_suggest_reviewers?).and_return(true)
+          end
+
+          it 'does not call fetch worker for the merge request' do
+            execute
+
+            expect(MergeRequests::FetchSuggestedReviewersWorker).not_to have_received(:perform_async)
+          end
+        end
       end
     end
   end
