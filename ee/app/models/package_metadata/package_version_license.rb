754f9def18bca3c5a01131ed037c9e887d31a43d
Igor Frenkel <ifrenkel@gitlab.com>
2023-01-10 23:51:24 +0000

    Add partitioning for package metadata
    Add partitioning for package metadata tables prefixed by pm_ and schema
    alias gitlab_pm which will allow this large data set to be more
    performant for client queries.
    
    Changelog: added

--- ee/app/models/package_metadata/package_version_license.rb
+++ ee/app/models/package_metadata/package_version_license.rb
@@ -2,6 +2,8 @@
 
 module PackageMetadata
   class PackageVersionLicense < ApplicationRecord
+    self.primary_key = :purl_type
+
     belongs_to :package_version,
         foreign_key: :pm_package_version_id,
         inverse_of: :package_version_licenses,
@@ -11,5 +13,9 @@ module PackageMetadata
         foreign_key: :pm_license_id,
         inverse_of: :package_version_licenses,
         optional: false
+
+    enum purl_type: ::Enums::Sbom.purl_types
+
+    validates :purl_type, presence: true
   end
 end
