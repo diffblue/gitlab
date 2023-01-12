2777ccc2c10426ccaed004d1a7a7d20070e7b852
Robert Hunt <rhunt@gitlab.com>
2023-01-12 15:09:28 +0000

    Update router to retrieve base from the backend
    Updated the dashboards HAML to use Rails paths to generate the base
    
    Signed-off-by: Robert Hunt <rhunt@gitlab.com>

--- ee/app/assets/javascripts/product_analytics/router.js
+++ ee/app/assets/javascripts/product_analytics/router.js
@@ -3,12 +3,10 @@ import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
-export default () => {
+export default (base) => {
   return new VueRouter({
     mode: 'history',
-    base: `${window.location.pathname
-      .split('/-/product_analytics/dashboards')[0]
-      .replace(/\/$/, '')}/-/product_analytics/dashboards`,
+    base,
     routes: [],
   });
 };
