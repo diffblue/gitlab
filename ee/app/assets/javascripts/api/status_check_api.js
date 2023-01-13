7ad21ef02eb14ec95809d4b464b124c6f003088a
Elwyn Benson <ebenson@gitlab.com>
2023-01-13 14:43:07 +1300

    Fix issue where URL was referenced on incorrect object
    Retry button can now correctly build the API url and make
    the network request.

--- ee/app/assets/javascripts/api/status_check_api.js
+++ ee/app/assets/javascripts/api/status_check_api.js
@@ -1,8 +1,8 @@
-import Api from '~/api';
+import Api from 'ee/api';
 import axios from '~/lib/utils/axios_utils';
 
 export function mrStatusCheckRetry({ projectId, mergeRequestId, externalStatusCheckId }) {
-  const url = Api.buildUrl(this.mrStatusCheckRetryPath)
+  const url = Api.buildUrl(Api.mrStatusCheckRetryPath)
     .replace(':id', encodeURIComponent(projectId))
     .replace(':merge_request_iid', encodeURIComponent(mergeRequestId))
     .replace(':external_status_check_id', encodeURIComponent(externalStatusCheckId));
