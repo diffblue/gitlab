c0b1fcd6db625a2d47f586e598d0f614f56239ab
David Pisek <dpisek@gitlab.com>
2023-01-16 09:55:59 +0000

    Add generic security report to GraphQL finding modal
    - Adds a GraphQL version of the generic report
    - Renders it on the findings modal
    - Fetches "url" report types

---
+++ ee/spec/frontend/vulnerabilities/generic_report/report_item_graphql_spec.js
@@ -0,0 +1,52 @@
+import { shallowMount } from '@vue/test-utils';
+import ReportItem from 'ee/vulnerabilities/components/generic_report/report_item_graphql.vue';
+import {
+  GRAPHQL_TYPENAME_URL,
+  GRAPHQL_TYPENAMES,
+} from 'ee/vulnerabilities/components/generic_report/types/constants';
+import { vulnerabilityDetailUrl } from 'ee_jest/security_dashboard/components/pipeline/mock_data';
+import { extendedWrapper } from 'helpers/vue_test_utils_helper';
+
+const TEST_DATA = {
+  [GRAPHQL_TYPENAME_URL]: vulnerabilityDetailUrl,
+};
+
+describe('ee/vulnerabilities/components/generic_report/report_item_graphql.vue', () => {
+  let wrapper;
+
+  const createWrapper = ({ props } = {}) =>
+    extendedWrapper(
+      shallowMount(ReportItem, {
+        propsData: {
+          item: {},
+          ...props,
+        },
+        // manual stubbing is needed because the components are dynamically imported
+        stubs: GRAPHQL_TYPENAMES,
+      }),
+    );
+
+  const findReportComponent = () => wrapper.findByTestId('reportComponent');
+
+  afterEach(() => {
+    wrapper.destroy();
+  });
+
+  describe.each(GRAPHQL_TYPENAMES)('with report type "%s"', (reportType) => {
+    const reportItem = { type: reportType, ...TEST_DATA[reportType] };
+
+    beforeEach(() => {
+      wrapper = createWrapper({ props: { item: reportItem } });
+    });
+
+    it('renders the corresponding component', () => {
+      expect(findReportComponent().exists()).toBe(true);
+    });
+
+    it('passes the report data as props', () => {
+      expect(findReportComponent().props()).toMatchObject({
+        item: reportItem,
+      });
+    });
+  });
+});
