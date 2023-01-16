e481c7d2454abb3eca5357da74226b85947f09a1
Eduardo Sanz García <esanz-garcia@gitlab.com>
2023-01-16 18:05:04 +0100

    Rename `js-flash-alert` to `js-error-alert`
    We are trying to reserve the word flash to alerts created by the Rails
    framework.

--- ee/app/assets/javascripts/pages/trials/apply/index.js
+++ ee/app/assets/javascripts/pages/trials/apply/index.js
@@ -1,6 +1,6 @@
 import 'ee/trials/track_trial_user_errors';
-import { initFlashAlert } from 'ee/trials/init_flash_alert';
+import { initErrorAlert } from 'ee/trials/init_error_alert';
 import { initNamespaceSelector } from 'ee/trials/init_namespace_selector';
 
-initFlashAlert();
+initErrorAlert();
 initNamespaceSelector();
--- ee/app/assets/javascripts/pages/trials/create_lead/index.js
+++ ee/app/assets/javascripts/pages/trials/create_lead/index.js
@@ -1,6 +1,6 @@
 import 'ee/trials/track_trial_user_errors';
 import { initTrialCreateLeadForm } from 'ee/trials/init_create_lead_form';
-import { initFlashAlert } from 'ee/trials/init_flash_alert';
+import { initErrorAlert } from 'ee/trials/init_error_alert';
 
 initTrialCreateLeadForm();
-initFlashAlert();
+initErrorAlert();
