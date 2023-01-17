dce751862c6199013f4a5ec31f1c0dc1e1af7aeb
Dave Pisek <dpisek@gitlab.com>
2023-01-17 12:46:36 +0100

    Add support for code to graphql generic security report
    * Adds to graphql query to fetch code type
    * Adds to component map and specs

--- ee/spec/frontend/vulnerabilities/generic_report/report_item_graphql_spec.js
+++ ee/spec/frontend/vulnerabilities/generic_report/report_item_graphql_spec.js
@@ -2,11 +2,13 @@ import { shallowMount } from '@vue/test-utils';
 import ReportItem from 'ee/vulnerabilities/components/generic_report/report_item_graphql.vue';
 import {
   GRAPHQL_TYPENAME_DIFF,
+  GRAPHQL_TYPENAME_CODE,
   GRAPHQL_TYPENAME_URL,
   GRAPHQL_TYPENAMES,
 } from 'ee/vulnerabilities/components/generic_report/types/constants';
 import {
   vulnerabilityDetailDiff,
+  vulnerabilityDetailCode,
   vulnerabilityDetailUrl,
 } from 'ee_jest/security_dashboard/components/pipeline/mock_data';
 import { extendedWrapper } from 'helpers/vue_test_utils_helper';
@@ -14,6 +16,7 @@ import { extendedWrapper } from 'helpers/vue_test_utils_helper';
 const TEST_DATA = {
   [GRAPHQL_TYPENAME_URL]: vulnerabilityDetailUrl,
   [GRAPHQL_TYPENAME_DIFF]: vulnerabilityDetailDiff,
+  [GRAPHQL_TYPENAME_CODE]: vulnerabilityDetailCode,
 };
 
 describe('ee/vulnerabilities/components/generic_report/report_item_graphql.vue', () => {
