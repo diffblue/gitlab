75e951aeecee3abb6e34be765046554967e32ec7
Madelein van Niekerk <mvanniekerk@gitlab.com>
2023-01-17 19:28:52 +0000

    Adds namespace filtering for group and project searches
    Changelog: changed
    EE: true

--- ee/spec/lib/gitlab/elastic/group_search_results_spec.rb
+++ ee/spec/lib/gitlab/elastic/group_search_results_spec.rb
@@ -56,6 +56,53 @@ RSpec.describe Gitlab::Elastic::GroupSearchResults, :elastic do
     it_behaves_like 'search results filtered by language'
   end
 
+  describe 'users' do
+    let(:query) { 'john' }
+    let(:scope) { 'users' }
+    let(:results) { described_class.new(user, query, group: group) }
+
+    it 'returns an empty list' do
+      create_list(:user, 3, name: "Sarah John")
+
+      ensure_elasticsearch_index!
+
+      users = results.objects('users')
+
+      expect(users).to eq([])
+      expect(results.users_count).to eq 0
+    end
+
+    context 'with group members' do
+      let_it_be(:parent_group) { create(:group) }
+      let_it_be(:group) { create(:group, parent: parent_group) }
+      let_it_be(:child_group) { create(:group, parent: group) }
+      let_it_be(:child_of_parent_group) { create(:group, parent: parent_group) }
+      let_it_be(:project_in_group) { create(:project, namespace: group) }
+      let_it_be(:project_in_child_group) { create(:project, namespace: child_group) }
+      let_it_be(:project_in_parent_group) { create(:project, namespace: parent_group) }
+      let_it_be(:project_in_child_of_parent_group) { create(:project, namespace: child_of_parent_group) }
+
+      it 'returns matching users who have access to the group' do
+        users = create_list(:user, 8, name: "Sarah John")
+
+        project_in_group.add_developer(users[0])
+        project_in_child_group.add_developer(users[1])
+        project_in_parent_group.add_developer(users[2])
+        project_in_child_of_parent_group.add_developer(users[3])
+
+        group.add_developer(users[4])
+        parent_group.add_developer(users[5])
+        child_group.add_developer(users[6])
+        child_of_parent_group.add_developer(users[7])
+
+        ensure_elasticsearch_index!
+
+        expect(results.objects('users')).to contain_exactly(users[0], users[1], users[4], users[5], users[6])
+        expect(results.users_count).to eq 5
+      end
+    end
+  end
+
   context 'query performance' do
     include_examples 'does not hit Elasticsearch twice for objects and counts', %w[projects notes blobs wiki_blobs commits issues merge_requests milestones users]
     include_examples 'does not load results for count only queries', %w[projects notes blobs wiki_blobs commits issues merge_requests milestones users]
