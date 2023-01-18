953991ab3d8a4e517568e13f443c38a58792c881
Jonathan Schafer <jschafer@gitlab.com>
2023-01-18 12:51:06 +0000

    Add Remediation type for Security Report Findings
    Changelog: changed
    EE: true

--- ee/spec/graphql/types/pipeline_security_report_finding_type_spec.rb
+++ ee/spec/graphql/types/pipeline_security_report_finding_type_spec.rb
@@ -8,11 +8,12 @@ RSpec.describe GitlabSchema.types['PipelineSecurityReportFinding'], feature_cate
   let_it_be(:project) { create(:project) }
   let_it_be(:user) { create(:user) }
   let_it_be(:pipeline) { create(:ci_pipeline, :with_sast_report, project: project) }
-  let_it_be(:build) { create(:ci_build, :success, name: 'sast', pipeline: pipeline) }
-  let_it_be(:artifact) { create(:ee_ci_job_artifact, :sast, job: build) }
-  let_it_be(:report) { create(:ci_reports_security_report, type: :sast, pipeline: pipeline) }
-  let_it_be(:scan) { create(:security_scan, :latest_successful, scan_type: :sast, build: artifact.job) }
-  let_it_be(:security_findings) { create_security_findings }
+
+  let_it_be(:sast_build) { create(:ci_build, :success, name: 'sast', pipeline: pipeline) }
+  let_it_be(:sast_artifact) { create(:ee_ci_job_artifact, :sast, job: sast_build) }
+  let_it_be(:sast_report) { create(:ci_reports_security_report, type: :sast, pipeline: pipeline) }
+  let_it_be(:sast_scan) { create(:security_scan, :latest_successful, scan_type: :sast, build: sast_artifact.job) }
+  let_it_be(:sast_findings) { create_findings(sast_scan, sast_report, sast_artifact) }
 
   let(:fields) do
     %i[report_type
@@ -37,15 +38,16 @@ RSpec.describe GitlabSchema.types['PipelineSecurityReportFinding'], feature_cate
        vulnerability
        issueLinks
        merge_request
+       remediations
        description_html]
   end
 
-  let(:query) do
+  let(:sast_query) do
     %(
       query {
         project(fullPath: "#{project.full_path}") {
           pipeline(iid: "#{pipeline.iid}") {
-            securityReportFindings {
+            securityReportFindings(reportType: ["sast"]) {
               nodes {
                 #{query_for_test}
               }
@@ -57,12 +59,12 @@ RSpec.describe GitlabSchema.types['PipelineSecurityReportFinding'], feature_cate
   end
 
   before do
-    stub_licensed_features(sast: true, security_dashboard: true, sast_fp_reduction: true)
+    stub_licensed_features(sast: true, dependency_scanning: true, security_dashboard: true, sast_fp_reduction: true)
 
     project.add_developer(user)
   end
 
-  subject { GitlabSchema.execute(query, context: { current_user: user }).as_json }
+  subject { GitlabSchema.execute(sast_query, context: { current_user: user }).as_json }
 
   specify { expect(described_class.graphql_name).to eq('PipelineSecurityReportFinding') }
   specify { expect(described_class).to require_graphql_authorizations(:read_security_resource) }
@@ -128,30 +130,31 @@ RSpec.describe GitlabSchema.types['PipelineSecurityReportFinding'], feature_cate
 
     context 'when the security finding has a related vulnerability' do
       let_it_be(:vulnerability) { create(:vulnerability, :with_issue_links, project: project) }
-      let_it_be(:vulnerability_finding) { create(:vulnerabilities_finding, project: project, vulnerability: vulnerability, uuid: security_findings.first.uuid) }
+      let_it_be(:vulnerability_finding) { create(:vulnerabilities_finding, project: project, vulnerability: vulnerability, uuid: sast_findings.first.uuid) }
+
+      let(:vulnerability_description) { get_findings_from_response(subject).first['vulnerability']['description'] }
 
       it 'returns vulnerabilities for the security findings' do
-        expect(get_findings_from_response(subject).first['vulnerability']['description']).to eq(vulnerability.description)
+        expect(vulnerability_description).to eq(vulnerability.description)
       end
 
       it 'avoids N+1 queries' do
         # Warm up table schema and other data (e.g. SAML providers, license)
-        GitlabSchema.execute(query, context: { current_user: user })
+        GitlabSchema.execute(sast_query, context: { current_user: user })
 
-        control_count = ActiveRecord::QueryRecorder.new { run_with_clean_state(query, context: { current_user: user }) }.count
+        control_count = ActiveRecord::QueryRecorder.new { run_with_clean_state(sast_query, context: { current_user: user }) }.count
 
-        response = GitlabSchema.execute(query, context: { current_user: user })
-        vulnerabilities = get_findings_from_response(response).pluck('vulnerability').compact
+        vulnerabilities = get_findings_from_response(subject).pluck('vulnerability').compact
         issues = get_issues_from_vulnerabilities(vulnerabilities)
         expect(vulnerabilities.count).to eq(1)
         expect(issues.count).to eq(2)
 
         new_vulnerability = create(:vulnerability, :with_issue_links, project: project)
-        create(:vulnerabilities_finding, project: project, vulnerability: new_vulnerability, uuid: security_findings.second.uuid)
+        create(:vulnerabilities_finding, project: project, vulnerability: new_vulnerability, uuid: sast_findings.second.uuid)
 
-        expect { run_with_clean_state(query, context: { current_user: user }) }.not_to exceed_query_limit(control_count)
+        expect { run_with_clean_state(sast_query, context: { current_user: user }) }.not_to exceed_query_limit(control_count)
 
-        response = GitlabSchema.execute(query, context: { current_user: user })
+        response = GitlabSchema.execute(sast_query, context: { current_user: user })
         vulnerabilities = get_findings_from_response(response).pluck('vulnerability').compact
         issues = get_issues_from_vulnerabilities(vulnerabilities)
         expect(vulnerabilities.count).to eq(2)
@@ -180,42 +183,43 @@ RSpec.describe GitlabSchema.types['PipelineSecurityReportFinding'], feature_cate
 
     context 'when there is a vulnerabillty with no issues' do
       let_it_be(:vulnerability) { create(:vulnerability, project: project) }
-      let_it_be(:vulnerability_finding) { create(:vulnerabilities_finding, project: project, vulnerability: vulnerability, uuid: security_findings.first.uuid) }
+      let_it_be(:vulnerability_finding) { create(:vulnerabilities_finding, project: project, vulnerability: vulnerability, uuid: sast_findings.first.uuid) }
+
+      let(:issue_links) { get_findings_from_response(subject).first['issueLinks']['nodes'] }
 
       it 'returns no issues' do
-        expect(get_findings_from_response(subject).first['issueLinks']['nodes']).to be_empty
+        expect(issue_links).to be_empty
       end
     end
 
     context 'when the security finding has a related vulnerability' do
       let_it_be(:issue) { create(:issue, description: 'Vulnerability issue description', project: project) }
       let_it_be(:vulnerability) { create(:vulnerability, project: project) }
-      let_it_be(:vulnerability_finding) { create(:vulnerabilities_finding, project: project, vulnerability: vulnerability, uuid: security_findings.first.uuid) }
+      let_it_be(:vulnerability_finding) { create(:vulnerabilities_finding, project: project, vulnerability: vulnerability, uuid: sast_findings.first.uuid) }
       let_it_be(:issue_link) { create(:vulnerabilities_issue_link, vulnerability: vulnerability, issue: issue) }
 
-      it 'returns issues for the security findings' do
-        response = GitlabSchema.execute(query, context: { current_user: user })
+      let(:issue_description) { get_findings_from_response(subject).first['issueLinks']['nodes'].first['issue']['description'] }
 
-        expect(get_findings_from_response(response).first['issueLinks']['nodes'].first['issue']['description']).to eq(issue.description)
+      it 'returns issues for the security findings' do
+        expect(issue_description).to eq(issue.description)
       end
 
       it 'avoids N+1 queries' do
         # Warm up table schema and other data (e.g. SAML providers, license)
-        GitlabSchema.execute(query, context: { current_user: user })
+        GitlabSchema.execute(sast_query, context: { current_user: user })
 
-        control_count = ActiveRecord::QueryRecorder.new { run_with_clean_state(query, context: { current_user: user }) }.count
+        control_count = ActiveRecord::QueryRecorder.new { run_with_clean_state(sast_query, context: { current_user: user }) }.count
 
-        response = GitlabSchema.execute(query, context: { current_user: user })
-        findings = get_findings_from_response(response)
+        findings = get_findings_from_response(subject)
         issues = get_issues_from_findings(findings)
         expect(issues.count).to eq(1)
 
         new_vulnerability = create(:vulnerability, :with_issue_links, project: project)
-        create(:vulnerabilities_finding, project: project, vulnerability: new_vulnerability, uuid: security_findings.second.uuid)
+        create(:vulnerabilities_finding, project: project, vulnerability: new_vulnerability, uuid: sast_findings.second.uuid)
 
-        expect { run_with_clean_state(query, context: { current_user: user }) }.not_to exceed_query_limit(control_count)
+        expect { run_with_clean_state(sast_query, context: { current_user: user }) }.not_to exceed_query_limit(control_count)
 
-        response = GitlabSchema.execute(query, context: { current_user: user })
+        response = GitlabSchema.execute(sast_query, context: { current_user: user })
         findings = get_findings_from_response(response)
         issues = get_issues_from_findings(findings)
         expect(issues.count).to eq(3)
@@ -238,7 +242,7 @@ RSpec.describe GitlabSchema.types['PipelineSecurityReportFinding'], feature_cate
     end
 
     context 'when there is a vulnerabillty with no merge request' do
-      let_it_be(:vulnerability_finding) { create(:vulnerabilities_finding, project: project, uuid: security_findings.first.uuid) }
+      let_it_be(:vulnerability_finding) { create(:vulnerabilities_finding, project: project, uuid: sast_findings.first.uuid) }
 
       it 'returns no merge requests' do
         expect(get_findings_from_response(subject).first['mergeRequest']).to be_nil
@@ -246,27 +250,28 @@ RSpec.describe GitlabSchema.types['PipelineSecurityReportFinding'], feature_cate
     end
 
     context 'when the security finding has a related vulnerability finding' do
-      let_it_be(:vulnerability_finding) { create(:vulnerabilities_finding, project: project, uuid: security_findings.first.uuid) }
+      let_it_be(:vulnerability_finding) { create(:vulnerabilities_finding, project: project, uuid: sast_findings.first.uuid) }
       let_it_be(:mr_feedback) { create(:vulnerability_feedback, :merge_request, project: project, finding_uuid: vulnerability_finding.uuid) }
 
+      let(:mr_description) { get_findings_from_response(subject).first['mergeRequest']['description'] }
+
       it 'returns the merge request for the security findings' do
-        response = GitlabSchema.execute(query, context: { current_user: user })
-        expect(get_findings_from_response(response).first['mergeRequest']['description']).to eq(mr_feedback.merge_request.description)
+        expect(mr_description).to eq(mr_feedback.merge_request.description)
       end
 
       it 'avoids N+1 queries' do
         # Warm up table schema and other data (e.g. SAML providers, license)
-        run_with_clean_state(query, context: { current_user: user })
+        run_with_clean_state(sast_query, context: { current_user: user })
 
-        initial_query = ActiveRecord::QueryRecorder.new { run_with_clean_state(query, context: { current_user: user }) }
+        initial_query = ActiveRecord::QueryRecorder.new { run_with_clean_state(sast_query, context: { current_user: user }) }
 
         expect(get_merge_requests_from_query.count).to eq(1)
 
         merge_request = create(:merge_request, source_project: project, target_branch: 'feature2')
-        new_vulnerability_finding = create(:vulnerabilities_finding, project: project, uuid: security_findings.second.uuid)
+        new_vulnerability_finding = create(:vulnerabilities_finding, project: project, uuid: sast_findings.second.uuid)
         create(:vulnerability_feedback, :merge_request, project: project, finding_uuid: new_vulnerability_finding.uuid, merge_request: merge_request)
 
-        expect { run_with_clean_state(query, context: { current_user: user }) }.not_to exceed_query_limit(initial_query)
+        expect { run_with_clean_state(sast_query, context: { current_user: user }) }.not_to exceed_query_limit(initial_query)
 
         # We test the data here to ensure we are pulling more data with the same number of queries
         expect(get_merge_requests_from_query.count).to eq(2)
@@ -274,16 +279,53 @@ RSpec.describe GitlabSchema.types['PipelineSecurityReportFinding'], feature_cate
     end
   end
 
-  def create_security_findings
+  describe 'remediations' do
+    let_it_be(:dep_scan_build) { create(:ci_build, :success, name: 'dependency_scanning', pipeline: pipeline) }
+    let_it_be(:dep_scan_artifact) { create(:ee_ci_job_artifact, :dependency_scanning_remediation, job: dep_scan_build) }
+    let_it_be(:dep_scan_report) { create(:ci_reports_security_report, type: :dependency_scanning, pipeline: pipeline) }
+    let_it_be(:dep_scan_scan) { create(:security_scan, :latest_successful, scan_type: :dependency_scanning, build: dep_scan_artifact.job) }
+    let_it_be(:dep_scan_findings) { create_findings(dep_scan_scan, dep_scan_report, dep_scan_artifact) }
+    let_it_be(:finding_data) { { remediation_byte_offsets: [{ start_byte: 3769, end_byte: 13792 }] } }
+
+    let(:response) { GitlabSchema.execute(remediations_query, context: { current_user: user }) }
+    let(:expected_remediation) { dep_scan_findings.flat_map(&:remediations).first }
+    let(:response_remediation) { get_findings_from_response(response).second['remediations'].first }
+    let(:remediations_query) do
+      %(
+        query {
+          project(fullPath: "#{project.full_path}") {
+            pipeline(iid: "#{pipeline.iid}") {
+              securityReportFindings(reportType: ["dependency_scanning"]) {
+                nodes {
+                  remediations {
+                    summary
+                    diff
+                  }
+                }
+              }
+            }
+          }
+        }
+      )
+    end
+
+    before do
+      dep_scan_findings.first.finding_data = finding_data
+      dep_scan_findings.first.save!
+    end
+
+    it 'returns remediations for the security findings' do
+      expect(response_remediation['summary']).to eq(expected_remediation['summary'])
+      expect(response_remediation['diff']).to eq(expected_remediation['diff'])
+    end
+  end
+
+  def create_findings(scan, report, artifact)
     content = File.read(artifact.file.path)
-    Gitlab::Ci::Parsers::Security::Sast.parse!(content, report)
+    ::Gitlab::Ci::Parsers.parsers[report.type].parse!(content, report)
     report.merge!(report)
     report.findings.map do |finding|
-      create(:security_finding,
-             severity: finding.severity,
-             confidence: finding.confidence,
-             uuid: finding.uuid,
-             scan: scan)
+      create(:security_finding, uuid: finding.uuid, scan: scan)
     end
   end
 
@@ -296,20 +338,8 @@ RSpec.describe GitlabSchema.types['PipelineSecurityReportFinding'], feature_cate
   end
   alias_method :get_issues_from_vulnerabilities, :get_issues_from_findings
 
-  def get_vulnerabilities_issues_from_query
-    response = run_with_clean_state(query, context: { current_user: user })
-    vulnerabilities = get_findings_from_response(response).pluck('vulnerability').compact
-    vulnerabilities.pluck('issueLinks').compact.pluck('nodes').flatten
-  end
-
-  def get_findings_issues_from_query
-    response = run_with_clean_state(query, context: { current_user: user })
-    findings = get_findings_from_response(response)
-    findings.pluck('issueLinks').compact.pluck('nodes').flatten
-  end
-
   def get_merge_requests_from_query
-    response = run_with_clean_state(query, context: { current_user: user })
+    response = run_with_clean_state(sast_query, context: { current_user: user })
     findings = get_findings_from_response(response)
     findings.pluck('mergeRequest').compact
   end
