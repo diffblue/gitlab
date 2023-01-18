84069214e434b1ae7b2fe7dafd3b261bb5cc040a
Luke Duncalfe <lduncalfe@gitlab.com>
2023-01-18 05:26:00 +0000

    Add missing incident trigger label
    Changelog: fixed
    EE: true

--- ee/app/assets/javascripts/integrations/constants.js
+++ ee/app/assets/javascripts/integrations/constants.js
@@ -11,14 +11,16 @@ export * from '~/integrations/constants';
 export const integrationTriggerEvents = {
   ...integrationTriggerEventsCE,
   ALERT: 'alert_events',
+  INCIDENT: 'incident_events',
   VULNERABILITY: 'vulnerability_events',
 };
 
 export const integrationTriggerEventTitles = {
   ...integrationTriggerEventTitlesCE,
   [integrationTriggerEvents.ALERT]: s__('IntegrationEvents|A new, unique alert is recorded'),
+  [integrationTriggerEvents.INCIDENT]: s__('IntegrationEvents|An incident is created'),
   [integrationTriggerEvents.VULNERABILITY]: s__(
-    'IntegrationEvents|A new, unique, vulnerability is recorded. (Note: This feature requires an Ultimate plan)',
+    'IntegrationEvents|A new, unique vulnerability is recorded (available only in GitLab Ultimate)',
   ),
 };
 /* eslint-enable import/export */
