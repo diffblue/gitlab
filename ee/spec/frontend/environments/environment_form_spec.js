fd1533371fac7e3b5dcca5626289417db089605d
Allen Cook <acook@gitlab.com>
2023-01-19 13:40:27 -0500

    Add help message and link for protecting existing environments
    Changelog: added
    EE: true

---
+++ ee/spec/frontend/environments/environment_form_spec.js
@@ -0,0 +1,45 @@
+import { mountExtended } from 'helpers/vue_test_utils_helper';
+import EnvironmentForm from '~/environments/components/environment_form.vue';
+
+jest.mock('~/lib/utils/csrf');
+
+const DEFAULT_PROPS = {
+  environment: { name: '', externalUrl: '' },
+  title: 'environment',
+  cancelPath: '/cancel',
+};
+
+const PROVIDE = { protectedEnvironmentSettingsPath: '/projects/not_real/settings/ci_cd' };
+
+describe('~/environments/components/form.vue', () => {
+  let wrapper;
+
+  const createWrapper = (propsData = {}) =>
+    mountExtended(EnvironmentForm, {
+      provide: PROVIDE,
+      propsData: {
+        ...DEFAULT_PROPS,
+        ...propsData,
+      },
+    });
+
+  afterEach(() => {
+    wrapper.destroy();
+  });
+
+  describe('when an existing environment is being edited', () => {
+    beforeEach(() => {
+      wrapper = createWrapper({
+        environment: {
+          id: 1,
+          name: 'test',
+          externalUrl: 'https://example.com',
+        },
+      });
+    });
+
+    it('does show protected environment documentation', () => {
+      expect(wrapper.findByRole('link', { name: 'Protected environments' }).exists()).toBe(true);
+    });
+  });
+});
