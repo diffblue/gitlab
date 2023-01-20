9bcf1a85f0e5e35677e971aa01b04a6c7f69e12c
Samantha Ming <sming@gitlab.com>
2023-01-20 18:38:53 +0100

    Create solution card for the GraphQL migration
    Issue: https://gitlab.com/gitlab-org/gitlab/-/issues/388816

---
+++ ee/app/assets/javascripts/vue_shared/security_reports/components/solution_card_graphql.vue
@@ -0,0 +1,34 @@
+<script>
+import { GlIcon, GlCard } from '@gitlab/ui';
+
+export default {
+  components: { GlIcon, GlCard },
+  props: {
+    solution: {
+      type: String,
+      default: '',
+      required: false,
+    },
+  },
+  computed: {
+    solutionText() {
+      // Note: this will be expanded for remediation in a follow-up issue
+      return this.solution;
+    },
+  },
+};
+</script>
+
+<template>
+  <gl-card v-if="solutionText" class="gl-my-6">
+    <template v-if="solutionText" #default>
+      <div class="gl-display-flex gl-align-items-center">
+        <div class="gl-pr-5 gl-display-flex gl-align-items-center gl-justify-content-end gl-pl-0">
+          <gl-icon class="gl-mr-5" name="bulb" />
+          <strong data-testid="solution-title">{{ s__('ciReport|Solution') }}:</strong>
+        </div>
+        <span class="flex-shrink-1 gl-pl-0" data-testid="solution-text">{{ solutionText }}</span>
+      </div>
+    </template>
+  </gl-card>
+</template>
