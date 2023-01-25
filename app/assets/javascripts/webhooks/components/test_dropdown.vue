fc92da7767e6191aaadf8d74a33ebc85dc0f9ad4
Mark Florian <mflorian@gitlab.com>
2023-01-25 19:45:48 +0000

    Add comment explaining data-method hack

--- app/assets/javascripts/webhooks/components/test_dropdown.vue
+++ app/assets/javascripts/webhooks/components/test_dropdown.vue
@@ -28,6 +28,27 @@ export default {
   },
   methods: {
     testHook(href) {
+      // HACK: Trigger @rails/ujs's data-method handling.
+      //
+      // The more obvious approaches of (1) declaratively rendering the
+      // links using GlDisclosureDropdown's list-item slot and (2) using
+      // item.extraAttrs to set the data-method attributes on the links
+      // do not work for reasons laid out in
+      // https://gitlab.com/gitlab-org/gitlab-ui/-/issues/2134.
+      //
+      // Sending the POST with axios also doesn't work, since the
+      // endpoints return 302 redirects. Since axios uses XMLHTTPRequest,
+      // it transparently follows redirects, meaning the Location header
+      // of the first response cannot be inspected/acted upon by JS. We
+      // could manually trigger a reload afterwards, but that would mean
+      // a duplicate fetch of the current page: one by the XHR, and one
+      // by the explicit reload. It would also mean losing the flash
+      // alert set by the backend, making the feature useless for the
+      // user.
+      //
+      // The ideal fix here would be to refactor the test endpoint to
+      // return a JSON response, removing the need for a redirect/page
+      // reload to show the result.
       const a = document.createElement('a');
       a.setAttribute('hidden', '');
       a.href = href;
