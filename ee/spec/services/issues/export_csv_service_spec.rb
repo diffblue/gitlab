88e6cb7c50e0d0ab98e6396f962fa463b5a5f8d4
Felipe Artur <felipefac@gmail.com>
2023-01-26 16:03:12 -0300

    Refactor CSV exporter classes
    Foundational refactoring to support
    Work items CSV export.
    
    Changelog: other

--- ee/spec/services/issues/export_csv_service_spec.rb
+++ ee/spec/services/issues/export_csv_service_spec.rb
@@ -2,7 +2,7 @@
 
 require 'spec_helper'
 
-RSpec.describe Issues::ExportCsvService do
+RSpec.describe Issues::ExportCsvService, feature_category: :importers do
   let_it_be(:user) { create(:user) }
 
   let(:group) { create(:group) }
