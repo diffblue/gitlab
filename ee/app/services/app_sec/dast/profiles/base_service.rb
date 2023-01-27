ca9bf3998efc19239fe114a7ba6ae2066c88282a
mc_rocha <mrocha@gitlab.com>
2023-01-27 17:44:47 -0500

    Fix DastProfile Update mutation
    This MR fix the DastProfile Update
    mutation to delete the tags when tagList
    is argument is an empty list.
    
    Changelog: fixed
    EE: true

--- ee/app/services/app_sec/dast/profiles/base_service.rb
+++ ee/app/services/app_sec/dast/profiles/base_service.rb
@@ -13,10 +13,12 @@ module AppSec
         end
 
         def tag_list?
-          Feature.enabled?(:on_demand_scans_runner_tags, project) && params[:tag_list].present?
+          Feature.enabled?(:on_demand_scans_runner_tags, project) && params.key?(:tag_list)
         end
 
         def tag_list
+          return [] if params[:tag_list].empty?
+
           @tag_list ||= ActsAsTaggableOn::Tag.named_any(params[:tag_list])
         end
 
