aaa00b24233b7f79c22ae147db0db1a686154ecf
Pedro Pombeiro <noreply@pedro.pombei.ro>
2023-01-30 16:26:24 +0000

    Add runner_machines relationships

--- ee/spec/lib/ee/gitlab/ci/pipeline/chain/validate/abilities_spec.rb
+++ ee/spec/lib/ee/gitlab/ci/pipeline/chain/validate/abilities_spec.rb
@@ -2,7 +2,7 @@
 
 require 'spec_helper'
 
-RSpec.describe Gitlab::Ci::Pipeline::Chain::Validate::Abilities, feature_category: :pipeline_execution do
+RSpec.describe Gitlab::Ci::Pipeline::Chain::Validate::Abilities, feature_category: :continuous_integration do
   let_it_be(:project) { create(:project, :repository) }
   let_it_be(:user) { create(:user) }
 
