fe80c18987c8551b5a92f8d0010d1bb4e1ecf378
Eulyeon Ko <5961404-euko@users.noreply.gitlab.com>
2023-01-30 00:53:49 +0000

    Enable quick actions on work items based on widgets availability
    List of available quick actions on work items depends
    on which widgets are enabled.
    
    Each widget needs to define a list of quick actions it can
    handle and based on that quick actions definition would
    make this quick action command available or not to the
    quick actions interpreter.

--- ee/app/models/work_items/widgets/weight.rb
+++ ee/app/models/work_items/widgets/weight.rb
@@ -4,6 +4,14 @@ module WorkItems
   module Widgets
     class Weight < Base
       delegate :weight, to: :work_item
+
+      def self.quick_action_commands
+        [:weight, :clear_weight]
+      end
+
+      def self.quick_action_params
+        [:weight]
+      end
     end
   end
 end
