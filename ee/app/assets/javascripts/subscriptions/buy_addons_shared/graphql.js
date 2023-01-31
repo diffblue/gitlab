4e746ad46e457e639a355e1d8616c4b87984d5de
Vamsi Vempati <vvempati@gitlab.com>
2023-01-31 06:33:51 +0000

    Extract customers dot client creation to remove duplication
    - https://gitlab.com/gitlab-org/gitlab/-/merge_requests/110136

--- ee/app/assets/javascripts/subscriptions/buy_addons_shared/graphql.js
+++ ee/app/assets/javascripts/subscriptions/buy_addons_shared/graphql.js
@@ -4,6 +4,7 @@ import VueApollo from 'vue-apollo';
 import purchaseFlowResolvers from 'ee/vue_shared/purchase_flow/graphql/resolvers';
 import typeDefs from 'ee/vue_shared/purchase_flow/graphql/typedefs.graphql';
 import createClient from '~/lib/graphql';
+import { createCustomersDotClient } from 'ee/lib/customers_dot_graphql';
 import typeDefsCDot from 'ee/subscriptions/buy_addons_shared/graphql/typedefs.graphql';
 import { CUSTOMERSDOT_CLIENT, GITLAB_CLIENT } from 'ee/subscriptions/buy_addons_shared/constants';
 import {
@@ -17,16 +18,9 @@ const gitlabClient = createClient(merge({}, gitLabResolvers, purchaseFlowResolve
   typeDefs,
 });
 
-const customersDotClient = createClient(
-  {
-    ...customersDotResolvers,
-  },
-  {
-    path: '/-/customers_dot/proxy/graphql',
-    useGet: true,
-    typeDefs: typeDefsCDot,
-  },
-);
+const customersDotClient = createCustomersDotClient(customersDotResolvers, {
+  typeDefs: typeDefsCDot,
+});
 
 export default new VueApollo({
   defaultClient: gitlabClient,
