d3b850a182284b931be28bb0a94282eb56c92e39
Krasimir Angelov <kangelov@gitlab.com>
2023-02-07 14:01:23 +1300

    Initialize conversion of snippet_user_mentions.note_id to bigint
    https://gitlab.com/gitlab-org/gitlab/-/issues/389306
    
    Changelog: added

--- db/structure.sql
+++ db/structure.sql
@@ -252,6 +252,15 @@ BEGIN
 END;
 $$;
 
+CREATE FUNCTION trigger_bfc6e47be8cc() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+  NEW."note_id_convert_to_bigint" := NEW."note_id";
+  RETURN NEW;
+END;
+$$;
+
 CREATE FUNCTION trigger_c5a5f48f12b0() RETURNS trigger
     LANGUAGE plpgsql
     AS $$
@@ -22053,7 +22062,8 @@ CREATE TABLE snippet_user_mentions (
     note_id integer,
     mentioned_users_ids integer[],
     mentioned_projects_ids integer[],
-    mentioned_groups_ids integer[]
+    mentioned_groups_ids integer[],
+    note_id_convert_to_bigint bigint
 );
 
 CREATE SEQUENCE snippet_user_mentions_id_seq
@@ -33520,6 +33530,8 @@ CREATE TRIGGER trigger_1a857e8db6cd BEFORE INSERT OR UPDATE ON vulnerability_occ
 CREATE TRIGGER trigger_3207b8d0d6f3 BEFORE INSERT OR UPDATE ON ci_build_needs FOR EACH ROW EXECUTE FUNCTION trigger_3207b8d0d6f3();
 
 CREATE TRIGGER trigger_7f4fcd5aa322 BEFORE INSERT OR UPDATE ON sent_notifications FOR EACH ROW EXECUTE FUNCTION trigger_7f4fcd5aa322();
+
+CREATE TRIGGER trigger_bfc6e47be8cc BEFORE INSERT OR UPDATE ON snippet_user_mentions FOR EACH ROW EXECUTE FUNCTION trigger_bfc6e47be8cc();
 
 CREATE TRIGGER trigger_c5a5f48f12b0 BEFORE INSERT OR UPDATE ON epic_user_mentions FOR EACH ROW EXECUTE FUNCTION trigger_c5a5f48f12b0();
 
