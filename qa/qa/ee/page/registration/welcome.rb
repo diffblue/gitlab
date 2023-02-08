bc9cb52233076acfc2b6372b38b43680b2ea0f5f
Sanad Liaquat <sliaquat@gitlab.com>
2023-02-08 07:50:58 +0000

    Fix and update user onboarding flow

--- qa/qa/ee/page/registration/welcome.rb
+++ qa/qa/ee/page/registration/welcome.rb
@@ -18,11 +18,19 @@ module QA
               view 'ee/app/views/registrations/welcome/_setup_for_company.html.haml' do
                 element :setup_for_company_radio
               end
+
+              view 'ee/app/views/registrations/welcome/_joining_project.html.haml' do
+                element :create_a_new_project_radio
+              end
             end
           end
 
+          def choose_create_a_new_project_if_available
+            click_element(:create_a_new_project_radio) if has_element?(:create_a_new_project_radio, wait: 1)
+          end
+
           def choose_setup_for_company_if_available
-            choose_element(:setup_for_company_radio) if has_element?(:setup_for_company_radio, wait: 1)
+            click_element(:setup_for_company_radio) if has_element?(:setup_for_company_radio, wait: 1)
           end
         end
       end
