8bd98fe19e223decbc0bcd8d525761b203f2c0cd
Simon Knox <simon@gitlab.com>
2023-02-09 22:35:55 +1100

    Remove many unused bg- CSS classes
    Used the following to search for usages:
    (?!gl-)(bg|text)-(primary|secondary|success|warning|danger)-

--- ee/app/assets/javascripts/boards/components/issues_lane_list.vue
+++ ee/app/assets/javascripts/boards/components/issues_lane_list.vue
@@ -191,7 +191,7 @@ export default {
         class="board-cell gl-p-2 gl-m-0 gl-h-full gl-list-style-none"
         :class="{
           'board-column-highlighted': highlighted,
-          'bg-danger-100 gl-rounded-base': boardItemsSizeExceedsMax,
+          'gl-bg-red-100 gl-rounded-base': boardItemsSizeExceedsMax,
         }"
         data-testid="tree-root-wrapper"
         @start="handleDragOnStart"
