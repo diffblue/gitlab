6d4a627c39af88b2657b4d00917ed5107dbbeba5
Igor Frenkel <ifrenkel@gitlab.com>
2023-02-13 07:14:48 +0000

    Add scheduled job to start package metadata sync
    Add worker and cron job setting which kicks off the package metadata
    synchronization process by trigerring the sync service.
    
    Changelog: added
    EE: true

---
+++ ee/app/services/package_metadata/sync_service.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module PackageMetadata
+  class SyncService
+    def self.execute
+      # noop
+      # 'service implemented as part of https://gitlab.com/gitlab-org/gitlab/-/merge_requests/109713'
+    end
+  end
+end
---
+++ ee/spec/workers/package_metadata/sync_worker_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe PackageMetadata::SyncWorker, type: :worker, feature_category: :license_compliance do
+  include_examples 'an idempotent worker' do
+    subject do
+      perform_multiple([], worker: described_class.new)
+    end
+  end
+
+  describe '#perform' do
+    subject(:perform) { described_class.new.perform }
+
+    context 'with feature flag enabled' do
+      it 'calls the sync service to do the work' do
+        expect(PackageMetadata::SyncService).to receive(:execute)
+        perform
+      end
+    end
+
+    context 'with feature flag disabled' do
+      before do
+        stub_feature_flags(package_metadata_synchronization: false)
+      end
+
+      it 'does not call the sync service to do the work' do
+        expect(PackageMetadata::SyncService).not_to receive(:execute)
+        perform
+      end
+    end
+  end
+end
