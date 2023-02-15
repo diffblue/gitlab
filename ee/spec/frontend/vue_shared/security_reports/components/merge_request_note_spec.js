748f2fbe33b1a26ee7864acbd0a5dae2dff7f202
Daniel Tian <dtian@gitlab.com>
2023-02-15 13:36:07 +0000

    Fix missing data in merge request note on vulnerability details page
    Changelog: fixed
    MR:
    EE: true

--- ee/spec/frontend/vue_shared/security_reports/components/merge_request_note_spec.js
+++ ee/spec/frontend/vue_shared/security_reports/components/merge_request_note_spec.js
@@ -1,84 +1,114 @@
 import { GlSprintf } from '@gitlab/ui';
-import { shallowMount } from '@vue/test-utils';
+import { shallowMountExtended } from 'helpers/vue_test_utils_helper';
 import EventItem from 'ee/vue_shared/security_reports/components/event_item.vue';
-import component from 'ee/vue_shared/security_reports/components/merge_request_note.vue';
+import MergeRequestNote from 'ee/vue_shared/security_reports/components/merge_request_note.vue';
+
+const author = { name: 'Tanuki', username: 'gitlab' };
+
+const FEEDBACK = {
+  author,
+  merge_request_path: '/path-to-the-mr',
+  merge_request_iid: 1,
+  created_at: '2022-02-22T22:22:22.222Z',
+};
+
+const FEEDBACK_CAMEL_CASE = {
+  author,
+  mergeRequestPath: '/path-to-the-mr',
+  mergeRequestIid: 1,
+  createdAt: '2022-02-22T22:22:22.222Z',
+};
+
+const PROJECT = {
+  value: 'Project one',
+  url: '/path-to-the-project',
+};
 
 describe('Merge request note', () => {
-  const now = new Date();
-  const feedback = {
-    author: {
-      name: 'Tanuki',
-      username: 'gitlab',
-    },
-    merge_request_path: '/path-to-the-issue',
-    merge_request_iid: 1,
-    created_at: now.toString(),
+  let wrapper;
+
+  const createWrapper = ({ feedback = FEEDBACK, project = PROJECT } = {}) => {
+    wrapper = shallowMountExtended(MergeRequestNote, {
+      stubs: { GlSprintf },
+      propsData: { feedback, project },
+    });
   };
-  const project = {
-    value: 'Project one',
-    url: '/path-to-the-project',
-  };
+
+  const findEventItem = () => wrapper.findComponent(EventItem);
+  const findMergeRequestLink = () => wrapper.findByTestId('mergeRequestLink');
+  const findProjectLink = () => wrapper.findByTestId('projectLink');
 
   describe('with no attached project', () => {
-    let wrapper;
+    beforeEach(() => createWrapper({ project: null }));
 
-    beforeEach(() => {
-      wrapper = shallowMount(component, {
-        stubs: { GlSprintf },
-        propsData: { feedback },
+    it('should pass the expected data to the event item', () => {
+      expect(findEventItem().props()).toMatchObject({
+        author: FEEDBACK.author,
+        createdAt: FEEDBACK.created_at,
       });
     });
 
-    it('should pass the author to the event item', () => {
-      expect(wrapper.findComponent(EventItem).props('author')).toBe(feedback.author);
-    });
-
-    it('should pass the created date to the event item', () => {
-      expect(wrapper.findComponent(EventItem).props('createdAt')).toBe(feedback.created_at);
-    });
-
     it('should return the event text with no project data', () => {
-      expect(wrapper.text()).toBe(`Created merge request !${feedback.merge_request_iid}`);
+      expect(wrapper.text()).toMatchInterpolatedText(
+        `Created merge request !${FEEDBACK.merge_request_iid}`,
+      );
+    });
+
+    it('should have the correct url for the merge request link', () => {
+      expect(findMergeRequestLink().attributes('href')).toBe(FEEDBACK.merge_request_path);
+    });
+
+    it('should not show the project link', () => {
+      expect(findProjectLink().exists()).toBe(false);
     });
   });
 
   describe('with an attached project', () => {
-    let wrapper;
-
-    beforeEach(() => {
-      wrapper = shallowMount(component, {
-        stubs: { GlSprintf },
-        propsData: { feedback, project },
-      });
-    });
+    beforeEach(createWrapper);
 
     it('should return the event text with project data', () => {
-      expect(wrapper.text()).toBe(
-        `Created merge request !${feedback.merge_request_iid} at ${project.value}`,
+      expect(wrapper.text()).toMatchInterpolatedText(
+        `Created merge request !${FEEDBACK.merge_request_iid} at ${PROJECT.value}`,
       );
+    });
+
+    it('should have the correct url for the project link', () => {
+      expect(findProjectLink().attributes('href')).toBe(PROJECT.url);
     });
   });
 
   describe('with unsafe data', () => {
-    let wrapper;
     const unsafeProject = {
-      ...project,
+      ...PROJECT,
       value: 'Foo <script>alert("XSS")</script>',
     };
 
-    beforeEach(() => {
-      wrapper = shallowMount(component, {
-        stubs: { GlSprintf },
-        propsData: {
-          feedback,
-          project: unsafeProject,
-        },
-      });
-    });
+    beforeEach(() => createWrapper({ project: unsafeProject }));
 
     it('should escape the project name', () => {
       // Test that the XSS text has been rendered literally as safe text.
       expect(wrapper.text()).toContain(unsafeProject.value);
+    });
+  });
+
+  describe('merge request from vulnerability details page', () => {
+    beforeEach(() => createWrapper({ feedback: FEEDBACK_CAMEL_CASE }));
+
+    it('should pass expected data to the event item', () => {
+      expect(findEventItem().props()).toMatchObject({
+        author: FEEDBACK_CAMEL_CASE.author,
+        createdAt: FEEDBACK_CAMEL_CASE.createdAt,
+      });
+    });
+
+    it('should show the event text with project data', () => {
+      expect(wrapper.text()).toMatchInterpolatedText(
+        `Created merge request !${FEEDBACK_CAMEL_CASE.mergeRequestIid} at ${PROJECT.value}`,
+      );
+    });
+
+    it('should have the correct url for the merge request link', () => {
+      expect(findMergeRequestLink().attributes('href')).toBe(FEEDBACK_CAMEL_CASE.mergeRequestPath);
     });
   });
 });
