09ffa66462a2796fd109ebd0aac6413c55b8d9d9
Peter Leitzen <pleitzen@gitlab.com>
2023-02-15 12:40:08 +0100

    Mimic Rails' autoloading with zeitwerk outside of Rails

--- spec/fast_spec_helper.rb
+++ spec/fast_spec_helper.rb
@@ -14,28 +14,8 @@ ENV['IN_MEMORY_APPLICATION_SETTINGS'] = 'true'
 # Enable zero monkey patching mode before loading any other RSpec code.
 RSpec.configure(&:disable_monkey_patching!)
 
-require 'zeitwerk'
-require 'rails/autoloaders/inflector'
+require_relative 'rails_autoload'
 
-module Rails
-  extend self
-
-  def root
-    Pathname.new(File.expand_path('..', __dir__))
-  end
-
-  def autoloaders
-    @autoloaders ||= [
-      Zeitwerk::Loader.new.tap do |loader|
-        loader.inflector = Rails::Autoloaders::Inflector
-      end
-    ]
-  end
-end
-
-require_relative '../lib/gitlab'
-require_relative '../config/initializers/0_inject_enterprise_edition_module'
-require_relative '../config/initializers_before_autoloader/004_zeitwerk'
 require_relative '../config/settings'
 require_relative 'support/rspec'
 require_relative '../lib/gitlab/utils'
@@ -44,13 +24,6 @@ require 'active_support/all'
 
 require_relative 'simplecov_env'
 SimpleCovEnv.start!
-
-Rails.autoloaders.each do |autoloader|
-  autoloader.push_dir('lib')
-  autoloader.push_dir('ee/lib') if Gitlab.ee?
-  autoloader.push_dir('jh/lib') if Gitlab.jh?
-  autoloader.setup
-end
 
 ActiveSupport::XmlMini.backend = 'Nokogiri'
 
