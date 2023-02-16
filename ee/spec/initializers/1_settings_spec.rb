dc225b4ce155afd0731edce5bb3d15cd22a047f5
Vijay Hawoldar <vhawoldar@gitlab.com>
2023-02-16 13:44:06 +0000

    Fix 1_settings.rb to be able to be loaded again
    1. Fix settings.dig to work again. Because a `default` key was defined
       in gitlab.yml, this overrode Hash.default in Settings which meant
       `dig` method broke. Work-around this by converting to a Hash object
       first.
    2. Fix transformation of Settings.repositories.storages to be idempotent

--- ee/spec/initializers/1_settings_spec.rb
+++ ee/spec/initializers/1_settings_spec.rb
@@ -12,5 +12,25 @@ RSpec.describe '1_settings' do
         expect(cron_jobs.dig('sync_seat_link_worker', 'cron')).to match(/[1-5]{0,1}[0-9]{1,2} [34] \* \* \* UTC/)
       end
     end
+
+    context 'gitlab.com', :saas do
+      let(:load_settings) do
+        load Rails.root.join('config/initializers/1_settings.rb')
+      end
+
+      let(:dot_com_cron_jobs) do
+        %w[
+          disable_legacy_open_source_license_for_inactive_projects
+          notify_seats_exceeded_batch_worker
+          gitlab_subscriptions_schedule_refresh_seats_worker
+        ]
+      end
+
+      it 'assigns .com only settings' do
+        load_settings
+
+        expect(cron_jobs.keys).to include(*dot_com_cron_jobs)
+      end
+    end
   end
 end
