aeac0f6114f6435b5e8debfbe4fa08131dc6aee1
Madelein van Niekerk <mvanniekerk@gitlab.com>
2023-02-16 02:10:25 +0000

    Refactor elasticsearchable concern for better readability
    EE: true

--- ee/spec/services/concerns/search/elasticsearchable_spec.rb
+++ ee/spec/services/concerns/search/elasticsearchable_spec.rb
@@ -60,11 +60,28 @@ RSpec.describe Search::Elasticsearchable, feature_category: :global_search do
       context 'when scope is users' do
         let(:params) { { scope: 'users' } }
 
-        [true, false].each do |matcher|
-          it 'is equal to advanced_user_search?' do
-            allow(class_instance).to receive(:advanced_user_search?).and_return(matcher)
+        using RSpec::Parameterized::TableSyntax
 
-            expect(class_instance.use_elasticsearch?).to eq(matcher)
+        where(:advanced_user_search_enabled, :create_user_index_finished, :backfill_users_finished, :result) do
+          true | true | true | true
+          true | true | false | false
+          true | false | true | false
+          true | false | false | false
+          false | true | true | false
+          false | false | false | false
+        end
+
+        with_them do
+          it 'returns the correct result' do
+            stub_feature_flags(advanced_user_search: advanced_user_search_enabled)
+
+            allow(Elastic::DataMigrationService).to receive(:migration_has_finished?)
+            .with(:create_user_index).and_return(create_user_index_finished)
+
+            allow(Elastic::DataMigrationService).to receive(:migration_has_finished?)
+            .with(:backfill_users).and_return(backfill_users_finished)
+
+            expect(class_instance.use_elasticsearch?).to eq(result)
           end
         end
       end
@@ -81,41 +98,6 @@ RSpec.describe Search::Elasticsearchable, feature_category: :global_search do
 
       it 'is true' do
         expect(class_instance).to be_global_elasticsearchable_scope
-      end
-    end
-  end
-
-  describe "#advanced_user_search?" do
-    it 'is true when the scope is not users' do
-      expect(class_instance).to be_advanced_user_search
-    end
-
-    context 'when scope is users' do
-      using RSpec::Parameterized::TableSyntax
-
-      let(:params) { { scope: 'users' } }
-
-      where(:advanced_user_search_enabled, :create_user_index_finished, :backfill_users_finished, :result) do
-        true | true | true | true
-        true | true | false | false
-        true | false | true | false
-        true | false | false | false
-        false | true | true | false
-        false | false | false | false
-      end
-
-      with_them do
-        it 'returns the correct result' do
-          stub_feature_flags(advanced_user_search: advanced_user_search_enabled)
-
-          allow(Elastic::DataMigrationService).to receive(:migration_has_finished?)
-          .with(:create_user_index).and_return(create_user_index_finished)
-
-          allow(Elastic::DataMigrationService).to receive(:migration_has_finished?)
-          .with(:backfill_users).and_return(backfill_users_finished)
-
-          expect(class_instance.advanced_user_search?).to eq(result)
-        end
       end
     end
   end
