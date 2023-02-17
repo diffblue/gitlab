685d56db94a8d6af81c77c32603a34a5f3a34b6a
Pushpa Raj Badu <przbadu.social@gmail.com>
2023-02-17 15:06:15 +0000

    Use declarative enum on external issue link type
    Changelog: fixed
    MR: https://gitlab.com/gitlab-org/gitlab/-/merge_requests/111998
    EE: true

--- ee/app/models/vulnerabilities/external_issue_link.rb
+++ ee/app/models/vulnerabilities/external_issue_link.rb
@@ -3,13 +3,14 @@
 module Vulnerabilities
   class ExternalIssueLink < ApplicationRecord
     include EachBatch
+
     self.table_name = 'vulnerability_external_issue_links'
 
     belongs_to :author, class_name: 'User'
     belongs_to :vulnerability
 
-    enum link_type: { created: 1 }
-    enum external_type: { jira: 1 }
+    declarative_enum LinkTypeEnum
+    declarative_enum ExternalTypeEnum
 
     validates :vulnerability, :external_issue_key, :external_type, :external_project_key, presence: true
     validates :external_issue_key, uniqueness: { scope: [:vulnerability_id, :external_type, :external_project_key], message: N_('has already been linked to another vulnerability') }
