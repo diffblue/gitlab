3c67ed493562f7e0eed5efabfede50fcc7b203cb
Stan Hu <stanhu@gmail.com>
2023-02-27 14:12:09 -0800

    Fix object storage bucket prefix handling for blob deletion
    In https://gitlab.com/gitlab-org/gitlab/-/merge_requests/91307, we
    added formal support for using bucket prefixes in object storage.
    However, deletion of objects did not handle this. This commit appends
    the prefix to the object key if it is available.

--- spec/models/uploads/fog_spec.rb
+++ spec/models/uploads/fog_spec.rb
@@ -3,10 +3,21 @@
 require 'spec_helper'
 
 RSpec.describe Uploads::Fog do
+  let(:credentials) do
+    {
+      provider: "AWS",
+      aws_access_key_id: "AWS_ACCESS_KEY_ID",
+      aws_secret_access_key: "AWS_SECRET_ACCESS_KEY",
+      region: "eu-central-1"
+    }
+  end
+
+  let(:bucket_prefix) { nil }
   let(:data_store) { described_class.new }
+  let(:config) { { connection: credentials, bucket_prefix: bucket_prefix, remote_directory: 'uploads' } }
 
   before do
-    stub_uploads_object_storage(FileUploader)
+    stub_uploads_object_storage(FileUploader, config: config)
   end
 
   describe '#available?' do
@@ -18,7 +29,7 @@ RSpec.describe Uploads::Fog do
 
     context 'when object storage is disabled' do
       before do
-        stub_uploads_object_storage(FileUploader, enabled: false)
+        stub_uploads_object_storage(FileUploader, config: config, enabled: false)
       end
 
       it { is_expected.to be_falsy }
@@ -28,7 +39,6 @@ RSpec.describe Uploads::Fog do
   context 'model with uploads' do
     let(:project) { create(:project) }
     let(:relation) { project.uploads }
-    let(:credentials) { FileUploader.object_store_credentials }
     let(:connection) { ::Fog::Storage.new(credentials) }
     let(:paths) { relation.pluck(:path) }
 
@@ -52,9 +62,6 @@ RSpec.describe Uploads::Fog do
       end
 
       before do
-        config = Gitlab.config.uploads.object_store.to_h.symbolize_keys.merge(connection: credentials)
-        stub_uploads_object_storage(FileUploader, config: config)
-
         uploads.each { |upload| upload.retrieve_uploader.migrate!(2) }
       end
 
@@ -98,7 +105,7 @@ RSpec.describe Uploads::Fog do
     end
 
     describe '#delete_keys' do
-      let(:connection) { ::Fog::Storage.new(FileUploader.object_store_credentials) }
+      let(:connection) { ::Fog::Storage.new(credentials) }
       let(:keys) { data_store.keys(relation) }
       let(:paths) { relation.pluck(:path) }
       let!(:uploads) { create_list(:upload, 2, :with_file, :issuable_upload, model: project) }
@@ -118,6 +125,22 @@ RSpec.describe Uploads::Fog do
 
         paths.each do |path|
           expect { connection.get_object('uploads', path)[:body] }.to raise_error(Excon::Error::NotFound)
+        end
+      end
+
+      context 'with bucket prefix' do
+        let(:bucket_prefix) { 'test-prefix' }
+
+        it 'deletes multiple data' do
+          paths.each do |path|
+            expect(connection.get_object('uploads', File.join(bucket_prefix, path))[:body]).not_to be_nil
+          end
+
+          subject
+
+          paths.each do |path|
+            expect { connection.get_object('uploads', File.join(bucket_prefix, path))[:body] }.to raise_error(Excon::Error::NotFound)
+          end
         end
       end
 
