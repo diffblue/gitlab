dad9f4d53fafb816d1eed5c91be433c654293531
Serhii Yarynovskyi <syarynovskyi@gitlab.com>
2023-02-28 10:57:41 +0000

    Remove reactivate/extend trial button
    Remove reactivate/extend trial button from
    profile billing page
    
    Changelog: removed

--- ee/app/assets/javascripts/api/subscriptions_api.js
+++ ee/app/assets/javascripts/api/subscriptions_api.js
@@ -2,13 +2,7 @@ import { buildApiUrl } from '~/api/api_utils';
 import axios from '~/lib/utils/axios_utils';
 
 const SUBSCRIPTIONS_PATH = '/api/:version/subscriptions';
-const EXTEND_REACTIVATE_TRIAL_PATH = '/-/trials/extend_reactivate';
 const CREATE_HAND_RAISE_LEAD_PATH = '/-/trials/create_hand_raise_lead';
-
-const TRIAL_EXTENSION_TYPE = Object.freeze({
-  extended: 1,
-  reactivated: 2,
-});
 
 export function createSubscription(groupId, customer, subscription) {
   const url = buildApiUrl(SUBSCRIPTIONS_PATH);
@@ -20,28 +14,6 @@ export function createSubscription(groupId, customer, subscription) {
 
   return axios.post(url, { params });
 }
-
-const updateTrial = async (namespaceId, trialExtensionType) => {
-  if (!Object.values(TRIAL_EXTENSION_TYPE).includes(trialExtensionType)) {
-    throw new TypeError('The "trialExtensionType" argument is invalid.');
-  }
-
-  const url = buildApiUrl(EXTEND_REACTIVATE_TRIAL_PATH);
-  const params = {
-    namespace_id: namespaceId,
-    trial_extension_type: trialExtensionType,
-  };
-
-  return axios.put(url, params);
-};
-
-export const extendTrial = async (namespaceId) => {
-  return updateTrial(namespaceId, TRIAL_EXTENSION_TYPE.extended);
-};
-
-export const reactivateTrial = async (namespaceId) => {
-  return updateTrial(namespaceId, TRIAL_EXTENSION_TYPE.reactivated);
-};
 
 export const sendHandRaiseLead = async (params) => {
   const url = buildApiUrl(CREATE_HAND_RAISE_LEAD_PATH);
