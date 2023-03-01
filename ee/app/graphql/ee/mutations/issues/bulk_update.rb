43618e9e686aa4a309673e6c7a0e3980be485079
Mario Celi <mcelicalderon@gitlab.com>
2023-03-01 17:25:25 +0000

    Add missing attributes to IssuesBulkUpdate
    Adds missing attributes necessary for the
    issue bulk update view and the group and project level
    
    state_event
    add_label_ids
    remove_label_ids
    subscription_event
    epic_id
    health_status

--- ee/app/graphql/ee/mutations/issues/bulk_update.rb
+++ ee/app/graphql/ee/mutations/issues/bulk_update.rb
@@ -11,18 +11,36 @@ module EE
           argument :iteration_id, ::Types::GlobalIDType[::Iteration],
             required: false,
             description: 'Global ID of the iteration that will be assigned to the issues.'
+
+          argument :epic_id, ::Types::GlobalIDType[::Epic],
+            required: false,
+            description: 'Global ID of the epic that will be assigned to the issues.'
+
+          argument :health_status, ::Types::HealthStatusEnum,
+            required: false,
+            description: 'Health status that will be assigned to the issues.'
         end
 
         private
 
         override :global_id_arguments
         def global_id_arguments
-          super + %i[iteration_id]
+          super + %i[iteration_id epic_id]
         end
 
         override :param_mappings
         def param_mappings
           super.merge(iteration_id: :sprint_id)
+        end
+
+        override :find_parent!
+        def find_parent!(parent_id)
+          parent = super
+          return parent unless parent.is_a?(::Group)
+
+          raise_resource_not_available_error! unless parent.licensed_feature_available?(:group_bulk_edit)
+
+          parent
         end
       end
     end
