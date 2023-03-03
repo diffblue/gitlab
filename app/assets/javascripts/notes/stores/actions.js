d2ad418250fbfc43ca0c448f0adebb917d08e6f9
Eduardo Sanz García <esanz-garcia@gitlab.com>
2023-03-03 20:17:18 +0100

    Replace `flash` imports to `alert` - 9/36
    Replace old `flash` import to the newer `alert`.
    
    Follow-up of
    https://gitlab.com/gitlab-org/gitlab/-/merge_requests/109449+

--- app/assets/javascripts/notes/stores/actions.js
+++ app/assets/javascripts/notes/stores/actions.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import Visibility from 'visibilityjs';
 import Vue from 'vue';
 import Api from '~/api';
-import { createAlert, VARIANT_INFO } from '~/flash';
+import { createAlert, VARIANT_INFO } from '~/alert';
 import { EVENT_ISSUABLE_VUE_APP_CHANGE } from '~/issuable/constants';
 import { TYPE_ISSUE } from '~/issues/constants';
 import axios from '~/lib/utils/axios_utils';
@@ -759,10 +759,10 @@ export const submitSuggestion = (
 
       const errorMessage = err.response.data?.message;
 
-      const flashMessage = errorMessage || defaultMessage;
+      const alertMessage = errorMessage || defaultMessage;
 
       createAlert({
-        message: flashMessage,
+        message: alertMessage,
         parent: flashContainer,
       });
     })
@@ -795,10 +795,10 @@ export const submitSuggestionBatch = ({ commit, dispatch, state }, { message, fl
 
       const errorMessage = err.response.data?.message;
 
-      const flashMessage = errorMessage || defaultMessage;
+      const alertMessage = errorMessage || defaultMessage;
 
       createAlert({
-        message: flashMessage,
+        message: alertMessage,
         parent: flashContainer,
       });
     })
