6abb040b1c137c43f8f306b61a07ae9a730ffffc
Artur Fedorov <afedorov@gitlab.com>
2023-03-03 13:52:13 +0000

    This MR removes Vue extend from test
    As part of Vue 3 migration
    Vue extend behaviour should be replaced
    with helper methods

--- ee/spec/frontend/roadmap/components/milestone_item_spec.js
+++ ee/spec/frontend/roadmap/components/milestone_item_spec.js
@@ -1,12 +1,9 @@
-import { mount } from '@vue/test-utils';
-import Vue from 'vue';
-
-import milestoneItemComponent from 'ee/roadmap/components/milestone_item.vue';
-
+import { GlLink } from '@gitlab/ui';
+import MilestoneItemComponent from 'ee/roadmap/components/milestone_item.vue';
 import { DATE_RANGES, PRESET_TYPES } from 'ee/roadmap/constants';
 import { getTimeframeForRangeType } from 'ee/roadmap/utils/roadmap_utils';
-
 import { mockTimeframeInitialDate, mockMilestone2 } from 'ee_jest/roadmap/mock_data';
+import { shallowMountExtended } from 'helpers/vue_test_utils_helper';
 
 const mockTimeframeMonths = getTimeframeForRangeType({
   timeframeRangeType: DATE_RANGES.THREE_YEARS,
@@ -14,35 +11,25 @@ const mockTimeframeMonths = getTimeframeForRangeType({
   initialDate: mockTimeframeInitialDate,
 });
 
-const createComponent = ({
-  presetType = PRESET_TYPES.MONTHS,
-  milestone = mockMilestone2,
-  timeframe = mockTimeframeMonths,
-  timeframeItem = mockTimeframeMonths[16], // timeframe item where milestone begins
-}) => {
-  const Component = Vue.extend(milestoneItemComponent);
-
-  return mount(Component, {
-    propsData: {
-      presetType,
-      milestone,
-      timeframe,
-      timeframeItem,
-    },
-    stubs: { GlPopover: true },
-  });
-};
-
 describe('MilestoneItemComponent', () => {
   let wrapper;
 
-  beforeEach(() => {
-    wrapper = createComponent({});
-  });
+  const createComponent = (props = {}) => {
+    wrapper = shallowMountExtended(MilestoneItemComponent, {
+      propsData: {
+        presetType: PRESET_TYPES.MONTHS,
+        milestone: mockMilestone2,
+        timeframe: mockTimeframeMonths,
+        timeframeItem: mockTimeframeMonths[16], // timeframe item where milestone begins,
+        ...props,
+      },
+    });
+  };
 
-  afterEach(() => {
-    wrapper.destroy();
-    wrapper = null;
+  const findLink = () => wrapper.findComponent(GlLink);
+
+  beforeEach(() => {
+    createComponent();
   });
 
   describe('computed', () => {
@@ -71,7 +58,7 @@ describe('MilestoneItemComponent', () => {
     });
 
     it('returns Milestone.startDate when start date is within range', () => {
-      wrapper = createComponent({ milestone: mockMilestone2 });
+      createComponent({ milestone: mockMilestone2 });
 
       expect(wrapper.vm.startDate).toBe(mockMilestone2.startDate);
     });
@@ -83,7 +70,7 @@ describe('MilestoneItemComponent', () => {
         startDateOutOfRange: true,
         originalStartDate: mockStartDate,
       };
-      wrapper = createComponent({ milestone: mockMilestoneItem });
+      createComponent({ milestone: mockMilestoneItem });
 
       expect(wrapper.vm.startDate).toBe(mockStartDate);
     });
@@ -91,7 +78,7 @@ describe('MilestoneItemComponent', () => {
 
   describe('endDate', () => {
     it('returns Milestone.endDate when end date is within range', () => {
-      wrapper = createComponent({ milestone: mockMilestone2 });
+      createComponent({ milestone: mockMilestone2 });
 
       expect(wrapper.vm.endDate).toBe(mockMilestone2.endDate);
     });
@@ -103,7 +90,7 @@ describe('MilestoneItemComponent', () => {
         endDateOutOfRange: true,
         originalEndDate: mockEndDate,
       };
-      wrapper = createComponent({ milestone: mockMilestoneItem });
+      createComponent({ milestone: mockMilestoneItem });
 
       expect(wrapper.vm.endDate).toBe(mockEndDate);
     });
@@ -111,21 +98,21 @@ describe('MilestoneItemComponent', () => {
 
   describe('timeframeString', () => {
     it('returns timeframe string correctly when both start and end dates are defined', () => {
-      wrapper = createComponent({ milestone: mockMilestone2 });
+      createComponent({ milestone: mockMilestone2 });
 
       expect(wrapper.vm.timeframeString(mockMilestone2)).toBe('Nov 10, 2017 – Jul 2, 2018');
     });
 
     it('returns timeframe string correctly when only start date is defined', () => {
       const mockMilestoneItem = { ...mockMilestone2, endDateUndefined: true };
-      wrapper = createComponent({ milestone: mockMilestoneItem });
+      createComponent({ milestone: mockMilestoneItem });
 
       expect(wrapper.vm.timeframeString(mockMilestoneItem)).toBe('Nov 10, 2017 – No end date');
     });
 
     it('returns timeframe string correctly when only end date is defined', () => {
       const mockMilestoneItem = { ...mockMilestone2, startDateUndefined: true };
-      wrapper = createComponent({ milestone: mockMilestoneItem });
+      createComponent({ milestone: mockMilestoneItem });
 
       expect(wrapper.vm.timeframeString(mockMilestoneItem)).toBe('No start date – Jul 2, 2018');
     });
@@ -136,7 +123,7 @@ describe('MilestoneItemComponent', () => {
         startDate: new Date(2018, 0, 1),
         endDate: new Date(2018, 3, 1),
       };
-      wrapper = createComponent({ milestone: mockMilestoneItem });
+      createComponent({ milestone: mockMilestoneItem });
 
       expect(wrapper.vm.timeframeString(mockMilestoneItem)).toBe('Jan 1 – Apr 1, 2018');
     });
@@ -148,19 +135,19 @@ describe('MilestoneItemComponent', () => {
     });
 
     it('renders component element class `milestone-item-details`', () => {
-      expect(wrapper.vm.$el.querySelector('.milestone-item-details')).not.toBeNull();
+      expect(wrapper.find('.milestone-item-details')).not.toBeNull();
     });
 
     it('renders Milestone item link element with class `milestone-url`', () => {
-      expect(wrapper.vm.$el.querySelector('.milestone-url')).not.toBeNull();
+      expect(findLink()).not.toBeNull();
     });
 
     it('renders Milestone timeline bar element with class `timeline-bar`', () => {
-      expect(wrapper.vm.$el.querySelector('.timeline-bar')).not.toBeNull();
+      expect(wrapper.find('.timeline-bar')).not.toBeNull();
     });
 
     it('renders Milestone title element with class `milestone-item-title`', () => {
-      expect(wrapper.vm.$el.querySelector('.milestone-item-title')).not.toBeNull();
+      expect(wrapper.findByText(mockMilestone2.title)).not.toBeNull();
     });
   });
 });
