e5fe6acf9ac0cfbd21d135df712e02efac1d861e
Allen Cook <acook@gitlab.com>
2023-03-06 15:47:27 +0000

    Move Cube API calls to it's own service

--- ee/lib/api/analytics/product_analytics.rb
+++ ee/lib/api/analytics/product_analytics.rb
@@ -5,7 +5,6 @@ module API
     class ProductAnalytics < ::API::Base
       before do
         authenticate!
-        check_application_settings!
       end
 
       feature_category :product_analytics
@@ -16,69 +15,33 @@ module API
           @project ||= find_project!(params[:project_id])
         end
 
-        def check_application_settings!
-          not_found! unless Gitlab::CurrentSettings.product_analytics_enabled?
-          not_found! unless Gitlab::CurrentSettings.cube_api_base_url.present?
-          not_found! unless Gitlab::CurrentSettings.cube_api_key.present?
-        end
-
-        def check_access_rights!
-          not_found! unless project.product_analytics_enabled?
-          unauthorized! unless can?(current_user, :developer_access, project)
-        end
-
-        def cube_server_url(endpoint)
-          "#{Gitlab::CurrentSettings.cube_api_base_url}/cubejs-api/v1/" + endpoint
-        end
-
-        def gitlab_token
-          return unless params[:include_token]
-
-          ::ResourceAccessTokens::CreateService.new(
-            current_user,
-            project,
-            { expires_at: 1.day.from_now }).execute.payload[:access_token]&.token
-        end
-
-        def cube_security_headers
-          payload = {
-            iat: Time.now.utc.to_i,
-            exp: Time.now.utc.to_i + 180,
-            appId: "gitlab_project_#{params[:project_id]}",
-            gitlabToken: gitlab_token,
-            iss: ::Settings.gitlab.host
-          }
-
-          {
-            "Content-Type": 'application/json',
-            Authorization: JWT.encode(payload, Gitlab::CurrentSettings.cube_api_key, 'HS256')
-          }
-        end
-
-        def database_exists?(body)
-          (body['error'] =~ %r{\AError: Code: (81|60)\..*(UNKNOWN_DATABASE|UNKNOWN_TABLE)}).nil?
+        def render_response(response)
+          if response.success?
+            status :ok
+            response.payload
+          else
+            render_api_error!(response.message, response.reason)
+          end
         end
 
         def cube_data_query(load_data)
-          check_access_rights!
+          params = declared_params(include_missing: false).merge(path: load_data ? 'load' : 'dry-run')
 
-          response = ::Gitlab::HTTP.post(
-            cube_server_url(load_data ? 'load' : 'dry-run'),
-            allow_local_requests: true,
-            headers: cube_security_headers,
-            body: { query: params["query"], "queryType": params["queryType"] }.to_json
-          )
+          response = ::ProductAnalytics::CubeDataQueryService
+                       .new(container: project,
+                            current_user: current_user,
+                            params: params).execute
 
-          body = Gitlab::Json.parse(response.body)
+          render_response(response)
+        end
 
-          if database_exists?(body)
-            status :ok
-
-            body
-          else
-            status :not_found
-
-            not_found!('Clickhouse Database')
+        def funnel_data
+          project.product_analytics_funnels.map do |funnel|
+            {
+              name: funnel.name,
+              sql: funnel.to_sql,
+              steps: funnel.steps.map(&:step_definition)
+            }
           end
         end
 
@@ -91,6 +54,10 @@ module API
                    type: String,
                    default: 'multi',
                    desc: 'The query type. Currently only "multi" is supported.'
+          optional :include_token,
+                   type: Boolean,
+                   default: false,
+                   desc: 'Whether to include the access token in the response. (Only required for funnel generation.)'
         end
       end
 
@@ -115,28 +82,25 @@ module API
           requires :project_id, type: Integer, desc: 'ID of the project to get meta data'
         end
         post ':project_id/product_analytics/request/meta' do
-          check_access_rights!
+          params = declared_params(include_missing: false).merge(path: 'meta')
 
-          response = ::Gitlab::HTTP.get(
-            cube_server_url('meta'),
-            allow_local_requests: true,
-            headers: cube_security_headers
-          )
-
-          status :ok
-          Gitlab::Json.parse(response.body)
+          response = ::ProductAnalytics::CubeDataQueryService.new(container: project,
+                                                                  current_user: current_user,
+                                                                  params: params).execute
+          render_response(response)
         end
 
         desc 'Get a list of defined funnels for a project'
         get ':project_id/product_analytics/funnels' do
-          check_access_rights!
+          response = ::ProductAnalytics::CubeDataQueryService.new(container: project,
+                                                                  current_user: current_user,
+                                                                  params: { path: 'funnels' })
+                                                             .cannot_query_data?
 
-          project.product_analytics_funnels.map do |funnel|
-            {
-              name: funnel.name,
-              sql: funnel.to_sql,
-              steps: funnel.steps.map(&:step_definition)
-            }
+          if response
+            render_response(response)
+          else
+            funnel_data
           end
         end
       end
--- ee/spec/requests/api/analytics/product_analytics_spec.rb
+++ ee/spec/requests/api/analytics/product_analytics_spec.rb
@@ -6,52 +6,10 @@ RSpec.describe API::Analytics::ProductAnalytics, feature_category: :product_anal
   let_it_be(:project) { create(:project, :with_product_analytics_funnel) }
 
   let(:current_user) { project.owner }
-  let(:cube_api_load_url) { "http://cube.dev/cubejs-api/v1/load" }
-  let(:cube_api_dry_run_url) { "http://cube.dev/cubejs-api/v1/dry-run" }
-  let(:cube_api_meta_url) { "http://cube.dev/cubejs-api/v1/meta" }
-  let(:query) { { query: { measures: ['TrackedEvents.count'] }, 'queryType': 'multi' } }
 
-  let(:request_meta) { post api("/projects/#{project.id}/product_analytics/request/meta", current_user) }
-
-  shared_examples 'a not found error' do
-    it 'load returns a 404' do
-      request_load(false)
-
-      expect(response).to have_gitlab_http_status(:not_found)
-    end
-
-    it 'dry-run returns a 404' do
-      request_load(true)
-
-      expect(response).to have_gitlab_http_status(:not_found)
-    end
-
-    it 'meta returns a 404' do
-      request_meta
-
-      expect(response).to have_gitlab_http_status(:not_found)
-    end
-  end
-
-  shared_examples 'does basics of a cube query' do |is_dry_run: false|
-    context 'when feature flag is disabled' do
-      before do
-        stub_feature_flags(product_analytics_internal_preview: false)
-      end
-
-      it 'returns a 404' do
-        request_load(is_dry_run)
-
-        expect(response).to have_gitlab_http_status(:not_found)
-      end
-    end
-
-    context 'when feature is unlicensed' do
-      before do
-        stub_licensed_features(product_analytics: false)
-      end
-
-      it_behaves_like 'a not found error'
+  shared_examples_for 'well behaved cube query' do |options = { stub_service: true }|
+    before do
+      stub_cube_proxy_setup
     end
 
     context 'when current user has guest project access' do
@@ -59,10 +17,11 @@ RSpec.describe API::Analytics::ProductAnalytics, feature_category: :product_anal
 
       before do
         project.add_guest(current_user)
+        stub_cube_data_service_unauthorized if options[:stub_service]
       end
 
       it 'returns an unauthorized error' do
-        request_load(is_dry_run)
+        request
 
         expect(response).to have_gitlab_http_status(:unauthorized)
       end
@@ -73,201 +32,48 @@ RSpec.describe API::Analytics::ProductAnalytics, feature_category: :product_anal
 
       before do
         project.add_developer(current_user)
+        stub_cube_data_service_success if options[:stub_service]
       end
 
       it 'returns a 200' do
-        request_load(is_dry_run)
-
-        expect(response).to have_gitlab_http_status(:ok)
-      end
-
-      context 'when query param is missing' do
-        let(:query) { { 'queryType': 'multi' } }
-
-        it 'returns a 400' do
-          request_load(is_dry_run)
-
-          expect(response).to have_gitlab_http_status(:bad_request)
-        end
-      end
-
-      context 'when a query param is unsupported' do
-        let(:query) { { query: { measures: ['TrackedEvents.count'] }, 'queryType': 'multi', 'badParam': 1 } }
-
-        it 'ignores the unsupported param' do
-          request_load(is_dry_run)
-
-          expect(WebMock).to have_requested(:post, is_dry_run ? cube_api_dry_run_url : cube_api_load_url).with(
-            body: { query: { measures: ['TrackedEvents.count'] }, 'queryType': 'multi' }
-          )
-
-          expect(response).to have_gitlab_http_status(:ok)
-        end
-      end
-    end
-
-    context 'when cube_api_base_url application setting is not set' do
-      before do
-        stub_ee_application_setting(cube_api_base_url: nil)
-      end
-
-      it_behaves_like 'a not found error'
-    end
-
-    context 'when cube_api_key application setting is not set' do
-      before do
-        stub_ee_application_setting(cube_api_key: nil)
-      end
-
-      it_behaves_like 'a not found error'
-    end
-
-    context 'when enable_product_analytics application setting is false' do
-      before do
-        stub_ee_application_setting(product_analytics_enabled: false)
-      end
-
-      it 'returns a 404' do
-        request_load(is_dry_run)
-
-        expect(response).to have_gitlab_http_status(:not_found)
-      end
-    end
-  end
-
-  shared_examples 'no resource access token is generated' do
-    it 'does not generate any project access tokens' do
-      expect(::ResourceAccessTokens::CreateService).not_to receive(:new)
-      request_load(false)
-    end
-  end
-
-  shared_examples 'a resource access token is generated' do
-    it 'generates a project access tokens' do
-      expect(::ResourceAccessTokens::CreateService).to receive(:new).once
-      request_load(false)
-    end
-  end
-
-  describe 'POST projects/:id/product_analytics/request/load' do
-    before do
-      stub_cube_proxy_setup
-    end
-
-    context 'when querying a database that does not exist' do
-      before do
-        stub_cube_load_no_db
-      end
-
-      it 'returns a 404' do
-        request_load(false)
-
-        expect(response).to have_gitlab_http_status(:not_found)
-        expect(response.parsed_body).to eq({ "message" => "404 Clickhouse Database Not Found" })
-      end
-    end
-
-    context 'when querying an existing database' do
-      before do
-        stub_cube_load
-      end
-
-      it_behaves_like 'does basics of a cube query', is_dry_run: false
-      it_behaves_like 'no resource access token is generated'
-    end
-
-    context 'when requesting a project with a resource access token' do
-      it_behaves_like 'a resource access token is generated' do
-        let(:query) { { query: { measures: ['TrackedEvents.count'] }, 'queryType': 'multi', include_token: true } }
-      end
-    end
-  end
-
-  describe 'POST projects/:id/product_analytics/request/dry-run' do
-    before do
-      stub_cube_dry_run
-      stub_cube_proxy_setup
-    end
-
-    it_behaves_like 'does basics of a cube query', is_dry_run: true
-  end
-
-  describe 'POST projects/:id/product_analytics/request/meta' do
-    before do
-      stub_cube_meta
-      stub_cube_proxy_setup
-    end
-
-    context 'when current user has guest project access' do
-      let_it_be(:current_user) { create(:user) }
-
-      before do
-        project.add_guest(current_user)
-      end
-
-      it 'returns an unauthorized error' do
-        request_meta
-
-        expect(response).to have_gitlab_http_status(:unauthorized)
-      end
-    end
-
-    context 'when current user is a project developer' do
-      let_it_be(:current_user) { create(:user) }
-
-      before do
-        project.add_developer(current_user)
-      end
-
-      it 'returns a 200' do
-        request_meta
+        request
 
         expect(response).to have_gitlab_http_status(:ok)
       end
     end
+  end
+
+  describe 'GET projects/:id/product_analytics/request/load' do
+    let(:request) do
+      post api("/projects/#{project.id}/product_analytics/request/load", current_user),
+               params: { query: { measures: ['TrackedEvents.count'] }, 'queryType': 'multi' }
+    end
+
+    it_behaves_like 'well behaved cube query'
+  end
+
+  describe 'GET projects/:id/product_analytics/request/dry-run' do
+    let(:request) do
+      post api("/projects/#{project.id}/product_analytics/request/dry-run", current_user),
+           params: { query: { measures: ['TrackedEvents.count'] }, 'queryType': 'multi' }
+    end
+
+    it_behaves_like 'well behaved cube query'
+  end
+
+  describe 'GET projects/:id/product_analytics/meta' do
+    let(:request) { post api("/projects/#{project.id}/product_analytics/request/meta", current_user) }
+
+    it_behaves_like 'well behaved cube query'
   end
 
   describe 'GET projects/:id/product_analytics/funnels' do
-    before do
-      stub_cube_meta
-      stub_cube_proxy_setup
-    end
+    let(:request) { get api("/projects/#{project.id}/product_analytics/funnels", current_user) }
 
-    context 'when current user has guest project access' do
-      let_it_be(:current_user) { create(:user) }
-
-      before do
-        project.add_guest(current_user)
-      end
-
-      it 'returns an unauthorized error' do
-        get api("/projects/#{project.id}/product_analytics/funnels", current_user)
-
-        expect(response).to have_gitlab_http_status(:unauthorized)
-      end
-    end
-
-    context 'when current user is a project developer' do
-      let_it_be(:current_user) { create(:user) }
-
-      before do
-        project.add_developer(current_user)
-      end
-
-      it 'returns a 200' do
-        get api("/projects/#{project.id}/product_analytics/funnels", current_user)
-
-        expect(response).to have_gitlab_http_status(:ok)
-      end
-    end
+    it_behaves_like 'well behaved cube query', { stub_service: false }
   end
 
   private
-
-  def request_load(is_dry_run)
-    post api("/projects/#{project.id}/product_analytics/request/#{is_dry_run ? 'dry-run' : 'load'}", current_user),
-    params: query
-  end
 
   def stub_cube_proxy_setup
     stub_licensed_features(product_analytics: true)
@@ -276,26 +82,17 @@ RSpec.describe API::Analytics::ProductAnalytics, feature_category: :product_anal
     stub_ee_application_setting(cube_api_base_url: 'http://cube.dev')
   end
 
-  def stub_cube_load
-    stub_request(:post, cube_api_load_url)
-      .to_return(status: 201, body: "{}", headers: {})
+  def stub_cube_data_service_success
+    expect_next_instance_of(::ProductAnalytics::CubeDataQueryService) do |service|
+      expect(service).to receive(:execute).and_return(ServiceResponse.success(message: 'test success'))
+    end
   end
 
-  def stub_cube_load_no_db
-    msg = '{ "error": "Error: Code: 81. DB::Exception: Database gitlab_project_12 doesn\'t exist.' \
-      '(UNKNOWN_DATABASE) (version 22.10.2.11 (official build))\n" }'
-
-    stub_request(:post, cube_api_load_url)
-      .to_return(status: 400, body: msg, headers: {})
-  end
-
-  def stub_cube_dry_run
-    stub_request(:post, cube_api_dry_run_url)
-      .to_return(status: 201, body: "{}", headers: {})
-  end
-
-  def stub_cube_meta
-    stub_request(:get, cube_api_meta_url)
-      .to_return(status: 201, body: "{}", headers: {})
+  def stub_cube_data_service_unauthorized
+    expect_next_instance_of(::ProductAnalytics::CubeDataQueryService) do |service|
+      expect(service).to receive(:execute).and_return(
+        ServiceResponse.error(message: 'test unauthorized', reason: :unauthorized)
+      )
+    end
   end
 end
