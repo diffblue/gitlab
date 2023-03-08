719c8e4fc369ae15d4bb8e4fc6ebb2671170063e
Paul Gascou-Vaillancourt <pgascouvaillancourt@gitlab.com>
2023-03-08 16:37:44 +0000

    Revert SVG raw imports loader change
    This reverts merge request https://gitlab.com/gitlab-org/gitlab/-/merge_requests/112836
    The change to Webpack's loaders config broke SVG imports. We are now
    seeing ES module export statements as raw string within the product.
    Let's revert the change until a solution can be found.

--- ee/app/assets/javascripts/security_orchestration/components/policy_editor/policy_selection.vue
+++ ee/app/assets/javascripts/security_orchestration/components/policy_editor/policy_selection.vue
@@ -1,6 +1,6 @@
 <script>
-import shieldCheckIllustration from '@gitlab/svgs/dist/illustrations/shield-check.svg?raw';
-import magnifyingGlassIllustration from '@gitlab/svgs/dist/illustrations/magnifying-glass.svg?raw';
+import shieldCheckIllustration from '@gitlab/svgs/dist/illustrations/shield-check.svg';
+import magnifyingGlassIllustration from '@gitlab/svgs/dist/illustrations/magnifying-glass.svg';
 import { GlCard, GlButton } from '@gitlab/ui';
 import glFeatureFlagsMixin from '~/vue_shared/mixins/gl_feature_flags_mixin';
 import SafeHtml from '~/vue_shared/directives/safe_html';
