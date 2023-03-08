edbbe56763458ddecf814bfdeaf8fcd972881e9b
Artur Fedorov <afedorov@gitlab.com>
2023-03-08 15:07:51 +0000

    This MR introduces alert component
    This MR introduces alert component
    for finished pre-scan verification
    for on-demand scans list
    
    Changelog: added
    EE: true

--- ee/spec/views/projects/on_demand_scans/index.html.haml_spec.rb
+++ ee/spec/views/projects/on_demand_scans/index.html.haml_spec.rb
@@ -2,14 +2,37 @@
 
 require 'spec_helper'
 
-RSpec.describe "projects/on_demand_scans/index", type: :view do
+RSpec.describe "projects/on_demand_scans/index", :dynamic_analysis,
+  feature_category: :dynamic_application_security_testing,
+  type: :view do
   before do
     @project = create(:project)
     @current_user = create(:user)
-    render
   end
 
   it 'renders Vue app root' do
+    render
+
     expect(rendered).to have_selector('#js-on-demand-scans')
+  end
+
+  context 'when pre scan verification is enabled' do
+    it 'render pre scan verification alert' do
+      render
+
+      expect(rendered).to render_template(partial: 'projects/on_demand_scans/_pre_scan_verification_alert')
+    end
+  end
+
+  context 'when pre scan verification is disabled' do
+    before do
+      stub_feature_flags(dast_pre_scan_verification: false)
+    end
+
+    it 'render pre scan verification alert' do
+      render
+
+      expect(rendered).not_to render_template(partial: 'projects/on_demand_scans/_pre_scan_verification_alert')
+    end
   end
 end
