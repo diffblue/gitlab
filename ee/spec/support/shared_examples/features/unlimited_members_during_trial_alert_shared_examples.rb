5d959f90e1d648827a79d6c4023bf3c3bb0cfb3e
Ross Byrne <robyrne@gitlab.com>
2023-03-09 14:52:52 +0000

    Added unlimited members alert to members pages
    Changelog: changed
    EE: true

---
+++ ee/spec/support/shared_examples/features/unlimited_members_during_trial_alert_shared_examples.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+RSpec.shared_examples_for 'unlimited members during trial alert' do
+  include Spec::Support::Helpers::Features::InviteMembersModalHelper
+
+  it 'displays alert with Explore paid plans link' do
+    visit page_path
+
+    expect(page).to have_selector(alert_selector)
+    expect(page).to have_link(text: 'Explore paid plans', href: group_billings_path(group))
+  end
+
+  it 'does not display alert after user dismisses' do
+    visit page_path
+
+    find('[data-testid="hide-unlimited-members-during-trial-alert"]').click
+    wait_for_all_requests
+
+    expect(page).not_to have_selector(alert_selector)
+  end
+end
