2000c9a086dd05a24683a33c7421decc6fb00ea7
Adam Hegyi <ahegyi@gitlab.com>
2023-03-10 14:11:39 +0100

    Fix label stage validation for ProjectNamespace
    This change fixes the validation of Namespaces::ProjectNamespace based
    value stream stages when labels are given.

--- ee/spec/models/analytics/cycle_analytics/stage_spec.rb
+++ ee/spec/models/analytics/cycle_analytics/stage_spec.rb
@@ -9,4 +9,36 @@ RSpec.describe Analytics::CycleAnalytics::Stage, feature_category: :value_stream
     let_it_be(:group_label) { create(:group_label, group: parent) }
     let_it_be(:parent_outside_of_group_label_scope) { create(:group) }
   end
+
+  context 'when Namespaces::ProjectNamespace record is given as namespace' do
+    let_it_be(:group) { create(:group) }
+    let_it_be(:project) { create(:project, group: group).reload }
+
+    let_it_be(:project_label) { create(:label, project: project) }
+    let_it_be(:group_label) { create(:group_label, group: group) }
+
+    let(:params) do
+      {
+        name: 'My Stage',
+        namespace: project.project_namespace,
+        start_event_identifier: :issue_label_added,
+        start_event_label_id: group_label.id,
+        end_event_identifier: :issue_closed
+      }
+    end
+
+    subject(:stage) { described_class.new(params) }
+
+    it 'is valid' do
+      expect(stage).to be_valid
+    end
+
+    context 'when project label is given' do
+      it 'is valid' do
+        params[:start_event_label_id] = project_label.id
+
+        expect(stage).to be_valid
+      end
+    end
+  end
 end
