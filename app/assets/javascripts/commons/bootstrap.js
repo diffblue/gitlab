4af0c3e6a1335ee4aa1b354b408f25877c601f8c
Mark Florian <mflorian@gitlab.com>
2023-03-10 23:54:06 +0000

    Extract Bootstrap modal script from main bundle
    The only place that appears to rely on it is in the user avatar cropper
    in the Edit Profile page.
    
    Nothing else appears to rely on the instantiation methods in
    https://github.com/twbs/bootstrap/blob/v4.6.2/js/src/modal.js.

--- app/assets/javascripts/commons/bootstrap.js
+++ app/assets/javascripts/commons/bootstrap.js
@@ -4,7 +4,6 @@ import $ from 'jquery';
 import 'bootstrap/js/dist/alert';
 import 'bootstrap/js/dist/button';
 import 'bootstrap/js/dist/collapse';
-import 'bootstrap/js/dist/modal';
 import 'bootstrap/js/dist/dropdown';
 import 'bootstrap/js/dist/tab';
 
