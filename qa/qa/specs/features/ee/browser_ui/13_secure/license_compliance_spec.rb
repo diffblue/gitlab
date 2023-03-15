b1fbbbcdd241bc6aa561440d9883d3675c06b68b
Adam Cohen <acohen@gitlab.com>
2023-03-15 06:08:37 +0000

    Add e2e tests for license scanning sbom files

--- qa/qa/specs/features/ee/browser_ui/13_secure/license_compliance_spec.rb
+++ qa/qa/specs/features/ee/browser_ui/13_secure/license_compliance_spec.rb
@@ -30,92 +30,164 @@ module QA
         end
       end
 
-      context 'License Management' do
-        approved_license_name = 'MIT License'
-        denied_license_name = 'Apache License 2.0'
-
-        before(:context) do
-          @runner = Resource::ProjectRunner.fabricate_via_api! do |runner|
-            runner.project = @project
-            runner.name = "runner-for-#{@project.name}"
-            runner.tags = ['secure_license']
-          end
-          # Push fixture to generate Secure reports
-          Resource::Repository::ProjectPush.fabricate! do |project_push|
-            project_push.project = @project
-            project_push.files = [
-              {
-                name: '.gitlab-ci.yml',
-                content: File.read(
-                  File.join(EE::Runtime::Path.fixtures_path, 'secure_license_files', '.gitlab-ci.yml')
-                )
-              },
-              {
-                name: 'gl-license-scanning-report.json',
-                content: File.read(
-                  File.join(
-                    EE::Runtime::Path.fixtures_path,
-                    'secure_premade_reports',
-                    'gl-license-scanning-report.json'
-                  )
-                )
-              }
-            ]
-            project_push.commit_message = 'Create Secure compatible application to serve premade reports'
-          end
-          Flow::Login.sign_in_unless_signed_in
-          @project.visit!
-          Flow::Pipeline.wait_for_latest_pipeline(status: 'passed')
-          @project.visit!
-          Page::Project::Menu.perform(&:click_on_license_compliance)
-          EE::Page::Project::Secure::LicenseCompliance.perform do |license_compliance|
-            license_compliance.open_tab
-            license_compliance.approve_license(approved_license_name)
-            license_compliance.deny_license(denied_license_name)
-          end
-        end
-
-        before do
-          Flow::Login.sign_in_unless_signed_in
-          @project.visit!
-          Page::Project::Menu.perform(&:click_on_license_compliance)
-        end
-
-        after(:context) do
-          @runner&.remove_via_api!
-        end
-
+      shared_examples 'approve license' do |approved_license_name, testcase|
         it 'can approve a license in the settings page',
-          testcase: 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/348078' do
+          testcase: testcase do
           EE::Page::Project::Secure::LicenseCompliance.perform do |license_compliance|
             license_compliance.open_tab
             expect(license_compliance).to have_approved_license(approved_license_name)
           end
         end
+      end
 
+      shared_examples 'deny license' do |denied_license_name, testcase|
         it 'can deny a license in the settings page',
-          testcase: 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/348077' do
+          testcase: testcase do
           EE::Page::Project::Secure::LicenseCompliance.perform do |license_compliance|
             license_compliance.open_tab
             expect(license_compliance).to have_denied_license(denied_license_name)
           end
         end
+      end
 
-        describe 'Pipeline Licence tab', only: { subdomain: %i[staging production pre staging-canary] } do
-          it 'can approve and deny licenses in the pipeline',
-            testcase: 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/348079' do
-            @project.visit!
-            Flow::Pipeline.visit_latest_pipeline
+      shared_examples 'pipeline license display' do |testcase|
+        it 'can approve and deny licenses in the pipeline',
+          testcase: testcase do
+          @project.visit!
+          Flow::Pipeline.visit_latest_pipeline
 
-            Page::Project::Pipeline::Show.perform do |pipeline|
-              pipeline.click_on_licenses
+          Page::Project::Pipeline::Show.perform do |pipeline|
+            pipeline.click_on_licenses
 
-              aggregate_failures do
-                expect(pipeline).to have_approved_license(approved_license_name)
-                expect(pipeline).to have_denied_license(denied_license_name)
-              end
+            aggregate_failures do
+              expect(pipeline).to have_approved_license(approved_license_name)
+              expect(pipeline).to have_denied_license(denied_license_name)
             end
           end
+        end
+      end
+
+      context 'when a license management report exists' do
+        context 'License Management' do
+          approved_license_name = 'MIT License'
+          denied_license_name = 'Apache License 2.0'
+
+          before(:context) do
+            @runner = Resource::ProjectRunner.fabricate_via_api! do |runner|
+              runner.project = @project
+              runner.name = "runner-for-#{@project.name}"
+              runner.tags = ['secure_license']
+            end
+            # Push fixture to generate Secure reports
+            Resource::Repository::ProjectPush.fabricate! do |project_push|
+              project_push.project = @project
+              project_push.files = [
+                {
+                  name: '.gitlab-ci.yml',
+                  content: File.read(
+                    File.join(EE::Runtime::Path.fixtures_path, 'secure_license_files', '.gitlab-ci.yml')
+                  )
+                },
+                {
+                  name: 'gl-license-scanning-report.json',
+                  content: File.read(
+                    File.join(
+                      EE::Runtime::Path.fixtures_path,
+                      'secure_premade_reports',
+                      'gl-license-scanning-report.json'
+                    )
+                  )
+                }
+              ]
+              project_push.commit_message = 'Create Secure compatible application to serve premade reports'
+            end
+
+            login_and_set_license_state(approved_license_name, denied_license_name)
+          end
+
+          before do
+            Flow::Login.sign_in_unless_signed_in
+            @project.visit!
+            Page::Project::Menu.perform(&:click_on_license_compliance)
+          end
+
+          after(:context) do
+            @runner&.remove_via_api!
+          end
+
+          it_behaves_like 'approve license', approved_license_name, 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/348078'
+          it_behaves_like 'deny license', denied_license_name, 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/348077'
+
+          describe 'Pipeline Licence tab', only: { subdomain: %i[staging production pre staging-canary] } do
+            it_behaves_like 'pipeline license display', 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/348079'
+          end
+        end
+      end
+
+      # this test relies on the pm_* db tables being populated, so it can only run in live environments
+      describe 'when a CycloneDX SBOM file exists', only: { subdomain: :staging } do
+        before(:all) do
+          @project = Resource::Project.fabricate_via_api! do |project|
+            project.name = Runtime::Env.auto_devops_project_name || 'project-with-secure'
+            project.description = 'Project with Secure'
+          end
+          Runtime::Feature.enable(:license_scanning_sbom_scanner, @project)
+        end
+
+        after(:all) do
+          Runtime::Feature.disable(:license_scanning_sbom_scanner, @project)
+          @project&.remove_via_api! if @project
+        end
+
+        context 'License Management' do
+          approved_license_name = 'ISC'
+          denied_license_name = 'Apache-2.0'
+
+          before(:context) do
+            @runner = Resource::ProjectRunner.fabricate_via_api! do |runner|
+              runner.project = @project
+              runner.name = "runner-for-#{@project.name}"
+              runner.tags = ['secure_sbom']
+            end
+
+            Resource::Repository::Commit.fabricate_via_api! do |resource|
+              resource.project = @project
+              resource.commit_message = 'Create sbom file'
+              resource.add_directory(Pathname.new(File.join(EE::Runtime::Path.fixtures_path, 'secure_sbom_files')))
+            end
+
+            login_and_set_license_state(approved_license_name, denied_license_name)
+          end
+
+          before do
+            Flow::Login.sign_in_unless_signed_in
+            @project.visit!
+            Page::Project::Menu.perform(&:click_on_license_compliance)
+          end
+
+          after(:context) do
+            @runner&.remove_via_api!
+          end
+
+          it_behaves_like 'approve license', approved_license_name, 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/393921'
+          it_behaves_like 'deny license', denied_license_name, 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/393922'
+
+          describe 'Pipeline Licence tab', only: { subdomain: %i[staging production pre staging-canary] } do
+            it_behaves_like 'pipeline license display', 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/393923'
+          end
+        end
+      end
+
+      def login_and_set_license_state(approved_license_name, denied_license_name)
+        Flow::Login.sign_in_unless_signed_in
+        @project.visit!
+        Flow::Pipeline.wait_for_latest_pipeline(status: 'passed')
+        @project.visit!
+        Page::Project::Menu.perform(&:click_on_license_compliance)
+        EE::Page::Project::Secure::LicenseCompliance.perform do |license_compliance|
+          license_compliance.open_tab
+          license_compliance.approve_license(approved_license_name)
+          license_compliance.deny_license(denied_license_name)
         end
       end
     end
--- qa/qa/specs/features/ee/browser_ui/13_secure/merge_request_license_widget_spec.rb
+++ qa/qa/specs/features/ee/browser_ui/13_secure/merge_request_license_widget_spec.rb
@@ -5,124 +5,258 @@ module QA
     name: 'license_scanning_sbom_scanner'
   } do
     describe 'License merge request widget' do
-      let(:approved_license_name) { "MIT License" }
-      let(:denied_license_name) { "zlib License" }
-      let(:executor) { "qa-runner-#{Time.now.to_i}" }
+      describe 'when a license scanning report exists' do
+        let(:approved_license_name) { "MIT License" }
+        let(:denied_license_name) { "zlib License" }
+        let(:executor) { "qa-runner-#{Time.now.to_i}" }
 
-      after do
-        @runner.remove_via_api!
-      end
-
-      before do
-        Flow::Login.sign_in
-
-        @project = Resource::Project.fabricate_via_api! do |project|
-          project.name = 'license-widget-project'
-          project.description = 'License widget test'
+        after do
+          @runner.remove_via_api!
         end
 
-        @runner = Resource::ProjectRunner.fabricate! do |runner|
-          runner.project = @project
-          runner.name = executor
-          runner.tags = ['secure_license']
-        end
+        before do
+          Flow::Login.sign_in
 
-        Resource::Repository::Commit.fabricate_via_api! do |resource|
-          resource.project = @project
-          resource.commit_message = 'Create license file'
-          resource.add_directory(Pathname.new(File.join(EE::Runtime::Path.fixtures_path, 'secure_license_files')))
-        end
-
-        @project.visit!
-        Flow::Pipeline.wait_for_latest_pipeline(status: 'passed', wait: 180)
-
-        @merge_request = Resource::MergeRequest.fabricate_via_api! do |mr|
-          mr.project = @project
-          mr.source_branch = 'license-management-mr'
-          mr.target_branch = @project.default_branch
-          mr.file_name = 'gl-license-scanning-report.json'
-          mr.file_content =
-            <<~FILE_UPDATE
-            {
-              "version": "2.1",
-              "licenses": [
-                {
-                  "id": "Apache-2.0",
-                  "name": "Apache License 2.0",
-                  "url": "http://www.apache.org/licenses/LICENSE-2.0.html"
-                },
-                {
-                  "id": "MIT",
-                  "name": "MIT License",
-                  "url": "https://opensource.org/licenses/MIT"
-                },
-                {
-                  "id": "Zlib",
-                  "name": "zlib License",
-                  "url": "https://opensource.org/licenses/Zlib"
-                }
-              ],
-              "dependencies": [
-                {
-                  "name": "actioncable",
-                  "version": "6.0.3.3",
-                  "package_manager": "bundler",
-                  "path": "Gemfile.lock",
-                  "licenses": ["MIT"]
-                },
-                {
-                  "name": "test_package",
-                  "version": "0.1.0",
-                  "package_manager": "bundler",
-                  "path": "Gemfile.lock",
-                  "licenses": ["Apache-2.0"]
-                },
-                {
-                  "name": "zlib",
-                  "version": "1.2.11",
-                  "package_manager": "bundler",
-                  "path": "Gemfile.lock",
-                  "licenses": ["Zlib"]
-                }
-              ]
-            }
-            FILE_UPDATE
-          mr.target_new_branch = false
-          mr.update_existing_file = true
-        end
-
-        @project.visit!
-        Flow::Pipeline.wait_for_latest_pipeline(status: 'passed', wait: 180)
-      end
-
-      it 'manage licenses from the merge request',
-         testcase: 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/348039' do
-        @merge_request.visit!
-
-        Page::MergeRequest::Show.perform do |show|
-          # Give time for the runner to complete pipeline
-          show.has_pipeline_status?('passed')
-
-          Support::Retrier.retry_until(max_attempts: 5, sleep_interval: 5) do
-            show.wait_for_license_compliance_report
+          @project = Resource::Project.fabricate_via_api! do |project|
+            project.name = 'license-widget-project'
+            project.description = 'License widget test'
           end
 
-          show.click_manage_licenses_button
+          @runner = Resource::ProjectRunner.fabricate! do |runner|
+            runner.project = @project
+            runner.name = executor
+            runner.tags = ['secure_license']
+          end
+
+          Resource::Repository::Commit.fabricate_via_api! do |resource|
+            resource.project = @project
+            resource.commit_message = 'Create license file'
+            resource.add_directory(Pathname.new(File.join(EE::Runtime::Path.fixtures_path, 'secure_license_files')))
+          end
+
+          @project.visit!
+          Flow::Pipeline.wait_for_latest_pipeline(status: 'passed', wait: 180)
+
+          @merge_request = Resource::MergeRequest.fabricate_via_api! do |mr|
+            mr.project = @project
+            mr.source_branch = 'license-management-mr'
+            mr.target_branch = @project.default_branch
+            mr.file_name = 'gl-license-scanning-report.json'
+            mr.file_content =
+              <<~FILE_UPDATE
+              {
+                "version": "2.1",
+                "licenses": [
+                  {
+                    "id": "Apache-2.0",
+                    "name": "Apache License 2.0",
+                    "url": "http://www.apache.org/licenses/LICENSE-2.0.html"
+                  },
+                  {
+                    "id": "MIT",
+                    "name": "MIT License",
+                    "url": "https://opensource.org/licenses/MIT"
+                  },
+                  {
+                    "id": "Zlib",
+                    "name": "zlib License",
+                    "url": "https://opensource.org/licenses/Zlib"
+                  }
+                ],
+                "dependencies": [
+                  {
+                    "name": "actioncable",
+                    "version": "6.0.3.3",
+                    "package_manager": "bundler",
+                    "path": "Gemfile.lock",
+                    "licenses": ["MIT"]
+                  },
+                  {
+                    "name": "test_package",
+                    "version": "0.1.0",
+                    "package_manager": "bundler",
+                    "path": "Gemfile.lock",
+                    "licenses": ["Apache-2.0"]
+                  },
+                  {
+                    "name": "zlib",
+                    "version": "1.2.11",
+                    "package_manager": "bundler",
+                    "path": "Gemfile.lock",
+                    "licenses": ["Zlib"]
+                  }
+                ]
+              }
+              FILE_UPDATE
+            mr.target_new_branch = false
+            mr.update_existing_file = true
+          end
+
+          @project.visit!
+          Flow::Pipeline.wait_for_latest_pipeline(status: 'passed', wait: 180)
         end
 
-        EE::Page::Project::Secure::LicenseCompliance.perform do |license_compliance|
-          license_compliance.open_tab
-          license_compliance.approve_license(approved_license_name)
-          license_compliance.deny_license(denied_license_name)
+        it 'manage licenses from the merge request',
+           testcase: 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/348039' do
+          @merge_request.visit!
+
+          Page::MergeRequest::Show.perform do |show|
+            # Give time for the runner to complete pipeline
+            show.has_pipeline_status?('passed')
+
+            Support::Retrier.retry_until(max_attempts: 5, sleep_interval: 5) do
+              show.wait_for_license_compliance_report
+            end
+
+            show.click_manage_licenses_button
+          end
+
+          EE::Page::Project::Secure::LicenseCompliance.perform do |license_compliance|
+            license_compliance.open_tab
+            license_compliance.approve_license(approved_license_name)
+            license_compliance.deny_license(denied_license_name)
+          end
+
+          @merge_request.visit!
+
+          Page::MergeRequest::Show.perform do |show|
+            show.wait_for_license_compliance_report
+            show.expand_license_report
+            expect(show).to have_approved_license approved_license_name
+            expect(show).to have_denied_license denied_license_name
+          end
+        end
+      end
+
+      describe 'when a CycloneDX SBOM file exists', only: { subdomain: :staging } do
+        let(:approved_license_name) { "ISC" }
+        let(:denied_license_name) { "Apache-2.0" }
+        let(:executor) { "qa-runner-#{Time.now.to_i}" }
+
+        after do
+          @runner.remove_via_api!
         end
 
-        @merge_request.visit!
+        before do
+          Flow::Login.sign_in
 
-        Page::MergeRequest::Show.perform do |show|
-          show.wait_for_license_compliance_report
-          show.expand_license_report
-          expect(show).to have_approved_license approved_license_name
-          expect(show).to have_denied_license denied_license_name
+          @project = Resource::Project.fabricate_via_api! do |project|
+            project.name = 'license-widget-project'
+            project.description = 'License widget test'
+          end
+
+          @runner = Resource::ProjectRunner.fabricate! do |runner|
+            runner.project = @project
+            runner.name = executor
+            runner.tags = ['secure_sbom']
+          end
+
+          Resource::Repository::Commit.fabricate_via_api! do |resource|
+            resource.project = @project
+            resource.commit_message = 'Create sbom file'
+            resource.add_directory(Pathname.new(File.join(EE::Runtime::Path.fixtures_path, 'secure_sbom_files')))
+          end
+
+          @project.visit!
+          Flow::Pipeline.wait_for_latest_pipeline(status: 'passed', wait: 180)
+
+          @merge_request = Resource::MergeRequest.fabricate_via_api! do |mr|
+            mr.project = @project
+            mr.source_branch = 'license-management-mr'
+            mr.target_branch = @project.default_branch
+            mr.file_name = 'gl-sbom-npm-npm.cdx.json'
+            mr.file_content =
+              <<~FILE_UPDATE
+              {
+                "bomFormat": "CycloneDX",
+                "specVersion": "1.4",
+                "serialNumber": "urn:uuid:aec33827-20ae-40d0-ae83-18ee846364d2",
+                "version": 1,
+                "metadata": {
+                  "timestamp": "2022-02-23T08:01:37Z",
+                  "tools": [
+                    {
+                      "vendor": "GitLab",
+                      "name": "Gemnasium",
+                      "version": "2.34.0"
+                    }
+                  ],
+                  "authors": [
+                    {
+                      "name": "GitLab",
+                      "email": "support@gitlab.com"
+                    }
+                  ],
+                  "properties": [
+                    {
+                      "name": "gitlab:dependency_scanning:input_file",
+                      "value": "package-lock.json"
+                    },
+                    {
+                      "name": "gitlab:dependency_scanning:package_manager",
+                      "value": "npm"
+                    }
+                  ]
+                },
+                "components": [
+                  {
+                    "name": "dragselect",
+                    "version": "1.3.6",
+                    "purl": "pkg:npm/dragselect@1.3.6",
+                    "type": "library"
+                  },
+                  {
+                    "name": "@polkadot/rpc-augment",
+                    "version": "8.4.1",
+                    "purl": "pkg:npm/@polkadot/rpc-augment@8.4.1",
+                    "type": "library"
+                  },
+                  {
+                    "name": "rblx-dev-packager",
+                    "version": "1.0.43",
+                    "purl": "pkg:npm/rblx-dev-packager@1.0.43",
+                    "type": "library"
+                  }
+                ]
+              }
+              FILE_UPDATE
+            mr.target_new_branch = false
+            mr.update_existing_file = true
+          end
+
+          @project.visit!
+          Flow::Pipeline.wait_for_latest_pipeline(status: 'passed', wait: 180)
+        end
+
+        it 'manage licenses from the merge request',
+           testcase: 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/394713' do
+          @merge_request.visit!
+
+          Page::MergeRequest::Show.perform do |show|
+            # Give time for the runner to complete pipeline
+            show.has_pipeline_status?('passed')
+
+            Support::Retrier.retry_until(max_attempts: 5, sleep_interval: 5) do
+              show.wait_for_license_compliance_report
+            end
+
+            show.click_manage_licenses_button
+          end
+
+          EE::Page::Project::Secure::LicenseCompliance.perform do |license_compliance|
+            license_compliance.open_tab
+            license_compliance.approve_license(approved_license_name)
+            license_compliance.deny_license(denied_license_name)
+          end
+
+          @merge_request.visit!
+
+          Page::MergeRequest::Show.perform do |show|
+            show.wait_for_license_compliance_report
+            show.expand_license_report
+            expect(show).to have_approved_license approved_license_name
+            expect(show).to have_denied_license denied_license_name
+          end
         end
       end
     end
