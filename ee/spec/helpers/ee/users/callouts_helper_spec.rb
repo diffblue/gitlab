48120a1e34ed283eed7cfb2d6ded549f764114b1
Luke Duncalfe <lduncalfe@eml.cc>
2023-03-16 15:26:41 +1300

    Fix webhook failure callout sticky from some pages
    Changelog: fixed

--- ee/spec/helpers/ee/users/callouts_helper_spec.rb
+++ ee/spec/helpers/ee/users/callouts_helper_spec.rb
@@ -371,69 +371,11 @@ RSpec.describe EE::Users::CalloutsHelper do
     end
   end
 
-  describe '#web_hook_disabled_dismissed?' do
+  describe '#web_hook_disabled_dismissed?', feature_category: :integrations do
     let_it_be(:user, refind: true) { create(:user) }
 
     before do
       allow(helper).to receive(:current_user).and_return(user)
-    end
-
-    context 'without a project or a group' do
-      it 'is false' do
-        expect(helper).not_to be_web_hook_disabled_dismissed(nil)
-      end
-    end
-
-    shared_examples 'web_hook_disabled shared examples' do
-      context 'when the web-hook failure callout has never been dismissed' do
-        it 'is false' do
-          expect(helper).not_to be_web_hook_disabled_dismissed(container)
-        end
-      end
-
-      context 'when the web-hook failure callout has been dismissed', :freeze_time, :clean_gitlab_redis_shared_state do
-        before do
-          create(factory,
-            feature_name: Users::CalloutsHelper::WEB_HOOK_DISABLED,
-            user: user,
-            dismissed_at: 1.week.ago,
-            container_key => container)
-        end
-
-        it 'is true' do
-          expect(helper).to be_web_hook_disabled_dismissed(container)
-        end
-
-        context 'when there was an older failure' do
-          before do
-            Gitlab::Redis::SharedState.with { |r| r.set(key, 1.month.ago.iso8601) }
-          end
-
-          it 'is true' do
-            expect(helper).to be_web_hook_disabled_dismissed(container)
-          end
-        end
-
-        context 'when there has been a more recent failure' do
-          before do
-            Gitlab::Redis::SharedState.with { |r| r.set(key, 1.day.ago.iso8601) }
-          end
-
-          it 'is false' do
-            expect(helper).not_to be_web_hook_disabled_dismissed(container)
-          end
-        end
-      end
-    end
-
-    context 'with a project' do
-      let_it_be(:project) { create(:project) }
-      let(:factory) { :project_callout }
-      let(:container_key) { :project }
-      let(:container) { project }
-      let(:key) { "web_hooks:last_failure:project-#{project.id}" }
-
-      include_examples 'web_hook_disabled shared examples'
     end
 
     context 'with a group' do
@@ -443,7 +385,7 @@ RSpec.describe EE::Users::CalloutsHelper do
       let(:container) { group }
       let(:key) { "web_hooks:last_failure:group-#{group.id}" }
 
-      include_examples 'web_hook_disabled shared examples'
+      include_examples 'CalloutsHelper#web_hook_disabled_dismissed shared examples'
     end
   end
 end
