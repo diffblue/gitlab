25f5089dd9d8be3ddd6dfec89c6656c65debdbbc
Tomas Bulva <tbulva@gitlab.com>
2023-03-20 16:30:35 +0000

    Remove the new_header_search FF
    Changelog: changed

--- ee/spec/features/search/elastic/snippet_search_spec.rb
+++ ee/spec/features/search/elastic/snippet_search_spec.rb
@@ -6,14 +6,8 @@ RSpec.describe 'Snippet elastic search', :js, :elastic_clean, :aggregate_failure
   let(:public_project) { create(:project, :public) }
   let(:authorized_user) { create(:user) }
   let(:authorized_project) { create(:project, namespace: authorized_user.namespace) }
-  let(:new_header_search) { false }
 
   before do
-    # This feature is diabled by default in spec_helper.rb.
-    # We missed a feature breaking bug, so to prevent this regression, testing both scenarios for this spec.
-    # This can be removed as part of closing https://gitlab.com/gitlab-org/gitlab/-/issues/339348.
-    stub_feature_flags(new_header_search: new_header_search)
-
     stub_ee_application_setting(elasticsearch_search: true, elasticsearch_indexing: true)
 
     authorized_project.add_maintainer(authorized_user)
@@ -153,27 +147,7 @@ RSpec.describe 'Snippet elastic search', :js, :elastic_clean, :aggregate_failure
     end
   end
 
-  context 'when :new_header_search is false' do
-    context 'when searching titles' do
-      before do
-        submit_search('snippet')
-      end
-
-      it_behaves_like 'expected snippet search results'
-    end
-
-    context 'when searching descriptions' do
-      before do
-        submit_search('snippet description')
-      end
-
-      it_behaves_like 'expected snippet search results'
-    end
-  end
-
-  context 'when :new_header_search is true' do
-    let(:new_header_search) { true }
-
+  context 'when header search' do
     context 'when searching titles' do
       before do
         submit_search('snippet')
