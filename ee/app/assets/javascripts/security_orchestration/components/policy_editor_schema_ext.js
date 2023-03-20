202c6008d88519a7b64b928f1673d4cb040c8f9b
Alexander Turinske <aturinske@gitlab.com>
2023-03-20 23:27:51 +0000

    Add yaml validation for scan result policies
    - gernalize current yaml validation
    - update tests
    
    Changelog: changed
    
    EE: true

--- ee/app/assets/javascripts/security_orchestration/components/policy_editor_schema_ext.js
+++ ee/app/assets/javascripts/security_orchestration/components/policy_editor_schema_ext.js
@@ -1,14 +1,5 @@
 import { registerSchema } from '~/ide/utils';
-import { NAMESPACE_TYPES } from '../constants';
-import { getSchemaUrl, getSingleScanExecutionPolicySchema } from './utils';
-import { POLICY_TYPE_COMPONENT_OPTIONS } from './constants';
-
-const SCHEMA_DICT = {
-  [POLICY_TYPE_COMPONENT_OPTIONS.scanExecution.urlParameter]: {
-    [NAMESPACE_TYPES.PROJECT]: getSingleScanExecutionPolicySchema,
-    [NAMESPACE_TYPES.GROUP]: getSingleScanExecutionPolicySchema,
-  },
-};
+import { getSchemaUrl, getSinglePolicySchema } from './utils';
 
 export class PolicySchemaExtension {
   static get extensionName() {
@@ -19,19 +10,18 @@ export class PolicySchemaExtension {
     return {
       registerSecurityPolicySchema: async (instance, options) => {
         const { namespacePath, namespaceType, policyType } = options;
-        if (SCHEMA_DICT[policyType]?.[namespaceType]) {
-          const singlePolicySchema = await SCHEMA_DICT[policyType][namespaceType]({
-            namespacePath,
-            namespaceType,
-          });
-          const modelFileName = instance.getModel().uri.path.split('/').pop();
+        const singlePolicySchema = await getSinglePolicySchema({
+          namespacePath,
+          namespaceType,
+          policyType,
+        });
+        const modelFileName = instance.getModel().uri.path.split('/').pop();
 
-          registerSchema({
-            uri: getSchemaUrl({ namespacePath, namespaceType }),
-            schema: singlePolicySchema,
-            fileMatch: [modelFileName],
-          });
-        }
+        registerSchema({
+          uri: getSchemaUrl({ namespacePath, namespaceType }),
+          schema: singlePolicySchema,
+          fileMatch: [modelFileName],
+        });
       },
     };
   }
