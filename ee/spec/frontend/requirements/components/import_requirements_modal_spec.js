1fd9eae52be5e9009aa4a79072a17bd57f759dce
Samantha Ming <sming@gitlab.com>
2023-03-20 03:39:23 +0000

    Fix spec import_requirements_modal_spec
    Remove setData and wrapper.vm usage

--- ee/spec/frontend/requirements/components/import_requirements_modal_spec.js
+++ ee/spec/frontend/requirements/components/import_requirements_modal_spec.js
@@ -1,10 +1,11 @@
 import { GlModal } from '@gitlab/ui';
 import { shallowMount } from '@vue/test-utils';
-
-import { nextTick } from 'vue';
 import ImportRequirementsModal from 'ee/requirements/components/import_requirements_modal.vue';
 
-const createComponent = ({ projectPath = 'gitLabTest' } = {}) =>
+const TEST_PROJECT_PATH = 'gitLabTest';
+const TEST_FILE = new File(['foo'], 'foo.csv');
+
+const createComponent = ({ projectPath = TEST_PROJECT_PATH } = {}) =>
   shallowMount(ImportRequirementsModal, {
     propsData: {
       projectPath,
@@ -14,60 +15,48 @@ const createComponent = ({ projectPath = 'gitLabTest' } = {}) =>
 describe('ImportRequirementsModal', () => {
   let wrapper;
 
+  const findModal = () => wrapper.findComponent(GlModal);
+  const selectFile = async (file = TEST_FILE) => {
+    const input = wrapper.find('input[type="file"]');
+    Object.defineProperty(input.element, 'files', { value: [file] });
+    return input.trigger('change');
+  };
+
   beforeEach(() => {
     wrapper = createComponent();
   });
 
-  describe('computed', () => {
-    describe('importDisabled', () => {
-      it('returns true when file is absent', () => {
-        expect(wrapper.vm.importDisabled).toBe(true);
-      });
+  describe('"Import requirements" button', () => {
+    it('disables modal button when file is absent', () => {
+      expect(findModal().attributes('ok-disabled')).toBe('true');
+    });
 
-      it('returns false when file is present', () => {
-        // setData usage is discouraged. See https://gitlab.com/groups/gitlab-org/-/epics/7330 for details
-        // eslint-disable-next-line no-restricted-syntax
-        wrapper.setData({ file: 'Some file' });
-
-        expect(wrapper.vm.importDisabled).toBe(false);
-      });
+    it('returns false when file is present', async () => {
+      await selectFile();
+      expect(findModal().attributes('ok-disabled')).toBeUndefined();
     });
   });
 
   describe('methods', () => {
     describe('handleCSVFile', () => {
       it('sets the first file selected', async () => {
-        const file = 'some file';
+        await selectFile();
+        findModal().vm.$emit('ok');
 
-        const event = {
-          target: {
-            files: [file],
-          },
-        };
-        wrapper.vm.handleCSVFile(event);
-
-        await nextTick();
-        expect(wrapper.vm.file).toBe(file);
+        expect(wrapper.emitted('import')[0][0].file).toBe(TEST_FILE);
       });
     });
   });
 
   describe('template', () => {
-    it('GlModal open click emits file and projectPath', () => {
-      const file = 'some file';
-
-      // setData usage is discouraged. See https://gitlab.com/groups/gitlab-org/-/epics/7330 for details
-      // eslint-disable-next-line no-restricted-syntax
-      wrapper.setData({
-        file,
-      });
-
-      wrapper.findComponent(GlModal).vm.$emit('ok');
+    it('GlModal open click emits file and projectPath', async () => {
+      await selectFile();
+      findModal().vm.$emit('ok');
 
       const emitted = wrapper.emitted('import')[0][0];
 
-      expect(emitted.file).toBe(file);
-      expect(emitted.projectPath).toBe(wrapper.vm.projectPath);
+      expect(emitted.file).toBe(TEST_FILE);
+      expect(emitted.projectPath).toBe(TEST_PROJECT_PATH);
     });
   });
 });
