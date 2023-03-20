c8d19426d598498da5c8907d850f3bc99df89a7b
Kamil Trzciński <ayufan@ayufan.eu>
2023-03-20 15:00:27 +0100

    Cells: Move `broadcast_messages` into `clusterwide`
    This table is already configured from `Admin Area` and are cluster-wide.
    
    This uses a work already done for `application_settings`.
    
    Changelog: changed

--- app/models/broadcast_message.rb
+++ app/models/broadcast_message.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class BroadcastMessage < ApplicationRecord
+class BroadcastMessage < MainClusterwide::ApplicationRecord
   include CacheMarkdownField
   include Sortable
 
