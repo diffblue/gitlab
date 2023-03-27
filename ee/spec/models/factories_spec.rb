81b3633185d7d96b28b58ef2b090950d3f967c83
Omar Qunsul <oqunsul@gitlab.com>
2023-03-27 06:04:07 +0000

    Fixing specs for single-db-two-connections
    Many of our specs are not considering the case when we
    have two connections pointing to the same database.
    
    Addressing: https://gitlab.com/gitlab-org/gitlab/-/issues/391646
    Changelog: other

--- ee/spec/models/factories_spec.rb
+++ ee/spec/models/factories_spec.rb
@@ -195,8 +195,7 @@ RSpec.describe 'factories', :saas, :with_license, feature_category: :tooling do
       factories_based_on_view.each do |factory|
         view = build(factory).class.table_name
         view_gitlab_schema = Gitlab::Database::GitlabSchema.table_schema(view)
-        Gitlab::Database.database_base_models.each_value.select do |base_model|
-          connection = base_model.connection
+        Gitlab::Database::EachDatabase.each_database_connection(include_shared: false) do |connection|
           next unless Gitlab::Database.gitlab_schemas_for_connection(connection).include?(view_gitlab_schema)
 
           swapout_view_for_table(view, connection: connection)
