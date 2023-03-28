3fb33785f650008bb940f782440e0c846f1cf3eb
Ammar Alakkad <am.alakkad@gmail.com>
2023-03-28 16:14:06 +0300

    Add frontend skeleton for plan limits
    It adds a placeholder for the plan limits UI, which is behind a feature
    flag. Part of https://gitlab.com/groups/gitlab-org/-/epics/9173

---
+++ ee/app/assets/javascripts/pages/admin/plan_limits/index.js
@@ -0,0 +1,4 @@
+import { initSimpleApp } from '~/helpers/init_simple_app_helper';
+import PlanLimitsApp from './components/plan_limits_app.vue';
+
+initSimpleApp('#js-plan-limits-app', PlanLimitsApp);
