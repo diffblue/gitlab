ff9490cbf273a20263c36fbf8a00f24944f852dd
Lukas Eipert <leipert@gitlab.com>
2023-03-29 10:09:41 +0200

    Project super sidebar: Reorder secure items
    This moves all old Security and Compliance menu items into the new
    Secure Menu and reorders them according to:
    https://gitlab.com/gitlab-org/gitlab/-/issues/393190

--- lib/sidebars/projects/super_sidebar_menus/secure_menu.rb
+++ lib/sidebars/projects/super_sidebar_menus/secure_menu.rb
@@ -13,6 +13,21 @@ module Sidebars
         def sprite_icon
           'shield'
         end
+
+        override :configure_menu_items
+        def configure_menu_items
+          [
+            :discover_project_security,
+            :audit_events,
+            :dashboard,
+            :vulnerability_report,
+            :on_demand_scans,
+            :scan_policies,
+            :dependency_list,
+            :license_compliance,
+            :configuration
+          ].each { |id| add_item(::Sidebars::NilMenuItem.new(item_id: id)) }
+        end
       end
     end
   end
