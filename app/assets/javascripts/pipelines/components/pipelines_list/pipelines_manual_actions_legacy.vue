dce30d421d03ba13e770bee1328b84e1f19ff6b1
Payton Burdette <pburdette@gitlab.com>
2023-03-31 03:24:35 +0000

    Lazy load pipeline actions

--- app/assets/javascripts/pipelines/components/pipelines_list/pipelines_manual_actions_legacy.vue
+++ app/assets/javascripts/pipelines/components/pipelines_list/pipelines_manual_actions_legacy.vue
@@ -1,5 +1,5 @@
 <script>
-import { GlDropdown, GlDropdownItem, GlIcon, GlLoadingIcon, GlTooltipDirective } from '@gitlab/ui';
+import { GlDropdown, GlDropdownItem, GlIcon, GlTooltipDirective } from '@gitlab/ui';
 import { createAlert } from '~/alert';
 import axios from '~/lib/utils/axios_utils';
 import { confirmAction } from '~/lib/utils/confirm_via_gl_modal/confirm_via_gl_modal';
@@ -8,10 +8,9 @@ import Tracking from '~/tracking';
 import GlCountdown from '~/vue_shared/components/gl_countdown.vue';
 import eventHub from '../../event_hub';
 import { TRACKING_CATEGORIES } from '../../constants';
-import getPipelineActionsQuery from '../../graphql/queries/get_pipeline_actions.query.graphql';
 
 export default {
-  name: 'PipelinesManualActions',
+  name: 'PipelinesManualActionsLegacy',
   directives: {
     GlTooltip: GlTooltipDirective,
   },
@@ -20,52 +19,22 @@ export default {
     GlDropdown,
     GlDropdownItem,
     GlIcon,
-    GlLoadingIcon,
   },
   mixins: [Tracking.mixin()],
-  inject: ['fullPath', 'manualActionsLimit'],
   props: {
-    iid: {
-      type: Number,
-      required: true,
-    },
-  },
-  apollo: {
     actions: {
-      query: getPipelineActionsQuery,
-      variables() {
-        return {
-          fullPath: this.fullPath,
-          iid: this.iid,
-          limit: this.manualActionsLimit,
-        };
-      },
-      skip() {
-        return !this.hasDropdownBeenShown;
-      },
-      update({ project }) {
-        return project?.pipeline?.jobs?.nodes || [];
-      },
+      type: Array,
+      required: true,
     },
   },
   data() {
     return {
       isLoading: false,
-      actions: [],
-      hasDropdownBeenShown: false,
     };
-  },
-  computed: {
-    isActionsLoading() {
-      return this.$apollo.queries.actions.loading;
-    },
-    isDropdownLimitReached() {
-      return this.actions.length === this.manualActionsLimit;
-    },
   },
   methods: {
     async onClickAction(action) {
-      if (action.scheduledAt) {
+      if (action.scheduled_at) {
         const confirmationMessage = sprintf(
           s__(
             'DelayedJobs|Are you sure you want to run %{jobName} immediately? Otherwise this job will run automatically after its timer finishes.',
@@ -86,12 +55,12 @@ export default {
        * Ideally, the component would not make an api call directly.
        * However, in order to use the eventhub and know when to
        * toggle back the `isLoading` property we'd need an ID
-       * to track the request with a watcher - since this component
+       * to track the request with a wacther - since this component
        * is rendered at least 20 times in the same page, moving the
        * api call directly here is the most performant solution
        */
       axios
-        .post(`${action.playPath}.json`)
+        .post(`${action.path}.json`)
         .then(() => {
           this.isLoading = false;
           eventHub.$emit('updateTable');
@@ -101,12 +70,12 @@ export default {
           createAlert({ message: __('An error occurred while making the request.') });
         });
     },
-    fetchActions() {
-      this.hasDropdownBeenShown = true;
+    isActionDisabled(action) {
+      if (action.playable === undefined) {
+        return false;
+      }
 
-      this.$apollo.queries.actions.refetch();
-
-      this.trackClick();
+      return !action.playable;
     },
     trackClick() {
       this.track('click_manual_actions', { label: TRACKING_CATEGORIES.table });
@@ -123,37 +92,21 @@ export default {
     right
     lazy
     icon="play"
-    @shown="fetchActions"
+    @shown="trackClick"
   >
-    <gl-dropdown-item v-if="isActionsLoading">
-      <div class="gl-display-flex">
-        <gl-loading-icon class="mr-2" />
-        <span>{{ __('Loading...') }}</span>
-      </div>
-    </gl-dropdown-item>
-
     <gl-dropdown-item
       v-for="action in actions"
-      v-else
-      :key="action.id"
-      :disabled="!action.canPlayJob"
+      :key="action.path"
+      :disabled="isActionDisabled(action)"
       @click="onClickAction(action)"
     >
-      <div class="gl-display-flex gl-justify-content-space-between gl-flex-wrap-wrap">
+      <div class="gl-display-flex gl-justify-content-space-between gl-flex-wrap">
         {{ action.name }}
-        <span v-if="action.scheduledAt">
+        <span v-if="action.scheduled_at">
           <gl-icon name="clock" />
-          <gl-countdown :end-date-string="action.scheduledAt" />
+          <gl-countdown :end-date-string="action.scheduled_at" />
         </span>
       </div>
     </gl-dropdown-item>
-
-    <template #footer>
-      <gl-dropdown-item v-if="isDropdownLimitReached">
-        <span class="gl-font-sm gl-text-gray-300!" data-testid="limit-reached-msg">
-          {{ __('Showing first 50 actions.') }}
-        </span>
-      </gl-dropdown-item>
-    </template>
   </gl-dropdown>
 </template>
