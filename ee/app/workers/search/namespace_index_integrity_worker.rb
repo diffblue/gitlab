91098f7999225d98636d937a681d1383c2e0f3ba
Terri Chu <tchu@gitlab.com>
2023-04-04 22:29:22 +0000

    Worker: adjust iterating for subgroups and projects

--- ee/app/workers/search/namespace_index_integrity_worker.rb
+++ ee/app/workers/search/namespace_index_integrity_worker.rb
@@ -13,7 +13,8 @@ module Search
     urgency :throttled
 
     LEASE_TIMEOUT = 30.minutes.freeze
-    DELAY_INTERVAL = 24.hours.freeze
+    PROJECT_DELAY_INTERVAL = 24.hours.freeze
+    NAMESPACE_DELAY_INTERVAL = 1.hour.freeze
 
     def perform(namespace_id)
       return if namespace_id.blank?
@@ -26,14 +27,22 @@ module Search
         return
       end
 
-      in_lock("#{self.class.name.underscore}/namespace/#{namespace_id}", ttl: LEASE_TIMEOUT) do
-        logger.info(structured_payload(message: 'enqueueing all projects for namespace', namespace_id: namespace_id))
+      return unless namespace.use_elasticsearch?
 
-        namespace.all_projects.each_batch do |relation|
+      in_lock("#{self.class.name.underscore}/namespace/#{namespace_id}", ttl: LEASE_TIMEOUT) do
+        logger.info(structured_payload(message: 'enqueueing all children for namespace', namespace_id: namespace_id))
+        namespace.children.each_batch do |relation|
+          relation.each do |n|
+            ::Search::NamespaceIndexIntegrityWorker.perform_in(rand(NAMESPACE_DELAY_INTERVAL).seconds, n.id)
+          end
+        end
+
+        logger.info(structured_payload(message: 'enqueueing all projects for namespace', namespace_id: namespace_id))
+        namespace.projects.each_batch do |relation|
           relation.each do |project|
             next unless project.should_check_index_integrity?
 
-            ::Search::ProjectIndexIntegrityWorker.perform_in(rand(DELAY_INTERVAL).seconds, project.id)
+            ::Search::ProjectIndexIntegrityWorker.perform_in(rand(PROJECT_DELAY_INTERVAL).seconds, project.id)
           end
         end
       end
--- ee/spec/workers/search/namespace_index_integrity_worker_spec.rb
+++ ee/spec/workers/search/namespace_index_integrity_worker_spec.rb
@@ -40,12 +40,11 @@ RSpec.describe ::Search::NamespaceIndexIntegrityWorker, feature_category: :globa
         let(:job_args) { [group.id] }
 
         it 'schedules ProjectIndexIntegrityWorker for each project with a delay' do
-          stub_const("#{described_class.name}::BATCH_SIZE", 1)
-          stub_const("#{described_class.name}::DELAY_INTERVAL", 10)
+          stub_const("#{described_class.name}::PROJECT_DELAY_INTERVAL", 5)
 
           group.all_projects.each do |p|
             expect(::Search::ProjectIndexIntegrityWorker).to receive(:perform_in).with(
-              within(10.seconds).of(10.seconds),
+              within(5.seconds).of(5.seconds),
               p.id
             ).and_call_original
           end
@@ -73,11 +72,40 @@ RSpec.describe ::Search::NamespaceIndexIntegrityWorker, feature_category: :globa
         end
       end
 
+      context 'when a namespace has sub-groups', :sidekiq_inline do
+        it 'recursively schedules itself for each child namespace with a delay', :aggregate_failures do
+          stub_const("#{described_class.name}::NAMESPACE_DELAY_INTERVAL", 10)
+
+          sg_1 = create(:group, parent: group)
+          sg_2 = create(:group, parent: sg_1)
+
+          expect(described_class).to receive(:perform_in)
+            .with(within(10.seconds).of(10.seconds), sg_1.id).and_call_original
+          expect(described_class).to receive(:perform_in)
+            .with(within(10.seconds).of(10.seconds), sg_2.id).and_call_original
+
+          worker.perform(group.id)
+        end
+      end
+
       context 'when namespace is not found' do
         it 'does nothing' do
           expect(::Search::ProjectIndexIntegrityWorker).not_to receive(:perform_in)
 
           worker.perform(non_existing_record_id)
+        end
+      end
+
+      context 'when namespace.use_elasticsearch? is false' do
+        it 'does nothing' do
+          allow_next_found_instance_of(Namespace) do |p|
+            allow(p).to receive(:use_elasticsearch?).and_return(false)
+          end
+
+          expect(::Search::ProjectIndexIntegrityWorker).not_to receive(:perform_in)
+          expect(described_class).not_to receive(:perform_in)
+
+          worker.perform(group.id)
         end
       end
     end
