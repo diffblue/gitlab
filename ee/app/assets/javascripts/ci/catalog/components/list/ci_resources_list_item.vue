bfd4946cd8c9d1779b87772d8bf5e9116cd83df7
Briley  Sandlin <bsandlin@gitlab.com>
2023-04-05 15:34:35 +0000

    Update ci_resources_list_item
    Adding structure to the list items in resources for CI Catalog

--- ee/app/assets/javascripts/ci/catalog/components/list/ci_resources_list_item.vue
+++ ee/app/assets/javascripts/ci/catalog/components/list/ci_resources_list_item.vue
@@ -1,15 +1,121 @@
 <script>
+import {
+  GlAvatar,
+  GlAvatarLink,
+  GlBadge,
+  GlButton,
+  GlIcon,
+  GlLink,
+  GlSprintf,
+  GlTooltipDirective,
+} from '@gitlab/ui';
+import { s__ } from '~/locale';
+import { formatDate, getTimeago } from '~/lib/utils/datetime_utility';
+
 export default {
+  i18n: {
+    releasedMessage: s__('CiCatalog|Released %{timeAgo} by %{author}'),
+  },
+  components: {
+    GlAvatar,
+    GlAvatarLink,
+    GlBadge,
+    GlButton,
+    GlIcon,
+    GlLink,
+    GlSprintf,
+  },
+  directives: {
+    GlTooltip: GlTooltipDirective,
+  },
   props: {
-    component: {
+    resource: {
       type: Object,
       required: true,
+    },
+  },
+  computed: {
+    authorName() {
+      return this.lastUpdate.user.name;
+    },
+    authorProfileUrl() {
+      return this.lastUpdate.user.webUrl;
+    },
+    hasFavorites() {
+      return this.resource.statistics?.favorites;
+    },
+    hasForks() {
+      return this.resource.statistics?.forks;
+    },
+    formattedDate() {
+      return formatDate(this.lastUpdate?.time);
+    },
+    formattedVersion() {
+      return `v${this.resource.latestVersion}`;
+    },
+    lastUpdate() {
+      return this.resource.lastUpdate;
+    },
+    releasedAt() {
+      return getTimeago().format(this.lastUpdate?.time);
+    },
+    resourcePath() {
+      return `${this.resource.namespace} / ${this.resource.group} / `;
     },
   },
 };
 </script>
 <template>
-  <li>
-    {{ component.id }}
+  <li
+    class="gl-display-flex gl-display-flex-wrap gl-border-b-1 gl-border-gray-100 gl-border-b-solid gl-text-gray-500 gl-py-3"
+  >
+    <gl-avatar-link :href="resource.webPath">
+      <gl-avatar
+        :entity-id="resource.id"
+        :entity-name="resource.icon"
+        :size="48"
+        shape="rect"
+        class="gl-mr-4"
+      />
+    </gl-avatar-link>
+    <div class="gl-display-flex gl-flex-direction-column gl-flex-grow-1">
+      <div class="gl-display-flex gl-flex-wrap-wrap gl-flex-grow-1 gl-gap-2">
+        <gl-button variant="link" :href="resource.webPath" class="gl-text-gray-900! gl-mr-1">
+          {{ resourcePath }} <b> {{ resource.name }}</b>
+        </gl-button>
+        <div class="gl-display-flex gl-flex-grow-1 gl-md-justify-content-space-between">
+          <gl-badge size="sm">{{ formattedVersion }}</gl-badge>
+          <span class="gl-display-flex gl-align-items-center gl-ml-5">
+            <span v-if="hasFavorites" class="gl--flex-center" data-testid="stats-favorites">
+              <gl-icon name="star" :size="14" class="gl-mr-1" />
+              <span class="gl-mr-3">{{ resource.statistics.favorites }}</span>
+            </span>
+            <span v-if="hasForks" class="gl--flex-center" data-testid="stats-forks">
+              <gl-icon name="fork" :size="14" class="gl-mr-1" />
+              <span>{{ resource.statistics.forks }}</span>
+            </span>
+          </span>
+        </div>
+      </div>
+      <div class="gl-display-flex gl-sm-flex-direction-column gl-justify-content-space-between">
+        <span class="gl-display-flex gl-flex-basis-two-thirds">{{ resource.description }}</span>
+        <div class="gl-display-flex gl-justify-content-end">
+          <span>
+            <gl-sprintf :message="$options.i18n.releasedMessage">
+              <template #timeAgo>
+                <span v-gl-tooltip.bottom :title="formattedDate">
+                  {{ releasedAt }}
+                </span>
+              </template>
+              <template #author>
+                <gl-link :href="authorProfileUrl">
+                  <span>{{ authorName }}</span>
+                </gl-link>
+              </template>
+            </gl-sprintf>
+          </span>
+        </div>
+      </div>
+    </div>
   </li>
 </template>
--- ee/spec/frontend/ci/catalog/components/list/ci_resources_list_item_spec.js
+++ ee/spec/frontend/ci/catalog/components/list/ci_resources_list_item_spec.js
@@ -1,14 +1,15 @@
+import { GlAvatar, GlAvatarLink, GlBadge, GlButton, GlLink, GlSprintf } from '@gitlab/ui';
 import { shallowMountExtended } from 'helpers/vue_test_utils_helper';
 import CiResourcesListItem from 'ee/ci/catalog/components/list/ci_resources_list_item.vue';
+import { mockCatalogResourceList } from 'ee/ci/catalog/constants';
 
-describe('CiResourcesList', () => {
+describe('CiResourcesListItem', () => {
   let wrapper;
 
+  const resource = mockCatalogResourceList[0];
   const defaultProps = {
-    component: { id: 1 },
+    resource,
   };
-
-  const findComponentId = () => wrapper.findByText(String(defaultProps.component.id));
 
   const createComponent = ({ props = {} } = {}) => {
     wrapper = shallowMountExtended(CiResourcesListItem, {
@@ -16,16 +17,96 @@ describe('CiResourcesList', () => {
         ...defaultProps,
         ...props,
       },
+      stubs: {
+        GlSprintf,
+      },
     });
   };
+
+  const findAvatar = () => wrapper.findComponent(GlAvatar);
+  const findAvatarLink = () => wrapper.findComponent(GlAvatarLink);
+  const findBadge = () => wrapper.findComponent(GlBadge);
+  const findResourceName = () => wrapper.findComponent(GlButton);
+  const findResourceDescription = () => wrapper.findByText(defaultProps.resource.description);
+  const findUserLink = () => wrapper.findComponent(GlLink);
+  const findTimeAgoMessage = () => wrapper.findComponent(GlSprintf);
+  const findFavorites = () => wrapper.findByTestId('stats-favorites');
+  const findForks = () => wrapper.findByTestId('stats-forks');
 
   describe('when mounted', () => {
     beforeEach(() => {
       createComponent();
     });
 
-    it('renders a component ID', () => {
-      expect(findComponentId().exists()).toBe(true);
+    it('renders the resource avatar', () => {
+      expect(findAvatar().exists()).toBe(true);
+    });
+
+    it('renders the resource avatar link', () => {
+      expect(findAvatarLink().attributes('href')).toBe(resource.webPath);
+    });
+
+    it('renders the resource name button', () => {
+      expect(findResourceName().exists()).toBe(true);
+      expect(findResourceName().attributes('href')).toBe(resource.webPath);
+    });
+
+    it('renders the resource version badge', () => {
+      expect(findBadge().exists()).toBe(true);
+    });
+
+    it('renders the resource description', () => {
+      expect(findResourceDescription().exists()).toBe(true);
+    });
+
+    it('renders the user link', () => {
+      expect(findUserLink().exists()).toBe(true);
+      expect(findUserLink().attributes('href')).toBe(resource.lastUpdate.user.webUrl);
+    });
+
+    it('renders the time since the resource was released', () => {
+      expect(findTimeAgoMessage().exists()).toBe(true);
+    });
+  });
+  describe('statistics', () => {
+    describe('when there are no statistics', () => {
+      beforeEach(() => {
+        createComponent({
+          props: {
+            resource: {
+              statistics: {},
+              lastUpdate: {
+                user: {
+                  name: 'username',
+                  webUrl: 'path/to/profile',
+                },
+              },
+            },
+          },
+        });
+      });
+
+      it('does not render favorites', () => {
+        expect(findFavorites().exists()).toBe(false);
+      });
+
+      it('does not render forks', () => {
+        expect(findForks().exists()).toBe(false);
+      });
+    });
+
+    describe('where there are statistics', () => {
+      beforeEach(() => {
+        createComponent();
+      });
+
+      it('does render favorites', () => {
+        expect(findFavorites().exists()).toBe(true);
+      });
+
+      it('does render forks', () => {
+        expect(findForks().exists()).toBe(true);
+      });
     });
   });
 });
