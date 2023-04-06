b3d4a6303d5deab55c7d41efa99a29157dc7cee0
Matthias Kaeppler <mkaeppler@gitlab.com>
2023-04-06 11:18:24 +0200

    GraphQL triggers: use symbols not strings
    It's confusing that we used camel case strings
    to reference triggers, when the actual trigger
    field uses a snake-case symbol. While both uses
    are correct, the official docs also use symbols
    since they should match the field name used
    for this trigger.
    
    This has no impact on behavior, it's cosmetic.

--- ee/app/graphql/ee/graphql_triggers.rb
+++ ee/app/graphql/ee/graphql_triggers.rb
@@ -6,19 +6,21 @@ module EE
 
     prepended do
       def self.issuable_weight_updated(issuable)
-        ::GitlabSchema.subscriptions.trigger('issuableWeightUpdated', { issuable_id: issuable.to_gid }, issuable)
+        ::GitlabSchema.subscriptions.trigger(:issuable_weight_updated, { issuable_id: issuable.to_gid }, issuable)
       end
 
       def self.issuable_iteration_updated(issuable)
-        ::GitlabSchema.subscriptions.trigger('issuableIterationUpdated', { issuable_id: issuable.to_gid }, issuable)
+        ::GitlabSchema.subscriptions.trigger(:issuable_iteration_updated, { issuable_id: issuable.to_gid }, issuable)
       end
 
       def self.issuable_health_status_updated(issuable)
-        ::GitlabSchema.subscriptions.trigger('issuableHealthStatusUpdated', { issuable_id: issuable.to_gid }, issuable)
+        ::GitlabSchema.subscriptions.trigger(
+          :issuable_health_status_updated, { issuable_id: issuable.to_gid }, issuable
+        )
       end
 
       def self.issuable_epic_updated(issuable)
-        ::GitlabSchema.subscriptions.trigger('issuableEpicUpdated', { issuable_id: issuable.to_gid }, issuable)
+        ::GitlabSchema.subscriptions.trigger(:issuable_epic_updated, { issuable_id: issuable.to_gid }, issuable)
       end
     end
   end
