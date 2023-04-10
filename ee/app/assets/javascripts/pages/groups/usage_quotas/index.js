b55ed8ef927fdaa181d2ecdf3934403af4b57202
Kos Palchyk <kpalchyk@gitlab.com>
2023-04-10 17:50:56 +0300

    Lazy loading of Usage Quotas tabs content

--- ee/app/assets/javascripts/pages/groups/usage_quotas/index.js
+++ ee/app/assets/javascripts/pages/groups/usage_quotas/index.js
@@ -1,27 +1,48 @@
 import initUsageQuotas from '~/usage_quotas';
-import SeatUsageApp from 'ee/usage_quotas/seats';
+import initSeatUsageApp from 'ee/usage_quotas/seats';
 import initPipelineUsageApp from 'ee/usage_quotas/pipelines';
 import initNamespaceStorage from 'ee/usage_quotas/storage/init_namespace_storage';
-import { GlTabsBehavior, HISTORY_TYPE_HASH } from '~/tabs';
+import { GlTabsBehavior, HISTORY_TYPE_HASH, TAB_SHOWN_EVENT } from '~/tabs';
 import { trackAddToCartUsageTab } from '~/google_tag_manager';
 import { createAlert } from '~/alert';
 import { s__ } from '~/locale';
 
-const initGlTabs = () => {
+const legacyInitUsageQuotas = () => {
   const tabsEl = document.querySelector('.js-storage-tabs');
   if (!tabsEl) {
     return;
   }
 
+  const initializers = {
+    'seats-quota-tab': {
+      wasInited: false,
+      init: initSeatUsageApp,
+    },
+    'pipelines-quota-tab': {
+      wasInited: false,
+      init: initPipelineUsageApp,
+    },
+    'storage-quota-tab': {
+      wasInited: false,
+      init: initNamespaceStorage,
+    },
+  };
+
+  tabsEl.addEventListener(TAB_SHOWN_EVENT, (event) => {
+    /** @type HTMLElement */
+    const el = event.detail?.activeTabPanel;
+    const initializer = initializers[el.id];
+    if (!initializer || initializer.wasInited) {
+      return;
+    }
+
+    initializer.init();
+    initializer.wasInited = true;
+  });
+
   // eslint-disable-next-line no-new
   new GlTabsBehavior(tabsEl, { history: HISTORY_TYPE_HASH });
-};
 
-const legacyInitUsageQuotas = () => {
-  SeatUsageApp();
-  initPipelineUsageApp();
-  initNamespaceStorage();
-  initGlTabs();
   trackAddToCartUsageTab();
 
   if (window.gon.features?.dataTransferMonitoring) {
