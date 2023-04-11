17b4a6991a88c921fe39c639bb8eea5be24c02ce
Lorenz van Herwaarden <lvanherwaarden@gitlab.com>
2023-04-11 19:20:38 +0000

    Show dismissal descriptions with popover
    - inject dismissal descriptions
    - show help icon for popover on hover
    - add tests for help popover
    - update milestone for dismissal_reason ff

--- ee/spec/frontend/vulnerabilities/mock_data.js
+++ ee/spec/frontend/vulnerabilities/mock_data.js
@@ -108,3 +108,15 @@ export const getVulnerabilityStatusMutationResponse = (queryName, expected) => (
     },
   },
 });
+
+export const dismissalDescriptions = {
+  acceptable_risk:
+    'The vulnerability is known, and has not been remediated or mitigated, but is considered to be an acceptable business risk.',
+  false_positive:
+    'An error in reporting in which a test result incorrectly indicates the presence of a vulnerability in a system when the vulnerability is not present.',
+  mitigating_control:
+    'A management, operational, or technical control (that is, safeguard or countermeasure) employed by an organization that provides equivalent or comparable protection for an information system.',
+  used_in_tests: 'The finding is not a vulnerability because it is part of a test or is test data.',
+  not_applicable:
+    'The vulnerability is known, and has not been remediated or mitigated, but is considered to be in a part of the application that will not be updated.',
+};
