6c6117751392e4cc1710b6fa8b6137fb1bc73cab
Andrei Zubov <azubov@gitlab.com>
2023-04-11 10:17:08 +0200

    Expose pending_approval_count field for deployment type

--- ee/spec/graphql/ee/types/deployment_type_spec.rb
+++ ee/spec/graphql/ee/types/deployment_type_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 
 RSpec.describe GitlabSchema.types['Deployment'], feature_category: :continuous_delivery do
   it 'includes the expected fields' do
-    expected_fields = %w[approvalSummary]
+    expected_fields = %w[approvalSummary pending_approval_count]
 
     expect(described_class).to include_graphql_fields(*expected_fields)
   end
