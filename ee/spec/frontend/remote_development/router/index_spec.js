bc753ad52041847154e5349ed016442975dd75e3
David O'Regan <doregan@gitlab.com>
2023-04-13 02:03:58 +0000

    Add mock workspaces list data
    - https://gitlab.com/gitlab-org/gitlab/-/merge_requests/112892

--- ee/spec/frontend/remote_development/router/index_spec.js
+++ ee/spec/frontend/remote_development/router/index_spec.js
@@ -1,11 +1,14 @@
 import { mount } from '@vue/test-utils';
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import VueApollo from 'vue-apollo';
 import App from 'ee/remote_development/pages/app.vue';
 import WorkspacesList from 'ee/remote_development/pages/list.vue';
 import createRouter from 'ee/remote_development/router/index';
+import createMockApollo from 'helpers/mock_apollo_helper';
 
 Vue.use(VueRouter);
+Vue.use(VueApollo);
 
 const SVG_PATH = '/assets/illustrations/empty_states/empty_workspaces.svg';
 
@@ -24,6 +27,7 @@ describe('remote_development/router/index.js', () => {
 
   it('renders WorkspacesList on route /', async () => {
     const wrapper = mount(App, {
+      apolloProvider: createMockApollo(),
       router,
       provide: {
         emptyStateSvgPath: SVG_PATH,
