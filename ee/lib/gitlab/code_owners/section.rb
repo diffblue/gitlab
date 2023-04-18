69d4969ca83dbae483b57c90bea865b9e2cec7d5
Vasilii Iakliushin <viakliushin@gitlab.com>
2023-04-18 21:13:46 +0000

    Add a basic linter for CODEOWNERS file
    Contributes to https://gitlab.com/gitlab-org/gitlab/-/issues/396476
    
    **Problem**
    
    We don't verify the correctness of the CODEOWNER file syntax. It leads
    to invalid/broken rules.
    
    **Solution**
    
    Add a linter to detect syntax errors in CODEOWNER file.

--- ee/lib/gitlab/code_owners/section.rb
+++ ee/lib/gitlab/code_owners/section.rb
@@ -5,35 +5,6 @@ module Gitlab
     class Section
       DEFAULT = 'codeowners'
 
-      REGEX = {
-        optional: /(?<optional>\^)?/,
-        name: /\[(?<name>.*?)\]/,
-        approvals: /(?:\[(?<approvals>\d*?)\])?/,
-        default_owners: /(?<default_owners>\s+[@\w_.\-\/\s+]*)?/ # rubocop: disable Style/RegexpLiteralMixedPreserve
-      }.freeze
-
-      HEADER_REGEX = /^#{REGEX.values_at(:optional, :name, :approvals, :default_owners).join}/
-
-      def self.parse(line, sectional_data)
-        match = line.match(HEADER_REGEX)
-        return unless match
-
-        new(
-          name: find_section_name(match[:name], sectional_data),
-          optional: match[:optional].present?,
-          approvals: match[:approvals].to_i,
-          default_owners: match[:default_owners]
-        )
-      end
-
-      def self.find_section_name(name, sectional_data)
-        section_headers = sectional_data.keys
-
-        return name if section_headers.last == DEFAULT
-
-        section_headers.find { |k| k.casecmp?(name) } || name
-      end
-
       attr_reader :name, :optional, :approvals, :default_owners
 
       def initialize(name:, optional: false, approvals: 0, default_owners: nil)
