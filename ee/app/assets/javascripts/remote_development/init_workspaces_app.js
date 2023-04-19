f4123cd660bfdd0c2618393738e8881d2e541431
Enrique Alcantara <ealcantara@gitlab.com>
2023-04-19 15:12:02 -0400

    Remove columns from the workspaces list
    Remove lastUsed and branch fields from the workspaces
    Do not display previewUrl when workspace is not running
    Refactor workspaces list graphql query to resemble the
    actual backend api

--- ee/app/assets/javascripts/remote_development/init_workspaces_app.js
+++ ee/app/assets/javascripts/remote_development/init_workspaces_app.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import VueApollo from 'vue-apollo';
 import createDefaultClient from '~/lib/graphql';
 import App from './pages/app.vue';
-import createRouter from './router';
+import createRouter from './router/index';
 import userWorkspacesListQuery from './graphql/queries/user_workspaces_list.query.graphql';
 
 Vue.use(VueApollo);
@@ -47,29 +47,46 @@ const createApolloProvider = () => {
   defaultClient.cache.writeQuery({
     query: userWorkspacesListQuery,
     data: {
-      userWorkspacesList: {
-        nodes: [
-          {
-            id: 1,
-            // eslint-disable-next-line @gitlab/require-i18n-strings
-            name: 'Workspace 1',
-            // eslint-disable-next-line @gitlab/require-i18n-strings
-            namespace: 'Namespace',
-            projectFullPath: 'GitLab.org / GitLab',
-            // eslint-disable-next-line @gitlab/require-i18n-strings
-            desiredState: 'Running',
-            // eslint-disable-next-line @gitlab/require-i18n-strings
-            actualState: 'Started',
-            // eslint-disable-next-line @gitlab/require-i18n-strings
-            displayedState: 'Started',
-            url: 'https://127.0.0.1',
-            // eslint-disable-next-line @gitlab/require-i18n-strings
-            editor: 'VSCode',
-            devfile: 'devfile',
-            branch: 'master',
-            lastUsed: '2020-01-01T00:00:00.000Z',
-          },
-        ],
+      user: {
+        id: 1,
+        workspaces: {
+          nodes: [
+            {
+              id: 'gid://gitlab/RemoteDevelopment::Workspace/2',
+              name: 'workspace-1-1-idmi02',
+              namespace: 'gl-rd-ns-1-1-idmi02',
+              // eslint-disable-next-line @gitlab/require-i18n-strings
+              desiredState: 'Stopped',
+              // eslint-disable-next-line @gitlab/require-i18n-strings
+              actualState: 'Creating',
+              url: 'http://8000-workspace-1-1-idmi02.workspaces.localdev.me?tkn=password',
+              devfileRef: 'main',
+              devfilePath: '.devfile.yaml',
+              project: {
+                id: 'gid://gitlab/Project/2',
+                // eslint-disable-next-line @gitlab/require-i18n-strings
+                nameWithNamespace: 'Gitlab Shell',
+              },
+            },
+            {
+              id: 'gid://gitlab/RemoteDevelopment::Workspace/1',
+              name: 'workspace-1-1-rfu27q',
+              namespace: 'gl-rd-ns-1-1-rfu27q',
+              // eslint-disable-next-line @gitlab/require-i18n-strings
+              desiredState: 'Running',
+              // eslint-disable-next-line @gitlab/require-i18n-strings
+              actualState: 'Running',
+              url: 'http://8000-workspace-1-1-rfu27q.workspaces.localdev.me?tkn=password',
+              devfileRef: 'main',
+              devfilePath: '.devfile.yaml',
+              project: {
+                id: 'gid://gitlab/Project/2',
+                // eslint-disable-next-line @gitlab/require-i18n-strings
+                nameWithNamespace: 'Gitlab Shell',
+              },
+            },
+          ],
+        },
       },
     },
   });
@@ -97,6 +114,7 @@ const initWorkspacesApp = () => {
     provide: {
       workspacesListPath,
       emptyStateSvgPath,
+      currentUsername: window.gon.current_usernanme,
     },
     render: (createElement) => createElement(App),
   });
