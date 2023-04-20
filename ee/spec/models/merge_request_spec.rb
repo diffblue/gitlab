a8fe8904a4a929ed4d6e9d849a867e21e23d5370
Brian Williams <bwilliams@gitlab.com>
2023-04-20 14:52:32 -0500

    Move merge base pipeline logic to EE
    `::Ci::CompareMetricsReportsService`,
    `::Ci::CompareCodequalityReportsService`, and
    `::Ci::CompareSecurityReportsService` are all EE classes. Since
    `use_merge_base_pipeline_for_comparison?` is related to those classes
    specifically, it should be in EE. This change moves the code to the EE
    file. To lower the risk of EE class usage happening the in the CE file
    in the future, we use constants instead of strings for the map keys.
    This means that trying to reference an EE constant inside CE will cause
    an undefined constant error and fail the pipeline when running tests.

--- ee/spec/models/merge_request_spec.rb
+++ ee/spec/models/merge_request_spec.rb
@@ -1119,6 +1119,44 @@ RSpec.describe MergeRequest, feature_category: :code_review_workflow do
     end
   end
 
+  describe '#use_merge_base_pipeline_for_comparison?' do
+    let(:merge_request) { create(:merge_request, :with_codequality_reports, source_project: project) }
+
+    subject { merge_request.use_merge_base_pipeline_for_comparison?(service_class) }
+
+    context 'when service class is Ci::CompareMetricsReportsService' do
+      let(:service_class) { ::Ci::CompareMetricsReportsService }
+
+      it { is_expected.to eq(true) }
+    end
+
+    context 'when service class is Ci::CompareCodequalityReportsService' do
+      let(:service_class) { ::Ci::CompareCodequalityReportsService }
+
+      it { is_expected.to eq(true) }
+    end
+
+    context 'when service class is Ci::CompareSecurityReportsService' do
+      let(:service_class) { ::Ci::CompareSecurityReportsService }
+
+      it { is_expected.to eq(true) }
+
+      context 'when use_merge_base_for_security_widget feature flag is off' do
+        before do
+          stub_feature_flags(use_merge_base_for_security_widget: false)
+        end
+
+        it { is_expected.to eq(false) }
+      end
+    end
+
+    context 'when service class is different' do
+      let(:service_class) { ::Ci::GenerateCoverageReportsService }
+
+      it { is_expected.to eq(false) }
+    end
+  end
+
   describe '#approver_group_ids=' do
     it 'create approver_groups' do
       group = create :group
