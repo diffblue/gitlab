3e92de4153d422bab0bdf912457d158904d9bf3f
Sashi Kumar Kumaresan <skumar@gitlab.com>
2023-04-26 00:16:28 +0000

    Remove transaction in ProcessScanResultPolicyWorker
    This change removes the transaction in ProcessScanResultPolicyWorker
    which is causing performance issues.
    
    EE: true
    Changelog: fixed

--- ee/app/workers/security/process_scan_result_policy_worker.rb
+++ ee/app/workers/security/process_scan_result_policy_worker.rb
@@ -19,20 +19,28 @@ module Security
       active_scan_result_policies = configuration.active_scan_result_policies
       return if active_scan_result_policies.empty?
 
-      configuration.transaction do
-        configuration.delete_scan_finding_rules_for_project(project_id)
-        configuration.delete_software_license_policies(project)
-
-        active_scan_result_policies.each_with_index do |policy, policy_index|
-          Security::SecurityOrchestrationPolicies::ProcessScanResultPolicyService
-            .new(project: project, policy_configuration: configuration, policy: policy, policy_index: policy_index)
-            .execute
+      if Feature.enabled?(:remove_scan_result_policy_transaction, project)
+        sync_policies(project, configuration, active_scan_result_policies)
+      else
+        configuration.transaction do
+          sync_policies(project, configuration, active_scan_result_policies)
         end
       end
 
       Security::SecurityOrchestrationPolicies::SyncOpenedMergeRequestsService
         .new(project: project, policy_configuration: configuration)
         .execute
+    end
+
+    def sync_policies(project, configuration, active_scan_result_policies)
+      configuration.delete_scan_finding_rules_for_project(project.id)
+      configuration.delete_software_license_policies(project)
+
+      active_scan_result_policies.each_with_index do |policy, policy_index|
+        Security::SecurityOrchestrationPolicies::ProcessScanResultPolicyService
+          .new(project: project, policy_configuration: configuration, policy: policy, policy_index: policy_index)
+          .execute
+      end
     end
   end
 end
