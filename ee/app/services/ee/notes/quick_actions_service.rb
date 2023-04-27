db8af96d797427cc10b0cf681b09433b5c5a55e2
Eulyeon Ko <eko@gitlab.com>
2023-04-27 15:52:45 +0900

    Refactor quick actions service
    Update services for noteable objects differ in their interfaces
    and it's more straightforward to call each update service
    inside conditionals.

--- ee/app/services/ee/notes/quick_actions_service.rb
+++ ee/app/services/ee/notes/quick_actions_service.rb
@@ -7,19 +7,23 @@ module EE
       include ::Gitlab::Utils::StrongMemoize
 
       prepended do
-        EE_UPDATE_SERVICES = const_get(:UPDATE_SERVICES, false).merge(
-          'Epic' => Epics::UpdateService
-        ).freeze
-        EE::Notes::QuickActionsService.private_constant :EE_UPDATE_SERVICES
+        EE_SUPPORTED_NOTEABLES = %w[Epic].freeze
+        EE::Notes::QuickActionsService.private_constant :EE_SUPPORTED_NOTEABLES
       end
 
       class_methods do
         extend ::Gitlab::Utils::Override
 
-        override :update_services
-        def update_services
-          EE_UPDATE_SERVICES
+        override :supported_noteables
+        def supported_noteables
+          super + EE_SUPPORTED_NOTEABLES
         end
+      end
+
+      def noteable_update_service(note, update_params)
+        return super unless note.for_epic?
+
+        Epics::UpdateService.new(group: note.resource_parent, current_user: current_user, params: update_params)
       end
     end
   end
