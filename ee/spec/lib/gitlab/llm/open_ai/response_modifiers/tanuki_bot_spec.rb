80ac6752410f160d4d0560ce204f420d560b3482
Terri Chu <tchu@gitlab.com>
2023-04-27 21:31:59 +0000

    Add source_url to gitlab chat graphql response

--- ee/spec/lib/gitlab/llm/open_ai/response_modifiers/tanuki_bot_spec.rb
+++ ee/spec/lib/gitlab/llm/open_ai/response_modifiers/tanuki_bot_spec.rb
@@ -3,23 +3,28 @@
 require 'spec_helper'
 
 RSpec.describe Gitlab::Llm::OpenAi::ResponseModifiers::TanukiBot, feature_category: :global_search do
-  let(:ai_response) { { choices: [{ text: "#{text} ATTRS: CNT-IDX-123" }] } }
+  let(:ai_response) { { choices: [{ text: "#{text} ATTRS: CNT-IDX-#{record_id}" }] } }
   let(:text) { 'some ai response text' }
 
   subject { described_class.new.execute(ai_response) }
 
-  it 'parses content from the ai response' do
-    record = create(:tanuki_bot_mvc, id: 123)
+  context 'when the ids match existing documents' do
+    let(:record) { create(:tanuki_bot_mvc) }
+    let(:record_id) { record.id }
 
-    response = {
-      msg: text,
-      sources: [record.metadata]
-    }
+    it 'parses content from the ai response and populates source_url into metadata' do
+      response = {
+        msg: text,
+        sources: [record.metadata.merge(source_url: record.url)]
+      }
 
-    expect(subject).to eq(response)
+      expect(subject).to eq(response)
+    end
   end
 
   context "when the ids don't match any documents" do
+    let(:record_id) { non_existing_record_id }
+
     it 'sets sources as empty' do
       response = {
         msg: text,
