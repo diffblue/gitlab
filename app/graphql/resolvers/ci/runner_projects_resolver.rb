45f9b5b619a01ad96c6c85e3dce50b1981092d81
Pedro Pombeiro <noreply@pedro.pombei.ro>
2023-04-28 16:21:06 +0200

    Deprecate CiRunner.projects default sort order value for removal in 17.0
    Changelog: deprecated

--- app/graphql/resolvers/ci/runner_projects_resolver.rb
+++ app/graphql/resolvers/ci/runner_projects_resolver.rb
@@ -15,9 +15,9 @@ module Resolvers
 
       argument :sort, GraphQL::Types::String,
                required: false,
-               default_value: 'id_asc', # TODO: Remove in %16.0 and move :sort to ProjectSearchArguments, see https://gitlab.com/gitlab-org/gitlab/-/issues/372117
+               default_value: 'id_asc', # TODO: Remove in %17.0 and move :sort to ProjectSearchArguments, see https://gitlab.com/gitlab-org/gitlab/-/issues/372117
                deprecated: {
-                 reason: 'Default sort order will change in 16.0. ' \
+                 reason: 'Default sort order will change in 17.0. ' \
                    'Specify `"id_asc"` if query results\' order is important',
                  milestone: '15.4'
                },
