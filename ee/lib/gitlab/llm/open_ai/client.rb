312b5f7e43db9bdeca7406586e6f5b57ccc0c5cb
George Koltsov <gkoltsov@gitlab.com>
2023-04-28 11:53:19 +0100

    Add moderation to OpenAI::Client
    Update existing OpenAI::Client public interface to
    have an ability to moderate content prior to sending
    the actual request to OpenAI.
    
    Changelog: added
    EE: true

--- ee/lib/gitlab/llm/open_ai/client.rb
+++ ee/lib/gitlab/llm/open_ai/client.rb
@@ -20,30 +20,34 @@ module Gitlab
           chat: "gpt-3.5-turbo",
           completions: "text-davinci-003",
           edits: "text-davinci-edit-001",
-          embeddings: "text-embedding-ada-002"
+          embeddings: "text-embedding-ada-002",
+          moderations: "text-moderation-latest"
         }.freeze
 
         include ExponentialBackoff
+
+        InputModerationError = Class.new(StandardError)
 
         def initialize(user, request_timeout: nil)
           @user = user
           @request_timeout = request_timeout
         end
 
-        def chat(content:, **options)
+        def chat(content:, moderated: true, **options)
           messages_chat(
-            **{ messages: [{ role: DEFAULT_ROLE, content: content }] }.merge(options)
+            **{ messages: [{ role: DEFAULT_ROLE, content: content }], moderated: moderated }.merge(options)
           )
         end
 
         # messages: an array with `role` and `content` a keys.
         # the value of `role` should be one of GPT_ROLES
         # this needed to pass back conversation history
-        def messages_chat(messages:, **options)
+        def messages_chat(messages:, moderated: true, **options)
           raise ArgumentError unless messages.all? { |m| GPT_ROLES.member? m.with_indifferent_access[:role] }
 
           request(
-            :chat,
+            endpoint: :chat,
+            moderated: moderated,
             parameters: {
               model: DEFAULT_MODELS[:chat],
               temperature: DEFAULT_TEMPERATURE,
@@ -52,9 +56,10 @@ module Gitlab
           )
         end
 
-        def completions(prompt:, **options)
+        def completions(prompt:, moderated: true, **options)
           request(
-            :completions,
+            endpoint: :completions,
+            moderated: moderated,
             parameters: {
               model: DEFAULT_MODELS[:completions],
               prompt: prompt,
@@ -63,9 +68,10 @@ module Gitlab
           )
         end
 
-        def edits(input:, instruction:, **options)
+        def edits(input:, instruction:, moderated: true, **options)
           request(
-            :edits,
+            endpoint: :edits,
+            moderated: moderated,
             parameters: {
               model: DEFAULT_MODELS[:edits],
               input: input,
@@ -74,9 +80,10 @@ module Gitlab
           )
         end
 
-        def embeddings(input:, **options)
+        def embeddings(input:, moderated: false, **options)
           request(
-            :embeddings,
+            endpoint: :embeddings,
+            moderated: moderated,
             parameters: {
               model: DEFAULT_MODELS[:embeddings],
               input: input
@@ -84,9 +91,20 @@ module Gitlab
           )
         end
 
+        def moderations(input:, **options)
+          request(
+            endpoint: :moderations,
+            moderated: false,
+            parameters: {
+              model: DEFAULT_MODELS[:moderations],
+              input: input
+            }.merge(options)
+          )
+        end
+
         private
 
-        retry_methods_with_exponential_backoff :chat, :completions, :edits, :embeddings
+        retry_methods_with_exponential_backoff :chat, :completions, :edits, :embeddings, :moderations
 
         attr_reader :user, :request_timeout
 
@@ -102,8 +120,10 @@ module Gitlab
           @token ||= ::Gitlab::CurrentSettings.openai_api_key
         end
 
-        def request(endpoint, options)
+        def request(endpoint:, moderated:, **options)
           return unless enabled?
+
+          moderate_input!(moderation_input(endpoint, options)) if moderated
 
           response = client.public_send(endpoint, **options) # rubocop:disable GitlabSecurity/PublicSend
 
@@ -140,6 +160,28 @@ module Gitlab
             :gitlab_cloud_cost_spend_entry_total,
             'Number of units spent per vendor entry'
           )
+        end
+
+        def moderate_input!(input)
+          return if Feature.disabled?(:openai_moderation)
+
+          flagged = moderations(input: input)
+            .parsed_response
+            &.dig('results')
+            &.any? { |r| r['flagged'] }
+
+          raise(InputModerationError, "Provided input violates OpenAI's Content Policy") if flagged
+        end
+
+        def moderation_input(endpoint, options)
+          case endpoint
+          when :chat
+            options.dig(:parameters, :messages).pluck(:content) # rubocop:disable CodeReuse/ActiveRecord
+          when :completions
+            options.dig(:parameters, :prompt)
+          when :edits, :embeddings
+            options.dig(:parameters, :input)
+          end
         end
       end
     end
