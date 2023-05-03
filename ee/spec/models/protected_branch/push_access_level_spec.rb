12b0b2041b7fc8c75239d604a20743dbd06fa95c
Joe Woodward <jwoodward@gitlab.com>
2023-05-03 23:40:18 +0100

    Refactor ProtectedRefAccess specs
    Adds more coverage to shared protected ref access examples
    Removes ProtectedRefAccess specs
    Adds ee spec coverage for ProtectedBranch access levels
    
    Related to https://gitlab.com/gitlab-org/gitlab/-/issues/12776

---
+++ ee/spec/models/protected_branch/push_access_level_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe ProtectedBranch::PushAccessLevel, feature_category: :source_code_management do
+  include_examples 'ee protected ref access', :protected_branch
+end
