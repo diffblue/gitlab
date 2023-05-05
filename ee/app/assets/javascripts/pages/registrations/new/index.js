f4423764bf4b063e31b8d9114e16fe979d533b68
Eduardo Sanz García <esanz-garcia@gitlab.com>
2023-05-05 11:57:26 +0200

    Enable password check on registration trial page
    Enabling the password checks in the registration trial page, allows the
    user to check if the password complies with the rules before submitting
    the registration form.
    
    Changelog: changed

--- ee/app/assets/javascripts/pages/registrations/new/index.js
+++ ee/app/assets/javascripts/pages/registrations/new/index.js
@@ -1,7 +1,9 @@
-import '~/pages/registrations/new/index';
+import '~/pages/registrations/new';
 import initPasswordValidator from 'ee/password/password_validator';
 import { setupArkoseLabsForSignup } from 'ee/arkose_labs';
 
+// Warning: initPasswordValidator has to run after initTogglePasswordVisibility
+// (which is executed when '~/pages/registrations/new' is imported)
 initPasswordValidator();
 
 if (gon.features.arkoseLabsSignupChallenge) {
