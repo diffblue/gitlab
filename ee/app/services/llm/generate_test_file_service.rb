7ab2c167cca36448f9d57a136177fab9b3b7d5fb
Phil Hughes <me@iamphill.com>
2023-05-05 16:48:31 +0100

    Use namespace settings for generate test file AI feature
    https://gitlab.com/gitlab-org/gitlab/-/issues/409540

--- ee/app/services/llm/generate_test_file_service.rb
+++ ee/app/services/llm/generate_test_file_service.rb
@@ -5,7 +5,8 @@ module Llm
     def valid?
       super &&
         Feature.enabled?(:generate_test_file_flag, user) &&
-        resource.resource_parent.root_ancestor.licensed_feature_available?(:generate_test_file)
+        resource.resource_parent.root_ancestor.licensed_feature_available?(:generate_test_file) &&
+        resource.resource_parent.root_ancestor.experiment_features_enabled
     end
 
     private
--- ee/spec/services/llm/generate_test_file_service_spec.rb
+++ ee/spec/services/llm/generate_test_file_service_spec.rb
@@ -2,19 +2,25 @@
 
 require 'spec_helper'
 
-RSpec.describe Llm::GenerateTestFileService, feature_category: :code_review_workflow do
-  let_it_be(:group) { create(:group, :public) }
+RSpec.describe Llm::GenerateTestFileService, :saas, feature_category: :code_review_workflow do
+  let_it_be(:group) { create(:group_with_plan, :public, plan: :ultimate_plan) }
   let_it_be(:user) { create(:user) }
   let_it_be(:project) { create(:project, :public, group: group) }
   let_it_be(:merge_request) { create(:merge_request, source_project: project) }
-  let(:options) { {} }
+  let_it_be(:options) { {} }
 
   subject { described_class.new(user, merge_request, options) }
 
+  before do
+    stub_ee_application_setting(should_check_namespace_plan: true)
+    stub_licensed_features(generate_test_file: true, ai_features: true)
+  end
+
   describe '#execute' do
     before do
-      stub_licensed_features(generate_test_file: true)
-      group.namespace_settings.update!(third_party_ai_features_enabled: true)
+      project.root_ancestor.namespace_settings.update!(
+        third_party_ai_features_enabled: true,
+        experiment_features_enabled: true)
       allow(Llm::CompletionWorker).to receive(:perform_async)
     end
 
@@ -56,6 +62,30 @@ RSpec.describe Llm::GenerateTestFileService, feature_category: :code_review_work
 
         expect(Llm::CompletionWorker).not_to have_received(:perform_async)
       end
+    end
+  end
+
+  describe '#valid?' do
+    using RSpec::Parameterized::TableSyntax
+
+    where(:experiment_features_enabled, :third_party_ai_features_enabled, :result) do
+      true   | true  | true
+      false  | true  | false
+      true   | false | false
+      false  | false | false
+    end
+
+    with_them do
+      before do
+        project.add_maintainer(user)
+        project.root_ancestor.namespace_settings.update!(
+          third_party_ai_features_enabled: third_party_ai_features_enabled,
+          experiment_features_enabled: experiment_features_enabled)
+      end
+
+      subject { described_class.new(user, merge_request, options) }
+
+      it { expect(subject.valid?).to eq(result) }
     end
   end
 end
