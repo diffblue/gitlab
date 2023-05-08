cea3657ceab7351c4016f63b77c85d3b145991ae
Kristina Doskich <kdos@softserveinc.com>
2023-05-08 18:24:21 +0000

    Move reviews under to pull_request namespace
    GitHub importer has a numerous pull request
    related importers and workers to migrate all
    pull request data, such as merged_by, review requests, etc.
    Some of them where put under pull_requests namespace,
    but some are not. To make it consistant all over GitHub importer
    all pull requests related importers and workers are planned
    to be moved under pull_request namespace.
    
    This commit moves
    
    workers/gitlab/github_import/import_pull_request_review_worker.rb
    to workers/gitlab/github_import/pull_requests/import_review_worker.rb
    
    lib/gitlab/github_import/importer/pull_request_review_importer.rb
    to lib/gitlab/github_import/importer/pull_requests/review_importer.rb
    lib/gitlab/github_import/importer/pull_requests_reviews_importer.rb
    to lib/gitlab/github_import/importer/pull_requests/reviews_importer.rb
    
    Details: https://gitlab.com/gitlab-org/gitlab/-/issues/377059
    
    Changelog: deprecated

--- spec/workers/gitlab/github_import/pull_requests/import_review_worker_spec.rb
+++ spec/workers/gitlab/github_import/pull_requests/import_review_worker_spec.rb
@@ -2,7 +2,7 @@
 
 require 'spec_helper'
 
-RSpec.describe Gitlab::GithubImport::ImportPullRequestReviewWorker, feature_category: :importers do
+RSpec.describe Gitlab::GithubImport::PullRequests::ImportReviewWorker, feature_category: :importers do
   it { is_expected.to include_module(Gitlab::GithubImport::ObjectImporter) }
 
   describe '#representation_class' do
@@ -11,5 +11,9 @@ RSpec.describe Gitlab::GithubImport::ImportPullRequestReviewWorker, feature_cate
 
   describe '#importer_class' do
     it { expect(subject.importer_class).to eq(Gitlab::GithubImport::Importer::PullRequests::ReviewImporter) }
+  end
+
+  describe '#object_type' do
+    it { expect(subject.object_type).to eq(:pull_request_review) }
   end
 end
