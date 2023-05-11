b1a71e98d95208e5fd7422b57dbb0693a81f726a
Doug Stull <dstull@gitlab.com>
2023-05-11 15:01:30 -0400

    Remove lingering dead code related to skipping trials
    - no longer used

--- ee/app/assets/javascripts/pages/trials/new/index.js
+++ ee/app/assets/javascripts/pages/trials/new/index.js
@@ -1,5 +1,3 @@
-import { trackSaasTrialSkip } from '~/google_tag_manager';
 import { initTrialCreateLeadForm } from 'ee/trials/init_create_lead_form';
 
 initTrialCreateLeadForm();
-trackSaasTrialSkip();
