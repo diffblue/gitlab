8beac8f853ba402cc520b8d6eb2db59cfc0a7c8f
Andy Soiron <asoiron@gitlab.com>
2023-05-12 09:13:14 +0000

    Move Security policy bot creation to service class
    I'm planning to add bot users in a backfill migration. In order to
    re-use the worker code, this moves it into a service class and
    adds some improvements along the way.
    
    - It raises an error if the configuration has no project
    - It checks if current user has permission to add project members
    - User creation, project member creation and updating config is now in
      a transaction

--- ee/app/workers/security/orchestration_configuration_create_bot_worker.rb
+++ ee/app/workers/security/orchestration_configuration_create_bot_worker.rb
@@ -13,39 +13,16 @@ module Security
     def perform(configuration_id, current_user_id)
       configuration = Security::OrchestrationPolicyConfiguration.find_by_id(configuration_id)
 
-      return if configuration.nil? || configuration.bot_user.present?
+      return if configuration.nil?
 
       current_user = User.find_by_id(current_user_id)
 
       return unless current_user
 
-      bot_user = ::Users::AuthorizedCreateService.new(
-        current_user,
-        bot_user_params(configuration.project_id)
-      ).execute
-
-      configuration.project.add_guest(bot_user, current_user: current_user)
-
-      configuration.update(bot_user: bot_user)
-    end
-
-    private
-
-    def bot_user_params(project_id)
-      {
-        name: 'GitLab Security Policy Bot',
-        email: username_and_email_generator(project_id).email,
-        username: username_and_email_generator(project_id).username,
-        user_type: :security_policy_bot,
-        skip_confirmation: true # Bot users should always have their emails confirmed.
-      }
-    end
-
-    def username_and_email_generator(project_id)
-      Gitlab::Utils::UsernameAndEmailGenerator.new(
-        username_prefix: "gitlab_security_policy_project_#{project_id}_bot",
-        email_domain: "noreply.#{Gitlab.config.gitlab.host}"
-      )
+      Security::Orchestration::CreateBotService.new(configuration, current_user).execute
+    rescue Gitlab::Access::AccessDeniedError,
+      Security::Orchestration::CreateBotService::SecurityOrchestrationPolicyConfigurationHasNoProjectError
+      # Rescue errors to avoid worker retry
     end
   end
 end
--- ee/spec/workers/security/orchestration_configuration_create_bot_worker_spec.rb
+++ ee/spec/workers/security/orchestration_configuration_create_bot_worker_spec.rb
@@ -5,12 +5,12 @@ require 'spec_helper'
 RSpec.describe Security::OrchestrationConfigurationCreateBotWorker, feature_category: :security_policy_management do
   describe '#perform' do
     let_it_be(:project) { create(:project) }
+    let_it_be(:current_user) { create(:user) }
     let_it_be(:security_orchestration_policy_configuration) do
       create(:security_orchestration_policy_configuration, project: project)
     end
 
-    let_it_be(:current_user) { create(:user) }
-    let(:security_orchestration_policy_configuration_id) { nil }
+    let(:security_orchestration_policy_configuration_id) { non_existing_record_id }
 
     subject(:run_worker) { described_class.new.perform(security_orchestration_policy_configuration_id, current_user) }
 
@@ -18,47 +18,53 @@ RSpec.describe Security::OrchestrationConfigurationCreateBotWorker, feature_cate
       project.add_owner(current_user)
     end
 
-    it 'does not raise an error' do
+    it 'exits without error' do
+      expect(Security::Orchestration::CreateBotService).not_to receive(:new)
+
       expect { run_worker }.not_to raise_error
     end
 
     context 'with valid security_orchestration_policy_configuration_id' do
       let(:security_orchestration_policy_configuration_id) { security_orchestration_policy_configuration.id }
 
-      it 'creates and assignes a bot user', :aggregate_failures do
-        expect { run_worker }.to change { User.count }.by(1)
-        expect(security_orchestration_policy_configuration.reload.bot_user_id).not_to be_nil
-      end
-
-      it 'creates the bot with correct params', :aggregate_failures do
-        run_worker
-
-        bot_user = security_orchestration_policy_configuration.reload.bot_user
-
-        expect(bot_user.name).to eq('GitLab Security Policy Bot')
-        expect(bot_user.username).to start_with("gitlab_security_policy_project_#{project.id}_bot")
-        expect(bot_user.email).to start_with("gitlab_security_policy_project_#{project.id}_bot")
-        expect(bot_user.user_type).to eq('security_policy_bot')
-      end
-
-      it 'adds the bot user as a guest to the project', :aggregate_failures do
-        expect { run_worker }.to change { project.members.count }.by(1)
-
-        bot_user = security_orchestration_policy_configuration.reload.bot_user
-
-        expect(project.members.find_by(user: bot_user).access_level).to eq(Gitlab::Access::GUEST)
-      end
-
-      context 'when a bot user is already assigned' do
-        let_it_be(:bot_user) { create(:user, user_type: :security_policy_bot) }
-        let_it_be(:security_orchestration_policy_configuration) do
-          create(:security_orchestration_policy_configuration, bot_user: bot_user)
+      it 'calls the create bot service' do
+        expect_next_instance_of(
+          Security::Orchestration::CreateBotService,
+          security_orchestration_policy_configuration,
+          current_user
+        ) do |service|
+          expect(service).to receive(:execute).and_call_original
         end
 
-        it 'does not assigne a new bot user', :aggregate_failures do
-          run_worker
+        run_worker
+      end
 
-          expect(security_orchestration_policy_configuration.reload.bot_user_id).to equal(bot_user.id)
+      context 'when the CreateBotService raises an access denied error' do
+        before do
+          allow_next_instance_of(Security::Orchestration::CreateBotService) do |service|
+            allow(service).to receive(:execute).and_raise(Gitlab::Access::AccessDeniedError)
+          end
+        end
+
+        it 'exits without error' do
+          expect { run_worker }.not_to raise_error
+        end
+      end
+
+      context 'when the CreateBotService raises SecurityOrchestrationPolicyConfigurationHasNoProjectError' do
+        before do
+          allow_next_instance_of(Security::Orchestration::CreateBotService) do |service|
+            allow(service).to(
+              receive(:execute)
+                .and_raise(
+                  Security::Orchestration::CreateBotService::SecurityOrchestrationPolicyConfigurationHasNoProjectError
+                )
+            )
+          end
+        end
+
+        it 'exits without error' do
+          expect { run_worker }.not_to raise_error
         end
       end
     end
