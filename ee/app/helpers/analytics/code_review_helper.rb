3faf91c6f76e773847d818a08f42c872917de975
Julia Miocene <jmiocene@gitlab.com>
2023-05-12 18:47:47 +0000

    Update illustrations

--- ee/app/helpers/analytics/code_review_helper.rb
+++ ee/app/helpers/analytics/code_review_helper.rb
@@ -7,7 +7,7 @@ module Analytics
         project_id: project.id,
         project_path: project_path(project),
         new_merge_request_url: merge_request_source_project_for_project(project) ? namespace_project_new_merge_request_path(project.namespace) : nil,
-        empty_state_svg_path: image_path('illustrations/merge_requests.svg'),
+        empty_state_svg_path: image_path('illustrations/empty-state/empty-merge-requests-md.svg'),
         milestone_path: project_milestones_path(project),
         labels_path: project_labels_path(project)
       }
