23c575f4f1fcbedb37fefec7785af850151706fd
Ross Byrne <robyrne@gitlab.com>
2023-05-15 17:38:04 +0000

    Added reveal/hide password input to login page
    Changelog: changed

--- ee/app/assets/javascripts/pages/registrations/new/index.js
+++ ee/app/assets/javascripts/pages/registrations/new/index.js
@@ -2,7 +2,7 @@ import '~/pages/registrations/new';
 import initPasswordValidator from 'ee/password/password_validator';
 import { setupArkoseLabsForSignup } from 'ee/arkose_labs';
 
-// Warning: initPasswordValidator has to run after initTogglePasswordVisibility
+// Warning: initPasswordValidator has to run after initPasswordInput
 // (which is executed when '~/pages/registrations/new' is imported)
 initPasswordValidator();
 
--- ee/app/assets/javascripts/pages/trial_registrations/new/index.js
+++ ee/app/assets/javascripts/pages/trial_registrations/new/index.js
@@ -9,7 +9,7 @@ import EmailFormatValidator from '~/pages/sessions/new/email_format_validator';
 import Tracking from '~/tracking';
 import { setupArkoseLabsForSignup } from 'ee/arkose_labs';
 import initPasswordValidator from 'ee/password/password_validator';
-import { initTogglePasswordVisibility } from '~/authentication/password';
+import { initPasswordInput } from '~/authentication/password';
 
 new UsernameValidator(); // eslint-disable-line no-new
 new LengthValidator(); // eslint-disable-line no-new
@@ -24,6 +24,6 @@ Tracking.enableFormTracking({
 });
 
 setupArkoseLabsForSignup();
-initTogglePasswordVisibility();
-// Warning: initPasswordValidator has to run after initTogglePasswordVisibility
+initPasswordInput();
+// Warning: initPasswordValidator has to run after initPasswordInput
 initPasswordValidator();
