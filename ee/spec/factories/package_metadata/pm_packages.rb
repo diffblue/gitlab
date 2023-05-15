509bd10f25dbacb75eab34e88c95180565a00bcd
Igor Frenkel <ifrenkel@gitlab.com>
2023-05-15 14:22:14 +0000

    Update package license json schema
    Add lowest version to licenses field json and allow both lowest and
    highest to be null.
    
    Changelog: changed
    EE: true

--- ee/spec/factories/package_metadata/pm_packages.rb
+++ ee/spec/factories/package_metadata/pm_packages.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :pm_package, class: 'PackageMetadata::Package' do
     purl_type { :npm }
     sequence(:name) { |n| "package-#{n}" }
-    licenses { [[1], 'v1.0.0', [[[2], ['v1.1.0']]]] }
+    licenses { [[1], 'v0.0.1', 'v1.0.0', [[[2], ['v1.1.0']]]] }
 
     initialize_with do
       PackageMetadata::Package.find_or_initialize_by(name: name, purl_type: purl_type)
--- ee/spec/models/package_metadata/package_spec.rb
+++ ee/spec/models/package_metadata/package_spec.rb
@@ -34,52 +34,55 @@ RSpec.describe PackageMetadata::Package, type: :model, feature_category: :softwa
     describe 'for licenses' do
       subject(:package) { build(:pm_package, licenses: licenses) }
 
-      let(:valid_default) { [1] }
-      let(:valid_highest) { '0.0.1' }
-      let(:valid_non_default) { [[[1, 2], ['v0.0.1', 'v0.0.2']], [[3], ['v0.0.3']]] }
-
-      context 'when all 3 fields are valid' do
-        let(:licenses) { [valid_default, valid_highest, valid_non_default] }
-
-        it { is_expected.to be_valid }
-      end
+      let(:default) { [1] }
+      let(:highest) { '0.0.2' }
+      let(:lowest) { '0.0.1' }
+      let(:non_default) { [[[1, 2], ['v0.0.3', 'v0.0.4']], [[3], ['v0.0.5']]] }
 
       context 'when non_default licenses are empty' do
-        let(:licenses) { [valid_default, valid_highest, []] }
+        let(:licenses) { [default, lowest, highest, []] }
 
         it { is_expected.to be_valid }
       end
 
-      context 'when invalid fields exist' do
+      context 'when field is an empty array' do
+        let(:licenses) { [] }
+
+        it { is_expected.to be_valid }
+      end
+
+      context 'with different field value permutations' do
         using RSpec::Parameterized::TableSyntax
 
-        where(:test_case_name, :valid, :default_licenses, :highest_version, :non_default_licenses) do
-          'all valid'           | true  | valid_default | valid_highest | valid_non_default
-          'all nil'             | true  | nil           | nil           | nil
-          '2 nil'               | false | valid_default | nil           | nil
-          'nil'                 | false | nil           | valid_highest | valid_non_default
-          'string'              | false | 's'           | valid_highest | valid_non_default
-          'array with string'   | false | ['s']         | valid_highest | valid_non_default
-          'empty array'         | false | []            | valid_highest | valid_non_default
-          'more than max items' | false | ([1] * 11)    | valid_highest | valid_non_default
-          'nil'                 | false | valid_default | nil           | valid_non_default
-          'int value'           | false | valid_default | 1             | valid_non_default
-          'empty string'        | false | valid_default | ''            | valid_non_default
-          'exceeds max chars'   | false | valid_default | ('v' * 256)   | valid_non_default
-          'empty array'         | true  | valid_default | valid_highest | []
-          'nil'                 | false | valid_default | valid_highest | nil
-          'elts not arrays'     | false | valid_default | valid_highest | [[[1, 'v1.0']]]
-          '1st elt not array'   | false | valid_default | valid_highest | [[[1, ['v1.0']]]]
-          '2nd elt not array'   | false | valid_default | valid_highest | [[[[1], 'v1.0']]]
-          'too many tuples'     | false | valid_default | valid_highest | [[[[1], ['v1.0']] * 11]]
-          'too many licenses'   | false | valid_default | valid_highest | [[[1] * 11, ['v1.0']]]
-          'too many versions'   | false | valid_default | valid_highest | [[[1], ['v1.0'] * 51]]
-          'any license invalid' | false | valid_default | valid_highest | [[[[1, 2], ['v1']], [[nil], ['v2']]]]
-          'any version invalid' | false | valid_default | valid_highest | [[[[1, 2], %w[v1 v2]], [[3], [nil]]]]
+        where(:test_case_name, :valid, :default_licenses, :lowest_version, :highest_version, :non_default_licenses) do
+          'all valid'           | true  | default    | lowest      | highest     | non_default
+          'nil'                 | false | nil        | lowest      | highest     | non_default
+          'string'              | false | 's'        | lowest      | highest     | non_default
+          'array with string'   | false | ['s']      | lowest      | highest     | non_default
+          'empty array'         | false | []         | lowest      | highest     | non_default
+          'more than max items' | false | ([1] * 11) | lowest      | highest     | non_default
+          'nil'                 | true  | default    | nil         | highest     | non_default
+          'int value'           | false | default    | 1           | highest     | non_default
+          'empty string'        | false | default    | ''          | highest     | non_default
+          'exceeds max chars'   | false | default    | ('v' * 256) | highest     | non_default
+          'nil'                 | true  | default    | lowest      | nil         | non_default
+          'int value'           | false | default    | lowest      | 1           | non_default
+          'empty string'        | false | default    | lowest      | ''          | non_default
+          'exceeds max chars'   | false | default    | lowest      | ('v' * 256) | non_default
+          'empty array'         | true  | default    | lowest      | highest     | []
+          'nil'                 | false | default    | lowest      | highest     | nil
+          'elts not arrays'     | false | default    | lowest      | highest     | [[1, 'v1.0']]
+          '1st elt not array'   | false | default    | lowest      | highest     | [[1, ['v1.0']]]
+          '2nd elt not array'   | false | default    | lowest      | highest     | [[[1], 'v1.0']]
+          'too many tuples'     | false | default    | lowest      | highest     | [[[1], ['v1.0']] * 11]
+          'too many licenses'   | false | default    | lowest      | highest     | [[[1] * 11, ['v1.0']]]
+          'too many versions'   | false | default    | lowest      | highest     | [[[1], ['v1.0'] * 51]]
+          'invalid license'     | false | default    | lowest      | highest     | [[[1, 2], ['v1']], [[nil], ['v2']]]
+          'invalid version'     | false | default    | lowest      | highest     | [[[1, 2], %w[v1 v2]], [[3], [nil]]]
         end
 
         with_them do
-          let(:licenses) { [default_licenses, highest_version, non_default_licenses].compact }
+          let(:licenses) { [default_licenses, lowest_version, highest_version, non_default_licenses] }
 
           specify { expect(package.valid?).to eq(valid) }
         end
