63b19e31670ddb52d594e7368c65f9790ed5a879
Winnie <gitlab@winniehell.de>
2023-05-16 12:21:36 +0000

    Mock mr_notes store in frontend tests (part 1)

--- ee/spec/frontend/diffs/components/app_spec.js
+++ ee/spec/frontend/diffs/components/app_spec.js
@@ -1,34 +1,35 @@
 import { shallowMount } from '@vue/test-utils';
-import MockAdapter from 'axios-mock-adapter';
 
 import Vue from 'vue';
-import Vuex from 'vuex';
-import { stubPerformanceWebAPI } from 'helpers/performance';
-import createDiffsStore from 'jest/diffs/create_diffs_store';
 import { TEST_HOST } from 'spec/test_constants';
 import App from '~/diffs/components/app.vue';
-import axios from '~/lib/utils/axios_utils';
-import { HTTP_STATUS_OK } from '~/lib/utils/http_status';
+import store from '~/mr_notes/stores';
 
 const TEST_ENDPOINT = `${TEST_HOST}/diff/endpoint`;
 
-Vue.use(Vuex);
+jest.mock('~/mr_notes/stores', () => jest.requireActual('helpers/mocks/mr_notes/stores'));
 
 Vue.config.ignoredElements = ['copy-code'];
 
 describe('diffs/components/app', () => {
-  let store;
-  let wrapper;
-  let mock;
+  const createComponent = (props = {}) => {
+    store.reset();
 
-  function createComponent(props = {}, extendStore = () => {}) {
-    store = createDiffsStore();
+    store.getters.isNotesFetched = false;
+    store.getters.getNoteableData = {
+      current_user: {
+        can_create_note: true,
+      },
+    };
+    store.getters['diffs/flatBlobsList'] = [];
+    store.getters['diffs/isBatchLoading'] = false;
+    store.getters['diffs/isBatchLoadingError'] = false;
+    store.getters['diffs/whichCollapsedTypes'] = { any: false };
+
     store.state.diffs.isLoading = false;
     store.state.diffs.isTreeLoaded = true;
 
-    extendStore(store);
-
-    wrapper = shallowMount(App, {
+    return shallowMount(App, {
       propsData: {
         endpoint: TEST_ENDPOINT,
         endpointMetadata: `${TEST_HOST}/diff/endpointMetadata`,
@@ -44,20 +45,15 @@ describe('diffs/components/app', () => {
         fileByFileUserPreference: false,
         ...props,
       },
-      store,
+      mocks: {
+        $store: store,
+      },
     });
-  }
-
-  beforeEach(() => {
-    stubPerformanceWebAPI();
-
-    mock = new MockAdapter(axios);
-    mock.onGet(TEST_ENDPOINT).reply(HTTP_STATUS_OK, {});
-  });
+  };
 
   describe('EE codequality diff', () => {
     it('fetches code quality data when endpoint is provided', () => {
-      createComponent();
+      const wrapper = createComponent();
       jest.spyOn(wrapper.vm, 'fetchCodequality');
       wrapper.vm.fetchData(false);
 
@@ -65,7 +61,7 @@ describe('diffs/components/app', () => {
     });
 
     it('does not fetch code quality data when endpoint is blank', () => {
-      createComponent({ endpointCodequality: '' });
+      const wrapper = createComponent({ endpointCodequality: '' });
       jest.spyOn(wrapper.vm, 'fetchCodequality');
       wrapper.vm.fetchData(false);
 
