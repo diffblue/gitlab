cc65decf2189c0f2e4413d83929bba655e5af177
Jennifer Li <jli@gitlab.com>
2023-05-17 19:36:54 +0000

    Revert "Merge branch 'minac_368371_dont_ingest_cve' into 'master'"
    This reverts merge request !120132

--- ee/lib/ee/gitlab/ci/parsers/security/common.rb
+++ ee/lib/ee/gitlab/ci/parsers/security/common.rb
@@ -15,7 +15,7 @@ module EE
               return report_data["vulnerabilities"] || [] unless report_data["remediations"]
 
               report_data["vulnerabilities"].map do |vulnerability|
-                remediation = fixes[vulnerability['id']]
+                remediation = fixes[vulnerability['id']] || fixes[vulnerability['cve']]
                 vulnerability.merge("remediations" => [remediation])
               end
             end
@@ -23,7 +23,7 @@ module EE
             def fixes
               @fixes ||= report_data['remediations'].each_with_object({}) do |item, memo|
                 item['fixes'].each do |fix|
-                  id = fix['id']
+                  id = fix['id'] || fix['cve']
                   memo[id] = item if id
                 end
                 memo
