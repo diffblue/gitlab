24f687fc05d477b5bdc528512939f0d99dd6a874
Vijay Hawoldar <vhawoldar@gitlab.com>
2023-05-23 08:36:38 +0000

    Add API support for plan limit notification limits
    Allowing admins to update a plan limit's `notification_limit` value
    
    Changelog: added
    EE: true

---
+++ ee/app/services/ee/admin/plan_limits/update_service.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module EE
+  module Admin
+    module PlanLimits
+      module UpdateService
+        extend ::Gitlab::Utils::Override
+
+        RESTRICTED_ATTRIBUTES = %i[notification_limit].freeze
+
+        private
+
+        override :parsed_params
+        def parsed_params
+          params.except!(*RESTRICTED_ATTRIBUTES) if plan.paid?
+
+          params
+        end
+      end
+    end
+  end
+end
