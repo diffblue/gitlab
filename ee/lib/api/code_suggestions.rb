6860ae4e18de921cb4b6f000bfa718e7adc90bff
Tan Le <tle@gitlab.com>
2023-05-25 23:38:05 +1000

    Use access code suggestions policy
    This refactors the authorisation logic in code_suggestions/tokens API to
    use the `access_code_suggestions` permission.

--- ee/lib/api/code_suggestions.rb
+++ ee/lib/api/code_suggestions.rb
@@ -6,25 +6,9 @@ module API
 
     before do
       authenticate!
-      check_feature_enabled!
-      check_user_allowed!
-    end
 
-    helpers do
-      def check_feature_enabled!
-        not_found! unless Feature.enabled?(:code_suggestions_tokens_api, type: :ops)
-      end
-
-      def check_user_allowed!
-        # Check if the feature is disabled for any of the user's groups
-        accessible_root_groups = current_user.groups.by_parent(nil)
-        code_suggestions_disabled_by_group = accessible_root_groups.reject(&:code_suggestions_enabled?).any?
-
-        # Check if the feature is disabled by the user
-        return if !code_suggestions_disabled_by_group && current_user.code_suggestions_enabled?
-
-        unauthorized!('Code Suggestions is disabled for user')
-      end
+      not_found! unless Feature.enabled?(:code_suggestions_tokens_api, type: :ops)
+      unauthorized!('Code Suggestions is disabled for user') unless current_user.can?(:access_code_suggestions)
     end
 
     namespace 'code_suggestions' do
