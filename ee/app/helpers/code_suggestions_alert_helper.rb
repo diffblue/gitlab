dcd650c56b99286729b57207b8083cdbefcfb0dd
Ross Byrne <robyrne@gitlab.com>
2023-05-29 23:42:51 +0000

    Only show code suggestions alert after new nav alert is dismissed
    Changelog: changed
    EE: true

--- ee/app/helpers/code_suggestions_alert_helper.rb
+++ ee/app/helpers/code_suggestions_alert_helper.rb
@@ -5,7 +5,8 @@ module CodeSuggestionsAlertHelper
     return false unless ::Gitlab::CurrentSettings.should_check_namespace_plan?
     return false if cookies[:code_suggestions_alert_dismissed] == 'true'
     return false if Feature.disabled?(:code_suggestions_alert, current_user)
-    return true if current_user.blank?
+    return true if current_user.blank? # check user is logged in
+    return false unless user_dismissed_before?(::Users::CalloutsHelper::NEW_NAVIGATION_CALLOUT, 30.minutes.ago)
 
     !current_user.code_suggestions_enabled?
   end
