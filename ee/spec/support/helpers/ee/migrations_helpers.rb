4d03d85e22937cfcae89e6da72cbdef8c60ed77b
Dylan Griffith <dyl.griffith@gmail.com>
2023-05-29 06:46:30 +0000

    Revert "Merge branch 'jp-embedd-mig' into 'master'"
    This reverts commit 4b6ddfeea26f6bfa867d23aec62eb8d6553b4060, reversing
    changes made to fc5baccc279c1f3da28264b3efcf98f68b76a3dd.

--- ee/spec/support/helpers/ee/migrations_helpers.rb
+++ ee/spec/support/helpers/ee/migrations_helpers.rb
@@ -12,8 +12,8 @@ module EE
 
     override :active_record_base
     def active_record_base(...)
-      if custom_migration?
-        db_base_model
+      if geo_migration?
+        ::Geo::TrackingBase
       else
         super
       end
@@ -21,8 +21,8 @@ module EE
 
     override :migrations_paths
     def migrations_paths
-      if custom_migration?
-        connection_db_config.configuration_hash[:migrations_paths]
+      if geo_migration?
+        geo_db_config.configuration_hash[:migrations_paths]
       else
         super
       end
@@ -30,40 +30,32 @@ module EE
 
     override :schema_migrate_down!
     def schema_migrate_down!
-      return super unless custom_migration?
-
-      with_db_config { migration_context.down(migration_schema_version) }
+      with_db_config { super }
     end
 
     override :schema_migrate_up!
-    def schema_migrate_up!(only_databases: nil)
-      return super unless custom_migration?
-
-      with_db_config { migration_context.up }
+    def schema_migrate_up!
+      with_db_config { super }
     end
 
     override :migrate!
     def migrate!
-      return super unless custom_migration?
-
       with_db_config { super }
     end
 
     def with_db_config(&block)
-      if custom_migration?
-        with_custom_connection { yield }
+      if geo_migration?
+        with_added_geo_connection { yield }
       else
         yield
       end
     end
 
-    def with_custom_connection
-      name = geo_migration? ? :geo : :embedding
-
+    def with_added_geo_connection
       with_reestablished_active_record_base(reconnect: true) do
         reconfigure_db_connection(
-          name: name,
-          config_model: db_base_model,
+          name: :geo,
+          config_model: Geo::TrackingBase,
           model: ActiveRecord::Base
         )
 
@@ -71,30 +63,12 @@ module EE
       end
     end
 
-    def custom_migration?
-      geo_migration? || embedding_migration?
-    end
-
     def geo_migration?
       self.class.metadata[:geo]
     end
 
-    def embedding_migration?
-      self.class.metadata[:embedding]
-    end
-
-    def connection_db_config
-      db_base_model.connection_db_config
-    end
-
-    def db_base_model
-      if geo_migration?
-        Geo::TrackingBase
-      elsif embedding_migration?
-        ::Embedding::ApplicationRecord
-      else
-        raise "unknown database migration"
-      end
+    def geo_db_config
+      Geo::TrackingBase.connection_db_config
     end
   end
 end
