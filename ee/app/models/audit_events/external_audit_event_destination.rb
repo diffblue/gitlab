1391775f1658cdcd273d95f6bf165ca754caefe8
Hitesh Raghuvanshi <hraghuvanshi@gitlab.com>
2023-06-02 04:36:36 +0000

    Adding table for instance streaming headers
    Added table and model for instance level audit events
    streaming headers
    
    Changelog: added

--- ee/app/models/audit_events/external_audit_event_destination.rb
+++ ee/app/models/audit_events/external_audit_event_destination.rb
@@ -5,8 +5,6 @@ module AuditEvents
     include ExternallyDestinationable
     include Limitable
 
-    MAXIMUM_HEADER_COUNT = 20
-
     self.limit_name = 'external_audit_event_destinations'
     self.limit_scope = :group
     self.table_name = 'audit_events_external_audit_event_destinations'
@@ -15,12 +13,7 @@ module AuditEvents
     has_many :headers, class_name: 'AuditEvents::Streaming::Header'
     has_many :event_type_filters, class_name: 'AuditEvents::Streaming::EventTypeFilter'
 
-    validate :has_fewer_than_20_headers?
     validate :root_level_group?
-
-    def headers_hash
-      { STREAMING_TOKEN_HEADER_KEY => verification_token }.merge(headers.map(&:to_hash).inject(:merge).to_h)
-    end
 
     # TODO: Remove audit_operation.present? guard clause once we implement names for all the audit event types.
     # Epic: https://gitlab.com/groups/gitlab-org/-/epics/8497
@@ -32,12 +25,6 @@ module AuditEvents
     end
 
     private
-
-    def has_fewer_than_20_headers?
-      return unless headers.count > MAXIMUM_HEADER_COUNT
-
-      errors.add(:headers, "are limited to #{MAXIMUM_HEADER_COUNT} per destination")
-    end
 
     def root_level_group?
       errors.add(:group, 'must not be a subgroup') if group.subgroup?
--- ee/spec/models/audit_events/instance_external_audit_event_destination_spec.rb
+++ ee/spec/models/audit_events/instance_external_audit_event_destination_spec.rb
@@ -13,11 +13,47 @@ RSpec.describe AuditEvents::InstanceExternalAuditEventDestination, feature_categ
 
   it_behaves_like 'includes Limitable concern'
 
+  describe 'Validations' do
+    it { is_expected.to have_many(:headers).class_name('AuditEvents::Streaming::InstanceHeader') }
+
+    it 'can have 20 headers' do
+      create_list(:instance_audit_events_streaming_header, 20, instance_external_audit_event_destination: subject)
+
+      expect(subject).to be_valid
+    end
+
+    it 'can have no more than 20 headers' do
+      create_list(:instance_audit_events_streaming_header, 21, instance_external_audit_event_destination: subject)
+
+      expect(subject).not_to be_valid
+      expect(subject.errors.full_messages).to contain_exactly('Headers are limited to 20 per destination')
+    end
+  end
+
   describe '#headers_hash' do
     subject { destination.headers_hash }
 
-    it do
-      is_expected.to eq({ 'X-Gitlab-Event-Streaming-Token' => destination.verification_token })
+    context "when destination has 2 headers" do
+      before do
+        create(:instance_audit_events_streaming_header, instance_external_audit_event_destination: destination,
+          key: 'X-GitLab-Hello')
+        create(:instance_audit_events_streaming_header, instance_external_audit_event_destination: destination,
+          key: 'X-GitLab-World')
+      end
+
+      it do
+        is_expected.to eq({ 'X-GitLab-Hello' => 'bar',
+                            'X-GitLab-World' => 'bar',
+                            'X-Gitlab-Event-Streaming-Token' => destination.verification_token })
+      end
+    end
+
+    it 'must have a unique destination_url', :aggregate_failures do
+      create(:instance_external_audit_event_destination, destination_url: 'https://example.com/1')
+      dup = build(:instance_external_audit_event_destination, destination_url: 'https://example.com/1')
+
+      expect(dup).to be_invalid
+      expect(dup.errors.full_messages).to include('Destination url has already been taken')
     end
   end
 end
