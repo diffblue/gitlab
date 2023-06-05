83535384f970efb68c399f490064e3620abbed17
David Fernandez <dfernandez@gitlab.com>
2023-06-05 14:43:12 +0000

    Add table for the dependency proxy packages settings
    Add the table and the model for the dependency proxy packages settings.
    
    Changelog: added

---
+++ ee/app/models/dependency_proxy/packages/setting.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module DependencyProxy
+  module Packages
+    class Setting < ApplicationRecord
+      self.primary_key = :project_id
+
+      belongs_to :project, inverse_of: :dependency_proxy_packages_setting
+
+      attr_encrypted :maven_external_registry_username,
+        mode: :per_attribute_iv,
+        key: Settings.attr_encrypted_db_key_base_32,
+        algorithm: 'aes-256-gcm',
+        encode: false,
+        encode_iv: false
+      attr_encrypted :maven_external_registry_password,
+        mode: :per_attribute_iv,
+        key: Settings.attr_encrypted_db_key_base_32,
+        algorithm: 'aes-256-gcm',
+        encode: false,
+        encode_iv: false
+
+      validates :project, presence: true
+      validates :maven_external_registry_url, addressable_url: true, if: :maven_external_registry_url?
+
+      validates :maven_external_registry_username, presence: true, if: :maven_external_registry_password?
+      validates :maven_external_registry_password, presence: true, if: :maven_external_registry_username?
+      validates :maven_external_registry_url,
+        :maven_external_registry_username,
+        :maven_external_registry_password,
+        length: { maximum: 255 }
+
+      validates_with AnyFieldValidator, fields: %w[maven_external_registry_url]
+    end
+  end
+end
