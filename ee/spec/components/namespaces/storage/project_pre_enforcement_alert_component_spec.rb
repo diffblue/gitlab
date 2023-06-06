b90c2e7c8ed9ac3e4ed289b54682e9d0e86b54da
Jason Goodman <jgoodman@gitlab.com>
2023-06-06 22:02:33 +0000

    Apply cost factor for forks to total storage consumed by namespaces
    Apply cost factor for forks to namespace storage limit checks
    Cost factor is not applied to reported storage on usage quotas page

--- ee/spec/components/namespaces/storage/project_pre_enforcement_alert_component_spec.rb
+++ ee/spec/components/namespaces/storage/project_pre_enforcement_alert_component_spec.rb
@@ -84,7 +84,9 @@ RSpec.describe Namespaces::Storage::ProjectPreEnforcementAlertComponent, :saas, 
     before do
       storage = instance_double(
         Namespace::RootStorageStatistics,
-        storage_size: 5.gigabytes
+        storage_size: 5.gigabytes,
+        public_forks_storage_size: 0,
+        internal_forks_storage_size: 0
       )
 
       allow(project.root_ancestor)
