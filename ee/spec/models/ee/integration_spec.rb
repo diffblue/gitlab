7233387a93e1415aea6e472b95472ab67619b817
Luke Duncalfe <lduncalfe@gitlab.com>
2023-06-06 01:28:51 +0000

    Move GitLab for Slack-related model code to FOSS
    This change is part of
    https://gitlab.com/gitlab-org/gitlab/-/issues/355896 to move all GitLab
    for Slack app code to FOSS.
    
    This MR moves model code related to the GitLab for Slack app.

--- ee/spec/models/ee/integration_spec.rb
+++ ee/spec/models/ee/integration_spec.rb
@@ -4,24 +4,13 @@ require 'spec_helper'
 
 RSpec.describe Integration do
   describe '.available_integration_names' do
-    let(:include_saas_only) { true }
-
     subject { described_class.available_integration_names }
 
     before do
-      allow(described_class).to receive(:integration_names).and_return(%w(foo saas_only))
-      allow(described_class).to receive(:saas_only_integration_names).and_return(['saas_only'])
-      allow(described_class).to receive(:include_saas_only?).and_return(include_saas_only)
+      allow(described_class).to receive(:integration_names).and_return(%w(foo))
     end
 
-    it { is_expected.to include('foo', 'saas_only') }
-
-    context 'when instance is not SaaS' do
-      let(:include_saas_only) { false }
-
-      it { is_expected.to include('foo') }
-      it { is_expected.not_to include('saas_only') }
-    end
+    it { is_expected.to include('foo') }
   end
 
   describe '.project_specific_integration_names' do
@@ -30,15 +19,6 @@ RSpec.describe Integration do
 
       expect(described_class.project_specific_integration_names)
         .to include('ee_project_specific_name')
-    end
-  end
-
-  describe '.saas_only_integration_names' do
-    specify do
-      stub_const("EE::#{described_class.name}::EE_SAAS_ONLY_INTEGRATION_NAMES", ['ee_sass_only_name'])
-
-      expect(described_class.saas_only_integration_names)
-        .to eq(['ee_sass_only_name'])
     end
   end
 
