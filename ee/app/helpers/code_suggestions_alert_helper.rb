b10debf86e208285c5c5749c237af05e1418dbae
Paul Gascou-Vaillancourt <pgascouvaillancourt@gitlab.com>
2023-06-08 03:32:20 +0000

    Hide new nav callout for new users
    Users that registered after the new navigation feature flag was fully
    rolled out do not need to see the announcement about it. This adds a
    date check to ensure only users created before June 2nd 2023 get the
    callout.
    
    Changelog: changed

--- ee/app/helpers/code_suggestions_alert_helper.rb
+++ ee/app/helpers/code_suggestions_alert_helper.rb
@@ -6,8 +6,13 @@ module CodeSuggestionsAlertHelper
     return false if cookies[:code_suggestions_alert_dismissed] == 'true'
     return false if Feature.disabled?(:code_suggestions_alert, current_user)
     return true if current_user.blank? # check user is logged in
-    return false unless user_dismissed_before?(::Users::CalloutsHelper::NEW_NAVIGATION_CALLOUT, 30.minutes.ago)
+    return false unless new_nav_callout_hidden?
 
     !current_user.code_suggestions_enabled?
+  end
+
+  def new_nav_callout_hidden?
+    user_dismissed_before?(::Users::CalloutsHelper::NEW_NAVIGATION_CALLOUT, 30.minutes.ago) ||
+      gitlab_com_user_created_after_new_nav_rollout?
   end
 end
