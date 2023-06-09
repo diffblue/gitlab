0545ca6d0cae6be1b90707d3002b41dd934d92e9
Luke Duncalfe <lduncalfe@gitlab.com>
2023-06-09 17:17:46 +0000

    Move frontend, specs and IM slash commands to Core
    This change is part of
    https://gitlab.com/gitlab-org/gitlab/-/issues/355896 to move all GitLab
    for Slack app code to Core.
    
    This MR focuses on moving:
    
    - Incident Management Slash Commands
    - Views
    - Specs
    
    There should be no functional changes.

--- ee/app/assets/javascripts/integrations/constants.js
+++ ee/app/assets/javascripts/integrations/constants.js
@@ -10,17 +10,11 @@ export * from '~/integrations/constants';
 
 export const integrationTriggerEvents = {
   ...integrationTriggerEventsCE,
-  ALERT: 'alert_events',
-  INCIDENT: 'incident_events',
   VULNERABILITY: 'vulnerability_events',
 };
 
 export const integrationTriggerEventTitles = {
   ...integrationTriggerEventTitlesCE,
-  [integrationTriggerEvents.ALERT]: s__('IntegrationEvents|A new, unique alert is recorded'),
-  [integrationTriggerEvents.INCIDENT]: s__(
-    'IntegrationEvents|An incident is created, closed, or reopened',
-  ),
   [integrationTriggerEvents.VULNERABILITY]: s__(
     'IntegrationEvents|A new, unique vulnerability is recorded (available only in GitLab Ultimate)',
   ),
