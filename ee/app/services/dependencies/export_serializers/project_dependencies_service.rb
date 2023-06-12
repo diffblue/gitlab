5ac0a3c30357b979151dfbfeddd80bee877bcb5b
Mehmet Emin INAC <minac@gitlab.com>
2023-06-12 06:38:19 +0000

    Encapsulate dependencies export logic in service layer

---
+++ ee/app/services/dependencies/export_serializers/project_dependencies_service.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+module Dependencies
+  module ExportSerializers
+    class ProjectDependenciesService
+      def self.execute(dependency_list_export)
+        new(dependency_list_export).execute
+      end
+
+      def initialize(dependency_list_export)
+        @dependency_list_export = dependency_list_export
+      end
+
+      def execute
+        DependencyListEntity.represent(dependencies_list, serializer_parameters)
+      end
+
+      private
+
+      attr_reader :dependency_list_export
+
+      delegate :project, :author, to: :dependency_list_export, private: true
+
+      def dependencies_list
+        return [] unless report_fetch_service.able_to_fetch?
+
+        ::Security::DependencyListService.new(pipeline: report_fetch_service.pipeline).execute
+      end
+
+      def report_fetch_service
+        @report_fetch_service ||= ::Security::ReportFetchService.new(project, job_artifacts)
+      end
+
+      def job_artifacts
+        ::Ci::JobArtifact.of_report_type(:dependency_list)
+      end
+
+      def serializer_parameters
+        {
+          request: EntityRequest.new({ project: project, user: author }),
+          build: report_fetch_service.build
+        }
+      end
+    end
+  end
+end
--- ee/spec/workers/dependencies/export_worker_spec.rb
+++ ee/spec/workers/dependencies/export_worker_spec.rb
@@ -10,76 +10,18 @@ RSpec.describe Dependencies::ExportWorker, type: :worker, feature_category: :dep
   let(:worker) { described_class.new }
 
   describe '#perform' do
+    let(:dependency_list_export) { create(:dependency_list_export) }
+
     subject(:export) { worker.perform(dependency_list_export.id) }
 
     before do
-      stub_licensed_features(dependency_scanning: true)
+      allow(Dependencies::ExportService).to receive(:execute)
     end
 
-    context 'when dependency list export has not been started' do
-      let_it_be(:dependency_list_export) { create(:dependency_list_export, project: project, author: user) }
+    it 'delegates the execution to `Dependencies::ExportService`' do
+      export
 
-      it 'generates a file related with empty dependency list export' do
-        export
-
-        latest_record = Dependencies::DependencyListExport.last
-
-        expect(latest_record).to be_finished
-
-        json_parsed = ::Gitlab::Json.parse(latest_record.file.read)
-        expect(json_parsed["dependencies"]).to be_empty
-      end
-
-      it 'schedules Dependencies::DestroyExportWorker' do
-        latest_record = Dependencies::DependencyListExport.last
-
-        expect(Dependencies::DestroyExportWorker).to receive(:perform_in).with(1.hour, latest_record.id)
-
-        export
-      end
-
-      context 'with existing report' do
-        let_it_be(:pipeline) { create(:ee_ci_pipeline, :with_dependency_list_report, project: project) }
-
-        it 'generates a file related with content related to dependency list export' do
-          export
-
-          latest_record = Dependencies::DependencyListExport.last
-
-          expect(latest_record).to be_finished
-
-          json_parsed = ::Gitlab::Json.parse(latest_record.file.read)
-
-          expect(json_parsed["dependencies"]).to be_present
-        end
-      end
-    end
-
-    context 'when dependency list export has been already running' do
-      let_it_be(:dependency_list_export) { create(:dependency_list_export, :running, project: project, author: user) }
-
-      it 'does not generates a file' do
-        export
-
-        latest_record = Dependencies::DependencyListExport.last
-
-        expect(latest_record).to be_running
-        expect(latest_record.file.read).to be_nil
-      end
-
-      it 'does not schedule Dependencies::DestroyExportWorker' do
-        expect(Dependencies::DestroyExportWorker).not_to receive(:perform_in)
-
-        export
-      end
-    end
-
-    context 'when dependency list export does not exist' do
-      subject(:export) { worker.perform(nil) }
-
-      it 'raises exception' do
-        expect { export }.to raise_error(ActiveRecord::RecordNotFound)
-      end
+      expect(Dependencies::ExportService).to have_received(:execute).with(dependency_list_export)
     end
   end
 
