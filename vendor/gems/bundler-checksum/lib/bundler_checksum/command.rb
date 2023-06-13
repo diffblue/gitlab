e6d766a8a2271e540c23ba055a3297fa8765608b
Peter Leitzen <pleitzen@gitlab.com>
2023-06-13 13:35:00 +0200

    BundlerChecksum: Add command to lint gems checksums
    This command checks if every gem has a corresponding checksum stored. It
    does not verify the checksum though.
    
    To avoid failing CI pipelines let lefthook lint gems when `Gemfile.lock`
    has changed and ensure that every gem has a checksum stored

--- vendor/gems/bundler-checksum/lib/bundler_checksum/command.rb
+++ vendor/gems/bundler-checksum/lib/bundler_checksum/command.rb
@@ -3,12 +3,13 @@
 module BundlerChecksum
   module Command
     autoload :Init, File.expand_path("command/init", __dir__)
+    autoload :Lint, File.expand_path("command/lint", __dir__)
     autoload :Verify, File.expand_path("command/verify", __dir__)
     autoload :Helper, File.expand_path("command/helper", __dir__)
 
     def self.execute(args)
       if args.empty?
-        $stderr.puts 'A command must be given [init,update,verify]'
+        $stderr.puts 'A command must be given [init,update,verify,lint]'
       end
 
       case args.first
@@ -16,6 +17,12 @@ module BundlerChecksum
         Init.execute
       when 'update'
         $stderr.puts 'Not implemented, please use init'
+      when 'lint'
+        linted = Lint.execute
+
+        unless linted
+          exit 1
+        end
       when 'verify'
         verified = Verify.execute
 
