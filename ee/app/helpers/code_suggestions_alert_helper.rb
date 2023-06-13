8fc75859c88714eb2bfe26d5cf7703ba7c40ea07
Serhii Yarynovskyi <syarynovskyi@gitlab.com>
2023-06-13 10:03:53 +0000

    Add third party alert
    Add third party warning alert for code suggestions
    
    Changelog: added
    EE: true

--- ee/app/helpers/code_suggestions_alert_helper.rb
+++ ee/app/helpers/code_suggestions_alert_helper.rb
@@ -15,4 +15,14 @@ module CodeSuggestionsAlertHelper
     user_dismissed_before?(::Users::CalloutsHelper::NEW_NAVIGATION_CALLOUT, 30.minutes.ago) ||
       gitlab_com_user_created_after_new_nav_rollout?
   end
+
+  def show_code_suggestions_third_party_alert?(root_ancestor)
+    current_user.present? &&
+      ::Gitlab::CurrentSettings.should_check_namespace_plan? &&
+      Feature.enabled?(:code_suggestions_third_party_alert, current_user) &&
+      show_code_suggestions_third_party_callout? &&
+      current_user.code_suggestions_enabled? &&
+      root_ancestor.code_suggestions &&
+      root_ancestor.third_party_ai_features_enabled
+  end
 end
