25752e39912ec0d2ca56278306789d7e743580af
Smriti Garg <sgarg@gitlab.com>
2023-06-13 05:43:26 +0000

    Changes in specs and group argument
    Rubocop offenses

---
+++ ee/spec/services/ee/personal_access_tokens/create_service_spec.rb
@@ -0,0 +1,75 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe PersonalAccessTokens::CreateService, feature_category: :system_access do
+  shared_examples_for 'an unsuccessfully created token' do
+    it { expect(create_token.success?).to be false }
+    it { expect(create_token.message).to eq('Not permitted to create') }
+    it { expect(token).to be_nil }
+  end
+
+  describe '#execute' do
+    subject(:create_token) { service.execute }
+
+    let(:current_user) { create(:user) }
+    let(:group) { create(:group) }
+    let(:params) do
+      { name: 'Test token', impersonation: false, scopes: [:api], expires_at: Date.today + 1.month, group: group }
+    end
+
+    let(:token) { create_token.payload[:personal_access_token] }
+
+    context 'when target user is a service account' do
+      let(:target_user) { create(:user, :service_account) }
+      let(:service) do
+        described_class.new(current_user: current_user, target_user: target_user,
+          params: params, concatenate_errors: false)
+      end
+
+      context 'when current user is a group owner' do
+        before do
+          group.add_owner(current_user)
+        end
+
+        context 'when the feature is licensed' do
+          before do
+            stub_licensed_features(service_accounts: true)
+          end
+
+          context 'when provisioned by group' do
+            before do
+              target_user.provisioned_by_group_id = group.id
+              target_user.save!
+            end
+
+            it 'creates a token succesfully' do
+              expect(create_token.success?).to be true
+            end
+          end
+
+          context 'when not provisioned by group' do
+            it_behaves_like 'an unsuccessfully created token'
+          end
+        end
+
+        context 'when feature is not licensed' do
+          before do
+            stub_licensed_features(service_accounts: false)
+          end
+
+          it_behaves_like 'an unsuccessfully created token'
+        end
+      end
+
+      context 'when current user is not a group owner' do
+        before do
+          group.add_guest(current_user)
+          stub_licensed_features(service_accounts: true)
+        end
+
+        it_behaves_like 'an unsuccessfully created token'
+      end
+    end
+  end
+end
