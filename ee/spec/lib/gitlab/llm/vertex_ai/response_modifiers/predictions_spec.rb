3a4280fee6ace167cb0b63f904981dcba46315c8
Patrick Bajao <ebajao@gitlab.com>
2023-06-16 17:36:27 +0000

    Support text response in predictions response modifier
    When `Gitlab::Llm::VertexAi::Client#text` is used and the response
    is passed to `Gitlab::Llm::VertexAi::ResponseModifiers::Predictions`,
    `#response_body` will return `nil`. This is because it doesn't
    support the response format of `text`.
    
    Example response:
    
    ```
    {
      "predictions": [
        {
          "citationMetadata": { "citations" : [] },
          "safetyAttributes": {
            "categories": [],
            "blocked": false,
            "scores": []
          },
          "content": "content"
        }
      ]
    }
    ```
    
    This modifies the predictions response modifier to support response
    format.

---
+++ ee/spec/lib/gitlab/llm/vertex_ai/response_modifiers/predictions_spec.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Gitlab::Llm::VertexAi::ResponseModifiers::Predictions, feature_category: :shared do
+  describe '#response_body' do
+    subject(:response_body) { described_class.new(ai_response.to_json).response_body }
+
+    context 'when AI response predictions has candidates' do
+      let(:ai_response) { { predictions: [{ candidates: [{ content: 'response' }] }] } }
+
+      it 'returns content' do
+        expect(response_body).to eq('response')
+      end
+    end
+
+    context 'when AI response predictions has no candidates' do
+      let(:ai_response) { { predictions: [{ content: 'response' }] } }
+
+      it 'returns content' do
+        expect(response_body).to eq('response')
+      end
+    end
+
+    context 'when AI response is nil' do
+      let(:ai_response) { nil }
+
+      it 'returns blank string' do
+        expect(response_body).to be_blank
+      end
+    end
+  end
+
+  describe '#errors' do
+    let(:ai_response) { { error: { message: 'error' } } }
+
+    subject(:errors) { described_class.new(ai_response.to_json).errors }
+
+    it 'returns array of errors' do
+      expect(errors).to eq(['error'])
+    end
+
+    context 'when AI response is nil' do
+      let(:ai_response) { nil }
+
+      it 'returns empty array' do
+        expect(errors).to be_empty
+      end
+    end
+  end
+end
