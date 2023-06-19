cbf5beb2e6b9d4aee34fc00233cd2b506107334f
Eduardo Sanz García <esanz-garcia@gitlab.com>
2023-06-19 16:42:43 +0200

    Enable language switcher in the SAML SSO page
    Show the language switcher in the footer of the SAML single-sign on
    page.
    
    Changelog: changed

--- ee/app/assets/javascripts/pages/groups/sso/index.js
+++ ee/app/assets/javascripts/pages/groups/sso/index.js
@@ -1,8 +1,10 @@
 import { initSamlAuthorize, redirectUserWithSSOIdentity } from 'ee/saml_sso';
 import UsernameValidator from '~/pages/sessions/new/username_validator';
 import initConfirmDanger from '~/init_confirm_danger';
+import { initLanguageSwitcher } from '~/language_switcher';
 
 new UsernameValidator(); // eslint-disable-line no-new
 initSamlAuthorize();
 redirectUserWithSSOIdentity();
 initConfirmDanger();
+initLanguageSwitcher();
