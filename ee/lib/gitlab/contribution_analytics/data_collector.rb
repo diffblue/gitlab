9aaf6ba313ef416597fddc9bf64416192db5240d
Felipe Artur <felipefac@gmail.com>
2023-06-27 16:09:36 -0300

    Refactor contribution analytics data collector
    Prepare ContributionAnalytics::DataCollector to
    eventually support ClickHouse with PostgreSQL.
    
    This splits data formatting method and database
    queries to additional two classes.

--- ee/lib/gitlab/contribution_analytics/data_collector.rb
+++ ee/lib/gitlab/contribution_analytics/data_collector.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-# rubocop: disable CodeReuse/ActiveRecord
-
 module Gitlab
   module ContributionAnalytics
     class DataCollector
@@ -9,114 +7,30 @@ module Gitlab
 
       attr_reader :group, :from, :to
 
+      delegate :users, :totals, to: :data_formatter
+
       def initialize(group:, from: 1.week.ago.to_date, to: Date.current)
         @group = group
         @from = from.beginning_of_day
         @to = to.end_of_day
       end
 
-      def push_by_author_count
-        all_counts.each_with_object({}) do |(event, count), hash|
-          hash[event.author_id] = count if event.target_type.nil? && event.pushed_action?
-        end
-      end
-
-      def issues_created_by_author_count
-        all_counts.each_with_object({}) do |(event, count), hash|
-          hash[event.author_id] = count if event.issue? && event.created_action?
-        end
-      end
-
-      def issues_closed_by_author_count
-        all_counts.each_with_object({}) do |(event, count), hash|
-          hash[event.author_id] = count if event.issue? && event.closed_action?
-        end
-      end
-
-      def merge_requests_closed_by_author_count
-        all_counts.each_with_object({}) do |(event, count), hash|
-          hash[event.author_id] = count if event.merge_request? && event.closed_action?
-        end
-      end
-
-      def merge_requests_created_by_author_count
-        all_counts.each_with_object({}) do |(event, count), hash|
-          hash[event.author_id] = count if event.merge_request? && event.created_action?
-        end
-      end
-
-      def merge_requests_merged_by_author_count
-        all_counts.each_with_object({}) do |(event, count), hash|
-          hash[event.author_id] = count if event.merge_request? && event.merged_action?
-        end
-      end
-
-      def merge_requests_approved_by_author_count
-        all_counts.each_with_object({}) do |(event, count), hash|
-          hash[event.author_id] = count if event.merge_request? && event.approved_action?
-        end
-      end
-
-      def total_events_by_author_count
-        all_counts.each_with_object({}) do |(event, count), hash|
-          hash[event.author_id] ||= 0
-          hash[event.author_id] += count
-        end
-      end
-
-      def users
-        @users ||= User
-          .select(:id, :name, :username)
-          .where(id: total_events_by_author_count.keys)
-          .reorder(:id)
-      end
-      # rubocop: enable CodeReuse/ActiveRecord
-
-      def totals
-        @totals ||= {
-          push: push_by_author_count,
-          issues_created: issues_created_by_author_count,
-          issues_closed: issues_closed_by_author_count,
-          merge_requests_closed: merge_requests_closed_by_author_count,
-          merge_requests_created: merge_requests_created_by_author_count,
-          merge_requests_merged: merge_requests_merged_by_author_count,
-          merge_requests_approved: merge_requests_approved_by_author_count,
-          total_events: total_events_by_author_count
-        }
-      end
-
-      # rubocop: disable CodeReuse/ActiveRecord
-      def base_query
-        cte = Gitlab::SQL::CTE.new(:project_ids,
-          ::Route
-            .where(source_type: 'Project')
-            .where(::Route.arel_table[:path].matches("#{::Route.sanitize_sql_like(group.full_path)}/%", nil, true))
-            .select('source_id AS id'))
-        cte_condition = 'project_id IN (SELECT id FROM project_ids)'
-
-        events_from_date = ::Event
-          .where(cte_condition)
-          .where(Event.arel_table[:created_at].gteq(from))
-          .where(Event.arel_table[:created_at].lteq(to))
-
-        ::Event.with(cte.to_arel).from_union(
-          [
-            events_from_date.where(action: :pushed, target_type: nil),
-            events_from_date.where(
-              action: [:created, :closed, :merged, :approved],
-              target_type: [::MergeRequest.name, ::Issue.name])
-          ], remove_duplicates: false)
-      end
-      # rubocop: enable CodeReuse/ActiveRecord
-
       private
 
-      def all_counts
-        @all_counts ||= raw_counts.transform_keys do |author_id, target_type, action|
+      def events
+        @events ||= raw_counts.transform_keys do |author_id, target_type, action|
           Event.new(author_id: author_id, target_type: target_type, action: action).tap do |event|
             event.readonly!
           end
         end
+      end
+
+      def data_formatter
+        @data_formatter ||= DataFormatter.new(events)
+      end
+
+      def db_data_collector
+        @data_formatter ||= PostgresqlDataCollector.new(group: group, from: from, to: to)
       end
 
       # Format:
@@ -126,7 +40,7 @@ module Gitlab
       # }
       def raw_counts
         Rails.cache.fetch(cache_key, expires_in: 1.minute) do
-          base_query.totals_by_author_target_type_action
+          db_data_collector.totals_by_author_target_type_action
         end
       end
 
---
+++ ee/spec/lib/gitlab/contribution_analytics/data_formatter_spec.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Gitlab::ContributionAnalytics::DataFormatter, feature_category: :value_stream_management do
+  let_it_be(:group) { create(:group) }
+  let_it_be(:project1) { create(:project, group: group) }
+  let_it_be(:project2) { create(:project, group: group) }
+  let_it_be(:user_1) { create(:user) }
+  let_it_be(:user_2) { create(:user) }
+  let_it_be(:issue) { create(:closed_issue, project: project1) }
+  let_it_be(:mr) { create(:merge_request, source_project: project2) }
+
+  let(:data) do
+    Gitlab::ContributionAnalytics::DataCollector.new(group: group).send(:events)
+  end
+
+  before_all do
+    create(:event, :closed, project: project1, target: issue, author: user_1)
+    create(:event, :created, project: project2, target: mr, author: user_1)
+    create(:event, :approved, project: project2, target: mr, author: user_1)
+    create(:event, :closed, project: project2, target: mr, author: user_1)
+    create(:event, :pushed, project: project1, target: nil, author: user_1)
+    create(:event, :pushed, project: project1, target: nil, author: user_2)
+  end
+
+  describe '#totals' do
+    it 'returns formatted data for received events' do
+      data_formatter = described_class.new(data)
+
+      expect(data_formatter.totals).to eq({
+        issues_closed: { user_1.id => 1 },
+        issues_created: {},
+        merge_requests_created: { user_1.id => 1 },
+        merge_requests_merged: {},
+        merge_requests_approved: { user_1.id => 1 },
+        merge_requests_closed: { user_1.id => 1 },
+        push: { user_1.id => 1, user_2.id => 1 },
+        total_events: { user_1.id => 5, user_2.id => 1 }
+      })
+    end
+  end
+
+  describe '#users' do
+    it 'returns correct users' do
+      users = described_class.new(data).users
+
+      expect(users).to match_array([user_1, user_2])
+    end
+  end
+end
