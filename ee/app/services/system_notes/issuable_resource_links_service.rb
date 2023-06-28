813d5bb96059cdad7c210238f4cb16637e211996
Ashutosh Gupta <ashutoshgupta1311@gmail.com>
2023-06-28 13:57:35 +0000

    Update file issuable_resource_links_service.rb
    Changelog: other
    EE: true
    
    Signed-off-by: Ashutosh Gupta <ashutoshgupta1311@gmail.com>

--- ee/app/services/system_notes/issuable_resource_links_service.rb
+++ ee/app/services/system_notes/issuable_resource_links_service.rb
@@ -3,14 +3,18 @@
 module SystemNotes
   class IssuableResourceLinksService < ::SystemNotes::BaseService
     def issuable_resource_link_added(resource_link)
-      link_type = resource_link.general? ? 'resource' : resource_link.link_type.capitalize
-      body = format(_("added a %{link_type} link"), link_type: link_type)
-      create_note(NoteSummary.new(noteable, project, author, body, action: 'relate'))
+      issuable_resource_link(resource_link, _("added a %{link_type} link"))
     end
 
     def issuable_resource_link_removed(resource_link)
+      issuable_resource_link(resource_link, _("removed a %{link_type} link"))
+    end
+
+    private
+
+    def issuable_resource_link(resource_link, message)
       link_type = resource_link.general? ? 'resource' : resource_link.link_type.capitalize
-      body = format(_("removed a %{link_type} link"), link_type: link_type)
+      body = format(message, link_type: link_type)
       create_note(NoteSummary.new(noteable, project, author, body, action: 'relate'))
     end
   end
