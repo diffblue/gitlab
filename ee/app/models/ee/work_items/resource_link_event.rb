0283e4a79ae14519d0ebf2054b88770d9c060832
Eulyeon Ko <eko@gitlab.com>
2023-07-04 17:46:21 +0900

    Migrate resource_link_events to ghost users
    When a user is destroyed, all rows that reference the user
    should either accept NULL for user or have the reference
    migrated to ghost user.
    
    `resource_link_events` table had a non-nullable reference to
    `users` table but did not have the required migration step
    to ghost users in MigrateRecordsToGhostUserService
    and it has blocked the process of user deletion.
    
    Changelog: security
    EE: true

--- ee/app/models/ee/work_items/resource_link_event.rb
+++ ee/app/models/ee/work_items/resource_link_event.rb
@@ -6,6 +6,8 @@ module EE
       extend ActiveSupport::Concern
 
       prepended do
+        belongs_to :user
+
         scope :aliased_for_timebox_report, -> do
           select("'link' AS event_type", "id", "created_at", "child_work_item_id as value", "action", "issue_id")
         end
