f7d738effa774f904017614e9a6098f015f7885e
Marcos Rocha <mrocha@gitlab.com>
2023-07-05 19:49:46 +0000

    Add Security Policy Default source
    This MR adds a new config source to allow scan
    execution policies to create pipelines to
    enforce security scans even if the Auto DevOps
    is disabled and no .gitlab-ci.yml is available.
    
    Changelog: added
    MR: https://gitlab.com/gitlab-org/gitlab/-/merge_requests/121087
    EE: true

---
+++ ee/lib/gitlab/ci/project_config/security_policy_default.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module Gitlab
+  module Ci
+    class ProjectConfig
+      class SecurityPolicyDefault < Gitlab::Ci::ProjectConfig::Source
+        def content
+          return unless @project.licensed_feature_available?(:security_orchestration_policies)
+          return unless security_policies_enforced?
+
+          # We merge the security scans with the pipeline configuration in ee/lib/ee/gitlab/ci/config_ee.rb.
+          # An empty config with no content is enough to trigger the merge process when the Auto DevOps is disabled
+          # and no .gitlab-ci.yml is present.
+          YAML.dump(nil)
+        end
+        strong_memoize_attr :content
+
+        def source
+          :security_policies_default_source
+        end
+
+        private
+
+        def security_policies_enforced?
+          ::Feature.enabled?(:scan_execution_policy_pipelines, @project) && active_scan_execution_policies?
+        end
+
+        def active_scan_execution_policies?
+          ::Gitlab::Security::Orchestration::ProjectPolicyConfigurations.new(@project).all
+                                                                        .to_a
+                                                                        .flat_map(&:active_scan_execution_policies)
+                                                                        .any?
+        end
+      end
+    end
+  end
+end
