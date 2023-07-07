850eae0872e7282a41c7b78c50b4c4fb9e352143
sameer shaik <sshaik@gitlab.com>
2023-07-07 14:53:24 +0000

    Expose enterprise group attributes
    Expose enterprise group attribites in the admin users API
    
    Changelog: changed
    MR: https://gitlab.com/gitlab-org/gitlab/-/merge_requests/125764
    EE: true

--- ee/lib/ee/api/entities/user_with_admin.rb
+++ ee/lib/ee/api/entities/user_with_admin.rb
@@ -10,6 +10,16 @@ module EE
           expose :using_license_seat?, as: :using_license_seat
           expose :auditor, as: :is_auditor, if: ->(_instance, _opts) { ::License.feature_available?(:auditor_user) }
           expose :provisioned_by_group_id, if: ->(_instance, _opts) { ::License.feature_available?(:group_saml) }
+
+          expose :enterprise_group_id, if: ->(_instance, _opts) { ::License.feature_available?(:group_saml) } do |user|
+            user.user_detail&.enterprise_group_id
+          end
+
+          expose :enterprise_group_associated_at, if: ->(_instance, _opts) {
+                                                        ::License.feature_available?(:group_saml)
+                                                      } do |user|
+            user.user_detail&.enterprise_group_associated_at
+          end
         end
       end
     end
