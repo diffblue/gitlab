fffbf6f3391a4c28c842ce025443f3432027eb5a
Vijay Hawoldar <vhawoldar@gitlab.com>
2023-07-14 16:35:31 +0100

    Removes namespace_limits_admin_dashboard FF
    With the completion of the admin UI for namespace limit controls, we can
    remove the feature flag that was preventing it being available
    
    Changelog: added
    EE: true

--- ee/app/controllers/admin/namespace_limits_controller.rb
+++ ee/app/controllers/admin/namespace_limits_controller.rb
@@ -5,7 +5,6 @@ module Admin
     feature_category :consumables_cost_management
     urgency :low
 
-    before_action :check_dashboard_enabled
     before_action :check_gitlab_com
 
     def index; end
@@ -14,10 +13,6 @@ module Admin
 
     def check_gitlab_com
       not_found unless ::Gitlab::CurrentSettings.should_check_namespace_plan?
-    end
-
-    def check_dashboard_enabled
-      not_found unless ::Feature.enabled?(:namespace_limits_admin_dashboard, current_user)
     end
   end
 end
