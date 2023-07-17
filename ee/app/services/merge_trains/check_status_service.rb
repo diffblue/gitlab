09e90bac604fef5da0c33c34c9ff2b65a9791e90
drew stachon <730684-drew@users.noreply.gitlab.com>
2023-07-17 19:58:02 +0000

    Add Project#merge_train_for helper method
    This allows for the project-based feature-enabled
    check to live in the Project class

--- ee/app/services/merge_trains/check_status_service.rb
+++ ee/app/services/merge_trains/check_status_service.rb
@@ -3,16 +3,17 @@
 module MergeTrains
   class CheckStatusService < BaseService
     def execute(target_project, target_branch, newrev)
-      return unless target_project.merge_trains_enabled?
+      train = target_project.merge_train_for(target_branch)
 
-      train = MergeTrains::Train.new(target_project.id, target_branch)
+      return unless train
 
       # If the new revision doesn't exist in the merge train history,
       # that means there was an unexpected commit came from out of merge train cycle.
-      unless train.sha_exists_in_history?(newrev)
-        car = train.first_car
-        car.outdate_pipeline if car
-      end
+      return if train.sha_exists_in_history?(newrev)
+
+      # If the sha doesn't exist and there is an unexpected commit, outdate_pipeline
+      # to create a new merge commit and restart CI
+      train.first_car&.outdate_pipeline
     end
   end
 end
