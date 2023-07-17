f1c7b1c36791102542ccf95513000f025d269b38
Miguel Rincon <mrincon@gitlab.com>
2023-07-17 16:15:47 +0200

    Enable "search as tokens" in runner search
    This change improves the handling of spaces in the runner search.
    Previously, users had to enter each word separately when searching for
    runner that matched a description.
    
    With this change, users are able to input a simple search string to get
    matching runners.
    
    Changelog: changed

--- ee/app/assets/javascripts/ci/runner/components/search_tokens/upgrade_status_token_config.js
+++ ee/app/assets/javascripts/ci/runner/components/search_tokens/upgrade_status_token_config.js
@@ -16,28 +16,17 @@ const getToken = () => {
     return null;
   }
 
-  const options = [
-    { value: UPGRADE_STATUS_AVAILABLE, title: s__('Runners|Available') },
-    { value: UPGRADE_STATUS_RECOMMENDED, title: s__('Runners|Recommended') },
-    { value: UPGRADE_STATUS_NOT_AVAILABLE, title: s__('Runners|Up to date') },
-  ];
-
   return {
     icon: 'upgrade',
     title: s__('Runners|Upgrade Status'),
     type: PARAM_KEY_UPGRADE_STATUS,
     token: BaseToken,
     unique: true,
-    options: options.map(({ value, title }) => ({
-      value,
-      // Replace whitespace with a special character to avoid
-      // splitting this value.
-      // Replacing in each option, as translations may also
-      // contain spaces!
-      // see: https://gitlab.com/gitlab-org/gitlab/-/issues/344142
-      // see: https://gitlab.com/gitlab-org/gitlab-ui/-/issues/1438
-      title: title.replace(/\s/g, '\u00a0'),
-    })),
+    options: [
+      { value: UPGRADE_STATUS_AVAILABLE, title: s__('Runners|Available') },
+      { value: UPGRADE_STATUS_RECOMMENDED, title: s__('Runners|Recommended') },
+      { value: UPGRADE_STATUS_NOT_AVAILABLE, title: s__('Runners|Up to date') },
+    ],
     operators: OPERATORS_IS,
   };
 };
