dd24c2457bf93871ece4126f4b63999a0dedd2e2
Aaron Huntsman <ahuntsman@gitlab.com>
2023-07-19 14:16:39 +0000

    Update PasswordsController to account for missing email
    - set audit scope to ::Gitlab::Audit::UnauthenticatedAuthor
      when password reset is requested for nonexistent email
    - use have_attributes in specs

--- ee/app/controllers/ee/passwords_controller.rb
+++ ee/app/controllers/ee/passwords_controller.rb
@@ -11,16 +11,18 @@ module EE
     private
 
     def log_audit_event
-      ::AuditEventService.new(
-        current_user,
-        resource,
-        action: :custom,
-        custom_message: 'Ask for password reset',
+      unauth_author = ::Gitlab::Audit::UnauthenticatedAuthor.new
+      requester = resource || ::User.new(id: unauth_author.id)
+
+      ::Gitlab::Audit::Auditor.audit({
+        name: "password_reset_requested",
+        author: current_user || unauth_author,
+        scope: requester,
+        target: requester,
+        target_details: resource_params[:email],
+        message: "Ask for password reset",
         ip_address: request.remote_ip
-      ).for_user(
-        full_path: resource_params[:email],
-        entity_id: nil
-      ).unauth_security_event
+      })
     end
   end
 end
