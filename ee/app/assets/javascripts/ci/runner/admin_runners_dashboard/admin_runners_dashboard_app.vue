ed5063a02f3e3b5e2a5227f08bd6305cc0cb2eeb
Miguel Rincon <mrincon@gitlab.com>
2023-07-20 11:46:51 +0200

    Add help text to indicate dashboard limitation
    Adds a text to the runner fleet dashboard explaining that the metrics
    are constrained to only instance runners.

--- ee/app/assets/javascripts/ci/runner/admin_runners_dashboard/admin_runners_dashboard_app.vue
+++ ee/app/assets/javascripts/ci/runner/admin_runners_dashboard/admin_runners_dashboard_app.vue
@@ -12,6 +12,9 @@ export default {
 <template>
   <div>
     <h1 class="gl-font-size-h1">{{ s__('Runners|Fleet dashboard') }}</h1>
+    <p>
+      {{ s__('Runners|Use the dashboard to view performance statistics of your runner fleet.') }}
+    </p>
 
     <div class="gl-md-display-flex gl-gap-3">
       <runner-wait-times class="gl-flex-grow-1 gl-flex-basis-0 gl-mb-3" />
