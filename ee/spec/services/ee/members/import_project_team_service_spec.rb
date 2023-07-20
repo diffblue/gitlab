4921cd19b9b5a2a5d1b808ce5b1b7f68523ab520
Doug Stull <dstull@gitlab.com>
2023-07-20 01:38:29 +0000

    Add error handling to import project members modal
    - better error handling
    
    Changelog: other
    EE: true

--- ee/spec/services/ee/members/import_project_team_service_spec.rb
+++ ee/spec/services/ee/members/import_project_team_service_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Members::ImportProjectTeamService, feature_category: :groups_and_
     let(:source_project_id) { source_project.id }
     let(:target_project_id) { target_project.id }
 
-    subject { described_class.new(user, { id: target_project_id, project_id: source_project_id }) }
+    subject(:import) { described_class.new(user, { id: target_project_id, project_id: source_project_id }) }
 
     before_all do
       source_project.add_guest(user)
@@ -20,23 +20,33 @@ RSpec.describe Members::ImportProjectTeamService, feature_category: :groups_and_
 
     context 'when the project team import fails' do
       context 'when the target project has locked their membership' do
-        context 'via the parent group' do
+        context 'for locking via the parent group' do
           before do
             target_project.group.update!(membership_lock: true)
           end
 
-          it 'returns false' do
-            expect(subject.execute).to be(false)
+          it 'returns unsuccessful response' do
+            result = import.execute
+
+            expect(result).to be_a(ServiceResponse)
+            expect(result.error?).to be(true)
+            expect(result.message).to eq('Forbidden')
+            expect(result.reason).to eq(:unprocessable_entity)
           end
         end
 
-        context 'via LDAP' do
+        context 'for locking via LDAP' do
           before do
             stub_application_setting(lock_memberships_to_ldap: true)
           end
 
-          it 'returns false' do
-            expect(subject.execute).to be(false)
+          it 'returns unsuccessful response' do
+            result = import.execute
+
+            expect(result).to be_a(ServiceResponse)
+            expect(result.error?).to be(true)
+            expect(result.message).to eq('Forbidden')
+            expect(result.reason).to eq(:unprocessable_entity)
           end
         end
       end
