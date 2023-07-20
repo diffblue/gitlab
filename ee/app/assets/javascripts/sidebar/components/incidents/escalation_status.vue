73bd55dd4a69a2038242fc1b9eb1f49d35532787
Mughees Pervaiz <mr.mb@tuta.io>
2023-07-20 21:50:04 +0000

    Migrate "escalation_status.vue" to  GlCollapsibleListbox

--- ee/app/assets/javascripts/sidebar/components/incidents/escalation_status.vue
+++ ee/app/assets/javascripts/sidebar/components/incidents/escalation_status.vue
@@ -1,46 +1,34 @@
 <script>
-import { GlDropdownDivider, GlDropdownSectionHeader, GlIcon, GlLink, GlPopover } from '@gitlab/ui';
 import EscalationStatus from '~/sidebar/components/incidents/escalation_status.vue';
 import glFeatureFlagMixin from '~/vue_shared/mixins/gl_feature_flags_mixin';
-import { helpPagePath } from '~/helpers/help_page_helper';
-import { i18nStatusText } from '../../constants';
+import { i18nStatusHeaderText, STATUS_SUBTEXTS } from '../../constants';
 
 export default {
-  i18n: i18nStatusText,
-  docsPath: helpPagePath('operations/incident_management/manage_incidents.html', {
-    anchor: 'change-status',
-  }),
+  i18n: i18nStatusHeaderText,
+
   components: {
     EscalationStatus,
-    GlDropdownDivider,
-    GlDropdownSectionHeader,
-    GlIcon,
-    GlPopover,
-    GlLink,
   },
   mixins: [glFeatureFlagMixin()],
-  data() {
-    return { isHelpVisible: false };
-  },
   computed: {
-    showHeader() {
+    headerText() {
+      return this.escalationPoliciesEnabled ? this.$options.i18n : '';
+    },
+    statusSubtexts() {
+      return this.escalationPoliciesEnabled ? STATUS_SUBTEXTS : {};
+    },
+
+    escalationPoliciesEnabled() {
       return this.glFeatures.escalationPolicies;
     },
   },
   methods: {
-    // Pass through to wrapped component
     show() {
       this.$refs.escalationStatus.show();
     },
     // Pass through to wrapped component
     hide() {
       this.$refs.escalationStatus.hide();
-    },
-    showPopover() {
-      this.isHelpVisible = true;
-    },
-    hidePopover() {
-      this.isHelpVisible = false;
     },
   },
 };
@@ -49,38 +37,9 @@ export default {
 <template>
   <escalation-status
     ref="escalationStatus"
+    :header-text="headerText"
+    :status-subtexts="statusSubtexts"
     v-bind="$attrs"
-    :prevent-dropdown-close="isHelpVisible"
     v-on="$listeners"
-  >
-    <template v-if="showHeader" #header>
-      <gl-dropdown-section-header id="escalation-status-dropdown" class="gl-mt-n2">
-        <div class="gl-text-center">
-          {{ $options.i18n.dropdownHeader }}
-          <gl-icon id="escalation-status-help" class="gl-ml-2 gl-text-blue-600" name="question-o" />
-          <gl-popover
-            ref="popover"
-            :title="$options.i18n.dropdownHeader"
-            placement="left"
-            target="escalation-status-help"
-            boundary="viewport"
-            @show="showPopover"
-            @hide="hidePopover"
-          >
-            <p @click.stop.prevent>{{ $options.i18n.dropdownInfo }}</p>
-            <gl-link
-              ref="link"
-              :aria-label="$options.i18n.learnMoreFull"
-              class="gl-font-sm"
-              :href="$options.docsPath"
-              target="_blank"
-            >
-              {{ $options.i18n.learnMoreShort }}
-            </gl-link>
-          </gl-popover>
-        </div>
-      </gl-dropdown-section-header>
-      <gl-dropdown-divider />
-    </template>
-  </escalation-status>
+  />
 </template>
--- ee/spec/frontend/sidebar/components/incidents/escalation_status_spec.js
+++ ee/spec/frontend/sidebar/components/incidents/escalation_status_spec.js
@@ -1,20 +1,16 @@
-import { GlDropdownSectionHeader, GlPopover, GlLink } from '@gitlab/ui';
-import { nextTick } from 'vue';
-import { mountExtended } from 'helpers/vue_test_utils_helper';
+import { shallowMount } from '@vue/test-utils';
 import EscalationStatus from 'ee/sidebar/components/incidents/escalation_status.vue';
 import InnerEscalationStatus from '~/sidebar/components/incidents/escalation_status.vue';
 import { STATUS_TRIGGERED } from '~/sidebar/constants';
+import { i18nStatusHeaderText, STATUS_SUBTEXTS } from 'ee/sidebar/constants';
 
 describe('EscalationStatus', () => {
   let wrapper;
 
   function createComponent(glFeatures = {}) {
-    wrapper = mountExtended(EscalationStatus, {
+    wrapper = shallowMount(EscalationStatus, {
       propsData: {
         status: STATUS_TRIGGERED,
-      },
-      stubs: {
-        InnerEscalationStatus,
       },
       provide: {
         glFeatures: {
@@ -25,61 +21,33 @@ describe('EscalationStatus', () => {
     });
   }
 
-  const findDropdownHeaderComponent = () => wrapper.findComponent(GlDropdownSectionHeader);
-  const findPopover = () => wrapper.findComponent(GlPopover);
-  const findLearnMoreLink = () => wrapper.findComponent(GlLink);
   const findInnerStatusComponent = () => wrapper.findComponent(InnerEscalationStatus);
-  const openPopover = async () => {
-    await findPopover().vm.$emit('show');
-    await nextTick();
-  };
-  const closePopover = async () => {
-    await findPopover().vm.$emit('hide');
-    await nextTick();
-  };
 
-  describe('help popover', () => {
+  describe('when licensed features enabled', () => {
     beforeEach(() => {
       createComponent();
     });
 
-    it('renders a popover', () => {
-      expect(findPopover().props('title')).toBe('Assign paging status');
-      expect(findPopover().text()).toContain('Setting the status');
+    it('provides the dropdown header', () => {
+      expect(findInnerStatusComponent().props('headerText')).toBe(i18nStatusHeaderText);
     });
 
-    it('shows the Learn More link', () => {
-      expect(findLearnMoreLink().text()).toBe('Learn More.');
-      expect(findLearnMoreLink().attributes('href')).toContain(
-        'manage_incidents.html#change-status',
-      );
-    });
-
-    it('prevents self from being removed from DOM when open', async () => {
-      await openPopover();
-
-      expect(findInnerStatusComponent().props('preventDropdownClose')).toBe(true);
-    });
-
-    it('allows self to be removed from DOM when closed', async () => {
-      await openPopover();
-      await closePopover();
-
-      expect(findInnerStatusComponent().props('preventDropdownClose')).toBe(false);
+    it('provides the status subtexts', () => {
+      expect(findInnerStatusComponent().props('statusSubtexts')).toBe(STATUS_SUBTEXTS);
     });
   });
 
-  describe('licensed features', () => {
-    it('when licensed, renders the dropdown header', () => {
-      createComponent();
-
-      expect(findDropdownHeaderComponent().exists()).toBe(true);
+  describe('when licensed features disabled', () => {
+    beforeEach(() => {
+      createComponent({ escalationPolicies: false });
     });
 
-    it('when unlicensed, does not render the dropdown header', () => {
-      createComponent({ escalationPolicies: false });
+    it("doesn't provide the dropdown header", () => {
+      expect(findInnerStatusComponent().props('headerText')).toBe('');
+    });
 
-      expect(findDropdownHeaderComponent().exists()).toBe(false);
+    it("doesn't provide the status subtexts", () => {
+      expect(findInnerStatusComponent().props('statusSubtexts')).toEqual({});
     });
   });
 });
