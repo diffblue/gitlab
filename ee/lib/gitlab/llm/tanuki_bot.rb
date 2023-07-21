0c67a39ef351aa8a507e893d8c947b7056cb8cd9
Tim Zallmann <tzallmann@gitlab.com>
2023-07-21 12:57:53 +0000

    Fixes for the Documentation Bot Part
    Changelog: removed
    MR: https://gitlab.com/gitlab-org/gitlab/-/merge_requests/127112
    EE: true

--- ee/lib/gitlab/llm/tanuki_bot.rb
+++ ee/lib/gitlab/llm/tanuki_bot.rb
@@ -8,7 +8,8 @@ module Gitlab
       REQUEST_TIMEOUT = 30
       CONTENT_ID_FIELD = 'ATTRS'
       CONTENT_ID_REGEX = /CNT-IDX-(?<id>\d+)/
-      RECORD_LIMIT = 7
+      RECORD_LIMIT = 4
+      MODEL = 'claude-instant-1.1'
 
       def self.execute(current_user:, question:, logger: nil)
         new(current_user: current_user, question: question, logger: logger).execute
@@ -38,7 +39,7 @@ module Gitlab
       def initialize(current_user:, question:, logger: nil)
         @current_user = current_user
         @question = question
-        @logger = logger || Gitlab::AppJsonLogger.build
+        @logger = logger || Gitlab::Llm::Logger.build
         @correlation_id = Labkit::Correlation::CorrelationId.current_id
       end
 
@@ -64,75 +65,27 @@ module Gitlab
         @client ||= ::Gitlab::Llm::Anthropic::Client.new(current_user)
       end
 
-      def build_initial_prompts(search_documents)
-        search_documents.to_h do |doc|
-          prompt = Gitlab::Llm::Anthropic::Templates::TanukiBot
-            .initial_prompt(question: question, content: doc[:content])
-
-          [doc, prompt[:prompt]]
-        end
-      end
-
-      def send_initial_prompt(doc:, prompt:)
-        result = client.complete(prompt: prompt)
-
-        info(
-          document_id: doc[:id],
-          prompt: prompt,
-          status_code: result.code,
-          openai_completions_response: result.parsed_response,
-          message: 'Initial prompt request'
-        )
-
-        raise result.dig('error', 'message') || "Initial prompt request failed with '#{result}'" unless result.success?
-
-        doc.merge(extracted_text: result['completion'].to_s.strip)
-      end
-
-      def sequential_competion(search_documents)
-        prompts = build_initial_prompts(search_documents)
-
-        prompts.map do |doc, prompt|
-          send_initial_prompt(doc: doc, prompt: prompt)
-        end
-      end
-
-      def parallel_completion(search_documents)
-        prompts = build_initial_prompts(search_documents)
-
-        threads = prompts.map do |doc, prompt|
-          Thread.new do
-            send_initial_prompt(doc: doc, prompt: prompt)
-          end
-        end
-
-        threads.map(&:value)
-      end
-
       def get_completions(search_documents)
-        documents = if Feature.enabled?(:tanuki_bot_parallel)
-                      parallel_completion(search_documents)
-                    else
-                      sequential_competion(search_documents)
-                    end
-
         final_prompt = Gitlab::Llm::Anthropic::Templates::TanukiBot
-          .final_prompt(question: question, documents: documents)
+          .final_prompt(question: question, documents: search_documents)
 
         final_prompt_result = client.complete(
-          prompt: final_prompt[:prompt]
+          prompt: final_prompt[:prompt],
+          options: {
+            model: "claude-instant-1.1"
+          }
         )
 
         unless final_prompt_result.success?
           raise final_prompt_result.dig('error', 'message') || "Final prompt failed with '#{final_prompt_result}'"
         end
 
-        info(
+        logger.debug(message: "Got Final Result", content: {
           prompt: final_prompt[:prompt],
           status_code: final_prompt_result.code,
           openai_completions_response: final_prompt_result.parsed_response,
           message: 'Final prompt request'
-        )
+        })
 
         final_prompt_result
       end
