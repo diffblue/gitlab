0d780d418b7ec7741538f2bb2539f6d78f506e79
Ross Byrne <robyrne@gitlab.com>
2023-07-21 16:00:38 +0100

    Cleaned up code_suggestions_alert and feature flag
    Changelog: changed
    EE: true

--- ee/app/helpers/code_suggestions_alert_helper.rb
+++ ee/app/helpers/code_suggestions_alert_helper.rb
@@ -1,21 +1,6 @@
 # frozen_string_literal: true
 
 module CodeSuggestionsAlertHelper
-  def show_code_suggestions_alert?
-    return false unless ::Gitlab::CurrentSettings.should_check_namespace_plan?
-    return false if cookies[:code_suggestions_alert_dismissed] == 'true'
-    return false if Feature.disabled?(:code_suggestions_alert, current_user)
-    return true if current_user.blank? # check user is logged in
-    return false unless new_nav_callout_hidden?
-
-    !current_user.code_suggestions_enabled?
-  end
-
-  def new_nav_callout_hidden?
-    user_dismissed_before?(::Users::CalloutsHelper::NEW_NAVIGATION_CALLOUT, 30.minutes.ago) ||
-      gitlab_com_user_created_after_new_nav_rollout?
-  end
-
   def show_code_suggestions_third_party_alert?(root_ancestor)
     current_user.present? &&
       ::Gitlab::CurrentSettings.should_check_namespace_plan? &&
--- ee/spec/views/layouts/project.html.haml_spec.rb
+++ ee/spec/views/layouts/project.html.haml_spec.rb
@@ -18,24 +18,4 @@ RSpec.describe 'layouts/project', feature_category: :groups_and_projects do
       expect(rendered).to render_template('projects/_free_user_cap_alert')
     end
   end
-
-  context 'when code suggestions alert is present' do
-    before do
-      allow(view).to receive(:show_code_suggestions_alert?).and_return(true)
-    end
-
-    it 'renders a alert with links to user profile preferences' do
-      render
-
-      expect(rendered).to have_content('Get started with Code Suggestions')
-      expect(rendered)
-        .to have_link('user profile preferences', href: profile_preferences_path(anchor: 'code-suggestions-settings'))
-      help_url = help_page_path(
-        'user/project/repository/code_suggestions'
-      )
-      expect(rendered).to have_link('see the documentation', href: help_url)
-      profile_url = profile_preferences_path(anchor: 'code-suggestions-settings')
-      expect(rendered).to have_link(s_('CodeSuggestionsAlert|Enable Code Suggestions'), href: profile_url)
-    end
-  end
 end
