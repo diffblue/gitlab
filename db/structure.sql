25ce6841a01a8a06fd8ce7191557a06f25eb29cf
Robert May <rmay@gitlab.com>
2023-07-24 16:15:30 +0100

    Add index to projects on namespace_id and repository_size_limit
    Changelog: performance

--- db/structure.sql
+++ db/structure.sql
@@ -32718,6 +32718,8 @@ CREATE INDEX index_projects_on_name_trigram ON projects USING gin (name gin_trgm
 
 CREATE INDEX index_projects_on_namespace_id_and_id ON projects USING btree (namespace_id, id);
 
+CREATE INDEX index_projects_on_namespace_id_and_repository_size_limit ON projects USING btree (namespace_id, repository_size_limit);
+
 CREATE INDEX index_projects_on_path_and_id ON projects USING btree (path, id);
 
 CREATE INDEX index_projects_on_path_trigram ON projects USING gin (path gin_trgm_ops);
