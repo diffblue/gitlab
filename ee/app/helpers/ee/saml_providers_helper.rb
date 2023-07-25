0f5aef3451c85dd58ac423bc4bcf87d2c2107023
Alexander Turinske <aturinske@gitlab.com>
2023-07-25 06:41:12 +0000

    Update SAML buttons to pajamas
    - update saml_link method to return button or link
    
    Changelog: changed
    
    EE: true

--- ee/app/helpers/ee/saml_providers_helper.rb
+++ ee/app/helpers/ee/saml_providers_helper.rb
@@ -3,13 +3,23 @@
 module EE
   module SamlProvidersHelper
     def saml_link_for_provider(text, provider, **args)
-      saml_link(text, provider.group.full_path, **args)
+      saml_button(text, provider.group.full_path, **args)
     end
 
-    def saml_link(text, group_path, redirect: nil, html_class: 'btn', id: nil, data: nil)
-      redirect ||= group_path(group_path)
-      url = omniauth_authorize_path(:user, :group_saml, group_path: group_path, redirect_to: redirect)
+    def saml_button(text, group_path, redirect: nil, variant: :default, block: false, **button_options)
+      url = saml_url(group_path, redirect)
+      render Pajamas::ButtonComponent.new(
+        href: url,
+        method: :post,
+        variant: variant,
+        block: block,
+        button_options: button_options) do
+        text
+      end
+    end
 
+    def saml_link(text, group_path, redirect: nil, html_class: '', id: nil, data: nil)
+      url = saml_url(group_path, redirect)
       link_to(text, url, method: :post, class: html_class, id: id, data: data)
     end
 
