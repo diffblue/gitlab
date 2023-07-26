c03aee648c31c2d74b0fb39839b16d03ccd17c7b
Vasilii Iakliushin <viakliushin@gitlab.com>
2023-07-26 15:13:49 +0200

    Fix accidental commits collection mutation
    Contributes to https://gitlab.com/gitlab-org/gitlab/-/issues/356094
    
    Introduced in
    https://gitlab.com/gitlab-org/gitlab/-/merge_requests/123950
    
    **Problem**
    
    `#prepend` method executes in-place update for commits collection. That
    breaks code executed after it.
    
    **Solution**
    
    Use a copy of commit collection to avoid an accidental mutation.
    
    EE: true

--- ee/spec/lib/gitlab/checks/changes_access_spec.rb
+++ ee/spec/lib/gitlab/checks/changes_access_spec.rb
@@ -2,7 +2,7 @@
 
 require 'spec_helper'
 
-RSpec.describe Gitlab::Checks::ChangesAccess do
+RSpec.describe Gitlab::Checks::ChangesAccess, feature_category: :source_code_management do
   describe '#validate!' do
     include_context 'push rules checks context'
 
@@ -33,6 +33,64 @@ RSpec.describe Gitlab::Checks::ChangesAccess do
       end
 
       subject.validate!
+    end
+
+    context 'with denylisted files check' do
+      let(:push_rule) { create(:push_rule, prevent_secrets: true) }
+
+      context 'when the repository was empty' do
+        let(:project) { create(:project, :empty_repo, push_rule: push_rule) }
+        let(:user) { project.first_owner }
+
+        let(:create_file_with_secrets) do
+          project.repository.create_file(user, file_path, "commit #{file_path}", message: "commit #{file_path}",
+            branch_name: "master")
+        end
+
+        let(:create_readme) do
+          project.repository.create_file(user, 'README', "README", message: "commit README", branch_name: "master")
+        end
+
+        let(:new_commits) do
+          [project.repository.commit(create_file_with_secrets), project.repository.commit(create_readme)]
+        end
+
+        let(:oldrev) { '0000000000000000000000000000000000000000' }
+        let(:newrev) { create_readme }
+
+        before do
+          allow(project.repository).to receive(:empty?).and_return(true)
+        end
+
+        context 'when file contains secrets' do
+          let(:file_path) { 'aws/credentials' }
+
+          it "returns an error if a new or renamed filed doesn't match the file name regex" do
+            expect do
+              subject.validate!
+            end.to raise_error(Gitlab::GitAccess::ForbiddenError,
+              /File name #{file_path} was prohibited by the pattern/)
+          end
+
+          context 'when feature flag "verify_push_rules_for_first_commit" is disabled' do
+            before do
+              stub_feature_flags(verify_push_rules_for_first_commit: false)
+            end
+
+            it 'does not raise an error' do
+              expect(subject.validate!).to be_truthy
+            end
+          end
+        end
+
+        context 'when file is permitted' do
+          let(:file_path) { 'aws/not_credentials' }
+
+          it 'does not raise an error' do
+            expect(subject.validate!).to be_truthy
+          end
+        end
+      end
     end
   end
 end
