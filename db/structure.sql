12ded410b7dac2fa2a08e25ec215f41c1579afd8
Lucas Charles <me@lucascharles.me>
2023-07-27 13:44:22 -0700

    fix: Drop unnecessary unique index on vulnerability_signatures
    Unique index is unneeded when vulnerabilities may have multiple
    signatures.
    
    Relates to https://gitlab.com/gitlab-org/gitlab/-/issues/419737
    
    Changelog: fixed

--- db/structure.sql
+++ db/structure.sql
@@ -30192,8 +30192,6 @@ CREATE UNIQUE INDEX idx_uniq_analytics_dashboards_pointers_on_project_id ON anal
 
 CREATE INDEX idx_user_details_on_provisioned_by_group_id_user_id ON user_details USING btree (provisioned_by_group_id, user_id);
 
-CREATE UNIQUE INDEX idx_vuln_signatures_on_occurrences_id_and_signature_sha ON vulnerability_finding_signatures USING btree (finding_id, signature_sha);
-
 CREATE UNIQUE INDEX idx_vuln_signatures_uniqueness_signature_sha ON vulnerability_finding_signatures USING btree (finding_id, algorithm_type, signature_sha);
 
 CREATE INDEX idx_vulnerabilities_on_project_id_and_id_active_cis_dft_branch ON vulnerabilities USING btree (project_id, id) WHERE ((report_type = 7) AND (state = ANY (ARRAY[1, 4])) AND (present_on_default_branch IS TRUE));
