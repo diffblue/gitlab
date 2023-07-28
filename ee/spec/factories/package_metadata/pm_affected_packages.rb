1c36e324b827aa4965c1c17544e6237884cff8ec
Igor Frenkel <ifrenkel@gitlab.com>
2023-07-28 16:47:52 -0400

    Update pm affected_package ingestion with new attribute
    Add versions attribute to the data object and ingestion task
    for affected packages to support its versions attribute.
    
    Changelog: added
    EE: true

--- ee/spec/factories/package_metadata/pm_affected_packages.rb
+++ ee/spec/factories/package_metadata/pm_affected_packages.rb
@@ -8,6 +8,11 @@ FactoryBot.define do
     solution { FFaker::Lorem.paragraph }
     affected_range { ">=5.2.0 <5.2.1.1" }
     fixed_versions { %w[5.2.1.1] }
+    versions do
+      [{ 'number' => '1.2.3',
+         'commit' => { 'tags' => ['v1.2.3-tag'], 'sha' => '295cf0778821bf08681e2bd0ef0e6cad04fc3001',
+                       'timestamp' => '20190626162700' } }]
+    end
     overridden_advisory_fields { { title: 'foo', published_date: Date.today - 1.day } }
 
     trait :os_advisory do
