daeaba624815e1bef199ee4e1a0352684ea6a8ca
Michał Wielich <mwielich@gitlab.com>
2023-07-28 16:13:17 +0000

    Migrate EE issue events to GIET
    Migrate EE issue events to GIET

--- ee/spec/lib/ee/gitlab/usage_data_counters/issue_activity_unique_counter_spec.rb
+++ ee/spec/lib/ee/gitlab/usage_data_counters/issue_activity_unique_counter_spec.rb
@@ -5,7 +5,7 @@ require 'spec_helper'
 RSpec.describe Gitlab::UsageDataCounters::IssueActivityUniqueCounter, :snowplow, :clean_gitlab_redis_shared_state do
   let_it_be(:user1) { build(:user, id: 1) }
   let_it_be(:user2) { build(:user, id: 2) }
-  let_it_be(:project) { build(:project) }
+  let_it_be(:project) { create(:project) }
   let_it_be(:category) { described_class::ISSUE_CATEGORY }
   let_it_be(:event_action) { described_class::ISSUE_ACTION }
   let_it_be(:event_label) { described_class::ISSUE_LABEL }
@@ -14,7 +14,7 @@ RSpec.describe Gitlab::UsageDataCounters::IssueActivityUniqueCounter, :snowplow,
   let(:original_params) { nil }
 
   context 'for Issue health status changed actions' do
-    it_behaves_like 'tracked issuable snowplow and service ping events with project' do
+    it_behaves_like 'tracked issuable internal event with project' do
       let_it_be(:action) { described_class::ISSUE_HEALTH_STATUS_CHANGED }
 
       def track_action(params)
@@ -24,7 +24,7 @@ RSpec.describe Gitlab::UsageDataCounters::IssueActivityUniqueCounter, :snowplow,
   end
 
   context 'for Issue iteration changed actions' do
-    it_behaves_like 'tracked issuable snowplow and service ping events with project' do
+    it_behaves_like 'tracked issuable internal event with project' do
       let_it_be(:action) { described_class::ISSUE_ITERATION_CHANGED }
 
       def track_action(params)
@@ -34,7 +34,7 @@ RSpec.describe Gitlab::UsageDataCounters::IssueActivityUniqueCounter, :snowplow,
   end
 
   context 'for Issue weight changed actions' do
-    it_behaves_like 'tracked issuable snowplow and service ping events with project' do
+    it_behaves_like 'tracked issuable internal event with project' do
       let_it_be(:action) { described_class::ISSUE_WEIGHT_CHANGED }
 
       def track_action(params)
@@ -44,7 +44,7 @@ RSpec.describe Gitlab::UsageDataCounters::IssueActivityUniqueCounter, :snowplow,
   end
 
   context 'for Issue added to epic actions' do
-    it_behaves_like 'tracked issuable snowplow and service ping events with project' do
+    it_behaves_like 'tracked issuable internal event with project' do
       let_it_be(:action) { described_class::ISSUE_ADDED_TO_EPIC }
 
       def track_action(params)
@@ -54,7 +54,7 @@ RSpec.describe Gitlab::UsageDataCounters::IssueActivityUniqueCounter, :snowplow,
   end
 
   context 'for Issue removed from epic actions' do
-    it_behaves_like 'tracked issuable snowplow and service ping events with project' do
+    it_behaves_like 'tracked issuable internal event with project' do
       let_it_be(:action) { described_class::ISSUE_REMOVED_FROM_EPIC }
 
       def track_action(params)
@@ -64,7 +64,7 @@ RSpec.describe Gitlab::UsageDataCounters::IssueActivityUniqueCounter, :snowplow,
   end
 
   context 'for Issue changed epic actions' do
-    it_behaves_like 'tracked issuable snowplow and service ping events with project' do
+    it_behaves_like 'tracked issuable internal event with project' do
       let_it_be(:action) { described_class::ISSUE_CHANGED_EPIC }
 
       def track_action(params)
