e0efba892464e7f2e83ec7e84909530ab311f610
Thong Kuah <tkuah@gitlab.com>
2023-07-28 16:19:38 +1200

    Remove group managed code from PAT policy worker
    We should revoke all invalid PATs now because group-managed accounts is
    end-of-life.

--- ee/app/workers/personal_access_tokens/instance/policy_worker.rb
+++ ee/app/workers/personal_access_tokens/instance/policy_worker.rb
@@ -17,16 +17,8 @@ module PersonalAccessTokens
 
         return unless expiration_date
 
-        # for users who are not managed by any group
-        User.not_managed.with_invalid_expires_at_tokens(expiration_date).find_each do |user|
+        User.with_invalid_expires_at_tokens(expiration_date).find_each do |user|
           PersonalAccessTokens::RevokeInvalidTokens.new(user, expiration_date).execute
-        end
-
-        # for users who are managed by groups, but these groups follow the instance level expiry policy
-        ::Group.with_managed_accounts_enabled.with_no_pat_expiry_policy.find_each do |group|
-          User.managed_by(group).with_invalid_expires_at_tokens(expiration_date).find_each do |user|
-            PersonalAccessTokens::RevokeInvalidTokens.new(user, expiration_date).execute
-          end
         end
       end
     end
