c7368aeead9287d19d33190e52900b12d788f1e0
Martin Čavoj <mcavoj@gitlab.com>
2023-07-28 11:15:01 +0000

    Add new merge strategy in frontend as preferred
    Changelog: changed
    EE: true

--- ee/spec/frontend/license_compliance/store/get_state_key_spec.js
+++ ee/spec/frontend/license_compliance/store/get_state_key_spec.js
@@ -1,4 +1,9 @@
 import getStateKey from 'ee/vue_merge_request_widget/stores/get_state_key';
+import {
+  DETAILED_MERGE_STATUS,
+  MWCP_MERGE_STRATEGY,
+  MWPS_MERGE_STRATEGY,
+} from '~/vue_merge_request_widget/constants';
 
 describe('getStateKey', () => {
   const canMergeContext = {
@@ -26,5 +31,31 @@ describe('getStateKey', () => {
 
       expect(bound()).toBe(state);
     });
+  });
+
+  describe('AutoMergeStrategy "merge_when_checks_pass"', () => {
+    const createContext = (detailedMergeStatus, preferredAutoMergeStrategy, autoMergeEnabled) => ({
+      ...canMergeContext,
+      detailedMergeStatus,
+      preferredAutoMergeStrategy,
+      autoMergeEnabled,
+    });
+
+    it.each`
+      scenario                                      | detailedMergeStatus                   | preferredAutoMergeStrategy | autoMergeEnabled | state
+      ${'MWCP and not approved'}                    | ${DETAILED_MERGE_STATUS.NOT_APPROVED} | ${MWCP_MERGE_STRATEGY}     | ${false}         | ${'readyToMerge'}
+      ${'MWCP, not approved and autoMerge enabled'} | ${DETAILED_MERGE_STATUS.NOT_APPROVED} | ${MWCP_MERGE_STRATEGY}     | ${true}          | ${'autoMergeEnabled'}
+      ${'MWCP and approved'}                        | ${DETAILED_MERGE_STATUS.MERGEABLE}    | ${MWCP_MERGE_STRATEGY}     | ${false}         | ${'readyToMerge'}
+      ${'MWPS and not approved'}                    | ${DETAILED_MERGE_STATUS.NOT_APPROVED} | ${MWPS_MERGE_STRATEGY}     | ${false}         | ${'checking'}
+    `(
+      'when $scenario, state should equal $state',
+      ({ detailedMergeStatus, preferredAutoMergeStrategy, autoMergeEnabled, state }) => {
+        const bound = getStateKey.bind(
+          createContext(detailedMergeStatus, preferredAutoMergeStrategy, autoMergeEnabled),
+        );
+
+        expect(bound()).toBe(state);
+      },
+    );
   });
 });
