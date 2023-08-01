d48a956775bbecfb8efea9476a05fdcd3c64d9c2
Heinrich Lee Yu <heinrich@gitlab.com>
2023-08-01 10:39:21 +0800

    Improve query performance
    Extract duplicate sub-queries into materialized CTEs to improve query
    performance

--- app/finders/autocomplete/group_users_finder.rb
+++ app/finders/autocomplete/group_users_finder.rb
@@ -10,21 +10,28 @@
 # so this should not be used for access control
 module Autocomplete
   class GroupUsersFinder
+    include Gitlab::Utils::StrongMemoize
+
     def initialize(group:)
       @group = group
     end
 
     def execute
-      members = Member.from_union(member_relations, remove_duplicates: false)
+      members = Member
+        .with(group_hierarchy_cte.to_arel) # rubocop:disable CodeReuse/ActiveRecord
+        .with(descendant_projects_cte.to_arel) # rubocop:disable CodeReuse/ActiveRecord
+        .from_union(member_relations, remove_duplicates: false)
 
-      User.id_in(members.select(:user_id))
+      User
+        .id_in(members.select(:user_id))
+        .allow_cross_joins_across_databases(url: "https://gitlab.com/gitlab-org/gitlab/-/issues/420387")
     end
 
     private
 
     def member_relations
       relations = [
-        @group.hierarchy_members.select(:user_id),
+        members_from_group_hierarchy.select(:user_id),
         members_from_descendant_projects.select(:user_id)
       ]
 
@@ -38,9 +45,14 @@ module Autocomplete
       relations
     end
 
+    def members_from_group_hierarchy
+      GroupMember
+        .with_source_id(group_hierarchy_ids)
+        .without_invites_and_requests
+    end
+
     def members_from_hierarchy_group_shares
-      source_ids = @group.self_and_hierarchy.select(:id)
-      invited_groups = GroupGroupLink.for_shared_groups(source_ids).select(:shared_with_group_id)
+      invited_groups = GroupGroupLink.for_shared_groups(group_hierarchy_ids).select(:shared_with_group_id)
 
       GroupMember
         .with_source_id(invited_groups)
@@ -49,16 +61,34 @@ module Autocomplete
 
     def members_from_descendant_projects
       ProjectMember
-        .with_source_id(@group.all_projects)
+        .with_source_id(descendant_project_ids)
         .without_invites_and_requests
     end
 
     def members_from_descendant_project_shares
-      descendant_project_invited_groups = ProjectGroupLink.for_projects(@group.all_projects).select(:group_id)
+      descendant_project_invited_groups = ProjectGroupLink.for_projects(descendant_project_ids).select(:group_id)
 
       GroupMember
         .with_source_id(descendant_project_invited_groups)
         .without_invites_and_requests
+    end
+
+    def group_hierarchy_cte
+      Gitlab::SQL::CTE.new(:group_hierarchy, @group.self_and_hierarchy.select(:id))
+    end
+    strong_memoize_attr :group_hierarchy_cte
+
+    def group_hierarchy_ids
+      Namespace.from(group_hierarchy_cte.table).select(:id) # rubocop:disable CodeReuse/ActiveRecord
+    end
+
+    def descendant_projects_cte
+      Gitlab::SQL::CTE.new(:descendant_projects, @group.all_projects.select(:id))
+    end
+    strong_memoize_attr :descendant_projects_cte
+
+    def descendant_project_ids
+      Project.from(descendant_projects_cte.table).select(:id) # rubocop:disable CodeReuse/ActiveRecord
     end
   end
 end
