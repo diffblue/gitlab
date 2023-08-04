a2bbb87c92e8509c727f7eb78d91c06b0892b916
Igor Drozdov <idrozdov@gitlab.com>
2023-08-04 11:16:52 +0200

    Add Google AI model to NL Git assistant endpoint
    It's now possible to send `model` param to use GoogleAI instead
    of OpenAI

--- ee/spec/requests/api/ai/llm/git_command_spec.rb
+++ ee/spec/requests/api/ai/llm/git_command_spec.rb
@@ -5,18 +5,17 @@ require 'spec_helper'
 RSpec.describe API::Ai::Llm::GitCommand, :saas, feature_category: :source_code_management do
   let_it_be(:current_user) { create :user }
 
-  let(:header) { { 'Authorization' => ['Bearer test-key'], 'Content-Type' => ['application/json'] } }
   let(:url) { '/ai/llm/git_command' }
-  let(:input_params) { { prompt: 'list 10 commit titles' } }
+  let(:model) { 'vertexai' }
+  let(:input_params) { { prompt: 'list 10 commit titles', model: model } }
 
   before do
-    stub_application_setting(openai_api_key: 'test-key')
     stub_licensed_features(ai_git_command: true)
     stub_ee_application_setting(should_check_namespace_plan: true)
   end
 
-  describe 'POST /ai/llm/git_command', :saas do
-    let_it_be(:group) { create(:group_with_plan, plan: :ultimate_plan) }
+  describe 'POST /ai/llm/git_command', :saas, :use_clean_rails_redis_caching do
+    let_it_be(:group, refind: true) { create(:group_with_plan, plan: :ultimate_plan) }
 
     before_all do
       group.add_developer(current_user)
@@ -25,30 +24,83 @@ RSpec.describe API::Ai::Llm::GitCommand, :saas, feature_category: :source_code_m
     include_context 'with ai features enabled for group'
 
     it_behaves_like 'delegates AI request to Workhorse' do
+      let(:header) do
+        {
+          'Authorization' => ['Bearer access token'],
+          'Content-Type' => ['application/json'],
+          'Accept' => ["application/json"],
+          'Host' => ['host']
+        }
+      end
+
       let(:expected_params) do
         expected_content = <<~PROMPT
         Provide the appropriate git commands for: list 10 commit titles.
-        Respond with JSON format
-        ##
-        {
-          "commands": [The list of commands],
-          "explanation": The explanation with the commands wrapped in backticks
-        }
         PROMPT
 
         {
-          'URL' => ::Gitlab::Llm::OpenAi::Workhorse::CHAT_URL,
+          'URL' => "https://host/v1/projects/c/locations/us-central1/publishers/google/models/codechat-bison:predict",
           'Header' => header,
           'Body' => {
-            model: 'gpt-3.5-turbo',
-            messages: [{
-              role: "user",
-              content: expected_content
+            instances: [{
+              messages: [{
+                author: "content",
+                content: expected_content
+              }]
             }],
-            temperature: 0.4,
-            max_tokens: 300
+            parameters: {
+              temperature: 0.2,
+              maxOutputTokens: 2048,
+              topK: 40,
+              topP: 0.95
+            }
           }.to_json
         }
+      end
+
+      before do
+        stub_ee_application_setting(vertex_ai_host: 'host', vertex_ai_project: 'c')
+
+        allow_next_instance_of(::Gitlab::Llm::VertexAi::Configuration) do |instance|
+          allow(instance).to receive(:access_token).and_return('access token')
+        end
+      end
+    end
+
+    context 'when openai model is requested' do
+      let(:model) { 'openai' }
+      let(:header) { { 'Authorization' => ['Bearer test-key'], 'Content-Type' => ['application/json'] } }
+
+      before do
+        stub_ee_application_setting(openai_api_key: 'test-key')
+      end
+
+      it_behaves_like 'delegates AI request to Workhorse' do
+        let(:expected_params) do
+          expected_content = <<~PROMPT
+          Provide the appropriate git commands for: list 10 commit titles.
+          Respond with JSON format
+          ##
+          {
+            "commands": [The list of commands],
+            "explanation": The explanation with the commands wrapped in backticks
+          }
+          PROMPT
+
+          {
+            'URL' => ::Gitlab::Llm::OpenAi::Workhorse::CHAT_URL,
+            'Header' => header,
+            'Body' => {
+              model: 'gpt-3.5-turbo',
+              messages: [{
+                role: "user",
+                content: expected_content
+              }],
+              temperature: 0.4,
+              max_tokens: 200
+            }.to_json
+          }
+        end
       end
     end
 
--- ee/spec/support/shared_contexts/services/llm/ai_features_enabled_for_group_shared_context.rb
+++ ee/spec/support/shared_contexts/services/llm/ai_features_enabled_for_group_shared_context.rb
@@ -5,7 +5,7 @@ RSpec.shared_context 'with ai features enabled for group' do
     allow(Gitlab).to receive(:com?).and_return(true)
     stub_ee_application_setting(should_check_namespace_plan: true)
     allow(group.namespace_settings).to receive(:ai_settings_allowed?).and_return(true)
-    group.namespace_settings.update!(third_party_ai_features_enabled: true, experiment_features_enabled: true)
+    group.namespace_settings.reload.update!(third_party_ai_features_enabled: true, experiment_features_enabled: true)
   end
 end
 
