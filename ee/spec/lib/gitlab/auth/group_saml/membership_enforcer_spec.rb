00ea25d4f6968a750d1d3f7f6a2f926875a98161
Jessie Young <jessieyoung@gitlab.com>
2023-08-09 03:54:49 +0000

    User cannot be added as member when SCIM identity inactive
    * With the feature flag
      `skip_saml_identity_destroy_during_scim_deprovision` disabled, a
      user's SCIM identity is deleted when they are SCIM deprovisioned.
    * As as result, the GroupSaml::Membership enforcer works as expected and
      does not let a user be added to a subgroup or project after they have
      been SCIM deprovisioned.
    * When the feature flag is enabled, however, we keep the SCIM identity
      when the user is SCIM deprovisioned.
    * As a result, a user could be added to a subgrou or project.
    * To remediate this, we are adding logic that also checks for any
      inactive SCIM identities for the root group when looking at whether a
      user can be added to a subgroup or project.
    * Fixes https://gitlab.com/gitlab-org/gitlab/-/issues/413079

--- ee/spec/lib/gitlab/auth/group_saml/membership_enforcer_spec.rb
+++ ee/spec/lib/gitlab/auth/group_saml/membership_enforcer_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 require 'spec_helper'
 
-RSpec.describe Gitlab::Auth::GroupSaml::MembershipEnforcer do
+RSpec.describe Gitlab::Auth::GroupSaml::MembershipEnforcer, feature_category: :system_access do
   let(:user) { create(:user) }
   let(:identity) { create(:group_saml_identity, user: user) }
   let(:group) { identity.saml_provider.group }
@@ -18,6 +18,29 @@ RSpec.describe Gitlab::Auth::GroupSaml::MembershipEnforcer do
     non_saml_user = create(:user)
 
     expect(described_class.new(group).can_add_user?(non_saml_user)).to be_falsey
+  end
+
+  context "when skip_saml_identity_destroy_during_scim_deprovision disabled" do
+    it 'allows adding a user with an inactive scim identity for the group' do
+      stub_feature_flags(skip_saml_identity_destroy_during_scim_deprovision: false)
+
+      create(:scim_identity, group: group, user: user, active: false)
+
+      expect(described_class.new(group).can_add_user?(user)).to be_truthy
+    end
+  end
+
+  it 'does not allow adding a user with an inactive scim identity for the group' do
+    create(:scim_identity, group: group, user: user, active: false)
+
+    expect(described_class.new(group).can_add_user?(user)).to be_falsey
+  end
+
+  it 'does allow adding a user with an active scim identity for the group' do
+    _inactive_scim_identity_for_other_user = create(:scim_identity, group: group, user: create(:user), active: false)
+    create(:scim_identity, group: group, user: user, active: true)
+
+    expect(described_class.new(group).can_add_user?(user)).to be_truthy
   end
 
   it 'allows adding a project bot as member' do
