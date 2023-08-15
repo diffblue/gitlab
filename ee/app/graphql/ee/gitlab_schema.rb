ae128928c1b10d034ca82da5bf663e354c8c4ee9
Eugenia Grieff <egrieff@gitlab.com>
2023-08-15 13:05:55 +0200

    Fix blockedByCount GraphQL field
    This fields returns a 500 error for work item links widgets
    because links aggregator is not being resolved
    
    Also fix feature flag scope to work at project level
    
    Changelog: fixed

--- ee/app/graphql/ee/gitlab_schema.rb
+++ ee/app/graphql/ee/gitlab_schema.rb
@@ -8,6 +8,7 @@ module EE
       lazy_resolve ::Gitlab::Graphql::Aggregations::Epics::LazyEpicAggregate, :epic_aggregate
       lazy_resolve ::Gitlab::Graphql::Aggregations::Epics::LazyLinksAggregate, :links_aggregate
       lazy_resolve ::Gitlab::Graphql::Aggregations::Issues::LazyLinksAggregate, :links_aggregate
+      lazy_resolve ::Gitlab::Graphql::Aggregations::WorkItems::LazyLinksAggregate, :links_aggregate
       lazy_resolve ::Gitlab::Graphql::Aggregations::VulnerabilityStatistics::LazyAggregate, :execute
       lazy_resolve ::Gitlab::Graphql::Aggregations::Vulnerabilities::LazyUserNotesCountAggregate, :execute
       lazy_resolve ::Gitlab::Graphql::Aggregations::SecurityOrchestrationPolicies::LazyDastProfileAggregate, :execute
