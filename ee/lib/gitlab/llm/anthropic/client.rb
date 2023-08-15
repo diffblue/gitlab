9b6a105861e206bb40377448a8e3be3f6f64ee1a
Nicolas Dular <ndular@gitlab.com>
2023-08-15 14:45:18 +0000

    Add streaming to Anthropic client
    This adds the option to stream data from Anthropic by calling the
    `stream` method. We added a new method instead of adding an option to
    `complete` because streaming does not work with the concept of
    exponential backoff.
    
    It also adds the `anthropic-version` header with '2023-06-01' as
    Anthropic changed the way streaming works and the previous version will
    be deprecated. The old version accumulated the response in the stream,
    while the new version only sends the next token.
    
    Changelog: changed
    EE: true

--- ee/lib/gitlab/llm/anthropic/client.rb
+++ ee/lib/gitlab/llm/anthropic/client.rb
@@ -20,18 +20,20 @@ module Gitlab
         def complete(prompt:, **options)
           return unless enabled?
 
-          logger.info(message: "Performing request to Anthropic", options: options)
+          # We do not allow to set `stream` because the separate `#stream` method should be used for streaming.
+          # The reason is that streaming the response would not work with the exponential backoff mechanism.
+          perform_completion_request(prompt: prompt, options: options.except(:stream))
+        rescue StandardError => e
+          increment_metric(client: :anthropic)
+          raise e
+        end
 
-          response = Gitlab::HTTP.post(
-            URI.join(URL, '/v1/complete'),
-            headers: request_headers,
-            body: request_body(prompt: prompt, options: options).to_json
-          )
+        def stream(prompt:, **options)
+          return unless enabled?
 
-          logger.debug(message: "Received response from Anthropic", response: response)
-          increment_metric(client: :anthropic, response: response)
-
-          response
+          perform_completion_request(prompt: prompt, options: options.merge(stream: true)) do |fragment|
+            yield parse_sse_event(fragment) if block_given?
+          end
         rescue StandardError => e
           increment_metric(client: :anthropic)
           raise e
@@ -43,6 +45,25 @@ module Gitlab
 
         retry_methods_with_exponential_backoff :complete
 
+        def perform_completion_request(prompt:, options:)
+          logger.info(message: "Performing request to Anthropic", options: options)
+
+          response = Gitlab::HTTP.post(
+            URI.join(URL, '/v1/complete'),
+            headers: request_headers,
+            body: request_body(prompt: prompt, options: options).to_json,
+            stream_body: options.fetch(:stream, false)
+          ) do |fragment|
+            yield fragment if block_given?
+          end
+
+          logger.debug(message: "Received response from Anthropic", response: response)
+
+          increment_metric(client: :anthropic, response: response)
+
+          response
+        end
+
         def enabled?
           api_key.present?
         end
@@ -51,10 +72,13 @@ module Gitlab
           @api_key ||= ::Gitlab::CurrentSettings.anthropic_api_key
         end
 
+        # We specificy the `anthropic-version` header to receive the stream word by word instead of the accumulated
+        # response https://docs.anthropic.com/claude/reference/streaming.
         def request_headers
           {
             'Accept' => 'application/json',
             'Content-Type' => 'application/json',
+            'anthropic-version' => '2023-06-01',
             'x-api-key' => api_key
           }
         end
@@ -66,6 +90,14 @@ module Gitlab
             max_tokens_to_sample: DEFAULT_MAX_TOKENS,
             temperature: DEFAULT_TEMPERATURE
           }.merge(options)
+        end
+
+        # Following the SSE spec
+        # https://html.spec.whatwg.org/multipage/server-sent-events.html#event-stream-interpretation
+        # and using the format from Anthropic: https://docs.anthropic.com/claude/reference/streaming#example
+        # we can assume that the JSON we're looking comes after `data: `
+        def parse_sse_event(fragment)
+          Gitlab::Json.parse(fragment.split('data: ').last)
         end
       end
     end
--- ee/spec/lib/gitlab/llm/anthropic/client_spec.rb
+++ ee/spec/lib/gitlab/llm/anthropic/client_spec.rb
@@ -9,16 +9,18 @@ RSpec.describe Gitlab::Llm::Anthropic::Client, feature_category: :shared do
 
   let(:api_key) { 'api-key' }
   let(:options) { {} }
+  let(:expected_request_body) { default_body_params }
 
   let(:expected_request_headers) do
     {
       'Accept' => 'application/json',
       'Content-Type' => 'application/json',
+      'anthropic-version' => '2023-06-01',
       'x-api-key' => api_key
     }
   end
 
-  let(:expected_request_body) do
+  let(:default_body_params) do
     {
       prompt: 'anything',
       model: described_class::DEFAULT_MODEL,
@@ -39,6 +41,8 @@ RSpec.describe Gitlab::Llm::Anthropic::Client, feature_category: :shared do
     }
   end
 
+  let(:response_body) { expected_response.to_json }
+
   before do
     stub_application_setting(anthropic_api_key: api_key)
     stub_request(:post, "#{described_class::URL}/v1/complete")
@@ -48,7 +52,7 @@ RSpec.describe Gitlab::Llm::Anthropic::Client, feature_category: :shared do
       )
       .to_return(
         status: 200,
-        body: expected_response.to_json,
+        body: response_body,
         headers: { 'Content-Type' => 'application/json' }
       )
   end
@@ -93,10 +97,87 @@ RSpec.describe Gitlab::Llm::Anthropic::Client, feature_category: :shared do
       end
     end
 
+    context 'when passing stream: true' do
+      let(:options) { { stream: true } }
+      let(:expected_request_body) { default_body_params }
+
+      it 'does not pass stream: true as we do not want to retrieve SSE events' do
+        expect(complete.parsed_response).to eq(expected_response)
+      end
+    end
+
     context 'when the API key is not present' do
       let(:api_key) { nil }
 
       it { is_expected.to be_nil }
+    end
+  end
+
+  describe '#stream' do
+    context 'when streaming the request' do
+      let(:response_body) { expected_response }
+      let(:options) { { stream: true } }
+      let(:expected_request_body) { default_body_params.merge(stream: true) }
+
+      context 'when response is successful' do
+        let(:expected_response) do
+          <<-DOC
+          event: completion\r\n
+          data: {"completion": "Hello", "stop_reason": null, "model": "claude-2.0"}\r\n
+          \r\n
+          DOC
+        end
+
+        it 'provides parsed streamed response' do
+          expect { |b| described_class.new(user).stream(prompt: 'anything', **options, &b) }.to yield_with_args(
+            {
+              "completion" => "Hello",
+              "stop_reason" => nil,
+              "model" => "claude-2.0"
+            }
+          )
+        end
+      end
+
+      context 'when response is an error' do
+        let(:expected_response) do
+          <<-DOC
+          event: error\r\n
+          data: {"error": {"type": "overloaded_error", "message": "Overloaded"}}\r\n
+          \r\n
+          DOC
+        end
+
+        it 'provides parsed streamed response' do
+          expect { |b| described_class.new(user).stream(prompt: 'anything', **options, &b) }.to yield_with_args(
+            {
+              "error" => { "message" => "Overloaded", "type" => "overloaded_error" }
+            }
+          )
+        end
+      end
+
+      context 'when response is a ping' do
+        let(:expected_response) do
+          <<-DOC
+          event: ping\r\n
+          data: {}\r\n
+          \r\n
+          DOC
+        end
+
+        it 'provides parsed streamed response' do
+          expect { |b| described_class.new(user).stream(prompt: 'anything', **options, &b) }.to yield_with_args({})
+        end
+      end
+    end
+
+    context 'when the API key is not present' do
+      let(:api_key) { nil }
+
+      it 'does not provide stream response' do
+        expect { |b| described_class.new(user).stream(prompt: 'anything', **options, &b) }.not_to yield_with_args
+      end
     end
   end
 end
