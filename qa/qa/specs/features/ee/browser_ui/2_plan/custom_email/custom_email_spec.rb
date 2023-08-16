61842a002c809b90f0a6b5582d85052cf6107341
Désirée Chevalier <dchevalier@gitlab.com>
2023-08-16 22:22:23 +0000

    Quarantine e2e airgapped custom email spec

--- qa/qa/specs/features/ee/browser_ui/2_plan/custom_email/custom_email_spec.rb
+++ qa/qa/specs/features/ee/browser_ui/2_plan/custom_email/custom_email_spec.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 module QA
-  RSpec.describe 'Plan', :reliable, product_group: :project_management do
+  RSpec.describe 'Plan', :reliable, product_group: :project_management,
+    quarantine: {
+      only: { job: 'airgapped' },
+      issue: 'https://gitlab.com/gitlab-org/gitlab/-/issues/414247',
+      type: :investigating
+    } do
     describe 'Custom email', :requires_admin do
       before do
         Flow::Login.sign_in_as_admin
