71e6a511082700e556f4b444fa5c3f4f6a50f53e
Adam Hegyi <ahegyi@gitlab.com>
2023-08-16 08:10:11 +0200

    Implement VSD GraphQL query
    This change implements the value stream dashboard overview count
    aggregation query.
    
    Changelog: added
    EE: true

--- ee/app/models/analytics/value_stream_dashboard/count.rb
+++ ee/app/models/analytics/value_stream_dashboard/count.rb
@@ -15,6 +15,42 @@ module Analytics
       validates :namespace_id, :count, :metric, :recorded_at, presence: true
 
       enum metric: { projects: 1, issues: 2, groups: 3, merge_requests: 4, pipelines: 5 }
+
+      scope :latest_first_order, -> { order(recorded_at: :desc, id: :desc) }
+      scope :for_period, ->(metric, from, to) {
+        where(metric: metric)
+          .where(arel_table[:recorded_at].gteq(from))
+          .where(arel_table[:recorded_at].lteq(to))
+      }
+
+      def self.aggregate_for_period(group, metric, from, to)
+        namespace_query = "(#{descendant_namespace_ids_for(group, metric).to_sql}) AS ids(id)"
+
+        lateral_query = for_period(metric, from, to)
+          .where('ids.id = value_stream_dashboard_counts.namespace_id')
+          .latest_first_order
+          .select(:count, :recorded_at)
+          .limit(1)
+          .to_sql
+
+        from("#{namespace_query}, LATERAL (#{lateral_query}) AS counts")
+          .limit(1)
+          .pick(Arel.sql("SUM(counts.count) OVER (), MAX(counts.recorded_at) OVER ()"))
+      end
+
+      def self.descendant_namespace_ids_for(group, metric)
+        metric_namespace_class = Analytics::ValueStreamDashboard::TopLevelGroupCounterService::COUNTS_TO_COLLECT
+          .fetch(metric)
+          .fetch(:namespace_class)
+
+        if metric_namespace_class == Group
+          group.self_and_descendant_ids
+        elsif metric_namespace_class == Namespaces::ProjectNamespace
+          group.all_projects.select(:project_namespace_id)
+        else
+          Group.none
+        end
+      end
     end
   end
 end
--- ee/spec/models/analytics/value_stream_dashboard/count_spec.rb
+++ ee/spec/models/analytics/value_stream_dashboard/count_spec.rb
@@ -14,4 +14,128 @@ RSpec.describe Analytics::ValueStreamDashboard::Count, feature_category: :value_
     it { is_expected.to validate_presence_of(:recorded_at) }
     it { is_expected.to validate_presence_of(:count) }
   end
+
+  describe '.latest_first_order' do
+    it 'returns the results in a correct order' do
+      timestamp1 = 2.months.ago
+      timestamp2 = 3.months.ago
+
+      count1 = create(:value_stream_dashboard_count, recorded_at: timestamp2)
+      count2 = create(:value_stream_dashboard_count, recorded_at: timestamp1)
+      count3 = create(:value_stream_dashboard_count, recorded_at: timestamp2)
+
+      expect(described_class.latest_first_order).to eq([count2, count3, count1])
+    end
+  end
+
+  describe '.aggregate_for_period' do
+    let_it_be(:group) { create(:group) }
+    let_it_be(:subgroup) { create(:group, parent: group) }
+    let_it_be(:subsubgroup) { create(:group, parent: subgroup) }
+
+    let_it_be(:project1) { create(:project, group: subgroup) }
+    let_it_be(:project2) { create(:project, group: subsubgroup) }
+
+    let_it_be(:issue_aggregation1) do
+      create(:value_stream_dashboard_count, metric: :issues, count: 2, namespace: project1.project_namespace,
+        recorded_at: '2023-05-25')
+    end
+
+    let_it_be(:issue_aggregation2) do
+      create(:value_stream_dashboard_count, metric: :issues, count: 1, namespace: project2.project_namespace,
+        recorded_at: '2023-05-26')
+    end
+
+    # Should not be calculated in the results
+    let_it_be(:issue_aggregation_past) do
+      create(:value_stream_dashboard_count, metric: :issues, count: 1, namespace: project2.project_namespace,
+        recorded_at: '2023-04-25')
+    end
+
+    let_it_be(:group_aggregation) do
+      create(:value_stream_dashboard_count, metric: :groups, count: 1, namespace: group, recorded_at: '2023-05-25')
+    end
+
+    let_it_be(:group_aggregation_subgroup) do
+      create(:value_stream_dashboard_count, metric: :groups, count: 1, namespace: subgroup, recorded_at: '2023-05-25')
+    end
+
+    let(:group_to_aggregate) { group }
+    let(:from) { Date.new(2023, 5, 1) }
+    let(:to) { Date.new(2023, 5, 31) }
+
+    let(:result) { described_class.aggregate_for_period(group_to_aggregate, metric, from, to) }
+    let(:last_recorded_at) { result&.last }
+
+    subject(:count) { result&.first }
+
+    context 'when requesting issue counts' do
+      let(:metric) { :issues }
+
+      it 'correctly counts the issues in the given time frame' do
+        expect(count).to eq(3)
+      end
+
+      context 'when there are more measurements within the given time frame' do
+        before do
+          create(:value_stream_dashboard_count, metric: :groups, count: 100, namespace: group,
+            recorded_at: '2023-05-10')
+        end
+
+        it 'takes the latest measurements' do
+          expect(count).to eq(3)
+        end
+      end
+
+      context 'when querying a subgroup' do
+        let(:group_to_aggregate) { subsubgroup }
+
+        it 'returns count scoped to the subgroup' do
+          expect(count).to eq(1)
+        end
+      end
+    end
+
+    context 'when requesting group counts' do
+      let(:metric) { :groups }
+
+      it 'correctly counts the groups in the given time frame' do
+        expect(count).to eq(2)
+      end
+
+      context 'when querying a subgroup' do
+        let(:group_to_aggregate) { subgroup }
+
+        it 'returns count scoped to the subgroup' do
+          expect(count).to eq(1)
+        end
+      end
+
+      context 'when querying group without data' do
+        let(:group_to_aggregate) { subsubgroup }
+
+        it 'returns nil count' do
+          expect(count).to eq(nil)
+        end
+      end
+
+      context 'when querying group in a date range where data is not available' do
+        let(:from) { Date.new(2023, 1, 1) }
+        let(:to) { Date.new(2023, 1, 31) }
+
+        it 'returns nil count' do
+          expect(count).to eq(nil)
+        end
+      end
+
+      context 'when unsupported namespace class is passed' do
+        it 'returns 0 count' do
+          stub_const('Analytics::ValueStreamDashboard::TopLevelGroupCounterService::COUNTS_TO_COLLECT',
+            { groups: { namespace_class: Namespace } })
+
+          expect(count).to eq(nil)
+        end
+      end
+    end
+  end
 end
