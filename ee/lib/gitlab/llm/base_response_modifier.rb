38cd4dfdbae91f62906a65ad5fbb4f96a0c2759a
Gregory Havenga <11164960-ghavenga@users.noreply.gitlab.com>
2023-08-18 16:10:50 +0000

    Implement explain vuln anthropic, use vertex by default
    Changelog: changed
    EE: true

--- ee/lib/gitlab/llm/base_response_modifier.rb
+++ ee/lib/gitlab/llm/base_response_modifier.rb
@@ -5,8 +5,21 @@ module Gitlab
     class BaseResponseModifier
       attr_accessor :ai_response
 
+      # Due to a difference in the implementation of the Anthropic
+      # and VertexAi clients where the `stream_body` paramter is
+      # only passed to one of them, it is possible for the ai_response
+      # to behave as a parsed Hash object, or an unparsed json string object.
+      #
+      # https://gitlab.com/gitlab-org/gitlab/-/issues/422519 is to follow up
+      # and make these clients behave consistently with each other and simplify
+      # the initializer.
+
       def initialize(ai_response)
-        @ai_response = Gitlab::Json.parse(ai_response)&.with_indifferent_access
+        @ai_response = if ai_response.respond_to?(:keys)
+                         ai_response
+                       else
+                         Gitlab::Json.parse(ai_response)
+                       end&.with_indifferent_access
       end
 
       def response_body
