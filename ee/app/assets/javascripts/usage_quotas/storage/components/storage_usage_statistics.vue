47794ff590afccc29360ea2d0d61e796b3995ada
Sheldon Led <sheldonled@gitlab.com>
2023-08-21 21:30:23 +0000

    Move total-storage-available-breakdown-card to its own component

--- ee/app/assets/javascripts/usage_quotas/storage/components/storage_usage_statistics.vue
+++ ee/app/assets/javascripts/usage_quotas/storage/components/storage_usage_statistics.vue
@@ -1,32 +1,26 @@
 <script>
-import { GlSprintf, GlIcon, GlLink, GlButton, GlCard, GlSkeletonLoader } from '@gitlab/ui';
+import { GlSprintf, GlLink, GlButton } from '@gitlab/ui';
 import { sprintf } from '~/locale';
 import { numberToHumanSize } from '~/lib/utils/number_utils';
-import StorageStatisticsCard from 'ee/usage_quotas/storage/components/storage_statistics_card.vue';
 import { usageQuotasHelpPaths } from '~/usage_quotas/storage/constants';
 import {
   BUY_STORAGE,
-  STORAGE_STATISTICS_USAGE_QUOTA_LEARN_MORE,
   STORAGE_INCLUDED_IN_PLAN_PROJECT_ENFORCEMENT,
   STORAGE_INCLUDED_IN_PLAN_NAMESPACE_ENFORCEMENT,
-  STORAGE_STATISTICS_PURCHASED_STORAGE,
-  STORAGE_STATISTICS_TOTAL_STORAGE,
   NAMESPACE_STORAGE_OVERVIEW_SUBTITLE,
   PROJECT_ENFORCEMENT_TYPE_SUBTITLE,
   NAMESPACE_ENFORCEMENT_TYPE_SUBTITLE,
 } from '../constants';
-import NumberToHumanSize from './number_to_human_size.vue';
+import StorageStatisticsCard from './storage_statistics_card.vue';
+import TotalStorageAvailableBreakdownCard from './total_storage_available_breakdown_card.vue';
 
 export default {
   components: {
     GlSprintf,
-    GlIcon,
     GlLink,
     GlButton,
-    GlCard,
-    GlSkeletonLoader,
     StorageStatisticsCard,
-    NumberToHumanSize,
+    TotalStorageAvailableBreakdownCard,
   },
   inject: [
     'purchaseStorageUrl',
@@ -52,14 +46,8 @@ export default {
     },
   },
   i18n: {
-    purchasedUsageHelpLink: usageQuotasHelpPaths.usageQuotasNamespaceStorageLimit,
-    purchasedUsageHelpText: STORAGE_STATISTICS_USAGE_QUOTA_LEARN_MORE,
-    usedUsageHelpLink: usageQuotasHelpPaths.usageQuotas,
-    usedUsageHelpText: STORAGE_STATISTICS_USAGE_QUOTA_LEARN_MORE,
     purchaseButtonText: BUY_STORAGE,
     namespaceStorageOverviewSubtitle: NAMESPACE_STORAGE_OVERVIEW_SUBTITLE,
-    storageStatisticsPurchasedStorage: STORAGE_STATISTICS_PURCHASED_STORAGE,
-    storageStatisticsTotalStorage: STORAGE_STATISTICS_TOTAL_STORAGE,
   },
   computed: {
     enforcementTypeLearnMoreUrl() {
@@ -73,10 +61,10 @@ export default {
         : NAMESPACE_ENFORCEMENT_TYPE_SUBTITLE;
 
       return sprintf(subtitle, {
-        planLimit: this.includedStorage,
+        planLimit: numberToHumanSize(this.includedStorage, 1),
       });
     },
-    storageStatisticsPlanStorage() {
+    planStorageDescription() {
       if (!this.namespacePlanName) {
         return '';
       }
@@ -90,14 +78,10 @@ export default {
       });
     },
     includedStorage() {
-      return numberToHumanSize(this.namespacePlanStorageIncluded || 0, 1);
+      return Number(this.namespacePlanStorageIncluded || 0);
     },
-    purchasedTotalStorage() {
-      if (!this.additionalPurchasedStorageSize) {
-        return 0;
-      }
-
-      return numberToHumanSize(this.additionalPurchasedStorageSize || 0, 1);
+    purchasedStorage() {
+      return Number(this.additionalPurchasedStorageSize || 0);
     },
     totalStorage() {
       return (
@@ -133,51 +117,21 @@ export default {
     </p>
     <div class="gl-display-flex gl-sm-flex-direction-column gl-gap-5 gl-py-4">
       <storage-statistics-card
-        :plan-storage-description="storageStatisticsPlanStorage"
+        :plan-storage-description="planStorageDescription"
         :used-storage="usedStorage"
         :total-storage="totalStorage"
         :loading="loading"
         data-qa-selector="namespace_usage_total"
         class="gl-w-full"
       />
-      <gl-card v-if="namespacePlanName" class="gl-w-full" data-testid="storage-detail-card">
-        <gl-skeleton-loader v-if="loading" :height="64">
-          <rect width="140" height="30" x="5" y="0" rx="4" />
-          <rect width="240" height="10" x="5" y="40" rx="4" />
-          <rect width="340" height="10" x="5" y="54" rx="4" />
-        </gl-skeleton-loader>
-        <div v-else>
-          <div
-            class="gl-display-flex gl-justify-content-space-between gl-gap-5"
-            data-testid="storage-included-in-plan"
-          >
-            <div class="gl-w-80p">{{ storageStatisticsPlanStorage }}</div>
-            <div class="gl-white-space-nowrap">{{ includedStorage }}</div>
-          </div>
-          <div
-            class="gl-display-flex gl-justify-content-space-between"
-            data-testid="storage-purchased"
-          >
-            <div class="gl-w-80p">
-              {{ $options.i18n.storageStatisticsPurchasedStorage }}
-              <gl-link
-                :href="$options.i18n.purchasedUsageHelpLink"
-                target="_blank"
-                class="gl-ml-2"
-                :aria-label="$options.i18n.purchasedUsageHelpText"
-              >
-                <gl-icon name="question-o" />
-              </gl-link>
-            </div>
-            <div class="gl-white-space-nowrap">{{ purchasedTotalStorage }}</div>
-          </div>
-          <hr />
-          <div class="gl-display-flex gl-justify-content-space-between" data-testid="total-storage">
-            <div class="gl-w-80p">{{ $options.i18n.storageStatisticsTotalStorage }}</div>
-            <number-to-human-size class="gl-white-space-nowrap" :value="totalStorage" />
-          </div>
-        </div>
-      </gl-card>
+      <total-storage-available-breakdown-card
+        v-if="namespacePlanName"
+        :plan-storage-description="planStorageDescription"
+        :included-storage="includedStorage"
+        :purchased-storage="purchasedStorage"
+        :total-storage="totalStorage"
+        :loading="loading"
+      />
     </div>
   </div>
 </template>
---
+++ ee/app/assets/javascripts/usage_quotas/storage/components/total_storage_available_breakdown_card.vue
@@ -0,0 +1,82 @@
+<script>
+import { GlIcon, GlLink, GlCard, GlSkeletonLoader } from '@gitlab/ui';
+import { usageQuotasHelpPaths } from '~/usage_quotas/storage/constants';
+import {
+  STORAGE_STATISTICS_PURCHASED_STORAGE,
+  STORAGE_STATISTICS_TOTAL_STORAGE,
+  STORAGE_STATISTICS_USAGE_QUOTA_LEARN_MORE,
+} from '../constants';
+import NumberToHumanSize from './number_to_human_size.vue';
+
+export default {
+  name: 'TotalStorageAvailableBreakdownCard',
+  components: { GlIcon, GlLink, GlCard, GlSkeletonLoader, NumberToHumanSize },
+  inject: ['namespacePlanName'],
+  props: {
+    planStorageDescription: {
+      type: String,
+      required: true,
+    },
+    includedStorage: {
+      type: Number,
+      required: true,
+    },
+    purchasedStorage: {
+      type: Number,
+      required: true,
+    },
+    totalStorage: {
+      type: Number,
+      required: true,
+    },
+    loading: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  i18n: {
+    PURCHASED_USAGE_HELP_LINK: usageQuotasHelpPaths.usageQuotasNamespaceStorageLimit,
+    STORAGE_STATISTICS_USAGE_QUOTA_LEARN_MORE,
+    STORAGE_STATISTICS_PURCHASED_STORAGE,
+    STORAGE_STATISTICS_TOTAL_STORAGE,
+  },
+};
+</script>
+
+<template>
+  <gl-card class="gl-w-full" data-testid="storage-detail-card">
+    <gl-skeleton-loader v-if="loading" :height="64">
+      <rect width="140" height="30" x="5" y="0" rx="4" />
+      <rect width="240" height="10" x="5" y="40" rx="4" />
+      <rect width="340" height="10" x="5" y="54" rx="4" />
+    </gl-skeleton-loader>
+    <div v-else>
+      <div
+        class="gl-display-flex gl-justify-content-space-between gl-gap-5"
+        data-testid="storage-included-in-plan"
+      >
+        <div class="gl-w-80p">{{ planStorageDescription }}</div>
+        <number-to-human-size class="gl-white-space-nowrap" :value="includedStorage" />
+      </div>
+      <div class="gl-display-flex gl-justify-content-space-between" data-testid="storage-purchased">
+        <div class="gl-w-80p">
+          {{ $options.i18n.STORAGE_STATISTICS_PURCHASED_STORAGE }}
+          <gl-link
+            :href="$options.i18n.PURCHASED_USAGE_HELP_LINK"
+            target="_blank"
+            class="gl-ml-2"
+            :aria-label="$options.i18n.STORAGE_STATISTICS_USAGE_QUOTA_LEARN_MORE"
+          >
+            <gl-icon name="question-o" />
+          </gl-link>
+        </div>
+        <number-to-human-size class="gl-white-space-nowrap" :value="purchasedStorage" />
+      </div>
+      <hr />
+      <div class="gl-display-flex gl-justify-content-space-between" data-testid="total-storage">
+        <div class="gl-w-80p">{{ $options.i18n.STORAGE_STATISTICS_TOTAL_STORAGE }}</div>
+        <number-to-human-size class="gl-white-space-nowrap" :value="totalStorage" />
+      </div>
+    </div>
+  </gl-card>
+</template>
