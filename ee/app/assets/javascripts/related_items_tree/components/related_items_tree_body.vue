452d8d27b4323c1f295ce7882af00792ec61cdea
Florie Guibert <fguibert@gitlab.com>
2023-08-29 15:39:55 +1000

    Fix and unquarantine epic tests for roadmap tab

--- ee/app/assets/javascripts/related_items_tree/components/related_items_tree_body.vue
+++ ee/app/assets/javascripts/related_items_tree/components/related_items_tree_body.vue
@@ -15,7 +15,7 @@ export default {
 </script>
 
 <template>
-  <div class="gl-new-card-body gl-py-0 gl-mx-n1 gl-mb-n2">
+  <div class="gl-new-card-body gl-py-0 gl-mx-n1 gl-mb-n2" data-testid="related-items-tree">
     <tree-root :parent-item="parentItem" :children="children" />
   </div>
 </template>
