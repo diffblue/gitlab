db62a6bd67516a0a05906adecd6cc6356881b7e2
Phil Hughes <me@iamphill.com>
2023-09-01 09:54:44 +0100

    Added settings view for target branch rules
    This includes the form to create new rules, though the form isn't setup
    to save yet, and the list of the current rules.

--- ee/app/assets/javascripts/pages/projects/settings/merge_requests/index.js
+++ ee/app/assets/javascripts/pages/projects/settings/merge_requests/index.js
@@ -1,11 +1,48 @@
 import '~/pages/projects/settings/merge_requests';
+import Vue from 'vue';
+import RefSelector from '~/ref/components/ref_selector.vue';
 import mountApprovals from 'ee/approvals/mount_project_settings';
 import { initMergeOptionSettings } from 'ee/pages/projects/edit/merge_options';
 import { initMergeRequestMergeChecksApp } from 'ee/merge_checks';
 import mountStatusChecks from 'ee/status_checks/mount';
+
+function initRefSwitcher() {
+  const refSwitcherEl = document.getElementById('js-target-branch');
+
+  if (!refSwitcherEl) return false;
+
+  const { projectId } = refSwitcherEl.dataset;
+
+  return new Vue({
+    el: refSwitcherEl,
+    render(createElement) {
+      return createElement(RefSelector, {
+        props: {
+          projectId,
+          value: '',
+          useSymbolicRefNames: true,
+          queryParams: { sort: 'updated_desc' },
+          enabledRefTypes: ['REF_TYPE_BRANCHES'],
+        },
+        on: {
+          input(selectedRef) {
+            document.getElementById('target_branch_rule_target_branch').value = selectedRef.replace(
+              /^refs\/(tags|heads)\//,
+              '',
+            );
+          },
+        },
+      });
+    },
+  });
+}
 
 mountApprovals(document.getElementById('js-mr-approvals-settings'));
 mountStatusChecks(document.getElementById('js-status-checks-settings'));
 
 initMergeOptionSettings();
 initMergeRequestMergeChecksApp();
+
+requestIdleCallback(() => {
+  initRefSwitcher();
+});
