140af2927bbb76f86727e62d3a5e43c91aa21596
Stan Hu <stanhu@gmail.com>
2023-09-05 09:25:20 -0700

    Add exact_matches_first_group_transfer feature flag
    This will ensure we roll this change in a way to avoid surprises with
    the SQL queries.

--- app/finders/groups/accepting_group_transfers_finder.rb
+++ app/finders/groups/accepting_group_transfers_finder.rb
@@ -15,8 +15,12 @@ module Groups
 
       items = find_all_groups
 
-      # Search will perform an ORDER BY to ensure exact matches are returned first.
-      return by_search(items, exact_matches_first: true) if params[:search].present?
+      if exact_matches_first_enabled?
+        # Search will perform an ORDER BY to ensure exact matches are returned first.
+        return by_search(items, exact_matches_first: true) if params[:search].present?
+      else
+        items = by_search(items)
+      end
 
       sort(items)
     end
@@ -61,6 +65,10 @@ module Groups
 
     def can_transfer_group?
       Ability.allowed?(current_user, :admin_group, group_to_be_transferred)
+    end
+
+    def exact_matches_first_enabled?
+      Feature.enabled?(:exact_matches_first_group_transfer, group_to_be_transferred)
     end
   end
 end
