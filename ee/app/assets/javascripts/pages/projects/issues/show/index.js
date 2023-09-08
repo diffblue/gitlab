6bad85c6fa77fad94836cc7c9a6bd086c61666be
Coung Ngo <cngo@gitlab.com>
2023-09-08 21:10:22 +0000

    Make issue/incident code more DRY

--- ee/app/assets/javascripts/pages/projects/issues/show/index.js
+++ ee/app/assets/javascripts/pages/projects/issues/show/index.js
@@ -1,17 +1,10 @@
 import { initRelatedFeatureFlags, initUnableToLinkVulnerabilityError } from 'ee/issues';
 import { initShow } from '~/issues';
-import { store } from '~/notes/stores';
-import { initRelatedIssues } from '~/related_issues';
-import initWorkItemLinks from '~/work_items/components/work_item_links';
-import initSidebarBundle from '~/sidebar/sidebar_bundle';
 import UserCallout from '~/user_callout';
 
 initShow();
-initSidebarBundle(store);
-initRelatedIssues();
 initRelatedFeatureFlags();
 initUnableToLinkVulnerabilityError();
-initWorkItemLinks();
 
 new UserCallout({ className: 'js-epics-sidebar-callout' }); // eslint-disable-line no-new
 new UserCallout({ className: 'js-weight-sidebar-callout' }); // eslint-disable-line no-new
