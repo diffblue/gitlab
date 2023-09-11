c2fac9a1bf96befce82f80f4b3f9ef652e1a0e3b
Frédéric Caplette <fcaplette@gitlab.com>
2023-09-11 21:13:51 +0000

    Move `~/jobs` to `~/ci/*`
    We move the jobs directory under the ci
    one to ease maintenance and conglomerate
    all related CI code-base. We also divide
    this "catchall" dir into 2 main new ones:
    
    - `~/ci/job_details` -> represents jobs/:id
    - `~ci/jobs_table` -> represents jobs/
    
    This way we can separate concerns from each page
    in its own directory.

---
+++ ee/spec/frontend/ci/runner/components/shared_runner_limit_block_spec.js
@@ -0,0 +1,59 @@
+import { GlButton } from '@gitlab/ui';
+import { shallowMount } from '@vue/test-utils';
+import SharedRunnerLimitBlock from 'ee/ci/runner/components/shared_runner_limit_block.vue';
+import { trimText } from 'helpers/text_helper';
+
+describe('Shared Runner Limit Block', () => {
+  let wrapper;
+
+  const projectPath = 'h5bp/html5-boilerplate';
+  const subscriptionsMoreMinutesUrl = 'https://customers.gitlab.com/buy_pipeline_minutes';
+
+  const factory = (options = {}) => {
+    wrapper = shallowMount(SharedRunnerLimitBlock, {
+      ...options,
+    });
+  };
+
+  describe('quota information', () => {
+    beforeEach(() => {
+      factory({
+        propsData: {
+          quotaUsed: 1000,
+          quotaLimit: 4000,
+          projectPath,
+          subscriptionsMoreMinutesUrl,
+        },
+      });
+    });
+
+    it('renders provided quota limit and used quota', () => {
+      expect(wrapper.text()).toContain(
+        'You have used 1000 out of 4000 of your shared Runners pipeline minutes',
+      );
+    });
+
+    it('renders call to action gl-button with the right href', () => {
+      const glButton = wrapper.findComponent(GlButton);
+
+      expect(glButton.isVisible()).toBe(true);
+      expect(glButton.attributes('variant')).toBe('danger');
+      expect(glButton.attributes('href')).toBe(subscriptionsMoreMinutesUrl);
+    });
+  });
+
+  describe('with runnersPath', () => {
+    it('renders runner link', () => {
+      factory({
+        propsData: {
+          quotaUsed: 1000,
+          quotaLimit: 4000,
+          projectPath,
+          subscriptionsMoreMinutesUrl,
+        },
+      });
+
+      expect(trimText(wrapper.text())).toContain('For more information, go to the Runners page.');
+    });
+  });
+});
