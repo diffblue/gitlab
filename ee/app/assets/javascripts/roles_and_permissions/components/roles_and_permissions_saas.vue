691ab1366cc2d57ff41b077fc89d4f4052734439
Eduardo Sanz García <esanz-garcia@gitlab.com>
2023-09-12 01:45:08 +0000

    Enable custom role UI for SaaS
    This is the last step to enable the custom role UI for SaaS.
    
    EE: true

---
+++ ee/app/assets/javascripts/roles_and_permissions/components/roles_and_permissions_saas.vue
@@ -0,0 +1,27 @@
+<script>
+import { I18N_EMPTY_TEXT_SAAS } from '../constants';
+import ListMemberRoles from './list_member_roles.vue';
+
+export default {
+  name: 'RolesAndPermissionsSaas',
+
+  components: {
+    ListMemberRoles,
+  },
+  props: {
+    groupId: {
+      type: String,
+      required: true,
+    },
+  },
+  i18n: {
+    emptyText: I18N_EMPTY_TEXT_SAAS,
+  },
+};
+</script>
+
+<template>
+  <div class="col gl-mt-5">
+    <list-member-roles :group-id="groupId" :empty-text="$options.i18n.emptyText" />
+  </div>
+</template>
