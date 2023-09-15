5b1bbb1373cee0a8b5bf77d78be68df671b62844
Pavel Shutsin <pshutsin@gitlab.com>
2023-09-15 09:56:21 +0000

    Cleanup JSON parsing for TanukiBot responses

--- ee/app/assets/javascripts/ai/tanuki_bot/store/actions.js
+++ ee/app/assets/javascripts/ai/tanuki_bot/store/actions.js
@@ -9,31 +9,12 @@ const proxyMessageContent = (message, propsToUpdate) => {
   };
 };
 
-const parseRawContent = (msgContent) => {
-  try {
-    const parsedContent = JSON.parse(msgContent);
-
-    // JSON.parse does not throw when parsing Strings that are coerced to primary data types, like Number or Boolean.
-    // JSON.parse("1") would return 1 (Number), same as JSON.parse("false"), which returns false (Boolean).
-    // We therefore check if the parsed response is really an Object.
-    // This will be solved with https://gitlab.com/gitlab-org/gitlab/-/issues/423315 when we no longer receive
-    // potential JSON as a string.
-    if (typeof parsedContent === 'object') {
-      return parsedContent.content;
-    }
-  } catch {
-    // no-op
-  }
-  return msgContent;
-};
-
 export const addDuoChatMessage = async ({ commit }, messageData = { content: '' }) => {
   const { errors = [], content = '' } = messageData || {};
   const msgContent = content || errors.join('; ');
 
   if (msgContent) {
-    const parsedContent = parseRawContent(msgContent);
-    const message = proxyMessageContent(messageData, { content: parsedContent });
+    const message = proxyMessageContent(messageData, { content: msgContent });
 
     if (message.role.toLowerCase() === GENIE_CHAT_MODEL_ROLES.system) {
       commit(types.ADD_TOOL_MESSAGE, message);
