b622939b54930f5401599eeee62867780f9da3b3
Dheeraj Joshi <djoshi@gitlab.com>
2023-09-15 16:53:22 +0000

    Add toggle for continuous vulnerability scans
    This add the feature to enable continuous vulnerability
    scans in Security Configuration page.
    
    This change is behind a feature flag.

--- ee/app/controllers/ee/projects/security/configuration_controller.rb
+++ ee/app/controllers/ee/projects/security/configuration_controller.rb
@@ -14,6 +14,7 @@ module EE
 
           before_action only: [:show] do
             push_frontend_feature_flag(:security_auto_fix, project)
+            push_frontend_feature_flag(:dependency_scanning_on_advisory_ingestion, project)
           end
 
           before_action only: [:auto_fix] do
