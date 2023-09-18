247a17887a40baf2fc0d8f60dde27f6132d86046
Robert Hunt <rhunt@gitlab.com>
2023-09-18 10:38:39 +0100

    Add `value` type to visualization designers yAxis definition
    Signed-off-by: Robert Hunt <rhunt@gitlab.com>

--- ee/spec/frontend/analytics/analytics_dashboards/utils/visualization_panel_options_spec.js
+++ ee/spec/frontend/analytics/analytics_dashboards/utils/visualization_panel_options_spec.js
@@ -6,9 +6,9 @@ describe('getPanelOptions', () => {
     visualizationType | hasTimeDimension | expectedResult
     ${'DataTable'}    | ${false}         | ${{}}
     ${'SingleStat'}   | ${false}         | ${{}}
-    ${'LineChart'}    | ${false}         | ${{ xAxis: { name: __('Time'), type: 'time' }, yAxis: { name: __('Counts') } }}
-    ${'ColumnChart'}  | ${false}         | ${{ xAxis: { name: __('Dimension'), type: 'category' }, yAxis: { name: __('Counts') } }}
-    ${'ColumnChart'}  | ${true}          | ${{ xAxis: { name: __('Time'), type: 'time' }, yAxis: { name: __('Counts') } }}
+    ${'LineChart'}    | ${false}         | ${{ xAxis: { name: __('Time'), type: 'time' }, yAxis: { name: __('Counts'), type: 'value' } }}
+    ${'ColumnChart'}  | ${false}         | ${{ xAxis: { name: __('Dimension'), type: 'category' }, yAxis: { name: __('Counts'), type: 'value' } }}
+    ${'ColumnChart'}  | ${true}          | ${{ xAxis: { name: __('Time'), type: 'time' }, yAxis: { name: __('Counts'), type: 'value' } }}
   `(
     `with the visualization type $visualizationType and the time dimension is $hasTimeDimension it should return the correct options`,
     ({ visualizationType, hasTimeDimension, expectedResult }) => {
