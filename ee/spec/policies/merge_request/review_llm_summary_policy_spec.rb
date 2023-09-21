fbaf8dd0da82fe41ae0792df8e225e41b9dccf99
Kerri Miller <kerrizor@kerrizor.com>
2023-09-21 07:14:34 -0500

    Refactor MergeRequest::ReviewLlmSummaryPolicy spec
    While testing another MR, I noticed this old spec that was creating extraneous
    merge_request_diff records. This caused problems in that other MR due to the
    created diffs not being backed by actual records (needed when creating patch_id shas
    for fingerprinting diffs.) Removing the extraneous record creation and using the
    merge_request_diff that is created as part of the MR itself (as the newly created
    record has additional information beyond that of the original merge_request_diff)
    allows these specs to continue to pass while improving their overall performance by
    reducing the number of queries executed.

--- ee/spec/policies/merge_request/review_llm_summary_policy_spec.rb
+++ ee/spec/policies/merge_request/review_llm_summary_policy_spec.rb
@@ -6,8 +6,9 @@ RSpec.describe MergeRequest::ReviewLlmSummaryPolicy, feature_category: :code_rev
   let_it_be_with_reload(:project) { create(:project) }
   let_it_be(:user) { create(:user) }
   let_it_be(:merge_request) { create(:merge_request, source_project: project, target_project: project) }
-  let_it_be(:mr_diff) { create(:merge_request_diff, merge_request: merge_request) }
-  let_it_be(:mr_review_summary) { create(:merge_request_review_llm_summary, merge_request_diff: mr_diff) }
+  let_it_be(:mr_review_summary) do
+    create(:merge_request_review_llm_summary, merge_request_diff: merge_request.merge_request_diff)
+  end
 
   subject(:policy) { described_class.new(user, mr_review_summary) }
 
