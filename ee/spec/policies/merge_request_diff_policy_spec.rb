41bf58e55a06f54b6e43fc65a181af7e8f474651
Kerri Miller <kerrizor@kerrizor.com>
2023-09-25 12:01:50 -0400

    Remove extraneous MergeRequestDiff creation
    While testing another MR, I noticed this old spec that was creating
    extraneous merge_request_diff records. This caused problems in that
    other MR due to the created diffs not being backed by actual repository
    entries (needed when creating patch_id shas for fingerprinting diffs.)
    Removing the extraneous record creation and using the
    merge_request_diff that is created as part of the MR itself (as the
    newly created record has additional information beyond that of the
    original merge_request_diff) allows these specs to continue to pass
    while improving their overall performance by reducing the number of
    queries executed.

--- ee/spec/policies/merge_request_diff_policy_spec.rb
+++ ee/spec/policies/merge_request_diff_policy_spec.rb
@@ -6,9 +6,8 @@ RSpec.describe MergeRequestDiffPolicy, feature_category: :code_review_workflow d
   let_it_be_with_reload(:project) { create(:project) }
   let_it_be(:user) { create(:user) }
   let_it_be(:merge_request) { create(:merge_request, source_project: project, target_project: project) }
-  let_it_be(:mr_diff) { create(:merge_request_diff, merge_request: merge_request) }
 
-  subject(:policy) { described_class.new(user, mr_diff) }
+  subject(:policy) { described_class.new(user, merge_request.merge_request_diff) }
 
   context 'when user is not permitted to read merge request' do
     it { is_expected.to be_disallowed(:read_merge_request) }
