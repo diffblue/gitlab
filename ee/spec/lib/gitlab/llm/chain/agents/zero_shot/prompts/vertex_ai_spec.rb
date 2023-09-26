85b479cf219f8212fb75a143c48455c004733eac
Eulyeon Ko <5961404-euko@users.noreply.gitlab.com>
2023-09-26 13:32:22 +0000

    Wrap injected code blob inside a XML tag
    Using a XML tag helps LLM to recognize
    the tag's content as a distinct section within a prompt.
    
    For code-related questions,
    the practice may help Anthropic Claude avoid
    choosing wrong tools based on the content of the code.
    
    For example, the code may reference "GitLab" in its comments.
    Without using a tag, there's a chance the injected code blob
    pollutes the overall context of a prompt.

--- ee/spec/lib/gitlab/llm/chain/agents/zero_shot/prompts/vertex_ai_spec.rb
+++ ee/spec/lib/gitlab/llm/chain/agents/zero_shot/prompts/vertex_ai_spec.rb
@@ -3,6 +3,8 @@
 require 'spec_helper'
 
 RSpec.describe Gitlab::Llm::Chain::Agents::ZeroShot::Prompts::VertexAi, feature_category: :duo_chat do
+  include FakeBlobHelpers
+
   describe '.prompt' do
     it 'returns prompt' do
       options = {
@@ -21,6 +23,18 @@ RSpec.describe Gitlab::Llm::Chain::Agents::ZeroShot::Prompts::VertexAi, feature_
       expect(prompt).to include('tool names')
       expect(prompt).to include(prompt_text)
       expect(prompt).to include(Gitlab::Llm::Chain::Utils::Prompt.default_system_prompt)
+    end
+  end
+
+  describe '.current_code_prompt' do
+    let(:project) { build(:project) }
+    let(:blob) { fake_blob(path: 'foobar.rb', data: 'puts "hello world"') }
+
+    subject(:prompt) { described_class.current_code_prompt(blob) }
+
+    it 'returns the base prompt' do
+      expect(prompt).to include("The current code file that user sees is foobar.rb and")
+      expect(prompt).to include("has the following content:\nputs \"hello world\"\n")
     end
   end
 end
